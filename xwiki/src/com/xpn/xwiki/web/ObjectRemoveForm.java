7ca5bd71cdb44e235c95a62fa322ee1540172e1b
ldubost <ludovic@xwiki.com>
2004-03-02 12:28:15 +0000

    More fixes linked to the API Implemented remove object Implemented RCS Attachment storage All tests should now pass
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/trunk@174 f329d543-caf0-0310-9063-dda96c69346f

---
+++ xwiki/src/com/xpn/xwiki/web/ObjectRemoveForm.java
@@ -0,0 +1,45 @@
+/**
+ * ===================================================================
+ *
+ * Copyright (c) 2003,2004 Ludovic Dubost, All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details, published at 
+ * http://www.gnu.org/copyleft/lesser.html or in lesser.txt in the
+ * root folder of this distribution.
+
+ * Created by
+ * User: Ludovic Dubost
+ * Date: 1 mars 2004
+ * Time: 15:19:19
+ */
+package com.xpn.xwiki.web;
+
+import org.apache.struts.action.ActionMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class ObjectRemoveForm extends ObjectAddForm {
+    private int classId;
+
+    public void reset(ActionMapping actionMapping, HttpServletRequest httpServletRequest) {
+        super.reset(actionMapping, httpServletRequest);
+        setClassId(Integer.parseInt(httpServletRequest.getParameter("classid")));
+    }
+
+    public int getClassId() {
+        return classId;
+    }
+
+    public void setClassId(int classId) {
+        this.classId = classId;
+    }
+
+}

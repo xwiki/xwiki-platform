424fb600f25a60fd812943ee4779d8f427855316
ldubost <ludovic@xwiki.com>
2004-03-21 20:53:52 +0000

    * alternate stylesheets (blue, bordeaux, sahara) * system to switch stylsheet using javascript
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/trunk@221 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki/src/web/skins/default/skin.js
+++ xwiki/src/web/skins/default/skin.js
@@ -1,127 +1,69 @@
 
-//
-// Store the Query Mode mode
-//
-var currentQueryMode = 0;
+// Thanks to the great article from Paul Sowden
+// http://www.alistapart.com/articles/alternate/
+// No copyright on this file.
 
-//
-// Show/Hide the Query Menu 
-//
-function switchQueryMenu() {
- var list = document.body.getElementsByTagName("div")["queries"].getElementsByTagName("a");
- for (var i=0;i<list.length;i++) {
-  list[i].style.display = (!currentQueryMode) ? "block" : "none";
- }
- currentQueryMode = !currentQueryMode;
- return false;
-}
-
-var currentAdminMode = 0;
-var currentContentRight;
-
-//
-// Show/Hide the Admin Menu
-//
-function switchAdmin() {
- var list = document.body.getElementsByTagName("div")["admin"].getElementsByTagName("a");
- for (var i=0;i<list.length;i++) {
-  list[i].style.display = (!currentAdminMode) ? "block" : "none";
- }
- currentAdminMode = !currentAdminMode;
- return false;
-}
-
-//
-// Store the current View/Edit/Search mode (normal/advanced) 
-//
-var currentMode = 0;
-var currentViewEditMode = 0;
-
-function switchMode(mode, mode1, mode2) {
- var mode1 = document.body.getElementsByTagName("div")[mode1];
- var mode2 = document.body.getElementsByTagName("div")[mode2];
- if (mode1) mode1.style.display =  (mode == 0) ? "block" : "none"; 
- if (mode2) mode2.style.display =  (mode == 1) ? "block" : "none"; 
- return (1 - mode);
-}
-
-function switchCreateMode() {
- currentMode = switchMode(currentMode,null,"advanced");
-}
-
-function switchShowViewMode() {
- currentMode = switchMode(currentMode,null,"viewadv");
-}
-
-function switchShowEditMode() {
- currentMode = switchMode(currentMode,null,"editadv");
-}
-
-function switchSearchMode() {
- var knob = document.body.getElementsByTagName("div").knob;
- var bchart = document.body.getElementsByTagName("div").bchart;
-
- currentMode = switchMode(currentMode,null,"advanced");
- if (knob) knob.style.display =  (currentMode == 0) ? "block" : "none"; 
- if (bchart) bchart.style.display =  (currentMode == 0) ? "block" : "none"; 
-}
-
-function switchTabs(mode) {
- if (currentViewEditMode != mode) {   
-  currentViewEditMode = switchMode(currentViewEditMode,"show_edit","show_view");
-  var mode = (currentViewEditMode) ? "show_view" : "show_edit";
-  var adv = document.body.getElementsByTagName("div")[mode + "adv"];
-  if (adv) adv.style.display = (currentMode) ? "block" : "none";
- }   
-}
-
-function showHideComment(name) {
-  var mydiv = document.getElementById(name);
-  if (mydiv.style.display == "none")
-   mydiv.style.display = "block";
-  else
-   mydiv.style.display = "none";
- }
-
-function showAllComments() {
- var count = 0;
-
- if (document.getElementById("allbutlast") != null)
-  document.getElementById("allbutlast").style.display = "block";
- if (document.getElementById("last") != null)
-  document.getElementById("last").style.display = "block";
-
- while (count<1000) {
-  var mydiv = document.getElementById("dc" + count);
-  if (mydiv != null ) {
-   mydiv.style.display = "block";
-  } 
-  else {
-   return;
+function setActiveStyleSheet(title) {
+  var i, a, main;
+  for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
+    if(a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("title")) {
+      a.disabled = true;
+      if(a.getAttribute("title") == title) a.disabled = false;
+    }
   }
-  count++;
- }
 }
 
-function hideAllComments() {
- var count = 0;
-
- if (document.getElementById("dc0") != null)
-  document.getElementById("dc0").style.display = "none";
- if (document.getElementById("allbutlast") != null)
-  document.getElementById("allbutlast").style.display = "none";
- if (document.getElementById("last") != null)
-  document.getElementById("last").style.display = "none";
-
- while (1) {
-  var mydiv = document.getElementById("dc" + count);
-  if (mydiv != null ) {
-   mydiv.style.display = "none";
-  } 
-  else {
-   return;
+function getActiveStyleSheet() {
+  var i, a;
+  for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
+    if(a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("title") && !a.disabled) return a.getAttribute("title");
   }
-  count++;
- }
+  return null;
 }
 
+function getPreferredStyleSheet() {
+  var i, a;
+  for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
+    if(a.getAttribute("rel").indexOf("style") != -1
+       && a.getAttribute("rel").indexOf("alt") == -1
+       && a.getAttribute("title")
+       ) return a.getAttribute("title");
+  }
+  return null;
+}
+
+function createCookie(name,value,days) {
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime()+(days*24*60*60*1000));
+    var expires = "; expires="+date.toGMTString();
+  }
+  else expires = "";
+  document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+    var c = ca[i];
+    while (c.charAt(0)==' ') c = c.substring(1,c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+
+window.onload = function(e) {
+  var cookie = readCookie("style");
+  var title = cookie ? cookie : getPreferredStyleSheet();
+  setActiveStyleSheet(title);
+}
+
+window.onunload = function(e) {
+  var title = getActiveStyleSheet();
+  createCookie("style", title, 365);
+}
+
+var cookie = readCookie("style");
+var title = cookie ? cookie : getPreferredStyleSheet();
+setActiveStyleSheet(title);

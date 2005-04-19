8b82c9aaef6906df3da878eb0cf0b11e1ea3c184
akartmann <akartmann@xwiki.com>
2005-04-19 23:33:02 +0000

    Updated : LDPA Test create all tree structure in LDAP server
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki/trunk@463 f329d543-caf0-0310-9063-dda96c69346f

--- src/com/xpn/xwiki/test/LDAPTest.java
+++ src/com/xpn/xwiki/test/LDAPTest.java
@@ -12,16 +12,32 @@ import com.xpn.xwiki.store.XWikiHibernateStore;
 import com.xpn.xwiki.store.XWikiStoreInterface;
 import com.xpn.xwiki.store.XWikiCacheStoreInterface;
 import com.opensymphony.module.access.provider.osuser.OSUserUserProvider;
+import com.novell.ldap.*;
 import net.sf.hibernate.HibernateException;
 
 import java.security.Principal;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 
 /**
  * Created by IntelliJ IDEA.
  * User: Alex
  * Date: 18 avr. 2005
  * Time: 16:02:41
- * To change this template use File | Settings | File Templates.
+ * Test for LDAP authentication
+ * ***********************************************************************
+ * This test need a TEST local LDAP server
+ * ***********************************************************************
+ * BEWARE : THIS TEST DELETE ALL LDAP SERVER CONTENTS !
+ * NEVER USE IT WITH PRODUCTION SERVER !
+ * ***********************************************************************
+ * If you use OpenLDAP server, the sldap.conf must containts the statements :
+ *       include	/etc/schema/inetorgperson.schema
+ *       suffix		"dc=xwiki,dc=com"
+ *       rootdn		"cn=Manager,dc=xwiki,dc=com"
+ *       rootpw		secret
+ * Other default values should work
+ * Test works with out of the box ldap database.
  */
 public class LDAPTest  extends TestCase {
     private XWiki xwiki;
@@ -35,6 +51,116 @@ public class LDAPTest  extends TestCase {
             return (XWikiHibernateStore)((XWikiCacheStoreInterface)store).getStore();
         else
             return (XWikiHibernateStore) store;
+    }
+
+    public void prepareLDAP()
+    {
+        int ldapPort = context.getWiki().getXWikiPreferenceAsInt("ldap_port", LDAPConnection.DEFAULT_PORT, context);
+        int ldapVersion = LDAPConnection.LDAP_V3;
+        String ldapHost = context.getWiki().getXWikiPreference("ldap_server",context);
+        String loginDN = context.getWiki().getXWikiPreference("ldap_bind_DN",context);
+        String password = context.getWiki().getXWikiPreference("ldap_bind_pass",context);
+
+        String containerName  =  context.getWiki().getXWikiPreference("ldap_base_DN",context);
+
+        try
+        {
+            LDAPConnection lc = new LDAPConnection();
+
+            lc.connect( ldapHost, ldapPort );
+            lc.bind( ldapVersion, loginDN, password.getBytes("UTF8") );
+
+            boolean hasRoot = false;
+            boolean attributeOnly = true;
+            String attrs[] = {LDAPConnection.NO_ATTRS};
+            LDAPSearchResults searchResults =
+                lc.search(  containerName,
+                            LDAPConnection.SCOPE_SUB,
+                            "(objectclass=*)",
+                            attrs,
+                            attributeOnly);
+            if (searchResults.hasMore())
+            {
+                ArrayList l = new ArrayList();
+                // print out all the objects
+                while ( searchResults.hasMore()) {
+                    LDAPEntry nextEntry = null;
+                    try {
+                        nextEntry = searchResults.next();
+                    }
+                    catch(LDAPException e) {
+                        System.out.println("Error: " + e.toString());
+                        continue;
+                    }
+                    hasRoot = true;
+                    l.add(nextEntry.getDN());
+                }
+
+                // Don't delete root
+                for(int i=l.size(); --i>0;)
+                {
+                    String dn = (String)l.get(i);
+                    lc.delete(dn);
+                }
+            }
+
+            LDAPAttribute  attribute = null;
+            LDAPEntry newEntry = null;
+            LDAPAttributeSet attributeSet = new LDAPAttributeSet();
+            String  dn;
+            if (!hasRoot)
+            {
+                // Root creation
+                attributeSet.add( new LDAPAttribute(
+                                     "dc", new String("xwiki")));
+                attributeSet.add( new LDAPAttribute(
+                                     "o", new String("XWiki")));
+                attributeSet.add( new LDAPAttribute("objectClass",
+                        new String[]{"dcObject", "organization", "top"}));
+                dn  = containerName;
+                newEntry = new LDAPEntry( dn, attributeSet );
+
+                lc.add( newEntry );
+            }
+
+            // Add manager branch
+            attributeSet = new LDAPAttributeSet();
+            attributeSet.add( new LDAPAttribute(
+                                 "cn", new String("Manager")));
+            attributeSet.add( new LDAPAttribute("objectClass",
+                    new String[]{"organizationalRole", "top"}));
+            dn  = "cn=Manager," + containerName;
+            newEntry = new LDAPEntry( dn, attributeSet );
+            lc.add( newEntry );
+
+            attributeSet = new LDAPAttributeSet();
+            attributeSet.add( new LDAPAttribute(
+                                 "cn", new String("akartmann")));
+            attributeSet.add( new LDAPAttribute(
+                                 "uid", new String("akartmann")));
+            attributeSet.add( new LDAPAttribute(
+                                 "sn", new String("KARTMANN")));
+            attributeSet.add( new LDAPAttribute(
+                                 "displayName", new String("Alexis KARTMANN")));
+            attributeSet.add( new LDAPAttribute(
+                                 "mail", new String("alexis@xwiki.com")));
+            attributeSet.add( new LDAPAttribute(
+                                 "userPassword", new String("alexis")));
+            attributeSet.add( new LDAPAttribute("objectClass",
+                    new String[]{"inetOrgPerson", "top", "uidObject"}));
+            dn  = "cn=akartmann,cn=Manager," + containerName;
+            newEntry = new LDAPEntry( dn, attributeSet );
+            lc.add( newEntry );
+
+
+            lc.disconnect();
+        }
+        catch( LDAPException e ) {
+            System.out.println( "Error:  " + e.toString());
+        }
+        catch( UnsupportedEncodingException e ) {
+            System.out.println( "Error: " + e.toString() );
+        }
     }
 
     public void prepareData() throws XWikiException {
@@ -74,6 +200,13 @@ public class LDAPTest  extends TestCase {
         context.setWiki(xwiki);
         StoreHibernateTest.cleanUp(getHibStore(), context);
         xwiki.flushCache();
+
+        Utils.setStringValue("XWiki.XWikiPreferences", "XWiki.XWikiPreferences", "ldap_server", "localhost", context);
+        Utils.setStringValue("XWiki.XWikiPreferences", "XWiki.XWikiPreferences", "ldap_port", "389", context);
+        Utils.setStringValue("XWiki.XWikiPreferences", "XWiki.XWikiPreferences", "ldap_base_DN", "dc=xwiki,dc=com", context);
+        Utils.setStringValue("XWiki.XWikiPreferences", "XWiki.XWikiPreferences", "ldap_bind_DN", "cn=Manager,dc=xwiki,dc=com", context);
+        Utils.setStringValue("XWiki.XWikiPreferences", "XWiki.XWikiPreferences", "ldap_bind_pass", "secret", context);
+        Utils.setStringValue("XWiki.XWikiPreferences", "XWiki.XWikiPreferences", "ldap_UID_attr", "uid", context);
     }
 
     public void tearDown() throws HibernateException {
@@ -85,13 +218,8 @@ public class LDAPTest  extends TestCase {
 
     public void testCheckLogon() throws ClassNotFoundException, IllegalAccessException, InstantiationException, XWikiException {
 
+        prepareLDAP();
         prepareData();
-        Utils.setStringValue("XWiki.XWikiPreferences", "XWiki.XWikiPreferences", "ldap_server", "localhost", context);
-        Utils.setStringValue("XWiki.XWikiPreferences", "XWiki.XWikiPreferences", "ldap_port", "389", context);
-        Utils.setStringValue("XWiki.XWikiPreferences", "XWiki.XWikiPreferences", "ldap_base_DN", "dc=xwiki,dc=com", context);
-        Utils.setStringValue("XWiki.XWikiPreferences", "XWiki.XWikiPreferences", "ldap_bind_DN", "cn=Manager,dc=xwiki,dc=com", context);
-        Utils.setStringValue("XWiki.XWikiPreferences", "XWiki.XWikiPreferences", "ldap_bind_pass", "secret", context);
-        Utils.setStringValue("XWiki.XWikiPreferences", "XWiki.XWikiPreferences", "ldap_UID_attr", "uid", context);
 
         XWikiAuthService service =  (XWikiAuthService) Class.forName("com.xpn.xwiki.user.impl.LDAP.LDAPAuthServiceImpl").newInstance();
         Principal principal = service.authenticate("akartmann", "alexis", context);

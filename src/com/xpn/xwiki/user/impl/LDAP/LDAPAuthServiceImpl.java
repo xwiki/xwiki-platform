4590146a44e0456d452da591fd45131eaa60e2e8
akartmann <akartmann@xwiki.com>
2005-04-20 15:54:27 +0000

    Added : Mixed LDAP/XWiki login mode Updated : LDAP Bind with user or anonymous
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki/trunk@467 f329d543-caf0-0310-9063-dda96c69346f

--- src/com/xpn/xwiki/user/impl/LDAP/LDAPAuthServiceImpl.java
+++ src/com/xpn/xwiki/user/impl/LDAP/LDAPAuthServiceImpl.java
@@ -20,6 +20,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.security.Principal;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 /**
@@ -29,157 +30,8 @@ import java.util.List;
  * Time: 16:18:50
  * To change this template use File | Settings | File Templates.
  */
-public class LDAPAuthServiceImpl implements XWikiAuthService {
+public class LDAPAuthServiceImpl extends XWikiAuthServiceImpl {
     private static final Log log = LogFactory.getLog(LDAPAuthServiceImpl.class);
-
-    protected Authenticator authenticator;
-
-    protected Authenticator getAuthenticator(XWikiContext context) throws XWikiException {
-        if (authenticator!=null)
-            return authenticator;
-
-        try {
-            XWiki xwiki = context.getWiki();
-
-            if ("basic".equals(xwiki.Param("xwiki.authentication"))) {
-                authenticator = new MyBasicAuthenticator();
-                SecurityConfig sconfig = new SecurityConfig(false);
-                sconfig.setAuthMethod("BASIC");
-                if (xwiki.Param("xwiki.authentication.realname")!=null)
-                    sconfig.setRealmName(xwiki.Param("xwiki.authentication.realname"));
-                else
-                    sconfig.setRealmName("XWiki");
-                authenticator.init(null, sconfig);
-            } else {
-                authenticator =  new MyFormAuthenticator();
-                SecurityConfig sconfig = new SecurityConfig(false);
-                sconfig.setAuthMethod("FORM");
-                if (xwiki.Param("xwiki.authentication.realname")!=null)
-                    sconfig.setRealmName(xwiki.Param("xwiki.authentication.realname"));
-                else
-                    sconfig.setRealmName("XWiki");
-                if (xwiki.Param("xwiki.authentication.defaultpage")!=null)
-                    sconfig.setDefaultPage(xwiki.Param("xwiki.authentication.defaultpage"));
-                else
-                    sconfig.setDefaultPage("/bin/view/Main/WebHome");
-                if (xwiki.Param("xwiki.authentication.loginpage")!=null)
-                    sconfig.setLoginPage(xwiki.Param("xwiki.authentication.loginpage"));
-                else
-                    sconfig.setLoginPage("/bin/login/XWiki/XWikiLogin");
-                if (xwiki.Param("xwiki.authentication.logoutpage")!=null)
-                    sconfig.setLogoutPage(xwiki.Param("xwiki.authentication.logoutpage"));
-                else
-                    sconfig.setLogoutPage("/bin/logout/XWiki/XWikiLogout");
-                if (xwiki.Param("xwiki.authentication.errorpage")!=null)
-                    sconfig.setErrorPage(xwiki.Param("xwiki.authentication.errorpage"));
-                else
-                    sconfig.setErrorPage("/bin/loginerror/XWiki/XWikiLogin");
-
-
-                MyPersistentLoginManager persistent = new MyPersistentLoginManager();
-                if (xwiki.Param("xwiki.authentication.cookiepath")!=null)
-                    persistent.setCookiePath(xwiki.Param("xwiki.authentication.cookiepath"));
-                if (xwiki.Param("xwiki.authentication.cookiedomains")!=null) {
-                    String[] cdomains = StringUtils.split(xwiki.Param("xwiki.authentication.cookiedomains"),",");
-                    persistent.setCookieDomains(cdomains);
-                }
-                if (xwiki.Param("xwiki.authentication.cookielife")!=null)
-                    persistent.setCookieLife(xwiki.Param("xwiki.authentication.cookielife"));
-                if (xwiki.Param("xwiki.authentication.protection")!=null)
-                    persistent.setProtection(xwiki.Param("xwiki.authentication.protection"));
-                if (xwiki.Param("xwiki.authentication.useip")!=null)
-                    persistent.setUseIP(xwiki.Param("xwiki.authentication.useip"));
-                if (xwiki.Param("xwiki.authentication.encryptionalgorithm")!=null)
-                    persistent.setEncryptionAlgorithm(xwiki.Param("xwiki.authentication.encryptionalgorithm"));
-                if (xwiki.Param("xwiki.authentication.encryptionmode")!=null)
-                    persistent.setEncryptionMode(xwiki.Param("xwiki.authentication.encryptionmode"));
-                if (xwiki.Param("xwiki.authentication.encryptionpadding")!=null)
-                    persistent.setEncryptionPadding(xwiki.Param("xwiki.authentication.encryptionpadding"));
-                if (xwiki.Param("xwiki.authentication.validationKey")!=null)
-                    persistent.setValidationKey(xwiki.Param("xwiki.authentication.validationKey"));
-                if (xwiki.Param("xwiki.authentication.encryptionKey")!=null)
-                    persistent.setEncryptionKey(xwiki.Param("xwiki.authentication.encryptionKey"));
-                sconfig.setPersistentLoginManager(persistent);
-
-                MyFilterConfig fconfig = new MyFilterConfig();
-                if (xwiki.Param("xwiki.authentication.loginsubmitpage")!=null)
-                    fconfig.setInitParameter("loginSubmitPattern", xwiki.Param("xwiki.authentication.loginsubmitpage"));
-                else
-                    fconfig.setInitParameter("loginSubmitPattern", "/login/XWiki/XWikiLogin");
-                authenticator.init(fconfig, sconfig);
-            }
-
-            return authenticator;
-        } catch (Exception e) {
-            throw new XWikiException(XWikiException.MODULE_XWIKI_USER,
-                    XWikiException.ERROR_XWIKI_USER_INIT,
-                    "Cannot initialize authentication system",e);
-        }
-    }
-
-    public XWikiUser checkAuth(XWikiContext context) throws XWikiException {
-        XWiki xwiki = context.getWiki();
-        HttpServletRequest request = null;
-        HttpServletResponse response = null;
-
-        if (context.getRequest()!=null)
-         request = context.getRequest().getHttpServletRequest();
-
-        if (context.getResponse()!=null)
-         response = context.getResponse().getHttpServletResponse();
-
-        if (request==null)
-            return null;
-
-        Authenticator auth = getAuthenticator(context);
-        SecurityRequestWrapper wrappedRequest = new SecurityRequestWrapper(request, null,
-                null, auth.getAuthMethod());
-        try {
-
-            // Process login out (this only works with FORMS
-            if (auth.processLogout(wrappedRequest, response, xwiki.getUrlPatternMatcher())) {
-                if (log.isInfoEnabled()) log.info("User " + context.getUser() + " has been logged-out");
-                wrappedRequest.setUserPrincipal(null);
-                return null;
-            }
-
-            if (auth.getAuthMethod().equals("BASIC")) {
-                if (((MyBasicAuthenticator)auth).processLogin(wrappedRequest, response, context)) {
-                    return null;
-                }
-            } else {
-                if (((MyFormAuthenticator)auth).processLogin(wrappedRequest, response, context)) {
-                    return null;
-                }
-            }
-
-           Principal user = wrappedRequest.getUserPrincipal();
-           if (log.isInfoEnabled()) {
-               if (user!=null)
-                   log.info("User " + user.getName() + " is authentified");
-           }
-
-           if (user==null)
-            return null;
-           else
-            return new XWikiUser(user.getName());
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    public void showLogin(XWikiContext context) throws XWikiException {
-        try {
-            if (context.getMode()==XWikiContext.MODE_SERVLET) {
-             getAuthenticator(context).showLogin(context.getRequest().getHttpServletRequest(),
-                                         context.getResponse().getHttpServletResponse());
-            }
-        } catch (IOException e) {
-            // If this fails we continue
-            e.printStackTrace();
-        }
-    }
 
     public Principal authenticate(String username, String password, XWikiContext context) throws XWikiException {
         Principal principal = null;
@@ -207,61 +59,105 @@ public class LDAPAuthServiceImpl implements XWikiAuthService {
             if (i!=-1)
                 susername = username.substring(i+1);
 
-           if (checkPassword(susername, password, context))
-           {
-                // First we check in the local database
-                try {
-                    String user = findUser(susername, context);
-                    if (user!=null) {
-                        principal = new SimplePrincipal(user);
-                    }
-                } catch (Exception e) {}
+           String DN = getLDAP_DN(susername, context);
 
-                if (context.isVirtual()) {
-                    if (principal==null) {
-                        // Then we check in the main database
-                        String db = context.getDatabase();
-                        try {
-                            context.setDatabase(context.getWiki().getDatabase());
-                            try {
-                                String user = findUser(susername, context);
-                                if (user==null)
-                                    return null;
-                                principal = new SimplePrincipal(context.getDatabase() + ":" + user);
-                            } catch (Exception e) {}
-                        } finally {
-                            context.setDatabase(db);
-                        }
-                    }
-                }
+           if (DN != null && DN.length()!=0)
+           {
+               if (checkDNPassword(DN, susername, password, context))
+               {
+                   principal = GetUserPrincipal(susername, context);
+               }
            }
+           else
+            {
+               if (checkUserPassword(susername, password, context))
+               {
+                   principal = GetUserPrincipal(susername, context);
+               }
+            }
         }
         return principal;
     }
 
-    private String findUser(String susername, XWikiContext context) throws XWikiException {
-        // First lets look in the cache
-        if (context.getWiki().exists("XWiki."  + susername, context))
-            return "XWiki." + susername;
+    protected Principal GetUserPrincipal(String susername, XWikiContext context) {
+        Principal principal = null;
 
-        String sql = "select distinct doc.web, doc.name from XWikiDocument as doc where doc.web='XWiki' and doc.name like '" + susername + "'";
-        List list = context.getWiki().search(sql, context);
-        if (list.size()==0)
-            return null;
-        if (list.size()>1) {
-            for (int i=0;i<list.size();i++) {
-                Object[] result = (Object[]) list.get(0);
-                if (result[1].equals(susername))
-                    return result[0] + "." + result[1];
+        // First we check in the local database
+        try {
+            String user = findUser(susername, context);
+            if (user!=null) {
+                principal = new SimplePrincipal(user);
+            }
+        } catch (Exception e) {}
+
+        if (context.isVirtual()) {
+            if (principal==null) {
+                // Then we check in the main database
+                String db = context.getDatabase();
+                try {
+                    context.setDatabase(context.getWiki().getDatabase());
+                    try {
+                        String user = findUser(susername, context);
+                        if (user!=null)
+                            principal = new SimplePrincipal(context.getDatabase() + ":" + user);
+                    } catch (Exception e) {}
+                } finally {
+                    context.setDatabase(db);
+                }
             }
         }
-        Object[] result = (Object[]) list.get(0);
-        return result[0] + "." + result[1];
+        return principal;
     }
 
-    protected boolean checkPassword(String username, String password, XWikiContext context) throws XWikiException {
+    public String getLDAP_DN(String susername, XWikiContext context)
+    {
+        String DN=null;
+        if (context!=null) {
+            // First we check in the local database
+            try {
+                String user = findUser(susername, context);
+                if (user!=null && user.length()!=0) {
+                    DN = readLDAP_DN(user, context);
+                }
+            } catch (Exception e) {}
+
+            if (context.isVirtual()) {
+                if (DN==null && DN.length()!=0) {
+                    // Then we check in the main database
+                    String db = context.getDatabase();
+                    try {
+                        context.setDatabase(context.getWiki().getDatabase());
+                        try {
+                            String user = findUser(susername, context);
+                            if (user!=null && user.length()!=0)
+                                DN = readLDAP_DN(user, context);
+                        } catch (Exception e) {}
+                    } finally {
+                        context.setDatabase(db);
+                    }
+                }
+            }
+        }
+        return DN;
+    }
+
+    private String readLDAP_DN(String username, XWikiContext context) {
+        String DN = null;
+        try {
+            XWikiDocument doc = context.getWiki().getDocument(username, context);
+            // We only allow empty password from users having a XWikiUsers object.
+            if (doc.getObject("XWiki.XWikiUsers")!=null) {
+              DN = doc.getStringValue("XWiki.XWikiUsers", "ldap_dn");
+            }
+
+        } catch (Throwable e) {}
+        return DN;
+    }
+
+    protected boolean checkUserPassword(String username, String password, XWikiContext context) throws XWikiException {
         LDAPConnection lc = new LDAPConnection();
         boolean result = false;
+        boolean notinLDAP = false;
 
         try {
 
@@ -270,14 +166,15 @@ public class LDAPAuthServiceImpl implements XWikiAuthService {
             String ldapHost = context.getWiki().getXWikiPreference("ldap_server",context);
             String bindDN = context.getWiki().getXWikiPreference("ldap_bind_DN",context);
             String bindPassword = context.getWiki().getXWikiPreference("ldap_bind_pass",context);
+            String baseDN = context.getWiki().getXWikiPreference("ldap_base_DN",context);
 
             lc.connect( ldapHost, ldapPort );
 
             // authenticate to the server
-            lc.bind( ldapVersion, bindDN, bindPassword.getBytes("UTF8") );
+            Bind(bindDN, bindPassword, "", "", lc, ldapVersion);
 
             LDAPSearchResults searchResults =
-                lc.search(  context.getWiki().getXWikiPreference("ldap_base_DN",context),
+                lc.search(  baseDN,
                             LDAPConnection.SCOPE_SUB ,
                             "("+ context.getWiki().getXWikiPreference("ldap_UID_attr",context) +
                                "=" + username + ")",
@@ -294,6 +191,8 @@ public class LDAPAuthServiceImpl implements XWikiAuthService {
                                                 "userPassword", password );
                 result = lc.compare( objectDN, attr );
             }
+            else
+                notinLDAP = true;
 
             if (log.isDebugEnabled()) {
                 if (result)
@@ -301,7 +200,16 @@ public class LDAPAuthServiceImpl implements XWikiAuthService {
                 else
                  log.debug("(debug) Password check for user " + username + " failed");
             }
-        } catch (Throwable e) {
+        }
+        catch( LDAPException e ) {
+            if ( e.getResultCode() == LDAPException.NO_SUCH_OBJECT ) {
+                notinLDAP = true;
+            } else if ( e.getResultCode() ==
+                                        LDAPException.NO_SUCH_ATTRIBUTE ) {
+                notinLDAP = true;
+            }
+        }
+        catch (Throwable e) {
             e.printStackTrace();
         }
         finally
@@ -312,6 +220,93 @@ public class LDAPAuthServiceImpl implements XWikiAuthService {
                 e.printStackTrace();
             }
         }
+        if (notinLDAP)
+        {
+            // Use XWiki password if user not in LDAP
+            result = checkPassword(username, password, context);
+        }
+
         return result;
+    }
+
+    protected boolean checkDNPassword(String DN, String username, String password, XWikiContext context) throws XWikiException {
+        LDAPConnection lc = new LDAPConnection();
+        boolean result = false;
+        boolean notinLDAP = false;
+        try {
+
+            int ldapPort = context.getWiki().getXWikiPreferenceAsInt("ldap_port", LDAPConnection.DEFAULT_PORT, context);
+            int ldapVersion = LDAPConnection.LDAP_V3;
+            String ldapHost = context.getWiki().getXWikiPreference("ldap_server",context);
+            String bindDN = context.getWiki().getXWikiPreference("ldap_bind_DN",context);
+            String bindPassword = context.getWiki().getXWikiPreference("ldap_bind_pass",context);
+            String baseDN = context.getWiki().getXWikiPreference("ldap_base_DN",context);
+
+            lc.connect( ldapHost, ldapPort );
+
+            // authenticate to the server
+            Bind(bindDN, bindPassword, DN, password, lc, ldapVersion);
+
+            LDAPAttribute attr = new LDAPAttribute(
+                                                "userPassword", password );
+            result = lc.compare( DN, attr );
+
+            if (log.isDebugEnabled()) {
+                if (result)
+                 log.debug("(debug) Password check for user " + DN + " successfull");
+                else
+                 log.debug("(debug) Password check for user " + DN + " failed");
+            }
+        }
+        catch( LDAPException e ) {
+            if ( e.getResultCode() == LDAPException.NO_SUCH_OBJECT ) {
+                notinLDAP = true;
+            } else if ( e.getResultCode() ==
+                                        LDAPException.NO_SUCH_ATTRIBUTE ) {
+                notinLDAP = true;
+            }
+        }
+        catch (Throwable e) {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try {
+                lc.disconnect();
+            } catch (LDAPException e) {
+                e.printStackTrace();
+            }
+        }
+        if (notinLDAP)
+        {
+            // Use XWiki password if user not in LDAP
+            result = checkPassword(username, password, context);
+        }
+        return result;
+    }
+
+    private boolean Bind(String bindDN, String bindPassword, String userDN, String userPassword, LDAPConnection lc, int ldapVersion) throws UnsupportedEncodingException {
+        boolean bound = false;
+        if (bindDN != null && bindDN.length() > 0 && bindPassword != null)
+        {
+            try
+            {
+                lc.bind( ldapVersion, bindDN, bindPassword.getBytes("UTF8") );
+                bound = true;
+            }
+            catch(LDAPException e) { };
+        }
+
+        if (!bound)
+        {
+            try
+            {
+                // Anonymously connect if no bind info is not set or incorrect
+                lc.bind( ldapVersion, userDN, userPassword.getBytes("UTF8") );
+                bound = true;
+            }
+            catch(LDAPException e) { };
+        }
+        return bound;
     }
 }

3b9f4ba3326da7bf489cc79ea5736f2d416ba8a0
namphunghai <namphunghai@xwiki.com>
2006-03-03 02:07:14 +0000

    Add DownloadRevAction.java and ViewAttachRevAction.java
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki/trunk@959 f329d543-caf0-0310-9063-dda96c69346f

---
+++ src/main/java/com/xpn/xwiki/web/DownloadRevAction.java
@@ -0,0 +1,69 @@
+package com.xpn.xwiki.web;
+
+import java.io.IOException;
+
+import com.xpn.xwiki.XWikiContext;
+import com.xpn.xwiki.XWikiException;
+import com.xpn.xwiki.doc.XWikiAttachment;
+import com.xpn.xwiki.doc.XWikiDocument;
+import com.xpn.xwiki.plugin.XWikiPluginManager;
+
+public class DownloadRevAction extends XWikiAction {
+    public String render(XWikiContext context) throws XWikiException {
+        XWikiRequest request = context.getRequest();
+        XWikiResponse response = context.getResponse();
+        XWikiDocument doc = context.getDoc();
+        String rev = request.getParameter("rev");
+        String path = request.getRequestURI();
+        String filename = Utils.decode(path.substring(path.lastIndexOf("/")+1),context);
+        XWikiAttachment attachment = null;
+
+        if (request.getParameter("id")!=null) {
+            int id = Integer.parseInt(request.getParameter("id"));
+            attachment = (XWikiAttachment) doc.getAttachmentList().get(id);
+        }
+        else {
+            attachment = doc.getAttachment(filename);
+        }
+
+        synchronized (attachment) {
+            try {
+                attachment = attachment.getAttachmentRevision(rev, context);
+            } catch(Exception e){
+                String url = context.getDoc().getURL("viewattachrev", true, context);
+                try {
+                    context.getResponse().sendRedirect(url+"/" + filename);
+                } catch (IOException ioe) {
+                    ioe.printStackTrace();
+                }
+            }
+        }
+
+        if (attachment==null) {
+            Object[] args = { filename };
+            throw new XWikiException(XWikiException.MODULE_XWIKI_APP,
+                    XWikiException.ERROR_XWIKI_APP_ATTACHMENT_NOT_FOUND,
+                    "Attachment {0} not found", null, args);
+        }
+
+	XWikiPluginManager plugins = context.getWiki().getPluginManager();
+	attachment = plugins.downloadAttachment(attachment, context);
+
+        // Choose the right content type
+        String mimetype = attachment.getMimeType(context);
+        response.setContentType(mimetype);
+
+        response.setDateHeader("Last-Modified", attachment.getDate().getTime());
+        // Sending the content of the attachment
+        byte[] data = attachment.getContent(context);
+        response.setContentLength(data.length);
+        try {
+            response.getOutputStream().write(data);
+        } catch (IOException e) {
+            throw new XWikiException(XWikiException.MODULE_XWIKI_APP,
+                    XWikiException.ERROR_XWIKI_APP_SEND_RESPONSE_EXCEPTION,
+                    "Exception while sending response", e);
+        }
+        return null;
+	}
+}

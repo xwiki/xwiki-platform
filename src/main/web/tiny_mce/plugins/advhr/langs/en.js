3e812d85e7aa1dace85d554a658a767e59982dae
ldubost <ludovic@xwiki.com>
2006-03-27 19:39:41 +0000

    Removing previous version of TinyMCE
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki/trunk@989 f329d543-caf0-0310-9063-dda96c69346f

--- src/main/web/tiny_mce/plugins/advhr/langs/en.js
+++
@@ -1,6 +0,0 @@
-// UK lang variables
-
-tinyMCELang['lang_insert_advhr_desc']    = 'Insert / edit Horizontale Rule'
-tinyMCELang['lang_insert_advhr_width']   = 'Width';
-tinyMCELang['lang_insert_advhr_size']    = 'Height';
-tinyMCELang['lang_insert_advhr_noshade'] = 'No shadow';

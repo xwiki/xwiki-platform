ee07d74e7242be872978ce3d74cb3f62fb2dcee7
vmassol <vincent@massol.net>
2006-10-22 15:54:49 +0000

    XWIKI-389: Move Laszlo plugin code outside of core into a plugin proper
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-plugins/trunk@1454 f329d543-caf0-0310-9063-dda96c69346f

---
+++ xwiki-platform-tag/plugin/laszlo/src/main/java/com/xpn/xwiki/plugin/laszlo/LaszloMacro.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2006, XpertNet SARL, and individual contributors as indicated
+ * by the contributors.txt.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ *
+ * @author ludovic
+ * @author sdumitriu
+ */
+
+package com.xpn.xwiki.plugin.laszlo;
+
+import com.xpn.xwiki.XWiki;
+import com.xpn.xwiki.XWikiContext;
+import com.xpn.xwiki.XWikiException;
+import com.xpn.xwiki.render.XWikiRadeoxRenderEngine;
+import org.radeox.api.engine.RenderEngine;
+import org.radeox.api.engine.context.RenderContext;
+import org.radeox.macro.BaseLocaleMacro;
+import org.radeox.macro.parameter.MacroParameter;
+
+import java.io.IOException;
+import java.io.Writer;
+
+public class LaszloMacro  extends BaseLocaleMacro {
+  public String getLocaleKey() {
+    return "macro.laszlo";
+  }
+
+    public void execute(Writer writer, MacroParameter params)
+            throws IllegalArgumentException, IOException {
+
+        RenderContext context = params.getContext();
+        RenderEngine engine = context.getRenderEngine();
+
+        XWikiContext xcontext = ((XWikiRadeoxRenderEngine)engine).getContext();
+        XWiki xwiki = xcontext.getWiki();
+
+        LaszloPlugin plugin = (LaszloPlugin) xwiki.getPlugin("laszlo", xcontext);
+        // If the plugin is not loaded
+        if (plugin==null) {
+            writer.write("Plugin not loaded");
+            return;
+        }
+
+        StringBuffer str = new StringBuffer();
+        String name = params.get("name", 0);
+        String height = params.get("height", 1);
+        String width = params.get("width", 2);
+
+        String laszlocode = params.getContent();
+        try {
+         str.append(plugin.getLaszloFlash(name, width, height, laszlocode, xcontext));
+        } catch (XWikiException e) {
+            str.append("Laszlo Error");
+        }
+        writer.write(str.toString());
+    }
+}
---
+++ xwiki-platform-tag/plugin/laszlo/src/main/java/com/xpn/xwiki/plugin/laszlo/LaszloPlugin.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2006, XpertNet SARL, and individual contributors as indicated
+ * by the contributors.txt.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ *
+ * @author ludovic
+ * @author sdumitriu
+ */
+
+package com.xpn.xwiki.plugin.laszlo;
+
+import com.xpn.xwiki.XWikiContext;
+import com.xpn.xwiki.XWikiException;
+import com.xpn.xwiki.api.Api;
+import com.xpn.xwiki.plugin.XWikiDefaultPlugin;
+import com.xpn.xwiki.plugin.XWikiPluginInterface;
+import com.xpn.xwiki.util.Util;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class LaszloPlugin extends XWikiDefaultPlugin implements XWikiPluginInterface {
+    private static Log mLogger =
+            LogFactory.getFactory().getInstance(com.xpn.xwiki.plugin.laszlo.LaszloPlugin.class);
+
+    private String laszloBaseURL;
+    private String laszloPath;
+
+    public LaszloPlugin(String name, String className, XWikiContext context) {
+        super(name, className, context);
+        init(context);
+    }
+
+    public String getName() {
+        return "laszlo";
+    }
+
+    public Api getPluginApi(XWikiPluginInterface plugin, XWikiContext context) {
+        return new LaszloPluginApi((LaszloPlugin) plugin, context);
+    }
+    
+    public void flushCache() {
+        try {
+            File[] filelist = (new File(laszloPath)).listFiles();
+            for (int i=0;i<filelist.length;i++) {
+                try {
+                    filelist[i].delete();
+                } catch (Exception e) {}
+            }
+        } catch (Exception e) {}
+    }
+
+    public void init(XWikiContext context) {
+        super.init(context);
+
+        laszloBaseURL = context.getWiki().Param("xwiki.plugin.laszlo.baseurl", "/openlaszlo/xwiki/");
+        if (!laszloBaseURL.endsWith("/"))
+            laszloBaseURL = laszloBaseURL + "/";
+
+        laszloPath = context.getWiki().Param("xwiki.plugin.laszlo.path", "./webapps/openlaszlo/xwiki/");
+        if (!laszloPath.endsWith("/"))
+            laszloPath = laszloPath + "/";
+
+        flushCache();
+    }
+
+    public String writeLaszloFile(String name, String laszlocode) throws IOException, XWikiException {
+        File laszloDir = new File(laszloPath);
+        if (!laszloDir.exists())
+            laszloDir.mkdirs();
+
+        String filename = getFileName(name, laszlocode);
+
+        laszlocode = Util.secureLaszloCode(laszlocode);
+
+        File dfile = new File(laszloDir, filename);
+        if (!dfile.exists()) {
+            FileWriter fwriter = new FileWriter(dfile);
+            fwriter.write(laszlocode);
+            fwriter.flush();
+            fwriter.close();
+        }
+
+        return filename;
+    }
+
+    public String getFileName(String name, String laszlocode) {
+        if ((name==null)||name.trim().equals(""))
+            name = "laszlo";
+
+        String filename = name + "-" + Math.abs(laszlocode.hashCode()) + ".lzx";
+        return filename;
+    }
+
+    public String getLaszloURL(String name, String laszlocode) throws IOException, XWikiException {
+        String filename = writeLaszloFile(name, laszlocode);
+        return laszloBaseURL + filename + "?lzt=swf";
+    }
+
+    public String getLaszloFlash(String name, String width, String height, String laszlocode, XWikiContext context) throws IOException, XWikiException {
+        String url = getLaszloURL(name, laszlocode);
+        return context.getWiki().getFlash(url, width, height, context);
+    }
+}

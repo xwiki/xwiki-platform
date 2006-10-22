16c70139ece752463e4579044a9b4dac429814cb
vmassol <vincent@massol.net>
2006-10-22 09:22:26 +0000

    XWIKI-385: Move Adwords plugin code outside of core into a plugin proper
    * Restore google plugin in the core as it was moved by mistake
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki/trunk/core@1441 f329d543-caf0-0310-9063-dda96c69346f

---
+++ src/main/java/com/xpn/xwiki/plugin/google/GooglePlugin.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2006, XpertNet SARL, and individual contributors as indicated
+ * by the contributors.txt.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ *
+ * @author sdumitriu
+ */
+
+package com.xpn.xwiki.plugin.google;
+
+import com.xpn.xwiki.XWikiContext;
+import com.xpn.xwiki.api.Api;
+import com.xpn.xwiki.plugin.XWikiDefaultPlugin;
+import com.xpn.xwiki.plugin.XWikiPluginInterface;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class GooglePlugin extends XWikiDefaultPlugin implements XWikiPluginInterface {
+        private static Log mLogger =
+                LogFactory.getFactory().getInstance(com.xpn.xwiki.plugin.google.GooglePlugin.class);
+
+        public GooglePlugin(String name, String className, XWikiContext context) {
+            super(name, className, context);
+            init(context);
+        }
+
+    public String getName() {
+        return "google";
+    }
+
+    public Api getPluginApi(XWikiPluginInterface plugin, XWikiContext context) {
+        return new GooglePluginApi((GooglePlugin) plugin, context);
+    }
+
+    public void flushCache() {
+    }
+
+    public void init(XWikiContext context) {
+        super.init(context);
+    }
+}

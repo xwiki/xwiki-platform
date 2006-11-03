558cf74bda1d1d222ea5b6c10b46ec40c203caef
jeremi <jeremi23@gmail.com>
2006-11-03 21:36:08 +0000

    [GELC] * modify renameDocument of the API, and remove the right checking from the non wrapped object
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki/trunk@1509 f329d543-caf0-0310-9063-dda96c69346f

--- core/src/main/java/com/xpn/xwiki/XWiki.java
+++ core/src/main/java/com/xpn/xwiki/XWiki.java
@@ -3928,13 +3928,7 @@ public class XWiki implements XWikiDocChangeNotificationInterface, XWikiInterfac
     public XWikiDocument renamePage(XWikiDocument doc, String newFullName, XWikiContext context) throws XWikiException {
         if (context.getWiki().exists(newFullName, context)){
             XWikiDocument delDoc = context.getWiki().getDocument(newFullName, context);
-            if (checkAccess("delete", delDoc, context))
-                context.getWiki().deleteDocument(delDoc, context);
-            else
-                throw new XWikiException(XWikiException.MODULE_XWIKI_ACCESS,
-                            XWikiException.ERROR_XWIKI_ACCESS_DENIED,
-                            "delete the destination document is denied");
-
+            context.getWiki().deleteDocument(delDoc, context);
         }
         XWikiDocument renamedDoc = doc.copyDocument(newFullName, context);
         saveDocument(renamedDoc, context);
@@ -4226,5 +4220,9 @@ public class XWiki implements XWikiDocChangeNotificationInterface, XWikiInterfac
 
     public String addTooltip(String html, String message, XWikiContext context) {
         return addTooltip(html, message, "this.WIDTH='300'", context);
+    }
+
+    public void renamePage(String fullName, String newFullName, XWikiContext context) throws XWikiException {
+        renamePage(context.getWiki().getDocument(fullName, context), newFullName, context);
     }
 }

60550e1c885b67a50a4f6ee0921ff52beb3afcce
vmassol <vincent@massol.net>
2006-11-18 14:59:47 +0000

    * XWIKI-411 (Define checkstyle rules and define an associated build strategy). Improved checkstyle rules. * Applied defined checkstyle rules on the Zip Explorer plugin, leading to some refactoring, including removing all Vector references (replaced by List). Hopefully this shouldn't affect existing scripts. * Fixed bug in Zip Explorere plugin found by Checkstyle (checkstyle forced me to refactor the downloadAttachment method because there were too many return statements. Thus I've created a isValidZipURL() method and thus I've added a unit test for it showing the bug). The bug was that "http://server/xwiki/bin/download/Main/Document" was considered a valid ZIP URL... Note: There's no JIRA issue for the creation of the Zip Explorer plugin and thus I couldn't refer it from this comment.
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki/trunk/core@1594 f329d543-caf0-0310-9063-dda96c69346f

--- src/main/java/com/xpn/xwiki/plugin/zipexplorer/ZipExplorerPluginAPI.java
+++ src/main/java/com/xpn/xwiki/plugin/zipexplorer/ZipExplorerPluginAPI.java
@@ -27,89 +27,102 @@ import com.xpn.xwiki.api.Document;
 import com.xpn.xwiki.doc.XWikiAttachment;
 
 import java.util.List;
-import java.util.Vector;
 
 /**
- * ZIP-related APIs to list content of a ZIP attachments and to intercept XWiki download requests so that it's possible
- * to display contents found inside ZIP files. This plugin accepts specially formatted URLs pointing to files inside
- * ZIP files by using the following syntax:
+ * ZIP-related APIs to list content of a ZIP attachments and to intercept XWiki download requests so
+ * that it's possible to display contents found inside ZIP files. This plugin accepts specially
+ * formatted URLs pointing to files inside ZIP files by using the following syntax:
  *
  * <code>http://[...]/download/Document/zipfile.zip/SomeDirectory/SomeFile.txt</code>.
  *
- * In this example, the URL points to the <code>SomeFile.txt</code> file located in a directory named
- * <code>SomeDirectory</code> inside a ZIP file named <code>zipfile.zip</code> and attached to the document named
- * <code>Document</code>.
+ * In this example, the URL points to the <code>SomeFile.txt</code> file located in a directory
+ * named <code>SomeDirectory</code> inside a ZIP file named <code>zipfile.zip</code> and attached to
+ * the document named <code>Document</code>.
  *
  * @version $Id: $
  * @see com.xpn.xwiki.plugin.zipexplorer.ZipExplorerPlugin
  */
-public class ZipExplorerPluginAPI extends Api {
-
+public class ZipExplorerPluginAPI extends Api
+{
+    /**
+     * The ZIP Explorer plugin hidden by this API class. This is to create a separation between
+     * the API used by users in XWiki documents and the actual implementations.
+     */
     private ZipExplorerPlugin plugin;
 
-    public ZipExplorerPluginAPI(ZipExplorerPlugin plugin, XWikiContext context) {
+    /**
+     * @param plugin the ZIP Explorer plugin that this class is hiding.
+     * @param context the XWiki context instance containing the last user request
+     */
+    public ZipExplorerPluginAPI(ZipExplorerPlugin plugin, XWikiContext context)
+    {
         super(context);
         this.plugin = plugin;
     }
 
     /**
-     * For ZIP URLs of the format <code>http://[...]/zipfile.zip/SomeDirectory/SomeFile.txt</code> return a new
-     * attachment containing the file pointed to inside the ZIP. If the original attachment does not point to a ZIP
-     * file or if it doesn't specify a location inside the ZIP then do nothing and return the original attachment.
+     * For ZIP URLs of the format <code>http://[...]/zipfile.zip/SomeDirectory/SomeFile.txt</code>
+     * return a new attachment containing the file pointed to inside the ZIP. If the original
+     * attachment does not point to a ZIP file or if it doesn't specify a location inside the ZIP
+     * then do nothing and return the original attachment.
      *
-     * @return a new attachment pointing to the file pointed to by the URL inside the ZIP or the original attachment
-     *         if the requested URL doesn't specify a file inside a ZIP
      * @param attachment the original attachment
-     * @see com.xpn.xwiki.plugin.XWikiDefaultPlugin#downloadAttachment(com.xpn.xwiki.doc.XWikiAttachment, com.xpn.xwiki.XWikiContext)
+     * @return a new attachment pointing to the file pointed to by the URL inside the ZIP or the
+     *         original attachment if the requested URL doesn't specify a file inside a ZIP
+     * @see com.xpn.xwiki.plugin.XWikiDefaultPlugin#downloadAttachment
      */
-    public XWikiAttachment downloadAttachment(XWikiAttachment attachment) {
+    public XWikiAttachment downloadAttachment(XWikiAttachment attachment)
+    {
         return this.plugin.downloadAttachment(attachment, context);
     }
 
     /**
-     * @return the list of file entries in the ZIP file attached under the passed attachment name inside the passed
-     *         document
      * @param document the document containing the ZIP file as an attachment
      * @param attachmentName the name under which the ZIP file is attached in the document
+     * @return the list of file entries in the ZIP file attached under the passed attachment name
+     *         inside the passed document
      */
-    public List getFileList(Document document, String attachmentName) {
+    public List getFileList(Document document, String attachmentName)
+    {
         return this.plugin.getFileList(document, attachmentName, context);
     }
 
     /**
-     * Finds the ZIP attachement with passed name from the passed document matching and parse the ZIP to generate
-     * a list of {@link com.xpn.xwiki.objects.classes.ListItem} elements representing a tree view of all directories
-     * and files in the ZIP. For example the following zip:
-     * </p>
-     * <code><pre>
+     * Finds the ZIP attachement with passed name from the passed document matching and parse the
+     * ZIP to generate a list of {@link com.xpn.xwiki.objects.classes.ListItem} elements
+     * representing a tree view of all directories and files in the ZIP. For example the following
+     * zip:
+     * <pre><code>
      * zipfile.zip:
      *   Directory/File.txt
      *   File2.txt
-     * </per></code>
+     * </code></pre>
      * generates the following ListItem list:
-     * <code><pre>
+     * <pre><code>
      *   { id = "Directory/", value = "Directory", parent = ""}
      *   { id = "Directory/File.txt", value = "File.txt", parent = "Directory/"}
      *   { id = "File2.txt", value = "File2.txt", parent = ""}
-     * </per></code>
+     * </code></pre>
      *
-     * @return a tree view list of {@link com.xpn.xwiki.objects.classes.ListItem} elements representing the content of
-     *         the ZIP file
      * @param document the document containing the ZIP file as an attachment
      * @param attachmentName the name under which the ZIP file is attached in the document
+     * @return a tree view list of {@link com.xpn.xwiki.objects.classes.ListItem} elements
+     *         representing the content of the ZIP file
      */
-    public Vector getFileTreeList(Document document, String attachmentName) {
+    public List getFileTreeList(Document document, String attachmentName)
+    {
         return this.plugin.getFileTreeList(document, attachmentName, context);
     }
 
     /**
-     * @return the attachment URL of the passed attachement located in the passed document to which the passed filename
-     *         has been suffixed.
      * @param document the document containing the ZIP file as an attachment
      * @param attachmentName the name under which the ZIP file is attached in the document
      * @param fileName the filename to concatenate at the end of the attachment URL
+     * @return the attachment URL of the passed attachement located in the passed document to which
+     *         the passed filename has been suffixed.
      */
-    public String getFileLink(Document document, String attachmentName, String fileName) {
+    public String getFileLink(Document document, String attachmentName, String fileName)
+    {
         return this.plugin.getFileLink(document, attachmentName, fileName, context);
     }
 }

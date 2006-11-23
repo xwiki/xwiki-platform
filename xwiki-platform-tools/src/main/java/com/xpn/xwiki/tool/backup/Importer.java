159f636acbc413e1405cc8297631af3b63ffecc0
vmassol <vincent@massol.net>
2006-11-23 14:16:36 +0000

    XWIKI-460: Mavenify the builds-tools/backup module and create a Maven2 plugin out of it
    * Started the m2 build. It's not working yet but I'm committing as I have an issue with the importer and I need help from others.
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-tools/backup@1628 f329d543-caf0-0310-9063-dda96c69346f

---
+++ xwiki-platform-tools/src/main/java/com/xpn/xwiki/tool/backup/Importer.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2006, XpertNet SARL, and individual contributors as indicated
+ * by the contributors.txt.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package com.xpn.xwiki.tool.backup;
+
+import com.xpn.xwiki.XWikiContext;
+import com.xpn.xwiki.XWikiConfig;
+import com.xpn.xwiki.XWiki;
+import com.xpn.xwiki.XWikiException;
+import com.xpn.xwiki.plugin.packaging.Package;
+import com.xpn.xwiki.plugin.packaging.PackageException;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Import a set of XWiki documents into an existing database.
+ *
+ * @version $Id$
+ */
+public class Importer
+{
+    /**
+     * Import documents defined in an XML file located in the passed document definition directory into a database
+     * defined by its passed name and by an Hibernate configuration file.
+     *
+     * <p>Note: I would have liked to call this method "import" but it's a reserved keyword... Strange that it's not
+     * allowed for method names though.</p>
+     *
+     * @param sourceDirectory the directory where the package.xml file is located and where the documents
+     *        to import are located
+     * @param databaseName some database name (TODO: find out what this name is really)
+     * @param hibernateConfig the Hibernate config fill containing the database definition (JDBC driver, username and
+     *        password, etc)
+     * @throws XWikiException if the import failed for any reason
+     * @todo Replace the Hibernate config file with a list of parameters required for the importation
+     */
+    public void importDocuments(File sourceDirectory, String databaseName, File hibernateConfig)
+        throws XWikiException
+    {
+        XWikiContext context = new XWikiContext();
+        context.setUser("XWiki.superadmin");
+        context.setDatabase(databaseName);
+
+        XWikiConfig config = new XWikiConfig();
+        config.put("xwiki.store.class", "com.xpn.xwiki.store.XWikiHibernateStore");
+        config.put("xwiki.store.hibernate.path", hibernateConfig);
+        config.put("xwiki.store.hibernate.updateschema", "1");
+        config.put("xwiki.virtual", "1");
+
+        XWiki xwiki = new XWiki(config, context);
+        context.setWiki(xwiki);
+
+        Package pack = new Package();
+        pack.setWithVersions(false);
+        pack.setBackupPack(true);
+
+        // TODO: The readFromDir method should not throw IOExceptions, only PackageException.
+        // See http://jira.xwiki.org/jira/browse/XWIKI-458
+        try {
+            pack.readFromDir(sourceDirectory, context);
+        } catch (IOException e) {
+            throw new PackageException(PackageException.ERROR_PACKAGE_UNKNOWN,
+                "Failed to import documents from [" + sourceDirectory + "]", e);
+        }
+
+        pack.install(context);
+    }
+}

88368d4b0d24f0ca159d7eac0ba746736de238cc
marta- <marta.girdea@gmail.com>
2006-12-06 14:08:05 +0000

    XWIKI-406: Add support for external documents in the attach and image macros
    New syntax for the attach macro:
    {attach:text|file|document|title|rel|id|fromIncludingDoc}
    
    New syntax for the image macro:
    {image:text|height|width|align|document|alt|title|link|fromIncludingDoc}
    
    Changes:
    - The macros can use a different document as the container of the image/attachment
    - If a parameter is not specified, and it's default position contains another parameter, it is no longer an error
    - Support for specifying more attributes
    
    
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki/trunk/core@1683 f329d543-caf0-0310-9063-dda96c69346f

--- src/main/java/com/xpn/xwiki/render/macro/AttachMacro.java
+++ src/main/java/com/xpn/xwiki/render/macro/AttachMacro.java
@@ -16,58 +16,138 @@
  * License along with this software; if not, write to the Free
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
- *
- * @author sdumitriu
+ */
+
+/**
+ * Macro that outputs links to document attachments.
+ * 
+ * Syntax: {attach:text|file|document|title|rel|id|fromIncludingDoc}
+ * <ul>
+ * <li>text: The link text. If missing, the file name is used.</li>
+ * <li>file: The target filename. If missing, the link text is used.</li>
+ * <li>document: The document to which the file is attached. If missing, 
+ *   the current document is used.</li>
+ * <li>title: The title attribute of the link. If missing, no title.</li>
+ * <li>rel: The rel attribute of the link. If missing, no rel.</li>
+ * <li>id: The id attribute of the link. If missing, no id.</li>
+ * <li>fromIncludingDoc: If present, when the current document is included using 
+ *   #include*, use the top level document instead of the included one.
+ *   This is useful for templates, for example.</li>
+ * </ul>
  */
 
 package com.xpn.xwiki.render.macro;
 
-import com.xpn.xwiki.XWikiContext;
-import com.xpn.xwiki.doc.XWikiDocument;
-import com.xpn.xwiki.render.XWikiRadeoxRenderEngine;
+import java.io.IOException;
+import java.io.Writer;
+
 import org.radeox.api.engine.RenderEngine;
 import org.radeox.api.engine.context.RenderContext;
 import org.radeox.macro.BaseLocaleMacro;
 import org.radeox.macro.parameter.MacroParameter;
 import org.radeox.util.Encoder;
 
-import java.io.IOException;
-import java.io.Writer;
+import com.xpn.xwiki.XWikiContext;
+import com.xpn.xwiki.doc.XWikiDocument;
+import com.xpn.xwiki.render.XWikiRadeoxRenderEngine;
 
-public class AttachMacro   extends BaseLocaleMacro {
-  public String getLocaleKey() {
-    return "macro.attach";
-  }
-
-  public void execute(Writer writer, MacroParameter params)
-      throws IllegalArgumentException, IOException {
-
-    RenderContext context = params.getContext();
-    RenderEngine engine = context.getRenderEngine();
-
-    String text = params.get("text", 0);
-    String filename = params.get("file", 1);
-
-    if(params.getLength() == 1) {
-        filename = text;
-        text = Encoder.toEntity(text.charAt(0)) + Encoder.escape(text.substring(1));
+public class AttachMacro extends BaseLocaleMacro {
+    public String getLocaleKey() {
+        return "macro.attach";
     }
 
+    public void execute(Writer writer, MacroParameter params) throws IllegalArgumentException, IOException {
 
-    XWikiContext xcontext = ((XWikiRadeoxRenderEngine)engine).getContext();
-    XWikiDocument doc = xcontext.getDoc();
+        RenderContext context = params.getContext();
+        RenderEngine engine = context.getRenderEngine();
 
-    StringBuffer str = new StringBuffer();
-    str.append("<a href=\"");
-    str.append(doc.getAttachmentURL(filename, "download", xcontext));
-    str.append("\" />");
-    str.append(text);
-    str.append("</a>");
-    writer.write(str.toString());
+        // Read the parameters
+        String text = params.get("text", 0);
+        if (null == text || text.indexOf("=") != -1) {
+            text = null;
+        }
+        String filename = params.get("file", 1);
+        if (null == filename || filename.indexOf("=") != -1) {
+            filename = null;
+        }
+        String document = params.get("document", 2);
+        if (null == document || document.indexOf("=") != -1) {
+            document = null;
+        }
+        String title = params.get("title", 3);
+        if (null == title || title.indexOf("=") != -1 || title.equals("")) {
+            title = null;
+        }
+        String rel = params.get("rel", 4);
+        if (null == rel || rel.indexOf("=") != -1 || rel.equals("")) {
+            rel = null;
+        }
+        String id = params.get("id", 5);
+        if (null == id || id.indexOf("=") != -1 || id.equals("")) {
+            id = null;
+        }
+        String useIncluder = params.get("fromIncludingDoc", 6);
+        if (null == useIncluder || useIncluder.indexOf("=") != -1 || useIncluder.toLowerCase().startsWith("f")) {
+            useIncluder = null;
+        }
 
-    // if (!"none".equals(img) && engine instanceof ImageRenderEngine) {
-    //    writer.write(((ImageRenderEngine) engine).getExternalImageLink());
-    // }
+        // Fix missing text or filename parameters
+        if (text == null && filename != null) {
+            text = filename;
+        } else if (text != null && filename == null) {
+            filename = text;
+        } else {
+            return;
+        }
+        text = Encoder.escape(text);
 
-  }
+        XWikiContext xcontext = ((XWikiRadeoxRenderEngine) engine).getContext();
+
+        // Get the target document
+        XWikiDocument doc = null;
+        try {
+            if (document == null || "".equals(document)) {
+                if (useIncluder == null) {
+                    doc = (XWikiDocument) xcontext.get("sdoc");
+                }
+            } else {
+                doc = xcontext.getWiki().getDocument(xcontext.getDoc().getSpace(), document, xcontext);
+            }
+        } catch (Exception ex) {
+            // NullPointer or ClassCast
+        }
+        if (doc == null) {
+            doc = xcontext.getDoc();
+        }
+
+        // Create the link code
+        StringBuffer str = new StringBuffer();
+        str.append("<a href=\"");
+        if (filename.indexOf("tp://") != -1) {
+            str.append(filename);
+        } else {
+            str.append(doc.getAttachmentURL(filename, "download", xcontext));
+        }
+        str.append("\"");
+        if (rel != null) {
+            str.append(" rel=\"" + rel + "\"");
+        }
+        if (title != null) {
+            str.append(" title=\"" + title + "\"");
+        }
+        if (id != null) {
+            str.append(" id=\"" + id + "\"");
+        }
+        str.append(">");
+        str.append(text);
+        str.append("</a>");
+
+        // All done, flush the StringBufer
+        writer.write(str.toString());
+
+        // if (!"none".equals(img) && engine instanceof ImageRenderEngine) {
+        // writer.write(((ImageRenderEngine) engine).getExternalImageLink());
+        // }
+
+    }
 }

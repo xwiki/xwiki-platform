b7fea7972646b883c860336c7f085cfe4d3fbd88
ldubost <ludovic@xwiki.com>
2006-12-18 00:18:01 +0000

    Added port in startup scripts Updated default wiki
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki/branches/XWIKI_1_0_BETA_1@1768 f329d543-caf0-0310-9063-dda96c69346f

--- standalone/scripts/start_xwiki.bat
+++ standalone/scripts/start_xwiki.bat
@@ -1,5 +1,6 @@
 set LANG=fr_FR.ISO8859-1
 set  JETTY_HOME=.
 set JETTY_PORT=8080
+set JAVA_OPTS=-Xmx300m
 
-java -Dfile.encoding=iso-8859-1 -Djetty.home=%JETTY_HOME% -Djetty.port=%JETTY_PORT% -jar %JETTY_HOME%/start.jar
+java %JAVA_OPTS% -Dfile.encoding=iso-8859-1 -Djetty.home=%JETTY_HOME% -Djetty.port=%JETTY_PORT% -jar %JETTY_HOME%/start.jar
--- standalone/scripts/start_xwiki.sh
+++ standalone/scripts/start_xwiki.sh
@@ -3,6 +3,6 @@
 export LANG=fr_FR.ISO8859-1
 JETTY_HOME=.
 JETTY_PORT=8080
-
-java -Dfile.encoding=iso-8859-1 -Djetty.port=$JETTY_PORT -Djetty.home=$JETTY_HOME -jar $JETTY_HOME/start.jar
+JAVA_OPTS=-Xmx300m
+java $JAVA_OPTS -Dfile.encoding=iso-8859-1 -Djetty.port=$JETTY_PORT -Djetty.home=$JETTY_HOME -jar $JETTY_HOME/start.jar
 

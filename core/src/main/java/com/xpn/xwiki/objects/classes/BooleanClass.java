b455f5112deab7be8ffaa59d918b1c1a9eb4d88e
marta- <marta.girdea@gmail.com>
2006-12-28 21:56:01 +0000

    XWIKI-607: The property editor of the Class editor screen has layout issues
    Java part: also generate div and label tags surrounding each radio button.
    
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki/trunk@1815 f329d543-caf0-0310-9063-dda96c69346f

--- core/src/main/java/com/xpn/xwiki/objects/classes/BooleanClass.java
+++ core/src/main/java/com/xpn/xwiki/objects/classes/BooleanClass.java
@@ -31,7 +31,10 @@ import com.xpn.xwiki.objects.meta.PropertyMetaClass;
 import com.xpn.xwiki.plugin.query.XWikiCriteria;
 import com.xpn.xwiki.plugin.query.XWikiQuery;
 import com.xpn.xwiki.web.XWikiMessageTool;
+
+import org.apache.ecs.xhtml.div;
 import org.apache.ecs.xhtml.input;
+import org.apache.ecs.xhtml.label;
 import org.apache.ecs.xhtml.option;
 import org.apache.ecs.xhtml.select;
 
@@ -111,8 +114,8 @@ public class BooleanClass extends PropertyClass {
         String displayFormType = getDisplayFormType();
 
         if (getDisplayType().equals("checkbox")) {
-         displayFormType = "checkbox";
-       }
+            displayFormType = "checkbox";
+        }
 
         if (displayFormType.equals("checkbox")) {
             displayCheckboxEdit(buffer, name, prefix, object, context);
@@ -130,10 +133,9 @@ public class BooleanClass extends PropertyClass {
         int nb1 = 1;
         int nb2 = 2;
 
-
         option[] options;
 
-        if (getDefaultValue()==-1) {
+        if (getDefaultValue() == -1) {
             options = new option[] { new option("---", ""), new option(String1, "1"), new option(String0, "0") };
             options[0].addElement("---");
             options[1].addElement(String1);
@@ -148,23 +150,23 @@ public class BooleanClass extends PropertyClass {
 
         try {
             IntegerProperty prop = (IntegerProperty) object.safeget(name);
-            Integer ivalue = (prop==null) ? null : (Integer) prop.getValue();
-                if (ivalue != null) {
-                    int value = ivalue.intValue();
-                    if (value == 1)
-                        options[nb1].setSelected(true);
-                    else if (value == 0)
-                        options[nb2].setSelected(true);
-                } else {
-                    int value = getDefaultValue();
-                    if (value == 1)
-                        options[nb1].setSelected(true);
-                    else if (value == 0)
-                        options[nb2].setSelected(true);
-                    else if (value == -1) {
-                        options[0].setSelected(true);
-                    }
+            Integer ivalue = (prop == null) ? null : (Integer) prop.getValue();
+            if (ivalue != null) {
+                int value = ivalue.intValue();
+                if (value == 1)
+                    options[nb1].setSelected(true);
+                else if (value == 0)
+                    options[nb2].setSelected(true);
+            } else {
+                int value = getDefaultValue();
+                if (value == 1)
+                    options[nb1].setSelected(true);
+                else if (value == 0)
+                    options[nb2].setSelected(true);
+                else if (value == -1) {
+                    options[0].setSelected(true);
                 }
+            }
         } catch (Exception e) {
             // This should not happen
             e.printStackTrace();
@@ -175,48 +177,62 @@ public class BooleanClass extends PropertyClass {
 
     public void displayRadioEdit(StringBuffer buffer, String name, String prefix, BaseCollection object, XWikiContext context) {
         String StringNone = getDisplayValue(context, 2);
-        String String0 = getDisplayValue(context, 0);
-        String String1 = getDisplayValue(context, 1);
-        input[] inputs;
+        String StringTrue = getDisplayValue(context, 1);
+        String StringFalse = getDisplayValue(context, 0);
+        div[] inputs;
 
-        input radio0 = new input(input.radio, prefix + name, "");
-        input radio1 = new input(input.radio, prefix + name, "1");
-        input radio2 = new input(input.radio, prefix + name, "0");
-        radio0.addElement(StringNone);
-        radio1.addElement(String1);
-        radio2.addElement(String0);
+        input radioNone = new input(input.radio, prefix + name, "");
+        input radioTrue = new input(input.radio, prefix + name, "1");
+        input radioFalse = new input(input.radio, prefix + name, "0");
+        label labelNone = new label();
+        label labelTrue = new label();
+        label labelFalse = new label();
+        div divNone = new div();
+        div divTrue = new div();
+        div divFalse = new div();
+        labelNone.addElement(radioNone);
+        labelNone.addElement(StringNone);
+        divNone.addElement(labelNone);
+        labelTrue.addElement(radioTrue);
+        labelTrue.addElement(StringTrue);
+        divTrue.addElement(labelTrue);
+        labelFalse.addElement(radioFalse);
+        labelFalse.addElement(StringFalse);
+        divFalse.addElement(labelFalse);
 
-        if (getDefaultValue()==-1) {
-            inputs = new input[] { radio0, radio1, radio2 };
+        radioTrue.setID(prefix + name);
+
+        if (getDefaultValue() == -1) {
+            inputs = new div[] { divNone, divTrue, divFalse };
         } else {
-            inputs = new input[] {radio1, radio2 };
+            inputs = new div[] { divTrue, divFalse };
         }
 
         try {
             IntegerProperty prop = (IntegerProperty) object.safeget(name);
-            Integer ivalue = (prop==null) ? null : (Integer) prop.getValue();
+            Integer ivalue = (prop == null) ? null : (Integer) prop.getValue();
             if (ivalue != null) {
                 int value = ivalue.intValue();
                 if (value == 1)
-                    radio1.setChecked(true);
+                    radioTrue.setChecked(true);
                 else if (value == 0)
-                    radio2.setChecked(true);
+                    radioFalse.setChecked(true);
             } else {
                 int value = getDefaultValue();
                 if (value == 1)
-                    radio1.setChecked(true);
+                    radioTrue.setChecked(true);
                 else if (value == 0)
-                    radio2.setChecked(true);
-                else if (value==-1)
-                    radio0.setChecked(true);
-                }
+                    radioFalse.setChecked(true);
+                else if (value == -1)
+                    radioNone.setChecked(true);
+            }
         } catch (Exception e) {
             // This should not happen
             e.printStackTrace();
         }
 
-        for(int i=0;i<inputs.length;i++) {
-          buffer.append(inputs[i].toString());
+        for (int i = 0; i < inputs.length; i++) {
+            buffer.append(inputs[i].toString());
         }
     }
 
@@ -256,10 +272,10 @@ public class BooleanClass extends PropertyClass {
             String strname = getDisplayType() + "_" + value;
             String result = msg.get(strname);
             if (result.equals(strname)) {
-                if (value==2)
-                 return "---";
+                if (value == 2)
+                    return "---";
                 else
-                 return "" + value;
+                    return "" + value;
             }
             return result;
         } catch (Exception e) {
@@ -267,7 +283,7 @@ public class BooleanClass extends PropertyClass {
         }
     }
 
-    public String displaySearch(String name, String prefix, XWikiCriteria criteria, XWikiContext context){
+    public String displaySearch(String name, String prefix, XWikiCriteria criteria, XWikiContext context) {
         if (getDisplayType().equals("input")) {
             return super.displaySearch(name, prefix, criteria, context);
         } else if (getDisplayType().equals("radio")) {
@@ -284,35 +300,17 @@ public class BooleanClass extends PropertyClass {
         String String0 = getDisplayValue(context, 0);
         String String1 = getDisplayValue(context, 1);
 
-        option[] options = { new option("---", "" ), new option(String1, "1" ), new option(String0, "0")};
+        option[] options = { new option("---", ""), new option(String1, "1"), new option(String0, "0") };
         options[0].addElement("---");
         options[1].addElement(String1);
         options[2].addElement(String0);
-        
+
         /*
-        try {
-        IntegerProperty prop = (IntegerProperty) object.safeget(name);
-        if (prop!=null) {
-            Integer ivalue = (Integer)prop.getValue();
-            if (ivalue!=null) {
-                int value = ivalue.intValue();
-                if (value==1)
-                    options[1].setSelected(true);
-                else if (value==0)
-                    options[2].setSelected(true);
-            }  else {
-                int value = getDefaultValue();
-                if (value==1)
-                    options[1].setSelected(true);
-                else if (value==0)
-                    options[2].setSelected(true);
-            }
-	        }
-        } catch (Exception e) {
-            // This should not happen
-            e.printStackTrace();
-        }
-        */
+         * try { IntegerProperty prop = (IntegerProperty) object.safeget(name); if (prop!=null) { Integer ivalue = (Integer)prop.getValue(); if (ivalue!=null) {
+         * int value = ivalue.intValue(); if (value==1) options[1].setSelected(true); else if (value==0) options[2].setSelected(true); } else { int value =
+         * getDefaultValue(); if (value==1) options[1].setSelected(true); else if (value==0) options[2].setSelected(true); } } } catch (Exception e) { // This
+         * should not happen e.printStackTrace(); }
+         */
         select.addElement(options);
         return select.toString();
     }
@@ -323,40 +321,22 @@ public class BooleanClass extends PropertyClass {
         org.apache.ecs.xhtml.input checkNo = new input(input.hidden, prefix + name, 0);
 
         /*
-        try {
-        IntegerProperty prop = (IntegerProperty) object.safeget(name);
-        if (prop!=null) {
-            Integer ivalue = (Integer)prop.getValue();
-            if (ivalue!=null) {
-                int value = ivalue.intValue();
-                if (value==1)
-                    check.setChecked(true);
-                else if (value==0)
-                	check.setChecked(false);
-            }
-            else {
-                int value = getDefaultValue();
-                if (value==1)
-                    check.setChecked(true);
-                else
-                    check.setChecked(false);
-            }
-        }} catch (Exception e) {
-            // This should not happen
-            e.printStackTrace();
-        }
-        */
+         * try { IntegerProperty prop = (IntegerProperty) object.safeget(name); if (prop!=null) { Integer ivalue = (Integer)prop.getValue(); if (ivalue!=null) {
+         * int value = ivalue.intValue(); if (value==1) check.setChecked(true); else if (value==0) check.setChecked(false); } else { int value =
+         * getDefaultValue(); if (value==1) check.setChecked(true); else check.setChecked(false); } }} catch (Exception e) { // This should not happen
+         * e.printStackTrace(); }
+         */
         buffer.append(check.toString());
         buffer.append(checkNo.toString());
         return buffer.toString();
     }
 
     public void fromSearchMap(XWikiQuery query, Map map) {
-        String[] data  = (String[])map.get("");
-        if (data!=null) {
+        String[] data = (String[]) map.get("");
+        if (data != null) {
             Object[] data2 = new Object[data.length];
-            for (int i=0;i<data.length;i++)
-               data2[i] = fromString(data[i]).getValue();
+            for (int i = 0; i < data.length; i++)
+                data2[i] = fromString(data[i]).getValue();
             query.setParam(getObject().getName() + "_" + getName(), data2);
         }
     }

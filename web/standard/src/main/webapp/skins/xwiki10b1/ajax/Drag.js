b556de9664ff9fc5fb0ea00b6e524227be15c572
marta- <marta.girdea@gmail.com>
2007-01-06 02:44:20 +0000

    XWIKI-529: WYSIWYG panel wizard Done
    XWIKI-562: Separate users into "normal" and "advanced"
    Done
    
    XWIKI-683: Add the "create" actions in the top menu
    Done
    
    XWIKI-677: Add macros for simple notice boxes
    Done
    
    XWIKI-661: Add macros for simple notice messages
    Modified according to Vincent's comment
    
    
    
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki/trunk@1889 f329d543-caf0-0310-9063-dda96c69346f

--- web/standard/src/main/webapp/skins/xwiki10b1/ajax/Drag.js
+++ web/standard/src/main/webapp/skins/xwiki10b1/ajax/Drag.js
@@ -9,33 +9,33 @@ var Drag={
 			a.root.onDragEnd=new Function();
 			a.root.onDrag=new Function();
 		},
-	"start":function(a){	
-			var b=Drag.obj=this;
-			a=Drag.fixE(a);
-			var c=parseInt(b.root.style.top);
-			var d=parseInt(b.root.style.left);
-			b.root.onDragStart(d,c,a.clientX,a.clientY);
-			b.lastMouseX=a.clientX;
-			b.lastMouseY=a.clientY;
+	"start":function(evt){
+			var obj=Drag.obj=this;
+			evt=Drag.fixE(evt);
+			var c=parseInt(obj.root.style.top);
+			var d=parseInt(obj.root.style.left);
+			obj.lastMouseX=evt.clientX;
+			obj.lastMouseY=evt.clientY;
+			obj.root.onDragStart(d,c,evt.clientX,evt.clientY);
 			document.onmousemove=Drag.drag;
 			document.onmouseup=Drag.end;
 			return false;
-		},	
-	"drag":function(a){
-			a=Drag.fixE(a);
-			var b=Drag.obj;
-			var c=a.clientY;
-			var d=a.clientX;
-			var e=parseInt(b.root.style.top);
-			var f=parseInt(b.root.style.left);
+		},
+	"drag":function(evt){
+			evt=Drag.fixE(evt);
+			var obj=Drag.obj;
+			var cy=evt.clientY;
+			var cx=evt.clientX;
+			var e=parseInt(obj.root.style.top);
+			var f=parseInt(obj.root.style.left);
 			var h,g;
-			h=f+d-b.lastMouseX;
-			g=e+c-b.lastMouseY;
-			b.root.style.left=h+"px";
-			b.root.style.top=g+"px";			
-			b.lastMouseX=d;
-			b.lastMouseY=c;
-			b.root.onDrag(h,g,a.clientX,a.clientY);
+			h=f+cx-obj.lastMouseX;
+			g=e+cy-obj.lastMouseY;
+			obj.root.style.left=h+"px";
+			obj.root.style.top=g+"px";			
+			obj.lastMouseX=cx;
+			obj.lastMouseY=cy;
+			obj.root.onDrag(h,g,cy,cx);
 			return false;
 		},
 	"end":function(){			
@@ -44,10 +44,10 @@ var Drag={
 			Drag.obj.root.onDragEnd(parseInt(Drag.obj.root.style.left),parseInt(Drag.obj.root.style.top));
 			Drag.obj=null;
 		},
-	"fixE":function(a){
-			if(typeof a=="undefined")a=window.event;
-			if(typeof a.layerX=="undefined")a.layerX=a.offsetX;
-			if(typeof a.layerY=="undefined")a.layerY=a.offsetY;
-			return a;
+	"fixE":function(evt){
+			if(typeof evt=="undefined")evt=window.event;
+			if(typeof evt.layerX=="undefined")evt.layerX=evt.offsetX;
+			if(typeof evt.layerY=="undefined")evt.layerY=evt.offsetY;
+			return evt;
 		}
 };

59eac8924dbe5c78c1431e320bd859723599b1bf
vmassol <vincent@massol.net>
2007-02-02 12:39:47 +0000

    Minor javadoc cleaning
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki/trunk/core@2027 f329d543-caf0-0310-9063-dda96c69346f

--- src/main/java/com/xpn/xwiki/api/Api.java
+++ src/main/java/com/xpn/xwiki/api/Api.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006, XpertNet SARL, and individual contributors as indicated
+ * Copyright 2006-2007, XpertNet SARL, and individual contributors as indicated
  * by the contributors.txt.
  *
  * This is free software; you can redistribute it and/or modify it
@@ -16,56 +16,58 @@
  * License along with this software; if not, write to the Free
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
- *
- * @author sdumitriu
  */
-
-
 package com.xpn.xwiki.api;
 
 import com.xpn.xwiki.XWikiContext;
 import com.xpn.xwiki.XWikiException;
 
-public class Api {
+public class Api
+{
     protected XWikiContext context;
 
-    public Api(XWikiContext context) {
-       this.context = context;
+    public Api(XWikiContext context)
+    {
+        this.context = context;
     }
 
     /**
-     * check if the current user has the programming right
-     * @return true if he has
+     * @return true if the current user has the Programming right or false otherwise
+     * @deprecated use #hasProgrammingRights() instead
      */
-    public boolean checkProgrammingRights() {
+    public boolean checkProgrammingRights()
+    {
         return hasProgrammingRights();
     }
 
     /**
-     * check if the current user has the programming right
-     * @return true if he has
+     * @return true if the current user has the Programming right or false otherwise
      */
-    public boolean hasProgrammingRights() {
-            com.xpn.xwiki.XWiki xwiki = context.getWiki();
-            return xwiki.getRightService().hasProgrammingRights(context);
-    }
-
-    /**
-     * check if the current user has the Admin right
-     * @return true if he has
-     */
-    public boolean hasAdminRights() {
-            com.xpn.xwiki.XWiki xwiki = context.getWiki();
-            return xwiki.getRightService().hasAdminRights(context);
-    }
-
-    /**
-     * check if the current user has the given right
-     * @return true if he has
-     */
-    public boolean hasAccessLevel(String right, String docname) throws XWikiException {
+    public boolean hasProgrammingRights()
+    {
         com.xpn.xwiki.XWiki xwiki = context.getWiki();
-        return xwiki.getRightService().hasAccessLevel(right, context.getUser(), docname, context);        
+        return xwiki.getRightService().hasProgrammingRights(context);
     }
 
+    /**
+     * @return true if the current user has the Admin right or false otherwise
+     */
+    public boolean hasAdminRights()
+    {
+        com.xpn.xwiki.XWiki xwiki = context.getWiki();
+        return xwiki.getRightService().hasAdminRights(context);
+    }
+
+    /**
+     * @param right the name of the right to verify (eg "programming", "admin", "register", etc)
+     * @param docname the document for which to verify the right
+     * @return true if the current user has the specified right
+     * @exception XWikiException in case of an error finding the document or accessing groups
+     *            information
+     */
+    public boolean hasAccessLevel(String right, String docname) throws XWikiException
+    {
+        com.xpn.xwiki.XWiki xwiki = context.getWiki();
+        return xwiki.getRightService().hasAccessLevel(right, context.getUser(), docname, context);
+    }
 }

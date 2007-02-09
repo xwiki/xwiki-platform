4748ad7007d346f9da8f0ac6b7939ec23c23ea33
vmassol <vincent@massol.net>
2007-02-09 21:42:00 +0000

    * Apply new coding style * Adding documentation for the Content-Disposition header.
    Merged from trunk
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki/branches/XWIKI_1_0@2074 f329d543-caf0-0310-9063-dda96c69346f

--- core/src/main/java/com/xpn/xwiki/web/DownloadAction.java
+++ core/src/main/java/com/xpn/xwiki/web/DownloadAction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006, XpertNet SARL, and individual contributors as indicated
+ * Copyright 2006-2007, XpertNet SARL, and individual contributors as indicated
  * by the contributors.txt.
  *
  * This is free software; you can redistribute it and/or modify it
@@ -16,9 +16,6 @@
  * License along with this software; if not, write to the Free
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
- *
- * @author moghrabix
- * @author sdumitriu
  */
 package com.xpn.xwiki.web;
 
@@ -31,62 +28,71 @@ import com.xpn.xwiki.plugin.XWikiPluginManager;
 
 import java.io.IOException;
 
-public class DownloadAction extends XWikiAction {
-
-
-    public String getFileName(String path, String action) {
+public class DownloadAction extends XWikiAction
+{
+    public String getFileName(String path, String action)
+    {
         path = path.substring(path.indexOf("/" + action));
         int pos = 0;
         for (int i = 0; i < 3; i++) {
             pos = path.indexOf("/", pos + 1);
         }
-        if (path.indexOf("/", pos + 1) > 0)
+        if (path.indexOf("/", pos + 1) > 0) {
             return path.substring(pos + 1, path.indexOf("/", pos + 1));
+        }
         return path.substring(pos + 1);
     }
 
-    public String render(XWikiContext context) throws XWikiException {
+    public String render(XWikiContext context) throws XWikiException
+    {
         XWikiRequest request = context.getRequest();
         XWikiResponse response = context.getResponse();
         XWikiDocument doc = context.getDoc();
         String path = request.getRequestURI();
         String filename = Utils.decode(getFileName(path, "download"), context);
-         XWikiAttachment attachment;
+        XWikiAttachment attachment;
 
-        if (request.getParameter("id")!=null) {
+        if (request.getParameter("id") != null) {
             int id = Integer.parseInt(request.getParameter("id"));
             attachment = (XWikiAttachment) doc.getAttachmentList().get(id);
-        }
-        else {
+        } else {
             attachment = doc.getAttachment(filename);
         }
 
-        if (attachment==null) {
-            Object[] args = { filename };
+        if (attachment == null) {
+            Object[] args = {filename};
             throw new XWikiException(XWikiException.MODULE_XWIKI_APP,
-                    XWikiException.ERROR_XWIKI_APP_ATTACHMENT_NOT_FOUND,
-                    "Attachment {0} not found", null, args);
- 	}
+                XWikiException.ERROR_XWIKI_APP_ATTACHMENT_NOT_FOUND,
+                "Attachment {0} not found", null, args);
+        }
 
-	XWikiPluginManager plugins = context.getWiki().getPluginManager();
-	attachment = plugins.downloadAttachment(attachment, context);
+        XWikiPluginManager plugins = context.getWiki().getPluginManager();
+        attachment = plugins.downloadAttachment(attachment, context);
         // Choose the right content type
         String mimetype = attachment.getMimeType(context);
         response.setContentType(mimetype);
 
         String ofilename = XWiki.getURLEncoded(attachment.getFilename()).replaceAll("\\+", " ");
-        response.addHeader("Content-disposition", "inline; filename=\"" +  ofilename + "\"");
+
+        // The inline attribute of Content-Disposition tells the browser that they should display
+        // the downloaded file in the page (see http://www.ietf.org/rfc/rfc1806.txt for more
+        // details). We do this so that JPG, GIF, PNG, etc are displayed without prompting a Save
+        // dialog box. However, all mime types that cannot be displayed by the browser do prompt a
+        // Save dialog box (exe, zip, xar, etc).
+        response.addHeader("Content-disposition", "inline; filename=\"" + ofilename + "\"");
+
         response.setDateHeader("Last-Modified", attachment.getDate().getTime());
+
         // Sending the content of the attachment
         byte[] data;
         try {
             data = attachment.getContent(context);
         }
-        catch(XWikiException e) {
-            Object[] args = { filename };
+        catch (XWikiException e) {
+            Object[] args = {filename};
             throw new XWikiException(XWikiException.MODULE_XWIKI_APP,
-                    XWikiException.ERROR_XWIKI_APP_ATTACHMENT_NOT_FOUND,
-                    "Attachment content {0} not found", null, args);
+                XWikiException.ERROR_XWIKI_APP_ATTACHMENT_NOT_FOUND,
+                "Attachment content {0} not found", null, args);
         }
 
         response.setContentLength(data.length);
@@ -94,9 +100,9 @@ public class DownloadAction extends XWikiAction {
             response.getOutputStream().write(data);
         } catch (IOException e) {
             throw new XWikiException(XWikiException.MODULE_XWIKI_APP,
-                    XWikiException.ERROR_XWIKI_APP_SEND_RESPONSE_EXCEPTION,
-                    "Exception while sending response", e);
+                XWikiException.ERROR_XWIKI_APP_SEND_RESPONSE_EXCEPTION,
+                "Exception while sending response", e);
         }
         return null;
-	}
+    }
 }

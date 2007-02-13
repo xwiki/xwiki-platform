7bd685ec744264ed70ce1c3e2fef8f06721fddc1
vmassol <vincent@massol.net>
2007-02-13 07:52:49 +0000

    XWIKI-844: Add script to start XWiki in debug mode for the standalone distribution to help in debugging
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki/trunk@2093 f329d543-caf0-0310-9063-dda96c69346f

---
+++ standalone/scripts/start_xwiki_debug.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+export LANG=fr_FR.ISO8859-1
+JETTY_HOME=.
+JETTY_PORT=8080
+JAVA_OPTS="-Xmx300m -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005"
+
+java $JAVA_OPTS -Dfile.encoding=iso-8859-1 -Djetty.port=$JETTY_PORT -Djetty.home=$JETTY_HOME -jar $JETTY_HOME/start.jar

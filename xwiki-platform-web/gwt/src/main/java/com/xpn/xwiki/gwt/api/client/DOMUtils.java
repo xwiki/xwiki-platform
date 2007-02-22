847e12c5d7a92a6de9c494d522cceb93dda604d4
jeremi <jeremi23@gmail.com>
2007-02-22 14:58:43 +0000

    XWIKI-894 Improve the GWT interface
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki/trunk/web@2235 f329d543-caf0-0310-9063-dda96c69346f

---
+++ xwiki-platform-web/gwt/src/main/java/com/xpn/xwiki/gwt/api/client/DOMUtils.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2006, XpertNet SARL, and individual contributors as indicated
+ * by the contributors.txt.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ *
+ * @author jeremi
+ */
+package com.xpn.xwiki.gwt.api.client;
+
+import com.google.gwt.user.client.Element;
+import com.google.gwt.user.client.DOM;
+
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class DOMUtils {
+
+    public static List getElementByTagName(Element el, String tagName){
+        return getElementByTagName(el, tagName, new ArrayList());
+    }
+
+    private static List getElementByTagName(Element parentEl, String tagName, List elms){
+        for(int i = 0; i < DOM.getChildCount(parentEl); i++){
+            Element el = DOM.getChild(parentEl, i);
+            if (DOM.getAttribute(el, "tagName").equalsIgnoreCase(tagName)){
+                elms.add(el);
+            }
+            getElementByTagName(el, tagName, elms);
+        }
+        return elms;
+    }
+
+    public static Element getFirstElementByTagName(Element parentEl, String tagName){
+        for(int i = 0; i < DOM.getChildCount(parentEl); i++){
+            Element el = DOM.getChild(parentEl, i);
+            if (DOM.getAttribute(el, "tagName").equalsIgnoreCase(tagName)){
+                return el;
+            }
+            getFirstElementByTagName(el, tagName);
+        }
+        return null;
+    }
+
+}

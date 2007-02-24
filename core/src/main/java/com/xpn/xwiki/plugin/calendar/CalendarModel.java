3ed208a73942cf78a753f5cf51d0e6cf8556f179
marta- <marta.girdea@gmail.com>
2007-02-24 12:01:39 +0000

    XWIKI-911: Improve the EventCalendar plugin Merging changes from trunk
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki/branches/XWIKI_1_0@2243 f329d543-caf0-0310-9063-dda96c69346f

--- core/src/main/java/com/xpn/xwiki/plugin/calendar/CalendarModel.java
+++ core/src/main/java/com/xpn/xwiki/plugin/calendar/CalendarModel.java
@@ -25,16 +25,14 @@ import java.util.Calendar;
 import java.util.Date;
 
 /**
- * Model interface for the CalendarTag. The CalendarTag will set a day,
- * then use the computeUrl method to get the URLs it needs.
+ * Model interface for the CalendarTag. The CalendarTag will set a day, then use the computeUrl method to get the URLs it needs.
  */
-public interface CalendarModel
-{
+public interface CalendarModel {
     public Calendar getCalendar();
 
-	public void setDay( String month ) throws Exception;
+    public void setDay(String month) throws Exception;
 
-	public Date getDay();
+    public Date getDay();
 
     public Date getNextMonth();
 
@@ -45,21 +43,20 @@ public interface CalendarModel
     public String computeNextMonthUrl();
 
     /**
-     * Create URL for use on edit-weblog page, preserves the request
-     * parameters used by the tabbed-menu tag for navigation.
-     *
-     * @param day   Day for URL
+     * Create URL for use on edit-weblog page, preserves the request parameters used by the tabbed-menu tag for navigation.
+     * 
+     * @param day Day for URL
      * @param valid Always return a URL, never return null
      * @return URL for day, or null if no weblog entry on that day
      */
-    public String computeUrl( java.util.Date day, boolean valid );
+    public String computeUrl(java.util.Date day, boolean valid);
 
     /**
      * Get calendar cell content or null if none.
-     *
+     * 
      * @param day Day for URL
      * @param valid Always return a URL, never return null
      * @return Calendar cell content or null if none.
      */
-    public String getContent( java.util.Date day );
+    public String getContent(java.util.Date day);
 }

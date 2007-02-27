ed8122fd704e808292a9c5ac282ce90d44ea8ffa
sdumitriu <sergiu@xwiki.com>
2007-02-27 19:52:43 +0000

    XWIKI-919: XWikiMessageTool does not support non-latin1 characters in the resource files/documents Fixed for XWiki document bundles.
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki/trunk@2269 f329d543-caf0-0310-9063-dda96c69346f

--- core/src/main/java/com/xpn/xwiki/web/XWikiMessageTool.java
+++ core/src/main/java/com/xpn/xwiki/web/XWikiMessageTool.java
@@ -23,6 +23,7 @@ package com.xpn.xwiki.web;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -36,29 +37,29 @@ import java.util.Properties;
 import java.util.ResourceBundle;
 import java.util.Set;
 
+import org.apache.log4j.Logger;
+
 import com.xpn.xwiki.XWikiContext;
 import com.xpn.xwiki.XWikiException;
 import com.xpn.xwiki.doc.XWikiDocument;
-import org.apache.log4j.Logger;
 
 /**
- * Internationalization service based on key/property values. The key is the id of the message
- * being looked for and the returned value is the message in the language requested. There are
- * 3 sources where properties are looked for (in the specified order):
+ * Internationalization service based on key/property values. The key is the id of the message being
+ * looked for and the returned value is the message in the language requested. There are 3 sources
+ * where properties are looked for (in the specified order):
  * <ol>
- *   <li>If there's a "documentBundles" property in the XWiki Preferences page then the XWiki
- *       documents listed there (separated by commas) are considered the source for properties</li>
- *   <li>If there's a "xwiki.documentBundles" property in the XWiki configuration file (xwiki.cfg)
- *       then the XWiki documents listed there (separated by commas) are considered for source for
- *       properties</li>
- *   <li>The Resource Bundle passed in the constructor</li>
+ * <li>If there's a "documentBundles" property in the XWiki Preferences page then the XWiki
+ * documents listed there (separated by commas) are considered the source for properties</li>
+ * <li>If there's a "xwiki.documentBundles" property in the XWiki configuration file (xwiki.cfg)
+ * then the XWiki documents listed there (separated by commas) are considered for source for
+ * properties</li>
+ * <li>The Resource Bundle passed in the constructor</li>
  * </ol>
  * If the property is not found in any of these 3 sources then the key is returned in place of the
- * value.
- * In addition the property values are cached for better performance but if one of the XWiki
- * documents containing the properties is modified, its content is cached again next time a key
- * is asked.
- *
+ * value. In addition the property values are cached for better performance but if one of the XWiki
+ * documents containing the properties is modified, its content is cached again next time a key is
+ * asked.
+ * 
  * @version $Id: $
  */
 public class XWikiMessageTool
@@ -75,8 +76,8 @@ public class XWikiMessageTool
     private static final String KEY = "documentBundles";
 
     /**
-     * The default Resource Bundle to fall back to if no document bundle is found when trying to
-     * get a key.
+     * The default Resource Bundle to fall back to if no document bundle is found when trying to get
+     * a key.
      */
     private ResourceBundle bundle;
 
@@ -87,32 +88,31 @@ public class XWikiMessageTool
     private XWikiContext context;
 
     /**
-     * Cache properties loaded from the document bundles for maximum efficiency. The map is of
-     * type (Long, Properties) where Long is the XWiki document ids.
+     * Cache properties loaded from the document bundles for maximum efficiency. The map is of type
+     * (Long, Properties) where Long is the XWiki document ids.
      */
     private Map propsCache = new HashMap();
 
     /**
-     * Cache for saving the last modified dates of document bundles that have been loaded. This
-     * is used so that we can reload them if they've been modified since last time they were
-     * cached. The map is of type (Long, Date) where Long is the XWiki document ids. 
+     * Cache for saving the last modified dates of document bundles that have been loaded. This is
+     * used so that we can reload them if they've been modified since last time they were cached.
+     * The map is of type (Long, Date) where Long is the XWiki document ids.
      */
     private Map previousDates = new HashMap();
 
     /**
      * List of document bundles that have been modified since the last time they were cached. The
-     * Set containers Long objects which are the XWiki document ids.
-     * 
-     * TODO: This instance variable should be removed as it's used internally and its state
-     * shouldn't encompass several calls to get().
+     * Set containers Long objects which are the XWiki document ids. TODO: This instance variable
+     * should be removed as it's used internally and its state shouldn't encompass several calls to
+     * get().
      */
     private Set docsToRefresh = new HashSet();
 
     /**
      * @param bundle the default Resource Bundle to fall back to if no document bundle is found when
-     *        trying to get a key
+     *            trying to get a key
      * @param context the {@link com.xpn.xwiki.XWikiContext} object, used to get access to XWiki
-     *        primitives for loading documents
+     *            primitives for loading documents
      */
     public XWikiMessageTool(ResourceBundle bundle, XWikiContext context)
     {
@@ -149,10 +149,10 @@ public class XWikiMessageTool
      * how to easily create an Array in Velocity whereas an Array is easily created. For example:
      * <code>$msg.get("key", ["1", "2", "3"])</code>.
      * </p>
-     *
+     * 
      * @param key the key of the string to find
      * @param params the list of parameters to use for replacing "{N}" elements in the string. See
-     *        {@link java.text.MessageFormat} for the full syntax
+     *            {@link java.text.MessageFormat} for the full syntax
      * @return the translated string with parameters resolved
      */
     public String get(String key, List params)
@@ -163,7 +163,7 @@ public class XWikiMessageTool
         }
         return translation;
     }
-    
+
     /**
      * @return the list of internationalisation document bundle names as a list of XWiki page names
      *         ("Space.Document") or an empty list if no such documents have been found
@@ -225,8 +225,8 @@ public class XWikiMessageTool
 
     /**
      * @param documentName the document's name (eg Space.Document)
-     * @return the document object corresponding to the passed document's name. A translated
-     *         version of the document for the current Locale is looked for.
+     * @return the document object corresponding to the passed document's name. A translated version
+     *         of the document for the current Locale is looked for.
      */
     private XWikiDocument getDocumentBundle(String documentName)
     {
@@ -243,19 +243,19 @@ public class XWikiMessageTool
                 // Error while loading the document.
                 // TODO: A runtime exception should be thrown that will bubble up till the
                 // topmost level. For now simply log the error
-                LOG.error("Failed to load internationalization document bundle ["
-                    + documentName + "].", e);
+                LOG.error("Failed to load internationalization document bundle [" + documentName
+                    + "].", e);
                 docBundle = null;
             }
         }
-        
+
         return docBundle;
     }
 
     /**
-     * Looks for a translation in the list of internationalization document bundles. It first
-     * checks if the translation can be found in the cache.
-     *
+     * Looks for a translation in the list of internationalization document bundles. It first checks
+     * if the translation can be found in the cache.
+     * 
      * @param key the key identifying the translation
      * @return the translation or null if not found or if the passed key is null
      */
@@ -267,31 +267,47 @@ public class XWikiMessageTool
             Iterator it = getDocumentBundles().iterator();
             while (it.hasNext()) {
                 XWikiDocument docBundle = (XWikiDocument) it.next();
-                Long docId = new Long(docBundle.getId());
-                Properties props = null;
-                if (this.docsToRefresh.contains(docId) || !this.propsCache.containsKey(docId)) {
-                    // Cache needs to be updated
-                    props = new Properties();
-                    if (docBundle != null) {
+                if (docBundle != null) {
+                    Long docId = new Long(docBundle.getId());
+                    Properties props = null;
+                    if (this.docsToRefresh.contains(docId) || !this.propsCache.containsKey(docId)) {
+                        // Cache needs to be updated
+                        props = new Properties();
                         String content = docBundle.getContent();
-                        InputStream is = new ByteArrayInputStream(content.getBytes());
+                        byte[] docContent = null;
+                        try {
+                            docContent = content.getBytes(this.context.getWiki().getEncoding());
+                        } catch (UnsupportedEncodingException ex) {
+                            LOG.error(
+                                "Unknown encoding: " + this.context.getWiki().getEncoding(), ex);
+                            docContent = content.getBytes();
+                        }
+                        InputStream is = new ByteArrayInputStream(docContent);
                         try {
                             props.load(is);
                         } catch (IOException e) {
                             // Cannot do anything
                         }
+                        // updates cache
+                        this.propsCache.put(docId, props);
+                        this.docsToRefresh.remove(docId);
+                    } else {
+                        // gets from cache
+                        props = (Properties) this.propsCache.get(docId);
                     }
-                    // updates cache
-                    this.propsCache.put(docId, props);
-                    this.docsToRefresh.remove(docId);
-                } else {
-                    // gets from cache
-                    props = (Properties) this.propsCache.get(docId);
-                }
-                String translation = props.getProperty(key);
-                if (translation != null) {
-                    returnValue = translation;
-                    break;
+                    String translation = props.getProperty(key);
+                    if (translation != null) {
+                        returnValue = translation;
+                        try {
+                            returnValue =
+                                new String(returnValue.getBytes("ISO-8859-1"), this.context
+                                    .getWiki().getEncoding());
+                        } catch (UnsupportedEncodingException ex) {
+                            LOG.error(
+                                "Unknown encoding: " + this.context.getWiki().getEncoding(), ex);
+                        }
+                        break;
+                    }
                 }
             }
         }

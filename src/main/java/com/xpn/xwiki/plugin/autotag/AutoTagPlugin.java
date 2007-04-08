574d4e24a36209f2f8242490186ce6311a51a8ed
sdumitriu <sergiu@xwiki.com>
2007-04-08 22:34:23 +0000

    - Cleanup Autotag plugin - Convert accented chars to unicode escapes
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki/trunk/core@2726 f329d543-caf0-0310-9063-dda96c69346f

--- src/main/java/com/xpn/xwiki/plugin/autotag/AutoTagPlugin.java
+++ src/main/java/com/xpn/xwiki/plugin/autotag/AutoTagPlugin.java
@@ -7,91 +7,104 @@ import com.xpn.xwiki.api.Api;
 
 import java.util.*;
 
-public class AutoTagPlugin extends XWikiDefaultPlugin implements XWikiPluginInterface {
+public class AutoTagPlugin extends XWikiDefaultPlugin implements XWikiPluginInterface
+{
     public static final int LANG_FRENCH = 0;
+
     public static final int LANG_ENGLISH = 1;
+
     public static String name = "autotag";
 
-    public final static String[] FRENCH_STOP_WORDS = {
-    "a", "afin", "ai", "ainsi", "après", "attendu", "au", "aujourd", "auquel", "aussi",
-    "autre", "autres", "aux", "auxquelles", "auxquels", "avait", "avant", "avec", "avoir",
-    "c", "car", "ce", "ceci", "cela", "celle", "celles", "celui", "cependant", "certain",
-    "certaine", "certaines", "certains", "ces", "cet", "cette", "ceux", "chez", "ci",
-    "combien", "comme", "comment", "concernant", "contre", "d", "dans", "de", "debout",
-    "dedans", "dehors", "delà", "depuis", "derrière", "des", "désormais", "desquelles",
-    "desquels", "dessous", "dessus", "devant", "devers", "devra", "divers", "diverse",
-    "diverses", "doit", "donc", "dont", "du", "duquel", "durant", "dès", "elle", "elles",
-    "en", "entre", "environ", "est", "et", "etc", "etre", "eu", "eux", "excepté", "hormis",
-    "hors", "hélas", "hui", "il", "ils", "j", "je", "jusqu", "jusque", "l", "la", "laquelle",
-    "le", "lequel", "les", "lesquelles", "lesquels", "leur", "leurs", "lorsque", "lui", "là",
-    "ma", "mais", "malgré", "me", "merci", "mes", "mien", "mienne", "miennes", "miens", "moi",
-    "moins", "mon", "moyennant", "même", "mêmes", "n", "ne", "ni", "non", "nos", "notre",
-    "nous", "néanmoins", "nôtre", "nôtres", "on", "ont", "ou", "outre", "où", "par", "parmi",
-    "partant", "pas", "passé", "pendant", "plein", "plus", "plusieurs", "pour", "pourquoi",
-    "proche", "près", "puisque", "qu", "quand", "que", "quel", "quelle", "quelles", "quels",
-    "qui", "quoi", "quoique", "revoici", "revoilà", "s", "sa", "sans", "sauf", "se", "selon",
-    "seront", "ses", "si", "sien", "sienne", "siennes", "siens", "sinon", "soi", "soit",
-    "son", "sont", "sous", "suivant", "sur", "ta", "te", "tes", "tien", "tienne", "tiennes",
-    "tiens", "toi", "ton", "tous", "tout", "toute", "toutes", "tu", "un", "une", "va", "vers",
-    "voici", "voilà", "vos", "votre", "vous", "vu", "vôtre", "vôtres", "y", "à", "ça", "ès",
-    "été", "être", "ô", "avez", "parce", "suis"
-  };
+    public final static String[] FRENCH_STOP_WORDS =
+        {"a", "afin", "ai", "ainsi", "apr\u00e8s", "attendu", "au", "aujourd", "auquel", "aussi",
+        "autre", "autres", "aux", "auxquelles", "auxquels", "avait", "avant", "avec", "avoir",
+        "c", "car", "ce", "ceci", "cela", "celle", "celles", "celui", "cependant", "certain",
+        "certaine", "certaines", "certains", "ces", "cet", "cette", "ceux", "chez", "ci",
+        "combien", "comme", "comment", "concernant", "contre", "d", "dans", "de", "debout",
+        "dedans", "dehors", "del\u00e0", "depuis", "derri\u00e8re", "des", "d\u00e9sormais",
+        "desquelles", "desquels", "dessous", "dessus", "devant", "devers", "devra", "divers",
+        "diverse", "diverses", "doit", "donc", "dont", "du", "duquel", "durant", "d\u00e8s",
+        "elle", "elles", "en", "entre", "environ", "est", "et", "etc", "etre", "eu", "eux",
+        "except\u00e9", "hormis", "hors", "h\u00e9las", "hui", "il", "ils", "j", "je", "jusqu",
+        "jusque", "l", "la", "laquelle", "le", "lequel", "les", "lesquelles", "lesquels", "leur",
+        "leurs", "lorsque", "lui", "l\u00e0", "ma", "mais", "malgr\u00e9", "me", "merci", "mes",
+        "mien", "mienne", "miennes", "miens", "moi", "moins", "mon", "moyennant", "m\u00eame",
+        "m\u00eames", "n", "ne", "ni", "non", "nos", "notre", "nous", "n\u00e9anmoins",
+        "n\u00f4tre", "n\u00f4tres", "on", "ont", "ou", "outre", "o\u00f9", "par", "parmi",
+        "partant", "pas", "pass\u00e9", "pendant", "plein", "plus", "plusieurs", "pour",
+        "pourquoi", "proche", "pr\u00e8s", "puisque", "qu", "quand", "que", "quel", "quelle",
+        "quelles", "quels", "qui", "quoi", "quoique", "revoici", "revoil\u00e0", "s", "sa",
+        "sans", "sauf", "se", "selon", "seront", "ses", "si", "sien", "sienne", "siennes",
+        "siens", "sinon", "soi", "soit", "son", "sont", "sous", "suivant", "sur", "ta", "te",
+        "tes", "tien", "tienne", "tiennes", "tiens", "toi", "ton", "tous", "tout", "toute",
+        "toutes", "tu", "un", "une", "va", "vers", "voici", "voil\u00e0", "vos", "votre", "vous",
+        "vu", "v\u00f4tre", "v\u00f4tres", "y", "\u00e0", "\u00e7a", "\u00e8s", "\u00e9t\u00e9",
+        "\u00eatre", "\u00f4", "avez", "parce", "suis"};
 
     List ignoreList = new ArrayList();
+
     List dontignoreList = null;
+
     int maxTag = 100;
+
     int maxTagSize = 64;
+
     int minTagSize = 12;
 
-    public AutoTagPlugin(String name, String className, XWikiContext context) {
+    public AutoTagPlugin(String name, String className, XWikiContext context)
+    {
         super(name, className, context);
         init(context);
     }
 
-    public void init(XWikiContext context) {
+    public void init(XWikiContext context)
+    {
         super.init(context);
-    }    
+    }
 
     /**
      * Allow to get the plugin name
-     *
+     * 
      * @return plugin name
      */
-    public String getName() {
+    public String getName()
+    {
         return name;
     }
 
-    
-    public Api getPluginApi(XWikiPluginInterface plugin, XWikiContext context) {
+    public Api getPluginApi(XWikiPluginInterface plugin, XWikiContext context)
+    {
         return new AutoTagPluginAPI((AutoTagPlugin) plugin, context);
     }
 
-    public TagCloud countWords(String text, int lang){
-        Map wordList;
+    public TagCloud countWords(String text, int lang)
+    {
         TagCloud tagcloud = new TagCloud();
         tagcloud.setText(text);
 
         splitWords(tagcloud);
-        wordList = countWords(tagcloud);
-        wordList = clearStopWords(tagcloud, lang);
-        wordList = stemmer(tagcloud, lang);
+        countWords(tagcloud);
+        clearStopWords(tagcloud, lang);
+        stemmer(tagcloud, lang);
         return tagcloud;
     }
 
-    public TagCloud generateTagCloud(String text, int lang){
+    public TagCloud generateTagCloud(String text, int lang)
+    {
         TagCloud tagcloud = countWords(text, lang);
         calculateTags(tagcloud);
         return tagcloud;
     }
 
-    private Set calculateTags(TagCloud tagcloud){
+    private Set calculateTags(TagCloud tagcloud)
+    {
         Map stemmedWords = tagcloud.getStemmedWordMap();
         Map stemmedWordFreqMap = new HashMap();
 
-        Iterator it =  stemmedWords.keySet().iterator();
+        Iterator it = stemmedWords.keySet().iterator();
 
         // we calculate the frequency of each word
-        while(it.hasNext()){
+        while (it.hasNext()) {
             Integer totalFreq = new Integer(0);
             String leadWord = "";
             Integer leadFreq = new Integer(0);
@@ -100,15 +113,15 @@ public class AutoTagPlugin extends XWikiDefaultPlugin implements XWikiPluginInte
             Map wordMap = (Map) stemmedWords.get(stemmedWord);
 
             Iterator itWordMap = wordMap.keySet().iterator();
-            while(itWordMap.hasNext()){
+            while (itWordMap.hasNext()) {
                 String word = (String) itWordMap.next();
 
-                Integer freq = ((Integer)wordMap.get(word));
+                Integer freq = ((Integer) wordMap.get(word));
 
                 totalFreq = new Integer(freq.intValue() + totalFreq.intValue());
 
-                if (freq.intValue() > leadFreq.intValue()){
-                    leadFreq = ((Integer)wordMap.get(word));
+                if (freq.intValue() > leadFreq.intValue()) {
+                    leadFreq = ((Integer) wordMap.get(word));
                     leadWord = word;
                 }
             }
@@ -123,10 +136,9 @@ public class AutoTagPlugin extends XWikiDefaultPlugin implements XWikiPluginInte
 
         LinkedHashMap tagMap = new LinkedHashMap();
 
-        for(int i = 0, pos = keyset.length - 1; i < keyset.length && i < maxTag; i++, pos--){
+        for (int i = 0, pos = keyset.length - 1; i < keyset.length && i < maxTag; i++, pos--) {
             tagMap.put(keyset[pos], orderedMap.get(keyset[pos]));
         }
-
 
         Object[] freqs = tagMap.values().toArray();
 
@@ -135,15 +147,17 @@ public class AutoTagPlugin extends XWikiDefaultPlugin implements XWikiPluginInte
 
         Integer ftot = new Integer(0);
 
-        for(int i = 0; i < freqs.length; i++)
-            ftot = new Integer(((Integer)freqs[i]).intValue() + ftot.intValue());
+        for (int i = 0; i < freqs.length; i++)
+            ftot = new Integer(((Integer) freqs[i]).intValue() + ftot.intValue());
 
         it = sortSet(tagMap.keySet()).iterator();
         SortedSet tagSet = new TreeSet();
 
-        while(it.hasNext()){
+        while (it.hasNext()) {
             String tagName = (String) it.next();
-            long size = getTagSize(((Integer)tagMap.get(tagName)).intValue(), maxFreq.intValue(), minFreq.intValue(), ftot.intValue());
+            long size =
+                getTagSize(((Integer) tagMap.get(tagName)).intValue(), maxFreq.intValue(),
+                    minFreq.intValue(), ftot.intValue());
             Tag tag = new Tag(tagName, size);
             tagSet.add(tag);
         }
@@ -151,112 +165,116 @@ public class AutoTagPlugin extends XWikiDefaultPlugin implements XWikiPluginInte
         return tagSet;
     }
 
-
-    private long getTagSize(double freq,double fmax, double fmin, double ftot)
+    private long getTagSize(double freq, double fmax, double fmin, double ftot)
     {
-      int fontrange = maxTagSize - minTagSize;
+        int fontrange = maxTagSize - minTagSize;
 
-      // tweak this if all the words seem too similar in size or extremely different
-      // rely on the cumulative by x% (0 = 0%, 1 = 100%)
-      double cuml_importance = 0.7;
+        // tweak this if all the words seem too similar in size or extremely different
+        // rely on the cumulative by x% (0 = 0%, 1 = 100%)
+        double cuml_importance = 0.7;
 
-      // sizes based on word's frequency vs total/cumulative frequency
-      double sumpx = ((fontrange * cuml_importance) + 1) * (fontrange * cuml_importance) / 2;
-      double px = freq / ftot * sumpx;
+        // sizes based on word's frequency vs total/cumulative frequency
+        double sumpx = ((fontrange * cuml_importance) + 1) * (fontrange * cuml_importance) / 2;
+        double px = freq / ftot * sumpx;
 
-      // sizes based on word's frequency deviation from max/min frequencies
-      px += Math.pow((freq-fmin)/(1 > fmax-fmin ? 1 : fmax-fmin ), 0.8) * (fontrange * (1 - cuml_importance));
-      double res = maxTagSize < px + minTagSize ? maxTagSize: px + minTagSize;
-      return Math.round(res);
+        // sizes based on word's frequency deviation from max/min frequencies
+        px +=
+            Math.pow((freq - fmin) / (1 > fmax - fmin ? 1 : fmax - fmin), 0.8)
+                * (fontrange * (1 - cuml_importance));
+        double res = maxTagSize < px + minTagSize ? maxTagSize : px + minTagSize;
+        return Math.round(res);
     }
 
-
-    public static SortedSet sortSet(Set oSet){
+    public static SortedSet sortSet(Set oSet)
+    {
         SortedSet set = new TreeSet();
 
         Iterator it = oSet.iterator();
-        while(it.hasNext()){
+        while (it.hasNext()) {
             set.add(it.next());
         }
         return set;
     }
 
-    public static LinkedHashMap sortMap(Map hmap){
-		LinkedHashMap map = new LinkedHashMap();
+    public static LinkedHashMap sortMap(Map hmap)
+    {
+        LinkedHashMap map = new LinkedHashMap();
 
         List mapKeys = new ArrayList(hmap.keySet());
-		List mapValues = new ArrayList(hmap.values());
+        List mapValues = new ArrayList(hmap.values());
 
-        //hmap.clear();
+        // hmap.clear();
 
         TreeSet sortedSet = new TreeSet(mapValues);
-		Object[] sortedArray = sortedSet.toArray();
+        Object[] sortedArray = sortedSet.toArray();
 
-		for (int i=0; i<sortedArray.length; i++)
-		{
-            for (int j = 0; j < mapValues.size(); j++){
+        for (int i = 0; i < sortedArray.length; i++) {
+            for (int j = 0; j < mapValues.size(); j++) {
                 if (mapValues.get(j).equals(sortedArray[i]))
                     map.put(mapKeys.get(j), sortedArray[i]);
             }
-		}
-		return map;
-	}
+        }
+        return map;
+    }
 
-    private Map clearStopWords(TagCloud tagcloud, int lang){
+    private Map clearStopWords(TagCloud tagcloud, int lang)
+    {
         Map words = tagcloud.getCountedWordMap();
-        if (lang == LANG_FRENCH){
-            for (int i = 0; i < FRENCH_STOP_WORDS.length; i++){
+        if (lang == LANG_FRENCH) {
+            for (int i = 0; i < FRENCH_STOP_WORDS.length; i++) {
                 words.remove(FRENCH_STOP_WORDS[i]);
             }
         }
 
         Iterator wit = words.keySet().iterator();
-        while(wit.hasNext()) {
+        while (wit.hasNext()) {
             String word = (String) wit.next();
-            if (word.contains("<")||word.contains(">")||word.contains("=")||word.contains("\"")||word.contains("/")||word.contains("“"))
+            if (word.indexOf("<") >= 0 || word.indexOf(">") >= 0 || word.indexOf("=") >= 0
+                || word.indexOf("\"") >= 0 || word.indexOf("/") >= 0
+                || word.indexOf("\u0093") >= 0)
                 if (!ignoreList.contains(word))
                     ignoreList.add(word);
         }
 
-        if (ignoreList != null){
+        if (ignoreList != null) {
             Iterator it = ignoreList.iterator();
-            while(it.hasNext()){
+            while (it.hasNext()) {
                 String word = (String) it.next();
                 if (dontignoreList == null || !dontignoreList.contains(word))
                     words.remove(word);
             }
         }
 
-
         tagcloud.setCountedWordMap(words);
         return words;
     }
 
-    private Map countWords(TagCloud tagcloud){
+    private Map countWords(TagCloud tagcloud)
+    {
         String[] words = tagcloud.getWordList();
         Map wordsCnt = new HashMap();
 
-        for (int i = 0; i < words.length; i++){
+        for (int i = 0; i < words.length; i++) {
             String word = words[i];
-            if (!wordsCnt.containsKey(word)){
+            if (!wordsCnt.containsKey(word)) {
                 wordsCnt.put(word, new Integer(0));
             }
-            wordsCnt.put(word, new Integer(((Integer)wordsCnt.get(word)).intValue() + 1));
+            wordsCnt.put(word, new Integer(((Integer) wordsCnt.get(word)).intValue() + 1));
         }
         tagcloud.setCountedWordMap(wordsCnt);
         return wordsCnt;
     }
 
-    private Map stemmer(TagCloud tagcloud, int lang){
+    private Map stemmer(TagCloud tagcloud, int lang)
+    {
         Map words = tagcloud.getCountedWordMap();
         FrenchStemmer stemmer;
         Map stemmedWordMap = new HashMap();
 
-            stemmer = new FrenchStemmer();
-
+        stemmer = new FrenchStemmer();
 
         Iterator it = words.keySet().iterator();
-        while(it.hasNext()) {
+        while (it.hasNext()) {
             String word = (String) it.next();
             if (word.length() <= 2)
                 continue;
@@ -269,7 +287,7 @@ public class AutoTagPlugin extends XWikiDefaultPlugin implements XWikiPluginInte
         return stemmedWordMap;
     }
 
-/*
+    /*
     private Map stemmer(TagCloud tagcloud, int lang){
         Map words = tagcloud.getCountedWordMap();
         SnowballProgram stemmer;
@@ -299,7 +317,9 @@ public class AutoTagPlugin extends XWikiDefaultPlugin implements XWikiPluginInte
         return stemmedWordMap;
     }
     */
-    private String[] splitWords(TagCloud tagcloud){
+
+    private String[] splitWords(TagCloud tagcloud)
+    {
         String text = tagcloud.getText();
         text = text.replaceAll("\n", " ");
         text = text.replaceAll("\r", " ");

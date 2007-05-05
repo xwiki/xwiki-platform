d445ca3b944627b64eb539f6a68485d8fbdbff47
sdumitriu <sergiu@xwiki.com>
2007-05-05 11:24:35 +0000

    Apply codestyle.
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki/trunk@3102 f329d543-caf0-0310-9063-dda96c69346f

--- core/src/main/java/com/xpn/xwiki/web/PropUpdateAction.java
+++ core/src/main/java/com/xpn/xwiki/web/PropUpdateAction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006, XpertNet SARL, and individual contributors as indicated
+ * Copyright 2006-2007, XpertNet SARL, and individual contributors as indicated
  * by the contributors.txt.
  *
  * This is free software; you can redistribute it and/or modify it
@@ -17,9 +17,13 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  *
- * @author sdumitriu
  */
 package com.xpn.xwiki.web;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 import com.xpn.xwiki.XWiki;
 import com.xpn.xwiki.XWikiContext;
@@ -28,86 +32,88 @@ import com.xpn.xwiki.doc.XWikiDocument;
 import com.xpn.xwiki.objects.classes.BaseClass;
 import com.xpn.xwiki.objects.classes.PropertyClass;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+public class PropUpdateAction extends XWikiAction
+{
+    public boolean propUpdate(XWikiContext context) throws XWikiException
+    {
+        XWiki xwiki = context.getWiki();
+        XWikiDocument doc = context.getDoc();
+        XWikiForm form = context.getForm();
+        XWikiDocument olddoc = (XWikiDocument) doc.clone();
 
-public class PropUpdateAction extends XWikiAction {
-	public boolean propUpdate(XWikiContext context) throws XWikiException {
-		XWiki xwiki = context.getWiki();
-		XWikiDocument doc = context.getDoc();
-		XWikiForm form = context.getForm();
-		XWikiDocument olddoc = (XWikiDocument) doc.clone();
+        // Prepare new class
+        BaseClass bclass = doc.getxWikiClass();
+        BaseClass bclass2 = (BaseClass) bclass.clone();
+        bclass2.setFields(new HashMap());
 
-		// Prepare new class
-		BaseClass bclass = doc.getxWikiClass();
-		BaseClass bclass2 = (BaseClass) bclass.clone();
-		bclass2.setFields(new HashMap());
+        // Prepare a Map for field renames
+        Map fieldsToRename = new HashMap();
 
-		// Prepare a Map for field renames
-		Map fieldsToRename = new HashMap();
+        Iterator it = bclass.getFieldList().iterator();
+        while (it.hasNext()) {
+            PropertyClass origproperty = (PropertyClass) it.next();
+            PropertyClass property = (PropertyClass) origproperty.clone();
+            String name = property.getName();
+            Map map = ((EditForm) form).getObject(name);
+            property.getxWikiClass(context).fromMap(map, property);
+            String newname = property.getName();
 
-		Iterator it = bclass.getFieldList().iterator();
-		while (it.hasNext()) {
-			PropertyClass origproperty = (PropertyClass) it.next();
-			PropertyClass property = (PropertyClass) origproperty.clone();
-			String name = property.getName();
-			Map map = ((EditForm) form).getObject(name);
-			property.getxWikiClass(context).fromMap(map, property);
-			String newname = property.getName();
+            if (newname == null || newname.equals("") || !newname.matches("[\\w\\.\\-\\_]+")) {
+                context.put("message", "propertynamenotcorrect");
+                return true;
+            }
 
-			if (newname == null || newname.equals("") || !newname.matches("[\\w\\.\\-\\_]+")) {
-				context.put("message", "propertynamenotcorrect");
-				return true;
-			}
+            if (newname.indexOf(" ") != -1) {
+                newname = newname.replaceAll(" ", "");
+                property.setName(newname);
+            }
+            bclass2.addField(newname, property);
+            if (!newname.equals(name)) {
+                fieldsToRename.put(name, newname);
+                bclass2.addPropertyForRemoval(origproperty);
+            }
+        }
 
-			if (newname.indexOf(" ") != -1) {
-				newname = newname.replaceAll(" ", "");
-				property.setName(newname);
-			}
-			bclass2.addField(newname, property);
-			if (!newname.equals(name)) {
-				fieldsToRename.put(name, newname);
-				bclass2.addPropertyForRemoval(origproperty);
-			}
-		}
+        doc.setxWikiClass(bclass2);
+        doc.renameProperties(bclass.getName(), fieldsToRename);
+        xwiki.saveDocument(doc, olddoc, context);
 
-		doc.setxWikiClass(bclass2);
-		doc.renameProperties(bclass.getName(), fieldsToRename);
-		xwiki.saveDocument(doc, olddoc, context);
+        // We need to load all documents that use this property and rename it
+        if (fieldsToRename.size() > 0) {
+            List list =
+                xwiki.getStore().searchDocumentsNames(
+                    ", BaseObject as obj where obj.name=" + xwiki.getFullNameSQL()
+                        + " and obj.className='" + Utils.SQLFilter(bclass.getName()) + "' and "
+                        + xwiki.getFullNameSQL() + "<> '" + Utils.SQLFilter(bclass.getName())
+                        + "'", context);
+            for (int i = 0; i < list.size(); i++) {
+                XWikiDocument doc2 = xwiki.getDocument((String) list.get(i), context);
+                doc2.renameProperties(bclass.getName(), fieldsToRename);
+                xwiki.saveDocument(doc2, doc2, context);
+            }
+        }
+        xwiki.flushCache();
+        return false;
+    }
 
-		// We need to load all documents that use this property and rename it
-		if (fieldsToRename.size() > 0) {
-			List list = xwiki.getStore().searchDocumentsNames(
-					", BaseObject as obj where obj.name=" + xwiki.getFullNameSQL() + " and obj.className='" + Utils.SQLFilter(bclass.getName()) + "' and "
-							+ xwiki.getFullNameSQL() + "<> '" + Utils.SQLFilter(bclass.getName()) + "'", context);
-			for (int i = 0; i < list.size(); i++) {
-				XWikiDocument doc2 = xwiki.getDocument((String) list.get(i), context);
-				doc2.renameProperties(bclass.getName(), fieldsToRename);
-				xwiki.saveDocument(doc2, doc2, context);
-			}
-		}
-		xwiki.flushCache();
-		return false;
-	}
+    public boolean action(XWikiContext context) throws XWikiException
+    {
+        try {
+            if (propUpdate(context)) {
+                return true;
+            }
+        } catch (XWikiException ex) {
+            context.put("exception", ex);
+            return true;
+        }
+        // forward to view
+        String redirect = Utils.getRedirect("view", context);
+        sendRedirect(context.getResponse(), redirect);
+        return false;
+    }
 
-	public boolean action(XWikiContext context) throws XWikiException {
-		try {
-			if (propUpdate(context)) {
-				return true;
-			}
-		} catch (XWikiException ex) {
-			context.put("exception", ex);
-			return true;
-		}
-		// forward to view
-		String redirect = Utils.getRedirect("view", context);
-		sendRedirect(context.getResponse(), redirect);
-		return false;
-	}
-
-	public String render(XWikiContext context) throws XWikiException {
-		return "exception";
-	}
+    public String render(XWikiContext context) throws XWikiException
+    {
+        return "exception";
+    }
 }

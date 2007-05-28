eb76af82ec82b63932767487919e9ca762ae431c
vmassol <vincent@massol.net>
2007-05-28 11:49:57 +0000

    Applied project code style
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki/trunk@3462 f329d543-caf0-0310-9063-dda96c69346f

--- core/src/main/java/com/xpn/xwiki/web/DeleteAction.java
+++ core/src/main/java/com/xpn/xwiki/web/DeleteAction.java
@@ -27,19 +27,23 @@ import com.xpn.xwiki.XWikiContext;
 import com.xpn.xwiki.XWikiException;
 import com.xpn.xwiki.doc.XWikiDocument;
 
-public class DeleteAction extends XWikiAction {
-	public boolean action(XWikiContext context) throws XWikiException {
+public class DeleteAction extends XWikiAction
+{
+    public boolean action(XWikiContext context) throws XWikiException
+    {
         XWiki xwiki = context.getWiki();
         XWikiRequest request = context.getRequest();
         XWikiResponse response = context.getResponse();
         XWikiDocument doc = context.getDoc();
-        if(doc.isNew()){
-        	return true;
+        if (doc.isNew()) {
+            return true;
         }
         String confirm = request.getParameter("confirm");
-        if ((confirm!=null)&&(confirm.equals("1"))) {
+        if ((confirm != null) && (confirm.equals("1"))) {
             String language = xwiki.getLanguagePreference(context);
-            if ((language==null)||(language.equals(""))||language.equals(doc.getDefaultLanguage())) {
+            if ((language == null) || (language.equals("")) ||
+                language.equals(doc.getDefaultLanguage()))
+            {
                 xwiki.deleteAllDocuments(doc, context);
             } else {
                 // Only delete the translation
@@ -49,22 +53,23 @@ public class DeleteAction extends XWikiAction {
             return true;
         } else {
             String redirect = Utils.getRedirect(request, null);
-            if (redirect==null)
+            if (redirect == null) {
                 return true;
-            else {
+            } else {
                 sendRedirect(response, redirect);
                 return false;
             }
         }
-	}
+    }
 
-	public String render(XWikiContext context) throws XWikiException {
+    public String render(XWikiContext context) throws XWikiException
+    {
         XWikiRequest request = context.getRequest();
         String confirm = request.getParameter("confirm");
-        if ((confirm!=null)&&(confirm.equals("1"))) {
+        if ((confirm != null) && (confirm.equals("1"))) {
             return "deleted";
         } else {
             return "delete";
         }
-	}
+    }
 }

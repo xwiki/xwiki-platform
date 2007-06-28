1153c5a6367becea8c932b7f84814f651ae954bd
sdumitriu <sergiu@xwiki.com>
2007-06-28 09:15:53 +0000

    Apply codestyle
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-platform/core/trunk@3751 f329d543-caf0-0310-9063-dda96c69346f

--- src/main/java/com/xpn/xwiki/web/ImportAction.java
+++ src/main/java/com/xpn/xwiki/web/ImportAction.java
@@ -11,21 +11,20 @@ import com.xpn.xwiki.plugin.packaging.DocumentInfoAPI;
 import java.util.List;
 
 /**
- * Created by IntelliJ IDEA.
- * User: ldubost
- * Date: 11 dec. 2006
- * Time: 17:18:05
- * To change this template use File | Settings | File Templates.
+ * Created by IntelliJ IDEA. User: ldubost Date: 11 dec. 2006 Time: 17:18:05 To change this template
+ * use File | Settings | File Templates.
  */
-public class ImportAction extends XWikiAction {
+public class ImportAction extends XWikiAction
+{
 
-    public String render(XWikiContext context) throws XWikiException {
+    public String render(XWikiContext context) throws XWikiException
+    {
         try {
             XWikiRequest request = context.getRequest();
             XWikiResponse response = context.getResponse();
             XWikiDocument doc = context.getDoc();
-            String name =  request.get("name");
-            String action =  request.get("action");
+            String name = request.get("name");
+            String action = request.get("action");
             String[] pages = request.getParameterValues("pages");
 
             if (!context.getWiki().getRightService().hasAdminRights(context)) {
@@ -33,11 +32,12 @@ public class ImportAction extends XWikiAction {
                 return "exception";
             }
 
-            if (name==null) {
+            if (name == null) {
                 return "import";
             }
 
-            PackageAPI importer = ((PackageAPI) context.getWiki().getPluginApi("package", context));
+            PackageAPI importer =
+                ((PackageAPI) context.getWiki().getPluginApi("package", context));
 
             // list files
             if ("getPackageInfos".equals(action)) {
@@ -53,24 +53,24 @@ public class ImportAction extends XWikiAction {
                 importer.Import(packFile.getContent(context));
                 String all = request.get("all");
                 if (!"1".equals(all)) {
-                    if (pages!=null) {
+                    if (pages != null) {
                         List filelist = importer.getFiles();
-                        for (int i=0;i<filelist.size();i++) {
+                        for (int i = 0; i < filelist.size(); i++) {
                             DocumentInfoAPI dia = (DocumentInfoAPI) filelist.get(i);
                             dia.setAction(DocumentInfo.ACTION_SKIP);
                         }
 
-                        for (int i=0;i<pages.length;i++) {
+                        for (int i = 0; i < pages.length; i++) {
                             String pageName = pages[i];
                             String language = request.get("language_" + pageName);
-                            String actionName =  "action_" + pageName;
-                            if (language!=null)
-                             actionName = "_" + language;
+                            String actionName = "action_" + pageName;
+                            if (language != null) {
+                                actionName = "_" + language;
+                            }
                             String defaultAction = request.get(actionName);
                             int iAction;
-                            if ((defaultAction==null)||(defaultAction.equals(""))) {
+                            if ((defaultAction == null) || (defaultAction.equals(""))) {
                                 iAction = DocumentInfo.ACTION_OVERWRITE;
-
                             } else {
                                 try {
                                     iAction = Integer.parseInt(defaultAction);
@@ -80,10 +80,11 @@ public class ImportAction extends XWikiAction {
                             }
 
                             String docName = pageName.replaceAll(":.*$", "");
-                            if (language==null)
-                             importer.setDocumentAction(docName, iAction);
-                            else
-                             importer.setDocumentAction(docName, language, iAction);
+                            if (language == null) {
+                                importer.setDocumentAction(docName, iAction);
+                            } else {
+                                importer.setDocumentAction(docName, language, iAction);
+                            }
                         }
                     }
                     // Import files
@@ -93,8 +94,9 @@ public class ImportAction extends XWikiAction {
             }
         } catch (Exception e) {
             throw new XWikiException(XWikiException.MODULE_XWIKI_APP,
-                    XWikiException.ERROR_XWIKI_APP_EXPORT,
-                    "Exception while importing", e);
+                XWikiException.ERROR_XWIKI_APP_EXPORT,
+                "Exception while importing",
+                e);
         }
         return null;
     }

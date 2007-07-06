9b8c1cbe6c23433969aec44f551826fa16464913
vmassol <vincent@massol.net>
2007-07-06 11:59:38 +0000

    * Applied code style * Small refactoring to make the code more legible
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-platform/core/trunk@3840 f329d543-caf0-0310-9063-dda96c69346f

--- src/main/java/com/xpn/xwiki/web/ExportAction.java
+++ src/main/java/com/xpn/xwiki/web/ExportAction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006, XpertNet SARL, and individual contributors as indicated
+ * Copyright 2006-2007, XpertNet SARL, and individual contributors as indicated
  * by the contributors.txt.
  *
  * This is free software; you can redistribute it and/or modify it
@@ -16,8 +16,6 @@
  * License along with this software; if not, write to the Free
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
- *
- * @author ludovic
  */
 package com.xpn.xwiki.web;
 
@@ -29,111 +27,141 @@ import com.xpn.xwiki.plugin.packaging.PackageAPI;
 
 import java.io.IOException;
 
-
 /**
  * Exports in XAR, PDF or RTF formats
  */
-public class ExportAction extends XWikiAction {
+public class ExportAction extends XWikiAction
+{
+    /**
+     * {@inheritDoc}
+     * @see XWikiAction#render(XWikiContext)
+     */
+    public String render(XWikiContext context) throws XWikiException
+    {
+        String defaultPage;
 
-    public String render(XWikiContext context) throws XWikiException {
         try {
             XWikiRequest request = context.getRequest();
             String format = request.get("format");
 
-            // Exports in xar format
-            if ((format==null)||(format.equals("xar"))) {
-                String history = request.get("history");
-                String backup = request.get("backup");
-                String author= request.get("author");
-                String description = request.get("description");
-                String licence = request.get("licence");
-                String version = request.get("version");
-                String name = request.get("name");
-                String[] pages = request.getParameterValues("pages");
-                boolean isBackup = ((pages==null)||(pages.length==0));
-
-
-                if (!context.getWiki().getRightService().hasAdminRights(context)) {
-                    context.put("message", "needadminrights");
-                    return "exception";
-                }
-
-                if (name==null) {
-                    return "export";
-                }
-
-                PackageAPI export = ((PackageAPI) context.getWiki().getPluginApi("package", context));
-                if ("true".equals(history))
-                    export.setWithVersions(true);
-                else
-                    export.setWithVersions(false);
-
-                if (author!=null)
-                    export.setAuthorName(author);
-
-                if (description!=null)
-                    export.setDescription(description);
-
-                if (licence!=null)
-                    export.setLicence(licence);
-
-                if (version!=null)
-                    export.setVersion(version);
-
-                if (name.trim().equals("")) {
-                    if (isBackup)
-                        name = "backup";
-                    else
-                        name = "export";
-                }
-
-                if ("true".equals(backup))
-                    export.setBackupPack(true);
-
-                export.setName(name);
-
-                if (isBackup)
-                    export.backupWiki();
-                else {
-                    if (pages!=null) {
-                        for (int i=0;i<pages.length;i++) {
-                            String pageName = pages[i];
-                            String defaultAction = request.get("action_" + pageName);
-                            int iAction = 0;
-                            try {
-                                iAction = Integer.parseInt(defaultAction);
-                            } catch (Exception e) {
-                                iAction = 0;
-                            }
-                            export.add(pageName, iAction);
-                        }
-                    }
-                    export.export();
-                }
+            if ((format == null) || (format.equals("xar"))) {
+                defaultPage = exportXAR(context);
             } else {
-                // PDF or RTF format
-                XWikiURLFactory urlf = context.getWiki().getURLFactoryService().createURLFactory(XWikiContext.MODE_PDF, context);
-                context.setURLFactory(urlf);
-                PdfExportImpl pdfexport = new PdfExportImpl();
-                XWikiDocument doc = context.getDoc();
-                handleRevision(context);
-
-
-                int type = PdfExportImpl.PDF;
-                if (format.equals("rtf")) {
-                    type = PdfExportImpl.RTF;
-                } else {
-                    format = "pdf";
-                }
-
-                context.getResponse().setContentType("application/" + format);
-                context.getResponse().addHeader("Content-disposition", "inline; filename=" + Utils.encode(doc.getSpace(), context) + "_" + Utils.encode(doc.getName(), context) + "." + format);
-                pdfexport.export(doc, context.getResponse().getOutputStream(), type, context);
+                defaultPage = exportPDFOrRTF(format, context);
             }
         } catch (Exception e) {
             throw new XWikiException(XWikiException.MODULE_XWIKI_APP,
-                    XWikiException.ERROR_XWIKI_APP_EXPORT,
-                    "Exception while exporting", e);
+                XWikiException.ERROR_XWIKI_APP_EXPORT,
+                "Exception while exporting", e);
+        }
+
+        return defaultPage;
+    }
+
+    private String exportPDFOrRTF(String format, XWikiContext context)
+        throws XWikiException, IOException
+    {
+        XWikiURLFactory urlf = context.getWiki().getURLFactoryService()
+            .createURLFactory(XWikiContext.MODE_PDF, context);
+        context.setURLFactory(urlf);
+        PdfExportImpl pdfexport = new PdfExportImpl();
+        XWikiDocument doc = context.getDoc();
+        handleRevision(context);
+
+        int type = PdfExportImpl.PDF;
+        if (format.equals("rtf")) {
+            type = PdfExportImpl.RTF;
+        } else {
+            format = "pdf";
+        }
+
+        context.getResponse().setContentType("application/" + format);
+        context.getResponse().addHeader("Content-disposition", "inline; filename=" +
+            Utils.encode(doc.getSpace(), context) + "_" +
+            Utils.encode(doc.getName(), context) + "." + format);
+        pdfexport.export(doc, context.getResponse().getOutputStream(), type, context);
+
+        return null;
+    }
+
+    private String exportXAR(XWikiContext context) throws XWikiException, IOException
+    {
+        XWikiRequest request = context.getRequest();
+
+        String history = request.get("history");
+        String backup = request.get("backup");
+        String author = request.get("author");
+        String description = request.get("description");
+        String licence = request.get("licence");
+        String version = request.get("version");
+        String name = request.get("name");
+        String[] pages = request.getParameterValues("pages");
+        boolean isBackup = ((pages == null) || (pages.length == 0));
+
+        if (!context.getWiki().getRightService().hasAdminRights(context)) {
+            context.put("message", "needadminrights");
+            return "exception";
+        }
+
+        if (name == null) {
+            return "export";
+        }
+
+        PackageAPI export =
+            ((PackageAPI) context.getWiki().getPluginApi("package", context));
+        if ("true".equals(history)) {
+            export.setWithVersions(true);
+        } else {
+            export.setWithVersions(false);
+        }
+
+        if (author != null) {
+            export.setAuthorName(author);
+        }
+
+        if (description != null) {
+            export.setDescription(description);
+        }
+
+        if (licence != null) {
+            export.setLicence(licence);
+        }
+
+        if (version != null) {
+            export.setVersion(version);
+        }
+
+        if (name.trim().equals("")) {
+            if (isBackup) {
+                name = "backup";
+            } else {
+                name = "export";
+            }
+        }
+
+        if ("true".equals(backup)) {
+            export.setBackupPack(true);
+        }
+
+        export.setName(name);
+
+        if (isBackup) {
+            export.backupWiki();
+        } else {
+            if (pages != null) {
+                for (int i = 0; i < pages.length; i++) {
+                    String pageName = pages[i];
+                    String defaultAction = request.get("action_" + pageName);
+                    int iAction;
+                    try {
+                        iAction = Integer.parseInt(defaultAction);
+                    } catch (Exception e) {
+                        iAction = 0;
+                    }
+                    export.add(pageName, iAction);
+                }
+            }
+            export.export();
         }
 
         return null;

50dd98438378d9c6e83ad115db98a3f7388e4917
vmassol <vincent@massol.net>
2007-07-09 12:57:10 +0000

    XWIKI-1104: The Maven2 packager plugin fails to import the XWikiUserTemplate document from the default wiki
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-platform/xwiki-tools/trunk@3882 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-platform-tools/packager/src/main/java/com/xpn/xwiki/tool/backup/Importer.java
+++ xwiki-platform-tools/packager/src/main/java/com/xpn/xwiki/tool/backup/Importer.java
@@ -21,11 +21,16 @@ package com.xpn.xwiki.tool.backup;
 
 import com.xpn.xwiki.XWikiContext;
 import com.xpn.xwiki.XWikiException;
+import com.xpn.xwiki.store.XWikiHibernateStore;
+import com.xpn.xwiki.store.XWikiStoreInterface;
+import com.xpn.xwiki.store.XWikiCacheStore;
 import com.xpn.xwiki.plugin.packaging.Package;
 import com.xpn.xwiki.plugin.packaging.PackageException;
 
 import java.io.File;
 import java.io.IOException;
+
+import org.hibernate.Session;
 
 /**
  * Import a set of XWiki documents into an existing database.
@@ -68,5 +73,55 @@ public class Importer extends AbstractPackager
         }
 
         pack.install(context);
+
+        // We MUST shutdown HSQLDB because otherwise the last transactions will not be flushed
+        // to disk and will be lost. In practice this means the last Document imported has a
+        // very high chance of not making it...
+        // TODO: Find a way to implement this generically for all databases and inside
+        // XWikiHibernateStore (cf http://jira.xwiki.org/jira/browse/XWIKI-471).
+        // WARNING: This Packager may not work with databases other than HSQLDB because of this
+        // and this needs to be fixed ASAP...
+        shutdownHSQLDB(context);
+    }
+
+    /**
+     * Shutdowns HSQLDB.
+     *
+     * @param context the XWiki Context object from which we can retrieve the Store implementation
+     * @throws XWikiException in case of shutdown error
+     */
+    private void shutdownHSQLDB(XWikiContext context) throws XWikiException
+    {
+        XWikiStoreInterface store = context.getWiki().getStore();
+        if (XWikiCacheStore.class.isAssignableFrom(store.getClass())) {
+            store = ((XWikiCacheStore) store).getStore();
+        }
+
+        if (XWikiHibernateStore.class.isAssignableFrom(store.getClass())) {
+            XWikiHibernateStore hibernateStore = (XWikiHibernateStore) store;
+
+            boolean bTransaction = true;
+            try {
+                hibernateStore.checkHibernate(context);
+                bTransaction = hibernateStore.beginTransaction(false, context);
+                Session session = hibernateStore.getSession(context);
+                session.connection().createStatement().execute("SHUTDOWN");
+                if (bTransaction) {
+                    hibernateStore.endTransaction(context, false, false);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw new PackageException(PackageException.ERROR_PACKAGE_UNKNOWN,
+                    "Failed to shutdown database", e);
+            } finally {
+                try {
+                    if (bTransaction) {
+                        hibernateStore.endTransaction(context, false, false);
+                    }
+                } catch (Exception e) {
+                }
+            }
+        }
+
     }
 }

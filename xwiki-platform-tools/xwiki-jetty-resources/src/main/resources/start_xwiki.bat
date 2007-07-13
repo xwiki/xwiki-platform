d4dd4a8dc92334b06a839162715c9d38809c7c38
vmassol <vincent@massol.net>
2007-07-13 08:15:01 +0000

    XE-85: Ensure that the Jetty logs directory always exist
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-platform/xwiki-tools/trunk@3967 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-platform-tools/xwiki-jetty-resources/src/main/resources/start_xwiki.bat
+++ xwiki-platform-tools/xwiki-jetty-resources/src/main/resources/start_xwiki.bat
@@ -4,4 +4,7 @@ set JETTY_HOME=.
 set JETTY_PORT=8080
 set JAVA_OPTS=-Xmx300m
 
+# Jetty requires a logs directory to exist
+if not exist logs mkdir logs
+
 java %JAVA_OPTS% -Dfile.encoding=iso-8859-1 -Djetty.home=%JETTY_HOME% -Djetty.port=%JETTY_PORT% -jar %JETTY_HOME%/start.jar

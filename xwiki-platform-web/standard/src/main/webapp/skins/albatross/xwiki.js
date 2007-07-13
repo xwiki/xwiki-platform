2aacf30cc12966dd60064634b90bbed39efe5e68
vmassol <vincent@massol.net>
2007-07-13 14:38:35 +0000

    XWIKI-1486: Replace xwiki.com reference by xwiki.org for the openHelp() JS macro so that it points to the new XWiki Syntax page XA-PANELS-30: Remove XWiki syntax page and link the Syntax Help Panel to xwiki.org
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-platform/web/trunk@3968 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-platform-web/standard/src/main/webapp/skins/albatross/xwiki.js
+++ xwiki-platform-web/standard/src/main/webapp/skins/albatross/xwiki.js
@@ -128,7 +128,7 @@ function openURL(url) {
 }
 
 function openHelp() {
-  win = open( "http://www.xwiki.com/xwiki/bin/view/Doc/XWikiSyntax?xpage=plain", "syntax", "titlebar=0,width=750,height=480,resizable,scrollbars");
+  win = open( "http://www.xwiki.org/xwiki/bin/view/UserGuide/XWikiSyntax?xpage=print", "XWiki Syntax", "titlebar=0,width=750,height=480,resizable,scrollbars");
   if( win ) {
     win.focus();
   }

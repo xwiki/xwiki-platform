5c305136a6ce0a004e916e3f3e3752eea7886f16
vmassol <vincent@massol.net>
2007-07-13 17:11:42 +0000

    XWIKI-807: Implement a Scheduler plugin
    * Fixed GroovyTask documentation
    * Moved programming check earlier so that unnecessary objects are not created
    * Handled case when the user ddidn't have programming rights when the job was scheduled
    * Added useful information to excpetions that were missing information (This is key and so many people don't handle exceptions correctly...)
    
    NOTE: No tests yet.
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-platform/core/trunk@3970 f329d543-caf0-0310-9063-dda96c69346f

--- src/main/java/com/xpn/xwiki/plugin/scheduler/GroovyTask.java
+++ src/main/java/com/xpn/xwiki/plugin/scheduler/GroovyTask.java
@@ -30,24 +30,68 @@ import com.xpn.xwiki.objects.BaseObject;
 import com.xpn.xwiki.api.Api;
 import com.xpn.xwiki.api.Context;
 
+/**
+ * The task that will get executed by the Scheduler when the Job is triggered. This task in
+ * turn calls a Groovy script to perform the execution.
+ *
+ * <p><b>Important:</b>: Note that the script will execute in the XWiki Context that was set at
+ * the time the Job was scheduled for execution. For example calling <code>context.getDoc()</code>
+ * will return the current document that was set at that time and not the current document that is
+ * set when the Groovy script executes...
+ *
+ * @version $Id: $
+ */
 public class GroovyTask implements Job
 {
+    /**
+     * Executes the Groovy script passed in the <code>script</code> property of the
+     * {@link com.xpn.xwiki.plugin.scheduler.SchedulerPlugin#TASK_CLASS} object extracted from the
+     * XWiki context passed in the Quartz's Job execution context. The XWiki Task object is
+     * looked for in the current document that was set in the context at the time the Job was
+     * scheduled.
+     *
+     * @param context the Quartz execution context containing the XWiki context from which the
+     *        script to execute is retrieved
+     * @exception JobExecutionException if the script fails to execute or if the user didn't have
+     *            programming rights when the Job was scheduled
+     * @see Job#execute(org.quartz.JobExecutionContext)
+     */
     public void execute(JobExecutionContext context) throws JobExecutionException
     {
         try {
             JobDataMap data = context.getJobDetail().getJobDataMap();
-            Context xcontext = (Context) data.get("context");
-            int task = data.getInt("task");
 
-            Binding binding = new Binding(data.getWrappedMap());
-            GroovyShell shell = new GroovyShell(binding);
-            BaseObject object = xcontext.getDoc().getObject(SchedulerPlugin.TASK_CLASS, task);
+            // The XWiki context was saved in the Job execution data map. Get it as we'll retrieve
+            // the script to execute from it.
+            Context xcontext = (Context) data.get("context");
+
             Api api = new Api(xcontext.getContext());
+
             if (api.hasProgrammingRights()) {
+
+                // The current task id. This is needed to find the correct XWiki Task object that
+                // was stored in the current document as there can be several tasks stored in that
+                // document.
+                int task = data.getInt("task");
+
+                // Get the correct task object from the current doc set when the Job was
+                // scheduled.
+                BaseObject object = xcontext.getDoc().getObject(SchedulerPlugin.TASK_CLASS, task);
+
+                Binding binding = new Binding(data.getWrappedMap());
+                GroovyShell shell = new GroovyShell(binding);
+
+                // Execute the Groovy script
                 shell.evaluate(object.getLargeStringValue("script"));
+                
+            } else {
+                throw new JobExecutionException("The user [" + xcontext.getUser() + "] didn't have "
+                    + "programming rights when the job [" + context.getJobDetail().getName()
+                    + "] was scheduled.");
             }
         } catch (CompilationFailedException e) {
-            throw new JobExecutionException(e);
+            throw new JobExecutionException("Failed to execute script for job ["
+                + context.getJobDetail().getName() + "]", e, true);
         }
     }
 }

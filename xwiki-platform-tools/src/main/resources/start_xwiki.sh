7bf9f641ce04b2bdba1c0c67741c0b5cd9b79160
sdumitriu <sergiu@xwiki.com>
2007-08-03 13:39:30 +0000

    XWIKI-1557: Starting XWiki messes the locale of the shell Fixed
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-platform/xwiki-tools/trunk/xwiki-jetty-resources@4086 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-platform-tools/src/main/resources/start_xwiki.sh
+++ xwiki-platform-tools/src/main/resources/start_xwiki.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-export LANG=fr_FR.ISO8859-1
 JETTY_HOME=.
 JAVA_OPTS=-Xmx300m
 
@@ -15,4 +14,4 @@ echo Starting Jetty on port $JETTY_PORT ...
 echo Logs are in the logs/ directory
 
 mkdir -p logs 2>/dev/null
-nohup java $JAVA_OPTS -Dfile.encoding=iso-8859-1 -Djetty.port=$JETTY_PORT -Djetty.home=$JETTY_HOME -jar $JETTY_HOME/start.jar > logs/xwiki_output.log 2> logs/xwiki_errors.log &
+LANG=fr_FR.ISO8859-1 nohup java $JAVA_OPTS -Dfile.encoding=iso-8859-1 -Djetty.port=$JETTY_PORT -Djetty.home=$JETTY_HOME -jar $JETTY_HOME/start.jar > logs/xwiki_output.log 2> logs/xwiki_errors.log &

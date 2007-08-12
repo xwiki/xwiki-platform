31c75c660c83f13893419b9636b4c3a5417859bb
amelentev <melenartem@ya.ru>
2007-08-12 18:14:24 +0000

    XWIKI-1468: History page version and normal page version do not match add "resave api" for migration.
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-platform/core/trunk@4204 f329d543-caf0-0310-9063-dda96c69346f

--- src/main/java/com/xpn/xwiki/doc/XWikiDocumentArchive.java
+++ src/main/java/com/xpn/xwiki/doc/XWikiDocumentArchive.java
@@ -1,11 +1,18 @@
 package com.xpn.xwiki.doc;
 
+import com.xpn.xwiki.XWikiContext;
 import com.xpn.xwiki.XWikiException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.tools.ant.filters.StringInputStream;
 import org.suigeneris.jrcs.rcs.Archive;
+import org.suigeneris.jrcs.rcs.Node;
+import org.suigeneris.jrcs.rcs.Version;
 import org.suigeneris.jrcs.util.ToString;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class XWikiDocumentArchive {
     private static final Log log = LogFactory.getLog(XWikiDocumentArchive.class);
@@ -71,6 +78,10 @@ public class XWikiDocumentArchive {
     }
 
     public void updateArchive(String docname, String text) throws XWikiException {
+        updateArchive(docname, text, "1.0");
+    }
+
+    public void updateArchive(String docname, String text, String version) throws XWikiException {
 
         // JRCS used the user.name System property to set the author of a change. However JRCS
         // has a bug when the user name has a space in the name
@@ -90,7 +101,7 @@ public class XWikiDocumentArchive {
             if (archive != null)
                 archive.addRevision(lines, "");
             else
-                archive = new Archive(lines, docname, "1.0");
+                archive = new Archive(lines, docname, version);
         }
         catch (Exception e) {
             Object[] args = { docname };
@@ -145,5 +156,76 @@ public class XWikiDocumentArchive {
     {
         return "id = [" + getId() + "], archive = ["
             + (getRCSArchive() == null ? "null" : getRCSArchive().toString()) + "]"; 
+    }
+
+    public String getRevision(String version) throws XWikiException {
+        return getRevision(new Version(version));    
+    }
+
+    public String getRevision(Version version) throws XWikiException {
+        try {
+            Object[] text = archive.getRevision(version);
+            StringBuffer content = new StringBuffer();
+            for (int i=0;i<text.length;i++) {
+                String line = text[i].toString();
+                content.append(line);
+                content.append("\n");
+            }
+            return content.toString();
+        }
+        catch (Exception e) {
+            Object[] args = { "" + getId() };
+            throw new XWikiException(XWikiException.MODULE_XWIKI_STORE, XWikiException.ERROR_XWIKI_STORE_ARCHIVEFORMAT,
+                    "Exception while manipulating archive with id {0}", e, args);
+        }
+    }
+
+    public String getLastVersion() {
+        if (archive==null)
+            return "1.1";
+        return archive.getRevisionVersion().toString();
+    }
+
+
+    /**
+     * Fix for bug XWIKI-1468 allowing to correct the RCS files
+     * @param context
+     * @throws XWikiException
+     * @deprecated
+     */
+    public void fixRCSArchive(XWikiContext context) throws XWikiException {
+        XWikiDocumentArchive newDocArchive = new XWikiDocumentArchive(this.getId());
+        String docname = getRCSArchive().getDesc();
+        Node[] nodes = archive.changeLog();
+        for (int i=0;i<nodes.length;i++) {
+            Node node = nodes[i];
+            String version = node.getVersion().toString();
+            String xml = getRevision(version);
+            XWikiDocument doc = new XWikiDocument();
+            doc.fromXML(xml);
+            doc.setVersion(version);
+            newDocArchive.updateArchive(docname, doc.toXML(context), version);
+        }
+        Archive newArchive = newDocArchive.getRCSArchive();
+        Node[] newNodes = newArchive.changeLog();
+        for (int i=0;i<newNodes.length;i++) {
+            Date date = nodes[i].getDate();
+            GregorianCalendar cal = new GregorianCalendar();
+            cal.setTime(date);
+            int[] newDate = new int[6];
+            newDate[0] = cal.get(Calendar.YEAR);
+            newDate[1] = cal.get(Calendar.MONTH) + 1;
+            newDate[2] = cal.get(Calendar.DAY_OF_MONTH);
+            newDate[3] = cal.get(Calendar.HOUR_OF_DAY);
+            newDate[4] = cal.get(Calendar.MINUTE);
+            newDate[5] = cal.get(Calendar.SECOND);
+            newNodes[i].setDate(newDate);
+        }
+
+        setRCSArchive(newArchive);
+        if (context!=null) {
+            // force saving directly
+            (context.getWiki().getVersioningStore()).saveXWikiDocArchive(this, true, context);
+        }
     }
 }

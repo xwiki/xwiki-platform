8df55fb32c2d78ce5b6df443fb147f7f11b8d458
ldubost <ludovic@xwiki.com>
2007-08-13 22:42:46 +0000

    XWIKI-1162 New Diff plugin at the word level and interface to display differences. DiffPlugin is activated by default. Fallsback to older presentation if diff plugin not activated
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-platform/core/trunk@4230 f329d543-caf0-0310-9063-dda96c69346f

---
+++ src/main/java/com/xpn/xwiki/plugin/diff/DiffPluginApi.java
@@ -0,0 +1,84 @@
+package com.xpn.xwiki.plugin.diff;
+
+import com.xpn.xwiki.api.Api;
+import com.xpn.xwiki.XWikiContext;
+import com.xpn.xwiki.XWikiException;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ldubost
+ * Date: 1 mai 2007
+ * Time: 15:55:04
+ * To change this template use File | Settings | File Templates.
+ */
+public class DiffPluginApi extends Api {
+    private DiffPlugin plugin;
+
+    public DiffPluginApi(DiffPlugin plugin, XWikiContext context) {
+        super(context);
+        setPlugin(plugin);
+    }
+
+    public DiffPlugin getPlugin() {
+        return plugin;
+    }
+
+    public void setPlugin(DiffPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    /**
+     * Return a list of Delta objects representing line differences in text1 and text2
+     * @param text1 original content
+     * @param text2 revised content
+     * @return list of Delta objects
+     */
+    public List getDifferencesAsList(String text1, String text2) throws XWikiException {
+        return plugin.getDifferencesAsList(text1, text2);
+    }
+
+    /**
+     * Return an html blocks representing line diffs between text1 and text2
+     * @param text1 original content
+     * @param text2 revised content
+     * @return list of Delta objects
+     */
+    public String getDifferencesAsHTML(String text1, String text2) throws XWikiException {
+        return plugin.getDifferencesAsHTML(text1, text2);
+    }
+    
+    /**
+     * Return an html blocks representing line diffs between text1 and text2
+     * @param text1 original content
+     * @param text2 revised content
+     * @param allDoc view all document or only changes
+     * @return list of Delta objects
+     */
+    public String getDifferencesAsHTML(String text1, String text2, boolean allDoc) throws XWikiException {
+        return plugin.getDifferencesAsHTML(text1, text2, allDoc);
+    }
+
+    /**
+     * Return a list of Delta objects representing word differences in text1 and text2
+     * @param text1 original content
+     * @param text2 revised content
+     * @return list of Delta objects
+     */
+    public List getWordDifferencesAsList(String text1, String text2) throws XWikiException {
+        return plugin.getWordDifferencesAsList(text1, text2);
+    }
+
+    /**
+     * Return an html blocks representing word diffs between text1 and text2
+     * @param text1 original content
+     * @param text2 revised content
+     * @return list of Delta objects
+     */
+    public String getWordDifferencesAsHTML(String text1, String text2) throws XWikiException {
+        return plugin.getWordDifferencesAsHTML(text1, text2);
+    }
+
+}
+
---
+++ src/test/java/com/xpn/xwiki/plugin/diff/DiffTest.java
@@ -0,0 +1,158 @@
+package com.xpn.xwiki.plugin.diff;
+
+import com.xpn.xwiki.XWikiException;
+
+import java.util.List;
+
+import org.suigeneris.jrcs.diff.delta.Chunk;
+import org.suigeneris.jrcs.diff.delta.Delta;
+
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ldubost
+ * Date: 1 mai 2007
+ * Time: 15:06:14
+ * To change this template use File | Settings | File Templates.
+ */
+public class DiffTest extends org.jmock.cglib.MockObjectTestCase
+{
+    private DiffPlugin plugin;
+
+      protected void setUp()
+    {
+        this.plugin = new DiffPlugin("diff", DiffPlugin.class.getName(), null);
+    }
+
+    public void testSimpleLineDiff() throws XWikiException {
+        String text1 = "A";
+        String text2 = "A B";
+        List diffs = this.plugin.getDifferencesAsList(text1, text2);
+        assertEquals("There should be one difference", 1, diffs.size());
+        Delta delta = (Delta) diffs.get(0);
+        Chunk orig = delta.getOriginal();
+        Chunk revised = delta.getRevised();
+        assertEquals("Original should be", "A", orig.toString());
+        assertEquals("Revised should be", "A B", revised.toString());
+    }
+
+    public void testSimpleLineDiff2() throws XWikiException {
+        String text1 = "A\nB\nC";
+        String text2 = "A\nB B\nC";
+        List diffs = this.plugin.getDifferencesAsList(text1, text2);
+        assertEquals("There should be one difference", 1, diffs.size());
+        Delta delta = (Delta) diffs.get(0);
+        Chunk orig = delta.getOriginal();
+        Chunk revised = delta.getRevised();
+        assertEquals("Original should be", "B", orig.toString());
+        assertEquals("Revised should be", "B B", revised.toString());
+    }
+
+    public void testSimpleWordDiff() throws XWikiException {
+        String text1 = "I love Paris";
+        String text2 = "I live in Paris";
+        List diffs = this.plugin.getWordDifferencesAsList(text1, text2);
+        assertEquals("There should be two differences", 1, diffs.size());
+        Delta delta1 = (Delta) diffs.get(0);
+        Chunk orig1 = delta1.getOriginal();
+        Chunk revised1 = delta1.getRevised();
+        Delta delta2 = (Delta) diffs.get(0);
+        Chunk orig2 = delta2.getOriginal();
+        Chunk revised2 = delta2.getRevised();
+        assertEquals("Original 1 should be", "love", orig1.toString());
+        assertEquals("Revised 1 should be", "livein", revised1.toString());
+    }
+
+    public void testSimpleWordDiff2() throws XWikiException {
+        String text1 = "I love Paris and London";
+        String text2 = "I live in Paris and London";
+        List diffs = this.plugin.getWordDifferencesAsList(text1, text2);
+        assertEquals("There should be two differences", 1, diffs.size());
+        Delta delta1 = (Delta) diffs.get(0);
+        Chunk orig1 = delta1.getOriginal();
+        Chunk revised1 = delta1.getRevised();
+
+        assertEquals("Original 1 should be", "love", orig1.toString());
+        assertEquals("Revised 1 should be", "livein", revised1.toString());
+    }
+
+    public void testSimpleWordDiff3() throws XWikiException {
+        String text1 = "I love Paris and London";
+        String text2 = "I love London and Paris";
+        List diffs = this.plugin.getWordDifferencesAsList(text1, text2);
+        assertEquals("There should be two differences", 2, diffs.size());
+        Delta delta1 = (Delta) diffs.get(0);
+        Chunk orig1 = delta1.getOriginal();
+        Chunk revised1 = delta1.getRevised();
+        Delta delta2 = (Delta) diffs.get(1);
+        Chunk orig2 = delta2.getOriginal();
+        Chunk revised2 = delta2.getRevised();
+
+        assertEquals("Original 1 should be", "Parisand", orig1.toString());
+        assertEquals("Revised 1 should be", "", revised1.toString());
+        assertEquals("Original 2 should be", "", orig2.toString());
+        assertEquals("Revised 2 should be", "andParis", revised2.toString());
+    }
+
+    public void testSimpleWordDiff4() throws XWikiException {
+        String text1 = "I love Paris and I like London";
+        String text2 = "I love London and I like Paris";
+        List diffs = this.plugin.getWordDifferencesAsList(text1, text2);
+        assertEquals("There should be two differences", 2, diffs.size());
+        Delta delta1 = (Delta) diffs.get(0);
+        Chunk orig1 = delta1.getOriginal();
+        Chunk revised1 = delta1.getRevised();
+        Delta delta2 = (Delta) diffs.get(1);
+        Chunk orig2 = delta2.getOriginal();
+        Chunk revised2 = delta2.getRevised();
+
+        assertEquals("Original 1 should be", "Paris", orig1.toString());
+        assertEquals("Revised 1 should be", "London", revised1.toString());
+        assertEquals("Original 2 should be", "London", orig2.toString());
+        assertEquals("Revised 2 should be", "Paris", revised2.toString());
+    }
+
+    /*
+    public void testSimpleWordDiffAsHTML() throws XWikiException {
+         String text1 = "A";
+         String text2 = "A B";
+         String html = this.plugin.getWordDifferencesAsHTML(text1, text2);
+         assertEquals("Diff is incorrect", "<div class=\"diffmodifiedline\"><span class=\"diffremoveword\">A</span><span class=\"diffaddword\">A B</span></div>", html);
+     }
+
+    public void testSimpleWordDiffAsHTML2() throws XWikiException {
+         String text1 = "A C";
+         String text2 = "A B";
+         String html = this.plugin.getWordDifferencesAsHTML(text1, text2);
+         assertEquals("Diff is incorrect", "<div class=\"diffmodifiedline\">A <span class=\"diffremoveword\">C</span><span class=\"diffaddword\">B</span></div>", html);
+     }
+
+    public void testSimpleWordDiffAsHTML3() throws XWikiException {
+         String text1 = "A B C D E F";
+         String text2 = "A C B D E G";
+         String html = this.plugin.getWordDifferencesAsHTML(text1, text2);
+         assertEquals("Diff is incorrect", "<div class=\"diffmodifiedline\">A <span class=\"diffremoveword\">B</span> C <span class=\"diffaddword\">B</span> D E <span class=\"diffremoveword\">F</span><span class=\"diffaddword\">G</span></div>", html);
+     }
+
+    public void testSimpleLineDiffAsHTML2() throws XWikiException {
+         String text1 = "A C";
+         String text2 = "A B";
+         String html = this.plugin.getDifferencesAsHTML(text1, text2);
+         assertEquals("Diff is incorrect", "<div class=\"diff\"><div class=\"diffmodifiedline\">A <span class=\"diffremoveword\">C</span><span class=\"diffaddword\">B</span></div></div>", html);
+     }
+
+    public void testSimpleLineDiffAsHTML3() throws XWikiException {
+         String text1 = "A B C D E F";
+         String text2 = "A C B D E G";
+         String html = this.plugin.getDifferencesAsHTML(text1, text2);
+         assertEquals("Diff is incorrect", "<div class=\"diff\"><div class=\"diffmodifiedline\">A <span class=\"diffremoveword\">B</span> C <span class=\"diffaddword\">B</span> D E <span class=\"diffremoveword\">F</span><span class=\"diffaddword\">G</span></div></div>", html);
+     }
+
+    public void testSimpleLineDiffAsHTML4() throws XWikiException {
+         String text1 = "A B C\nD E F\nG H I\nJ K L\n";
+         String text2 = "A B C\nG H I\nD E F\nJ K L\n";
+         String html = this.plugin.getDifferencesAsHTML(text1, text2);
+         assertEquals("Diff is incorrect", "<div class=\"diff\"><div class=\"diffunmodifiedline\">A B C</div><div class=\"diffmodifiedline\"><span class=\"diffremoveword\">D E F</span></div><div class=\"diffunmodifiedline\">G H I</div><div class=\"diffmodifiedline\"><span class=\"diffaddword\">D E F</span></div><div class=\"diffunmodifiedline\">J K L</div></div>", html);
+     }
+     */
+}

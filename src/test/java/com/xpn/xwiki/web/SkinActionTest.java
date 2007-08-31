ee2b2fb8f7ec97c93c57d093225a496f4c468fb4
sdumitriu <sergiu@xwiki.com>
2007-08-31 18:57:31 +0000

    XWIKI-1690: NullPointerException when parsing skin files Added tests.
    XWIKI-1692: Improve the javascript mimetype detection in SkinAction#isJavascriptMimeType
    Done.
    
    
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-platform/core/trunk/xwiki-core@4681 f329d543-caf0-0310-9063-dda96c69346f

---
+++ src/test/java/com/xpn/xwiki/web/SkinActionTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2007, XpertNet SARL, and individual contributors as indicated
+ * by the contributors.txt.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ *
+ * @author ravenees
+ */
+package com.xpn.xwiki.web;
+
+import org.jmock.cglib.MockObjectTestCase;
+
+/**
+ * Unit tests for the {@link com.xpn.xwiki.web.SkinAction} class.
+ * 
+ * @version $Id: $
+ */
+public class SkinActionTest extends MockObjectTestCase
+{
+    private SkinAction action;
+
+    protected void setUp()
+    {
+        this.action = new SkinAction();
+    }
+
+    public void testIsTextJavascriptJavaScriptMimetype()
+    {
+        assertTrue(this.action.isJavascriptMimeType("text/javascript"));
+    }
+
+    public void testIsApplicationJavascriptJavaScriptMimetype()
+    {
+        assertTrue(this.action.isJavascriptMimeType("application/javascript"));
+    }
+
+    public void testIsApplicationXJavascriptJavaScriptMimetype()
+    {
+        assertTrue(this.action.isJavascriptMimeType("application/x-javascript"));
+    }
+
+    public void testIsTextEcmascriptJavaScriptMimetype()
+    {
+        assertTrue(this.action.isJavascriptMimeType("text/ecmascript"));
+    }
+
+    public void testIsApplicationEcmascriptJavaScriptMimetype()
+    {
+        assertTrue(this.action.isJavascriptMimeType("application/ecmascript"));
+    }
+
+    public void testNPEJavascriptMimetype()
+    {
+        assertFalse(this.action.isJavascriptMimeType(null));
+    }
+}

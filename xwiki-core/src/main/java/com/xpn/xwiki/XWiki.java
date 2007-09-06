13cf347ca87a37506095617593793af92e43b6b1
ldubost <ludovic@xwiki.com>
2007-09-06 09:08:24 +0000

    XWIKI-1741 Security Issue in multiwiki mode
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-platform/core/trunk@4735 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-core/src/main/java/com/xpn/xwiki/XWiki.java
+++ xwiki-core/src/main/java/com/xpn/xwiki/XWiki.java
@@ -534,6 +534,35 @@ public class XWiki implements XWikiDocChangeNotificationInterface, XWikiInterfac
         return "XWiki.XWikiServer" + servername.substring(0, 1).toUpperCase()
             + servername.substring(1);
     }
+    
+    public String getWikiOwner(String servername, XWikiContext context) throws XWikiException
+    {       
+        String wikiOwner = context.getWikiOwner();
+        
+        if (isVirtual()) {
+            String serverwikipage = getServerWikiPage(servername);
+            
+            String currentdatabase = context.getDatabase();
+
+            try {
+                context.setDatabase(context.getMainXWiki());
+                
+                XWikiDocument doc = getDocument(serverwikipage, context);
+                
+                if (doc.isNew()) {
+                    throw new XWikiException(XWikiException.MODULE_XWIKI,
+                        XWikiException.ERROR_XWIKI_DOES_NOT_EXIST,
+                        "The wiki " + servername + " does not exist");
+                }
+                
+                wikiOwner = doc.getStringValue("XWiki.XWikiServerClass", "owner");                
+            } finally {
+                context.setDatabase(currentdatabase);
+            }
+        }
+        
+        return wikiOwner;
+    }
 
     public XWiki(XWikiConfig config, XWikiContext context) throws XWikiException
     {

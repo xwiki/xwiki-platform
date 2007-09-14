69ba87c6b885de80007b6e4dcb62548a7679f780
vmassol <vincent@massol.net>
2007-09-14 15:19:14 +0000

    XWIKI-1745: Replace license headers in source files
    * Replace licenses in Java files
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-platform/core/trunk@4942 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-containers/xwiki-container-servlet/src/main/java/org/xwiki/container/servlet/ServletRequest.java
+++ xwiki-containers/xwiki-container-servlet/src/main/java/org/xwiki/container/servlet/ServletRequest.java
@@ -1,6 +1,6 @@
 /*
- * Copyright 2007, XpertNet SARL, and individual contributors as indicated
- * by the contributors.txt.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
  *
  * This is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as

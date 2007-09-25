7fe598843cc58ad87b32d825b0496d2c9490d4d5
vmassol <vincent@massol.net>
2007-09-25 14:49:24 +0000

    XTOOLS-16: Make Jetty configuration use the work/ directory inside the jetty home directory as the temporary directory rather than java.io.tmpdir
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-platform/xwiki-tools/trunk/xwiki-jetty-resources@5083 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-platform-tools/src/main/resources/start_xwiki.bat
+++ xwiki-platform-tools/src/main/resources/start_xwiki.bat
@@ -4,7 +4,10 @@ set JETTY_HOME=.
 set JETTY_PORT=8080
 set JAVA_OPTS=-Xmx300m
 
-REM Jetty requires a logs directory to exist
+REM Ensure the logs directory exists as otherwise Jetty reports an error
 if not exist logs mkdir logs
+
+REM Ensure the work directory exists so that Jetty uses it for its temporary files.
+if not exist work mkdir work
 
 java %JAVA_OPTS% -Dfile.encoding=iso-8859-1 -Djetty.home=%JETTY_HOME% -Djetty.port=%JETTY_PORT% -jar %JETTY_HOME%/start.jar

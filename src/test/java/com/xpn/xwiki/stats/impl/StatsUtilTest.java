2d213590b7a16b3d5f642ddb0a4a7de2caf83c1b
vmassol <vincent@massol.net>
2007-10-18 11:31:18 +0000

    XWIKI-1814: Day period incorrectly computed Patch submitted by Marius Florea
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-platform/core/trunk/xwiki-core@5421 f329d543-caf0-0310-9063-dda96c69346f

---
+++ src/test/java/com/xpn/xwiki/stats/impl/StatsUtilTest.java
@@ -0,0 +1,51 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package com.xpn.xwiki.stats.impl;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import junit.framework.TestCase;
+
+/**
+ * Unit tests for the {@link StatsUtil} class.
+ * 
+ * @version $Id: $
+ */
+public class StatsUtilTest extends TestCase
+{
+    /**
+     * Test for the {@link StatsUtil#getPeriodAsInt(java.util.Date, int)}
+     */
+    public void testGetPeriodAsInt()
+    {
+        Calendar cal = Calendar.getInstance();
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMM");
+        String a = sdf.format(cal.getTime());
+        String b = StatsUtil.getPeriodAsInt(cal.getTime(), StatsUtil.PERIOD_MONTH) + "";
+        assertEquals("Wrong month period format", a, b);
+
+        sdf = new SimpleDateFormat("yyyyMMdd");
+        a = sdf.format(cal.getTime());
+        b = StatsUtil.getPeriodAsInt(cal.getTime(), StatsUtil.PERIOD_DAY) + "";
+        assertEquals("Wrong day period format", a, b);
+    }
+}

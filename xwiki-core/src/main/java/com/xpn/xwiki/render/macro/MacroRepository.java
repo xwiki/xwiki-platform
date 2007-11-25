2431490312cd9b305f479c50efa1050589d7953d
vmassol <vincent@massol.net>
2007-11-25 06:13:03 +0000

    Add Radeox copyright license headers + explain why we've copied these 2 classes from Radeox
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-platform/core/trunk@6065 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-core/src/main/java/com/xpn/xwiki/render/macro/MacroRepository.java
+++ xwiki-core/src/main/java/com/xpn/xwiki/render/macro/MacroRepository.java
@@ -19,7 +19,30 @@
  *
  */
 
-
+/*
+ * This file is part of "SnipSnap Radeox Rendering Engine".
+ *
+ * Copyright (c) 2002 Stephan J. Schmidt, Matthias L. Jugel
+ * All Rights Reserved.
+ *
+ * Please visit http://radeox.org/ for updates and contact.
+ *
+ * --LICENSE NOTICE--
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * --LICENSE NOTICE--
+ */
 package com.xpn.xwiki.render.macro;
 
 import org.apache.commons.logging.Log;
@@ -29,6 +52,9 @@ import org.radeox.macro.Macro;
 
 import java.util.*;
 
+/**
+ * See {@link MacroLoader} for why we have copied this class from Radeox.
+ */
 public class MacroRepository extends org.radeox.macro.PluginRepository {
   protected static Log log = LogFactory.getLog(MacroRepository.class);
   protected InitialRenderContext context;

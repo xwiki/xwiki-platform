1274bc91505238537c27f4fa1437e3c7231f4168
vmassol <vincent@massol.net>
2007-11-25 06:13:03 +0000

    Add Radeox copyright license headers + explain why we've copied these 2 classes from Radeox
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-platform/core/trunk/xwiki-core@6065 f329d543-caf0-0310-9063-dda96c69346f

--- src/main/java/com/xpn/xwiki/render/macro/MacroLoader.java
+++ src/main/java/com/xpn/xwiki/render/macro/MacroLoader.java
@@ -19,11 +19,43 @@
  *
  */
 
-
+/*
+ * This file is part of "SnipSnap Radeox Rendering Engine".
+ *
+ * Copyright (c) 2002 Stephan J. Schmidt, Matthias L. Jugel
+ * All Rights Reserved.
+ *
+ * Please visit http://radeox.org/ for updates and contact.
+ *
+ * --LICENSE NOTICE--
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * --LICENSE NOTICE--
+ */
 package com.xpn.xwiki.render.macro;
 
-public class MacroLoader extends org.radeox.macro.MacroLoader {
-    public Class getLoadClass() {
+/**
+ * This class has been copied from Radeox. The reason is because the class returned getLoadClass() is used to load
+ * the service providers. We are passing our own XWikiMacro class so that the
+ * META-INF/com.xpn.xwiki.render.macro.XWikiMacro file is used to load the Macros instead of the default macro list
+ * provided in the Radeox JAR. Since we need to pass this MacroLoader class to the MacroRepository class we have
+ * also copied the MacroRepository class.
+ */
+public class MacroLoader extends org.radeox.macro.MacroLoader
+{
+    public Class getLoadClass()
+    {
       return XWikiMacro.class;
     }
 }

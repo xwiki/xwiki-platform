236f517caa394c04601aa77e5e61f1949ed8bf22
vmassol <vincent@massol.net>
2007-12-17 16:49:21 +0000

    Added more javadoc to explain how the Code Macro works.
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-platform/core/trunk@6391 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-core/src/main/java/com/xpn/xwiki/render/macro/XWikiCodeMacro.java
+++ xwiki-core/src/main/java/com/xpn/xwiki/render/macro/XWikiCodeMacro.java
@@ -51,6 +51,27 @@ import org.apache.commons.lang.StringUtils;
 import java.io.IOException;
 import java.io.Writer;
 
+/**
+ * The Code Macro renders its content as is but pretty printed according to the language passed as parameter. Supporting
+ * escaping Radeox, Velocity and Groovy wasn't trivial. Here's how it's currently implemented (waiting for a better
+ * implementation):
+ * <ol>
+ * <li>The first renderer to execute is the MacroMappingRenderer. That's because it's registered first in
+ *     {@link com.xpn.xwiki.render.DefaultXWikiRenderingEngine#DefaultXWikiRenderingEngine(com.xpn.xwiki.XWiki, com.xpn.xwiki.XWikiContext)}}.
+ *     The code macro is registered in the macros.txt file used by the MacroMappingRenderer and thus it is processed
+ *     by the macro mapping feature. Since it recognizes it's a radeox macro, it calls the Radeox Renderer's
+ *     {@link com.xpn.xwiki.render.XWikiRadeoxRenderer#convertMultiLine(String, String, String, String, com.xpn.xwiki.render.XWikiVirtualMacro, com.xpn.xwiki.XWikiContext)}
+ *     method. This method has a hack and verifies if the macro to render is the code macro. If so it escapes Velocity
+ *     and Groovy special characters ($, #, <% and %>) so that when the Velocity and Radeox renderers execute they do
+ *     not process its content.</li>
+ * <li>We also don't want the Radeox renderer to render the code macro's content. We do this by having a special Radeox
+ *     Filter ({@link com.xpn.xwiki.render.filter.CodeRemoveFilter} that is defined before all other filters in
+ *     the <code>META-INF/services/com.xpn.xwiki.render.filter.XWikiFilter</code> file and that removes the content of
+ *     the code macro. The other filters then execute. The last but one defined filter is
+ *     {@link com.xpn.xwiki.render.filter.CodeRestoreFilter} which puts back the content of the code macro and the last
+ *     filter is the {@link com.xpn.xwiki.render.filter.CodeFilter} filter which calls this macro.</li>
+ * </ol>
+ */
 public class XWikiCodeMacro extends CodeMacro
 {
     public XWikiCodeMacro()

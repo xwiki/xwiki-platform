29e70f31ba7bce55023dae5ea3aa924d07338ebc
vmassol <vincent@massol.net>
2008-01-29 21:45:27 +0000

    Fixed checkstyle errors (line longer than 100 chars)
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-platform/core/trunk@7171 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-core/src/main/java/com/xpn/xwiki/store/migration/XWikiMigratorInterface.java
+++ xwiki-core/src/main/java/com/xpn/xwiki/store/migration/XWikiMigratorInterface.java
@@ -52,11 +52,13 @@ public interface XWikiMigratorInterface
      * @param context used everywhere
      * @throws XWikiException if any error
      */
-    void migrate(XWikiMigrationManagerInterface manager, XWikiContext context) throws XWikiException;
+    void migrate(XWikiMigrationManagerInterface manager, XWikiContext context)
+        throws XWikiException;
 
     /**
-     * @param startupVersion the database version when the migration process starts (before any migrator is applied).
-     *        This is useful for migrator which need to run only when the database is in a certain version.
+     * @param startupVersion the database version when the migration process starts (before any
+     *        migrator is applied). This is useful for migrator which need to run only when the
+     *        database is in a certain version.
      * @return true if the migration should be executed or false otherwise
      */
     boolean shouldExecute(XWikiDBVersion startupVersion);

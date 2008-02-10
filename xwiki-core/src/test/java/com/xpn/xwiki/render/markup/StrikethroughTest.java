7c06bb7128fdf7fe50a3357446949d8a404426ba
sdumitriu <sergiu@xwiki.com>
2008-02-10 23:24:49 +0000

    XWIKI-2080: Server hangs while rendering certain pages Addet unit tests (the build will take forever if it happens again).
    XWIKI-1207: Strikethrough XWiki syntax
    XWIKI-1243: Underline XWiki syntax
    Added unit tests
    
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-platform/core/trunk@7480 f329d543-caf0-0310-9063-dda96c69346f

---
+++ xwiki-core/src/test/java/com/xpn/xwiki/render/markup/StrikethroughTest.java
@@ -0,0 +1,146 @@
+package com.xpn.xwiki.render.markup;
+
+import java.util.ArrayList;
+
+public class StrikethroughTest extends SyntaxTestsParent
+{
+    protected void setUp()
+    {
+        super.setUp();
+    }
+
+    public void testNotTriggeredWithWhitespace()
+    {
+        ArrayList tests = new ArrayList();
+        ArrayList expects = new ArrayList();
+        tests.add("This is not -- stroked --");
+        expects.add("This is not -- stroked --");
+        tests.add("This is not --stroked --");
+        expects.add("This is not --stroked --");
+        tests.add("This is not -- stroked--");
+        expects.add("This is not -- stroked--");
+        test(tests, expects);
+    }
+
+    public void testSimple()
+    {
+        ArrayList tests = new ArrayList();
+        ArrayList expects = new ArrayList();
+        tests.add("This is --stroked--");
+        expects.add("This is <del>stroked</del>");
+        tests.add("This is --a-- stroke");
+        expects.add("This is <del>a</del> stroke");
+        tests.add("--a--");
+        expects.add("<del>a</del>");
+        test(tests, expects);
+    }
+
+    public void testThree()
+    {
+        ArrayList tests = new ArrayList();
+        ArrayList expects = new ArrayList();
+        tests.add("This is --a-- short stroke--");
+        expects.add("This is <del>a</del> short stroke--");
+        tests.add("This is --all -- stroked--");
+        expects.add("This is <del>all -- stroked</del>");
+        tests.add("This is --all --stroked--");
+        expects.add("This is <del>all --stroked</del>");
+        tests.add("This is --one--stroked--");
+        expects.add("This is <del>one</del>stroked--");
+        test(tests, expects);
+    }
+
+    public void testMultiple()
+    {
+        ArrayList tests = new ArrayList();
+        ArrayList expects = new ArrayList();
+        tests.add("More --strokes-- on a --line--");
+        expects.add("More <del>strokes</del> on a <del>line</del>");
+        test(tests, expects);
+    }
+
+    public void testExtraDashesAreInside()
+    {
+        ArrayList tests = new ArrayList();
+        ArrayList expects = new ArrayList();
+        tests.add("The extra dashes are ---inside---");
+        expects.add("The extra dashes are <del>-inside-</del>");
+        tests.add("The extra dashes are --- inside ---");
+        expects.add("The extra dashes are <del>- inside -</del>");
+        test(tests, expects);
+    }
+
+    public void testWithoutWhitespace()
+    {
+        ArrayList tests = new ArrayList();
+        ArrayList expects = new ArrayList();
+        tests.add("This--is--stroked");
+        expects.add("This<del>is</del>stroked");
+        test(tests, expects);
+    }
+
+    public void testSequence()
+    {
+        ArrayList tests = new ArrayList();
+        ArrayList expects = new ArrayList();
+        tests.add("--Eeny--meeny--miny--moe--");
+        expects.add("<del>Eeny</del>meeny<del>miny</del>moe--");
+        tests.add("-- Eeny--meeny--miny--moe--");
+        expects.add("...<li>Eeny<del>meeny</del>miny<del>moe</del></li>...");
+        test(tests, expects);
+    }
+
+    public void testHR()
+    {
+        ArrayList tests = new ArrayList();
+        ArrayList expects = new ArrayList();
+        tests.add("this is a ------ line");
+        expects.add("this is a <hr class=\"line\"/> line");
+        test(tests, expects);
+    }
+
+    public void testWithLists()
+    {
+        ArrayList tests = new ArrayList();
+        ArrayList expects = new ArrayList();
+        tests.add("-- this is a list item--");
+        expects.add("...<li>this is a list item--</li>...");
+        test(tests, expects);
+    }
+
+    public void testHtmlComments()
+    {
+        ArrayList tests = new ArrayList();
+        ArrayList expects = new ArrayList();
+        tests.add("HTML <!-- comments are ignored -->");
+        expects.add("HTML <!-- comments are ignored -->");
+        tests.add("Multiple HTML <!-- comments --> are <!-- ignored -->");
+        expects.add("Multiple HTML <!-- comments --> are <!-- ignored -->");
+        test(tests, expects);
+    }
+
+    public void testMultiline()
+    {
+        ArrayList tests = new ArrayList();
+        ArrayList expects = new ArrayList();
+        tests.add("This is not --not\nstroked--");
+        expects.add("This is not --not\nstroked--");
+        test(tests, expects);
+    }
+
+    public void testTimeComplexity()
+    {
+        ArrayList tests = new ArrayList();
+        ArrayList expects = new ArrayList();
+        // Something like this should be (negatively) matched in linear time, thus it should take no
+        // time. If the build takes a lot, then the regular expression is not in linear time, thus
+        // wrong.
+        String text = "--";
+        for (int i = 0; i < 1000; ++i) {
+            text += "abc-";
+        }
+        tests.add(text);
+        expects.add(text);
+        test(tests, expects);
+    }
+}

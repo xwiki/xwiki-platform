3c0a84a456298d1e1e25f66a42dd78371af3cd0c
sdumitriu <sergiu@xwiki.com>
2008-02-20 01:45:28 +0000

    XWIKI-1751: Display a warning message instead of stack trace when no classid parameter was sent in a ObjectRemove action Fixed.
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-platform/core/trunk/xwiki-core@7861 f329d543-caf0-0310-9063-dda96c69346f

--- src/main/java/com/xpn/xwiki/web/ObjectRemoveAction.java
+++ src/main/java/com/xpn/xwiki/web/ObjectRemoveAction.java
@@ -20,28 +20,90 @@
  */
 package com.xpn.xwiki.web;
 
+import java.io.IOException;
+
+import org.apache.commons.lang.BooleanUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.velocity.VelocityContext;
+
 import com.xpn.xwiki.XWiki;
 import com.xpn.xwiki.XWikiContext;
 import com.xpn.xwiki.XWikiException;
 import com.xpn.xwiki.doc.XWikiDocument;
 import com.xpn.xwiki.objects.BaseObject;
 
-public class ObjectRemoveAction extends XWikiAction {
-	public boolean action(XWikiContext context) throws XWikiException {
+public class ObjectRemoveAction extends XWikiAction
+{
+    protected BaseObject getObject(XWikiDocument doc, XWikiContext context)
+    {
+        ObjectRemoveForm form = (ObjectRemoveForm) context.getForm();
+        BaseObject obj = null;
+
+        String className = form.getClassName();
+        int classId = form.getClassId();
+        if (StringUtils.isBlank(className)) {
+            ((VelocityContext) context.get("vcontext")).put("message", context.getMessageTool()
+                .get("platform.core.action.objectRemove.noClassnameSpecified"));
+        } else if (classId < 0) {
+            ((VelocityContext) context.get("vcontext")).put("message", context.getMessageTool()
+                .get("platform.core.action.objectRemove.noObjectSpecified"));
+        } else {
+            obj = doc.getObject(className, classId);
+            if (obj == null) {
+                ((VelocityContext) context.get("vcontext")).put("message", context.getMessageTool()
+                    .get("platform.core.action.objectRemove.invalidObject"));                
+            }
+        }
+        return obj;
+    }
+    /**
+     * {@inheritDoc}
+     * 
+     * @see XWikiAction#action(XWikiContext)
+     */
+    public boolean action(XWikiContext context) throws XWikiException
+    {
         XWiki xwiki = context.getWiki();
         XWikiResponse response = context.getResponse();
         XWikiDocument doc = context.getDoc();
-        XWikiForm form = context.getForm();
-
-        String className = ((ObjectRemoveForm) form).getClassName();
-        int classId = ((ObjectRemoveForm) form).getClassId();
-        BaseObject obj = doc.getObject(className, classId);
+        BaseObject obj = getObject(doc, context);
+        if (obj == null) {
+            return true;
+        }
         doc.removeObject(obj);
-        xwiki.saveDocument(doc, context.getMessageTool().get("core.comment.deleteObject"), true, context);
+        xwiki.saveDocument(doc, context.getMessageTool().get("core.comment.deleteObject"), true,
+            context);
 
-        // forward to edit
-        String redirect = Utils.getRedirect("edit", context);
-        sendRedirect(response, redirect);
+        if (BooleanUtils.isTrue((Boolean) context.get("ajax"))) {
+            response.setStatus(204);
+            response.setContentLength(0);
+        } else {
+            // forward to edit
+            String redirect = Utils.getRedirect("edit", context);
+            sendRedirect(response, redirect);
+        }
         return false;
-	}
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see XWikiAction#render(XWikiContext)
+     */
+    public String render(XWikiContext context) throws XWikiException
+    {
+        if (BooleanUtils.isTrue((Boolean) context.get("ajax"))) {
+            XWikiResponse response = context.getResponse();
+            response.setStatus(409);
+            response.setContentType("text/plain");
+            try {
+                response.getWriter().write("failed");
+                response.setContentLength(6);
+            } catch (IOException e) {
+            }
+            return null;
+        } else {
+            return "error";
+        }
+    }
 }

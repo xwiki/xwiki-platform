c068778baf78ba4422a7bd30e2956667c2bf4e7e
vmassol <vincent@massol.net>
2008-02-29 13:16:27 +0000

    XTJETTY-5: Allow stopping Jetty on a non default port (unix)
    Revert
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-platform/xwiki-tools/trunk@8148 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-platform-tools/xwiki-jetty-resources/src/main/resources/stop_xwiki.bat
+++ xwiki-platform-tools/xwiki-jetty-resources/src/main/resources/stop_xwiki.bat
@@ -1,6 +1,5 @@
 @echo off
 set JETTY_HOME=jetty
-set JETTY_PORT=8080
 
-java -Djetty.home=%JETTY_HOME% -Djetty.port=%JETTY_PORT% -jar %JETTY_HOME%/stop.jar
+java -Djetty.home=%JETTY_HOME% -jar %JETTY_HOME%/stop.jar
 

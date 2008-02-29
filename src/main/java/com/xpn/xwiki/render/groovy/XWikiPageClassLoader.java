5f673f93ae60c9be6bb42e69687232ce95196cae
vmassol <vincent@massol.net>
2008-02-29 22:19:39 +0000

    XWIKI-2163: XWiki.parseGroovyFromPage() doesn't work when passing the a page containing JARs to be put in the Groovy classloader
    Reformatting + added logging + remove unnecessary temporary Array creation (I hope that was not a special trick for some reason, it's undocumented anyway).
    
    Merged from trunk (rev 8155 + 8157)
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-platform/core/branches/xwiki-core-1.3/xwiki-core@8158 f329d543-caf0-0310-9063-dda96c69346f

--- src/main/java/com/xpn/xwiki/render/groovy/XWikiPageClassLoader.java
+++ src/main/java/com/xpn/xwiki/render/groovy/XWikiPageClassLoader.java
@@ -1,16 +1,3 @@
-package com.xpn.xwiki.render.groovy;
-
-import com.xpn.xwiki.XWikiContext;
-import com.xpn.xwiki.XWikiException;
-import com.xpn.xwiki.doc.XWikiDocument;
-import com.xpn.xwiki.doc.XWikiAttachment;
-
-import java.net.URLClassLoader;
-import java.net.URL;
-import java.net.URLStreamHandlerFactory;
-import java.util.List;
-import java.util.ArrayList;
-
 /**
  * See the NOTICE file distributed with this work for additional
  * information regarding copyright ownership.
@@ -29,47 +16,69 @@ import java.util.ArrayList;
  * License along with this software;if not,write to the Free
  * Software Foundation,Inc.,51 Franklin St,Fifth Floor,Boston,MA
  * 02110-1301 USA,or see the FSF site:http://www.fsf.org.
- *
- * @author ldubost
  */
+package com.xpn.xwiki.render.groovy;
 
-public class XWikiPageClassLoader extends URLClassLoader {
+import com.xpn.xwiki.XWikiContext;
+import com.xpn.xwiki.XWikiException;
+import com.xpn.xwiki.doc.XWikiDocument;
+import com.xpn.xwiki.doc.XWikiAttachment;
 
-    public XWikiPageClassLoader(URL[] urls, ClassLoader parent) {
+import java.net.URLClassLoader;
+import java.net.URL;
+import java.net.URLStreamHandlerFactory;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class XWikiPageClassLoader extends URLClassLoader
+{
+    private static final Log LOG = LogFactory.getLog(XWikiPageClassLoader.class);
+
+    public XWikiPageClassLoader(URL[] urls, ClassLoader parent)
+    {
         super(urls, parent);
     }
 
-    public XWikiPageClassLoader(URL[] urls) {
+    public XWikiPageClassLoader(URL[] urls)
+    {
         super(urls);
     }
 
-    public XWikiPageClassLoader(URL[] urls, ClassLoader parent, URLStreamHandlerFactory factory) {
+    public XWikiPageClassLoader(URL[] urls, ClassLoader parent, URLStreamHandlerFactory factory)
+    {
         super(urls, parent, factory);
     }
 
-    public XWikiPageClassLoader(String jarWikiPage, XWikiContext context) throws XWikiException {
+    public XWikiPageClassLoader(String jarWikiPage, XWikiContext context) throws XWikiException
+    {
         this(jarWikiPage, Thread.currentThread().getContextClassLoader(), context);
     }
 
-    public XWikiPageClassLoader(String jarWikiPage, ClassLoader parent, XWikiContext context) throws XWikiException {
+    public XWikiPageClassLoader(String jarWikiPage, ClassLoader parent, XWikiContext context)
+        throws XWikiException
+    {
         super(new URL[0], parent);
         XWikiDocument doc = context.getWiki().getDocument(jarWikiPage, context);
-        List urlList = new ArrayList();
         if (!doc.isNew()) {
             List attachList = doc.getAttachmentList();
             for (int i=0;i<attachList.size();i++) {
                 XWikiAttachment attach = (XWikiAttachment) attachList.get(i);
                 String filename = attach.getFilename();
                 if (filename.endsWith(".jar")) {
+                    String downloadURL = doc.getExternalAttachmentURL(filename, "download", context);
                     try{
-                        urlList.add(new URL(doc.getExternalAttachmentURL(filename, "download", context)));
-                    } catch (Exception e) {};
+                        addURL(new URL(downloadURL));
+                        if (LOG.isDebugEnabled()) {
+                            LOG.debug("Adding [" + downloadURL + "] JAR from page ["
+                                + jarWikiPage + "] to Groovy classloader");
+                        }
+                    } catch (Exception e) {
+                        LOG.warn("Failed to add [" + downloadURL + "] JAR from page ["
+                            + jarWikiPage + "], ignoring it.");
+                    }
                 }
-            }
-        }
-        if (urlList.size()>0) {
-            for (int i=0;i<urlList.size();i++) {
-                addURL((URL) urlList.get(i));
             }
         }
     }

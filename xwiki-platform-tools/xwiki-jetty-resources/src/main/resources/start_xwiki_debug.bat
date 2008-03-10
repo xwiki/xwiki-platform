601628efeeac9785801d5f48e107099748bd4586
vmassol <vincent@massol.net>
2008-03-10 09:35:46 +0000

    XTJETTY-6: Increase the default maximum form content size on Jetty * Fixed regression introduced in previous commit * Fixed invalid comment in start_xwiki_debug.bat * Added explanation as to why we introduced that new Jetty parameter
    Note1: We need to refactor the debug scripts so that they use the main scripts to prevent duplication
    Note2: We need to add explanation why we use the LANG=fr_FR.ISO8859-1 param in the start_xwiki.sh script. I would add it if I knew why we're doing this.
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-platform/xwiki-tools/trunk@8336 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-platform-tools/xwiki-jetty-resources/src/main/resources/start_xwiki_debug.bat
+++ xwiki-platform-tools/xwiki-jetty-resources/src/main/resources/start_xwiki_debug.bat
@@ -2,9 +2,18 @@
 set LANG=fr_FR.ISO8859-1
 set JETTY_HOME=jetty
 set JETTY_PORT=8080
-set JAVA_OPTS="-Xmx300m -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -Dorg.mortbay.http.HttpRequest.maxFormContentSize=1000000"
+set JAVA_OPTS=-Xmx300m
+set JAVA_OPTS=%JAVA_OPTS% -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005
 
-# Jetty requires a logs directory to exist
+REM In order to avoid getting a "java.lang.IllegalStateException: Form too large" error
+REM when editing large page in XWiki we need to tell Jetty to allow for large content
+REM since by default it only allows for 20K. We do this by passing the
+REM org.mortbay.http.HttpRequest.maxFormContentSize property.
+REM Note that setting this value too high can leave your server vulnerable to denial of
+REM service attacks.
+set JAVA_OPTS=%JAVA_OPTS% -Dorg.mortbay.http.HttpRequest.maxFormContentSize=1000000
+
+REM Jetty requires a logs directory to exist
 if not exist %JETTY_HOME%\logs mkdir %JETTY_HOME%\logs
 
 java %JAVA_OPTS% -Dfile.encoding=iso-8859-1 -Djetty.home=%JETTY_HOME% -Djetty.port=%JETTY_PORT% -jar %JETTY_HOME%/start.jar
--- xwiki-platform-tools/xwiki-jetty-resources/src/main/resources/start_xwiki_debug.sh
+++ xwiki-platform-tools/xwiki-jetty-resources/src/main/resources/start_xwiki_debug.sh
@@ -17,7 +17,16 @@ PRGDIR=`dirname "$PRG"`
 cd "$PRGDIR"
 
 JETTY_HOME=jetty
-JAVA_OPTS="-Xmx300m -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -Dorg.mortbay.http.HttpRequest.maxFormContentSize=1000000"
+JAVA_OPTS=-Xmx300m
+JAVA_OPTS="$JAVA_OPTS -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005"
+
+# In order to avoid getting a "java.lang.IllegalStateException: Form too large" error
+# when editing large page in XWiki we need to tell Jetty to allow for large content
+# since by default it only allows for 20K. We do this by passing the
+# org.mortbay.http.HttpRequest.maxFormContentSize property.
+# Note that setting this value too high can leave your server vulnerable to denial of
+# service attacks.
+JAVA_OPTS="$JAVA_OPTS -Dorg.mortbay.http.HttpRequest.maxFormContentSize=1000000"
 
 # For enabling YourKit Profiling:
 # JAVA_OPTS="$JAVA_OPTS -agentlib:yjpagent"
@@ -33,5 +42,7 @@ fi
 echo Starting Jetty on port $JETTY_PORT ...
 echo Logs are in the $PRGDIR/xwiki.log file
 
+# Ensure the logs directory exists as otherwise Jetty reports an error
 mkdir -p $JETTY_HOME/logs 2>/dev/null
+
 LANG=fr_FR.ISO8859-1 java $JAVA_OPTS -Dfile.encoding=iso-8859-1 -Djetty.port=$JETTY_PORT -Djetty.home=$JETTY_HOME -jar $JETTY_HOME/start.jar

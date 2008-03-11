a43f277fa8bc5c28d2bb6984a165feb4ce4d22f4
vmassol <vincent@massol.net>
2008-03-11 22:31:36 +0000

    XPMAIL-5: Allow setting of Reply-To, Sender (and other) headers Patch submitted by David Ward Reviewed and substantially modified by Vincent Massol: * Added new MailConfiguration class * Added unit tests * Cleaned up some code (still a lot to clean up, especially all error handling)
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-platform/xwiki-plugins/trunk/mailsender@8413 f329d543-caf0-0310-9063-dda96c69346f

---
+++ xwiki-platform-mailsender/src/main/java/com/xpn/xwiki/plugin/mailsender/MailConfiguration.java
@@ -0,0 +1,100 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package com.xpn.xwiki.plugin.mailsender;
+
+import com.xpn.xwiki.api.XWiki;
+
+/**
+ * Represents a Mail Server configuration.
+ */
+public class MailConfiguration
+{
+    private int port;
+    private String host;
+    private String from;
+
+    public MailConfiguration()
+    {
+        setPort(25);
+        setHost("localhost");
+    }
+
+    public MailConfiguration(XWiki xwiki)
+    {
+        this();
+
+        String smtpServer = xwiki.getXWikiPreference("smtp_server");
+        if (smtpServer.length() > 0) {
+            setHost(smtpServer);
+        }
+
+        String from = xwiki.getXWikiPreference("smtp_from");
+        if (from.length() > 0) {
+            setFrom(from);
+        }
+    }
+
+    public void setHost(String host)
+    {
+        this.host = host;
+    }
+
+    public String getHost()
+    {
+        return this.host;
+    }
+    
+    public void setPort(int port)
+    {
+        this.port = port;
+    }
+
+    public int getPort()
+    {
+        return this.port;
+    }
+
+    public void setFrom(String from)
+    {
+        this.from = from;
+    }
+
+    public String getFrom()
+    {
+        return this.from;
+    }
+
+    public String toString()
+    {
+        StringBuffer buffer = new StringBuffer();
+
+        if (getHost() != null) {
+            buffer.append("Host [" + getHost() + "]");
+        }
+
+        if (getFrom() != null) {
+            buffer.append(", From [" + getFrom() + "]");
+        }
+
+        buffer.append(", Port [" + getPort() + "]");
+
+        return buffer.toString();
+    }
+}
--- xwiki-platform-mailsender/src/main/java/com/xpn/xwiki/plugin/mailsender/MailSender.java
+++ xwiki-platform-mailsender/src/main/java/com/xpn/xwiki/plugin/mailsender/MailSender.java
@@ -23,6 +23,8 @@ import org.apache.velocity.VelocityContext;
 
 import java.util.List;
 
+import com.xpn.xwiki.api.XWiki;
+
 /**
  * Send mails to recipients (to, cc, bcc). Both text and HTML emails can be sent along with
  * attachments. Also support XWiki page templates and allows sending a collection of emails
@@ -32,6 +34,48 @@ import java.util.List;
  */
 public interface MailSender
 {
+    /**
+     * A helper method for Velocity scripts since we cannot create Java objects from Velocity.
+     *
+     * @return An empty mail message to be populated with recipient addresses, subject, message, etc.
+     */
+    Mail createMail();
+
+    /**
+     * A helper method for Velocity scripts since we cannot create Java objects from Velocity.
+     *
+     * @param xwiki the XWiki object used to get the default values from the XWiki Preferences
+     *        ("smtp_server" and "smtp_from"). 
+     * @return A mail server configuration, initialized with values from XWiki Preferences, but which
+     *         can be overriden by users. 
+     */
+    MailConfiguration createMailConfiguration(XWiki xwiki);
+
+    /**
+     * Generic method for sending emails. The passed Mail object has to be populated by the caller
+     * to set the correct fields. All the other <code>sendHtmlXXX()</code> and
+     * <code>sendTextXXX()</code> methods are specialized helper versions of this generic method.
+     *
+     * @param mail the already populated mail Object to be sent
+     * @return 0 on success, -1 on failure. On failure the error message is stored in the XWiki
+     *         context under the "error" key.
+     */
+    int sendMail(Mail mail);
+
+    /**
+     * Generic method for sending emails. The passed Mail object has to be populated by the caller
+     * to set the correct fields. The passed Mail Configuration allows the user to override the
+     * default connection properties (SMTP host, SMTP port, SMTP from, etc).
+     * All the other <code>sendHtmlXXX()</code> and <code>sendTextXXX()</code> methods are
+     * specialized helper versions of this generic method.
+     *
+     * @param mail the already populated mail Object to be sent
+     * @param mailConfiguration the configuration to use
+     * @return 0 on success, -1 on failure. On failure the error message is stored in the XWiki
+     *         context under the "error" key.
+     */
+    int sendMail(Mail mail, MailConfiguration mailConfiguration);
+
     /**
      * Sends an HTML mail, with a list of attachments
      *
@@ -43,7 +87,8 @@ public interface MailSender
      * @param body the body content of the mail
      * @param alternative the alternative text offered to the mail client
      * @param attachments List of com.xpn.xwiki.api.Attachment that will be attached to the mail.
-     * @return 0 on success, -1 on failure. on failure the error message is stored in XWiki context
+     * @return 0 on success, -1 on failure. On failure the error message is stored in the XWiki
+     *         context under the "error" key.
      */
     int sendHtmlMessage(String from, String to, String cc, String bcc, String subject,
         String body, String alternative, List attachments);
@@ -55,7 +100,8 @@ public interface MailSender
      * @param from the sender
      * @param subject the subject of the message
      * @param message the body of the message
-     * @return 0 on success, -1 on failure. on failure the error message is stored in XWiki context
+     * @return 0 on success, -1 on failure. On failure the error message is stored in the XWiki
+     *         context under the "error" key.
      */
     int sendTextMessage(String from, String to, String subject, String message);
 
@@ -69,7 +115,8 @@ public interface MailSender
      * @param subject the subject of the message
      * @param message the body of the message
      * @param attachments List of com.xpn.xwiki.api.Attachment that will be attached to the mail.
-     * @return 0 on success, -1 on failure. on failure the error message is stored in XWiki context
+     * @return 0 on success, -1 on failure. On failure the error message is stored in the XWiki
+     *         context under the "error" key.
      */
     int sendTextMessage(String from, String to, String cc, String bcc, String subject,
         String message, List attachments);
@@ -86,7 +133,8 @@ public interface MailSender
      * @param documentFullName Full name of the template to be used (example:
      * XWiki.MyEmailTemplate). The template needs to have an XWiki.Email object attached
      * @param vcontext Velocity context passed to the velocity renderer
-     * @return True if the email has been sent
+     * @return 0 on success, -1 on failure. On failure the error message is stored in the XWiki
+     *         context under the "error" key.
      */
     int sendMessageFromTemplate(String from, String to, String cc, String bcc,
         String language, String documentFullName, VelocityContext vcontext);

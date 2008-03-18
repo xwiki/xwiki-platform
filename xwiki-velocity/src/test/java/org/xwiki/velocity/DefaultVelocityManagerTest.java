434f7a8318d0253c90c923f2359a226b53f9a155
vmassol <vincent@massol.net>
2008-03-18 11:17:52 +0000

    XWIKI-2220: Replace current Velocity configuration code by a Velocity Component
    * Added a unit test for DefaultVelocityManager
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-platform/core/trunk@8593 f329d543-caf0-0310-9063-dda96c69346f

---
+++ xwiki-velocity/src/test/java/org/xwiki/velocity/DefaultVelocityManagerTest.java
@@ -0,0 +1,82 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ *
+ */
+package org.xwiki.velocity;
+
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.Properties;
+
+import com.xpn.xwiki.test.AbstractXWikiComponentTestCase;
+
+/**
+ * Unit tests for {@link DefaultVelocityManager}.
+ */
+public class DefaultVelocityManagerTest extends AbstractXWikiComponentTestCase
+{
+    private VelocityManager manager;
+
+    protected void setUp() throws Exception
+    {
+        this.manager = (VelocityManager) getComponentManager().lookup(VelocityManager.ROLE);
+    }
+
+    public void testEvaluateReader() throws Exception
+    {
+        manager.initialize(new Properties());
+        StringWriter writer = new StringWriter();
+        manager.evaluate(new org.apache.velocity.VelocityContext(), writer, "mytemplate",
+            new StringReader("#set($foo='hello')$foo World"));
+        assertEquals("hello World", writer.toString());
+    }
+
+    public void testEvaluateString() throws Exception
+    {
+        manager.initialize(new Properties());
+        StringWriter writer = new StringWriter();
+        manager.evaluate(new org.apache.velocity.VelocityContext(), writer, "mytemplate",
+            "#set($foo='hello')$foo World");
+        assertEquals("hello World", writer.toString());
+    }
+
+    /**
+     * Verify that the default configuration doesn't allow calling Class.forName.
+     */
+    public void testSecureUberspectorActiveByDefault() throws Exception
+    {
+        manager.initialize(new Properties());
+        StringWriter writer = new StringWriter();
+        manager.evaluate(new org.apache.velocity.VelocityContext(), writer, "mytemplate",
+            "#set($foo = 'test')#set($object = $foo.class.forName('java.util.ArrayList').newInstance())$object.size()");
+        assertEquals("$object.size()", writer.toString());
+    }
+
+    public void testOverrideConfiguration() throws Exception
+    {
+        // For example try setting a non secure Uberspector.
+        Properties properties = new Properties();
+        properties.setProperty("runtime.introspector.uberspect", "org.apache.velocity.util.introspection.UberspectImpl");
+        manager.initialize(properties);
+        StringWriter writer = new StringWriter();
+        manager.evaluate(new org.apache.velocity.VelocityContext(), writer, "mytemplate",
+            "#set($foo = 'test')#set($object = $foo.class.forName('java.util.ArrayList').newInstance())$object.size()");
+        assertEquals("0", writer.toString());
+    }
+}

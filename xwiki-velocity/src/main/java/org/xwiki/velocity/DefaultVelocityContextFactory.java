55cd31f5855704626e82235be77ca2fa5b64bb6f
vmassol <vincent@massol.net>
2008-03-20 08:26:24 +0000

    XWIKI-2220: Replace current Velocity configuration code by a Velocity Component
    * Added missing license headers + some javadoc
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-platform/core/trunk@8625 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-velocity/src/main/java/org/xwiki/velocity/DefaultVelocityContextFactory.java
+++ xwiki-velocity/src/main/java/org/xwiki/velocity/DefaultVelocityContextFactory.java
@@ -1,3 +1,23 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ *
+ */
 package org.xwiki.velocity;
 
 import java.util.Enumeration;
@@ -10,11 +30,22 @@ import org.xwiki.component.logging.AbstractLogEnabled;
 import org.xwiki.component.phase.Initializable;
 import org.xwiki.component.phase.InitializationException;
 
+/**
+ * Default implementation for {@link VelocityContextFactory}.
+ */
 public class DefaultVelocityContextFactory extends AbstractLogEnabled
     implements VelocityContextFactory, Initializable
 {
+    /**
+     * The Velocity tools coming from the component's configuration and injected
+     * by the Component Manager.
+     */
     private Properties properties;
 
+    /**
+     * The Velocity Tools (instantiated as Objects) that we use to create new
+     * Velocity Contexts.
+     */
     private Map<String, Object> tools;
     
     /**
@@ -43,9 +74,12 @@ public class DefaultVelocityContextFactory extends AbstractLogEnabled
         }
     }
     
+    /**
+     * {@inheritDoc}
+     * @see VelocityContextFactory#createContext()
+     */
     public VelocityContext createContext()
     {
         return new VelocityContext(this.tools);
     }
-
 }
--- xwiki-velocity/src/main/java/org/xwiki/velocity/DefaultVelocityFactory.java
+++ xwiki-velocity/src/main/java/org/xwiki/velocity/DefaultVelocityFactory.java
@@ -17,7 +17,8 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  *
- */package org.xwiki.velocity;
+ */
+package org.xwiki.velocity;
 
 import java.util.HashMap;
 import java.util.Map;

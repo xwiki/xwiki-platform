d7f370190f862a38806196803f03ae3a63b27add
jvelo <jerome.velociter@gmail.com>
2008-03-25 18:10:50 +0000

    XTSHAREDTESTS-4 Allow to wait for different http response code than 400 in XWikiTestSetup (functional tests)
    - Also applied checkstyle
    
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-platform/xwiki-tools/trunk@8668 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-platform-tools/xwiki-shared-tests/src/main/java/com/xpn/xwiki/test/XWikiTestSetup.java
+++ xwiki-platform-tools/xwiki-shared-tests/src/main/java/com/xpn/xwiki/test/XWikiTestSetup.java
@@ -32,8 +32,9 @@ import java.net.URL;
 
 /**
  * JUnit TestSetup extension that starts/stops XWiki using a script passed using System Properties.
- * These properties are meant to be passed by the underlying build system. This class is meant
- * to wrap a JUnit TestSuite. For example:
+ * These properties are meant to be passed by the underlying build system. This class is meant to
+ * wrap a JUnit TestSuite. For example:
+ * 
  * <pre><code>
  * public static Test suite()
  * {
@@ -41,19 +42,28 @@ import java.net.URL;
  *     return new XWikiTestSetup(suite);
  * }
  * </code></pre>
- *
- * <p>Note: We could start XWiki using Java directly but we're using a script so that we can test
- * the exact same script used by XWiki users who download the standalone distribution.</p>
- *
+ * 
+ * <p>
+ * Note: We could start XWiki using Java directly but we're using a script so that we can test the
+ * exact same script used by XWiki users who download the standalone distribution.
+ * </p>
+ * 
  * @version $Id: $
  */
 public class XWikiTestSetup extends TestSetup
 {
-    private static final String EXECUTION_DIRECTORY = System.getProperty("xwikiExecutionDirectory");
+    private static final String EXECUTION_DIRECTORY =
+        System.getProperty("xwikiExecutionDirectory");
+
     private static final String START_COMMAND = System.getProperty("xwikiExecutionStartCommand");
+
     private static final String STOP_COMMAND = System.getProperty("xwikiExecutionStopCommand");
+
     private static final String PORT = System.getProperty("xwikiPort", "8080");
-    private static final boolean DEBUG = System.getProperty("debug", "false").equalsIgnoreCase("true");
+
+    private static final boolean DEBUG =
+        System.getProperty("debug", "false").equalsIgnoreCase("true");
+
     private static final int TIMEOUT_SECONDS = 60;
 
     private Project project;
@@ -63,7 +73,7 @@ public class XWikiTestSetup extends TestSetup
         super(test);
 
         this.project = new Project();
-	    this.project.init();
+        this.project.init();
         this.project.addBuildListener(new AntBuildListener(DEBUG));
     }
 
@@ -75,8 +85,10 @@ public class XWikiTestSetup extends TestSetup
 
     private void startXWikiInSeparateThread()
     {
-        Thread startThread = new Thread(new Runnable() {
-            public void run() {
+        Thread startThread = new Thread(new Runnable()
+        {
+            public void run()
+            {
                 try {
                     startXWiki();
                 } catch (Exception e) {
@@ -109,7 +121,7 @@ public class XWikiTestSetup extends TestSetup
     {
         // Wait till the main page becomes available which means the server is started fine
         System.out.println("Checking that XWiki is up and running...");
-        URL url = new URL("http://localhost:" + PORT + "/xwiki/bin/view/Main/");
+        URL url = new URL("http://localhost:" + PORT + "/xwiki/bin/view/Main/WebHome");
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         boolean connected = false;
         boolean timedOut = false;
@@ -119,11 +131,10 @@ public class XWikiTestSetup extends TestSetup
                 connection.connect();
                 int responseCode = connection.getResponseCode();
                 if (DEBUG) {
-                    System.out.println("Result of pinging [" + url + "] = ["
-                        + responseCode + "], Message = ["
-                        + connection.getResponseMessage() + "]");
+                    System.out.println("Result of pinging [" + url + "] = [" + responseCode
+                        + "], Message = [" + connection.getResponseMessage() + "]");
                 }
-                connected = (responseCode == 200);
+                connected = (responseCode < 400 || responseCode == 401);
             } catch (IOException e) {
                 // Do nothing as it simply means the server is not ready yet...
             }

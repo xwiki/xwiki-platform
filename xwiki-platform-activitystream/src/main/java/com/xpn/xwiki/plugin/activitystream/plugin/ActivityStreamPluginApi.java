fc89900ddda4e914bbfdb3e87d1e2b7bf438923a
jvelo <jerome.velociter@gmail.com>
2008-03-31 15:11:19 +0000

    XPAS-1 Integrate the activity stream plugin in platform-plugins/
    * Added javadoc documenting the plugin exposed API. The plugin implementation still need more documentation.
    
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-platform/xwiki-plugins/trunk/activitystream@8795 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-platform-activitystream/src/main/java/com/xpn/xwiki/plugin/activitystream/plugin/ActivityStreamPluginApi.java
+++ xwiki-platform-activitystream/src/main/java/com/xpn/xwiki/plugin/activitystream/plugin/ActivityStreamPluginApi.java
@@ -55,6 +55,13 @@ public class ActivityStreamPluginApi extends PluginApi
         return ((ActivityStreamPlugin) getProtectedPlugin()).getActivityStream();
     }
 
+    /**
+     * Record in database the given {@link com.xpn.xwiki.plugin.activitystream.api.ActivityEvent}
+     * event
+     * 
+     * @param event the event to record in databases
+     * @throws ActivityStreamException
+     */
     public void addActivityEvent(com.xpn.xwiki.plugin.activitystream.api.ActivityEvent event)
         throws ActivityStreamException
     {
@@ -63,6 +70,16 @@ public class ActivityStreamPluginApi extends PluginApi
         }
     }
 
+    /**
+     * Records in database the a event built upon the passed parameters
+     * 
+     * @param streamName the name of the stream to record the event for
+     * @param type the type of event. The type can be a value picked from the list of values defined
+     *            in {@link com.xpn.xwiki.plugin.activitystream.api.ActivityEventType} or any other
+     *            String
+     * @param title the event title
+     * @throws ActivityStreamException
+     */
     public void addActivityEvent(String streamName, String type, String title)
         throws ActivityStreamException
     {
@@ -71,6 +88,29 @@ public class ActivityStreamPluginApi extends PluginApi
         }
     }
 
+    /**
+     * @see #addActivityEvent(String, String, String)
+     * @param params a list of up to 5 "free" String parameters that will be associated with the
+     *            event
+     */
+    public void addActivityEvent(String streamName, String type, String title, List params)
+        throws ActivityStreamException
+    {
+        if (hasProgrammingRights()) {
+            getActivityStream().addActivityEvent(streamName, type, title, params, context);
+        }
+    }
+
+    /**
+     * Records in database an event built upon the passed document and other parameters. The passed
+     * document is used to retrieve document-related data for the event : date, page name, database.
+     * 
+     * @param streamName the name of the stream to add the event to
+     * @param doc the document from which to retrieve document data for the event
+     * @param type the type of event. (see {@link #addActivityEvent(String, String, String)}
+     * @param title the title of the event
+     * @throws ActivityStreamException
+     */
     public void addDocumentActivityEvent(String streamName, Document doc, String type,
         String title) throws ActivityStreamException
     {
@@ -80,6 +120,12 @@ public class ActivityStreamPluginApi extends PluginApi
         }
     }
 
+    /**
+     * @see #addDocumentActivityEvent(String, Document, String, String)
+     * @param priority the priority for this event. see
+     *            {@link com.xpn.xwiki.plugin.activitystream.api.ActivityEventPriority}
+     * @throws ActivityStreamException
+     */
     public void addDocumentActivityEvent(String streamName, Document doc, String type,
         int priority, String title) throws ActivityStreamException
     {
@@ -89,14 +135,12 @@ public class ActivityStreamPluginApi extends PluginApi
         }
     }
 
-    public void addActivityEvent(String streamName, String type, String title, List params)
-        throws ActivityStreamException
-    {
-        if (hasProgrammingRights()) {
-            getActivityStream().addActivityEvent(streamName, type, title, params, context);
-        }
-    }
-
+    /**
+     * @see #addDocumentActivityEvent(String, Document, String, String)
+     * @param params a list of up to 5 "free" String parameters that will be associated with the
+     *            event
+     * @throws ActivityStreamException
+     */
     public void addDocumentActivityEvent(String streamName, Document doc, String type,
         String title, List params) throws ActivityStreamException
     {
@@ -106,6 +150,10 @@ public class ActivityStreamPluginApi extends PluginApi
         }
     }
 
+    /**
+     * @see #addDocumentActivityEvent(String, Document, String, int, String)
+     * @see #addDocumentActivityEvent(String, Document, String, String, List)
+     */
     public void addDocumentActivityEvent(String streamName, Document doc, String type,
         int priority, String title, List params) throws ActivityStreamException
     {
@@ -115,6 +163,18 @@ public class ActivityStreamPluginApi extends PluginApi
         }
     }
 
+    /**
+     * Search in database activity events matching the given hql query. Retrieve events are ordered
+     * by date descending.
+     * 
+     * @param hql the "where" clause of the hql query to look events for
+     * @param filter if true, group the matched events by priority
+     * @param nb the number of events to retrieve
+     * @param start the offset to start retrieving event at
+     * @return a list of matching events, wrapped as
+     *         {@link com.xpn.xwiki.plugin.activitystream.plugin.ActivityEvent} objects.
+     * @throws ActivityStreamException
+     */
     public List searchEvents(String hql, boolean filter, int nb, int start)
         throws ActivityStreamException
     {
@@ -125,6 +185,14 @@ public class ActivityStreamPluginApi extends PluginApi
         }
     }
 
+    /**
+     * Return the latest recorded events
+     * 
+     * @param filter if true, group the matched events by priority
+     * @param nb the number of events to retrieve
+     * @param start the offset to start retrieving event at
+     * @throws ActivityStreamException
+     */
     public List getEvents(boolean filter, int nb, int start) throws ActivityStreamException
     {
         if (hasProgrammingRights()) {
@@ -134,6 +202,13 @@ public class ActivityStreamPluginApi extends PluginApi
         }
     }
 
+    /**
+     * Return the latest recorded events for the given wiki space
+     * 
+     * @see #getEvents(boolean, int, int)
+     * @param space the space to retrieve latest events for
+     * @throws ActivityStreamException
+     */
     public List getEventsForSpace(String space, boolean filter, int nb, int start)
         throws ActivityStreamException
     {
@@ -144,7 +219,14 @@ public class ActivityStreamPluginApi extends PluginApi
             return null;
         }
     }
-
+    
+    /**
+     * Return the latest recorded events triggered by the given user.
+     * 
+     * @see #getEvents(boolean, int, int)
+     * @param user the user to retrieve latest events for
+     * @throws ActivityStreamException
+     */
     public List getEventsForUser(String user, boolean filter, int nb, int start)
         throws ActivityStreamException
     {
@@ -156,6 +238,13 @@ public class ActivityStreamPluginApi extends PluginApi
         }
     }
 
+    /**
+     * Return the latest events recorded for the given stream name
+     * 
+     * @see #getEvents(boolean, int, int)
+     * @param streamName the name of the stream to retrieve latest events for
+     * @throws ActivityStreamException
+     */
     public List getEvents(String streamName, boolean filter, int nb, int start)
         throws ActivityStreamException
     {
@@ -167,6 +256,12 @@ public class ActivityStreamPluginApi extends PluginApi
         }
     }
 
+    /**
+     * Returns the latest events recorded for the given stream name and wiki space name
+     * 
+     * @see #getEventsForSpace(String, boolean, int, int)
+     * @see #getEvents(String, boolean, int, int)
+     */
     public List getEventsForSpace(String streamName, String space, boolean filter, int nb,
         int start) throws ActivityStreamException
     {
@@ -178,6 +273,12 @@ public class ActivityStreamPluginApi extends PluginApi
         }
     }
 
+    /**
+     * Returns the latest events recorded for the given stream name and user name
+     * 
+     * @see #getEventsForUser(String, boolean, int, int)
+     * @see #getEvents(String, boolean, int, int)
+     */
     public List getEventsForUser(String streamName, String user, boolean filter, int nb, int start)
         throws ActivityStreamException
     {

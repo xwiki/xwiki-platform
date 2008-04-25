891c69252bdff1e7aaa26960ffcd2d456523535c
sdumitriu <sergiu@xwiki.com>
2008-04-25 00:50:17 +0000

    XPLUCENE-10: Reduce the default indexing period to 1 minute
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-platform/xwiki-plugins/trunk@9365 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-platform-tag/plugin/lucene/src/main/java/com/xpn/xwiki/plugin/lucene/IndexUpdater.java
+++ xwiki-platform-tag/plugin/lucene/src/main/java/com/xpn/xwiki/plugin/lucene/IndexUpdater.java
@@ -55,7 +55,7 @@ public class IndexUpdater implements Runnable, XWikiDocChangeNotificationInterfa
     /**
      * Milliseconds of sleep between checks for changed documents
      */
-    private int indexingInterval = 3000;
+    private int indexingInterval = 60000;
 
     private boolean exit = false;
 
@@ -405,7 +405,7 @@ public class IndexUpdater implements Runnable, XWikiDocChangeNotificationInterfa
 
         this.indexingInterval =
             1000 * Integer.parseInt(config
-                .getProperty(LucenePlugin.PROP_INDEXING_INTERVAL, "300"));
+                .getProperty(LucenePlugin.PROP_INDEXING_INTERVAL, "60"));
 
         // Note: There's no need to open the Searcher here (with a call to
         // openSearcher()) as each

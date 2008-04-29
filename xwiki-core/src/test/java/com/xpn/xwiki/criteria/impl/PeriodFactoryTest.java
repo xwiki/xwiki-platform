c09e0bb52e5886e49330f82a616acf13edd6391b
tmortagne <thomas.mortagne@xwiki.com>
2008-04-29 14:44:08 +0000

    XWIKI-2326: Activity statistics merge two days in one in graphics
    Applied patch from Marius Dumitru Florea without any modification.
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-platform/core/trunk@9581 f329d543-caf0-0310-9063-dda96c69346f

---
+++ xwiki-core/src/test/java/com/xpn/xwiki/criteria/impl/PeriodFactoryTest.java
@@ -0,0 +1,69 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package com.xpn.xwiki.criteria.impl;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import junit.framework.TestCase;
+
+import com.xpn.xwiki.stats.impl.xwiki.XWikiStatsReader;
+
+/**
+ * Unit tests for {@link PeriodFactory}.
+ */
+public class PeriodFactoryTest extends TestCase
+{
+    /**
+     * Tests if the start code of a day period is strictly less than its end code. This is needed
+     * because the {@link XWikiStatsReader} retrieves the statistics that have been recorded between
+     * the start code, including it, and the end code, excluding it.
+     */
+    public void testDayPeriodStartEndCode()
+    {
+        Period day = PeriodFactory.createDayPeriod(System.currentTimeMillis());
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+        try {
+            long start = sdf.parse(String.valueOf(day.getStartCode())).getTime();
+            long end = sdf.parse(String.valueOf(day.getEndCode())).getTime();
+            assertTrue("Start code must be less than end code", start < end);
+        } catch (ParseException e) {
+            assertTrue(e.getMessage(), false);
+        }
+    }
+
+    /**
+     * Tests if the start code of a month period is strictly less than its end code. This is needed
+     * because the {@link XWikiStatsReader} retrieves the statistics that have been recorded between
+     * the start code, including it, and the end code, excluding it.
+     */
+    public void testMonthPeriodStartEndCode()
+    {
+        Period month = PeriodFactory.createMonthPeriod(System.currentTimeMillis());
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMM");
+        try {
+            long start = sdf.parse(String.valueOf(month.getStartCode())).getTime();
+            long end = sdf.parse(String.valueOf(month.getEndCode())).getTime();
+            assertTrue("Start code must be less than end code", start < end);
+        } catch (ParseException e) {
+            assertTrue(e.getMessage(), false);
+        }
+    }
+}

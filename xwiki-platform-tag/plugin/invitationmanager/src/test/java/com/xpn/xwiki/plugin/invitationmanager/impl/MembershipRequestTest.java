a1dd207e19bcb3cd1b904149e0b192707b38a40b
jvelo <jerome.velociter@gmail.com>
2008-05-08 10:19:13 +0000

    XPIM-1 Integrate the Invitation Manager plugin as a platform-plugin
    - Renamed IM package to match package naming strategy defined at http://tinyurl.com/2m3dmd
    - Moved the plugin from products-curriki/ to platform-plugins/
    - Modified curriki's build accordingly (web/pom.xml, plugins/pom.xml & currikispacemanager's pom.xml)
    - Added the invitationmanager plugin to curriki and XWS profiles in plaform-plugins parent pom
    
    
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-platform/xwiki-plugins/trunk@9691 f329d543-caf0-0310-9063-dda96c69346f

---
+++ xwiki-platform-tag/plugin/invitationmanager/src/test/java/com/xpn/xwiki/plugin/invitationmanager/impl/MembershipRequestTest.java
@@ -0,0 +1,53 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package com.xpn.xwiki.plugin.invitationmanager.impl;
+
+import com.xpn.xwiki.plugin.invitationmanager.api.MembershipRequest;
+
+/**
+ * Unit tests for classes implementing {@link MembershipRequest} interface
+ */
+public abstract class MembershipRequestTest extends JoinRequestTest
+{
+    public MembershipRequest getMembershipRequest()
+    {
+        return (MembershipRequest) joinRequest;
+    }
+
+    /**
+     * test for {@link MembershipRequest#getRequester()}
+     */
+    public void testRequester()
+    {
+        String requester = "foo";
+        getMembershipRequest().setRequester(requester);
+        assertEquals(requester, getMembershipRequest().getRequester());
+    }
+
+    /**
+     * test for {@link MembershipRequest#getResponder()}
+     */
+    public void testResponder()
+    {
+        String responder = "bar";
+        getMembershipRequest().setResponder(responder);
+        assertEquals(responder, getMembershipRequest().getResponder());
+    }
+}

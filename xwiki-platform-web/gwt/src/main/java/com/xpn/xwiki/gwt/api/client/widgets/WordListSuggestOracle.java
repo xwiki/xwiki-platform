d06c430407a33ac094af94e785dbed83cc43887d
lucaa <lucaa@xwiki.com>
2008-05-30 13:59:07 +0000

    XWIKI-2418: Improve GWT suggest API * added a flag to the WordListSuggestOracle to specify whether to eliminate the suggestions that have already been typed or not.
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-platform/web/branches/xwiki-web-1.4@10005 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-platform-web/gwt/src/main/java/com/xpn/xwiki/gwt/api/client/widgets/WordListSuggestOracle.java
+++ xwiki-platform-web/gwt/src/main/java/com/xpn/xwiki/gwt/api/client/widgets/WordListSuggestOracle.java
@@ -22,15 +22,19 @@ package com.xpn.xwiki.gwt.api.client.widgets;
 
 import com.google.gwt.user.client.ui.SuggestOracle;
 import com.google.gwt.user.client.ui.MultiWordSuggestOracle;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.ArrayList;
+import java.util.Set;
 
 /**
  * SuggestOracle to suggest a list of words with respect to a given list of separators.
  *
  * The <tt>SuggestOracle</tt> used for a single word suggestion is set through
  * {@link com.xpn.xwiki.gwt.api.client.widgets.WordListSuggestOracle#setWordOracle(com.google.gwt.user.client.ui.SuggestOracle)}
- * and the separators through {@link com.xpn.xwiki.gwt.api.client.widgets.WordListSuggestOracle#setSeparators(String)}
+ * and the separators through {@link com.xpn.xwiki.gwt.api.client.widgets.WordListSuggestOracle#setSeparators(String)}.
+ * Whether this oracle generates unique suggestions with respect to the already typed list, can 
+ * be configured through {@link WordListSuggestOracle#isSuggestUnique()}.
  */
 public class WordListSuggestOracle extends SuggestOracle {
     /**
@@ -41,32 +45,55 @@ public class WordListSuggestOracle extends SuggestOracle {
      * multiple words separator
      */
     protected String separators;
+    /**
+     * whether this oracle omits words that already are in the typed list or not
+     */
+    protected boolean suggestUnique;
 
     /**
-     * Default constructor: builds an oracle with an empty {@link com.google.gwt.user.client.ui.SuggestOracle}
-     * and the space as a separator.
+     * Default constructor: builds an oracle with an empty {@link com.google.gwt.user.client.ui.SuggestOracle} 
+     * and the space as a separator. 
+     * By default, this oracle generates suggestions that duplicate existing words in the list.
      */
     public WordListSuggestOracle() {
-        this(new MultiWordSuggestOracle(), " ");
+        this(new MultiWordSuggestOracle(), " ", false);
     }
 
     /**
-     * Builds an oracle with the specified {@link com.google.gwt.user.client.ui.SuggestOracle}
-     * and the space as a default separator.
+     * Builds an oracle with the specified {@link com.google.gwt.user.client.ui.SuggestOracle} and
+     * the space as a default separator. By default, this oracle generates suggestions that
+     * duplicate existing words in the list.
+     * 
      * @param wordOracle the oracle to be used for predicting a single word.
      */
     public WordListSuggestOracle(SuggestOracle wordOracle) {
-        this(wordOracle, " ");
+        this(wordOracle, " ", false);
     }
 
     /**
-     * Builds an wordOracle with the specified {@link com.google.gwt.user.client.ui.SuggestOracle} and separators.
+     * Builds an wordOracle with the specified {@link com.google.gwt.user.client.ui.SuggestOracle}
+     * and separators. By default, this oracle generates suggestions that duplicate existing words
+     * in the list.
+     * 
      * @param wordOracle the wordOracle to be used for predicting a single word
      * @param separators the separators between the words in the word list
      */
     public WordListSuggestOracle(SuggestOracle wordOracle, String separators) {
+        this(wordOracle, separators, false);
+    }
+
+    /**
+     * Builds an wordOracle with the specified {@link com.google.gwt.user.client.ui.SuggestOracle},
+     * separators and specifying weather it suggests words that are already in the list or not.
+     * 
+     * @param wordOracle the wordOracle to be used for predicting a single word
+     * @param separators the separators between the words in the word list
+     */
+    public WordListSuggestOracle(SuggestOracle wordOracle, String separators,
+        boolean suggestUnique) {
         this.setWordOracle(wordOracle);
         this.setSeparators(separators);
+        this.setSuggestUnique(suggestUnique);
     }
 
     /**
@@ -87,20 +114,34 @@ public class WordListSuggestOracle extends SuggestOracle {
             }
         }
         String oneWordQuery = query.substring(lastSeparator + 1);
-        final String prefix = query.substring(0, lastSeparator + 1);
-        //get the response
+        final String prefix = query.substring(0, lastSeparator + 1);    
+        // If this oracle is set to suggest unique, parse the rest of the list and get the
+        // existing words
+        final Set existingWords;
+        // Do the parsing only if needed. It might be too time consuming 
+        if (this.suggestUnique) {
+            existingWords = this.parseQueryString(prefix);
+        } else {
+            existingWords = new HashSet();
+        }
+        // Get the response from the embedded wordOracle
         request.setQuery(oneWordQuery);
         Callback innerCallback = new Callback() {
             public void onSuggestionsReady(Request defaultRequest, Response defaultResponse) {
                 Response wordListResponse = new Response();
                 ArrayList suggestions = new ArrayList();
-                //generate the word list response from the single response one
+                // Generate the word list response from the single response one
                 for (Iterator sIt = defaultResponse.getSuggestions().iterator(); sIt.hasNext();) {
-                    Suggestion oldSuggestion = (Suggestion)sIt.next();
-                    //display one, replace all
+                    Suggestion oldSuggestion = (Suggestion) sIt.next();
+                    String oneWordSuggestion = oldSuggestion.getReplacementString();
+                    // If we suggest unique and suggestion already exists, skip it
+                    if (suggestUnique && existingWords.contains(oneWordSuggestion)) {
+                        continue;
+                    }
+                    // Display one, replace all
                     MultiWordSuggestOracle.MultiWordSuggestion newSuggestion =
-                            new MultiWordSuggestOracle.MultiWordSuggestion(prefix + oldSuggestion.getReplacementString(),
-                            oldSuggestion.getDisplayString());
+                        new MultiWordSuggestOracle.MultiWordSuggestion(
+                            prefix + oneWordSuggestion, oldSuggestion.getDisplayString());
                     suggestions.add(newSuggestion);
                 }
                 wordListResponse.setSuggestions(suggestions);
@@ -108,6 +149,40 @@ public class WordListSuggestOracle extends SuggestOracle {
             }
         };
         this.wordOracle.requestSuggestions(request, innerCallback);
+    }
+    
+    /**
+     * Parses the passed query string into a list of tokens, based on the instance 
+     * <tt>separators</tt>.
+     * 
+     * @param queryString the string to parse
+     * @return the list of words, excluding empty words and duplicates 
+     */
+    protected Set parseQueryString(String queryString) {
+        Set existingWords = new HashSet();
+        int lastSeparator = queryString.length();
+        if (this.suggestUnique) {
+            while (lastSeparator > 0) {
+                int currentLastSeparator = -1;
+                // Find the last occurrence of any separator 
+                for (int i = 0; i < this.separators.length(); i++) {
+                    int currentSeparator =
+                        queryString.substring(0, lastSeparator).lastIndexOf(
+                            this.separators.charAt(i));
+                    if (currentSeparator > currentLastSeparator) {
+                        currentLastSeparator = currentSeparator;
+                    }
+                }
+                // Get the current word determined by the last separator
+                String currentWord =
+                    queryString.substring(currentLastSeparator + 1, lastSeparator);
+                if (currentWord.length() > 0) {
+                    existingWords.add(currentWord);
+                }
+                lastSeparator = currentLastSeparator;
+            }
+        }
+        return existingWords;
     }
 
     public String getSeparators() {
@@ -128,5 +203,13 @@ public class WordListSuggestOracle extends SuggestOracle {
 
     public void setWordOracle(SuggestOracle wordOracle) {
         this.wordOracle = wordOracle;
+    }
+
+    public boolean getSuggestUnique() {
+        return suggestUnique;
+    }
+
+    public void setSuggestUnique(boolean suggestUnique) {
+        this.suggestUnique = suggestUnique;
     }
 }

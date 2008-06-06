2c5d0f3e1e5416d328e6b9c615ae66c1ab9d1fd8
jvdrean <jv@drean.org>
2008-06-06 17:39:23 +0000

    XWIKI-2422 : Curly Quote coming from a Copy Paste is transformed into ? after cache is reset
    Applied patch from Ludovic Dubost with modifications.
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-platform/core/trunk/xwiki-core@10137 f329d543-caf0-0310-9063-dda96c69346f

--- src/main/java/com/xpn/xwiki/web/XWikiServletRequest.java
+++ src/main/java/com/xpn/xwiki/web/XWikiServletRequest.java
@@ -34,9 +34,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.security.Principal;
-import java.util.Enumeration;
-import java.util.Locale;
-import java.util.Map;
+import java.util.*;
 
 import com.xpn.xwiki.util.Util;
 
@@ -47,8 +45,96 @@ public class XWikiServletRequest implements XWikiRequest {
         this.request = request;
     }
 
+    /**
+     * Turn Windows CP1252 Characters to iso-8859-1 characters when possible, HTML entities when needed.
+     * This filtering works on Tomcat (not Jetty).
+     *
+     * @param text The text to filter
+     * @return filtered text
+     */
+    public String filterString(String text) {
+        if (text==null)
+            return null;
+
+        // In case we are running in ISO we need to take care or some windows-1252 characters
+        // that are commonly copy pasted by users from web sites or desktop applications
+        // If we don't transform these characters then some databases running in the latin charset mode
+        // will drop the characters and will we only see that on server restart.
+        // This happens for example using MySQL both with tomcat and Jetty
+        // See bug : http://jira.xwiki.org/jira/browse/XWIKI-2422
+        // Source : http://www.microsoft.com/typography/unicode/1252.htm
+        if (request.getCharacterEncoding().startsWith("ISO-8859")) {
+            // EURO SIGN
+            text = text.replaceAll("\u0080", "&euro;");
+            // SINGLE LOW-9 QUOTATION MARK
+            text = text.replaceAll("\u0082", "&sbquo;");
+            // LATIN SMALL LETTER F WITH HOOK
+            text = text.replaceAll("\u0083", "&fnof;");
+            // DOUBLE LOW-9 QUOTATION MARK
+            text = text.replaceAll("\u0084", "&bdquo;");
+            // HORIZONTAL ELLIPSIS, entity : &hellip;
+            text = text.replaceAll("\u0085", "...");
+            // DAGGER
+            text = text.replaceAll("\u0086", "&dagger;");
+            // DOUBLE DAGGER
+            text = text.replaceAll("\u0087", "&Dagger;");
+            // MODIFIER LETTER CIRCUMFLEX ACCENT
+            text = text.replaceAll("\u0088", "&circ;");
+            // PER MILLE SIGN
+            text = text.replaceAll("\u0089", "&permil;");
+            // LATIN CAPITAL LETTER S WITH CARON
+            text = text.replaceAll("\u008a", "&Scaron;");
+            // SINGLE LEFT-POINTING ANGLE QUOTATION MARK, entity : &lsaquo;
+            text = text.replaceAll("\u008b", "'");
+            // LATIN CAPITAL LIGATURE OE
+            text = text.replaceAll("\u008c", "&OElig;");
+            // LATIN CAPITAL LETTER Z WITH CARON
+            text = text.replaceAll("\u008e", "&#381;");
+            // LEFT SINGLE QUOTATION MARK, entity : &lsquo;
+            text = text.replaceAll("\u0091", "'");
+            // RIGHT SINGLE QUOTATION MARK, entity : &rsquo;
+            text = text.replaceAll("\u0092", "'");
+            // LEFT DOUBLE QUOTATION MARK, entity : &ldquo;
+            text = text.replaceAll("\u0093", "\"");
+            // RIGHT DOUBLE QUOTATION MARK, entity : &rdquo;
+            text = text.replaceAll("\u0094", "\"");
+            // BULLET
+            text = text.replaceAll("\u0095", "&bull;");
+            // EN DASH, entity : &ndash;
+            text = text.replaceAll("\u0096", "-");
+            // EM DASH, entity : &mdash;
+            text = text.replaceAll("\u0097", "-");
+            // SMALL TILDE
+            text = text.replaceAll("\u0098", "&tilde;");
+            // TRADE MARK SIGN
+            text = text.replaceAll("\u0099", "&trade;");
+            // LATIN SMALL LETTER S WITH CARON
+            text = text.replaceAll("\u009a", "&scaron;");
+            // SINGLE RIGHT-POINTING ANGLE QUOTATION MARK, entity : &rsaquo;
+            text = text.replaceAll("\u009b", "'");
+            // LATIN SMALL LIGATURE OE
+            text = text.replaceAll("\u009c", "&oelig;");
+            // LATIN SMALL LETTER Z WITH CARON
+            text = text.replaceAll("\u009e", "&#382;");
+            // LATIN CAPITAL LETTER Y WITH DIAERESIS
+            text = text.replaceAll("\u009f", "&Yuml;");
+
+        }
+        return text;
+    }
+
+    public String[] filterStringArray(String[] text) {
+        if (text==null)
+         return null;
+
+        for (int i=0;i<text.length;i++) {
+            text[i] = filterString(text[i]);
+        }
+        return text;
+    }
+
     public String get(String name) {
-        return request.getParameter(name);
+        return filterString(request.getParameter(name));
     }
 
     public HttpServletRequest getHttpServletRequest() {
@@ -195,7 +281,7 @@ public class XWikiServletRequest implements XWikiRequest {
     }
 
     public String getParameter(String s) {
-        return request.getParameter(s);
+        return filterString(request.getParameter(s));
     }
 
     public Enumeration getParameterNames() {
@@ -203,11 +289,25 @@ public class XWikiServletRequest implements XWikiRequest {
     }
 
     public String[] getParameterValues(String s) {
-        return request.getParameterValues(s);
+        String[] origResult = request.getParameterValues(s);
+        return filterStringArray(origResult);
     }
 
     public Map getParameterMap() {
-        return request.getParameterMap();
+        Map newMap = new HashMap();
+        Map map = request.getParameterMap();
+        Iterator it = map.keySet().iterator();
+        while (it.hasNext()) {
+           String key = (String) it.next();
+           Object value = map.get(key);
+           if (value instanceof String)
+             newMap.put(key, filterString((String) value));
+           else if (value instanceof String[])
+             newMap.put(key, filterStringArray((String[]) value));
+           else
+             newMap.put(key, value);
+        }
+        return map;
     }
 
     public String getProtocol() {

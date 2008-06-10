f0185731abdcc772706d986d2d14436459319c6d
sdumitriu <sergiu@xwiki.com>
2008-06-10 21:56:23 +0000

    XWIKI-2451: document.clone() does not keep the original document XWIKI-2450: Wrong notifications sent when saving documents from a script Fixed.
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-platform/core/trunk/xwiki-core@10239 f329d543-caf0-0310-9063-dda96c69346f

--- src/test/java/com/xpn/xwiki/XWikiNotificationTest.java
+++ src/test/java/com/xpn/xwiki/XWikiNotificationTest.java
@@ -20,18 +20,21 @@
  */
 package com.xpn.xwiki;
 
+import org.jmock.Mock;
+import org.jmock.MockObjectTestCase;
+
+import com.xpn.xwiki.api.Document;
 import com.xpn.xwiki.doc.XWikiDocument;
 import com.xpn.xwiki.notify.DocChangeRule;
 import com.xpn.xwiki.notify.XWikiDocChangeNotificationInterface;
 import com.xpn.xwiki.notify.XWikiNotificationManager;
 import com.xpn.xwiki.notify.XWikiNotificationRule;
 import com.xpn.xwiki.store.XWikiStoreInterface;
-import org.jmock.Mock;
-import org.jmock.MockObjectTestCase;
+import com.xpn.xwiki.user.api.XWikiRightService;
 
 /**
  * Verify that notifications are correctly sent in the {@link XWiki} class.
- *
+ * 
  * @version $Id$
  */
 public class XWikiNotificationTest extends MockObjectTestCase
@@ -40,36 +43,72 @@ public class XWikiNotificationTest extends MockObjectTestCase
     {
         public boolean hasListenerBeenCalled = false;
 
-        public void notify(XWikiNotificationRule rule, XWikiDocument newdoc, XWikiDocument olddoc,
-            int event, XWikiContext context)
+        public boolean expectedNewStatus = true;
+
+        public void notify(XWikiNotificationRule rule, XWikiDocument newdoc, XWikiDocument olddoc, int event,
+            XWikiContext context)
         {
             assertEquals("Space.Page", newdoc.getFullName());
             assertNotNull("Shouldn't have been null", olddoc);
-            assertTrue("Should have been new, since this is a new document", olddoc.isNew());
+            assertEquals("Should have been new, since this is a new document", this.expectedNewStatus, olddoc.isNew());
             this.hasListenerBeenCalled = true;
         }
     }
 
+    XWikiContext context;
+
+    XWiki wiki;
+
+    @Override
+    public void setUp() throws XWikiException
+    {
+        this.context = new XWikiContext();
+        this.wiki = new XWiki(new XWikiConfig(), this.context);
+        this.wiki.setNotificationManager(new XWikiNotificationManager());
+
+        Mock mockStore = mock(XWikiStoreInterface.class);
+        mockStore.expects(atLeastOnce()).method("saveXWikiDoc");
+        this.wiki.setStore((XWikiStoreInterface) mockStore.proxy());
+    }
+
     /**
-     * We only verify here that the saveDocument API calls the Notification Manager. Detailed
-     * tests of the notification classes are implemented in the notification package.
+     * We only verify here that the saveDocument API calls the Notification Manager. Detailed tests of the notification
+     * classes are implemented in the notification package.
      */
     public void testSaveDocumentSendNotifications() throws Exception
     {
-        XWikiContext context = new XWikiContext();
-        XWiki wiki = new XWiki(new XWikiConfig(), context);
-        wiki.setNotificationManager(new XWikiNotificationManager());
-
-        Mock mockStore = mock(XWikiStoreInterface.class);
-        mockStore.expects(once()).method("saveXWikiDoc");
-        wiki.setStore((XWikiStoreInterface) mockStore.proxy());
-
         TestListener listener = new TestListener();
-        wiki.getNotificationManager().addGeneralRule(new DocChangeRule(listener));
+        this.wiki.getNotificationManager().addGeneralRule(new DocChangeRule(listener));
 
         XWikiDocument document = new XWikiDocument("Space", "Page");
 
-        wiki.saveDocument(document, context);
-        assertTrue("Listener not called",listener.hasListenerBeenCalled);
+        this.wiki.saveDocument(document, this.context);
+        assertTrue("Listener not called", listener.hasListenerBeenCalled);
+    }
+
+    /**
+     * We only verify here that the saveDocument API calls the Notification Manager. Detailed tests of the notification
+     * classes are implemented in the notification package.
+     */
+    public void testSaveDocumentFromAPIUsesCorrectOriginalDocument() throws Exception
+    {
+        Mock mockRights = mock(XWikiRightService.class);
+        mockRights.stubs().method("hasAccessLevel").will(returnValue(true));
+        this.wiki.setRightService((XWikiRightService) mockRights.proxy());
+
+        TestListener listener = new TestListener();
+        listener.expectedNewStatus = false;
+        this.wiki.getNotificationManager().addGeneralRule(new DocChangeRule(listener));
+
+        XWikiDocument original = new XWikiDocument("Space", "Page");
+        original.setNew(false);
+        original.setContent("Old content");
+        XWikiDocument document = new XWikiDocument("Space", "Page");
+        document.setContent("New content");
+        document.setOriginalDocument(original);
+
+        Document api = new Document(document, this.context);
+        api.save();
+        assertTrue("Listener not called", listener.hasListenerBeenCalled);
     }
 }

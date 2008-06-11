7c479def56080d9e7053de4f805527e4f11dd7b4
sdumitriu <sergiu@xwiki.com>
2008-06-11 23:51:17 +0000

    [cleanup] Apply codestyle
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-platform/core/trunk/xwiki-core@10269 f329d543-caf0-0310-9063-dda96c69346f

--- src/test/java/com/xpn/xwiki/doc/XWikiDocumentTest.java
+++ src/test/java/com/xpn/xwiki/doc/XWikiDocumentTest.java
@@ -24,6 +24,7 @@ import java.util.List;
 
 import org.jmock.Mock;
 import org.jmock.cglib.MockObjectTestCase;
+
 import com.xpn.xwiki.XWiki;
 import com.xpn.xwiki.XWikiConfig;
 import com.xpn.xwiki.XWikiContext;
@@ -34,7 +35,7 @@ import com.xpn.xwiki.store.XWikiVersioningStoreInterface;
 
 /**
  * Unit tests for {@link XWikiDocument}.
- *
+ * 
  * @version $Id$
  */
 public class XWikiDocumentTest extends MockObjectTestCase
@@ -49,26 +50,24 @@ public class XWikiDocumentTest extends MockObjectTestCase
 
     private Mock mockXWikiVersioningStore;
 
+    @Override
     protected void setUp()
     {
         this.context = new XWikiContext();
         this.document = new XWikiDocument("Space", "Page");
 
         this.mockXWiki =
-            mock(XWiki.class, new Class[]{XWikiConfig.class, XWikiContext.class}, new Object[]{
-                new XWikiConfig(), this.context});
+            mock(XWiki.class, new Class[] {XWikiConfig.class, XWikiContext.class}, new Object[] {new XWikiConfig(),
+            this.context});
         this.mockXWiki.stubs().method("Param").will(returnValue(null));
 
         this.mockXWikiRenderingEngine = mock(XWikiRenderingEngine.class);
 
         this.mockXWikiVersioningStore = mock(XWikiVersioningStoreInterface.class);
-        this.mockXWikiVersioningStore.stubs().method("getXWikiDocumentArchive").will(
-            returnValue(null));
+        this.mockXWikiVersioningStore.stubs().method("getXWikiDocumentArchive").will(returnValue(null));
 
-        this.mockXWiki.stubs().method("getRenderingEngine").will(
-            returnValue(this.mockXWikiRenderingEngine.proxy()));
-        this.mockXWiki.stubs().method("getVersioningStore").will(
-            returnValue(this.mockXWikiVersioningStore.proxy()));
+        this.mockXWiki.stubs().method("getRenderingEngine").will(returnValue(this.mockXWikiRenderingEngine.proxy()));
+        this.mockXWiki.stubs().method("getVersioningStore").will(returnValue(this.mockXWikiVersioningStore.proxy()));
 
         this.context.setWiki((XWiki) this.mockXWiki.proxy());
     }
@@ -84,8 +83,8 @@ public class XWikiDocumentTest extends MockObjectTestCase
     {
         this.document.setContent("Some content");
         this.document.setTitle("Title");
-        this.mockXWikiRenderingEngine.expects(once()).method("interpretText").with(eq("Title"),
-            ANYTHING, ANYTHING).will(returnValue("Title"));
+        this.mockXWikiRenderingEngine.expects(once()).method("interpretText").with(eq("Title"), ANYTHING, ANYTHING)
+            .will(returnValue("Title"));
 
         assertEquals("Title", this.document.getDisplayTitle(this.context));
     }
@@ -93,8 +92,8 @@ public class XWikiDocumentTest extends MockObjectTestCase
     public void testGetDisplayWhenNoTitleButSectionExists()
     {
         this.document.setContent("Some content\n1 Title");
-        this.mockXWikiRenderingEngine.expects(once()).method("interpretText").with(eq("Title"),
-            ANYTHING, ANYTHING).will(returnValue("Title"));
+        this.mockXWikiRenderingEngine.expects(once()).method("interpretText").with(eq("Title"), ANYTHING, ANYTHING)
+            .will(returnValue("Title"));
 
         assertEquals("Title", this.document.getDisplayTitle(this.context));
     }
@@ -124,8 +123,7 @@ public class XWikiDocumentTest extends MockObjectTestCase
     {
         String author = "Albatross";
         this.document.setAuthor(author);
-        XWikiDocument copy =
-            this.document.copyDocument(this.document.getName() + " Copy", this.context);
+        XWikiDocument copy = this.document.copyDocument(this.document.getName() + " Copy", this.context);
 
         assertTrue(author.equals(copy.getAuthor()));
     }
@@ -134,8 +132,7 @@ public class XWikiDocumentTest extends MockObjectTestCase
     {
         String creator = "Condor";
         this.document.setCreator(creator);
-        XWikiDocument copy =
-            this.document.copyDocument(this.document.getName() + " Copy", this.context);
+        XWikiDocument copy = this.document.copyDocument(this.document.getName() + " Copy", this.context);
 
         assertTrue(creator.equals(copy.getCreator()));
     }
@@ -144,15 +141,14 @@ public class XWikiDocumentTest extends MockObjectTestCase
     {
         Date sourceCreationDate = this.document.getCreationDate();
         Thread.sleep(1000);
-        XWikiDocument copy =
-            this.document.copyDocument(this.document.getName() + " Copy", this.context);
+        XWikiDocument copy = this.document.copyDocument(this.document.getName() + " Copy", this.context);
 
         assertTrue(copy.getCreationDate().equals(sourceCreationDate));
     }
 
     public void testToStringReturnsFullName()
     {
-        assertEquals("Space.Page", document.toString());
+        assertEquals("Space.Page", this.document.toString());
         assertEquals("Main.WebHome", new XWikiDocument().toString());
     }
 
@@ -160,52 +156,48 @@ public class XWikiDocumentTest extends MockObjectTestCase
     {
         List<DocumentSection> sections;
         // Simple test
-        document.setContent("1 Section 1\n" + "Content of first section\n" + "1.1 Subsection 2\n"
+        this.document.setContent("1 Section 1\n" + "Content of first section\n" + "1.1 Subsection 2\n"
             + "Content of second section\n" + "1 Section 3\n" + "Content of section 3");
-        sections = document.getSplitSectionsAccordingToTitle();
+        sections = this.document.getSplitSectionsAccordingToTitle();
         assertEquals(3, sections.size());
         assertEquals("Section 1", sections.get(0).getSectionTitle());
         assertEquals("1 Section 1\n" + "Content of first section\n" + "1.1 Subsection 2\n"
-            + "Content of second section\n", document.getContentOfSection(1));
+            + "Content of second section\n", this.document.getContentOfSection(1));
         assertEquals("1.1", sections.get(1).getSectionLevel());
-        assertEquals("1.1 Subsection 2\nContent of second section\n",
-            document.getContentOfSection(2));
+        assertEquals("1.1 Subsection 2\nContent of second section\n", this.document.getContentOfSection(2));
         assertEquals(3, sections.get(2).getSectionNumber());
         assertEquals(80, sections.get(2).getSectionIndex());
-        assertEquals("1 Section 3\nContent of section 3", document.getContentOfSection(3));
+        assertEquals("1 Section 3\nContent of section 3", this.document.getContentOfSection(3));
         // Test comments don't break the section editing
-        document.setContent("1 Section 1\n" + "Content of first section\n"
-            + "## 1.1 Subsection 2\n" + "Content of second section\n" + "1 Section 3\n"
-            + "Content of section 3");
-        sections = document.getSplitSectionsAccordingToTitle();
+        this.document.setContent("1 Section 1\n" + "Content of first section\n" + "## 1.1 Subsection 2\n"
+            + "Content of second section\n" + "1 Section 3\n" + "Content of section 3");
+        sections = this.document.getSplitSectionsAccordingToTitle();
         assertEquals(2, sections.size());
         assertEquals("Section 1", sections.get(0).getSectionTitle());
         assertEquals("1", sections.get(1).getSectionLevel());
         assertEquals(2, sections.get(1).getSectionNumber());
         assertEquals(83, sections.get(1).getSectionIndex());
         // Test spaces are ignored
-        document.setContent("1 Section 1\n" + "Content of first section\n"
-            + "   1.1    Subsection 2  \n" + "Content of second section\n" + "1 Section 3\n"
-            + "Content of section 3");
-        sections = document.getSplitSectionsAccordingToTitle();
+        this.document.setContent("1 Section 1\n" + "Content of first section\n" + "   1.1    Subsection 2  \n"
+            + "Content of second section\n" + "1 Section 3\n" + "Content of section 3");
+        sections = this.document.getSplitSectionsAccordingToTitle();
         assertEquals(3, sections.size());
         assertEquals("Subsection 2  ", sections.get(1).getSectionTitle());
         assertEquals("1.1", sections.get(1).getSectionLevel());
         // Test lower headings are ignored
-        document.setContent("1 Section 1\n" + "Content of first section\n"
-            + "1.1.1 Lower subsection\n" + "This content is not important\n"
-            + "   1.1    Subsection 2  \n" + "Content of second section\n" + "1 Section 3\n"
-            + "Content of section 3");
-        sections = document.getSplitSectionsAccordingToTitle();
+        this.document.setContent("1 Section 1\n" + "Content of first section\n" + "1.1.1 Lower subsection\n"
+            + "This content is not important\n" + "   1.1    Subsection 2  \n" + "Content of second section\n"
+            + "1 Section 3\n" + "Content of section 3");
+        sections = this.document.getSplitSectionsAccordingToTitle();
         assertEquals(3, sections.size());
         assertEquals("Section 1", sections.get(0).getSectionTitle());
         assertEquals("Subsection 2  ", sections.get(1).getSectionTitle());
         assertEquals("1.1", sections.get(1).getSectionLevel());
         // Test blank lines are preserved
-        document.setContent("\n\n1 Section 1\n\n\n" + "Content of first section\n\n\n"
-            + "   1.1    Subsection 2  \n\n" + "Content of second section\n" + "1 Section 3\n"
-            + "Content of section 3");
-        sections = document.getSplitSectionsAccordingToTitle();
+        this.document
+            .setContent("\n\n1 Section 1\n\n\n" + "Content of first section\n\n\n" + "   1.1    Subsection 2  \n\n"
+                + "Content of second section\n" + "1 Section 3\n" + "Content of section 3");
+        sections = this.document.getSplitSectionsAccordingToTitle();
         assertEquals(3, sections.size());
         assertEquals(2, sections.get(0).getSectionIndex());
         assertEquals("Subsection 2  ", sections.get(1).getSectionTitle());
@@ -216,24 +208,21 @@ public class XWikiDocumentTest extends MockObjectTestCase
     {
         List<DocumentSection> sections;
         // Fill the document
-        document.setContent("1 Section 1\n" + "Content of first section\n" + "1.1 Subsection 2\n"
+        this.document.setContent("1 Section 1\n" + "Content of first section\n" + "1.1 Subsection 2\n"
             + "Content of second section\n" + "1 Section 3\n" + "Content of section 3");
-        String content =
-            document.updateDocumentSection(3, "1 Section 3\n" + "Modified content of section 3");
+        String content = this.document.updateDocumentSection(3, "1 Section 3\n" + "Modified content of section 3");
         assertEquals("1 Section 1\n" + "Content of first section\n" + "1.1 Subsection 2\n"
-            + "Content of second section\n" + "1 Section 3\n" + "Modified content of section 3",
-            content);
-        document.setContent(content);
-        sections = document.getSplitSectionsAccordingToTitle();
+            + "Content of second section\n" + "1 Section 3\n" + "Modified content of section 3", content);
+        this.document.setContent(content);
+        sections = this.document.getSplitSectionsAccordingToTitle();
         assertEquals(3, sections.size());
         assertEquals("Section 1", sections.get(0).getSectionTitle());
         assertEquals("1 Section 1\n" + "Content of first section\n" + "1.1 Subsection 2\n"
-            + "Content of second section\n", document.getContentOfSection(1));
+            + "Content of second section\n", this.document.getContentOfSection(1));
         assertEquals("1.1", sections.get(1).getSectionLevel());
-        assertEquals("1.1 Subsection 2\nContent of second section\n",
-            document.getContentOfSection(2));
+        assertEquals("1.1 Subsection 2\nContent of second section\n", this.document.getContentOfSection(2));
         assertEquals(3, sections.get(2).getSectionNumber());
         assertEquals(80, sections.get(2).getSectionIndex());
-        assertEquals("1 Section 3\nModified content of section 3", document.getContentOfSection(3));
+        assertEquals("1 Section 3\nModified content of section 3", this.document.getContentOfSection(3));
     }
 }

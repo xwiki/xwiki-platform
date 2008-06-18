5835ce5d4d4bdcbe39e7565755e66c696424fb26
vmassol <vincent@massol.net>
2008-06-18 07:24:49 +0000

    XWIKI-2481: Remove all unneeded plexus.xml and use shared-tests's plexus.xml instead
    * Use Shared tests AbstractXWikiComponentTestCase
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-platform/core/trunk@10386 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-rendering/xwiki-rendering-tests/src/test/java/org/xwiki/rendering/scaffolding/AbstractRenderingTestCase.java
+++ xwiki-rendering/xwiki-rendering-tests/src/test/java/org/xwiki/rendering/scaffolding/AbstractRenderingTestCase.java
@@ -19,64 +19,16 @@
  */
 package org.xwiki.rendering.scaffolding;
 
-import org.codehaus.plexus.DefaultContainerConfiguration;
-import org.codehaus.plexus.DefaultPlexusContainer;
-import org.codehaus.plexus.personality.plexus.lifecycle.phase.PlexusContainerLocator;
-import org.jmock.MockObjectTestCase;
-import org.xwiki.component.manager.ComponentManager;
-import org.xwiki.plexus.manager.PlexusComponentManager;
 import org.xwiki.rendering.block.Block;
 import org.xwiki.rendering.block.XDOM;
-import org.xwiki.context.ExecutionContextInitializerManager;
-import org.xwiki.context.Execution;
-import org.xwiki.context.ExecutionContext;
 
 import java.util.List;
 import java.io.StringWriter;
 
-public abstract class AbstractRenderingTestCase extends MockObjectTestCase
+import com.xpn.xwiki.test.AbstractXWikiComponentTestCase;
+
+public abstract class AbstractRenderingTestCase extends AbstractXWikiComponentTestCase
 {
-    private ComponentManager componentManager;
-
-    public AbstractRenderingTestCase()
-    {
-        super();
-    }
-
-    public AbstractRenderingTestCase(String name)
-    {
-        super(name);        
-    }
-
-    protected ComponentManager getComponentManager() throws Exception
-    {
-        if (this.componentManager == null) {
-            DefaultContainerConfiguration configuration = new DefaultContainerConfiguration();
-            configuration.setContainerConfiguration("/plexus.xml");
-            DefaultPlexusContainer container = new DefaultPlexusContainer(configuration);
-            PlexusContainerLocator locator = new PlexusContainerLocator(container);
-            this.componentManager = new PlexusComponentManager(locator);
-
-            // Initialize the Execution Context
-            ExecutionContextInitializerManager ecim =
-                (ExecutionContextInitializerManager) getComponentManager().lookup(
-                    ExecutionContextInitializerManager.ROLE);
-            Execution execution = (Execution) getComponentManager().lookup(Execution.ROLE);
-            ExecutionContext ec = new ExecutionContext();
-            ecim.initialize(ec);
-            execution.setContext(ec);
-        }
-        return this.componentManager;
-    }
-
-    protected void tearDown() throws Exception
-    {
-        if (this.componentManager != null) {
-            Execution execution = (Execution) getComponentManager().lookup(Execution.ROLE);
-            execution.removeContext();
-        }
-    }
-
     protected void assertBlocks(String expected, List<Block> blocks)
     {
         // Assert the result by parsing it through the TestEventsListener to generate easily

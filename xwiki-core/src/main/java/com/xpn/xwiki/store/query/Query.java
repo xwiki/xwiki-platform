575da2bb2548bc76c1e8d589ebdeaa6ed657ef80
amelentev <melenartem@ya.ru>
2008-07-15 09:24:35 +0000

    XWIKI-2444: QueryManager in Store
    introduce interfaces.
    
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-platform/core/trunk@11240 f329d543-caf0-0310-9063-dda96c69346f

---
+++ xwiki-core/src/main/java/com/xpn/xwiki/store/query/Query.java
@@ -0,0 +1,85 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package com.xpn.xwiki.store.query;
+
+import java.util.List;
+
+import com.xpn.xwiki.XWikiException;
+
+/**
+ * This is a Query interface, representing all queries in various languages for various stores.
+ * @see javax.jcr.query.Query
+ * @version $Id$
+ * @since 1.6M1   
+ */
+public interface Query
+{
+    /**
+     * This component's role, used when code needs to look it up.
+     */
+    String ROLE = Query.class.getName();
+
+    /**
+     * Indicator for Hibernate Query Language.
+     */
+    String HQL   = "hql";
+
+    /**
+     * Indicator for XPath language.
+     */
+    String XPATH = "xpath";
+
+    /**
+     * @return Query statement.
+     */
+    String getStatement();
+
+    /**
+     * @return Query language. See {@link Query#HQL} and others.
+     */
+    String getLanguage();
+
+    /**
+     * Bind named parameter var with value val in query statement.
+     * @param var variable in query statement (:var).
+     * @param val value of the variable.
+     * @return this query.
+     */
+    Query bindValue(String var, Object val);
+
+    /**
+     * @param limit limit of result list to set
+     * @return this query.
+     */
+    Query setLimit(int limit);
+
+    /**
+     * @param offset offset of query result to set.
+     * @return this query.
+     */
+    Query setOffset(int offset);
+    
+    /**
+     * @param <T> expected type of elements in result list.
+     * @return result of the query. If several fields selected then T=Object[].
+     * @throws XWikiException if something wrong.
+     */
+    <T> List<T> execute() throws XWikiException;
+}

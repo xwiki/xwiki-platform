81222585cf3bd917a8dfb9ab8476b8d3ce25f7c3
jvelo <jerome.velociter@gmail.com>
2008-08-11 10:38:43 +0000

    Renamed UpdateDocumentMojoTest to AbstractDocumentMojoTest to match the tested class
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/xwiki-tools/trunk@11747 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-platform-tools/xwiki-xmldoc-update-plugin/src/test/java/com/xpn/xwiki/tool/doc/AbstractDocumentMojoTest.java
+++ xwiki-platform-tools/xwiki-xmldoc-update-plugin/src/test/java/com/xpn/xwiki/tool/doc/AbstractDocumentMojoTest.java
@@ -32,7 +32,7 @@ import junit.framework.TestCase;
  * 
  * @version $Id: $
  */
-public class UpdateDocumentMojoTest extends TestCase
+public class AbstractDocumentMojoTest extends TestCase
 {
     /**
      * Test that a document loaded in memory from XML by the mojo then written back to XML does not lose any

5f037c9f81c4c2fbf300160d9f6f2f5a631f980a
vmassol <vincent@massol.net>
2008-08-26 09:13:54 +0000

    XWIKI-2568: Continue implementing the new Rendering Architecture
    * Added license header + javadoc + fixed some messages
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@12048 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-rendering/src/main/java/org/xwiki/rendering/macro/parameter/EnumConverter.java
+++ xwiki-rendering/src/main/java/org/xwiki/rendering/macro/parameter/EnumConverter.java
@@ -1,8 +1,33 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package org.xwiki.rendering.macro.parameter;
 
 import org.apache.commons.beanutils.ConversionException;
 import org.apache.commons.beanutils.Converter;
 
+/**
+ * Bean Utils converter that converts a value into an enumeration class value.
+ *
+ * @version $Id: $
+ * @since 1.6M1
+ */
 public class EnumConverter implements Converter
 {
     public Object convert(Class type, Object value)
@@ -20,9 +45,9 @@ public class EnumConverter implements Converter
 
     private String generateInvalidErrorMessage(Object[] enumValues, Object value)
     {
-        StringBuffer errorMessage = new StringBuffer("Can't find coversionn for value [" + value + "]");
+        StringBuffer errorMessage = new StringBuffer("Unable to convert value [" + value + "].");
 
-        errorMessage.append(" Alowed values are (case insensitive) ");
+        errorMessage.append(" Allowed values are (case insensitive) ");
 
         StringBuffer valueList = new StringBuffer();
 

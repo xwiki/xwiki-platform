f9fea757b1c52bf2fc3002847040bc47a9da67bd
vmassol <vincent@massol.net>
2008-08-26 15:40:04 +0000

    XWIKI-2568: Continue implementing the new Rendering Architecture
    * Added missing license headers + some javadoc
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@12060 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-xml/src/main/java/org/xwiki/xml/internal/html/TagSwapCleaningFilter.java
+++ xwiki-xml/src/main/java/org/xwiki/xml/internal/html/TagSwapCleaningFilter.java
@@ -1,3 +1,22 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package org.xwiki.xml.internal.html;
 
 import org.jdom.Document;
@@ -6,6 +25,12 @@ import org.jdom.filter.ElementFilter;
 
 import java.util.Iterator;
 
+/**
+ * Replace tags by others. For example replace tags that have been deprecated in XHTML with their equivalent.
+ * 
+ * @version $Id: $
+ * @since 1.6M1
+ */
 public class TagSwapCleaningFilter implements CleaningFilter
 {
     public void filter(Document document)

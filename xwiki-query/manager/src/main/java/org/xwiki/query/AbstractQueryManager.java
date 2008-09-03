b7b9b8593a88c221f43250229eb4c8cc60f84bde
amelentev <melenartem@ya.ru>
2008-09-03 18:48:53 +0000

    XWIKI-2637: Move QueryManager to xwiki-query/manager maven module copied from xwiki-core. package name changed to org.xwiki.query QueryException don't extend XWikiException.
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@12397 f329d543-caf0-0310-9063-dda96c69346f

---
+++ xwiki-query/manager/src/main/java/org/xwiki/query/AbstractQueryManager.java
@@ -0,0 +1,80 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.query;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * This is abstract QueryManager implementation.
+ * 
+ * @version $Id$
+ * @since 1.6M1
+ */
+public abstract class AbstractQueryManager implements QueryManager
+{
+    /**
+     * Field for supported languages.
+     */
+    protected Set<String> languages = new HashSet<String>();
+
+    /**
+     * {@inheritDoc}
+     */
+    public Set<String> getLanguages()
+    {
+        return Collections.unmodifiableSet(this.languages);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public boolean hasLanguage(String language)
+    {
+        return getLanguages().contains(language);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Query createQuery(String statement, String language) throws QueryException
+    {
+        if (hasLanguage(language)) {
+            return new DefaultQuery(statement, language, getExecutor(language));
+        } else {
+            throw new QueryException("Language [" + language + "] is not supported", null, null);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Query getNamedQuery(String queryName) throws QueryException
+    {
+        return new DefaultQuery(queryName, getExecutor(null));
+    }
+
+    /**
+     * @param language query language
+     * @return QueryExecutor for this language
+     */
+    protected abstract QueryExecutor getExecutor(String language);
+}
---
+++ xwiki-query/manager/src/main/java/org/xwiki/query/Query.java
@@ -0,0 +1,119 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.query;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * This is a Query interface, representing all queries in various languages for various stores.
+ * @see javax.jcr.query.Query
+ * @version $Id$
+ * @since 1.6M1   
+ */
+public interface Query
+{
+    /**
+     * Indicator for Hibernate Query Language.
+     */
+    String HQL   = "hql";
+
+    /**
+     * Indicator for XPath language.
+     */
+    String XPATH = "xpath";
+
+    /**
+     * Indicator for XWiki Query Language.
+     */
+    String XWQL = "xwql";
+
+    /**
+     * @return Query statement or query name depends on {@link #isNamed()}
+     */
+    String getStatement();
+
+    /**
+     * @return Query language. See {@link Query#HQL} and others.
+     */
+    String getLanguage();
+
+    /**
+     * if the query is named, then {@link #getStatement()} returns a name of the query, else - a query statement.
+     * @return is the query named.
+     */
+    boolean isNamed();
+
+    /**
+     * @param wiki virtual wiki to run the query. null is a current wiki.
+     */
+    void setWiki(String wiki);
+
+    /**
+     * @return virtual wiki to run the query. null is a current wiki.
+     * @see #setWiki(String)
+     */
+    String getWiki();
+
+    /**
+     * Bind named parameter var with value val in query statement.
+     * @param var variable in query statement (:var).
+     * @param val value of the variable.
+     * @return this query.
+     */
+    Query bindValue(String var, Object val);
+
+    /**
+     * @return map from parameter name to value.
+     * @see #bindValue(String, Object)
+     */
+    Map<String, Object> getParameters();
+
+    /**
+     * @param limit limit of result list to set (so {@link #execute()}.size() will be <= limit).
+     * @return this query.
+     */
+    Query setLimit(int limit);
+
+    /**
+     * @param offset offset of query result to set (skip first "offset" rows).
+     * @return this query.
+     */
+    Query setOffset(int offset);
+
+    /**
+     * @return limit  limit of result list.
+     * @see #setLimit(int)
+     */
+    int getLimit();
+
+    /**
+     * @return offset  offset of query result.
+     * @see #setOffset(int)
+     */
+    int getOffset();
+
+    /**
+     * @param <T> expected type of elements in the result list.
+     * @return result list of the query. If several fields are selected then T=Object[].
+     * @throws QueryException if something goes wrong.
+     */
+    <T> List<T> execute() throws QueryException;
+}

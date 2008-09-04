8a5e39e9989901eb0f9343e3a85d572bdce2cdd0
tmortagne <thomas.mortagne@xwiki.com>
2008-09-04 09:48:21 +0000

    Improve info log
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@12410 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-core/src/main/java/com/xpn/xwiki/plugin/ldap/XWikiLDAPUtils.java
+++ xwiki-core/src/main/java/com/xpn/xwiki/plugin/ldap/XWikiLDAPUtils.java
@@ -380,6 +380,10 @@ public class XWikiLDAPUtils
             LOG.error("Unknown error with cache", e);
         }
 
+        if (LOG.isInfoEnabled()) {
+            LOG.info("Found goup (" + groupDN + ") members :" + groupMembers);
+        }
+
         return groupMembers;
     }
 

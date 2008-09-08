5680394afc91bb6e5bd9c2cdc8635bd582ccdfe1
tmortagne <thomas.mortagne@xwiki.com>
2008-09-08 15:27:32 +0000

    XWIKI-2649: xwiki.authentication.ldap.mode_group_sync is not "always" by default
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/branches/xwiki-core-1.5@12507 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-core/src/main/java/com/xpn/xwiki/user/impl/LDAP/XWikiLDAPAuthServiceImpl.java
+++ xwiki-core/src/main/java/com/xpn/xwiki/user/impl/LDAP/XWikiLDAPAuthServiceImpl.java
@@ -516,7 +516,7 @@ public class XWikiLDAPAuthServiceImpl extends XWikiAuthServiceImpl
             // flag if always sync or just on create of the user
             String syncmode = config.getLDAPParam("ldap_mode_group_sync", "", context);
 
-            if ((syncmode.equalsIgnoreCase("create") && createuser) || syncmode.equalsIgnoreCase("always")) {
+            if (!syncmode.equalsIgnoreCase("create") || createuser) {
                 syncGroupsMembership(xwikiUserName, userDN, groupMappings, ldapUtils, context);
             }
 

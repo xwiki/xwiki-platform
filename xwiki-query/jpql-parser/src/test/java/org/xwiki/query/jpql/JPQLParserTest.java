37b54090e9a85f253f5c76cfe3aba13ed4435ece
amelentev <melenartem@ya.ru>
2008-09-22 00:59:18 +0000

    add missing license headers add "member of" jpql test
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@12839 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-query/jpql-parser/src/test/java/org/xwiki/query/jpql/JPQLParserTest.java
+++ xwiki-query/jpql-parser/src/test/java/org/xwiki/query/jpql/JPQLParserTest.java
@@ -1,3 +1,22 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package org.xwiki.query.jpql;
 
 import junit.framework.TestCase;
@@ -35,5 +54,10 @@ public class JPQLParserTest extends TestCase
         parser.parse("select doc from Document doc, doc.object(XWiki.XWikiGroups) as g order by g.number");
         parser.parse("select doc from Document doc, doc.object(XWiki.XWikiGroups) as g order by g.number desc");
         parser.parse("select doc from Document doc, doc.object(XWiki.XWikiGroups) as g order by g.number asc");
+    }
+
+    public void testMemberOf() throws Exception
+    {
+        parser.parse("select a from A as a where :param member of a.prop");
     }
 }

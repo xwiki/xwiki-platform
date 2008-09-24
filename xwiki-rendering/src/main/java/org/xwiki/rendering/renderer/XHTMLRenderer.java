d23bc4cce7c20aa37554e91ce2ed228aa9e37cf0
vmassol <vincent@massol.net>
2008-09-24 15:43:38 +0000

    Explained why we use <div></div> notation
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@12964 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-rendering/src/main/java/org/xwiki/rendering/renderer/XHTMLRenderer.java
+++ xwiki-rendering/src/main/java/org/xwiki/rendering/renderer/XHTMLRenderer.java
@@ -481,6 +481,8 @@ public class XHTMLRenderer extends AbstractPrintRenderer
     {
         // We need to use a special tag for empty lines since in XHTML the BR tag cannot be used outside of content
         // tags.
+    	// Note: We're using <div><div/> and not <div/> since some browsers do not support the <div/> syntax (FF3)
+    	// when the content type is set to HTML instead of XHTML.
         print(StringUtils.repeat("<div class=\"wikimodel-emptyline\"></div>", count));
     }
 

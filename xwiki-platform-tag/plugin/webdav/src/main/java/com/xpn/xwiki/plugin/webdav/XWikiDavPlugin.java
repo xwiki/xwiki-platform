b286f500fd0dc09257f223e4c4c0f58028a4f298
sdumitriu <sergiu@xwiki.com>
2008-10-16 15:28:20 +0000

    [cleanup] Apply codestyle, add license headers, fix dependencies
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/xwiki-plugins/trunk@13636 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-platform-tag/plugin/webdav/src/main/java/com/xpn/xwiki/plugin/webdav/XWikiDavPlugin.java
+++ xwiki-platform-tag/plugin/webdav/src/main/java/com/xpn/xwiki/plugin/webdav/XWikiDavPlugin.java
@@ -1,3 +1,23 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
 package com.xpn.xwiki.plugin.webdav;
 
 import com.xpn.xwiki.XWikiContext;
@@ -5,19 +25,21 @@ import com.xpn.xwiki.api.Api;
 import com.xpn.xwiki.plugin.XWikiDefaultPlugin;
 import com.xpn.xwiki.plugin.XWikiPluginInterface;
 
-public class XWikiDavPlugin extends XWikiDefaultPlugin {
+public class XWikiDavPlugin extends XWikiDefaultPlugin
+{
+    public XWikiDavPlugin(String name, String className, XWikiContext context)
+    {
+        super(name, className, context);
+        init(context);
+    }
 
-	public XWikiDavPlugin(String name, String className, XWikiContext context) {
-		super(name, className, context);
-		init(context);
-	}
+    public String getName()
+    {
+        return "webdav";
+    }
 
-	public String getName() {
-		return "webdav";
-	}
-
-	public Api getPluginApi(XWikiPluginInterface plugin, XWikiContext context) {
-		return new XWikiDavApi((XWikiDavPlugin) plugin, context);
-	}
-
+    public Api getPluginApi(XWikiPluginInterface plugin, XWikiContext context)
+    {
+        return new XWikiDavApi((XWikiDavPlugin) plugin, context);
+    }
 }

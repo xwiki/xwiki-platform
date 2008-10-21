9aee734883f9de5d1a2aae91021c3443b35b1395
sdumitriu <sergiu@xwiki.com>
2008-10-21 02:48:11 +0000

    XPXARPLUGIN-1: If a package.xml page is present the xar builder should only take the pages listed in the package definition file Done. Patch submitted by Dan Miron, applied with changes.
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/xwiki-tools/trunk@13729 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-platform-tools/xwiki-xar-plugin/src/main/java/com/xpn/xwiki/tool/xar/XarMojo.java
+++ xwiki-platform-tools/xwiki-xar-plugin/src/main/java/com/xpn/xwiki/tool/xar/XarMojo.java
@@ -19,31 +19,29 @@
  */
 package com.xpn.xwiki.tool.xar;
 
-import org.apache.maven.artifact.Artifact;
-import org.apache.maven.artifact.resolver.filter.ScopeArtifactFilter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.LinkedList;
+
 import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
 import org.codehaus.plexus.archiver.ArchiveEntry;
+import org.codehaus.plexus.archiver.ArchiverException;
 import org.codehaus.plexus.archiver.manager.ArchiverManager;
 import org.codehaus.plexus.archiver.zip.ZipArchiver;
-
 import org.dom4j.Document;
 import org.dom4j.Element;
 import org.dom4j.dom.DOMDocument;
 import org.dom4j.dom.DOMElement;
 import org.dom4j.io.OutputFormat;
-
-import java.io.File;
-import java.io.IOException;
-import java.io.FileWriter;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Set;
+import org.dom4j.io.SAXReader;
 import org.dom4j.io.XMLWriter;
 
 /**
- * Gather all resources in a XAR file (which is actually a ZIP file). Also generates a XAR
- * descriptor if none is provided.
+ * Gather all resources in a XAR file (which is actually a ZIP file). Also generates a XAR descriptor if none is
+ * provided.
  * <p>
  * Note that the generated descriptor currently doesn't handle translations.
  * </p>
@@ -68,7 +66,7 @@ public class XarMojo extends AbstractXarMojo
      * 
      * @see org.apache.maven.plugin.AbstractMojo#execute()
      */
-    public void execute() throws MojoExecutionException, MojoFailureException
+    public void execute() throws MojoExecutionException
     {
         if (this.project.getResources().size() < 1) {
             this.getLog().warn("No XAR created as no resources were found");
@@ -89,14 +87,14 @@ public class XarMojo extends AbstractXarMojo
      */
     private void performArchive() throws Exception
     {
-        File xarFile =
-            new File(this.project.getBuild().getDirectory(), this.project.getArtifactId()
-                + ".xar");
+        // The target file.
+        File xarFile = new File(this.project.getBuild().getDirectory(), this.project.getArtifactId() + ".xar");
 
         // The source dir points to the target/classes directory where the Maven resources plugin
-        // has copied the XAR files during the process-resources phase.
+        // has copied the XML files during the process-resources phase.
         File sourceDir = new File(this.project.getBuild().getOutputDirectory());
 
+        // The XAR is just a plain zip archive, with a special layout of the files, and a special package.xml file.
         ZipArchiver archiver = new ZipArchiver();
         archiver.setDestFile(xarFile);
         archiver.setIncludeEmptyDirs(false);
@@ -106,13 +104,14 @@ public class XarMojo extends AbstractXarMojo
         // existing files since we want this projet's files to be used if they override a file
         // present in a XAR dependency.
         unpackDependentXars();
-        archiver.addDirectory(sourceDir, getIncludes(), getExcludes());
 
         // If no package.xml can be found at the top level of the current project, generate one
-        if (archiver.getFiles().get(PACKAGE_XML) == null) {
-            File generatedPackageFile = new File(sourceDir, PACKAGE_XML);
-            generatePackageXml(generatedPackageFile, archiver.getFiles().values());
-            archiver.addFile(generatedPackageFile, PACKAGE_XML);
+        // otherwise, try to use the existing one
+        File packageXml = new File(sourceDir, PACKAGE_XML);
+        if (packageXml.exists()) {
+            addFilesToArchive(archiver, sourceDir, packageXml);
+        } else {
+            addFilesToArchive(archiver, sourceDir);
         }
 
         archiver.createArchive();
@@ -127,10 +126,9 @@ public class XarMojo extends AbstractXarMojo
      * @param files the files in the package.
      * @throws IOException error when writing the configuration file.
      */
-    private void generatePackageXml(File packageFile, Collection files) throws IOException
+    private void generatePackageXml(File packageFile, Collection<ArchiveEntry> files) throws IOException
     {
-        this.getLog()
-            .info("Generating package.xml descriptor at [" + packageFile.getPath() + "]");
+        getLog().info("Generating package.xml descriptor at [" + packageFile.getPath() + "]");
 
         OutputFormat outputFormat = new OutputFormat("", true);
         outputFormat.setEncoding("ISO-8859-1");
@@ -143,11 +141,11 @@ public class XarMojo extends AbstractXarMojo
 
     /**
      * Generate a DOM4J Document containing the generated XML.
-     *
+     * 
      * @param files the list of files that we want to include in the generated package XML file.
      * @return the DOM4J Document containing the generated XML
      */
-    private Document toXML(Collection files)
+    private Document toXML(Collection<ArchiveEntry> files)
     {
         Document doc = new DOMDocument();
 
@@ -158,7 +156,7 @@ public class XarMojo extends AbstractXarMojo
         packageElement.add(infoElement);
         addInfoElements(infoElement);
 
-        Element filesElement = new DOMElement("files");
+        Element filesElement = new DOMElement(FILES_TAG);
         packageElement.add(filesElement);
         addFileElements(files, filesElement);
 
@@ -166,9 +164,9 @@ public class XarMojo extends AbstractXarMojo
     }
 
     /**
-     * Add all the XML elements under the &lt;info&gt; element (name, description, license,
-     * author, version and whether it's a backup pack or not).
-     *
+     * Add all the XML elements under the &lt;info&gt; element (name, description, license, author, version and whether
+     * it's a backup pack or not).
+     * 
      * @param infoElement the info element to which to add to
      */
     private void addInfoElements(Element infoElement)
@@ -199,23 +197,20 @@ public class XarMojo extends AbstractXarMojo
     }
 
     /**
-     * Add all the XML elements under the &lt;files&gt; element (the list of files present in the
-     * XAR).
-     *
+     * Add all the XML elements under the &lt;files&gt; element (the list of files present in the XAR).
+     * 
      * @param files the list of files that we want to include in the generated package XML file.
      * @param filesElement the files element to which to add to
      */
-    private void addFileElements(Collection files, Element filesElement)
+    private void addFileElements(Collection<ArchiveEntry> files, Element filesElement)
     {
-        for (Iterator it = files.iterator(); it.hasNext();) {
-            ArchiveEntry entry = (ArchiveEntry) it.next();
-
+        for (ArchiveEntry entry : files) {
             // Don't add files in META-INF to the package.xml file
             if (entry.getFile().getPath().indexOf("META-INF") == -1) {
-                XWikiDocument xdoc  = getDocFromXML(entry.getFile());
+                XWikiDocument xdoc = getDocFromXML(entry.getFile());
                 if (xdoc != null) {
                     String fullName = xdoc.getFullName();
-                    Element element = new DOMElement("file");
+                    Element element = new DOMElement(FILE_TAG);
                     element.setText(fullName);
                     element.addAttribute("language", xdoc.getLanguage());
                     element.addAttribute("defaultAction", "0");
@@ -239,48 +234,92 @@ public class XarMojo extends AbstractXarMojo
             doc = new XWikiDocument();
             doc.fromXML(file);
         } catch (Exception e) {
-            this.getLog().warn("Failed to parse [" + file.getAbsolutePath() + "], skipping it. "
-                + "The error was [" + e.getMessage() + "]", e);
+            this.getLog().warn(
+                "Failed to parse [" + file.getAbsolutePath() + "], skipping it. " + "The error was [" + e.getMessage()
+                    + "]", e);
         }
 
         return doc;
     }
 
     /**
-     * Unpack xar dependencies before pack then into it.
+     * Gets the list of document names from a 'package.xml' document.
      * 
-     * @throws MojoExecutionException error when unpack dependencies.
+     * @param xmlPath the path to the XML document to parse
+     * @return the list of document names contained in the XML document
+     * @throws Exception if the XML document is invalid or it contains no document list or it doesn't exist
      */
-    private void unpackDependentXars() throws MojoExecutionException
+    private Collection<String> getDocumentNamesFromXML(String xmlPath) throws Exception
     {
-        Set artifacts = this.project.getArtifacts();
-        for (Iterator iter = artifacts.iterator(); iter.hasNext();) {
-            Artifact artifact = (Artifact) iter.next();
-            ScopeArtifactFilter filter = new ScopeArtifactFilter(Artifact.SCOPE_RUNTIME);
-            if (!artifact.isOptional() && filter.include(artifact)) {
-                String type = artifact.getType();
-                if ("xar".equals(type)) {
-                    unpackXarToOutputDirectory(artifact);
-                }
+        Collection<String> result = new LinkedList<String>();
+        SAXReader reader = new SAXReader();
+        Document domdoc;
+        domdoc = reader.read(new FileReader(xmlPath));
+
+        Element filesElement = domdoc.getRootElement().element(FILES_TAG);
+
+        if (filesElement == null) {
+            throw new Exception("The supplied document contains no document list ");
+        }
+        Collection elements = filesElement.elements(FILE_TAG);
+        for (Object item : elements) {
+            if (item instanceof Element) {
+                Element currentElement = (Element) item;
+                String documentName = currentElement.getText();
+                result.add(documentName);
             }
         }
+        return result;
     }
 
     /**
-     * Unpacks A XAR artifacts into the build output directory, along with the project's XAR files.
+     * Adds the files from a specific directory to an archive. It also builds a package.xml file based on that content
+     * which is also added to the archive.
      * 
-     * @param artifact the XAR artifact to unpack.
-     * @throws MojoExecutionException in case of unpack error
+     * @param archiver the archive in which the files will be added
+     * @param sourceDir the directory whose contents will be added to the archive
+     * @throws Exception if the files cannot be added to the archive
      */
-    private void unpackXarToOutputDirectory(Artifact artifact) throws MojoExecutionException
+    private void addFilesToArchive(ZipArchiver archiver, File sourceDir) throws Exception
     {
-        File outputLocation = new File(this.project.getBuild().getOutputDirectory());
+        archiver.addDirectory(sourceDir, getIncludes(), getExcludes());
+        File generatedPackageFile = new File(sourceDir, PACKAGE_XML);
+        generatePackageXml(generatedPackageFile, archiver.getFiles().values());
+        archiver.addFile(generatedPackageFile, PACKAGE_XML);
+    }
 
-        if (!outputLocation.exists()) {
-            outputLocation.mkdirs();
+    /**
+     * Adds files from a specific directory to an archive. It uses an existing package.xml to filter the files to be
+     * added.
+     * 
+     * @param archiver the archive in which the files will be added
+     * @param sourceDir the directory whose contents will be added to the archive
+     * @param packageXml the corresponding package.xml file
+     * @throws Exception if the files cannot be added to the archive
+     */
+    private void addFilesToArchive(ZipArchiver archiver, File sourceDir, File packageXml) throws Exception
+    {
+        Collection<String> documentNames = null;
+        this.getLog().info("Using the existing package.xml descriptor at [" + packageXml.getPath() + "]");
+        try {
+            documentNames = getDocumentNamesFromXML(sourceDir.getAbsolutePath() + File.separator + PACKAGE_XML);
+        } catch (Exception e) {
+            // When the provided package.xml is invalid, abort the build.
+            this.getLog().error("The existing " + PACKAGE_XML + " is invalid.");
+            throw e;
         }
-
-        File file = artifact.getFile();
-        unpack(file, outputLocation, "XarMojo", false);
+        for (String currentItem : documentNames) {
+            try {
+                String currentFileName = currentItem.replace('.', File.separatorChar) + ".xml";
+                File generatedFile = new File(sourceDir, currentFileName);
+                // This will further throw an exception if a file listed in package.xml does not exist.
+                getLog().debug("Adding file [" + currentFileName + "]...");
+                archiver.addFile(generatedFile, currentFileName);
+            } catch (ArchiverException ex) {
+                getLog().error("The file [" + currentItem + "] was listed in " + PACKAGE_XML + " but was not found.");
+                throw ex;
+            }
+        }
+        archiver.addFile(packageXml, PACKAGE_XML);
     }
 }

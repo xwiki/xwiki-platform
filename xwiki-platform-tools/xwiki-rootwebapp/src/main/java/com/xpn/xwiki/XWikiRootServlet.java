0353e729f0abce6aa6c6a9181f0a914e043aef7a
sdumitriu <sergiu@xwiki.com>
2008-10-21 01:43:55 +0000

    XWIKI-2763: Complete migration of webdav related projects into trunk Cleanup the root webapp module: - make the default redirect path configurable - add javadoc - remove unneeded welcome files config from web.xml
    Patch submitted by Asiri Rathnayake, polished and extended.
    
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/xwiki-tools/trunk@13727 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-platform-tools/xwiki-rootwebapp/src/main/java/com/xpn/xwiki/XWikiRootServlet.java
+++ xwiki-platform-tools/xwiki-rootwebapp/src/main/java/com/xpn/xwiki/XWikiRootServlet.java
@@ -7,26 +7,68 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * The root servlet for XWiki. The purpose of this servlet is to respond to WebDAV requests correctly and to redirect
+ * get requests on server root appropriately.
+ * 
+ * @version $Id$
+ */
 public class XWikiRootServlet extends HttpServlet
 {
-    private static final long serialVersionUID = -4907199005755391420L;    
+    /** Class version identifier. Needed for serialization. */
+    private static final long serialVersionUID = -4907199005755391420L;
 
-    protected void doOptions(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException
-    {        
+    /**
+     * The address to redirect to (the name of the XWiki webapp). Configured using the <code>redirectTo</code>
+     * initialization parameter in <code>web.xml</code>.
+     */
+    private String xwiki = "xwiki";
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see javax.servlet.GenericServlet#init()
+     */
+    @Override
+    public void init() throws ServletException
+    {
+        super.init();
+        String redirectParameter = getInitParameter("redirectTo");
+        if (redirectParameter != null) {
+            this.xwiki = redirectParameter;
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Responds to an OPTIONS requests on / with appropriate headers.
+     * </p>
+     */
+    @Override
+    protected void doOptions(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+        IOException
+    {
         response.setStatus(HttpServletResponse.SC_OK);
         response.setHeader("MS-Author-Via", "DAV");
         response.setHeader("Content-Language", "en");
         response.setHeader("DAV", "1,2");
         response.setHeader("Allow", "OPTIONS, GET, HEAD, PROPFIND, LOCK, UNLOCK");
         response.setHeader("Content-Length", "0");
-        response.flushBuffer();                
+        response.flushBuffer();
     }
 
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-        throws ServletException, IOException
-    {        
-        resp.sendRedirect("/xwiki/");
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Redirects GET requests on <code>/</code> (the server root) to the XWiki application context. That is usually
+     * <code>/xwiki/</code>, but can be configured from <code>web.xml</code> using the <code>redirectTo</code>
+     * initialization parameter for this servlet.
+     * </p>
+     */
+    @Override
+    protected void doGet(HttpServletRequest reqest, HttpServletResponse response) throws ServletException, IOException
+    {
+        response.sendRedirect(response.encodeRedirectURL(this.xwiki));
     }
-        
 }

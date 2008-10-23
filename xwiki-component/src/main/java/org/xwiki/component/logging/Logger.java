314b1a2aa3d0e9635292aa79682ab634f5f639f9
sdumitriu <sergiu@xwiki.com>
2008-10-23 22:56:37 +0000

    XWIKI-2667: Defer logging message build to Logger Done. Patch submitted by Jonas von Malottki, applied with enhancements.
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@13806 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-component/src/main/java/org/xwiki/component/logging/Logger.java
+++ xwiki-component/src/main/java/org/xwiki/component/logging/Logger.java
@@ -20,6 +20,13 @@
  */
 package org.xwiki.component.logging;
 
+/**
+ * Logger component, used for logging messages. Classes that want to log should <em>not</em> declare a dependency on
+ * this component, but implement the {@link org.xwiki.component.phase.LogEnabled} interface. A quick way to enable
+ * logging is to extend {@link AbstractLogEnabled}.
+ * 
+ * @version $Id$
+ */
 public interface Logger
 {
     /** Typecode for debugging messages. */
@@ -37,35 +44,302 @@ public interface Logger
     /** Typecode for disabled log levels. */
     int LEVEL_DISABLED = 5;
 
+    /**
+     * Log a message with the <code>debug</code> priority.
+     * 
+     * @param message The message to be logged. No further processing of the message is performed.
+     */
     void debug(String message);
 
+    /**
+     * Log a message with the <code>debug</code> priority, also printing the exception that caused this message, along
+     * with its stack trace.
+     * 
+     * @param message The message to be logged. No further processing of the message is performed.
+     * @param throwable The exception associated with the message, for stack trace output.
+     */
     void debug(String message, Throwable throwable);
 
+    /**
+     * <p>
+     * Logs the {@code message} to the logger with <code>debug</code> priority while expanding the message with the
+     * objects passed. The message uses the {@link java.text.MessageFormat} syntax.
+     * </p>
+     * <p>
+     * Example:<br/> <code>
+     * log.debug("Doc was created on {0} and was changed {1} times", date, 5);
+     * log.debug("There {0,choice,0#are|1#is|1<are} {0,choice,0#no|1#one|1<{0}} file{0,choice,0#s|1#|1<s}", nrFiles);
+     * </code>
+     * </p>
+     * <p>
+     * The logged message will only be formatted if the message is actually logged.
+     * </p>
+     * 
+     * @param message The message to be formatted via {@link java.text.MessageFormat#format()}
+     * @param objects The objects to be filled into the message
+     * @see java.text.MessageFormat
+     */
+    void debug(String message, Object... objects);
+
+    /**
+     * <p>
+     * Logs the {@code message} to the logger with <code>debug</code> priority while expanding the message with the
+     * objects passed, also printing the exception that caused this message, along with its stack trace. The message
+     * uses the {@link java.text.MessageFormat} syntax.
+     * </p>
+     * <p>
+     * Example:<br/> <code>
+     * log.debug("Unhandled exception while executing the query [{0}]", exception, query);
+     * </code>
+     * </p>
+     * <p>
+     * The logged message will only be assembled if the message is actually logged.
+     * </p>
+     * 
+     * @param message The message to be formatted via {@link java.text.MessageFormat#format()}
+     * @param throwable The exception associated with the message, for stack trace output.
+     * @param objects The objects to be filled into the message
+     * @see java.text.MessageFormat
+     */
+    void debug(String message, Throwable throwable, Object... objects);
+
+    /**
+     * Check if the <code>debug</code> priority is enabled.
+     * 
+     * @return <code>true</code> if messages with <code>debug</code> priority will be logged, <code>false</code>
+     *         otherwise
+     */
     boolean isDebugEnabled();
 
+    /**
+     * Log a message with the <code>info</code> priority.
+     * 
+     * @param message The message to be logged. No further processing of the message is performed.
+     */
     void info(String message);
 
+    /**
+     * Log a message with the <code>info</code> priority, also printing the exception that caused this message, along
+     * with its stack trace.
+     * 
+     * @param message The message to be logged. No further processing of the message is performed.
+     * @param throwable The exception associated with the message, for stack trace output.
+     */
     void info(String message, Throwable throwable);
 
+    /**
+     * <p>
+     * Logs the {@code message} to the logger with <code>info</code> priority while expanding the message with the
+     * objects passed. The message uses the {@link java.text.MessageFormat} syntax. <br>
+     * </p>
+     * <p>
+     * Example:<br/> <code>
+     * log.info("Doc was created on {0} and was changed {1} times", date, 5);
+     * log.info("There {0,choice,0#are|1#is|1<are} {0,choice,0#no|1#one|1<{0}} file{0,choice,0#s|1#|1<s}", nrFiles);
+     * </code>
+     * </p>
+     * <p>
+     * The logged message will only be assembled if the message is actually logged.
+     * </p>
+     * 
+     * @param message The message to be formatted via {@link java.text.MessageFormat#format()}
+     * @param objects The objects to be filled into the message
+     * @see java.text.MessageFormat
+     */
+    void info(String message, Object... objects);
+
+    /**
+     * <p>
+     * Logs the {@code message} to the logger with <code>info</code> priority while expanding the message with the
+     * objects passed, also printing the exception that caused this message, along with its stack trace. The message
+     * uses the {@link java.text.MessageFormat} syntax.
+     * </p>
+     * <p>
+     * Example:<br/> <code>
+     * log.info("Unhandled exception while executing the query [{0}]", exception, query);
+     * </code>
+     * </p>
+     * <p>
+     * The logged message will only be assembled if the message is actually logged.
+     * </p>
+     * 
+     * @param message The message to be formatted via {@link java.text.MessageFormat#format()}
+     * @param throwable The exception associated with the message, for stack trace output.
+     * @param objects The objects to be filled into the message
+     * @see java.text.MessageFormat
+     */
+    void info(String message, Throwable throwable, Object... objects);
+
+    /**
+     * Check if the <code>info</code> priority is enabled.
+     * 
+     * @return <code>true</code> if messages with <code>info</code> priority will be logged, <code>false</code>
+     *         otherwise
+     */
     boolean isInfoEnabled();
 
+    /**
+     * Log a message with the <code>warn</code> priority.
+     * 
+     * @param message The message to be logged. No further processing of the message is performed.
+     */
     void warn(String message);
 
+    /**
+     * Log a message with the <code>warn</code> priority, also printing the exception that caused this message, along
+     * with its stack trace.
+     * 
+     * @param message The message to be logged. No further processing of the message is performed.
+     * @param throwable The exception associated with the message, for stack trace output.
+     */
     void warn(String message, Throwable throwable);
 
+    /**
+     * <p>
+     * Logs the {@code message} to the logger with <code>warn</code> priority while expanding the message with the
+     * objects passed. The message uses the {@link java.text.MessageFormat} syntax.
+     * </p>
+     * <p>
+     * Example:<br/> <code>
+     * log.warn("Doc was created on {0} and was changed {1} times", date, 5);
+     * log.warn("There {0,choice,0#are|1#is|1<are} {0,choice,0#no|1#one|1<{0}} file{0,choice,0#s|1#|1<s}", nrFiles);
+     * </code>
+     * </p>
+     * <p>
+     * The logged message will only be assembled if the message is actually logged.
+     * </p>
+     * 
+     * @param message The message to be formatted via {@link java.text.MessageFormat#format()}
+     * @param objects The objects to be filled into the message
+     * @see java.text.MessageFormat
+     */
+    void warn(String message, Object... objects);
+
+    /**
+     * <p>
+     * Logs the {@code message} to the logger with <code>warn</code> priority while expanding the message with the
+     * objects passed, also printing the exception that caused this message, along with its stack trace. The message
+     * uses the {@link java.text.MessageFormat} syntax.
+     * </p>
+     * <p>
+     * Example:<br/> <code>
+     * log.warn("Unhandled exception while executing the query [{0}]", exception, query);
+     * </code>
+     * </p>
+     * <p>
+     * The logged message will only be assembled if the message is actually logged.
+     * </p>
+     * 
+     * @param message The message to be formatted via {@link java.text.MessageFormat#format()}
+     * @param throwable The exception associated with the message, for stack trace output.
+     * @param objects The objects to be filled into the message
+     * @see java.text.MessageFormat
+     */
+    void warn(String message, Throwable throwable, Object... objects);
+
+    /**
+     * Check if the <code>warn</code> priority is enabled.
+     * 
+     * @return <code>true</code> if messages with <code>warn</code> priority will be logged, <code>false</code>
+     *         otherwise
+     */
     boolean isWarnEnabled();
 
+    /**
+     * Log a message with the <code>error</code> priority.
+     * 
+     * @param message The message to be logged. No further processing of the message is performed.
+     */
     void error(String message);
 
+    /**
+     * Log a message with the <code>error</code> priority, also printing the exception that caused this message, along
+     * with its stack trace.
+     * 
+     * @param message The message to be logged. No further processing of the message is performed.
+     * @param throwable The exception associated with the message, for stack trace output.
+     */
     void error(String message, Throwable throwable);
 
+    /**
+     * <p>
+     * Logs the {@code message} to the logger with <code>error</code> priority while expanding the message with the
+     * objects passed. The message uses the {@link java.text.MessageFormat} syntax.
+     * </p>
+     * <p>
+     * Example:<br/> <code>
+     * log.error("Doc was created on {0} and was changed {1} times", date, 5);
+     * log.error("There {0,choice,0#are|1#is|1<are} {0,choice,0#no|1#one|1<{0}} file{0,choice,0#s|1#|1<s}", nrFiles);
+     * </code>
+     * </p>
+     * <p>
+     * The logged message will only be assembled if the message is actually logged.
+     * </p>
+     * 
+     * @param message The message to be formatted via {@link java.text.MessageFormat#format()}
+     * @param objects The objects to be filled into the message
+     * @see java.text.MessageFormat
+     */
+    void error(String message, Object... objects);
+
+    /**
+     * <p>
+     * Logs the {@code message} to the logger with <code>error</code> priority while expanding the message with the
+     * objects passed, also printing the exception that caused this message, along with its stack trace. The message
+     * uses the {@link java.text.MessageFormat} syntax.
+     * </p>
+     * <p>
+     * Example:<br/> <code>
+     * log.error("Unhandled exception while executing the query [{0}]", exception, query);
+     * </code>
+     * </p>
+     * <p>
+     * The logged message will only be assembled if the message is actually logged.
+     * </p>
+     * 
+     * @param message The message to be formatted via {@link java.text.MessageFormat#format()}
+     * @param throwable The exception associated with the message, for stack trace output.
+     * @param objects The objects to be filled into the message
+     * @see java.text.MessageFormat
+     */
+    void error(String message, Throwable throwable, Object... objects);
+
+    /**
+     * Check if the <code>error</code> priority is enabled.
+     * 
+     * @return <code>true</code> if messages with <code>error</code> priority will be logged, <code>false</code>
+     *         otherwise
+     */
     boolean isErrorEnabled();
 
+    /**
+     * Create and return a child logger, which will inherit the parent's settings.
+     * 
+     * @param name The name of the new logger.
+     * @return A child {@link Logger} object.
+     */
     Logger getChildLogger(String name);
 
+    /**
+     * Returns the current priority threshold. Messages with the priority lower than the threshold will not be logged.
+     * 
+     * @return The value of the current threshold, one of {@link #LEVEL_DEBUG}, {@link #LEVEL_INFO}, {@link #LEVEL_WARN}
+     *         , {@link #LEVEL_ERROR}, {@link #LEVEL_FATAL}, {@link #LEVEL_DISABLED}.
+     */
     int getThreshold();
 
+    /**
+     * Sets the current priority threshold. Messages with the priority lower than the threshold will not be logged.
+     * 
+     * @param threshold The new priority threshold, one of {@link #LEVEL_DEBUG}, {@link #LEVEL_INFO},
+     *            {@link #LEVEL_WARN} , {@link #LEVEL_ERROR}, {@link #LEVEL_FATAL}, {@link #LEVEL_DISABLED}.
+     */
     void setThreshold(int threshold);
 
+    /**
+     * The name of the logger.
+     * 
+     * @return
+     */
     String getName();
 }
--- xwiki-plexus/src/main/java/org/xwiki/plexus/logging/PlexusLogger.java
+++ xwiki-plexus/src/main/java/org/xwiki/plexus/logging/PlexusLogger.java
@@ -20,92 +20,317 @@
  */
 package org.xwiki.plexus.logging;
 
+import java.text.MessageFormat;
+
 import org.xwiki.component.logging.Logger;
 
+/**
+ * Logger implementation that should be used when the component manager used is Plexus. That this is a wrapper over the
+ * Plexus logging facade, further configuration of the logging system should be done
+ * 
+ * @see Logger
+ * @version $Id$
+ */
 public class PlexusLogger implements Logger
 {
+    /** Wrapped Plexus logger object. This communicates with the underlying logging framework. */
     private org.codehaus.plexus.logging.Logger logger;
 
+    /**
+     * Formats the message like {@code MessageFormat.format(String, Object...)} but also checks for Exceptions and
+     * catches them as logging should be robust and not interfere with normal program flow. The Exception caught will be
+     * passed to the loggers debug output.
+     * 
+     * @param message message in Formatter format syntax
+     * @param objects Objects to fill in
+     * @return the formatted String if possible, else the message and all objects concatenated.
+     * @see MessageFormat
+     */
+    private String formatMessage(String message, Object... objects)
+    {
+        try {
+            return MessageFormat.format(message, objects);
+        } catch (IllegalArgumentException e) {
+            this.logger.debug("Caught exception while formatting logging message: " + message, e);
+
+            // Try to save the message for logging output and just append the passed objects instead
+            if (objects != null) {
+                StringBuffer sb = new StringBuffer(message);
+                for (Object object : objects) {
+                    if (object != null) {
+                        sb.append(object);
+                    } else {
+                        sb.append("(null)");
+                    }
+                    sb.append(" ");
+                }
+                return sb.toString();
+            }
+            return message;
+        }
+    }
+
+    /**
+     * Default constructor, wrapping the actual logger created by Plexus.
+     * 
+     * @param logger The internal Plexus logger.
+     */
     public PlexusLogger(org.codehaus.plexus.logging.Logger logger)
     {
         this.logger = logger;
     }
 
+    /**
+     * {@inheritDoc}
+     * 
+     * @see Logger#debug(String)
+     */
     public void debug(String message)
     {
         this.logger.debug(message);
     }
 
+    /**
+     * {@inheritDoc}
+     * 
+     * @see Logger#debug(String, Throwable)
+     */
     public void debug(String message, Throwable throwable)
     {
         this.logger.debug(message, throwable);
     }
 
+    /**
+     * {@inheritDoc}
+     * 
+     * @see Logger#debug(String, Object...)
+     */
+    public void debug(String message, Object... objects)
+    {
+        if (this.logger.isDebugEnabled()) {
+            this.logger.debug(formatMessage(message, objects));
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see Logger#debug(String, Throwable, Object...)
+     */
+    public void debug(String message, Throwable throwable, Object... objects)
+    {
+        if (this.logger.isDebugEnabled()) {
+            this.logger.debug(formatMessage(message, objects), throwable);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see Logger#isDebugEnabled()
+     */
     public boolean isDebugEnabled()
     {
         return this.logger.isDebugEnabled();
     }
 
+    /**
+     * {@inheritDoc}
+     * 
+     * @see Logger#info(String)
+     */
     public void info(String message)
     {
         this.logger.info(message);
     }
 
+    /**
+     * {@inheritDoc}
+     * 
+     * @see Logger#info(String, Throwable)
+     */
     public void info(String message, Throwable throwable)
     {
         this.logger.info(message, throwable);
     }
 
+    /**
+     * {@inheritDoc}
+     * 
+     * @see Logger#info(String, Object...)
+     */
+    public void info(String message, Object... objects)
+    {
+        if (this.logger.isInfoEnabled()) {
+            this.logger.info(formatMessage(message, objects));
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see Logger#info(String, Throwable, Object...)
+     */
+    public void info(String message, Throwable throwable, Object... objects)
+    {
+        if (this.logger.isInfoEnabled()) {
+            this.logger.info(formatMessage(message, objects), throwable);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see Logger#isInfoEnabled()
+     */
     public boolean isInfoEnabled()
     {
         return this.logger.isInfoEnabled();
     }
 
+    /**
+     * {@inheritDoc}
+     * 
+     * @see Logger#warn(String)
+     */
     public void warn(String message)
     {
         this.logger.warn(message);
     }
 
+    /**
+     * {@inheritDoc}
+     * 
+     * @see Logger#warn(String, Throwable)
+     */
     public void warn(String message, Throwable throwable)
     {
         this.logger.warn(message, throwable);
     }
 
+    /**
+     * {@inheritDoc}
+     * 
+     * @see Logger#warn(String, Object...)
+     */
+    public void warn(String message, Object... objects)
+    {
+        if (this.logger.isWarnEnabled()) {
+            this.logger.warn(formatMessage(message, objects));
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see Logger#warn(String, Throwable, Object...)
+     */
+    public void warn(String message, Throwable throwable, Object... objects)
+    {
+        if (this.logger.isWarnEnabled()) {
+            this.logger.warn(formatMessage(message, objects), throwable);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see Logger#isWarnEnabled()
+     */
     public boolean isWarnEnabled()
     {
         return this.logger.isWarnEnabled();
     }
 
+    /**
+     * {@inheritDoc}
+     * 
+     * @see Logger#error(String)
+     */
     public void error(String message)
     {
         this.logger.error(message);
     }
 
+    /**
+     * {@inheritDoc}
+     * 
+     * @see Logger#error(String, Throwable)
+     */
     public void error(String message, Throwable throwable)
     {
         this.logger.error(message, throwable);
     }
 
+    /**
+     * {@inheritDoc}
+     * 
+     * @see Logger#error(String, Object...)
+     */
+    public void error(String message, Object... objects)
+    {
+        if (this.logger.isErrorEnabled()) {
+            this.logger.error(formatMessage(message, objects));
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see Logger#error(String, Throwable, Object...)
+     */
+    public void error(String message, Throwable throwable, Object... objects)
+    {
+        if (this.logger.isErrorEnabled()) {
+            this.logger.error(formatMessage(message, objects), throwable);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see Logger#isErrorEnabled()
+     */
     public boolean isErrorEnabled()
     {
         return this.logger.isErrorEnabled();
     }
 
+    /**
+     * {@inheritDoc}
+     * 
+     * @see Logger#getChildLogger(String)
+     */
     public Logger getChildLogger(String name)
     {
         return new PlexusLogger(this.logger.getChildLogger(name));
     }
 
+    /**
+     * {@inheritDoc}
+     * 
+     * @see Logger#getThreshold()
+     */
     public int getThreshold()
     {
         return this.logger.getThreshold();
     }
 
+    /**
+     * {@inheritDoc}
+     * 
+     * @see Logger#setThreshold(int)
+     */
     public void setThreshold(int threshold)
     {
         this.logger.setThreshold(threshold);
     }
 
+    /**
+     * {@inheritDoc}
+     * 
+     * @see Logger#getName()
+     */
     public String getName()
     {
         return this.logger.getName();

70c7abbeb5ecb20e0bd0b4e273b949be79453916
tmortagne <thomas.mortagne@xwiki.com>
2008-11-13 15:37:22 +0000

    XWIKI-2830: Move velocity macro in xwiki-macros * update rendering test that was using velocity
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@14196 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-rendering/src/test/java/org/xwiki/rendering/RenderingTests.java
+++ xwiki-rendering/src/test/java/org/xwiki/rendering/RenderingTests.java
@@ -88,6 +88,7 @@ public class RenderingTests extends TestCase
         suite.addTestsFromResource("macros/macro3", false);
         suite.addTestsFromResource("macros/macro4", false);
         suite.addTestsFromResource("macros/macro5", false);
+        suite.addTestsFromResource("macros/macro6", true);
         suite.addTestsFromResource("macros/macrohtml", true);
         suite.addTestsFromResource("macros/xhtml/macroxhtml1", true);
         suite.addTestsFromResource("macros/xhtml/macroxhtml2", true);

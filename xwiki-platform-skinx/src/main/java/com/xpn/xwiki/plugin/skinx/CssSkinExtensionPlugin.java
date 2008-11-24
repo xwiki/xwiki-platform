b73819652aa0522bdb29cda1018312a6ba3e178c
jvelo <jerome.velociter@gmail.com>
2008-11-24 09:48:43 +0000

    * XSKINX-8 Implement always used extensions
    This patch makes "use always" extensions available for JSX/SSX. 
    * An always used extension needs to have its page being saved with programming rights to be injected on each page (this for example to prevent site CSS defacing on a whole web site/wiki)
    * Always used extensions are cached for each wiki, in order not to query the database for each request. The cache is updated according to document save notifications that concerns SX objects.
    * When importing a XAR, the cache for the concerned wiki is flushed, in order for potential extensions to be available right away. See http://jira.xwiki.org/jira/browse/XWIKI-2868.
    * Always used extensions are available for "view" and "admin" actions only. In the future, extensions should be able to declare the actions they apply to themselves. However, this is a different issue, see http://jira.xwiki.org/jira/browse/XSKINX-14
    
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/xwiki-plugins/trunk/skinx@14413 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-platform-skinx/src/main/java/com/xpn/xwiki/plugin/skinx/CssSkinExtensionPlugin.java
+++ xwiki-platform-skinx/src/main/java/com/xpn/xwiki/plugin/skinx/CssSkinExtensionPlugin.java
@@ -1,15 +1,12 @@
 package com.xpn.xwiki.plugin.skinx;
 
-import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import com.xpn.xwiki.XWikiContext;
 import com.xpn.xwiki.XWikiException;
-import com.xpn.xwiki.doc.XWikiDocument;
-import com.xpn.xwiki.objects.classes.BaseClass;
 
-public class CssSkinExtensionPlugin extends SkinExtensionPlugin
+public class CssSkinExtensionPlugin extends AbstractDocumentSkinExtensionPlugin
 {
     /** Log object to log messages in this class. */
     private static final Log LOG = LogFactory.getLog(CssSkinExtensionPlugin.class);
@@ -34,26 +31,6 @@ public class CssSkinExtensionPlugin extends SkinExtensionPlugin
     }
 
     /**
-     * @see com.xpn.xwiki.plugin.XWikiDefaultPlugin#init(com.xpn.xwiki.XWikiContext)
-     */
-    @Override
-    public void init(XWikiContext context)
-    {
-        super.init(context);
-        getSsxClass(context);
-    }
-
-    /**
-     * @see com.xpn.xwiki.plugin.XWikiDefaultPlugin#virtualInit(com.xpn.xwiki.XWikiContext)
-     */
-    @Override
-    public void virtualInit(XWikiContext context)
-    {
-        super.virtualInit(context);
-        getSsxClass(context);
-    }
-
-    /**
      * {@inheritDoc}
      * 
      * @see SkinExtensionPlugin#getLink(String, XWikiContext)
@@ -71,6 +48,18 @@ public class CssSkinExtensionPlugin extends SkinExtensionPlugin
     }
 
     @Override
+    protected String getExtensionClassName()
+    {
+        return SSX_CLASS_NAME;
+    }
+
+    @Override
+    protected String getExtensionName()
+    {
+        return "Stylesheet";
+    }
+
+    @Override
     public void beginParsing(XWikiContext context)
     {
         super.beginParsing(context);
@@ -82,49 +71,15 @@ public class CssSkinExtensionPlugin extends SkinExtensionPlugin
         return super.endParsing(content, context);
     }
 
-    public BaseClass getSsxClass(XWikiContext context)
+    @Override
+    public void init(XWikiContext context)
     {
-        try {
-            XWikiDocument doc = context.getWiki().getDocument(SSX_CLASS_NAME, context);
-            boolean needsUpdate = false;
+        super.init(context);
+    }
 
-            BaseClass bclass = doc.getxWikiClass();
-            if (context.get("initdone") != null) {
-                return bclass;
-            }
-
-            bclass.setName(SSX_CLASS_NAME);
-
-            needsUpdate |= bclass.addTextField("name", "Name", 30);
-            needsUpdate |= bclass.addTextAreaField("code", "Code", 50, 20);
-            needsUpdate |= bclass.addStaticListField("use", "Use this extension", "onDemand=On demand|always=Always");
-            needsUpdate |= bclass.addBooleanField("parse", "Parse content", "yesno");
-            needsUpdate |= bclass.addStaticListField("cache", "Caching policy", "long|short|default|forbid");
-
-            if (StringUtils.isBlank(doc.getAuthor())) {
-                needsUpdate = true;
-                doc.setAuthor("XWiki.Admin");
-            }
-            if (StringUtils.isBlank(doc.getCreator())) {
-                needsUpdate = true;
-                doc.setCreator("XWiki.Admin");
-            }
-            if (StringUtils.isBlank(doc.getParent())) {
-                needsUpdate = true;
-                doc.setParent("XWiki.XWikiClasses");
-            }
-            if (StringUtils.isBlank(doc.getContent())) {
-                needsUpdate = true;
-                doc.setContent("1 XWiki Stylesheet Extension Class");
-            }
-
-            if (needsUpdate) {
-                context.getWiki().saveDocument(doc, context);
-            }
-            return bclass;
-        } catch (Exception ex) {
-            LOG.error("Cannot initialize SsxClass", ex);
-        }
-        return null;
+    @Override
+    public void virtualInit(XWikiContext context)
+    {
+        super.virtualInit(context);
     }
 }

8d06eecd155b1139fd06ca11022758647fae3a7e
sdumitriu <sergiu@xwiki.com>
2008-12-03 23:37:15 +0000

    XWIKI-2928: Regression: searchDocuments fails on PostgreSQL Fixed. Thanks to Artem for the hint.
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@14535 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-core/src/main/java/com/xpn/xwiki/store/XWikiHibernateStore.java
+++ xwiki-core/src/main/java/com/xpn/xwiki/store/XWikiHibernateStore.java
@@ -2423,7 +2423,7 @@ public class XWikiHibernateStore extends XWikiHibernateBaseStore implements XWik
         if (idx >= 0) {
             // With 'WHERE'
             idx = idx + 6;
-            result = result.substring(0, idx) + "(doc.hidden <> 1 or doc.hidden is null) and " + result.substring(idx);
+            result = result.substring(0, idx) + "(doc.hidden <> true or doc.hidden is null) and " + result.substring(idx);
         } else {
             // Without 'WHERE'
             int oidx = Math.min(result.toLowerCase().indexOf("order by "), Integer.MAX_VALUE);
@@ -2432,10 +2432,10 @@ public class XWikiHibernateStore extends XWikiHibernateBaseStore implements XWik
             if (idx > 0 && idx < Integer.MAX_VALUE) {
                 // Without 'WHERE', but with 'ORDER BY' or 'GROUP BY'
                 result =
-                    result.substring(0, idx) + "where doc.hidden <> 1 or doc.hidden is null " + result.substring(idx);
+                    result.substring(0, idx) + "where doc.hidden <> true or doc.hidden is null " + result.substring(idx);
             } else {
                 // Without 'WHERE', 'ORDER BY' or 'GROUP BY'... This should not happen at all.
-                result = result + " where (doc.hidden <> 1 or doc.hidden is null)";
+                result = result + " where (doc.hidden <> true or doc.hidden is null)";
             }
             // TODO: Take into account GROUP BY, HAVING and other keywords when there's no WHERE in the query
         }

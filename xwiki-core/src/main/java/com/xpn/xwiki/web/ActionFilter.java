5e32b906a299f58cc049a2a52eb646d27be485de
sdumitriu <sergiu@xwiki.com>
2009-01-04 05:47:04 +0000

    XWIKI-3064: Better backend support for different form actions Done (core part).
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@15048 f329d543-caf0-0310-9063-dda96c69346f

---
+++ xwiki-core/src/main/java/com/xpn/xwiki/web/ActionFilter.java
@@ -0,0 +1,132 @@
+package com.xpn.xwiki.web;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * <p>
+ * A filter that dispatches requests to the right action, depending on the button that was pressed in the editing form.
+ * This is needed since the browser cannot send the form data to different URLs depending on the button pressed, and an
+ * XHTML form has only one target URL. In previous versions of XWiki this was accomplished using javascript code, with a
+ * fall-back on a pseudo-dispatcher inside the {@link PreviewAction}, which was on obvious case of bad code design.
+ * </p>
+ * <p>
+ * The filter dispatches requests based on the presence of a request parameter starting with <tt>action_</tt> followed
+ * by the name of the struts action that should actually process the request. For example, the button that does
+ * <tt>Save and Continue</tt> looks like:
+ * 
+ * <pre>
+ * &lt;input type=&quot;submit&quot; name=&quot;action_saveandcontinue&quot; value=&quot;...&quot;/&gt;
+ * </pre>
+ * 
+ * As a result, when clicking the button, the request is not sent to the form's target (<tt>preview</tt>), but is
+ * actually forwarded internally to <tt>/bin/saveandcontinue/The/Document</tt>.
+ * </p>
+ * 
+ * @version $Id$
+ * @since 1.8M1
+ */
+public class ActionFilter implements Filter
+{
+    /** Logging helper. */
+    private static final Log LOG = LogFactory.getLog(ActionFilter.class);
+
+    /** The query property name prefix that indicates the target action. */
+    private static final String ACTION_PREFIX = "action_";
+
+    /** URL path separator. */
+    private static final String PATH_SEPARATOR = "/";
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see Filter#init(FilterConfig)
+     */
+    public void init(FilterConfig filterConfig) throws ServletException
+    {
+        // No initialization needed.
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+     */
+    @SuppressWarnings("unchecked")
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
+        ServletException
+    {
+        // Only HTTP requests can be dispatched.
+        if (request instanceof HttpServletRequest) {
+            HttpServletRequest hrequest = (HttpServletRequest) request;
+            Enumeration<String> parameterNames = hrequest.getParameterNames();
+            while (parameterNames.hasMoreElements()) {
+                String parameter = parameterNames.nextElement();
+                if (parameter.startsWith(ACTION_PREFIX)) {
+                    String targetURL = getTargetURL(hrequest, parameter);
+                    RequestDispatcher dispatcher = hrequest.getRequestDispatcher(targetURL);
+                    if (dispatcher != null) {
+                        LOG.debug("Forwarding request to " + targetURL);
+                        dispatcher.forward(hrequest, response);
+                        // If the request was forwarder to another path, don't continue the normal processing chain.
+                        return;
+                    }
+                }
+            }
+        }
+        // Let the request pass through unchanged.
+        chain.doFilter(request, response);
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see Filter#destroy()
+     */
+    public void destroy()
+    {
+        // No finalization needed.
+    }
+
+    /**
+     * Compose a new URL path based on the original request and the specified action. The result is relative to the
+     * application context, so that it can be used with {@link HttpServletRequest#getRequestDispatcher(String)}. For
+     * example, calling this method with a request for <tt>/xwiki/bin/edit/Some/Document</tt> and <tt>action_save</tt>,
+     * the result is <tt>/bin/save/Some/Document</tt>.
+     * 
+     * @param request the original request
+     * @param action the action parameter, starting with <tt>action_</tt>
+     * @return The rebuilt URL path, with the specified action in place of the original Struts action. Note that unlike
+     *         the HTTP path, this does not contain the application context part.
+     */
+    private String getTargetURL(HttpServletRequest request, String action)
+    {
+        String newAction = PATH_SEPARATOR + action.substring(ACTION_PREFIX.length());
+        // Extract the document name from the requested path. We don't use getPathInfo() since it is decoded
+        // by the container, thus it will not work when XWiki uses a non-UTF-8 encoding.
+        // First step, remove the context path, if any.
+        String document = StringUtils.substringAfter(request.getRequestURI(), request.getContextPath());
+        // Second step, remove the servlet path, if any.
+        document = StringUtils.substringAfter(document, request.getServletPath());
+        // Third step, remove the struts mapping. This step is mandatory, so this filter will fail if the
+        // requested action was a hidden (default) 'view', like in '/bin/Main/'. This is OK, since forms
+        // don't use 'view' as a target.
+        document = PATH_SEPARATOR + StringUtils.substringAfter(document.substring(1), PATH_SEPARATOR);
+
+        // Compose the target URL starting with the servlet path.
+        return request.getServletPath() + newAction + document;
+    }
+}

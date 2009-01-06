50a132df2904371f7f73cffbe433a742e712b1de
tmortagne <thomas.mortagne@xwiki.com>
2009-01-06 11:21:41 +0000

    XPXARPLUGIN-11:  XarMojo.addFilesToArchive function fails under Windows OS due to the use of String.split function without escaping FileSeparatorChar '\'
    Applied patch from Pascal Voitot without modification.
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/xwiki-tools/trunk/xwiki-xar-plugin@15089 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-platform-tools/src/main/java/com/xpn/xwiki/tool/xar/XarMojo.java
+++ xwiki-platform-tools/src/main/java/com/xpn/xwiki/tool/xar/XarMojo.java
@@ -337,8 +337,14 @@ public class XarMojo extends AbstractXarMojo
                 if (documentNames.contains(documentName)) {
 
                     // building the path the current file will have within the archive
+                    /*
+                     * DO NOT USE String.split since it requires a regexp. Under Windows XP, the FileSeparator is '\'
+                     * when not escaped is a special character of the regexp String archivedFilePath =
+                     * currentFile.getAbsolutePath().split(sourceDir.getAbsolutePath() + File.separator)[1];
+                     */
                     String archivedFilePath =
-                        currentFile.getAbsolutePath().split(sourceDir.getAbsolutePath() + File.separator)[1];
+                        currentFile.getAbsolutePath()
+                            .substring((sourceDir.getAbsolutePath() + File.separator).length());
                     archivedFilePath.replace(File.separatorChar, '/');
 
                     archiver.addFile(currentFile, archivedFilePath);

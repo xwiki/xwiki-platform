e1c466379fd5481c6cbce3687e6e47fc20e8451b
vmassol <vincent@massol.net>
2009-01-25 13:19:06 +0000

    Fixed missing call to parent's constructor.
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@15708 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-rendering/xwiki-rendering-macros/xwiki-rendering-macro-script/src/test/java/org/xwiki/rendering/scaffolding/AbstractScriptRenderingTestCase.java
+++ xwiki-rendering/xwiki-rendering-macros/xwiki-rendering-macro-script/src/test/java/org/xwiki/rendering/scaffolding/AbstractScriptRenderingTestCase.java
@@ -25,7 +25,7 @@ public abstract class AbstractScriptRenderingTestCase extends AbstractRenderingT
 {
     public AbstractScriptRenderingTestCase()
     {
-
+        super();
     }
 
     public AbstractScriptRenderingTestCase(String testName)

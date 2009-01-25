385fbd75e8137992b8bad1dff2c92088b0016bb8
vmassol <vincent@massol.net>
2009-01-25 14:33:12 +0000

    Moved include macro test into the include macro module (it was in the velocity macro module) + fixed pom.xml that wasn't configured properly thus not running a unit test (which incidentally had a bug that I've fixed too).
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@15710 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-rendering/xwiki-rendering-macros/xwiki-rendering-macro-include/src/test/java/org/xwiki/rendering/internal/macro/IncludeMacroTest.java
+++ xwiki-rendering/xwiki-rendering-macros/xwiki-rendering-macro-include/src/test/java/org/xwiki/rendering/internal/macro/IncludeMacroTest.java
@@ -19,12 +19,15 @@
  */
 package org.xwiki.rendering.internal.macro;
 
+import java.io.StringWriter;
 import java.util.List;
 
 import org.jmock.Mock;
 import org.xwiki.bridge.DocumentAccessBridge;
 import org.xwiki.rendering.block.Block;
 import org.xwiki.rendering.internal.macro.include.IncludeMacro;
+import org.xwiki.rendering.internal.macro.velocity.VelocityMacro;
+import org.xwiki.rendering.internal.transformation.MacroTransformation;
 import org.xwiki.rendering.macro.Macro;
 import org.xwiki.rendering.macro.include.IncludeMacroParameters;
 import org.xwiki.rendering.macro.include.IncludeMacroParameters.Context;
@@ -32,6 +35,8 @@ import org.xwiki.rendering.parser.Syntax;
 import org.xwiki.rendering.parser.SyntaxType;
 import org.xwiki.rendering.scaffolding.AbstractRenderingTestCase;
 import org.xwiki.rendering.transformation.MacroTransformationContext;
+import org.xwiki.rendering.transformation.Transformation;
+import org.xwiki.velocity.VelocityManager;
 
 /**
  * Unit tests for {@link IncludeMacro}.
@@ -41,6 +46,49 @@ import org.xwiki.rendering.transformation.MacroTransformationContext;
  */
 public class IncludeMacroTest extends AbstractRenderingTestCase
 {
+    public void testIncludeMacroWithNewContext() throws Exception
+    {
+        String expected = "beginDocument\n"
+            + "beginMacroMarker [velocity] [] [$myvar]\n"
+            + "beginParagraph\n"
+            + "onSpecialSymbol [$]\n"
+            + "onWord [myvar]\n"
+            + "endParagraph\n"
+            + "endMacroMarker [velocity] [] [$myvar]\n"
+            + "endDocument";
+
+        // Since it's not in the same context, we verify that a Velocity variable set in the including page is not
+        // seen in the included page.
+        VelocityManager velocityManager = (VelocityManager) getComponentManager().lookup(VelocityManager.ROLE);
+        StringWriter writer = new StringWriter();
+        velocityManager.getVelocityEngine().evaluate(velocityManager.getVelocityContext(), writer, "template",
+            "#set ($myvar = 'hello')");
+
+        IncludeMacro macro = (IncludeMacro) getComponentManager().lookup(VelocityMacro.ROLE, "include");
+        Mock mockDocumentAccessBridge = mock(DocumentAccessBridge.class);
+        mockDocumentAccessBridge.expects(once()).method("isDocumentViewable").will(returnValue(true));
+        mockDocumentAccessBridge.expects(once()).method("getDocumentContent").will(
+            returnValue("{{velocity}}$myvar{{/velocity}}"));
+        mockDocumentAccessBridge.expects(once()).method("getDocumentSyntaxId").will(
+            returnValue(new Syntax(SyntaxType.XWIKI, "2.0").toIdString()));
+        macro.setDocumentAccessBridge((DocumentAccessBridge) mockDocumentAccessBridge.proxy());
+
+        IncludeMacroParameters parameters = new IncludeMacroParameters();
+        parameters.setDocument("wiki:Space.Page");
+        parameters.setContext(Context.NEW);
+
+        // Create a Macro transformation context with the Macro transformation object defined so that the include
+        // macro can transform included page which is using a new context.
+        MacroTransformation macroTransformation = 
+            (MacroTransformation) getComponentManager().lookup(Transformation.ROLE, "macro");
+        MacroTransformationContext context = new MacroTransformationContext();
+        context.setMacroTransformation(macroTransformation);
+        
+        List<Block> blocks = macro.execute(parameters, null, context);
+
+        assertBlocks(expected, blocks);
+    }
+    
     public void testIncludeMacroWithCurrentContext() throws Exception
     {
         String expected = "beginDocument\n"

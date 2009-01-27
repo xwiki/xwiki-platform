e72ad75ea379202b7dc05ba843544692afed7386
asiri <asiri@xwiki.com>
2009-01-27 08:41:12 +0000

    XWIKI-3153: Possibility of data corruption in office importer
    * Fixed: Each request will now have it's own file storage for doing it's activities so that there are no conflicts.
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@15748 f329d543-caf0-0310-9063-dda96c69346f

---
+++ xwiki-officeimporter/src/main/java/org/xwiki/officeimporter/internal/OfficeImporterFileStorage.java
@@ -0,0 +1,112 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.officeimporter.internal;
+
+import java.io.File;
+
+/**
+ * Keeps track of file system storage for office importer.
+ * 
+ * @version $Id$
+ * @since 1.8M2
+ */
+public class OfficeImporterFileStorage
+{
+    /**
+     * Top-level temporary working directory.
+     */
+    private File tempDir;
+
+    /**
+     * Storage for input document.
+     */
+    private File inputFile = null;
+
+    /**
+     * Output directory (inside tempDir). All the by-products (artifacts) + outputFile will land here.
+     */
+    private File outputDir = null;
+
+    /**
+     * Output file (html).
+     */
+    private File outputFile = null;
+
+    /**
+     * Default constructor.
+     * 
+     * @param tempDirName name of the temporary files directory.
+     */
+    public OfficeImporterFileStorage(String tempDirName)
+    {
+        tempDir = new File(System.getProperty("java.io.tmpdir"), tempDirName);
+        tempDir.mkdir();
+        inputFile = new File(tempDir, "input.tmp");
+        outputDir = new File(tempDir, "output");
+        outputDir.mkdir();
+        outputFile = new File(outputDir, "output.html");
+    }
+
+    /**
+     * Cleans up the allocated file storage.
+     */
+    public void cleanUp()
+    {
+        File[] outputFiles = outputDir.listFiles();
+        for (File file : outputFiles) {
+            file.delete();
+        }
+        outputDir.delete();
+        inputFile.delete();
+        tempDir.delete();
+    }
+
+    /**
+     * @return the top level temporary directory.
+     */
+    public File getTempDir()
+    {
+        return tempDir;
+    }
+
+    /**
+     * @return the input file holding the content for jodaconverter.
+     */
+    public File getInputFile()
+    {
+        return inputFile;
+    }
+
+    /**
+     * @return the output directory where the output form jodaconverter lands.
+     */
+    public File getOutputDir()
+    {
+        return outputDir;
+    }
+
+    /**
+     * @return the main html output file for the conversion.
+     */
+    public File getOutputFile()
+    {
+        return outputFile;
+    }
+}
--- xwiki-officeimporter/src/main/java/org/xwiki/officeimporter/internal/transformer/OfficeToHtmlTransformer.java
+++ xwiki-officeimporter/src/main/java/org/xwiki/officeimporter/internal/transformer/OfficeToHtmlTransformer.java
@@ -30,6 +30,7 @@ import org.xwiki.component.phase.Initializable;
 import org.xwiki.component.phase.InitializationException;
 import org.xwiki.officeimporter.OfficeImporterContext;
 import org.xwiki.officeimporter.OfficeImporterException;
+import org.xwiki.officeimporter.internal.OfficeImporterFileStorage;
 import org.xwiki.officeimporter.transformer.DocumentTransformer;
 
 import com.artofsolving.jodconverter.DefaultDocumentFormatRegistry;
@@ -74,26 +75,6 @@ public class OfficeToHtmlTransformer extends AbstractLogEnabled implements Docum
     private DocumentFormat htmlFormat;
 
     /**
-     * Temporary working directory.
-     */
-    private File tempDir;
-
-    /**
-     * Storage for input document.
-     */
-    private File inputFile = null;
-
-    /**
-     * Output directory (inside tempDir). All the by-products (artifacts) + outputFile will land here.
-     */
-    private File outputDir = null;
-
-    /**
-     * Output file (html).
-     */
-    private File outputFile = null;
-
-    /**
      * {@inheritDoc}
      */
     public void initialize() throws InitializationException
@@ -108,49 +89,51 @@ public class OfficeToHtmlTransformer extends AbstractLogEnabled implements Docum
      */
     public void transform(OfficeImporterContext importerContext) throws OfficeImporterException
     {
-        // Make a connection with the Open Office server.
-        try {
-            openOfficeServerConnection.connect();
-        } catch (ConnectException ex) {
-            String message =
-                "Could not connect to OpenOffice server at " + openOfficeServerIp + ":" + openOfficeServerPort;
-            getLogger().error(message, ex);
-            throw new OfficeImporterException(message, ex);
-        }
-        // Create an instance of the converter.
-        openOfficeDocumentConverter = new OpenOfficeDocumentConverter(openOfficeServerConnection);
+        // Make a connection to the OO server.
+        if (!openOfficeServerConnection.isConnected()) {
+            try {
+                openOfficeServerConnection.connect();
+                // Create an instance of the converter.
+                openOfficeDocumentConverter = new OpenOfficeDocumentConverter(openOfficeServerConnection);
+            } catch (ConnectException ex) {
+                String message =
+                    "Could not connect to OpenOffice server at " + openOfficeServerIp + ":" + openOfficeServerPort;
+                getLogger().error(message, ex);
+                throw new OfficeImporterException(message, ex);
+            }
+        }        
         // Prepare the temporary directory structure.
-        prepareTempStorage("xwiki-office-importer-" + importerContext.getCurrentUser());
+        OfficeImporterFileStorage storage =
+            new OfficeImporterFileStorage("xwiki-office-importer-" + importerContext.getCurrentUser());
         // Fill in the input file.
         try {
-            FileOutputStream fos = new FileOutputStream(inputFile);
+            FileOutputStream fos = new FileOutputStream(storage.getInputFile());
             fos.write(importerContext.getSourceData());
             fos.close();
         } catch (IOException ex) {
             String message = "Internal error while creating temporary files.";
             getLogger().error(message, ex);
-            cleanTempStorage();
+            storage.cleanUp();
             throw new OfficeImporterException(message, ex);
         }
         // Make the conversion.
-        openOfficeDocumentConverter.convert(inputFile, importerContext.getSourceFormat(), outputFile, htmlFormat);
-        openOfficeServerConnection.disconnect();
-        // Collect the output into context.
-        // First the html output.
+        openOfficeDocumentConverter.convert(storage.getInputFile(), importerContext.getSourceFormat(), storage
+            .getOutputFile(), htmlFormat);
+        // Collect the output into context. First the html output.
         try {
-            FileInputStream fis = new FileInputStream(outputFile);
-            byte[] content = new byte[(int) outputFile.length()];
+            FileInputStream fis = new FileInputStream(storage.getOutputFile());
+            byte[] content = new byte[(int) storage.getOutputFile().length()];
             fis.read(content);
             fis.close();
             importerContext.setTargetDocumentContent(new String(content));
         } catch (IOException ex) {
             String message = "Internal error while reading temporary files.";
             getLogger().error(message, ex);
-            cleanTempStorage();
+            storage.cleanUp();
             throw new OfficeImporterException(message, ex);
         }
         // Start collecting the artifacts.
-        File[] artifacts = outputDir.listFiles();
+        File[] artifacts = storage.getOutputDir().listFiles();
         for (File artifact : artifacts) {
             try {
                 FileInputStream fis = new FileInputStream(artifact);
@@ -159,40 +142,11 @@ public class OfficeToHtmlTransformer extends AbstractLogEnabled implements Docum
                 importerContext.addArtifact(artifact.getName(), data);
             } catch (IOException ex) {
                 String message = "Internal error while reading artifact : " + artifact.getName();
-                getLogger().error(message, ex);                
+                getLogger().error(message, ex);
                 // Skip the artifact.
             }
         }
         // Cleanup the mess.
-        cleanTempStorage();
-    }
-
-    /**
-     * Creates the temporary directory structure required for the conversion.
-     * 
-     * @param tempDirName Name of the top level temporary directory.
-     */
-    private void prepareTempStorage(String tempDirName)
-    {
-        tempDir = new File(System.getProperty("java.io.tmpdir"), tempDirName);
-        tempDir.mkdir();
-        inputFile = new File(tempDir, "input.tmp");
-        outputDir = new File(tempDir, "output");
-        outputDir.mkdir();
-        outputFile = new File(outputDir, "output.html");
-    }
-
-    /**
-     * Sweeps out all the temporary files created.
-     */
-    private void cleanTempStorage()
-    {
-        File[] outputFiles = outputDir.listFiles();
-        for (File file : outputFiles) {
-            file.delete();
-        }
-        outputDir.delete();
-        inputFile.delete();
-        tempDir.delete();
+        storage.cleanUp();
     }
 }

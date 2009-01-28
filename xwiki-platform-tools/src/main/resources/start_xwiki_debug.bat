c3e59e9b65a3d48b464ab6f8afb683d5590cb4dc
vmassol <vincent@massol.net>
2009-01-28 18:30:58 +0000

    XTJETTY-10: Upgrade to Jetty 6.1.15 rc2
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/xwiki-tools/trunk/xwiki-jetty-resources@15835 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-platform-tools/src/main/resources/start_xwiki_debug.bat
+++ xwiki-platform-tools/src/main/resources/start_xwiki_debug.bat
@@ -5,14 +5,6 @@ set JETTY_PORT=8080
 set JAVA_OPTS=-Xmx300m
 set JAVA_OPTS=%JAVA_OPTS% -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005
 
-REM In order to avoid getting a "java.lang.IllegalStateException: Form too large" error
-REM when editing large page in XWiki we need to tell Jetty to allow for large content
-REM since by default it only allows for 20K. We do this by passing the
-REM org.mortbay.http.HttpRequest.maxFormContentSize property.
-REM Note that setting this value too high can leave your server vulnerable to denial of
-REM service attacks.
-set JAVA_OPTS=%JAVA_OPTS% -Dorg.mortbay.http.HttpRequest.maxFormContentSize=1000000
-
 REM Jetty requires a logs directory to exist
 if not exist %JETTY_HOME%\logs mkdir %JETTY_HOME%\logs
 
@@ -26,6 +18,9 @@ if "%2"=="profiler" (
   set PATH=%3;%PATH%
   set JAVA_TOOL_OPTIONS=-agentlib:yjpagent
 )
+
+REM Specify port and key to stop a running Jetty instance
+set JAVA_OPTS=%JAVA_OPTS% -DSTOP.KEY=xwiki -DSTOP.PORT=8079
 
 java %JAVA_OPTS% -Dfile.encoding=iso-8859-1 -Djetty.home=%JETTY_HOME% -Djetty.port=%JETTY_PORT% -jar %JETTY_HOME%/start.jar
 

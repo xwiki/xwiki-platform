05bbaa62daf2929ef1af485f0f6e18fdd3c916cd
vmassol <vincent@massol.net>
2009-02-25 18:06:34 +0000

    XTSHAREDTESTS-12: Make sure that the Container component is initialized
    * All done
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/xwiki-tools/trunk/xwiki-shared-tests@17047 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-platform-tools/src/main/java/com/xpn/xwiki/test/AbstractXWikiComponentTestCase.java
+++ xwiki-platform-tools/src/main/java/com/xpn/xwiki/test/AbstractXWikiComponentTestCase.java
@@ -20,8 +20,14 @@
  */
 package com.xpn.xwiki.test;
 
+import org.jmock.Mock;
 import org.jmock.cglib.MockObjectTestCase;
 import org.xwiki.component.manager.ComponentManager;
+import org.xwiki.container.ApplicationContext;
+import org.xwiki.container.Container;
+import org.xwiki.container.Request;
+import org.xwiki.container.Response;
+import org.xwiki.container.Session;
 
 /**
  * Tests which needs to have XWiki Components set up should extend this class which makes the Component Manager
@@ -31,6 +37,26 @@ public abstract class AbstractXWikiComponentTestCase extends MockObjectTestCase
 {
     private XWikiComponentInitializer initializer = new XWikiComponentInitializer();
 
+    /**
+     * @see #getApplicationContextMock()
+     */
+    private Mock mockApplicationContext;
+    
+    /**
+     * @see #getRequestMock()
+     */
+    private Mock mockRequest;
+    
+    /**
+     * @see #getResponseMock()
+     */
+    private Mock mockResponse;
+    
+    /**
+     * @see #getSessionMock()
+     */
+    private Mock mockSession;
+    
     public AbstractXWikiComponentTestCase()
     {
         super();
@@ -43,6 +69,26 @@ public abstract class AbstractXWikiComponentTestCase extends MockObjectTestCase
 
     protected void setUp() throws Exception
     {
+        // Create a mock Container implementation since we're not running in a real container. We need to do this
+        // since some components are using the Container component (for example to access resource such as the XWiki 
+        // configuration file).
+    	// Tests can use the getter methods to access the mocks and configure them as needed.
+        Container container = (Container) getComponentManager().lookup(Container.ROLE);
+
+        this.mockApplicationContext = mock(ApplicationContext.class);
+        this.mockApplicationContext.stubs().method("getResource").with(contains("xwiki.properties")).will(
+            returnValue(this.getClass().getClassLoader().getResource("xwiki.properties")));
+        container.setApplicationContext((ApplicationContext) this.mockApplicationContext.proxy());
+
+        this.mockRequest = mock(Request.class);
+        container.setRequest((Request) this.mockRequest.proxy());
+        
+        this.mockResponse = mock(Response.class);
+        container.setResponse((Response) this.mockResponse.proxy());
+        
+        this.mockSession = mock(Session.class);
+        container.setSession((Session) this.mockSession.proxy());
+        
         this.initializer.initialize();
     }
 
@@ -58,5 +104,37 @@ public abstract class AbstractXWikiComponentTestCase extends MockObjectTestCase
     public ComponentManager getComponentManager() throws Exception
     {
         return this.initializer.getComponentManager();
+    }
+
+    /**
+     * @return the Container {@link ApplicationContext} mock
+     */
+    public Mock getApplicationContextMock()
+    {
+        return this.mockApplicationContext;
+    }
+    
+    /**
+     * @return the Container {@link Request} mock
+     */
+    public Mock getRequestMock()
+    {
+        return this.mockRequest;
+    }
+    
+    /**
+     * @return the Container {@link Response} mock
+     */
+    public Mock getResponseMock()
+    {
+        return this.mockResponse;
+    }
+    
+    /**
+     * @return the Container {@link Session} mock
+     */
+    public Mock getSessionMock()
+    {
+        return this.mockSession;
     }
 }

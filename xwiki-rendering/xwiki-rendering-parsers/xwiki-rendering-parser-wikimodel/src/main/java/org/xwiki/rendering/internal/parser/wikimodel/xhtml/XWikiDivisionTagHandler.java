f4d34170001dd3e1ef6c41ea6cf7ebfa7e6221ca
vmassol <vincent@massol.net>
2009-02-27 15:12:49 +0000

    Added more explanations
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@17089 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-rendering/xwiki-rendering-parsers/xwiki-rendering-parser-wikimodel/src/main/java/org/xwiki/rendering/internal/parser/wikimodel/xhtml/XWikiDivisionTagHandler.java
+++ xwiki-rendering/xwiki-rendering-parsers/xwiki-rendering-parser-wikimodel/src/main/java/org/xwiki/rendering/internal/parser/wikimodel/xhtml/XWikiDivisionTagHandler.java
@@ -3,7 +3,9 @@ package org.xwiki.rendering.internal.parser.wikimodel.xhtml;
 import org.wikimodel.wem.xhtml.handler.DivisionTagHandler;
 
 /**
- * Change the class value indicating that the division is an embedded document.
+ * Change the class value indicating that the division is an embedded document. We do this in order to be independent 
+ * of WikiModel in what we expose to the outside world. Thus if one day we need to change to another implementation
+ * we won't be tied to WikiModel.
  * 
  * @version $Id$
  * @since 1.8M2

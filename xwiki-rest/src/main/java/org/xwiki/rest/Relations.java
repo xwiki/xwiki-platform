7326d27ecb6a0e8d91770ae2f0d7ce1e3440602f
fmancinelli <fabio.mancinelli@xwiki.com>
2009-03-01 23:00:32 +0000

    XWIKI-3271: Add tags related resources to the REST API
    Implemented.
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@17110 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-rest/src/main/java/org/xwiki/rest/Relations.java
+++ xwiki-rest/src/main/java/org/xwiki/rest/Relations.java
@@ -68,4 +68,8 @@ public class Relations
 
     public static final String CHILDREN = "http://www.xwiki.org/rel/children";
 
+    public static final String TAGS = "http://www.xwiki.org/rel/tags";
+
+    public static final String TAG = "http://www.xwiki.org/rel/tag";
+
 }
---
+++ xwiki-rest/src/main/java/org/xwiki/rest/resources/tags/TagsResource.java
@@ -0,0 +1,65 @@
+package org.xwiki.rest.resources.tags;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
+
+import org.xwiki.rest.Relations;
+import org.xwiki.rest.XWikiResource;
+import org.xwiki.rest.model.jaxb.Link;
+import org.xwiki.rest.model.jaxb.Tag;
+import org.xwiki.rest.model.jaxb.Tags;
+
+import com.xpn.xwiki.XWikiException;
+import com.xpn.xwiki.plugin.tag.TagPlugin;
+
+@Path("/wikis/{wikiName}/tags")
+public class TagsResource extends XWikiResource
+{
+
+    public TagsResource(@Context UriInfo uriInfo)
+    {
+        super(uriInfo);
+    }
+
+    @GET
+    public Tags getTags(@PathParam("wikiName") String wikiName) throws XWikiException
+    {
+        String database = xwikiContext.getDatabase();
+
+        Tags tags = objectFactory.createTags();
+
+        /* This try is just needed for executing the finally clause. */
+        try {
+            xwikiContext.setDatabase(wikiName);
+
+            TagPlugin tagPlugin = (TagPlugin) xwiki.getPlugin("tag", xwikiContext);
+
+            List<String> tagNames = tagPlugin.getAllTags(xwikiContext);
+
+            for (String tagName : tagNames) {
+                Tag tag = objectFactory.createTag();
+                tag.setName(tagName);
+
+                String tagUri =
+                    UriBuilder.fromUri(uriInfo.getBaseUri()).path(PagesForTagsResource.class).build(wikiName, tagName)
+                        .toString();
+                Link tagLink = objectFactory.createLink();
+                tagLink.setHref(tagUri);
+                tagLink.setRel(Relations.TAG);
+                tag.getLinks().add(tagLink);
+
+                tags.getTags().add(tag);
+            }
+        } finally {
+            xwikiContext.setDatabase(database);
+        }
+
+        return tags;
+    }
+}

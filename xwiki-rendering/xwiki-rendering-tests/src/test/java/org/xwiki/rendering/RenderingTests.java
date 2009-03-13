f66f0072e816133b858542b5afb10a4482bd6aec
tmortagne <thomas.mortagne@xwiki.com>
2009-03-13 14:21:18 +0000

    XWIKI-3327: When a closed header (with ending =*) is followed by a new line and some text, the new line is part of the generated paragraph
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@17615 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-rendering/xwiki-rendering-tests/src/test/java/org/xwiki/rendering/RenderingTests.java
+++ xwiki-rendering/xwiki-rendering-tests/src/test/java/org/xwiki/rendering/RenderingTests.java
@@ -160,6 +160,7 @@ public class RenderingTests extends TestCase
         suite.addTestsFromResource("section/section6", false);
         suite.addTestsFromResource("section/section7", false);
         suite.addTestsFromResource("section/section8", false);
+        suite.addTestsFromResource("section/section9", false);
 
         // Other
         suite.addTestsFromResource("escape/escape1", false);

e365294682f9edd449d8d42bbe092df17944f44e
asiri <asiri@xwiki.com>
2009-03-22 07:16:13 +0000

    XWIKI-3415: Office Importer fails when importing documents with TOC (Table Of Contents) structures
    * Fixed.
    * But there is still a problem (not a failure) with importing word documents with TOC structures. That's a different issue.
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@17894 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-officeimporter/src/test/java/org/xwiki/officeimporter/internal/cleaner/LinkOpenOfficeCleaningTest.java
+++ xwiki-officeimporter/src/test/java/org/xwiki/officeimporter/internal/cleaner/LinkOpenOfficeCleaningTest.java
@@ -72,6 +72,21 @@ public class LinkOpenOfficeCleaningTest extends AbstractHTMLCleaningTest
     }
 
     /**
+     * Test duplicate anchor filtering with TOC structures. see: http://jira.xwiki.org/jira/browse/XWIKI-3415
+     */
+    public void testAnchorFilteringWithTOC()
+    {
+        String html = header + "<div>some text<h1><a name=\"Topic1\"/>Topic1</h1></div>" + footer;
+        try {
+            Document doc = openOfficeHTMLCleaner.clean(new StringReader(html));
+            NodeList nodes = doc.getElementsByTagName("a");
+            assertEquals(1, nodes.getLength());
+        } catch (ClassCastException ex) {
+            fail(ex.getMessage());
+        }
+    }
+
+    /**
      * Test wrapping of html anchors with xwiki specific xhtml elements so that XHTML parser can recognize them.
      */
     public void testAnchorWrapping()

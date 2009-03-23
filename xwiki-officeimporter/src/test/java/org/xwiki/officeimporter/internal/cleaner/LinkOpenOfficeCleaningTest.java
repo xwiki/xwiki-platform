433c93cd1bd515ec91f51b903030e5717177edb2
asiri <asiri@xwiki.com>
2009-03-23 05:38:27 +0000

    XWIKI-3415: Office Importer fails when importing documents with TOC (Table Of Contents) structures
    * Applying review suggestions.
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@17896 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-officeimporter/src/test/java/org/xwiki/officeimporter/internal/cleaner/LinkOpenOfficeCleaningTest.java
+++ xwiki-officeimporter/src/test/java/org/xwiki/officeimporter/internal/cleaner/LinkOpenOfficeCleaningTest.java
@@ -74,16 +74,12 @@ public class LinkOpenOfficeCleaningTest extends AbstractHTMLCleaningTest
     /**
      * Test duplicate anchor filtering with TOC structures. see: http://jira.xwiki.org/jira/browse/XWIKI-3415
      */
-    public void testAnchorFilteringWithTOC()
+    public void testAnchorFilteringWithTOC() throws ClassCastException
     {
         String html = header + "<div>some text<h1><a name=\"Topic1\"/>Topic1</h1></div>" + footer;
-        try {
-            Document doc = openOfficeHTMLCleaner.clean(new StringReader(html));
-            NodeList nodes = doc.getElementsByTagName("a");
-            assertEquals(1, nodes.getLength());
-        } catch (ClassCastException ex) {
-            fail(ex.getMessage());
-        }
+        Document doc = openOfficeHTMLCleaner.clean(new StringReader(html));
+        NodeList nodes = doc.getElementsByTagName("a");
+        assertEquals(1, nodes.getLength());
     }
 
     /**

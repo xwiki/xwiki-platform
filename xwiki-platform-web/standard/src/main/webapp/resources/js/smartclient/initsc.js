c6b797448cde463d15cac649bf914a2e19ca22ee
jvdrean <jv@drean.org>
2009-04-15 09:06:06 +0000

    XWIKI-3583 : Smartclient skin fail to load in specific environments
    Fixed.
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/web/trunk@18667 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-platform-web/standard/src/main/webapp/resources/js/smartclient/initsc.js
+++ xwiki-platform-web/standard/src/main/webapp/resources/js/smartclient/initsc.js
@@ -1,1 +1,5 @@
-var isomorphicDir = "$xwiki.getSkinFile('js/smartclient/')";
+//
+// TODO : When XWIKI-3582 will be fixed we'd better use the line above.
+// var isomorphicDir = "$xwiki.getSkinFile('js/smartclient/')";
+//
+var isomorphicDir = "${request.getContextPath()}/resources/js/smartclient/";

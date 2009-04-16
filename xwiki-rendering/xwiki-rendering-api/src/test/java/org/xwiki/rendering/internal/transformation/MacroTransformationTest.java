f8f1c2cd921a650e4943e87e70892fc2fbca3ff5
vmassol <vincent@massol.net>
2009-04-16 15:54:47 +0000

    Removed unused import
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@18749 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-rendering/xwiki-rendering-api/src/test/java/org/xwiki/rendering/internal/transformation/MacroTransformationTest.java
+++ xwiki-rendering/xwiki-rendering-api/src/test/java/org/xwiki/rendering/internal/transformation/MacroTransformationTest.java
@@ -21,7 +21,6 @@ package org.xwiki.rendering.internal.transformation;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
 import org.xwiki.rendering.scaffolding.AbstractRenderingTestCase;

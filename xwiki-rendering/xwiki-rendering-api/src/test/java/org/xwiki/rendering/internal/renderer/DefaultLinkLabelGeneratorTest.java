8148dc7ee06927811e67c59c546d5212f2ffa663
tmortagne <thomas.mortagne@xwiki.com>
2009-04-18 09:28:30 +0000

    XWIKI-3623: Backslash inside link syntax fail to render
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/branches/xwiki-core-1.8@18796 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-rendering/xwiki-rendering-api/src/test/java/org/xwiki/rendering/internal/renderer/DefaultLinkLabelGeneratorTest.java
+++ xwiki-rendering/xwiki-rendering-api/src/test/java/org/xwiki/rendering/internal/renderer/DefaultLinkLabelGeneratorTest.java
@@ -11,24 +11,30 @@ import org.xwiki.rendering.listener.Link;
 public class DefaultLinkLabelGeneratorTest extends MockObjectTestCase
 {
     private DefaultLinkLabelGenerator generator;
-    
+
     private Mock mockModelBridge;
-    
+
     private Mock mockAccessBridge;
-    
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see junit.framework.TestCase#setUp()
+     */
+    @Override
     protected void setUp()
     {
         this.generator = new DefaultLinkLabelGenerator();
-    
+
         Mock mockConfiguration = mock(RenderingConfiguration.class);
         mockConfiguration.stubs().method("getLinkLabelFormat").will(returnValue("[%w:%s.%p] %P (%t)"));
         this.generator.setRenderingConfiguration((RenderingConfiguration) mockConfiguration.proxy());
-        
+
         this.mockModelBridge = mock(DocumentModelBridge.class);
-        
+
         this.mockAccessBridge = mock(DocumentAccessBridge.class);
-        this.mockAccessBridge.stubs().method("getDocumentName").will(returnValue(
-            new DocumentName("xwiki", "Main", "HelloWorld")));
+        this.mockAccessBridge.stubs().method("getDocumentName").will(
+            returnValue(new DocumentName("xwiki", "Main", "HelloWorld")));
         this.generator.setDocumentAccessBridge((DocumentAccessBridge) mockAccessBridge.proxy());
     }
 
@@ -41,12 +47,9 @@ public class DefaultLinkLabelGeneratorTest extends MockObjectTestCase
         this.mockAccessBridge.stubs().method("getDocument").will(returnValue(this.mockModelBridge.proxy()));
         assertEquals("[xwiki:Main.HelloWorld] Hello World (My title)", this.generator.generate(link));
     }
-    
+
     public void testGenerateWhenDocumentFailsToLoad()
     {
-        Link link = new Link();
-        link.setReference("HelloWorld");
-
         this.mockAccessBridge.stubs().method("getDocument").will(throwException(new Exception("error")));
 
         assertEquals("HelloWorld", this.generator.generate(new Link()));
@@ -61,5 +64,15 @@ public class DefaultLinkLabelGeneratorTest extends MockObjectTestCase
         this.mockAccessBridge.stubs().method("getDocument").will(returnValue(this.mockModelBridge.proxy()));
 
         assertEquals("HelloWorld", this.generator.generate(new Link()));
+    }
+
+    public void testGenerateWhithRegexpSyntax()
+    {
+        this.mockModelBridge.stubs().method("getTitle").will(returnValue("$0"));
+        this.mockAccessBridge.stubs().method("getDocument").will(returnValue(this.mockModelBridge.proxy()));
+
+        this.mockAccessBridge.stubs().method("getDocumentName").will(returnValue(new DocumentName("$0", "\\", "$0")));
+
+        assertEquals("[$0:\\.$0] $0 ($0)", this.generator.generate(new Link()));
     }
 }

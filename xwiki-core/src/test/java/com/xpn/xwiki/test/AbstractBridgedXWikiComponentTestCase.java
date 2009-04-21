35757c2d686d6963afd21033112371094d808c54
tmortagne <thomas.mortagne@xwiki.com>
2009-04-21 14:35:37 +0000

    XWIKI-3563: Allow setting the default rendering syntax wiki by wiki * fix tests
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/branches/xwiki-core-1.8@18909 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-core/src/test/java/com/xpn/xwiki/test/AbstractBridgedXWikiComponentTestCase.java
+++ xwiki-core/src/test/java/com/xpn/xwiki/test/AbstractBridgedXWikiComponentTestCase.java
@@ -29,9 +29,8 @@ import com.xpn.xwiki.XWikiContext;
 import com.xpn.xwiki.web.Utils;
 
 /**
- * Extension of {@link org.xwiki.test.AbstractXWikiComponentTestCase} that sets up a bridge between the new
- * Execution Context and the old XWikiContext. This allows code that uses XWikiContext to be tested using this Test Case
- * class.
+ * Extension of {@link org.xwiki.test.AbstractXWikiComponentTestCase} that sets up a bridge between the new Execution
+ * Context and the old XWikiContext. This allows code that uses XWikiContext to be tested using this Test Case class.
  * 
  * @version $Id: AbstractBridgedXWikiComponentTestCase.java 11544 2008-07-29 14:43:19Z amelentev $
  * @since 1.6M1
@@ -46,7 +45,7 @@ public abstract class AbstractBridgedXWikiComponentTestCase extends AbstractXWik
         super.setUp();
 
         this.context = new XWikiContext();
-        
+
         this.context.setDatabase("xwiki");
         this.context.setMainXWiki("xwiki");
 
@@ -58,12 +57,14 @@ public abstract class AbstractBridgedXWikiComponentTestCase extends AbstractXWik
         Utils.setComponentManager(getComponentManager());
 
         // Bridge with old XWiki Context, required for old code.
-        Execution execution = (Execution) getComponentManager().lookup(Execution.ROLE);
+        Execution execution = (Execution) getComponentManager().lookup(Execution.class);
         execution.getContext().setProperty("xwikicontext", this.context);
 
         // Set a simple application context, as some components fail to start without one.
-        Container c = (Container) getComponentManager().lookup(Container.ROLE);
+        Container c = (Container) getComponentManager().lookup(Container.class);
         c.setApplicationContext(new TestApplicationContext());
+
+        getComponentManager().registerComponent(TestCoreConfiguration.getComponentDescriptor());
     }
 
     @Override

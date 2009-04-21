f98c4cc3ca4170713cb9b755f00eeb7a9bcbfe98
mflorea <mariusdumitru.florea@xwiki.com>
2009-04-21 16:42:26 +0000

    XWIKI-3442: Review the font plugin * Font family and font size can be changed, but they are not yet detected
    Merged from trunk, revision 18916.
    
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/web/branches/xwiki-web-1.8@18917 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-platform-web/wysiwyg/src/main/java/com/xpn/xwiki/wysiwyg/client/plugin/font/FontPlugin.java
+++ xwiki-platform-web/wysiwyg/src/main/java/com/xpn/xwiki/wysiwyg/client/plugin/font/FontPlugin.java
@@ -19,8 +19,13 @@
  */
 package com.xpn.xwiki.wysiwyg.client.plugin.font;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.xwiki.gwt.dom.client.Style;
+
 import com.google.gwt.user.client.ui.ChangeListener;
-import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.FocusWidget;
 import com.google.gwt.user.client.ui.Widget;
 import com.xpn.xwiki.wysiwyg.client.Wysiwyg;
 import com.xpn.xwiki.wysiwyg.client.editor.Strings;
@@ -29,6 +34,7 @@ import com.xpn.xwiki.wysiwyg.client.plugin.internal.FocusWidgetUIExtension;
 import com.xpn.xwiki.wysiwyg.client.util.Config;
 import com.xpn.xwiki.wysiwyg.client.widget.rta.RichTextArea;
 import com.xpn.xwiki.wysiwyg.client.widget.rta.cmd.Command;
+import com.xpn.xwiki.wysiwyg.client.widget.rta.cmd.internal.InlineStyleExecutable;
 
 /**
  * Plug-in for manipulating the font size and font family used while editing.
@@ -38,47 +44,14 @@ import com.xpn.xwiki.wysiwyg.client.widget.rta.cmd.Command;
 public class FontPlugin extends AbstractStatefulPlugin implements ChangeListener
 {
     /**
-     * The list of known font families.
+     * The association between pickers and the commands that are executed on change events.
      */
-    public static final FontFamily[] FAMILIES;
-
-    /**
-     * The list box used for changing the font family. The selected option in the list corresponds to the font family of
-     * the current selection.
-     */
-    private ListBox families;
-
-    /**
-     * The list box used for changing the font size. The selected option in the list corresponds to the font size of the
-     * current selection.
-     */
-    private ListBox sizes;
+    private final Map<Picker, Command> pickers = new HashMap<Picker, Command>();
 
     /**
      * User interface extension for the editor tool bar.
      */
     private final FocusWidgetUIExtension toolBarExtension = new FocusWidgetUIExtension("toolbar");
-
-    static {
-        FAMILIES =
-            new FontFamily[] {new FontFamily("Andale Mono", "andale mono,times"),
-                new FontFamily("Arial", "arial,helvetica,sans-serif"),
-                new FontFamily("Arial Black", "arial black,avant garde"),
-                new FontFamily("Book Antiqua", "book antiqua,palatino"),
-                new FontFamily("Comic Sans MS", "comic sans ms,sand"),
-                new FontFamily("Courier New", "courier new,courier"),
-                new FontFamily("Georgia", "georgia,palatino"),
-                new FontFamily("Helvetica", "helvetica"),
-                new FontFamily("Impact", "impact,chicago"),
-                new FontFamily("Symbol", "symbol"),
-                new FontFamily("Tahoma", "tahoma,arial,helvetica,sans-serif"),
-                new FontFamily("Terminal", "terminal,monaco"),
-                new FontFamily("Times New Roman", "times new roman,times"),
-                new FontFamily("Trebuchet MS", "trebuchet ms,geneva"),
-                new FontFamily("Verdana", "verdana,geneva"),
-                new FontFamily("Webdings", "webdings"),
-                new FontFamily("Wingdings", "wingdings,zapf dingbats")};
-    }
 
     /**
      * {@inheritDoc}
@@ -89,35 +62,14 @@ public class FontPlugin extends AbstractStatefulPlugin implements ChangeListener
     {
         super.init(wysiwyg, textArea, config);
 
-        if (getTextArea().getCommandManager().isSupported(Command.FONT_NAME)) {
-            families = new ListBox(false);
-            families.addChangeListener(this);
-            families.setVisibleItemCount(1);
-            families.setTitle(Strings.INSTANCE.font());
+        // Register custom executables.
+        getTextArea().getCommandManager().registerCommand(Command.FONT_NAME,
+            new InlineStyleExecutable(Style.FONT_FAMILY));
+        getTextArea().getCommandManager()
+            .registerCommand(Command.FONT_SIZE, new InlineStyleExecutable(Style.FONT_SIZE));
 
-            for (int i = 0; i < FAMILIES.length; i++) {
-                families.addItem(FAMILIES[i].getName());
-            }
-
-            toolBarExtension.addFeature("fontname", families);
-        }
-
-        if (getTextArea().getCommandManager().isSupported(Command.FONT_SIZE)) {
-            sizes = new ListBox(false);
-            sizes.addChangeListener(this);
-            sizes.setVisibleItemCount(1);
-            sizes.setTitle(Strings.INSTANCE.fontSize());
-
-            sizes.addItem("8pt");
-            sizes.addItem("10pt");
-            sizes.addItem("12pt");
-            sizes.addItem("14pt");
-            sizes.addItem("18pt");
-            sizes.addItem("24pt");
-            sizes.addItem("36pt");
-
-            toolBarExtension.addFeature("fontsize", sizes);
-        }
+        addFontNameFeature();
+        addFontSizeFeature();
 
         if (toolBarExtension.getFeatures().length > 0) {
             getTextArea().addMouseListener(this);
@@ -128,23 +80,73 @@ public class FontPlugin extends AbstractStatefulPlugin implements ChangeListener
     }
 
     /**
+     * Makes the font name feature available to be used on the tool bar.
+     */
+    private void addFontNameFeature()
+    {
+        if (getTextArea().getCommandManager().isSupported(Command.FONT_NAME)) {
+            FontFamilyPicker picker = new FontFamilyPicker();
+            picker.setTitle(Strings.INSTANCE.font());
+            picker.addChangeListener(this);
+
+            picker.addItem("andale mono");
+            picker.addItem("arial");
+            picker.addItem("arial black");
+            picker.addItem("book antiqua");
+            picker.addItem("comic sans ms");
+            picker.addItem("courier new");
+            picker.addItem("georgia");
+            picker.addItem("helvetica");
+            picker.addItem("impact");
+            picker.addItem("symbol");
+            picker.addItem("tahoma");
+            picker.addItem("terminal");
+            picker.addItem("times new roman");
+            picker.addItem("trebuchet ms");
+            picker.addItem("verdana");
+            picker.addItem("webdings");
+            picker.addItem("wingdings");
+
+            toolBarExtension.addFeature("fontname", picker);
+            pickers.put(picker, Command.FONT_NAME);
+        }
+    }
+
+    /**
+     * Makes the font size feature available to be used on the tool bar.
+     */
+    private void addFontSizeFeature()
+    {
+        if (getTextArea().getCommandManager().isSupported(Command.FONT_SIZE)) {
+            FontSizePicker picker = new FontSizePicker();
+            picker.setTitle(Strings.INSTANCE.fontSize());
+            picker.addChangeListener(this);
+
+            picker.addItem("8pt");
+            picker.addItem("10pt");
+            picker.addItem("12pt");
+            picker.addItem("14pt");
+            picker.addItem("18pt");
+            picker.addItem("24pt");
+            picker.addItem("36pt");
+
+            toolBarExtension.addFeature("fontsize", picker);
+            pickers.put(picker, Command.FONT_SIZE);
+        }
+    }
+
+    /**
      * {@inheritDoc}
      * 
      * @see AbstractStatefulPlugin#destroy()
      */
     public void destroy()
     {
-        if (families != null) {
-            families.removeFromParent();
-            families.removeChangeListener(this);
-            families = null;
+        for (Picker picker : pickers.keySet()) {
+            ((Widget) picker).removeFromParent();
+            picker.removeChangeListener(this);
         }
-
-        if (sizes != null) {
-            sizes.removeFromParent();
-            sizes.removeChangeListener(this);
-            sizes = null;
-        }
+        pickers.clear();
 
         if (toolBarExtension.getFeatures().length > 0) {
             getTextArea().removeMouseListener(this);
@@ -163,32 +165,9 @@ public class FontPlugin extends AbstractStatefulPlugin implements ChangeListener
      */
     public void onChange(Widget sender)
     {
-        if (sender == families) {
-            onFamily();
-        } else if (sender == sizes) {
-            onSize();
-        }
-    }
-
-    /**
-     * Changes the font family for the current selection.
-     */
-    public void onFamily()
-    {
-        if (families.isEnabled()) {
-            String selectedFamily = FAMILIES[families.getSelectedIndex()].getStringValue();
-            getTextArea().getCommandManager().execute(Command.FONT_NAME, selectedFamily);
-        }
-    }
-
-    /**
-     * Changes the font size for the current selection.
-     */
-    public void onSize()
-    {
-        if (sizes.isEnabled()) {
-            int size = sizes.getSelectedIndex() + 1;
-            getTextArea().getCommandManager().execute(Command.FONT_SIZE, size);
+        Command command = pickers.get(sender);
+        if (command != null && ((FocusWidget) sender).isEnabled()) {
+            getTextArea().getCommandManager().execute(command, ((Picker) sender).getSelectedValue());
         }
     }
 
@@ -199,55 +178,8 @@ public class FontPlugin extends AbstractStatefulPlugin implements ChangeListener
      */
     public void update()
     {
-        if (families != null) {
-            String family = getTextArea().getCommandManager().getStringValue(Command.FONT_NAME);
-            int index = indexOfFamily(family);
-            if (index < 0) {
-                // We cannot detect the default font name (before font name command is executed) so we suppose
-                // it is "sans-serif". This value should match the one present in the RichTextArea.css file or the
-                // browser's default value.
-                families.setSelectedIndex(1);
-            } else {
-                families.setSelectedIndex(index);
-            }
-        }
-
-        if (sizes != null) {
-            Integer size = getTextArea().getCommandManager().getIntegerValue(Command.FONT_SIZE);
-            if (size != null && size.intValue() > 0 && size.intValue() < 8) {
-                sizes.setSelectedIndex(size.intValue() - 1);
-            } else {
-                // We cannot detect the default font size (before font size command is executed) so we suppose
-                // it is 3 (which corresponds to 12pt). This value should match the one present in the RichTextArea.css
-                // file or the browser's default value.
-                sizes.setSelectedIndex(2);
-            }
-        }
-    }
-
-    /**
-     * @param family a font family, as the value of the CSS {@code font-family} property
-     * @return the index of the {@link #FAMILIES} entry associated with the given font family
-     */
-    private static int indexOfFamily(String family)
-    {
-        if (family == null) {
-            return -1;
-        }
-        String[] alt = family.trim().toLowerCase().split("\\s*,\\s*");
-        int index = 0;
-        int max = FAMILIES[0].match(alt);
-        for (int i = 1; i < FAMILIES.length; i++) {
-            int match = FAMILIES[i].match(alt);
-            if (match > max) {
-                max = match;
-                index = i;
-            }
-        }
-        if (max > 0) {
-            return index;
-        } else {
-            return -1;
+        for (Map.Entry<Picker, Command> entry : pickers.entrySet()) {
+            entry.getKey().setSelectedValue(getTextArea().getCommandManager().getStringValue(entry.getValue()));
         }
     }
 }

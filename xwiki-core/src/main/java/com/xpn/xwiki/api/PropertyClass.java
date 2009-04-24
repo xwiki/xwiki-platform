696e5b6ec1a6f72548ed7447d8cd01f257477711
sdumitriu <sergiu@xwiki.com>
2009-04-24 08:45:59 +0000

    XWIKI-3584: getListValues and getMapValues in PropertyClass api Done. Patch submitted by Ludovic Dubost, applied with cleanup. Merged from trunk@19010
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/branches/xwiki-core-1.8@19011 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-core/src/main/java/com/xpn/xwiki/api/PropertyClass.java
+++ xwiki-core/src/main/java/com/xpn/xwiki/api/PropertyClass.java
@@ -21,11 +21,18 @@
 
 package com.xpn.xwiki.api;
 
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+
 import com.xpn.xwiki.XWikiContext;
+import com.xpn.xwiki.objects.classes.ListClass;
+import com.xpn.xwiki.objects.classes.ListItem;
+import com.xpn.xwiki.util.Programming;
 
 public class PropertyClass extends Collection
 {
-
     public PropertyClass(com.xpn.xwiki.objects.classes.PropertyClass property,
         XWikiContext context)
     {
@@ -81,4 +88,41 @@ public class PropertyClass extends Collection
         return getBasePropertyClass().getTooltip();
     }
 
+    /**
+     * If the property is a {@link ListClass}, returns the possible values. These are the internal values (keys), and
+     * not the user-friendly or translated values that would be displayed to the user.
+     * 
+     * @return the list of possible ({@code String}) values
+     * @see #getMapValues() {@code getMapValues()} returns both the keys and their user-friendly displayed values
+     **/
+    public List<String> getListValues()
+    {
+        com.xpn.xwiki.objects.classes.PropertyClass pclass = getBasePropertyClass();
+        if (pclass instanceof ListClass) {
+            return ((ListClass) pclass).getList(this.context);
+        } else {
+            // Although we prefer to return empty lists from API methods, here returning any kind of list doesn't make
+            // sense, since the property does not have a list of possible values at all (like, a number property).
+            return null;
+        }
+    }
+
+    /**
+     * If the property is a {@link ListClass}, returns the possible values as a map {@code internal key <-> displayed
+     * value}.
+     * 
+     * @return the map of possible ({@code String}) values and their associated ({@code ListItem}) displayed values
+     * @see #getListValues() {@code getListValues()} returns only the list of possible internal keys
+     **/
+    public Map<String, ListItem> getMapValues()
+    {
+        com.xpn.xwiki.objects.classes.PropertyClass pclass = getBasePropertyClass();
+        if (pclass instanceof ListClass) {
+            return ((ListClass) pclass).getMap(this.context);
+        } else {
+            // Although we prefer to return empty maps from API methods, here returning any kind of map doesn't make
+            // sense, since the property does not have a list of possible values at all (like, a number property).
+            return null;
+        }
+    }
 }

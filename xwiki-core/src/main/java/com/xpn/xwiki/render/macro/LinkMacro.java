2ed9ed9688b68bd431ec21a7c747917a4cae5dbd
sdumitriu <sergiu@xwiki.com>
2009-04-27 18:25:17 +0000

    XWIKI-1481: Document the {link} macro Done (javadoc).
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@19147 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-core/src/main/java/com/xpn/xwiki/render/macro/LinkMacro.java
+++ xwiki-core/src/main/java/com/xpn/xwiki/render/macro/LinkMacro.java
@@ -19,8 +19,10 @@
  *
  */
 
-
 package com.xpn.xwiki.render.macro;
+
+import java.io.IOException;
+import java.io.Writer;
 
 import org.radeox.api.engine.ImageRenderEngine;
 import org.radeox.api.engine.RenderEngine;
@@ -29,55 +31,83 @@ import org.radeox.macro.BaseLocaleMacro;
 import org.radeox.macro.parameter.MacroParameter;
 import org.radeox.util.Encoder;
 
-import java.io.IOException;
-import java.io.Writer;
-
-import com.xpn.xwiki.XWikiContext;
 import com.xpn.xwiki.XWiki;
+import com.xpn.xwiki.XWikiContext;
 
-public class LinkMacro extends BaseLocaleMacro {
-  public String getLocaleKey() {
-    return "macro.link";
-  }
-
-  public void execute(Writer writer, MacroParameter params)
-      throws IllegalArgumentException, IOException {
-
-    RenderContext context = params.getContext();
-    RenderEngine engine = context.getRenderEngine();
-
-    String text = params.get("text", 0);
-    String url = params.get("url", 1);
-    String img = params.get("img", 2);
-
-    // check for single url argument (text == url)
-    if(params.getLength() == 1) {
-      url = text;
-      text = Encoder.toEntity(text.charAt(0)) + Encoder.escape(text.substring(1));
+/**
+ * Radeox macro for the xwiki 1.0 syntax, offering linking capabilities. Syntax: <tt>{link:text|url|image}</tt>
+ * <dl>
+ * <dt>text</dt>
+ * <dd>The link text.</dd>
+ * <dt>url</dt>
+ * <dd>The target address. If missing, it is the same as the <tt>text</tt></dd>
+ * <dt>img</dt>
+ * <dd>An image to print before the URL. Actually not used, this parameter is always ignored.</dd>
+ * </dl>
+ * 
+ * @version $Id$
+ */
+public class LinkMacro extends BaseLocaleMacro
+{
+    /**
+     * The name of the macro.
+     * 
+     * @see org.radeox.macro.BaseLocaleMacro#getLocaleKey()
+     */
+    public String getLocaleKey()
+    {
+        return "macro.link";
     }
 
-    if (url != null && text != null) {
-      writer.write("<span class=\"nobr\">");
-      if (!"none".equals(img) && engine instanceof ImageRenderEngine) {
-        writer.write(((ImageRenderEngine) engine).getExternalImageLink());
-      }
+    /**
+     * Main macro execution method, replaces the macro instance with the generated output.
+     * 
+     * @param writer the place where to write the output
+     * @param params the parameters this macro is called with
+     * @throws IllegalArgumentException if the mandatory argument ({@code text}) is missing
+     * @throws IOException if the output cannot be written
+     * @see org.radeox.macro.BaseMacro#execute(Writer, MacroParameter)
+     */
+    @Override
+    public void execute(Writer writer, MacroParameter params) throws IllegalArgumentException, IOException
+    {
+        RenderContext context = params.getContext();
+        RenderEngine engine = context.getRenderEngine();
 
-      // A URL should not have starting and trailing white spaces
-      url = url.trim();
+        String text = params.get("text", 0);
+        String url = params.get("url", 1);
+        String img = params.get("img", 2);
 
-      writer.write("<a href=\"");
-      writer.write(Encoder.escape(url));
-      XWikiContext xcontext = (XWikiContext)params.getContext().get("xcontext");
-      XWiki xwiki = xcontext.getWiki();
-      String defaulttarget = xwiki.Param("xwiki.render.externallinks.defaulttarget", "");
-      if (!defaulttarget.equals(""))
-            writer.write(" target=\"" + defaulttarget + "\"");
-      writer.write("\">");
-      writer.write(text);
-      writer.write("</a></span>");
-    } else {
-      throw new IllegalArgumentException("link needs a name and a url as argument");
+        // Check for single url argument (text == url)
+        if (params.getLength() == 1) {
+            url = text;
+            // Since this is an URL, escape the first character in order to prevent starting the linkfilter.
+            text = Encoder.toEntity(text.charAt(0)) + Encoder.escape(text.substring(1));
+        }
+
+        if (url != null && text != null) {
+            writer.write("<span class=\"nobr\">");
+            if (!"none".equals(img) && engine instanceof ImageRenderEngine) {
+                writer.write(((ImageRenderEngine) engine).getExternalImageLink());
+            }
+
+            // A URL should not have starting and trailing white spaces
+            url = url.trim();
+
+            writer.write("<a href=\"");
+            writer.write(Encoder.escape(url));
+            XWikiContext xcontext = (XWikiContext) params.getContext().get("xcontext");
+            XWiki xwiki = xcontext.getWiki();
+            String defaulttarget = xwiki.Param("xwiki.render.externallinks.defaulttarget", "");
+            if (!defaulttarget.equals("")) {
+                writer.write(" target=\"" + defaulttarget + "\"");
+            }
+            writer.write("\">");
+            writer.write(text);
+            writer.write("</a></span>");
+        } else {
+            throw new IllegalArgumentException("link needs a name and a url as argument");
+        }
+        return;
     }
-    return;
-  }
 }

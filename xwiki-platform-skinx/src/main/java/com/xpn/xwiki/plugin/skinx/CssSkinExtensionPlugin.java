0be7cb36fcafea9a4693c2fdb03d2243433a38f9
sdumitriu <sergiu@xwiki.com>
2009-05-03 21:37:56 +0000

    XSKINX-24: Extension XClasses are not created anymore in virtual wikis Fixed.
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/xwiki-plugins/trunk/skinx@19304 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-platform-skinx/src/main/java/com/xpn/xwiki/plugin/skinx/CssSkinExtensionPlugin.java
+++ xwiki-platform-skinx/src/main/java/com/xpn/xwiki/plugin/skinx/CssSkinExtensionPlugin.java
@@ -1,8 +1,5 @@
 package com.xpn.xwiki.plugin.skinx;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 import com.xpn.xwiki.XWikiContext;
 
 /**
@@ -22,9 +19,6 @@ public class CssSkinExtensionPlugin extends AbstractDocumentSkinExtensionPlugin
      */
     public static final String PLUGIN_NAME = "ssx";
 
-    /** Log helper for logging messages in this class. */
-    private static final Log LOG = LogFactory.getLog(CssSkinExtensionPlugin.class);
-
     /**
      * XWiki plugin constructor.
      * 
@@ -36,7 +30,21 @@ public class CssSkinExtensionPlugin extends AbstractDocumentSkinExtensionPlugin
     public CssSkinExtensionPlugin(String name, String className, XWikiContext context)
     {
         super(PLUGIN_NAME, className, context);
-        init(context);
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * We must override this method since the plugin manager only calls it for classes that provide their own
+     * implementation, and not an inherited one.
+     * </p>
+     * 
+     * @see com.xpn.xwiki.plugin.XWikiPluginInterface#virtualInit(com.xpn.xwiki.XWikiContext)
+     */
+    @Override
+    public void virtualInit(XWikiContext context)
+    {
+        super.virtualInit(context);
     }
 
     /**
@@ -49,8 +57,7 @@ public class CssSkinExtensionPlugin extends AbstractDocumentSkinExtensionPlugin
     {
         return "<link rel='stylesheet' type='text/css' href='"
             + context.getWiki().getURL(documentName, PLUGIN_NAME,
-                "language=" + context.getLanguage() + parametersAsQueryString(documentName, context), context)
-            + "'/>";
+                "language=" + context.getLanguage() + parametersAsQueryString(documentName, context), context) + "'/>";
     }
 
     /**

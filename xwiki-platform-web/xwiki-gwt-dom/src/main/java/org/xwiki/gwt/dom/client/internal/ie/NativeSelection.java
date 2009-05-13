d0b15cf260d0cc19b52fc2f031ae7091cb4991ce
mflorea <mariusdumitru.florea@xwiki.com>
2009-05-13 13:48:10 +0000

    XWIKI-3760: When the caret is at the end of paragraph in IE style is applied to the next paragraph XWIKI-3778: Adding a link / image / macro in an empty list item in IE moves it to the beginning of the next item XWIKI-3776: Cannot add a link at the end of a list item in Internet Explorer XWIKI-3646: Adding columns to the correct location * All have been fixed by changing the way the selection is saved and restored in IE.
    Merged from trunk, revision 19893.
    
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/web/branches/xwiki-web-1.9@19894 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-platform-web/xwiki-gwt-dom/src/main/java/org/xwiki/gwt/dom/client/internal/ie/NativeSelection.java
+++ xwiki-platform-web/xwiki-gwt-dom/src/main/java/org/xwiki/gwt/dom/client/internal/ie/NativeSelection.java
@@ -65,16 +65,17 @@ public final class NativeSelection extends JavaScriptObject
             // inner selection. For instance, by using the scroll bars we move the focus from the BODY element to the
             // HTML element but the BODY element keeps its inner selection. In consequence, we don't have to restore
             // the selection if the focus comes from the HTML element.
-            if (event.fromElement == document.documentElement) {
-                return;
+            if (event.fromElement != document.documentElement) {
+                var range = document.parentWindow.__savedRange;
+                // Clear the reference to the saved range.
+                document.parentWindow.__savedRange = undefined;
+                try {
+                    // Restore the saved range.
+                    range.select();
+                } catch (e) {
+                    // ignore
+                }
             }
-            var range = 
-    @org.xwiki.gwt.dom.client.internal.ie.NativeSelection::createRange(Lorg/xwiki/gwt/dom/client/Document;)(document);
-            // Reset the bookmark to prevent redundant calls to this function.
-            // NOTE: We don't use "null" but "undefined" to reset the bookmark because typeof(bookmark) returns
-            // "object" when the bookmark is null but defined (previously had a non null value).
-            document.body.__bookmark = undefined;
-            range.select();
         });
 
         // Save the selection when the edited document is about to loose focus.
@@ -83,33 +84,11 @@ public final class NativeSelection extends JavaScriptObject
             // inner selection. For instance, by using the scroll bars we move the focus from the BODY element to the
             // HTML element but the BODY element keeps its inner selection. In consequence, we don't have to save the
             // selection if the focus goes to the HTML element.
-            if (event.toElement == document.documentElement) {
-                return;
-            }
-            document.body.__bookmark = undefined;
-            var range = document.selection.createRange();
-            // Check the type of the range and if the range is inside the edited document.
-            if (range.getBookmark && range.parentElement().ownerDocument == document) {
-                // Text range.
-                document.body.__bookmark = range.getBookmark();
-            } else if (range.item && range.length > 0 && range.item(0).ownerDocument == document) {
-                // Control range.
-                document.body.__bookmark = range.item(0);
+            if (event.toElement != document.documentElement) {
+                document.parentWindow.__savedRange = document.selection.createRange();
             }
         });
     }-*/;
-
-    /**
-     * This is a utility method needed in order to create a range in a static environment, because
-     * "only static references to overlay types are allowed from JSNI".
-     * 
-     * @param document a DOM document for which to retrieve the current range
-     * @return the current range of the given document
-     */
-    public static NativeRange createRange(Document document)
-    {
-        return getInstance(document).createRange();
-    }
 
     /**
      * Creates a TextRange object from the current text selection, or a ControlRange object from a control selection.
@@ -125,47 +104,31 @@ public final class NativeSelection extends JavaScriptObject
         // to create range return the range from the focused document. In order to overcome this severe limitation of
         // Internet Explorer we have to save the selection whenever a document looses focus and restore it when it gains
         // it back. Additionally, we have to be able to work with a document's selection while it doesn't have the
-        // focus. To do so, we save the selection in a book mark stored as an attribute of that document's body. While
-        // the document doesn't have the focus we read and overwrite this book mark, instead of using the selection
-        // object.
-        var bookmark = this.ownerDocument.body.__bookmark;
-        switch (typeof(bookmark)) {
-            case 'undefined':
-                // There's no saved range. Let's use the selection object to create a new range.
-                var range = this.createRange();
-                range.ownerDocument = this.ownerDocument;
-                return range;
-            case 'string':
-                // There's a saved text range. The bookmark is an opaque string that can be used with moveToBookmark to
-                // recreate the original text range.
-                var textRange = this.ownerDocument.body.createTextRange();
-                textRange.ownerDocument = this.ownerDocument;
-                try {
-                    textRange.moveToBookmark(bookmark);
-                } catch(e) {
-                    // The document could have been changed in the mean time, leaving the bookmark invalid. Fallback on
-                    // a default text range.
-                    textRange.moveToElementText(this.ownerDocument.body);
-                    textRange.collapse();
+        // focus. To do so, we save a reference to the current range as a property of the window object. While the
+        // document doesn't have the focus we read and overwrite this range, instead of using the selection object.
+        var range;
+        var savedRange = this.ownerDocument.parentWindow.__savedRange;
+        if (typeof(savedRange) == 'undefined') {
+            // There's no saved range. Let's create a new range.
+            range = this.createRange();
+        } else {
+            // There's a saved range. Return a clone of the saved range. 
+            if (savedRange.duplicate) {
+                // Text Range
+                range = savedRange.duplicate();
+            } else if (savedRange.item) {
+                // Control Range
+                range = this.ownerDocument.body.createControlRange();
+                for(var i = 0; i < savedRange.length; i++) {
+                    range.addElement(savedRange.item(i));
                 }
-                return textRange;
-            case 'object':
-                // There's a saved control range. The bookmark is a reference to the element previously selected.
-                var controlRange = this.ownerDocument.body.createControlRange();
-                try {
-                    controlRange.addElement(bookmark);
-                } catch(e) {
-                    // The element could have been detached in the mean time. Fallback on a default text range.
-                    controlRange = this.ownerDocument.body.createTextRange();
-                    controlRange.moveToElementText(this.ownerDocument.body);
-                    controlRange.collapse();
-                }
-                controlRange.ownerDocument = this.ownerDocument;
-                return controlRange;
-            default:
-                // There seems to be a saved range, but the type of the bookmark is not supported.
-                throw 'NativeSelection#createRange: Unsupported bookmark.';
+            } else {
+                // The saved range seems to be invalid.
+                throw 'NativeSelection#createRange: Invalid saved range.';
+            }
         }
+        range.ownerDocument = this.ownerDocument;
+        return range;
     }-*/;
 
     /**
@@ -173,11 +136,11 @@ public final class NativeSelection extends JavaScriptObject
      */
     public native void empty()
     /*-{
-        if (typeof(this.ownerDocument.body.__bookmark) == 'undefined') {
+        if (typeof(this.ownerDocument.parentWindow.__savedRange) == 'undefined') {
             this.empty();
         } else {
-            // Set the bookmark to an unsupported value.
-            this.ownerDocument.body.__bookmark = false;
+            // Save an invalid range.
+            this.ownerDocument.parentWindow.__savedRange = {select : function(){}};
         }
     }-*/;
 

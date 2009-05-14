65a5c80f23083442cca213581d21e70106312ab2
asiri <asiri@xwiki.com>
2009-05-14 08:32:39 +0000

    [mics] moved filter components inside internal package.
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@19936 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-officeimporter/src/main/java/org/xwiki/officeimporter/internal/filter/LineBreakFilter.java
+++ xwiki-officeimporter/src/main/java/org/xwiki/officeimporter/internal/filter/LineBreakFilter.java
@@ -17,7 +17,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.xwiki.officeimporter.filter;
+package org.xwiki.officeimporter.internal.filter;
 
 import java.util.Arrays;
 import java.util.List;

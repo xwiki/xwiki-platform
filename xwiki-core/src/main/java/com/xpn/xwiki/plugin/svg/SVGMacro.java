d8a1a0e6d6953236c6eb2b671d46ff98ca9b0b82
sdumitriu <sergiu@xwiki.com>
2009-05-15 02:33:43 +0000

    [cleanup] Apply codestyle, document, reduce exceptions
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@19995 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-core/src/main/java/com/xpn/xwiki/plugin/svg/SVGMacro.java
+++ xwiki-core/src/main/java/com/xpn/xwiki/plugin/svg/SVGMacro.java
@@ -21,65 +21,114 @@
 
 package com.xpn.xwiki.plugin.svg;
 
-import com.xpn.xwiki.XWiki;
-import com.xpn.xwiki.XWikiContext;
-import com.xpn.xwiki.XWikiException;
-import com.xpn.xwiki.render.XWikiRadeoxRenderEngine;
+import java.io.IOException;
+import java.io.Writer;
+
+import org.apache.commons.lang.StringUtils;
 import org.radeox.api.engine.RenderEngine;
 import org.radeox.api.engine.context.RenderContext;
 import org.radeox.macro.BaseLocaleMacro;
 import org.radeox.macro.parameter.MacroParameter;
 
-import java.io.IOException;
-import java.io.Writer;
+import com.xpn.xwiki.XWiki;
+import com.xpn.xwiki.XWikiContext;
+import com.xpn.xwiki.XWikiException;
+import com.xpn.xwiki.render.XWikiRadeoxRenderEngine;
 
-public class SVGMacro  extends BaseLocaleMacro {
-  public String getLocaleKey() {
-    return "macro.svg";
-  }
+/**
+ * Radeox macro for the xwiki 1.0 syntax, converting SVG code into bitmap images. Syntax:
+ * <tt>{svg:alternate text|height|width} SVG content here {svg}</tt>
+ * <dl>
+ * <dt>text</dt>
+ * <dd>The alternate text for the image.</dd>
+ * <dt>height</dt>
+ * <dd>The height of the generated image. If missing or invalid, the default is 400.</dd>
+ * <dt>width</dt>
+ * <dd>The width of the generated image. If missing or invalid, the default is 400.</dd>
+ * </dl>
+ * <p>
+ * You can get the content from an attachment using Velocity, as in:
+ * </p>
+ * 
+ * <pre>
+ * {svg}
+ * $doc.getAttachment('image.svg').getContentAsString()
+ * {svg}
+ * </pre>
+ * <p>
+ * The macro relies on the {@link SVGPlugin} to actually transform the SVG content into an image.
+ * </p>
+ * 
+ * @deprecated The Radeox macros are deprecated in favor of the new wiki macros.
+ * @version $Id$
+ */
+@Deprecated
+public class SVGMacro extends BaseLocaleMacro
+{
+    /**
+     * The name of the macro.
+     * 
+     * @see org.radeox.macro.BaseLocaleMacro#getLocaleKey()
+     */
+    public String getLocaleKey()
+    {
+        return "macro.svg";
+    }
 
-    public void execute(Writer writer, MacroParameter params)
-            throws IllegalArgumentException, IOException {
-
+    /**
+     * Main macro execution method, replaces the macro instance with the generated output.
+     * 
+     * @param writer the place where to write the output
+     * @param params the parameters this macro is called with
+     * @throws IllegalArgumentException if the mandatory argument ({@code text}) is missing
+     * @throws IOException if the output cannot be written
+     * @see org.radeox.macro.BaseMacro#execute(Writer, MacroParameter)
+     */
+    @Override
+    public void execute(Writer writer, MacroParameter params) throws IllegalArgumentException, IOException
+    {
         RenderContext context = params.getContext();
         RenderEngine engine = context.getRenderEngine();
 
-        XWikiContext xcontext = ((XWikiRadeoxRenderEngine)engine).getXWikiContext();
+        XWikiContext xcontext = ((XWikiRadeoxRenderEngine) engine).getXWikiContext();
         XWiki xwiki = xcontext.getWiki();
 
         SVGPlugin plugin = (SVGPlugin) xwiki.getPlugin("svg", xcontext);
-        // If the plugin is not loaded
-        if (plugin==null) {
+        // If the SVG plugin is not loaded, exit.
+        if (plugin == null) {
             writer.write("Plugin not loaded");
             return;
         }
-        /* {svg:image|height|width} */
+        // {svg:alternate text|height|width}
         StringBuffer str = new StringBuffer();
         String text = params.get("text", 0);
         String height = params.get("height", 1);
-        if ((height==null)||("none".equals(height))) {
-        	height = "400";
+        if (StringUtils.isBlank(height) || "none".equals(height) || !StringUtils.isNumeric(height.trim())) {
+            height = "400";
         }
         String width = params.get("width", 2);
-        if ((width==null)||("none".equals(width))) {
-        	width = "400";
+        if (StringUtils.isBlank(width) || "none".equals(width) || !StringUtils.isNumeric(width.trim())) {
+            width = "400";
         }
         try {
-	        int intHeight = Integer.parseInt(height);
-	        int intWidth = Integer.parseInt(width);
-	        String svgtext = params.getContent();
-	        str.append("<img src=\"");
-	        str.append(plugin.getSVGImageURL(svgtext, intHeight, intWidth, xcontext));
-	        str.append("\" ");
-	        str.append("height=\"" + height + "\" ");
-	        str.append("width=\"" + width + "\" ");
-	        str.append("alt=\"");
-	        str.append(text);
-	        str.append("\" />");
-	        writer.write(str.toString());
-	    } catch (Throwable t) {
-	    	XWikiException e = new XWikiException(XWikiException.MODULE_XWIKI_PLUGINS, XWikiException.ERROR_XWIKI_UNKNOWN, "SVG Issue", t);
-	    	writer.write("Exception converting SVG: " + e.getFullMessage());
-	    }
+            int intHeight = Integer.parseInt(height.trim());
+            int intWidth = Integer.parseInt(width.trim());
+            String svgtext = StringUtils.trimToEmpty(params.getContent());
+            str.append("<img src=\"");
+            // The SVG plugin generates the image and returns an URL for accessing it.
+            str.append(plugin.getSVGImageURL(svgtext, intHeight, intWidth, xcontext));
+            str.append("\" ");
+            str.append("height=\"" + height + "\" ");
+            str.append("width=\"" + width + "\" ");
+            str.append("alt=\"");
+            str.append(text);
+            str.append("\" />");
+            writer.write(str.toString());
+        } catch (Throwable t) {
+            XWikiException e =
+                new XWikiException(XWikiException.MODULE_XWIKI_PLUGINS, XWikiException.ERROR_XWIKI_UNKNOWN,
+                    "SVG Issue", t);
+            writer.write("Exception converting SVG: " + e.getFullMessage());
+        }
     }
 }

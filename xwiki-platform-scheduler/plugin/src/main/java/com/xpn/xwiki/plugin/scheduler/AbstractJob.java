cc4235a0352635faad0c5ae53a6786d956a45bf0
tmortagne <thomas.mortagne@xwiki.com>
2009-05-19 19:15:51 +0000

    XASCH-29: It's not possible to call getRenderedContent() from the scheduler
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/xwiki-plugins/trunk/scheduler@20122 f329d543-caf0-0310-9063-dda96c69346f

---
+++ xwiki-platform-scheduler/plugin/src/main/java/com/xpn/xwiki/plugin/scheduler/AbstractJob.java
@@ -0,0 +1,76 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package com.xpn.xwiki.plugin.scheduler;
+
+import org.quartz.Job;
+import org.quartz.JobDataMap;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.xwiki.context.Execution;
+import org.xwiki.context.ExecutionContext;
+import org.xwiki.context.ExecutionContextException;
+import org.xwiki.context.ExecutionContextManager;
+
+import com.xpn.xwiki.XWikiContext;
+import com.xpn.xwiki.web.Utils;
+
+/**
+ * Base class for any XWiki Quartz Job. This class take care of initializing ExecutionContext properly.
+ * <p>
+ * A class extending {@link AbstractJob} should implements {@link #executeJob(JobExecutionContext)}.
+ * 
+ * @version $Id$
+ */
+public abstract class AbstractJob implements Job
+{
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.quartz.Job#execute(org.quartz.JobExecutionContext)
+     */
+    public final void execute(JobExecutionContext jobContext) throws JobExecutionException
+    {
+        JobDataMap data = jobContext.getJobDetail().getJobDataMap();
+
+        // The XWiki context was saved in the Job execution data map. Get it as we'll retrieve
+        // the script to execute from it.
+        XWikiContext xwikiContext = (XWikiContext) data.get("context");
+
+        // Init execution context
+        try {
+            ExecutionContextManager ecim = (ExecutionContextManager) Utils.getComponent(ExecutionContextManager.ROLE);
+            Execution execution = (Execution) Utils.getComponent(Execution.ROLE);
+
+            ExecutionContext ec = new ExecutionContext();
+            // Bridge with old XWiki Context, required for old code.
+            ec.setProperty("xwikicontext", xwikiContext);
+
+            ecim.initialize(ec);
+            execution.setContext(ec);
+        } catch (ExecutionContextException e) {
+            throw new JobExecutionException("Fail to initialize execution context", e);
+        }
+
+        // Execute the job
+        executeJob(jobContext);
+    }
+
+    protected abstract void executeJob(JobExecutionContext jobContext) throws JobExecutionException;
+}

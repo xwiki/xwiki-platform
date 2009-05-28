59761e0246ca7a560f729e866d28b897c52bcbe4
lucaa <lucaa@xwiki.com>
2009-05-28 16:49:11 +0000

    XWIKI-3517: Indenting multiple items at once in a list * Added support for indent and outdent * Added GWTUnit tests for indent and outdent selected items Merged from trunk (r20595).
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/web/branches/xwiki-web-1.9@20597 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-platform-web/wysiwyg/src/main/java/com/xpn/xwiki/wysiwyg/client/plugin/indent/exec/OutdentExecutable.java
+++ xwiki-platform-web/wysiwyg/src/main/java/com/xpn/xwiki/wysiwyg/client/plugin/indent/exec/OutdentExecutable.java
@@ -19,12 +19,14 @@
  */
 package com.xpn.xwiki.wysiwyg.client.plugin.indent.exec;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.xwiki.gwt.dom.client.DocumentFragment;
 import org.xwiki.gwt.dom.client.Element;
 import org.xwiki.gwt.dom.client.Range;
 
 import com.google.gwt.dom.client.Node;
-import com.xpn.xwiki.wysiwyg.client.widget.rta.RichTextArea;
 
 /**
  * Outdent executable to handle valid XHTML lists outdent, semantically: when a list item is outdented, all its subitems
@@ -41,23 +43,24 @@ public class OutdentExecutable extends AbstractListExecutable
 
     /**
      * {@inheritDoc}
-     * 
-     * @see AbstractListExecutable#execute(RichTextArea, String)
      */
-    public boolean execute(RichTextArea rta, String param)
+    @Override
+    protected boolean canExecute(Element listItem)
     {
-        // find enclosing list item
-        Element listItem = getListItem(rta);
-        if (listItem == null) {
+        if (!super.canExecute(listItem)) {
             return false;
         }
-        // look for the "parent" list
-        Element parentList = (Element) listItem.getParentNode();
-        if (parentList == null) {
-            return false;
-        }
+        return listItem.getParentNode() != null;
+    }
 
-        Range range = rta.getDocument().getSelection().getRangeAt(0);
+    /**
+     * {@inheritDoc} Moves the passed lust item one level up, as a next sibling of the list item it takes part of. If
+     * it's a first level list item, it is pulled out of its list.
+     */
+    @Override
+    protected void execute(Element listItem)
+    {
+        Element parentList = (Element) listItem.getParentNode();
         // check if this item is a first level item or inside a sublist
         Element parentListItem = (Element) parentList.getParentNode();
         if (parentListItem != null && parentListItem.getNodeName().equalsIgnoreCase(LIST_ITEM_TAG)) {
@@ -65,11 +68,71 @@ public class OutdentExecutable extends AbstractListExecutable
         } else {
             outdentFirstLevelItem(listItem, parentList);
         }
+    }
 
-        // restore selection
-        rta.getDocument().getSelection().removeAllRanges();
-        rta.getDocument().getSelection().addRange(range);
-        return true;
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected boolean executeOnMultipleItems(Range range, boolean perform)
+    {
+        // outdent all list items in the selection, whose ancestors are not to be outdented, and make sure a list item
+        // is not indented twice.
+        // List items in the selection are detected as the li ancestors of all leaves in the selection, the outdent is
+        // done on a given list item if it is not the descendant or self of the previously processed list item
+        // the previously processed item is updated each time the condition passes
+
+        // since the condition test result is altered by the actual outdent during the pass through the elements, we'll
+        // store all that need to be outdented in a list and outdent at the end
+        List<Element> toOutdent = new ArrayList<Element>();
+
+        // assume none can be outdented, and || on actual outdent results
+        boolean outdentResult = false;
+        // iterate through the leafs in the range
+        Node rangeLeaf = domUtils.getFirstLeaf(range);
+        Node lastLeaf = domUtils.getLastLeaf(range);
+        // store the last li on which an outent was executed, regardless of the actual result
+        Element lastProcessed = null;
+        // check the ancestor li to indent for each leaf in the range
+        while (rangeLeaf != null) {
+            Element currentLi = (Element) domUtils.getFirstAncestor(rangeLeaf, LIST_ITEM_TAG);
+            // check the condition
+            if (lastProcessed == null || (currentLi != null && !lastProcessed.isOrHasChild(currentLi))) {
+                boolean canOutdent = canExecute(currentLi);
+                if (canOutdent && perform) {
+                    toOutdent.add(currentLi);
+                }
+                outdentResult = canOutdent || outdentResult;
+                // update the last item attempted to be handled, regardless of the actual result of the indent function
+                lastProcessed = currentLi;
+            }
+            // go to next leaf
+            rangeLeaf = getNextLeafBefore(rangeLeaf, lastLeaf);
+        }
+        // actually execute the outdents
+        if (perform) {
+            for (Element listItem : toOutdent) {
+                execute(listItem);
+            }
+        }
+        return outdentResult;
+    }
+
+    /**
+     * Returns the next leaf before the passed leaf, including {@code lastLeaf}.
+     * 
+     * @param currentLeaf the leaf to get the following leaf of
+     * @param lastLeaf the last leaf to be returned
+     * @return the following leaf after {@code currentLeaf}, before (an including) {@code lastLeaf}, or {@code null} if
+     *         no more leaves exist to satisfy this condition
+     */
+    private Node getNextLeafBefore(Node currentLeaf, Node lastLeaf)
+    {
+        if (currentLeaf == lastLeaf) {
+            return null;
+        } else {
+            return domUtils.getNextLeaf(currentLeaf);
+        }
     }
 
     /**
@@ -81,35 +144,42 @@ public class OutdentExecutable extends AbstractListExecutable
      */
     public void outdentItem(Element listItem, Element parentList, Element parentListItem)
     {
-        // split the sublist in which this item is into 2 lists: one to stay in this parent, the other under outdented
-        // list item
-        Element newList = (Element) domUtils.splitNode(parentList, domUtils.getNodeIndex(listItem) + 1);
-        // and pull it out of its parent
-        parentListItem.removeChild(newList);
-        // put the newList as a sublist of the current item: append its elements to the list on the last position there,
-        // if any, or append it all as a child to the item.
-        Node lastChild = listItem.getLastChild();
-        if (newList.hasChildNodes()) {
-            if (isList(lastChild)) {
-                // append all items in the list to this item
-                for (int i = 0; i < newList.getChildNodes().getLength(); i++) {
-                    lastChild.appendChild(newList.getChildNodes().getItem(i));
-                }
-            } else {
-                // add the new list as is
-                listItem.appendChild(newList);
-            }
-        }
-        // move the list item as a next sibling of the parent list item
-        domUtils.insertAfter(listItem, parentListItem);
-        // check if the list in the parent needs to be removed because it is left empty
+        // split the parent list item in two lists, at the point where the listItem to outdent is
+        domUtils.splitNode(parentListItem.getParentNode(), parentList, domUtils.getNodeIndex(listItem));
+        Element newListItem = (Element) parentListItem.getNextSibling();
+        // if the split list is now empty, remove it
         if (!parentList.hasChildNodes()) {
-            // remove it from its parent
             // remove the line break first, if any, and then the sublist
             if (parentList.getPreviousSibling().getNodeName().equalsIgnoreCase(LINE_BREAK_TAG)) {
                 parentListItem.removeChild(parentList.getPreviousSibling());
             }
             parentListItem.removeChild(parentList);
+        }
+        // store the new parent list of the listItem to outdent
+        Element newParentList = (Element) listItem.getParentNode();
+        // now replace the newListItem in its parent, as a sibling of the parentListItem, with the list item
+        // we need to make sure the listItem is the one that stays in the tree to not invalidate listItem after the call
+        // of this function
+        newListItem.getParentNode().replaceChild(listItem, newListItem);
+        // get the subtree of the new List item created through split
+        DocumentFragment newListItemChildren = newListItem.extractContents();
+        // append the contents of the newListItem as children of the listItem...
+        listItem.appendChild(newListItemChildren);
+        // ... handling the merging or removal of the newParentList
+        if (!newParentList.hasChildNodes()) {
+            // if the split parent list of the list item to outdent has no more elements, remove it
+            newParentList.getParentNode().removeChild(newParentList);
+        } else {
+            // merge, if the new previous sibling is a list
+            Node previousNewListSibling = newParentList.getPreviousSibling();
+            if (previousNewListSibling != null && isList(previousNewListSibling)) {
+                // merge
+                while (newParentList.getFirstChild() != null) {
+                    previousNewListSibling.appendChild(newParentList.getFirstChild());
+                }
+                // and remove it since now it's empty
+                newParentList.getParentNode().removeChild(newParentList);
+            }
         }
     }
 
@@ -123,9 +193,9 @@ public class OutdentExecutable extends AbstractListExecutable
     protected void outdentFirstLevelItem(Element listItem, Element parentList)
     {
         // split the list in two, using the listItem as separator
-        Element newList = (Element) domUtils.splitNode(parentList, domUtils.getNodeIndex(listItem) + 1);
+        Element newList = (Element) domUtils.splitNode(parentList, domUtils.getNodeIndex(listItem));
         // remove the separator
-        parentList.removeChild(listItem);
+        newList.removeChild(listItem);
         // extract the list item and insert it after the parentList
         DocumentFragment extractedListItem = listItem.extractContents();
         domUtils.insertAfter(extractedListItem, parentList);
@@ -136,29 +206,17 @@ public class OutdentExecutable extends AbstractListExecutable
 
         if (!newList.hasChildNodes()) {
             newList.getParentNode().removeChild(newList);
+        } else {
+            // merge, if the new previous sibling is a list
+            Node previousNewListSibling = newList.getPreviousSibling();
+            if (previousNewListSibling != null && isList(previousNewListSibling)) {
+                // merge
+                while (newList.getFirstChild() != null) {
+                    previousNewListSibling.appendChild(newList.getFirstChild());
+                }
+                // and remove it since now it's empty
+                newList.getParentNode().removeChild(newList);
+            }
         }
-    }
-
-    /**
-     * {@inheritDoc}
-     * 
-     * @see AbstractListExecutable#isEnabled(RichTextArea)
-     */
-    public boolean isEnabled(RichTextArea rta)
-    {
-        if (!super.isEnabled(rta)) {
-            return false;
-        }
-        // find parent list item
-        Element listItem = getListItem(rta);
-        if (listItem == null) {
-            return false;
-        }
-        // look for the "parent" list item
-        Element parentList = (Element) listItem.getParentNode();
-        if (parentList == null) {
-            return false;
-        }
-        return true;
     }
 }

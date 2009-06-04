0a497381f521aa7fe31082fe4616a2eb725b3ec1
vmassol <vincent@massol.net>
2009-06-04 09:30:07 +0000

    Add new example
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@20786 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-rendering/xwiki-rendering-standalone/src/it/example/src/test/java/org/xwiki/rendering/example/ExampleTest.java
+++ xwiki-rendering/xwiki-rendering-standalone/src/it/example/src/test/java/org/xwiki/rendering/example/ExampleTest.java
@@ -21,12 +21,17 @@
 package org.xwiki.rendering.example;
 
 import java.io.StringReader;
+import java.util.Collections;
 
 import org.junit.Test;
 import org.junit.Assert;
 
 import org.xwiki.component.embed.EmbeddableComponentManager;
+import org.xwiki.rendering.block.Block;
+import org.xwiki.rendering.block.FormatBlock;
+import org.xwiki.rendering.block.LinkBlock;
 import org.xwiki.rendering.block.XDOM;
+import org.xwiki.rendering.listener.Format;
 import org.xwiki.rendering.parser.Parser;
 import org.xwiki.rendering.parser.Syntax;
 import org.xwiki.rendering.renderer.PrintRendererFactory;
@@ -35,6 +40,12 @@ import org.xwiki.rendering.renderer.printer.DefaultWikiPrinter;
 import org.xwiki.rendering.renderer.printer.WikiPrinter;
 import org.xwiki.rendering.transformation.TransformationManager;
 
+/**
+ * Examples of using the XWiki Rendering API standalone, using the Embedded Component Manager.
+ *  
+ * @version $Id$
+ * @since 2.0M1
+ */
 public class ExampleTest
 {
     @Test
@@ -54,11 +65,40 @@ public class ExampleTest
 
         // Generate HTML for example
         WikiPrinter printer = new DefaultWikiPrinter();
-        PrintRendererFactory rf = ecm.lookup(PrintRendererFactory.class);
-        Renderer htmlRenderer = rf.createRenderer(Syntax.XHTML_1_0, printer);
+        PrintRendererFactory prf = ecm.lookup(PrintRendererFactory.class);
+        Renderer htmlRenderer = prf.createRenderer(Syntax.XHTML_1_0, printer);
         
         xdom.traverse(htmlRenderer);
 
         Assert.assertEquals("<p>This is <strong>bold</strong></p>", printer.toString());
+    }
+    
+    @Test
+    public void makeAllLinksItalic() throws Exception
+    {
+        // Initialize Rendering components and allow getting instances
+        EmbeddableComponentManager ecm = new EmbeddableComponentManager(this.getClass().getClassLoader());
+        ecm.initialize();
+        
+        // Parse XWiki 2.0 Syntax
+        Parser parser = ecm.lookup(Parser.class, Syntax.XWIKI_2_0.toIdString());
+        XDOM xdom = parser.parse(new StringReader("This a [[link>MyPage]]"));
+        
+        // Find all links and make them italic
+        for (LinkBlock block : xdom.getChildrenByType(LinkBlock.class, true)) {
+            // Note: in XWiki Core 2.0 the API has been improved and there's no need to clone the
+            // block anymore and the replace() API has been modified.
+            Block newBlock = new FormatBlock(Collections.<Block>singletonList(block.clone()), Format.ITALIC);
+            block.replace(Collections.<Block>singletonList(newBlock));
+        }
+        
+        // Generate XWiki 2.0 Syntax for example
+        WikiPrinter printer = new DefaultWikiPrinter();
+        PrintRendererFactory prf = ecm.lookup(PrintRendererFactory.class);
+        Renderer xwikiRenderer = prf.createRenderer(Syntax.XWIKI_2_0, printer);
+
+        xdom.traverse(xwikiRenderer);
+
+        Assert.assertEquals("This a //[[link>MyPage]]//", printer.toString());
     }
 }

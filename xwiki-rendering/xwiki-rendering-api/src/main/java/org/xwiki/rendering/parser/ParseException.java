424666882f7197940fb6c305b565e11c24e7cb8b
vmassol <vincent@massol.net>
2009-06-08 16:12:07 +0000

    Removed unused stuff since WikiModel doesn't generate exception on parsing. If it does it means the underlying grammar is buggy and needs to be fixed.
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@20972 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-rendering/xwiki-rendering-api/src/main/java/org/xwiki/rendering/parser/ParseException.java
+++ xwiki-rendering/xwiki-rendering-api/src/main/java/org/xwiki/rendering/parser/ParseException.java
@@ -21,8 +21,6 @@ package org.xwiki.rendering.parser;
 
 /**
  * Encapsulate a parsing error.
- * 
- * @todo Add Apache header license since this was ripped from Apache Maven Doxia.
  */
 public class ParseException extends Exception
 {
@@ -32,96 +30,24 @@ public class ParseException extends Exception
     private static final long serialVersionUID = 442523704445037944L;
 
     /**
-     * The file that caused the ParseException..
-     */
-    private String fileName;
-
-    /**
-     * Line number where the parse exception occurred.
-     */
-    private int lineNumber;
-
-    /**
      * Construct a new ParseException with the specified detail message.
      * 
      * @param message The detailed message. This can later be retrieved by the Throwable.getMessage() method.
      */
     public ParseException(String message)
     {
-        this(null, message, null, -1);
+        super(message);
     }
 
     /**
      * Construct a new ParseException with the specified detail message and cause.
      * 
      * @param message The detailed message. This can later be retrieved by the Throwable.getMessage() method.
-     * @param e the cause. This can be retrieved later by the Throwable.getCause() method. (A null value is permitted,
+     * @param throwable the cause. This can be retrieved later by the Throwable.getCause() method. (A null value is permitted,
      *            and indicates that the cause is nonexistent or unknown.)
      */
-    public ParseException(String message, Exception e)
+    public ParseException(String message, Throwable throwable)
     {
-        this(e, message, null, -1);
-    }
-
-    /**
-     * Constructs a new exception with the specified cause. The error message is (cause == null ? null :
-     * cause.toString() ).
-     * 
-     * @param e the cause. This can be retrieved later by the Throwable.getCause() method. (A null value is permitted,
-     *            and indicates that the cause is nonexistent or unknown.)
-     */
-    public ParseException(Exception e)
-    {
-        this(e, null, null, -1);
-    }
-
-    /**
-     * Construct a new ParseException with the specified cause, filename and linenumber.
-     * 
-     * @param e the cause. This can be retrieved later by the Throwable.getCause() method. (A null value is permitted,
-     *            and indicates that the cause is nonexistent or unknown.)
-     * @param file Name of a file that couldn't be parsed. This can later be retrieved by the getFileName() method.
-     * @param line The line number where the parsing failed. This can later be retrieved by the getLineNumber() method.
-     */
-    public ParseException(Exception e, String file, int line)
-    {
-        this(e, null, file, line);
-    }
-
-    /**
-     * Construct a new ParseException with the specified cause, detail message, filename and linenumber.
-     * 
-     * @param e the cause. This can be retrieved later by the Throwable.getCause() method. (A null value is permitted,
-     *            and indicates that the cause is nonexistent or unknown.)
-     * @param message The detailed message. This can later be retrieved by the Throwable.getMessage() method.
-     * @param file Name of a file that couldn't be parsed. This can later be retrieved by the getFileName() method.
-     * @param line The line number where the parsing failed. This can later be retrieved by the getLineNumber() method.
-     */
-    public ParseException(Exception e, String message, String file, int line)
-    {
-        super((message == null) ? ((e == null) ? null : e.getMessage()) : message, e);
-
-        this.fileName = file;
-        this.lineNumber = line;
-    }
-
-    /**
-     * Returns the file that caused the ParseException.
-     * 
-     * @return the file that caused the ParseException.
-     */
-    public String getFileName()
-    {
-        return fileName;
-    }
-
-    /**
-     * Returns the line number where the ParseException ocurred.
-     * 
-     * @return the line number.
-     */
-    public int getLineNumber()
-    {
-        return lineNumber;
+        super(message, throwable);
     }
 }

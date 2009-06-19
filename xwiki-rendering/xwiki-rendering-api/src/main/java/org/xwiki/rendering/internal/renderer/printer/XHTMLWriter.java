256756a6d10bac295ada8d9f946fe2d54b9cf675
vmassol <vincent@massol.net>
2009-06-19 15:42:24 +0000

    Added missing license headers + removed commented out code
    Merged from trunk (rev 21356)
    
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/branches/xwiki-core-1.9@21357 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-rendering/xwiki-rendering-api/src/main/java/org/xwiki/rendering/internal/renderer/printer/XHTMLWriter.java
+++ xwiki-rendering/xwiki-rendering-api/src/main/java/org/xwiki/rendering/internal/renderer/printer/XHTMLWriter.java
@@ -1,3 +1,22 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package org.xwiki.rendering.internal.renderer.printer;
 
 import java.io.UnsupportedEncodingException;
@@ -16,11 +35,7 @@ public class XHTMLWriter extends XMLWriter
     protected static final OutputFormat DEFAULT_XHTML_FORMAT;
 
     static {
-        // Enable indentation based on two spaces characters
-        // DEFAULT_XHTML_FORMAT = new OutputFormat(/*" ", true*/);
-
         DEFAULT_XHTML_FORMAT = new OutputFormat();
-
         DEFAULT_XHTML_FORMAT.setXHTML(true);
     }
 

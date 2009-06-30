db00c28fc9d832b9e22072e7dd13d8eea0ab6399
vmassol <vincent@massol.net>
2009-06-30 11:35:53 +0000

    Added missing license header + some javadoc
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@21723 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-component/xwiki-component-default/src/main/java/org/xwiki/component/internal/StackingComponentEventManager.java
+++ xwiki-component/xwiki-component-default/src/main/java/org/xwiki/component/internal/StackingComponentEventManager.java
@@ -1,3 +1,22 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package org.xwiki.component.internal;
 
 import java.util.Stack;
@@ -7,6 +26,14 @@ import org.xwiki.component.manager.ComponentDescriptorAddedEvent;
 import org.xwiki.component.manager.ComponentEventManager;
 import org.xwiki.observation.ObservationManager;
 
+/**
+ * Allow stacking component events and flush them whenever the user of this class wants to. This is used for example
+ * at application initialization time when we don't want to send events before the Application Context has been
+ * initialized since components subscribing to these events may want to use the Application Context.
+ * 
+ * @version $Id$
+ * @since 2.0M1
+ */
 public class StackingComponentEventManager implements ComponentEventManager
 {
     private ObservationManager observationManager;
@@ -15,6 +42,10 @@ public class StackingComponentEventManager implements ComponentEventManager
     
     private boolean shouldStack = true;
     
+    /**
+     * {@inheritDoc}
+     * @see ComponentEventManager#notify(ComponentDescriptor)
+     */
     public <T> void notify(ComponentDescriptor< T > descriptor)
     {
         if (this.shouldStack) {

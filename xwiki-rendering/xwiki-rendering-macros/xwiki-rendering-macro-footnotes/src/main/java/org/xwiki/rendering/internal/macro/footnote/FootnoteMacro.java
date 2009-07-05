3474167194857a0ed52591f2a1dd51a8081f3cc2
sdumitriu <sergiu@xwiki.com>
2009-07-05 14:32:57 +0000

    XWIKI-4065: New Footnote macro Done.
    More functional tests need to be added.
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@21829 f329d543-caf0-0310-9063-dda96c69346f

---
+++ xwiki-rendering/xwiki-rendering-macros/xwiki-rendering-macro-footnotes/src/main/java/org/xwiki/rendering/internal/macro/footnote/FootnoteMacro.java
@@ -0,0 +1,106 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.rendering.internal.macro.footnote;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.xwiki.component.annotation.Component;
+import org.xwiki.rendering.block.Block;
+import org.xwiki.rendering.block.MacroBlock;
+import org.xwiki.rendering.macro.AbstractMacro;
+import org.xwiki.rendering.macro.MacroExecutionException;
+import org.xwiki.rendering.macro.descriptor.DefaultContentDescriptor;
+import org.xwiki.rendering.macro.descriptor.DefaultMacroDescriptor;
+import org.xwiki.rendering.macro.footnote.FootnoteMacroParameters;
+import org.xwiki.rendering.transformation.MacroTransformationContext;
+
+/**
+ * Generate footnotes, listed at the end of the page. A reference to the generated footnote is inserted at the location
+ * where the macro is called.
+ * 
+ * @version $Id$
+ * @since 2.0M2
+ */
+@Component(FootnoteMacro.MACRO_NAME)
+public class FootnoteMacro extends AbstractMacro<FootnoteMacroParameters>
+{
+    /** The name of this macro. */
+    public static final String MACRO_NAME = "footnote";
+
+    /** The description of the macro. */
+    private static final String DESCRIPTION = "Genereates a footnote to display at the end of the page.";
+
+    /** The description of the macro content. */
+    private static final String CONTENT_DESCRIPTION = "the text to place in the footnote";
+
+    /**
+     * Create and initialize the descriptor of the macro.
+     */
+    public FootnoteMacro()
+    {
+        super(new DefaultMacroDescriptor(DESCRIPTION, new DefaultContentDescriptor(CONTENT_DESCRIPTION),
+            FootnoteMacroParameters.class));
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.xwiki.rendering.macro.Macro#supportsInlineMode()
+     */
+    public boolean supportsInlineMode()
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritDoc} Decrease the priority of the footnote macro, so that the putFootnote macro executes first.
+     * 
+     * @see org.xwiki.rendering.macro.Macro#getPriority()
+     */
+    @Override
+    public int getPriority()
+    {
+        return 500;
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.xwiki.rendering.macro.Macro#execute(Object, String, MacroTransformationContext)
+     */
+    public List<Block> execute(FootnoteMacroParameters parameters, String content, MacroTransformationContext context)
+        throws MacroExecutionException
+    {
+        Block root = context.getXDOM();
+        List<MacroBlock> macros = root.getChildrenByType(MacroBlock.class, true);
+        for (MacroBlock macro : macros) {
+            if (PutFootnotesMacro.MACRO_NAME.equals(macro.getName())) {
+                return Collections.emptyList();
+            }
+        }
+
+        Block putFootnotesMacro =
+            new MacroBlock(PutFootnotesMacro.MACRO_NAME, Collections.<String, String> emptyMap(), false);
+        root.addChild(putFootnotesMacro);
+
+        return Collections.emptyList();
+    }
+}
---
+++ xwiki-rendering/xwiki-rendering-macros/xwiki-rendering-macro-footnotes/src/main/java/org/xwiki/rendering/macro/footnote/FootnoteMacroParameters.java
@@ -0,0 +1,76 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.rendering.macro.footnote;
+
+/**
+ * Parameters for the {@link org.xwiki.rendering.internal.macro.footnote.FootnoteMacro} Macro.
+ * 
+ * @version $Id$
+ * @since 2.0M1
+ */
+public class FootnoteMacroParameters
+{
+    // /**
+    // * @version $Id$
+    // */
+    // public enum Scope
+    // {
+    // /**
+    // * Put the footnote at the end of the current section.
+    // */
+    // SECTION,
+    //    
+    // /**
+    // * Inside included documents, put the footnote at the end of the included document.
+    // */
+    // DOCUMENT_LOCAL,
+    //    
+    // /**
+    // * Put the footnote at the end of the global (including) document.
+    // */
+    // GLOBAL;
+    // };
+    //    
+    // /**
+    // *
+    // */
+    // private Scope scope = Scope.GLOBAL;
+    //    
+    // /**
+    // * @param scope If local only section in the current scope will be listed. For example if the macro is written in
+    // a
+    // * section, only subsections of this section will be listed.
+    // */
+    // @ParameterDescription("if local only section in the current scope will be listed. "
+    // + "For example if the macro is written in a section, only subsections of this section will be listed")
+    // public void setScope(Scope scope)
+    // {
+    // this.scope = scope;
+    // }
+    //    
+    // /**
+    // * @return if {@link Scope#LOCAL} only section in the current scope will be listed. For example if the macro is
+    // * written in a section, only subsections of this section will be listed.
+    // */
+    // public Scope getScope()
+    // {
+    // return this.scope;
+    // }
+}

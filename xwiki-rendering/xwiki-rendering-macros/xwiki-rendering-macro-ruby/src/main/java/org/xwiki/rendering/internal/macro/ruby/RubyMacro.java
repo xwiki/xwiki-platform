26739f63fbd807e1814e78808d1cdf160b31e667
asiri <asiri@xwiki.com>
2009-07-23 12:19:58 +0000

    XWIKI-2849: Create ruby macro for rendering 2.0
    * Implemented.
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@22042 f329d543-caf0-0310-9063-dda96c69346f

---
+++ xwiki-rendering/xwiki-rendering-macros/xwiki-rendering-macro-ruby/src/main/java/org/xwiki/rendering/internal/macro/ruby/RubyMacro.java
@@ -0,0 +1,66 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.rendering.internal.macro.ruby;
+
+import javax.script.ScriptContext;
+import javax.script.ScriptEngine;
+import javax.script.ScriptException;
+
+import org.xwiki.component.annotation.Component;
+import org.xwiki.rendering.macro.descriptor.DefaultContentDescriptor;
+import org.xwiki.rendering.macro.script.AbstractJRSR223ScriptMacro;
+import org.xwiki.rendering.macro.script.JSR223ScriptMacroParameters;
+
+/**
+ * A macro for executing ruby scripts.
+ * 
+ * @version $Id:$
+ * @since 2.0M2
+ */
+@Component("ruby")
+public class RubyMacro extends AbstractJRSR223ScriptMacro<JSR223ScriptMacroParameters>
+{
+    /**
+     * The description of the macro.
+     */
+    private static final String DESCRIPTION = "Execute a ruby script.";
+
+    /**
+     * The description of the macro content.
+     */
+    private static final String CONTENT_DESCRIPTION = "The ruby script to execute";
+    
+    /**
+     * Create and initialize the descriptor of the macro.
+     */
+    public RubyMacro()
+    {
+        super(DESCRIPTION, new DefaultContentDescriptor(CONTENT_DESCRIPTION));
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected Object eval(String content, ScriptEngine engine, ScriptContext scriptContext) throws ScriptException
+    {
+        return super.eval(content, engine, scriptContext);
+    }
+}
---
+++ xwiki-rendering/xwiki-rendering-macros/xwiki-rendering-macro-ruby/src/test/java/org/xwiki/rendering/macro/ruby/RenderingTests.java
@@ -0,0 +1,86 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.rendering.macro.ruby;
+
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.xwiki.bridge.DocumentAccessBridge;
+import org.xwiki.component.descriptor.DefaultComponentDescriptor;
+import org.xwiki.component.embed.EmbeddableComponentManager;
+import org.xwiki.component.manager.ComponentManager;
+import org.xwiki.rendering.scaffolding.RenderingTestSuite;
+import org.xwiki.test.ComponentManagerTestSetup;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+
+/**
+ * Rendering tests for ruby macro.
+ * 
+ * @version $Id:$
+ * @since 2.0M2
+ */
+public class RenderingTests extends TestCase
+{
+    /**
+     * Creates a rendering test suit for testing ruby macro.
+     * 
+     * @return rendering test suit for testing ruby macro.
+     * @throws Exception if an error occurs while setting up the test suite.
+     */
+    public static Test suite() throws Exception
+    {
+        RenderingTestSuite suite = new RenderingTestSuite("Test Ruby Macro");
+
+        suite.addTestsFromResource("macroruby1", true);
+        suite.addTestsFromResource("macroruby2", true);
+        suite.addTestsFromResource("macroruby3", true);
+
+        ComponentManagerTestSetup testSetup = new ComponentManagerTestSetup(suite);
+        setUpMocks(testSetup.getComponentManager());
+
+        return testSetup;
+    }
+
+    /**
+     * Sets up mock component descriptors.
+     * 
+     * @param componentManager the {@link ComponentManager} component.
+     */
+    public static void setUpMocks(EmbeddableComponentManager componentManager)
+    {
+        Mockery context = new Mockery();
+
+        // Document Access Bridge Mock setup
+        final DocumentAccessBridge mockDocumentAccessBridge = context.mock(DocumentAccessBridge.class);
+        context.checking(new Expectations()
+        {
+            {
+                allowing(mockDocumentAccessBridge).hasProgrammingRights();
+                will(returnValue(true));
+            }
+        });
+
+        DefaultComponentDescriptor<DocumentAccessBridge> descriptorDAB =
+            new DefaultComponentDescriptor<DocumentAccessBridge>();
+        descriptorDAB.setRole(DocumentAccessBridge.class);
+        componentManager.registerComponent(descriptorDAB, mockDocumentAccessBridge);
+    }
+}

c6426a488394780aedda575b4d98659bfaae79a2
asiri <asiri@xwiki.com>
2009-07-30 16:42:47 +0000

    XWIKI-4114: Error, Info and Warning macros not working
    * Added a unit test.
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@22251 f329d543-caf0-0310-9063-dda96c69346f

---
+++ xwiki-rendering/xwiki-rendering-macros/xwiki-rendering-macro-message/src/test/java/org/xwiki/rendering/MessageMacroTest.java
@@ -0,0 +1,47 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.xwiki.rendering;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.xwiki.rendering.internal.macro.message.MessageMacro;
+import org.xwiki.rendering.macro.Macro;
+import org.xwiki.test.AbstractComponentTestCase;
+
+/**
+ * Unit test for {@link MessageMacro} that cannot be performed with rendering tests.
+ *
+ * @version $Id$
+ * @since 2.0M3
+ */
+public class MessageMacroTest extends AbstractComponentTestCase
+{
+    /**
+     * Tests whether message macro content descriptor is never null.
+     */
+    @Test
+    public void testMacroContentDescriptorIsNotNull() throws Exception
+    {
+        MessageMacro messageMacro = (MessageMacro) getComponentManager().lookup(Macro.class, "info");
+        Assert.assertNotNull(messageMacro.getDescriptor().getContentDescriptor());
+    }
+}

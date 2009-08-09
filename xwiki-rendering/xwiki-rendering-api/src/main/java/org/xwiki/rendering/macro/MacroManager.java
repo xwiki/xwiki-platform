d849b4b2940cec28b6f25d6b58adc90b2014e3a7
vmassol <vincent@massol.net>
2009-08-09 09:19:18 +0000

    Improved javadoc
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@22438 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-rendering/xwiki-rendering-api/src/main/java/org/xwiki/rendering/macro/MacroManager.java
+++ xwiki-rendering/xwiki-rendering-api/src/main/java/org/xwiki/rendering/macro/MacroManager.java
@@ -25,9 +25,12 @@ import org.xwiki.component.annotation.ComponentRole;
 import org.xwiki.rendering.parser.Syntax;
 
 /**
- * Main interface to lookup {@link org.xwiki.rendering.macro.Macro} implementations available for the rendering at
- * run-time. Rendering macros are not necessarily registered as components, thus code that needs to look up a certain
- * macro, or list all available macros should always go through such a manager.
+ * Allow retrieving all macros or all macros registered for a given syntax only. Indeed, a macro can be registered
+ * and thus made available for all syntaxes or only available for a given syntax. The latter is useful for example
+ * if we want to support copy pasting wiki content from another wiki and we want to support transparently the macros
+ * defined in that content; in this case we could implement these macros only for that syntax and in the implementation
+ * make the bridge with XWiki macros for example. 
+ *
  * 
  * @version $Id$
  * @since 1.9M1
@@ -37,38 +40,41 @@ public interface MacroManager
 {
     /**
      * @param syntax the desired syntax
-     * @return a set of all names of available macros for the desired syntax.
+     * @return the available macros for the desired syntax (this includes macros registered for all syntaces +
+     *         macros registered only for a given syntax)
      * @throws MacroLookupException error when lookup macros
      */
     Set<String> getMacroNames(Syntax syntax) throws MacroLookupException;
 
     /**
-     * @param macroName the name of the macro looked up
-     * @param syntax the syntax of the macro looked up
-     * @return the macro if found, looked-up first as macro for the desired syntax identifier, and then in macro
-     *         registered for all syntaxes if not found.
+     * @param macroName the name of the macro to look up (eg "toc" for the TOC Macro)
+     * @param syntax the syntax of the macro to look up
+     * @return the macro, looked-up first as a macro for the desired syntax identifier, and then as a macro
+     *         registered for all syntaxes if not found
      * @throws MacroLookupException when no macro with such name was found in both the list of macro for the specified
-     *             syntax identifier and in for all syntaxes.
+     *             syntax identifier and for all syntaxes
      */
     Macro< ? > getMacro(String macroName, Syntax syntax) throws MacroLookupException;
 
     /**
-     * @param macroName the name of the macro to lookup
-     * @return the macro if found, looked-up in the list of macro registered for all syntaxes.
-     * @throws MacroLookupException when no macro with such name was found in the list of macro for all syntaxes.
+     * @param macroName the name of the macro to look up (eg "toc" for the TOC Macro)
+     * @return the macro if found, looked-up in the list of macro registered for all syntaxes
+     * @throws MacroLookupException when no macro with such name was found in the list of macro for all syntaxes
      */
     Macro< ? > getMacro(String macroName) throws MacroLookupException;
 
     /**
-     * @param macroName the name of the macro
+     * @param macroName the name of the macro (eg "toc" for the TOC Macro)
      * @param syntax the syntax of the macro
-     * @return true if the manager can return a macro with given name for the given syntax, false otherwise
+     * @return true if a macro with the given name and for the given syntax can be found, false otherwise. Returns
+     *         false if a macro with the given name exists but has been registered only for all syntaxes
      */
     boolean exists(String macroName, Syntax syntax);
 
     /**
-     * @param macroName the name of the macro
-     * @return true if the manager can return a macro with given name for all syntaxes, false otherwise
+     * @param macroName the name of the macro (eg "toc" for the TOC Macro)
+     * @return true if a macro with given name and registered for all syntaxes can be found, false otherwise. Returns
+     *         true if a macro with the given name has been registered only for a given syntax
      */
     boolean exists(String macroName);
 }

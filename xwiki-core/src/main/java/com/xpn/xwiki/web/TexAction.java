e04d392f9e69f429afce9032060baf1b3f9f8e59
sdumitriu <sergiu@xwiki.com>
2009-08-10 10:36:30 +0000

    [misc] Fixing broken build
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@22462 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-core/src/main/java/com/xpn/xwiki/web/TexAction.java
+++ xwiki-core/src/main/java/com/xpn/xwiki/web/TexAction.java
@@ -23,6 +23,8 @@ import java.io.IOException;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.xwiki.equation.ImageData;
+import org.xwiki.equation.ImageStorage;
 
 import com.xpn.xwiki.XWikiContext;
 import com.xpn.xwiki.XWikiException;

afe1d0cfaab6a420f90b1b82c2a42ca605b1ffe2
tmortagne <thomas.mortagne@xwiki.com>
2009-08-12 12:35:16 +0000

    [misc] Set XWiki rule for test classes as default configuration of maven surefire plugin
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@22560 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-observation/xwiki-observation-remote/src/test/java/org/xwiki/observation/remote/test/AbstractROMTestCase.java
+++ xwiki-observation/xwiki-observation-remote/src/test/java/org/xwiki/observation/remote/test/AbstractROMTestCase.java
@@ -1,3 +1,23 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ *
+ */
 package org.xwiki.observation.remote.test;
 
 import java.io.File;
@@ -12,6 +32,11 @@ import org.xwiki.container.Container;
 import org.xwiki.observation.remote.internal.jgroups.JGroupsRemoteObservationManager;
 import org.xwiki.test.XWikiComponentInitializer;
 
+/**
+ * Base class to easily emulate two instances of observation manager communicate with each other by network.
+ * 
+ * @version $Id$
+ */
 public abstract class AbstractROMTestCase
 {
     private XWikiComponentInitializer initializer1 = new XWikiComponentInitializer();
--- xwiki-observation/xwiki-observation-remote/src/test/java/org/xwiki/observation/remote/test/TestEvent.java
+++ xwiki-observation/xwiki-observation-remote/src/test/java/org/xwiki/observation/remote/test/TestEvent.java
@@ -1,18 +1,56 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ *
+ */
 package org.xwiki.observation.remote.test;
 
 import java.io.Serializable;
 
-import org.junit.Ignore;
 import org.xwiki.observation.event.Event;
 
-@Ignore
+/**
+ * Simple Serializable event type.
+ * 
+ * @version $Id$
+ */
 public class TestEvent implements Event, Serializable
 {
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.xwiki.observation.event.Event#matches(java.lang.Object)
+     */
     public boolean matches(Object otherEvent)
     {
         return true;
     }
 
+    /**
+     * {@inheritDoc}
+     * 
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
     @Override
     public boolean equals(Object obj)
     {

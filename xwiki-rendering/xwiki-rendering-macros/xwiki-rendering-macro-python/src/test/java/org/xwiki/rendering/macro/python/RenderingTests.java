3a2de829de679af4c99ed2567d94ba220ebf5b3a
tmortagne <thomas.mortagne@xwiki.com>
2009-09-04 13:28:52 +0000

    XWIKI-3963: Improve the way to register Pygments on jython engine in code macro XWIKI-4278: Upgrade to jython 2.5.1RC1 XWIKI-2798: Create jython macro for rendering 2.0 [misc] Fix AbstractJSR223ScriptMacro name [misc] Improve script macro category setup
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@23279 f329d543-caf0-0310-9063-dda96c69346f

---
+++ xwiki-rendering/xwiki-rendering-macros/xwiki-rendering-macro-python/src/test/java/org/xwiki/rendering/macro/python/RenderingTests.java
@@ -0,0 +1,56 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.rendering.macro.python;
+
+import org.xwiki.rendering.macro.script.ScriptMockSetup;
+import org.xwiki.rendering.scaffolding.RenderingTestSuite;
+import org.xwiki.test.ComponentManagerTestSetup;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+
+/**
+ * Rendering tests for ruby macro.
+ * 
+ * @version $Id$
+ * @since 2.0M2
+ */
+public class RenderingTests extends TestCase
+{
+    /**
+     * Creates a rendering test suit for testing ruby macro.
+     * 
+     * @return rendering test suit for testing ruby macro.
+     * @throws Exception if an error occurs while setting up the test suite.
+     */
+    public static Test suite() throws Exception
+    {
+        RenderingTestSuite suite = new RenderingTestSuite("Test Ruby Macro");
+
+        suite.addTestsFromResource("macropython1", true);
+        suite.addTestsFromResource("macropython2", true);
+        suite.addTestsFromResource("macropython3", true);
+
+        ComponentManagerTestSetup testSetup = new ComponentManagerTestSetup(suite);
+        new ScriptMockSetup(testSetup.getComponentManager());
+
+        return testSetup;
+    }
+}

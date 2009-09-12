2895d868a0cb1792af2a9251cd7402c71db52efd
vmassol <vincent@massol.net>
2009-09-12 16:48:49 +0000

    XWIKI-4330: Make section editing work for any section level
    Merged from trunk (revs 23490, 23491, 23492)
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/branches/xwiki-core-2.0@23493 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-core/src/main/java/com/xpn/xwiki/XWiki.java
+++ xwiki-core/src/main/java/com/xpn/xwiki/XWiki.java
@@ -6174,6 +6174,15 @@ public class XWiki implements XWikiDocChangeNotificationInterface
     {
         return (context.getWiki().ParamAsLong("xwiki.section.edit", 0) == 1);
     }
+    
+    /**
+     * @return the section depth for which section editing is available (can be configured through 
+     *         {@code xwiki.section.depth} configuration property. Defaults to 2 when not defined
+     */
+    public long getSectionEditingDepth()
+    {
+        return ParamAsLong("xwiki.section.depth", 2);
+    }
 
     public boolean hasCaptcha(XWikiContext context)
     {

665f6746f9433efaeca54a80f4b71e9f48203901
mflorea <mariusdumitru.florea@xwiki.com>
2009-09-15 08:34:28 +0000

    XWIKI-4092: Upgrade to GWT 1.7.0 XWIKI-3448: WYSIWIG toolbox not rendered corectly in IE8 XWIKI-3446: WYSIWIG dialogs not displayed corectly in IE 8 final Also: * Improved xwiki-gwt-dom's module descriptor by preventing a useless GWT compiler permutation * Updated hosted mode scripts
    Merged from trunk, revision 23495.
    
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/web/branches/xwiki-web-2.0@23535 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-platform-web/wysiwyg/src/test/resources/start_wysiwyg_noserver_debug.sh
+++ xwiki-platform-web/wysiwyg/src/test/resources/start_wysiwyg_noserver_debug.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 XE_VERSION=2.0-SNAPSHOT;
-GWT_VERSION=1.6.4;
+GWT_VERSION=1.7.0;
 
 JAVA32_HOME=/usr/lib/jvm/ia32-java-1.5.0-sun/bin;
 M2_REPO=~/.m2/repository;
@@ -19,8 +19,8 @@ $APP_DIR/WEB-INF/lib/xwiki-web-wysiwyg-$XE_VERSION.jar:\
 $M2_REPO/com/xpn/xwiki/platform/xwiki-web-wysiwyg/$XE_VERSION/xwiki-web-wysiwyg-$XE_VERSION-sources.jar:\
 $M2_REPO/com/xpn/xwiki/platform/xwiki-web-gwt/$XE_VERSION/xwiki-web-gwt-$XE_VERSION-sources.jar:\
 $M2_REPO/org/xwiki/platform/xwiki-web-gwt-dom/$XE_VERSION/xwiki-web-gwt-dom-$XE_VERSION.jar:\
-$M2_REPO/com/google/gwt/incubator-glasspanel/r729/incubator-glasspanel-r729.jar:\
-$M2_REPO/com/smartgwt/smartgwt/1.0b2/smartgwt-1.0b2.jar:\
+$M2_REPO/com/google/gwt/gwt-incubator/july-14-2009/gwt-incubator-july-14-2009.jar:\
+$M2_REPO/com/smartgwt/smartgwt/1.2/smartgwt-1.2.jar:\
 $M2_REPO/com/google/gwt/gwt-dev/$GWT_VERSION/gwt-dev-$GWT_VERSION-linux.jar:\
 $M2_REPO/com/google/gwt/gwt-user/$GWT_VERSION/gwt-user-$GWT_VERSION.jar \
 com.google.gwt.dev.HostedMode \

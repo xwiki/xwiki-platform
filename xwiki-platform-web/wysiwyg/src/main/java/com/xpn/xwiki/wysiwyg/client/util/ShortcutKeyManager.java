42e86a5a262f2ad0882f9df2beaa382fe9b7f7cc
mflorea <mariusdumitru.florea@xwiki.com>
2009-09-30 09:27:06 +0000

    Improve the ShortcutKeyManager to properly prevent the default browser behavior and to execute the command when the key is held down.
    Merged from trunk, revision 24142.
    
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/web/branches/xwiki-web-2.0@24143 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-platform-web/wysiwyg/src/main/java/com/xpn/xwiki/wysiwyg/client/util/ShortcutKeyManager.java
+++ xwiki-platform-web/wysiwyg/src/main/java/com/xpn/xwiki/wysiwyg/client/util/ShortcutKeyManager.java
@@ -29,6 +29,10 @@ import org.xwiki.gwt.dom.client.Event;
 import com.google.gwt.event.dom.client.HasAllKeyHandlers;
 import com.google.gwt.event.dom.client.KeyDownEvent;
 import com.google.gwt.event.dom.client.KeyDownHandler;
+import com.google.gwt.event.dom.client.KeyPressEvent;
+import com.google.gwt.event.dom.client.KeyPressHandler;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.DeferredCommand;
@@ -39,12 +43,28 @@ import com.xpn.xwiki.wysiwyg.client.util.ShortcutKey.ModifierKey;
  * 
  * @version $Id$
  */
-public class ShortcutKeyManager extends HashMap<ShortcutKey, Command> implements KeyDownHandler
+public class ShortcutKeyManager extends HashMap<ShortcutKey, Command> implements KeyDownHandler, KeyPressHandler,
+    KeyUpHandler
 {
     /**
      * Field required by all {@link java.io.Serializable} classes.
      */
     private static final long serialVersionUID = -1888376929779432766L;
+
+    /**
+     * The last command executed. The command is reset on each KeyDown event because only then we have the right key
+     * code and key modifiers. Also, KeyPress and KeyUp events shouldn't be triggered without a KeyDown event.
+     */
+    private Command command;
+
+    /**
+     * Flag used to avoid handling the shortcut key on both KeyDown and KeyPress events. This flag is needed because of
+     * the inconsistencies between browsers regarding keyboard events. For instance IE doesn't generate the KeyPress
+     * event for navigation (arrow) keys and generates multiple KeyDown events while a key is hold down. On the
+     * contrary, FF generates the KeyPress event for navigation (arrow) keys and generates just one KeyDown event while
+     * a key is hold down. FF generates multiple KeyPress events when a key is hold down.
+     */
+    private boolean ignoreNextKeyPress;
 
     /**
      * Adds the necessary key handlers to be able to catch shortcut keys.
@@ -56,6 +76,8 @@ public class ShortcutKeyManager extends HashMap<ShortcutKey, Command> implements
     {
         List<HandlerRegistration> registrations = new ArrayList<HandlerRegistration>();
         registrations.add(source.addKeyDownHandler(this));
+        registrations.add(source.addKeyPressHandler(this));
+        registrations.add(source.addKeyUpHandler(this));
         return registrations;
     }
 
@@ -66,16 +88,47 @@ public class ShortcutKeyManager extends HashMap<ShortcutKey, Command> implements
      */
     public void onKeyDown(KeyDownEvent event)
     {
-        // We listen to key down events because:
-        // * It seems the key modifiers are not detected on key press.
-        // * It seems that the Meta key is not detected on apple keyboards (where it is mapped to apple command key) for
-        // key up events.
-        Command command = get(new ShortcutKey(event.getNativeKeyCode(), getModifiers(event)));
+        ignoreNextKeyPress = true;
+        // We reset the command here because:
+        // * Key modifiers are not properly detected on key press.
+        // * Meta key is not detected on apple keyboards (where it is mapped to apple command key) for key up events.
+        command = get(new ShortcutKey(event.getNativeKeyCode(), getModifiers(event)));
         if (command != null) {
             // Prevent default browser behavior.
             ((Event) event.getNativeEvent()).xPreventDefault();
             // Schedule command.
             DeferredCommand.addCommand(command);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see KeyPressHandler#onKeyPress(KeyPressEvent)
+     */
+    public void onKeyPress(KeyPressEvent event)
+    {
+        if (command != null) {
+            // Prevent default browser behavior (apparently it's not enough to cancel the KeyDown event).
+            ((Event) event.getNativeEvent()).xPreventDefault();
+            if (!ignoreNextKeyPress) {
+                // Schedule command.
+                DeferredCommand.addCommand(command);
+            }
+            ignoreNextKeyPress = false;
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see KeyUpHandler#onKeyUp(KeyUpEvent)
+     */
+    public void onKeyUp(KeyUpEvent event)
+    {
+        if (command != null) {
+            // Prevent default browser behavior (apparently it's not enough to cancel the KeyDown event).
+            ((Event) event.getNativeEvent()).xPreventDefault();
         }
     }
 

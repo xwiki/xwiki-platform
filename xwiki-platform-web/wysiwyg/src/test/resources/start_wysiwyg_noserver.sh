8dda92599f3200b822c53ea91a79af7273e8d9f8
mflorea <mariusdumitru.florea@xwiki.com>
2009-10-23 09:43:43 +0000

    * Removed the login code that was executed in hosted mode in order to decrease the dependency on XWiki. Now we need to login before running the WYSIWYG in hosted mode. * Added xwiki-core-component-api to classpath when running in hosted mode because new services are component based. * Changed the hosted mode HTML to load the editor on demand. * Fixed two exceptions that were thrown in hosted mode (ClassCastException in MenuBar and ConcurentModificationException in TablePlugin).
    Merged from trunk, revision 24665.
    
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/web/branches/xwiki-web-2.0@24666 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-platform-web/wysiwyg/src/test/resources/start_wysiwyg_noserver.sh
+++ xwiki-platform-web/wysiwyg/src/test/resources/start_wysiwyg_noserver.sh
@@ -15,6 +15,7 @@ $APP_DIR/WEB-INF/lib/xwiki-web-wysiwyg-$XE_VERSION.jar:\
 $M2_REPO/com/xpn/xwiki/platform/xwiki-web-wysiwyg/$XE_VERSION/xwiki-web-wysiwyg-$XE_VERSION-sources.jar:\
 $M2_REPO/com/xpn/xwiki/platform/xwiki-web-gwt/$XE_VERSION/xwiki-web-gwt-$XE_VERSION-sources.jar:\
 $M2_REPO/org/xwiki/platform/xwiki-web-gwt-dom/$XE_VERSION/xwiki-web-gwt-dom-$XE_VERSION.jar:\
+$M2_REPO/org/xwiki/platform/xwiki-core-component-api/$XE_VERSION/xwiki-core-component-api-$XE_VERSION.jar:\
 $M2_REPO/com/google/gwt/gwt-incubator/july-14-2009/gwt-incubator-july-14-2009.jar:\
 $M2_REPO/com/smartgwt/smartgwt/1.2/smartgwt-1.2.jar:\
 $M2_REPO/com/google/gwt/gwt-dev/$GWT_VERSION/gwt-dev-$GWT_VERSION-linux.jar:\

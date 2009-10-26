631b302b406863624c79f2c02b57d22a4ad20049
sdumitriu <sergiu@xwiki.com>
2009-10-26 16:43:45 +0000

    XWIKI-4507: PHP support (via Quercus) Done. Initial patch submitted by Jessica Hawkwell.
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@24724 f329d543-caf0-0310-9063-dda96c69346f

---
+++ xwiki-rendering/xwiki-rendering-macros/xwiki-rendering-macro-php/src/test/java/org/xwiki/rendering/macro/php/RenderingTests.java
@@ -0,0 +1,60 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.rendering.macro.php;
+
+import org.xwiki.rendering.macro.script.ScriptMockSetup;
+import org.xwiki.rendering.scaffolding.RenderingTestSuite;
+import org.xwiki.test.ComponentManagerTestSetup;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+
+/**
+ * Rendering tests for the php macro.
+ * 
+ * @version $Id$
+ * @since 2.1M1
+ */
+public class RenderingTests extends TestCase
+{
+    /**
+     * Creates a rendering test suit for testing the PHP macro.
+     * 
+     * @return rendering test suit for testing the PHP macro.
+     * @throws Exception if an error occurs while setting up the test suite.
+     */
+    public static Test suite() throws Exception
+    {
+        RenderingTestSuite suite = new RenderingTestSuite("Test PHP Macro");
+
+        suite.addTestsFromResource("macrophp1", true);
+        suite.addTestsFromResource("macrophp2", true);
+        suite.addTestsFromResource("macrophp3", true);
+        // Disabled for the moment, since the variables defined in a macro instance don't live past the macro block.
+        // suite.addTestsFromResource("macrophp4", true);
+        // Disabled for the moment, since the classes defined in a macro instance don't live past the macro block.
+        // suite.addTestsFromResource("macrophp5", true);
+
+        ComponentManagerTestSetup testSetup = new ComponentManagerTestSetup(suite);
+        new ScriptMockSetup(testSetup.getComponentManager());
+
+        return testSetup;
+    }
+}

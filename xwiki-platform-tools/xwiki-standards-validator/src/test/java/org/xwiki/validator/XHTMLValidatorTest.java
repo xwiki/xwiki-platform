b86d3d414bccd62d34d440b27af1029e4bc7a1ed
jvdrean <jv@drean.org>
2009-11-27 18:26:01 +0000

    XTVALIDATOR-1 : Initial import
    Renamed package, added resource files svn failed to commit previously.
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/xwiki-tools/trunk@25330 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-platform-tools/xwiki-standards-validator/src/test/java/org/xwiki/validator/XHTMLValidatorTest.java
+++ xwiki-platform-tools/xwiki-standards-validator/src/test/java/org/xwiki/validator/XHTMLValidatorTest.java
@@ -17,7 +17,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.xwiki.accessibility;
+package org.xwiki.validator;
 
 import org.xwiki.validator.XHTMLValidator;
 

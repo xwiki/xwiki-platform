197df29c12de1cb1b8ce9682865d090774c0c77a
vmassol <vincent@massol.net>
2010-01-05 13:01:58 +0000

    testing $Id$ expansion
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@26032 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-model/src/main/java/org/xwiki/model/reference/EntityReferenceFactory.java
+++ xwiki-model/src/main/java/org/xwiki/model/reference/EntityReferenceFactory.java
@@ -26,7 +26,7 @@ import org.xwiki.model.EntityType;
  * Transforms an Entity reference defined in a given representation into an {@link EntityReference} object.
  *
  * @param <T> the object to transform into an Entity Reference
- * @version $Id:$
+ * @version $Id$
  * @since 2.2M1
  */
 @ComponentRole

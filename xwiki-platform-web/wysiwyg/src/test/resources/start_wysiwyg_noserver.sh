ac89581fa520c4ba0dce66a00b97415ae213d0b3
mflorea <mariusdumitru.florea@xwiki.com>
2010-01-26 21:42:08 +0000

    XWIKI-4797: Upgrade to GWT 2.0.0
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/web/trunk@26423 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-platform-web/wysiwyg/src/test/resources/start_wysiwyg_noserver.sh
+++ xwiki-platform-web/wysiwyg/src/test/resources/start_wysiwyg_noserver.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 XE_VERSION=2.2-SNAPSHOT;
-GWT_VERSION=1.7.0;
+GWT_VERSION=2.0.0;
+GWT_INCUBATOR_VERSION=20091216-r1739;
+SMART_GWT_VERSION=1.2;
 
-JAVA32_HOME=/usr/lib/jvm/ia32-java-6-sun/bin/;
 M2_REPO=~/.m2/repository;
 APP_DIR=`dirname $0`/webapps/xwiki;
-WYSIWYG_PATH=resources/js/xwiki/wysiwyg/xwe;
 
-$JAVA32_HOME/java \
+java \
 -Xmx1024m \
 -cp \
 $APP_DIR/WEB-INF/lib/xwiki-web-wysiwyg-$XE_VERSION.jar:\
@@ -16,14 +16,13 @@ $M2_REPO/com/xpn/xwiki/platform/xwiki-web-wysiwyg/$XE_VERSION/xwiki-web-wysiwyg-
 $M2_REPO/org/xwiki/platform/xwiki-web-gwt-dom/$XE_VERSION/xwiki-web-gwt-dom-$XE_VERSION.jar:\
 $M2_REPO/org/xwiki/platform/xwiki-web-gwt-user/$XE_VERSION/xwiki-web-gwt-user-$XE_VERSION.jar:\
 $M2_REPO/org/xwiki/platform/xwiki-core-component-api/$XE_VERSION/xwiki-core-component-api-$XE_VERSION.jar:\
-$M2_REPO/com/google/gwt/gwt-incubator/july-14-2009/gwt-incubator-july-14-2009.jar:\
-$M2_REPO/com/smartgwt/smartgwt/1.2/smartgwt-1.2.jar:\
-$M2_REPO/com/google/gwt/gwt-dev/$GWT_VERSION/gwt-dev-$GWT_VERSION-linux.jar:\
+$M2_REPO/com/google/gwt/gwt-incubator/$GWT_INCUBATOR_VERSION/gwt-incubator-$GWT_INCUBATOR_VERSION.jar:\
+$M2_REPO/com/smartgwt/smartgwt/$SMART_GWT_VERSION/smartgwt-$SMART_GWT_VERSION.jar:\
+$M2_REPO/com/google/gwt/gwt-dev/$GWT_VERSION/gwt-dev-$GWT_VERSION.jar:\
 $M2_REPO/com/google/gwt/gwt-user/$GWT_VERSION/gwt-user-$GWT_VERSION.jar \
-com.google.gwt.dev.HostedMode \
--logLevel WARN \
--style DETAILED \
+com.google.gwt.dev.DevMode \
 -noserver \
 -port 8080 \
--startupUrl xwiki/$WYSIWYG_PATH/Wysiwyg.html \
+-war $APP_DIR/resources/js/xwiki/wysiwyg \
+-startupUrl xwiki/bin/edit/Sandbox/Test?editor=wysiwyg \
 com.xpn.xwiki.wysiwyg.Wysiwyg

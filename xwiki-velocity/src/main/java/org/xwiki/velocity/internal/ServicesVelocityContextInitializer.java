c9e08f6b185690ae6df9d38648e7bc7cb13cbd36
vmassol <vincent@massol.net>
2010-02-04 13:05:21 +0000

    XWIKI-4855: Introduce new Model Script Service for accessing the Model APIs from scripts
    * Refactor to not have xwiki-script depend on xwiki-velocity
    * Added a very first rough version of what ModelScriptService could be. I'll be refining it during the XWiki 2.3 version development time
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@26736 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-velocity/src/main/java/org/xwiki/velocity/internal/ServicesVelocityContextInitializer.java
+++ xwiki-velocity/src/main/java/org/xwiki/velocity/internal/ServicesVelocityContextInitializer.java
@@ -17,7 +17,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.xwiki.script.internal.service;
+package org.xwiki.velocity.internal;
 
 import org.apache.velocity.VelocityContext;
 import org.xwiki.component.annotation.Component;

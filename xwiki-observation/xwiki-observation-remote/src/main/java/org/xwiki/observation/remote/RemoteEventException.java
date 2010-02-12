953e5e21daebbfb102a4d1064ce4ccfe3af0b88a
vmassol <vincent@massol.net>
2010-02-12 18:04:36 +0000

    Fix checkstyle errors in prevision of the move to checkstyle 5
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@26977 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-observation/xwiki-observation-remote/src/main/java/org/xwiki/observation/remote/RemoteEventException.java
+++ xwiki-observation/xwiki-observation-remote/src/main/java/org/xwiki/observation/remote/RemoteEventException.java
@@ -35,9 +35,10 @@ public class RemoteEventException extends Exception
     private static final long serialVersionUID = 1L;
 
     /**
-     * {@inheritDoc}
-     * 
-     * @see Exception#Exception(String)
+     * Constructs a new exception with the specified detail message. The cause is not initialized, and may
+     * subsequently be initialized by a call to {@link #initCause(Throwable)}.
+     *
+     * @param message the detail message (which is saved for later retrieval by the {@link #getMessage()} method)
      */
     public RemoteEventException(String message)
     {
@@ -45,9 +46,11 @@ public class RemoteEventException extends Exception
     }
 
     /**
-     * {@inheritDoc}
-     * 
-     * @see Exception#Exception(String, Throwable)
+     * Constructs a new exception with the specified detail message and cause.
+     *
+     * @param message the detail message (which is saved for later retrieval by the {@link #getMessage()} method)
+     * @param cause the cause (which is saved for later retrieval by the {@link #getCause()} method). A null value is
+     *        permitted, and indicates that the cause is nonexistent or unknown
      */
     public RemoteEventException(String message, Throwable cause)
     {

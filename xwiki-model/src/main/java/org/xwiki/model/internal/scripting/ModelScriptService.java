4120f96698f1307c9b7a2fddefb1beb62c19f9f6
vmassol <vincent@massol.net>
2010-02-16 13:22:04 +0000

    XWIKI-4855: Introduce new Model Script Service for accessing the Model APIs from scripts
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@27044 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-model/src/main/java/org/xwiki/model/internal/scripting/ModelScriptService.java
+++ xwiki-model/src/main/java/org/xwiki/model/internal/scripting/ModelScriptService.java
@@ -19,9 +19,16 @@
  */
 package org.xwiki.model.internal.scripting;
 
+import org.apache.commons.lang.StringUtils;
 import org.xwiki.component.annotation.Component;
 import org.xwiki.component.annotation.Requirement;
+import org.xwiki.component.manager.ComponentLookupException;
+import org.xwiki.component.manager.ComponentManager;
+import org.xwiki.model.EntityType;
+import org.xwiki.model.reference.DocumentReference;
 import org.xwiki.model.reference.DocumentReferenceResolver;
+import org.xwiki.model.reference.EntityReference;
+import org.xwiki.model.reference.EntityReferenceSerializer;
 import org.xwiki.script.service.ScriptService;
 
 /**
@@ -33,11 +40,74 @@ import org.xwiki.script.service.ScriptService;
 @Component("model")
 public class ModelScriptService implements ScriptService
 {
-    @Requirement("current")
-    private DocumentReferenceResolver currentDocumentReferenceResolver; 
+    @Requirement
+    private ComponentManager componentManager;
 
-    public DocumentReferenceResolver getCurrentStringDocumentReferenceResolver()
+    /**
+     * Create a Document Reference from a passed wiki, space and page names, which can be empty strings or null in
+     * which case they are resolved against the Resolver having the hint passed as parameter. Valid hints are for
+     * example "default", "current" "currentmixed".
+     *
+     * @param wiki the wiki reference name to use (can be empty or null)
+     * @param space the space reference name to use (can be empty or null)
+     * @param page the page referene name to use (can be empty or null)
+     * @param hint the hint of the Resolver to use in case any parameter is empty or null
+     * @return the typed Document Reference object or null if no Resolver with the passed hint could be found
+     */
+    public DocumentReference createDocumentReference(String wiki, String space, String page, String hint)
     {
-        return this.currentDocumentReferenceResolver;
+        EntityReference reference = null;
+        if (!StringUtils.isEmpty(wiki)) {
+            reference = new EntityReference(wiki, EntityType.WIKI);
+        }
+        if (!StringUtils.isEmpty(space)) {
+            reference = new EntityReference(space, EntityType.SPACE, reference);
+        }
+        if (!StringUtils.isEmpty(page)) {
+            reference = new EntityReference(page, EntityType.DOCUMENT, reference);
+        }
+
+        DocumentReference documentReference;
+        try {
+            documentReference = this.componentManager.lookup(DocumentReferenceResolver.class, hint).resolve(reference);
+        } catch (ComponentLookupException e) {
+            documentReference = null;
+        }
+        return documentReference;
+    }
+
+    /**
+     * @param stringRepresentation the document reference specified as a String (using the "wiki:space.page" format
+     *        and with special characters escaped where required).
+     * @param hint the hint of the Resolver to use in case any part of the reference is missing (no wiki specified,
+     *        no space or no page)
+     * @return the typed Document Reference object or null if no Resolver with the passed hint could be found
+     */
+    public DocumentReference resolveDocument(String stringRepresentation, String hint)
+    {
+        DocumentReference result;
+        try {
+            result = this.componentManager.lookup(DocumentReferenceResolver.class, hint).resolve(
+                stringRepresentation);
+        } catch (ComponentLookupException e) {
+            result = null;
+        }
+        return result;
+    }
+
+    /**
+     * @param reference the entity reference to transform into a String representation
+     * @param hint the hint of the Serializer to use (valid hints are for example "default", "compact", "local")
+     * @return the string representation of the passed entity reference
+     */
+    public String serialize(EntityReference reference, String hint)
+    {
+        String result;
+        try {
+            result = (String) this.componentManager.lookup(EntityReferenceSerializer.class, hint).serialize(reference);
+        } catch (ComponentLookupException e) {
+            result = null;
+        }
+        return result;
     }
 }

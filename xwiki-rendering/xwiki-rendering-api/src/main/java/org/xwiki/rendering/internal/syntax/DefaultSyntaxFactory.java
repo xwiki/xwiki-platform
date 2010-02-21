4127a3a52bfd6fb83eb7addb6dae8be327bb8679
tmortagne <thomas.mortagne@xwiki.com>
2010-02-21 16:32:41 +0000

    XWIKI-4916: Move DefaultSyntaxFactory in org.xwiki.rendering.internal.syntax
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@27195 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-rendering/xwiki-rendering-api/src/main/java/org/xwiki/rendering/internal/syntax/DefaultSyntaxFactory.java
+++ xwiki-rendering/xwiki-rendering-api/src/main/java/org/xwiki/rendering/internal/syntax/DefaultSyntaxFactory.java
@@ -17,7 +17,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.xwiki.rendering.internal.parser;
+package org.xwiki.rendering.internal.syntax;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;

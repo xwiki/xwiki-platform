3b005eb4fe8ae8cac0587defc86fb03d76db415f
lucaa <lucaa@xwiki.com>
2010-02-22 11:15:45 +0000

    [misc] Added javadoc for the XWiki Rights service interface.
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@27202 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-core/src/main/java/com/xpn/xwiki/user/api/XWikiRightService.java
+++ xwiki-core/src/main/java/com/xpn/xwiki/user/api/XWikiRightService.java
@@ -44,21 +44,67 @@ public interface XWikiRightService
      */
     public static final String GUEST_USER_FULLNAME = "XWiki.XWikiGuest";
     
+    /**
+     * Checks if the wiki current user has the right to execute (@code action} on the document {@code doc}, along with
+     * redirecting to the login if it's not the case. It returns {@code true} if the user has access, and {@code false}
+     * otherwise <strong> along with requesting the login from the authentication service (redirecting to the login page
+     * in the case of a form authenticator, for example) <strong/>.
+     * 
+     * @param action the action to be executed on the document
+     * @param doc the document to perform action on
+     * @param context the xwiki context in which to perform the verification (from which to get the user, for example)
+     * @return {@code true} if the current user has the right to execute {@code action} on {@code doc}
+     * @throws XWikiException if something goes wrong during the rights checking process
+     */
     public boolean checkAccess(String action, XWikiDocument doc, XWikiContext context) throws XWikiException;
 
+    /**
+     * Verifies if the user identified by {@code username} has the access level identified by {@code right} on the
+     * document with the name {@code docname}.
+     * 
+     * @param right the access level to check (for example, 'view' or 'edit' or 'comment').
+     * @param username the name of the user to check the right for
+     * @param docname the document on which to check the right
+     * @param context the xwiki context in which to perform the verification
+     * @return {@code true} if the user has the specified right on the document, {@code false} otherwise
+     * @throws XWikiException if something goes wrong during the rights checking process
+     */
     public boolean hasAccessLevel(String right, String username, String docname, XWikiContext context)
         throws XWikiException;
 
+    /**
+     * Checks if the author of the current document in the context has programming rights in this wiki (used to
+     * determine if the protected calls in the script contained in the document should be executed or not).
+     * 
+     * @param context the xwiki context of this request
+     * @return {@code true} if the author of the current document in the context has programming rights, {@code false}
+     *         otherwise.
+     */
     public boolean hasProgrammingRights(XWikiContext context);
 
+    /**
+     * Checks if the author of the passed document has programming rights in this wiki (used to determine if the
+     * protected calls in the script contained in the document should be executed or not).
+     * 
+     * @param doc the document to check programming rights for
+     * @param context the xwiki context of this request
+     * @return {@code true} if the author of {@code doc} has programming rights, {@code false} otherwise.
+     */
     public boolean hasProgrammingRights(XWikiDocument doc, XWikiContext context);
 
     /**
-     * Check if the current user has administration rights on the current wiki.
+     * Checks that the current user in the context (the currently authenticated user) has administration rights on the
+     * current wiki.
      * 
-     * @return <code>true</code> if the current user has the <code>admin</code> right or <code>false</code> otherwise.
+     * @param context the xwiki context of this request
+     * @return {@code true} if the current user in the context has the {@code admin} right, {@code false} otherwise
      */
     public boolean hasAdminRights(XWikiContext context);
 
+    /**
+     * @param context the xwiki context of this request
+     * @return the list of all the possible access levels
+     * @throws XWikiException if something goes wrong during the rights checking process
+     */
     public List<String> listAllLevels(XWikiContext context) throws XWikiException;
 }

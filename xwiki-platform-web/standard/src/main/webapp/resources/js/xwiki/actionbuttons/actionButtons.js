ceb1bcf9502234d9348be21a70e60b7d226c19ce
sdumitriu <sergiu@xwiki.com>
2010-03-23 13:23:48 +0000

    XWIKI-5021: "Save and Continue" doesn't work in Preview mode only with IE7 Fixed.
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/web/trunk@27806 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-platform-web/standard/src/main/webapp/resources/js/xwiki/actionbuttons/actionButtons.js
+++ xwiki-platform-web/standard/src/main/webapp/resources/js/xwiki/actionbuttons/actionButtons.js
@@ -143,16 +143,16 @@ XWiki.actionButtons.AjaxSaveAndContinue = Class.create({
       if (typeof (event.memo.originalEvent) != 'undefined') {
         event.memo.originalEvent.stop();
       }
-      this.form = event.memo.form;
+      this.form = $(event.memo.form);
       this.savedBox.hide();
       this.failedBox.hide();
       this.savingBox.show();
-      var formData = new Hash(event.memo.form.serialize({hash: true, submit: 'action_saveandcontinue'}));
+      var formData = new Hash(this.form.serialize({hash: true, submit: 'action_saveandcontinue'}));
       if (!Prototype.Browser.Opera) {
         // Opera can't handle properly 204 responses.
         formData.set('ajax', 'true');
       }
-      new Ajax.Request(event.memo.form.action, {
+      new Ajax.Request(this.form.action, {
         method : 'post',
         parameters : formData.toQueryString(),
         onSuccess : this.onSuccess.bindAsEventListener(this),

51a3ba68965732b8649f7589a5528cea24ec3a66
vmassol <vincent@massol.net>
2010-03-26 10:31:42 +0000

    Moved test from JUnit 3.x to Junit 4.x and to JMock 2.x
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@27942 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-rendering/xwiki-rendering-api/src/test/java/org/xwiki/rendering/internal/parser/XWikiLinkParserTest.java
+++ xwiki-rendering/xwiki-rendering-api/src/test/java/org/xwiki/rendering/internal/parser/XWikiLinkParserTest.java
@@ -19,99 +19,110 @@
  */
 package org.xwiki.rendering.internal.parser;
 
-import org.jmock.Mock;
+import org.jmock.Mockery;
+import org.junit.*;
 import org.xwiki.component.descriptor.ComponentInstantiationStrategy;
 import org.xwiki.component.descriptor.DefaultComponentDescriptor;
 import org.xwiki.rendering.listener.Link;
 import org.xwiki.rendering.listener.LinkType;
 import org.xwiki.rendering.parser.LinkParser;
 import org.xwiki.rendering.wiki.WikiModel;
-import org.xwiki.test.AbstractXWikiComponentTestCase;
+import org.xwiki.test.AbstractComponentTestCase;
 
 /**
  * @version $Id$
  * @since 1.5M2
  */
-public class XWikiLinkParserTest extends AbstractXWikiComponentTestCase
+public class XWikiLinkParserTest extends AbstractComponentTestCase
 {
-    public void testParseLinksWhenInWikiMode() throws Exception
+    private LinkParser parser;
+
+    private Mockery mockery = new Mockery();
+
+    @Override
+    protected void registerComponents() throws Exception
     {
         // Create a Mock WikiModel implementation so that the link parser works in wiki mode
-        Mock mockWikiModel = mock(WikiModel.class);
+        WikiModel mockWikiModel = this.mockery.mock(WikiModel.class); 
 
         DefaultComponentDescriptor<WikiModel> componentDescriptor = new DefaultComponentDescriptor<WikiModel>();
         componentDescriptor.setRole(WikiModel.class);
         componentDescriptor.setInstantiationStrategy(ComponentInstantiationStrategy.SINGLETON);
         componentDescriptor.setImplementation(null);
 
-        getComponentManager().registerComponent(componentDescriptor, (WikiModel) mockWikiModel.proxy());
-        LinkParser parser = getComponentManager().lookup(LinkParser.class, "xwiki/2.0");
+        getComponentManager().registerComponent(componentDescriptor, mockWikiModel);
+        this.parser = getComponentManager().lookup(LinkParser.class, "xwiki/2.0");
+    }
+
+    @Test
+    public void testParseLinksWhenInWikiMode() throws Exception
+    {
 
         Link link = parser.parse("");
-        assertEquals("", link.getReference());
-        assertEquals("Reference = []", link.toString());
+        Assert.assertEquals("", link.getReference());
+        Assert.assertEquals("Reference = []", link.toString());
 
         link = parser.parse("Hello World");
-        assertEquals("Hello World", link.getReference());
-        assertEquals(LinkType.DOCUMENT, link.getType());
-        assertEquals("Reference = [Hello World]", link.toString());
+        Assert.assertEquals("Hello World", link.getReference());
+        Assert.assertEquals(LinkType.DOCUMENT, link.getType());
+        Assert.assertEquals("Reference = [Hello World]", link.toString());
 
         link = parser.parse("HelloWorld#anchor?param1=1&param2=2@wikipedia");
-        assertEquals("HelloWorld", link.getReference());
-        assertEquals("anchor", link.getAnchor());
-        assertEquals("param1=1&param2=2", link.getQueryString());
-        assertEquals("wikipedia", link.getInterWikiAlias());
-        assertEquals("Reference = [HelloWorld] QueryString = [param1=1&param2=2] "
+        Assert.assertEquals("HelloWorld", link.getReference());
+        Assert.assertEquals("anchor", link.getAnchor());
+        Assert.assertEquals("param1=1&param2=2", link.getQueryString());
+        Assert.assertEquals("wikipedia", link.getInterWikiAlias());
+        Assert.assertEquals("Reference = [HelloWorld] QueryString = [param1=1&param2=2] "
             + "Anchor = [anchor] InterWikiAlias = [wikipedia]", link.toString());
 
         link = parser.parse("Hello World?xredirect=../whatever");
-        assertEquals("Hello World", link.getReference());
-        assertEquals("xredirect=../whatever", link.getQueryString());
-        assertEquals("Reference = [Hello World] QueryString = [xredirect=../whatever]", link.toString());
+        Assert.assertEquals("Hello World", link.getReference());
+        Assert.assertEquals("xredirect=../whatever", link.getQueryString());
+        Assert.assertEquals("Reference = [Hello World] QueryString = [xredirect=../whatever]", link.toString());
 
         link = parser.parse("HelloWorld?xredirect=http://xwiki.org");
-        assertEquals("HelloWorld", link.getReference());
-        assertEquals("xredirect=http://xwiki.org", link.getQueryString());
-        assertEquals("Reference = [HelloWorld] QueryString = [xredirect=http://xwiki.org]", link.toString());
+        Assert.assertEquals("HelloWorld", link.getReference());
+        Assert.assertEquals("xredirect=http://xwiki.org", link.getQueryString());
+        Assert.assertEquals("Reference = [HelloWorld] QueryString = [xredirect=http://xwiki.org]", link.toString());
 
         link = parser.parse("http://xwiki.org");
-        assertEquals("http://xwiki.org", link.getReference());
-        assertEquals(LinkType.URI, link.getType());
-        assertEquals("Reference = [http://xwiki.org]", link.toString());
+        Assert.assertEquals("http://xwiki.org", link.getReference());
+        Assert.assertEquals(LinkType.URI, link.getType());
+        Assert.assertEquals("Reference = [http://xwiki.org]", link.toString());
 
         link = parser.parse("#anchor");
-        assertEquals("anchor", link.getAnchor());
-        assertEquals("Reference = [] Anchor = [anchor]", link.toString());
+        Assert.assertEquals("anchor", link.getAnchor());
+        Assert.assertEquals("Reference = [] Anchor = [anchor]", link.toString());
 
         link = parser.parse("Hello#anchor");
-        assertEquals("Hello", link.getReference());
-        assertEquals("anchor", link.getAnchor());
-        assertEquals("Reference = [Hello] Anchor = [anchor]", link.toString());
+        Assert.assertEquals("Hello", link.getReference());
+        Assert.assertEquals("anchor", link.getAnchor());
+        Assert.assertEquals("Reference = [Hello] Anchor = [anchor]", link.toString());
 
         // Verify mailto: URI is recognized
         link = parser.parse("mailto:john@smith.com");
-        assertEquals("mailto:john@smith.com", link.getReference());
-        assertEquals(LinkType.URI, link.getType());
-        assertEquals("Reference = [mailto:john@smith.com]", link.toString());
+        Assert.assertEquals("mailto:john@smith.com", link.getReference());
+        Assert.assertEquals(LinkType.URI, link.getType());
+        Assert.assertEquals("Reference = [mailto:john@smith.com]", link.toString());
 
         // Verify image: URI is recognized
         link = parser.parse("image:some:content");
-        assertEquals("image:some:content", link.getReference());
-        assertEquals(LinkType.URI, link.getType());
-        assertEquals("Reference = [image:some:content]", link.toString());
+        Assert.assertEquals("image:some:content", link.getReference());
+        Assert.assertEquals(LinkType.URI, link.getType());
+        Assert.assertEquals("Reference = [image:some:content]", link.toString());
 
         // Verify attach: URI is recognized
         link = parser.parse("attach:some:content");
-        assertEquals("attach:some:content", link.getReference());
-        assertEquals(LinkType.URI, link.getType());
-        assertEquals("Reference = [attach:some:content]", link.toString());
+        Assert.assertEquals("attach:some:content", link.getReference());
+        Assert.assertEquals(LinkType.URI, link.getType());
+        Assert.assertEquals("Reference = [attach:some:content]", link.toString());
 
         // Verify that unknown URIs are ignored
         // Note: We consider that myxwiki is the wiki name and http://xwiki.org is the page name
         link = parser.parse("mywiki:http://xwiki.org");
-        assertEquals("mywiki:http://xwiki.org", link.getReference());
-        assertEquals(LinkType.DOCUMENT, link.getType());
-        assertEquals("Reference = [mywiki:http://xwiki.org]", link.toString());
+        Assert.assertEquals("mywiki:http://xwiki.org", link.getReference());
+        Assert.assertEquals(LinkType.DOCUMENT, link.getType());
+        Assert.assertEquals("Reference = [mywiki:http://xwiki.org]", link.toString());
 
     }
 }

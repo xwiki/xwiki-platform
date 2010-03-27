19ab65425a41932212c15dcadcb48cfbc7b35ae9
vmassol <vincent@massol.net>
2010-03-27 14:14:54 +0000

    XWIKI-4957: Allow entering @, ? and # characters in link references
    * Added support for escaping the escape char. Thanks to Sergiu for catching that it was missing in my previous commits.
    
    Merged from trunk (rev 27957)
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/branches/xwiki-core-2.2@27958 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-rendering/xwiki-rendering-api/src/test/java/org/xwiki/rendering/internal/parser/XWikiLinkParserTest.java
+++ xwiki-rendering/xwiki-rendering-api/src/test/java/org/xwiki/rendering/internal/parser/XWikiLinkParserTest.java
@@ -138,5 +138,12 @@ public class XWikiLinkParserTest extends AbstractComponentTestCase
         link = parser.parse("page\\@notinterwiki");
         Assert.assertEquals("page@notinterwiki", link.getReference());
         Assert.assertNull(link.getInterWikiAlias());
+
+        // Verify that \ can be escaped and that escaped chars in query string, anchors and interwikilinks are escaped
+        link = parser.parse("page\\\\#anchor\\\\?querystring\\\\@interwikilink\\\\");
+        Assert.assertEquals("page\\\\", link.getReference());
+        Assert.assertEquals("anchor\\", link.getAnchor());
+        Assert.assertEquals("querystring\\", link.getQueryString());
+        Assert.assertEquals("interwikilink\\", link.getInterWikiAlias());
     }
 }

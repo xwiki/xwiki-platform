0803fe6f6bc08ece5c5fe7d3871876b5a59115b3
dgervalle <dgl@softec.lu>
2010-04-20 20:39:55 +0000

    XWIKI-5004 - Inactive user should be able to access the same UI than XWikiGuest XWIKI-4378 - Should never deny resources access through skin URL
    Merged from trunk #28470 and #27365
    
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/branches/xwiki-core-2.2@28472 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-core/src/main/java/com/xpn/xwiki/XWiki.java
+++ xwiki-core/src/main/java/com/xpn/xwiki/XWiki.java
@@ -3966,7 +3966,7 @@ public class XWiki implements XWikiDocChangeNotificationInterface
 
     public boolean checkAccess(String action, XWikiDocument doc, XWikiContext context) throws XWikiException
     {
-        if (action.equals("skin") && doc.getSpace().equals("skins")) {
+        if (action.equals("skin") && (doc.getSpace().equals("skins") || doc.getSpace().equals("resources"))) {
             // We still need to call checkAuth to set the proper user.
             XWikiUser user = checkAuth(context);
             if (user != null) {
@@ -5157,15 +5157,32 @@ public class XWiki implements XWikiDocChangeNotificationInterface
             setPhonyDocument(docName, context, vcontext);
             throw new XWikiException(XWikiException.MODULE_XWIKI_ACCESS, XWikiException.ERROR_XWIKI_ACCESS_DENIED,
                 "Access to document {0} has been denied to user {1}", null, args);
-        } else if (checkActive(context) == 0) {
+        } else if (checkActive(context) == 0) {  // if auth_active_check, check if user is inactive
             boolean allow = false;
-            String allowed = Param("xwiki.inactiveuser.allowedpages", "");
-            if (context.getAction().equals("view") && !allowed.equals("")) {
-                String[] allowedList = StringUtils.split(allowed, " ,");
-                for (int i = 0; i < allowedList.length; i++) {
-                    if (allowedList[i].equals(doc.getFullName())) {
-                        allow = true;
-                        break;
+            String action = context.getAction();
+            /*
+             * Allow inactive users to see skins, ressources, SSX, JSX and downloads they could have seen as guest. The
+             * rational behind this behaviour is that inactive users should be able to access the same UI that guests
+             * are used to see, including custom icons, panels, and so on...
+             */
+            if ((action.equals("skin") && (doc.getSpace().equals("skins") || doc.getSpace().equals("resources")))
+                ||
+                ((action.equals("skin") || action.equals("download") || action.equals("ssx") || action.equals("jsx")) &&
+                    getRightService()
+                        .hasAccessLevel("view", XWikiRightService.GUEST_USER_FULLNAME, doc.getPrefixedFullName(),
+                            context))
+                || ((action.equals("view") && doc.getFullName().equals("XWiki.AccountValidation"))))
+            {
+                allow = true;
+            } else {
+                String allowed = Param("xwiki.inactiveuser.allowedpages", "");
+                if (context.getAction().equals("view") && !allowed.equals("")) {
+                    String[] allowedList = StringUtils.split(allowed, " ,");
+                    for (int i = 0; i < allowedList.length; i++) {
+                        if (allowedList[i].equals(doc.getFullName())) {
+                            allow = true;
+                            break;
+                        }
                     }
                 }
             }

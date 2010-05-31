ade4c45de0cc0650390d54b7ebb8aa49d2480fbe
asiri <asiri@xwiki.com>
2010-05-31 10:55:04 +0000

    XWIKI-5227: Introduce a temp resource action.
    * Temporary files should not be shared across multiple wikis, so we need to group them by wiki name.
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@29066 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-core/src/main/java/com/xpn/xwiki/web/TempResourceAction.java
+++ xwiki-core/src/main/java/com/xpn/xwiki/web/TempResourceAction.java
@@ -75,7 +75,7 @@ public class TempResourceAction extends XWikiAction
         String uri = request.getRequestURI();
 
         // Locate the temporary file.
-        File tempFile = getTemporaryFile(uri);
+        File tempFile = getTemporaryFile(uri, context);
         if (null == tempFile) {
             throw new XWikiException(XWikiException.MODULE_XWIKI_APP, XWikiException.ERROR_XWIKI_APP_URL_EXCEPTION,
                 "Invalid temporary resource URL");
@@ -105,18 +105,20 @@ public class TempResourceAction extends XWikiAction
      * Returns the temporary file corresponding to the specified URI.
      * 
      * @param uri request URI.
+     * @param context xwiki context.
      * @return temporary file corresponding to the specified URI or null if no such file can be located.
      */
-    private File getTemporaryFile(String uri)
+    private File getTemporaryFile(String uri, XWikiContext context)
     {
         Matcher matcher = URI_PATTERN.matcher(uri);
         File result = null;
         if (matcher.find()) {
+            String wiki = context.getWiki().getDatabase();
             String space = matcher.group(1);
             String page = matcher.group(2);
             String module = matcher.group(3);
             String filePath = matcher.group(4);
-            String prefix = String.format("temp/%s/%s/%s/", module, space, page);
+            String prefix = String.format("temp/%s/%s/%s/%s/", module, wiki, space, page);
             String path = URI.create(prefix + filePath).normalize().toString();
             if (path.startsWith(prefix)) {
                 result = new File(container.getApplicationContext().getTemporaryDirectory(), path);

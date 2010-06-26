e0c60f5a99f50f607c6a6b453a13f7c6b588d48d
vmassol <vincent@massol.net>
2010-06-26 19:20:04 +0000

    Seems the IDEA codestyle needs a fix for the checkstyle {} rule ( '}' is not preceded with whitespace.)
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@29762 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-query/manager/src/main/java/org/xwiki/query/internal/SecureQueryExecutorManager.java
+++ xwiki-query/manager/src/main/java/org/xwiki/query/internal/SecureQueryExecutorManager.java
@@ -37,7 +37,7 @@ import org.xwiki.query.QueryExecutorManager;
 //Note that we force the Component annotation so that this component is only registered as a QueryExecutorManager
 //and not a QueryExecutor too since we don't want this manager to be visible to users as a valid QueryExecutor
 //component.
-@Component(value = "secure", roles = {QueryExecutorManager.class})
+@Component(value = "secure", roles = { QueryExecutorManager.class })
 public class SecureQueryExecutorManager implements QueryExecutorManager
 {
     /**

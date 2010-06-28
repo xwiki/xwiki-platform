c0ea04205042f6a342b4a9df576100ddc0110d26
tmortagne <thomas.mortagne@xwiki.com>
2010-06-28 11:52:32 +0000

    [cleanup] Remove unused imports
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@29780 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-rendering/xwiki-rendering-syntaxes/xwiki-rendering-syntax-wikimodel/src/main/java/org/xwiki/rendering/internal/parser/wikimodel/XWikiGeneratorListener.java
+++ xwiki-rendering/xwiki-rendering-syntaxes/xwiki-rendering-syntax-wikimodel/src/main/java/org/xwiki/rendering/internal/parser/wikimodel/XWikiGeneratorListener.java
@@ -19,12 +19,10 @@
  */
 package org.xwiki.rendering.internal.parser.wikimodel;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.ListIterator;
 import java.util.Map;
 import java.util.Set;
 import java.util.Stack;

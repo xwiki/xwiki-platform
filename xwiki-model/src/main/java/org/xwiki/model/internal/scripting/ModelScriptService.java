37dd510c83484d18dd1943d930029fad734bc649
mflorea <mariusdumitru.florea@xwiki.com>
2010-09-24 11:52:12 +0000

    XWIKI-5519: Extend model service with methods for creating and resolving attachment references
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@31348 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-model/src/main/java/org/xwiki/model/internal/scripting/ModelScriptService.java
+++ xwiki-model/src/main/java/org/xwiki/model/internal/scripting/ModelScriptService.java
@@ -25,9 +25,11 @@ import org.xwiki.component.annotation.Requirement;
 import org.xwiki.component.manager.ComponentLookupException;
 import org.xwiki.component.manager.ComponentManager;
 import org.xwiki.model.EntityType;
+import org.xwiki.model.reference.AttachmentReference;
 import org.xwiki.model.reference.DocumentReference;
 import org.xwiki.model.reference.DocumentReferenceResolver;
 import org.xwiki.model.reference.EntityReference;
+import org.xwiki.model.reference.EntityReferenceResolver;
 import org.xwiki.model.reference.EntityReferenceSerializer;
 import org.xwiki.script.service.ScriptService;
 
@@ -40,6 +42,14 @@ import org.xwiki.script.service.ScriptService;
 @Component("model")
 public class ModelScriptService implements ScriptService
 {
+    /**
+     * The default hint used when resolving string references.
+     */
+    private static final String DEFAULT_STRING_RESOLVER_HINT = "currentmixed";
+
+    /**
+     * Used to dynamically look up component implementations based on a given hint.
+     */
     @Requirement
     private ComponentManager componentManager;
 
@@ -49,7 +59,7 @@ public class ModelScriptService implements ScriptService
      * 
      * @param wiki the wiki reference name to use (can be empty or null)
      * @param space the space reference name to use (can be empty or null)
-     * @param page the page referene name to use (can be empty or null)
+     * @param page the page reference name to use (can be empty or null)
      * @return the typed Document Reference object or null if no Resolver with the passed hint could be found
      * @since 2.3M2
      */
@@ -65,10 +75,11 @@ public class ModelScriptService implements ScriptService
      * 
      * @param wiki the wiki reference name to use (can be empty or null)
      * @param space the space reference name to use (can be empty or null)
-     * @param page the page referene name to use (can be empty or null)
+     * @param page the page reference name to use (can be empty or null)
      * @param hint the hint of the Resolver to use in case any parameter is empty or null
      * @return the typed Document Reference object or null if no Resolver with the passed hint could be found
      */
+    @SuppressWarnings("unchecked")
     public DocumentReference createDocumentReference(String wiki, String space, String page, String hint)
     {
         EntityReference reference = null;
@@ -92,32 +103,80 @@ public class ModelScriptService implements ScriptService
     }
 
     /**
-     * @param stringRepresentation the document reference specified as a String (using the "wiki:space.page" format and
-     *            with special characters escaped where required).
-     * @return the typed Document Reference object (resolved using the "currentmixed" resolver)
-     * @since 2.3M2
+     * Creates an {@link AttachmentReference} from a file name and a reference to the document holding that file.
+     * 
+     * @param documentReference a reference to the document the file is attached to
+     * @param fileName the name of a file attached to a document
+     * @return a reference to the specified attachment
+     * @since 2.5M2
      */
-    public DocumentReference resolveDocument(String stringRepresentation)
+    public AttachmentReference createAttachmentReference(DocumentReference documentReference, String fileName)
     {
-        return resolveDocument(stringRepresentation, "currentmixed");
+        return new AttachmentReference(fileName, documentReference);
     }
 
     /**
      * @param stringRepresentation the document reference specified as a String (using the "wiki:space.page" format and
-     *            with special characters escaped where required).
+     *            with special characters escaped where required)
+     * @return the typed Document Reference object (resolved using the {@value #DEFAULT_STRING_RESOLVER_HINT} resolver)
+     * @since 2.3M2
+     */
+    public DocumentReference resolveDocument(String stringRepresentation)
+    {
+        return resolveDocument(stringRepresentation, DEFAULT_STRING_RESOLVER_HINT);
+    }
+
+    /**
+     * @param stringRepresentation the document reference specified as a String (using the "wiki:space.page" format and
+     *            with special characters escaped where required)
      * @param hint the hint of the Resolver to use in case any part of the reference is missing (no wiki specified, no
      *            space or no page)
+     * @param parameters extra parameters to pass to the resolver; you can use these parameters to resolve a document
+     *            reference relative to another entity reference
      * @return the typed Document Reference object or null if no Resolver with the passed hint could be found
      */
-    public DocumentReference resolveDocument(String stringRepresentation, String hint)
+    @SuppressWarnings("unchecked")
+    public DocumentReference resolveDocument(String stringRepresentation, String hint, Object... parameters)
     {
-        DocumentReference result;
         try {
-            result = this.componentManager.lookup(DocumentReferenceResolver.class, hint).resolve(stringRepresentation);
+            EntityReferenceResolver<String> resolver = componentManager.lookup(EntityReferenceResolver.class, hint);
+            return new DocumentReference(resolver.resolve(stringRepresentation, EntityType.DOCUMENT, parameters));
         } catch (ComponentLookupException e) {
-            result = null;
+            return null;
         }
-        return result;
+    }
+
+    /**
+     * @param stringRepresentation an attachment reference specified as {@link String} (using the "wiki:space.page@file"
+     *            format and with special characters escaped where required)
+     * @return the corresponding typed {@link AttachmentReference} object (resolved using the
+     *         {@value #DEFAULT_STRING_RESOLVER_HINT} resolver)
+     * @since 2.5M2
+     */
+    public AttachmentReference resolveAttachment(String stringRepresentation)
+    {
+        return resolveAttachment(stringRepresentation, DEFAULT_STRING_RESOLVER_HINT);
+    }
+
+    /**
+     * @param stringRepresentation an attachment reference specified as {@link String} (using the "wiki:space.page@file"
+     *            format and with special characters escaped where required)
+     * @param hint the hint of the resolver to use in case any part of the reference is missing (no wiki specified, no
+     *            space or no page)
+     * @param parameters extra parameters to pass to the resolver; you can use these parameters to resolve an attachment
+     *            reference relative to another entity reference
+     * @return the corresponding typed {@link AttachmentReference} object
+     * @since 2.5M2
+     */
+    @SuppressWarnings("unchecked")
+    public AttachmentReference resolveAttachment(String stringRepresentation, String hint, Object... parameters)
+    {
+        try {
+            EntityReferenceResolver<String> resolver = componentManager.lookup(EntityReferenceResolver.class, hint);
+            return new AttachmentReference(resolver.resolve(stringRepresentation, EntityType.ATTACHMENT, parameters));
+        } catch (ComponentLookupException e) {
+            return null;
+        }
     }
 
     /**

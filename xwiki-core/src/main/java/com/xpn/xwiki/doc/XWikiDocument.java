6362f6cb4a81819cbfa2858f8995c2668f2a1c1a
tmortagne <thomas.mortagne@xwiki.com>
2010-11-17 14:22:29 +0000

    XWIKI-5700: XWikiDocument#setXClass should make sure the class has the proper document reference
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@32935 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-core/src/main/java/com/xpn/xwiki/doc/XWikiDocument.java
+++ xwiki-core/src/main/java/com/xpn/xwiki/doc/XWikiDocument.java
@@ -1897,6 +1897,8 @@ public class XWikiDocument implements DocumentModelBridge
      */
     public void setXClass(BaseClass xwikiClass)
     {
+        xwikiClass.setDocumentReference(getDocumentReference());
+
         this.xClass = xwikiClass;
     }
 

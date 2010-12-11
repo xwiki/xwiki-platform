244866371c919e1f7451d04c19c22f91a06a53e3
mflorea <mariusdumitru.florea@xwiki.com>
2010-12-11 17:10:59 +0000

    XWIKI-5784: TempResource action fails in multiwiki mode Merged from trunk, revision 33351 and 33352.
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/branches/xwiki-core-2.6@33354 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-core/src/main/java/com/xpn/xwiki/web/TempResourceAction.java
+++ xwiki-core/src/main/java/com/xpn/xwiki/web/TempResourceAction.java
@@ -108,12 +108,12 @@ public class TempResourceAction extends XWikiAction
      * @param context xwiki context.
      * @return temporary file corresponding to the specified URI or null if no such file can be located.
      */
-    private File getTemporaryFile(String uri, XWikiContext context)
+    protected File getTemporaryFile(String uri, XWikiContext context)
     {
         Matcher matcher = URI_PATTERN.matcher(uri);
         File result = null;
         if (matcher.find()) {
-            String wiki = context.getWiki().getDatabase();
+            String wiki = context.getDatabase();
             String space = matcher.group(1);
             String page = matcher.group(2);
             String module = matcher.group(3);

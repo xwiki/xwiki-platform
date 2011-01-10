c4b53296bea8a598a274d237b192e5ecd7a66c1a
lucaa <lucaa@xwiki.com>
2011-01-10 10:56:58 +0000

    XSKINX-47: Implement a bridge to the skin extension API
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/xwiki-plugins/trunk@33873 f329d543-caf0-0310-9063-dda96c69346f

---
+++ xwiki-platform-skinx/src/main/java/org/xwiki/skinx/AbstractWrapperSkinExtension.java
@@ -0,0 +1,81 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.skinx;
+
+import java.util.Map;
+
+import org.xwiki.component.annotation.Requirement;
+import org.xwiki.context.Execution;
+
+import com.xpn.xwiki.XWiki;
+import com.xpn.xwiki.XWikiContext;
+import com.xpn.xwiki.plugin.skinx.SkinExtensionPluginApi;
+
+/**
+ * The abstract implementation of the wrapper around the skinx plugin. Provides the mechanism needed to grab the skin
+ * extensions plugins and call the use methods on them, subclasses only need to provide the name of the skin extension.
+ * 
+ * @version $Id$
+ * @since 3.0M1
+ */
+public abstract class AbstractWrapperSkinExtension implements SkinExtension
+{
+
+    /**
+     * Execution context, needed to grab the sx plugins to include js and css.
+     */
+    @Requirement
+    private Execution execution;
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.xwiki.skinx.SkinExtension#use(java.lang.String)
+     */
+    public void use(String resource)
+    {
+        getSkinExtensionPluginApi().use(resource);
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.xwiki.skinx.SkinExtension#use(java.lang.String, java.util.Map)
+     */
+    public void use(String resource, Map<String, Object> parameters)
+    {
+        getSkinExtensionPluginApi().use(resource, parameters);
+    }
+
+    /**
+     * @return the {@link SkinExtensionPluginApi} in the running wiki.
+     */
+    private SkinExtensionPluginApi getSkinExtensionPluginApi()
+    {
+        XWikiContext xwikiContext = (XWikiContext) execution.getContext().getProperty("xwikicontext");
+        XWiki wiki = xwikiContext.getWiki();
+        return (SkinExtensionPluginApi) wiki.getPluginApi(getName(), xwikiContext);
+    }
+
+    /**
+     * @return the name of the skin extension (e.g. ssx, jsfx, etc) to wrap.
+     */
+    public abstract String getName();
+}

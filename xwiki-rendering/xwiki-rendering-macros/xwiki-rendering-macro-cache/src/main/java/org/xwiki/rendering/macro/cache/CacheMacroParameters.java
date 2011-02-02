a5bc963211e9762232dea764ec6262495d6f10b7
vmassol <vincent@massol.net>
2011-02-02 11:17:42 +0000

    XWIKI-4322: Add a {{cache}} macro Merged from 2.5 branch (rev 34152)
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/branches/xwiki-core-2.7@34351 f329d543-caf0-0310-9063-dda96c69346f

---
+++ xwiki-rendering/xwiki-rendering-macros/xwiki-rendering-macro-cache/src/main/java/org/xwiki/rendering/macro/cache/CacheMacroParameters.java
@@ -0,0 +1,56 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.rendering.macro.cache;
+
+import org.xwiki.properties.annotation.PropertyDescription;
+
+/**
+ * Parameters for the Cache macro.
+ *
+ * @version $Id$
+ */
+public class CacheMacroParameters
+{
+    /**
+     * @see #getId()
+     */
+    private String id;
+
+    /**
+     * @return the optional unique id to use to cache the content. If not defined then use the content itself as the id
+     *         but this doesn't guarantee unicity since the same content could be located on several pages with
+     *         different results. Also note that the id is considered containing wiki syntax; this is done so that the
+     *         user can use script macros to generate the id
+     */
+    public String getId()
+    {
+        return this.id;
+    }
+
+    /**
+     * @param id refer to {@link #getId()}
+     */
+    @PropertyDescription("a unique id under which the content is cached")
+    public void setId(String id)
+    {
+        this.id = id;
+    }
+
+}
---
+++ xwiki-rendering/xwiki-rendering-macros/xwiki-rendering-macro-cache/src/test/java/org/xwiki/rendering/internal/macro/cache/CacheMacroTest.java
@@ -0,0 +1,189 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.rendering.internal.macro.cache;
+
+import java.io.StringWriter;
+import java.util.List;
+
+import org.junit.*;
+import org.xwiki.rendering.block.Block;
+import org.xwiki.rendering.internal.transformation.MacroTransformation;
+import org.xwiki.rendering.macro.Macro;
+import org.xwiki.rendering.macro.cache.CacheMacroParameters;
+import org.xwiki.rendering.macro.script.ScriptMockSetup;
+import org.xwiki.rendering.renderer.PrintRendererFactory;
+import org.xwiki.rendering.syntax.Syntax;
+import org.xwiki.rendering.transformation.MacroTransformationContext;
+import org.xwiki.rendering.transformation.Transformation;
+import org.xwiki.test.AbstractComponentTestCase;
+import org.xwiki.velocity.VelocityManager;
+
+import static org.xwiki.rendering.scaffolding.BlockAssert.*;
+
+/**
+ * Unit tests for {@link CacheMacro}.
+ *
+ * @version $Id$
+ */
+public class CacheMacroTest extends AbstractComponentTestCase
+{
+    private ScriptMockSetup mockSetup;
+
+    private CacheMacro cacheMacro;
+
+    private PrintRendererFactory rendererFactory;
+
+    @Override
+    protected void registerComponents() throws Exception
+    {
+        super.registerComponents();
+
+        this.mockSetup = new ScriptMockSetup(getMockery(), getComponentManager());
+        this.cacheMacro = (CacheMacro) getComponentManager().lookup(Macro.class, "cache");
+        this.rendererFactory = getComponentManager().lookup(PrintRendererFactory.class, "event/1.0");
+    }
+
+    @Test
+    public void testExecuteWhenNoIdAndSameContent() throws Exception
+    {
+        String expected = "beginDocument\n"
+            + "beginMacroMarkerStandalone [velocity] [] [$var]\n"
+            + "beginParagraph\n"
+            + "onWord [content]\n"
+            + "endParagraph\n"
+            + "endMacroMarkerStandalone [velocity] [] [$var]\n"
+            + "endDocument";
+
+        CacheMacroParameters params = new CacheMacroParameters();
+        MacroTransformationContext context = createMacroTransformationContext();
+
+        VelocityManager velocityManager = getComponentManager().lookup(VelocityManager.class);
+        StringWriter writer = new StringWriter();
+        velocityManager.getVelocityEngine().evaluate(velocityManager.getVelocityContext(), writer, "template",
+            "#set ($var = 'content')");
+
+        List<Block> result = this.cacheMacro.execute(params, "{{velocity}}$var{{/velocity}}", context);
+        assertBlocks(expected, result, this.rendererFactory);
+
+        // Execute a second time with a different value for the velocity $var variable to ensure the returned result
+        // is the cached one.
+        velocityManager.getVelocityEngine().evaluate(velocityManager.getVelocityContext(), writer, "template",
+            "#set ($var = 'newcontent')");
+        result = this.cacheMacro.execute(params, "{{velocity}}$var{{/velocity}}", context);
+        assertBlocks(expected, result, this.rendererFactory);
+    }
+
+    @Test
+    public void testExecuteWhenNoIdAndDifferentContent() throws Exception
+    {
+        String expected1 = "beginDocument\n"
+            + "beginMacroMarkerStandalone [velocity] [] [$var]\n"
+            + "beginParagraph\n"
+            + "onWord [content]\n"
+            + "endParagraph\n"
+            + "endMacroMarkerStandalone [velocity] [] [$var]\n"
+            + "endDocument";
+
+        String expected2 = "beginDocument\n"
+            + "beginMacroMarkerStandalone [velocity] [] [$var##]\n"
+            + "beginParagraph\n"
+            + "onWord [newcontent]\n"
+            + "endParagraph\n"
+            + "endMacroMarkerStandalone [velocity] [] [$var##]\n"
+            + "endDocument";
+
+        CacheMacroParameters params = new CacheMacroParameters();
+        MacroTransformationContext context = createMacroTransformationContext();
+
+        VelocityManager velocityManager = getComponentManager().lookup(VelocityManager.class);
+        StringWriter writer = new StringWriter();
+        velocityManager.getVelocityEngine().evaluate(velocityManager.getVelocityContext(), writer, "template",
+            "#set ($var = 'content')");
+
+        List<Block> result = this.cacheMacro.execute(params, "{{velocity}}$var{{/velocity}}", context);
+        assertBlocks(expected1, result, this.rendererFactory);
+
+        // Execute a second time with a different cache macro content to ensure it's not cached
+        velocityManager.getVelocityEngine().evaluate(velocityManager.getVelocityContext(), writer, "template",
+            "#set ($var = 'newcontent')");
+        result = this.cacheMacro.execute(params, "{{velocity}}$var##{{/velocity}}", context);
+        assertBlocks(expected2, result, this.rendererFactory);
+    }
+
+    @Test
+    public void testExecuteWhenSameIdAndDifferentContent() throws Exception
+    {
+        String expected = "beginDocument\n"
+            + "beginMacroMarkerStandalone [velocity] [] [$var]\n"
+            + "beginParagraph\n"
+            + "onWord [content]\n"
+            + "endParagraph\n"
+            + "endMacroMarkerStandalone [velocity] [] [$var]\n"
+            + "endDocument";
+
+        CacheMacroParameters params = new CacheMacroParameters();
+        params.setId("uniqueid");
+        MacroTransformationContext context = createMacroTransformationContext();
+
+        VelocityManager velocityManager = getComponentManager().lookup(VelocityManager.class);
+        StringWriter writer = new StringWriter();
+        velocityManager.getVelocityEngine().evaluate(velocityManager.getVelocityContext(), writer, "template",
+            "#set ($var = 'content')");
+
+        List<Block> result = this.cacheMacro.execute(params, "{{velocity}}$var{{/velocity}}", context);
+        assertBlocks(expected, result, this.rendererFactory);
+
+        // Execute a second time with a different content but with the same id, to ensure the returned result
+        // is the cached one.
+        result = this.cacheMacro.execute(params, "whatever here...", context);
+        assertBlocks(expected, result, this.rendererFactory);
+    }
+
+    @Test
+    public void testExecuteWithIdGeneratedByVelocityMacro() throws Exception
+    {
+        VelocityManager velocityManager = getComponentManager().lookup(VelocityManager.class);
+        StringWriter writer = new StringWriter();
+        velocityManager.getVelocityEngine().evaluate(velocityManager.getVelocityContext(), writer, "template",
+            "#set ($var = 'generatedid')");
+
+        CacheMacroParameters params = new CacheMacroParameters();
+        params.setId("{{velocity}}$var{{/velocity}}");
+        MacroTransformationContext context = createMacroTransformationContext();
+
+        List<Block> result1 = this.cacheMacro.execute(params, "whatever", context);
+
+        // Execute a second time with the same id (specified explicitly this time) and ensures that the returned result
+        // is the same even the cache macro content is different.
+        params.setId("generatedid");
+        List<Block> result2 = this.cacheMacro.execute(params, "something else", context);
+        Assert.assertEquals(result1, result2);
+    }
+
+    private MacroTransformationContext createMacroTransformationContext() throws Exception
+    {
+        MacroTransformation macroTransformation =
+            (MacroTransformation) getComponentManager().lookup(Transformation.class, "macro");
+        MacroTransformationContext context = new MacroTransformationContext();
+        context.setTransformation(macroTransformation);
+        context.setSyntax(Syntax.XWIKI_2_0);
+        return context;
+    }
+}

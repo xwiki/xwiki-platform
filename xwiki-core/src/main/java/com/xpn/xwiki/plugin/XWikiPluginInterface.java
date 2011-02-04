b34701f9fd9dbe788273f96a3db5387cbe1239ce
sdumitriu <sergiu@xwiki.com>
2011-02-04 00:53:20 +0000

    [cleanup] Applied codestyle
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@34398 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-core/src/main/java/com/xpn/xwiki/plugin/XWikiPluginInterface.java
+++ xwiki-core/src/main/java/com/xpn/xwiki/plugin/XWikiPluginInterface.java
@@ -16,9 +16,7 @@
  * License along with this software; if not, write to the Free
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
- *
  */
-
 package com.xpn.xwiki.plugin;
 
 import com.xpn.xwiki.XWikiContext;
@@ -26,49 +24,56 @@ import com.xpn.xwiki.XWikiException;
 import com.xpn.xwiki.api.Api;
 import com.xpn.xwiki.doc.XWikiAttachment;
 
-public interface XWikiPluginInterface {
+public interface XWikiPluginInterface
+{
     String getClassName();
+
     String getName();
 
     void setClassName(String name);
+
     void setName(String name);
+
     void init(XWikiContext context) throws XWikiException;
+
     void virtualInit(XWikiContext context);
 
     /*
-    Called to flush cache
-    */
+     * Called to flush cache
+     */
     void flushCache(XWikiContext context);
 
-    /**
-     * @deprecated use flushCache(XWikiContext context) instead
-     * @see #flushCache(XWikiContext)
-     */
-    @Deprecated
-    void flushCache();
     void beginRendering(XWikiContext context);
+
     void endRendering(XWikiContext context);
+
     /*
-    Called at the begin of each request
-    */
+     * Called at the begin of each request
+     */
     void beginParsing(XWikiContext context);
+
     /*
-    Called at the end of each request
-    */
+     * Called at the end of each request
+     */
     String endParsing(String content, XWikiContext context);
 
     String commonTagsHandler(String line, XWikiContext context);
+
     String startRenderingHandler(String line, XWikiContext context);
+
     String outsidePREHandler(String line, XWikiContext context);
+
     String insidePREHandler(String line, XWikiContext context);
+
     String endRenderingHandler(String line, XWikiContext context);
+
     Api getPluginApi(XWikiPluginInterface plugin, XWikiContext context);
 
     /**
-     * Plugin extension point allowing the plugin to perform modifications to an attachment when the
-     * user clicks on an attachment in a document. The plugin is passed the original attachment and it has
-     * to return the new modified attachment.
-     *
+     * Plugin extension point allowing the plugin to perform modifications to an attachment when the user clicks on an
+     * attachment in a document. The plugin is passed the original attachment and it has to return the new modified
+     * attachment.
+     * 
      * @param attachment the original attachment
      * @param context the xwiki context object
      * @return the modified attachment

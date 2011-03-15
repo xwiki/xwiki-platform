dc853a7d5e50fc75465c9a298da7e9d23724de3b
vmassol <vincent@massol.net>
2011-03-15 14:26:43 +0000

    Use new Rendering Test Framework for code macro tests
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/core/trunk@35608 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-rendering/xwiki-rendering-macros/xwiki-rendering-macro-code/src/test/java/org/xwiki/rendering/macro/code/IntegrationTests.java
+++ xwiki-rendering/xwiki-rendering-macros/xwiki-rendering-macro-code/src/test/java/org/xwiki/rendering/macro/code/IntegrationTests.java
@@ -17,25 +17,19 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.xwiki.rendering;
+package org.xwiki.rendering.macro.code;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-
-import org.xwiki.rendering.scaffolding.RenderingTestSuite;
-import org.xwiki.test.ComponentManagerTestSetup;
+import org.junit.runner.RunWith;
+import org.xwiki.rendering.test.integration.RenderingTestSuite;
 
 /**
- * All Rendering integration tests defined in text files using a special format.
- * 
+ * Run all tests found in {@code *.test} files located in the classpath. These {@code *.test} files must follow the
+ * conventions described in {@link org.xwiki.rendering.test.integration.TestDataParser}.
+ *
  * @version $Id$
- * @since 1.7M3
+ * @since 3.0RC1
  */
-public class RenderingTests extends TestCase
+@RunWith(RenderingTestSuite.class)
+public class IntegrationTests
 {
-    public static Test suite() throws Exception
-    {
-        RenderingTestSuite suite = new RenderingTestSuite("Test Code Macro");
-        return new ComponentManagerTestSetup(suite);
-    }
 }

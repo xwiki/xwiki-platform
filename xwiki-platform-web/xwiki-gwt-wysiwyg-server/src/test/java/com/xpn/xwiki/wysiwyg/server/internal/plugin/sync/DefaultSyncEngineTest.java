1681d31957b4a0bfe39a72b9d3db575ecad7c146
vmassol <vmassol@f329d543-caf0-0310-9063-dda96c69346f>
2011-03-19 13:08:00 +0000

    Convert to JUnit4
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/platform/web/trunk@35733 f329d543-caf0-0310-9063-dda96c69346f

--- xwiki-platform-web/xwiki-gwt-wysiwyg-server/src/test/java/com/xpn/xwiki/wysiwyg/server/internal/plugin/sync/DefaultSyncEngineTest.java
+++ xwiki-platform-web/xwiki-gwt-wysiwyg-server/src/test/java/com/xpn/xwiki/wysiwyg/server/internal/plugin/sync/DefaultSyncEngineTest.java
@@ -19,15 +19,15 @@
  */
 package com.xpn.xwiki.wysiwyg.server.internal.plugin.sync;
 
-import junit.framework.TestCase;
-
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.xwiki.gwt.wysiwyg.client.diff.Diff;
 import org.xwiki.gwt.wysiwyg.client.diff.Revision;
 import org.xwiki.gwt.wysiwyg.client.diff.ToString;
 import org.xwiki.gwt.wysiwyg.client.plugin.sync.SyncResult;
 import org.xwiki.gwt.wysiwyg.client.plugin.sync.SyncStatus;
 
-import com.xpn.xwiki.wysiwyg.server.internal.plugin.sync.DefaultSyncEngine;
 import com.xpn.xwiki.wysiwyg.server.plugin.sync.SyncEngine;
 
 /**
@@ -35,7 +35,7 @@ import com.xpn.xwiki.wysiwyg.server.plugin.sync.SyncEngine;
  * 
  * @version $Id$
  */
-public class DefaultSyncEngineTest extends TestCase
+public class DefaultSyncEngineTest
 {
     /**
      * The synchronization engine.
@@ -44,15 +44,14 @@ public class DefaultSyncEngineTest extends TestCase
 
     /**
      * {@inheritDoc}
-     * 
-     * @see MockObjectTestCase#setUp()
      */
+    @Before
     public void setUp() throws Exception
     {
-        super.setUp();
         this.syncEngine = new DefaultSyncEngine();
     }
 
+    @Test
     public void testSimpleChanges() throws Exception
     {
         String currentContent = "";
@@ -62,20 +61,21 @@ public class DefaultSyncEngineTest extends TestCase
 
         Revision rev = sync.getRevision();
         int currentVersion = sync.getVersion();
-        assertNotNull("There should be a first revision", rev);
+        Assert.assertNotNull("There should be a first revision", rev);
 
         currentContent = ToString.arrayToString(rev.patch(ToString.stringToArray(currentContent)));
-        assertEquals("Content should be A", "A\n", currentContent);
+        Assert.assertEquals("Content should be A", "A\n", currentContent);
 
         Revision syncRev = Diff.diff(ToString.stringToArray(currentContent), ToString.stringToArray("A B\n"));
         SyncResult sync2 = syncEngine.sync(syncStatus, syncRev, currentVersion);
 
         rev = sync2.getRevision();
         currentVersion = sync.getVersion();
-        assertNull("There should be a no revision", rev);
-        assertEquals("Current version should be 1", 1, sync.getVersion());
+        Assert.assertNull("There should be a no revision", rev);
+        Assert.assertEquals("Current version should be 1", 1, sync.getVersion());
     }
 
+    @Test
     public void testSimpleChanges2() throws Exception
     {
         String currentContent = "";
@@ -85,21 +85,105 @@ public class DefaultSyncEngineTest extends TestCase
 
         Revision rev = sync.getRevision();
         int currentVersion = sync.getVersion();
-        assertNotNull("There should be a first revision", rev);
+        Assert.assertNotNull("There should be a first revision", rev);
 
         currentContent = ToString.arrayToString(rev.patch(ToString.stringToArray(currentContent)));
-        assertEquals("Content should be A", "A\n", currentContent);
+        Assert.assertEquals("Content should be A", "A\n", currentContent);
 
         Revision syncRev = Diff.diff(ToString.stringToArray(currentContent), ToString.stringToArray("A B\n"));
         SyncResult sync2 = syncEngine.sync(syncStatus, syncRev, currentVersion);
 
         rev = sync2.getRevision();
         currentVersion = sync.getVersion();
-        assertNull("There should be a no revision", rev);
-        assertEquals("Current version should be 1", 1, sync.getVersion());
+        Assert.assertNull("There should be a no revision", rev);
+        Assert.assertEquals("Current version should be 1", 1, sync.getVersion());
     }
 
-    public void testMerge(String original, String change1, String change2, String expected) throws Exception
+    @Test
+    public void testSpacedMerge1() throws Exception
+    {
+        testMerge("A\n", "A B\n", "A C\n", "A C B\n");
+    }
+
+    @Test
+    public void testSpacedMerge2() throws Exception
+    {
+        testMerge("A\n", "A\nB\n", "A\nC\n", "A\nC\nB\n");
+    }
+
+    @Test
+    public void testSpacedMerge3() throws Exception
+    {
+        testMerge("A B C D E\n", "A X B C D E\n", "A B C D Y E\n", "A X B C D Y E\n");
+    }
+
+    @Test
+    public void testCloseMerge() throws Exception
+    {
+        testMerge("A\n", "AB\n", "AC\n", "ACB\n");
+    }
+
+    @Test
+    public void testDoubleMerge1() throws Exception
+    {
+        testMerge("A\n", "XA B\n", "A C\n", "XA C B\n");
+    }
+
+    @Test
+    public void testDoubleMerge2() throws Exception
+    {
+        testMerge("A\n", "XYA B\n", "A C\n", "XYA C B\n");
+    }
+
+    @Test
+    public void testDoubleMerge3() throws Exception
+    {
+        testMerge("A\n", "XYZA B\n", "A C\n", "XYZA C B\n");
+    }
+
+    @Test
+    public void testDoubleMerge4() throws Exception
+    {
+        testMerge("A\n", "XYZTA B\n", "A C\n", "XYZTA C B\n");
+    }
+
+    @Test
+    public void testDoubleMergeWithSpace1() throws Exception
+    {
+        testMerge("A\n", "X A B\n", "A C\n", "X A C B\n");
+    }
+
+    @Test
+    public void testDoubleMergeWithSpace2() throws Exception
+    {
+        testMerge("A\n", "XY A B\n", "A C\n", "XY A C B\n");
+    }
+
+    @Test
+    public void testDoubleMergeWithSpace3() throws Exception
+    {
+        testMerge("A\n", "XYZ A B\n", "A C\n", "XYZ A C B\n");
+    }
+
+    @Test
+    public void testDeleteMergeOk1() throws Exception
+    {
+        testMerge("ABCDEF\n", "ABEF\n", "ABCDEFGH\n", "ABEFGH\n");
+    }
+
+    @Test
+    public void failingTestDeleteMergeOk2() throws Exception
+    {
+        testMerge("ABCD\n", "\n", "ABCDEF\n", "EF\n");
+    }
+
+    @Test
+    public void testSameEditMerge() throws Exception
+    {
+        testMerge("flower\n", "flowers\n", "flowers\n", "flowerss\n");
+    }
+
+    private void testMerge(String original, String change1, String change2, String expected) throws Exception
     {
         String currentContentUser1 = "";
         String currentContentUser2 = "";
@@ -108,137 +192,66 @@ public class DefaultSyncEngineTest extends TestCase
 
         SyncStatus syncStatus = new SyncStatus("Test.Test", "1.1", original);
         syncEngine.setSyncStatus("Test.Test", syncStatus);
-        assertEquals("Current version should be 1", 1, syncStatus.getCurrentVersionNumber());
+        Assert.assertEquals("Current version should be 1", 1, syncStatus.getCurrentVersionNumber());
 
         // Client 1 loads the data
         SyncResult syncResultUser1 = syncEngine.sync(syncStatus, null, currentVersionUser1);
-        assertEquals("Current version should be 1", 1, syncStatus.getCurrentVersionNumber());
-        assertEquals("Sync result version should be 1", 1, syncResultUser1.getVersion());
+        Assert.assertEquals("Current version should be 1", 1, syncStatus.getCurrentVersionNumber());
+        Assert.assertEquals("Sync result version should be 1", 1, syncResultUser1.getVersion());
 
         Revision syncRevUser1 = syncResultUser1.getRevision();
         currentVersionUser1 = syncResultUser1.getVersion();
-        assertNotNull("There should be a first revision", syncRevUser1);
+        Assert.assertNotNull("There should be a first revision", syncRevUser1);
 
         currentContentUser1 = ToString.arrayToString(syncRevUser1.patch(ToString.stringToArray(currentContentUser1)));
-        assertEquals("Content should be A", original, currentContentUser1);
+        Assert.assertEquals("Content should be A", original, currentContentUser1);
 
         // Client 2 loads the data
         SyncResult syncResultUser2 = syncEngine.sync(syncStatus, null, currentVersionUser2);
-        assertEquals("Current version should be 1", 1, syncStatus.getCurrentVersionNumber());
-        assertEquals("Sync result version should be 1", 1, syncResultUser2.getVersion());
+        Assert.assertEquals("Current version should be 1", 1, syncStatus.getCurrentVersionNumber());
+        Assert.assertEquals("Sync result version should be 1", 1, syncResultUser2.getVersion());
 
         Revision syncRevUser2 = syncResultUser2.getRevision();
         currentVersionUser2 = syncResultUser2.getVersion();
-        assertNotNull("There should be a first revision", syncRevUser2);
+        Assert.assertNotNull("There should be a first revision", syncRevUser2);
 
         currentContentUser2 = ToString.arrayToString(syncRevUser2.patch(ToString.stringToArray(currentContentUser2)));
-        assertEquals("Content should be A", original, currentContentUser2);
+        Assert.assertEquals("Content should be A", original, currentContentUser2);
 
         // Client 1 sends it's patch
         Revision revUser1 = Diff.diff(ToString.stringToArray(currentContentUser1), ToString.stringToArray(change1));
         syncResultUser1 = syncEngine.sync(syncStatus, revUser1, currentVersionUser1);
-        assertEquals("Current version should be 2", 2, syncStatus.getCurrentVersionNumber());
+        Assert.assertEquals("Current version should be 2", 2, syncStatus.getCurrentVersionNumber());
 
         syncRevUser1 = syncResultUser1.getRevision();
         currentVersionUser1 = syncResultUser1.getVersion();
-        assertNull("There should be a no revision", syncRevUser1);
+        Assert.assertNull("There should be a no revision", syncRevUser1);
         currentContentUser1 = change1;
-        assertEquals("Sync result version should be 2", 2, syncResultUser1.getVersion());
+        Assert.assertEquals("Sync result version should be 2", 2, syncResultUser1.getVersion());
 
         // Client 2 sends it's patch
         Revision revUser2 = Diff.diff(ToString.stringToArray(currentContentUser2), ToString.stringToArray(change2));
         syncResultUser2 = syncEngine.sync(syncStatus, revUser2, currentVersionUser2);
-        assertEquals("Current version should be 3", 3, syncStatus.getCurrentVersionNumber());
+        Assert.assertEquals("Current version should be 3", 3, syncStatus.getCurrentVersionNumber());
 
         syncRevUser2 = syncResultUser2.getRevision();
         currentVersionUser2 = syncResultUser2.getVersion();
-        assertNotNull("There should be a revision", syncRevUser2);
-        assertEquals("Sync result version should be 3", 3, syncResultUser2.getVersion());
+        Assert.assertNotNull("There should be a revision", syncRevUser2);
+        Assert.assertEquals("Sync result version should be 3", 3, syncResultUser2.getVersion());
 
         currentContentUser2 = ToString.arrayToString(syncRevUser2.patch(ToString.stringToArray(currentContentUser2)));
-        assertEquals("New client 2 content should be " + expected, currentContentUser2, expected);
+        Assert.assertEquals("New client 2 content should be " + expected, currentContentUser2, expected);
 
         // Client 1 retrieves it's patch
         syncResultUser1 = syncEngine.sync(syncStatus, null, currentVersionUser1);
-        assertEquals("Current version should be 3", 3, syncStatus.getCurrentVersionNumber());
+        Assert.assertEquals("Current version should be 3", 3, syncStatus.getCurrentVersionNumber());
 
         syncRevUser1 = syncResultUser1.getRevision();
         currentVersionUser1 = syncResultUser1.getVersion();
-        assertNotNull("There should be a revision", syncRevUser1);
-        assertEquals("Sync result version should be 3", 3, syncResultUser1.getVersion());
+        Assert.assertNotNull("There should be a revision", syncRevUser1);
+        Assert.assertEquals("Sync result version should be 3", 3, syncResultUser1.getVersion());
 
         currentContentUser1 = ToString.arrayToString(syncRevUser1.patch(ToString.stringToArray(currentContentUser1)));
-        assertEquals("New client 1 content should be " + expected, currentContentUser1, expected);
-
-    }
-
-    public void testSpacedMerge1() throws Exception
-    {
-        testMerge("A\n", "A B\n", "A C\n", "A C B\n");
-    }
-
-    public void testSpacedMerge2() throws Exception
-    {
-        testMerge("A\n", "A\nB\n", "A\nC\n", "A\nC\nB\n");
-    }
-
-    public void testSpacedMerge3() throws Exception
-    {
-        testMerge("A B C D E\n", "A X B C D E\n", "A B C D Y E\n", "A X B C D Y E\n");
-    }
-
-    public void testCloseMerge() throws Exception
-    {
-        testMerge("A\n", "AB\n", "AC\n", "ACB\n");
-    }
-
-    public void testDoubleMerge1() throws Exception
-    {
-        testMerge("A\n", "XA B\n", "A C\n", "XA C B\n");
-    }
-
-    public void testDoubleMerge2() throws Exception
-    {
-        testMerge("A\n", "XYA B\n", "A C\n", "XYA C B\n");
-    }
-
-    public void testDoubleMerge3() throws Exception
-    {
-        testMerge("A\n", "XYZA B\n", "A C\n", "XYZA C B\n");
-    }
-
-    public void testDoubleMerge4() throws Exception
-    {
-        testMerge("A\n", "XYZTA B\n", "A C\n", "XYZTA C B\n");
-    }
-
-    public void testDoubleMergeWithSpace1() throws Exception
-    {
-        testMerge("A\n", "X A B\n", "A C\n", "X A C B\n");
-    }
-
-    public void testDoubleMergeWithSpace2() throws Exception
-    {
-        testMerge("A\n", "XY A B\n", "A C\n", "XY A C B\n");
-    }
-
-    public void testDoubleMergeWithSpace3() throws Exception
-    {
-        testMerge("A\n", "XYZ A B\n", "A C\n", "XYZ A C B\n");
-    }
-
-    public void testDeleteMergeOk1() throws Exception
-    {
-        testMerge("ABCDEF\n", "ABEF\n", "ABCDEFGH\n", "ABEFGH\n");
-    }
-
-    public void failingTestDeleteMergeOk2() throws Exception
-    {
-        testMerge("ABCD\n", "\n", "ABCDEF\n", "EF\n");
-    }
-
-    public void testSameEditMerge() throws Exception
-    {
-        testMerge("flower\n", "flowers\n", "flowers\n", "flowerss\n");
+        Assert.assertEquals("New client 1 content should be " + expected, currentContentUser1, expected);
     }
 }

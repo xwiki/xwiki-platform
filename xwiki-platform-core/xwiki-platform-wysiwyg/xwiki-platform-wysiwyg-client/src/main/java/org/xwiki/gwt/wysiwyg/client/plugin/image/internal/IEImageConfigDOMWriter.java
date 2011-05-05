921fb9f83b0099f5a27978edc50f934e256b1b98
Marius Dumitru Florea <mariusdumitru.florea@xwiki.com>
2011-05-05 20:49:28 +0300

    XWIKI-6592: Can't move the caret after an image at the end of a paragraph in IE8 * Implemented the quick fix: insert a non-breaking space after the image if the image is not followed by a visible node inside the same block-level container.

--- xwiki-platform-core/xwiki-platform-wysiwyg/xwiki-platform-wysiwyg-client/src/main/java/org/xwiki/gwt/wysiwyg/client/plugin/image/internal/IEImageConfigDOMWriter.java
+++ xwiki-platform-core/xwiki-platform-wysiwyg/xwiki-platform-wysiwyg-client/src/main/java/org/xwiki/gwt/wysiwyg/client/plugin/image/internal/IEImageConfigDOMWriter.java
@@ -19,11 +19,14 @@
  */
 package org.xwiki.gwt.wysiwyg.client.plugin.image.internal;
 
+import org.xwiki.gwt.dom.client.DOMUtils;
+import org.xwiki.gwt.dom.client.Element;
 import org.xwiki.gwt.user.client.StringUtils;
+import org.xwiki.gwt.wysiwyg.client.plugin.image.ImageConfig;
 import org.xwiki.gwt.wysiwyg.client.plugin.image.ImageConfigDOMWriter;
 
 import com.google.gwt.dom.client.ImageElement;
-import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.dom.client.Node;
 
 /**
  * Overwrites {@link ImageConfigDOMWriter} in order to fix some IE bugs.
@@ -35,21 +38,64 @@ public class IEImageConfigDOMWriter extends ImageConfigDOMWriter
     /**
      * {@inheritDoc}
      * 
-     * @see ImageConfigDOMWriter#updateDimension(ImageElement, String, String)
+     * @see ImageConfigDOMWriter#write(ImageConfig, ImageElement)
      */
     @Override
-    protected void updateDimension(ImageElement image, String dimension, String value)
+    public void write(ImageConfig imageConfig, ImageElement image)
     {
-        // Use the style attribute because IE fails to update the image size when the width/height attributes are used.
-        if (StringUtils.isEmpty(value)) {
-            image.removeAttribute(dimension);
-            image.getStyle().clearProperty(dimension);
-        } else {
-            String computedValue = image.getPropertyString(dimension);
-            if (!value.equals(computedValue) && !value.equals(computedValue + Unit.PX.getType())) {
-                image.removeAttribute(dimension);
-                image.getStyle().setProperty(dimension, value);
-            }
+        super.write(imageConfig, image);
+
+        // Add a non-breaking space after the image if the image is the last visible node inside its closest block-level
+        // ancestor. Without this space the user can't move the caret after the image to add new content inside the
+        // current block-level container.
+        // FIXME: This isn't the right place for this code because this method should affect only the given image.
+        // Ensure that the image is attached, otherwise we can't add the space.
+        if (image.getParentNode() != null && endsBlock(image)) {
+            DOMUtils.getInstance().insertAfter(image.getOwnerDocument().createTextNode("\u00A0"), image);
         }
+    }
+
+    /**
+     * @param node a DOM node
+     * @return {@code true} if the given node is not followed by any visible node inside the same block-level container,
+     *         {@code false} otherwise
+     */
+    private boolean endsBlock(Node node)
+    {
+        Node ancestor = node;
+        // Look for an ancestor that is followed by a visible sibling. Stop when a block-level ancestor is found.
+        do {
+            if (getNextVisibleSibling(ancestor) != null) {
+                return false;
+            }
+            ancestor = ancestor.getParentNode();
+        } while (ancestor != null && DOMUtils.getInstance().isInline(ancestor));
+        return true;
+    }
+
+    /**
+     * @param node a DOM node
+     * @return the next visible sibling of the given node
+     * @see #isVisible(Node)
+     */
+    private Node getNextVisibleSibling(Node node)
+    {
+        Node sibling = node;
+        do {
+            sibling = sibling.getNextSibling();
+        } while (sibling != null && !isVisible(sibling));
+        return sibling;
+    }
+
+    /**
+     * @param node a DOM node
+     * @return {@code true} if the give node is visible, {@code false} otherwise
+     */
+    private boolean isVisible(Node node)
+    {
+        // We treat BR differently than other elements because it is visible even though it has 0 offset width.
+        return (node.getNodeType() == Node.ELEMENT_NODE && ("br".equalsIgnoreCase(node.getNodeName()) || Element.as(
+            node).getOffsetWidth() > 0))
+            || !StringUtils.isEmpty(node.getNodeValue());
     }
 }

472a795ab0f35aa905b46108d43b1cdad35a653b
Marius Dumitru Florea <mariusdumitru.florea@xwiki.com>
2011-05-05 11:26:51 +0300

    XWIKI-6584: Table of contents is missing from the office export * Added a XSLT file that prepares the XHTML before it is converted to an office foramt. * Generate the ToC inside the officeExport.xsl using the simplest method: indented divs. Transforming the flat headings structure into an hierarchical structure based on nested ordered lists is hard to do with XSLT 1.0

--- xwiki-core/src/main/java/com/xpn/xwiki/internal/export/OfficeExporter.java
+++ xwiki-core/src/main/java/com/xpn/xwiki/internal/export/OfficeExporter.java
@@ -113,21 +113,7 @@ public class OfficeExporter extends PdfExportImpl
     private void exportXHtml(String xhtml, OutputStream out, DocumentFormat format, XWikiContext context)
         throws XWikiException
     {
-        String html = xhtml;
-
-        // FIXME: put that in some XSL transformation instead (no time and knowledge to do that before 2.4)
-        // html = applyXsl(xhtml, getXhtmlOfficexsl(context));
-        String pageBreakBeforeMatch = "<p style=\"page-break-before: always;\" />\n$1";
-        html = html.replaceAll("(<div[^>]+class=\"pdftoc\"[^>]*>)", pageBreakBeforeMatch);
-        html = html.replaceAll("(<div[^>]+id=\"xwikimaincontainer\"[^>]*>)", pageBreakBeforeMatch);
-
-        // OpenOffice does not support XHTML so we remove the XML marker and let it parse it as if it was HTML content
-        html = html.substring(xhtml.indexOf("?>") + 2);
-
-        // id attribute on body element makes openoffice converter to fail
-        html = html.replaceFirst("(<body[^>]+)id=\"body\"([^>]*>)", "$1$2");
-
-        OpenOfficeConverter documentConverter = ooManager.getConverter();
+        String html = applyXsl(xhtml, "officeExport.xsl");
 
         String inputFileName = "export_input.html";
         String outputFileName = "export_output." + format.getExtension();
@@ -136,6 +122,7 @@ public class OfficeExporter extends PdfExportImpl
         inputStreams.put(inputFileName, new ByteArrayInputStream(html.getBytes()));
         addEmbeddedObjects(inputStreams, context);
 
+        OpenOfficeConverter documentConverter = ooManager.getConverter();
         try {
             Map<String, byte[]> ouput = documentConverter.convert(inputStreams, inputFileName, outputFileName);
 

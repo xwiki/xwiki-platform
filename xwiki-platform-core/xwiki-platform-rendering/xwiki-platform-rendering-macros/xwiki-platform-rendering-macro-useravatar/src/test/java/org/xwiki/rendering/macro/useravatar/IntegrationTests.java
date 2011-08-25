752dc5c05e041bbab44737019e99379ff4f6be53
Sergiu Dumitriu <sergiu@xwiki.com>
2011-08-24 23:42:50 -0400

    XWIKI-6837: Make it possible to use the UserAvatar Macro without passing a fully quantified username Added test

--- xwiki-platform-core/xwiki-platform-rendering/xwiki-platform-rendering-macros/xwiki-platform-rendering-macro-useravatar/src/test/java/org/xwiki/rendering/macro/useravatar/IntegrationTests.java
+++ xwiki-platform-core/xwiki-platform-rendering/xwiki-platform-rendering-macros/xwiki-platform-rendering-macro-useravatar/src/test/java/org/xwiki/rendering/macro/useravatar/IntegrationTests.java
@@ -83,6 +83,9 @@ public class IntegrationTests
             allowing(mockDocumentReferenceResolver).resolve("XWiki.Admin",
                 new EntityReference("XWiki", EntityType.SPACE));
                 will(returnValue(adminUserReference));
+            allowing(mockDocumentReferenceResolver).resolve("Admin",
+                new EntityReference("XWiki", EntityType.SPACE));
+                will(returnValue(adminUserReference));
             allowing(mockDocumentReferenceResolver).resolve("XWiki.ExistingUserWithoutAvatar",
                 new EntityReference("XWiki", EntityType.SPACE));
                 will(returnValue(userWithoutAvatarReference));

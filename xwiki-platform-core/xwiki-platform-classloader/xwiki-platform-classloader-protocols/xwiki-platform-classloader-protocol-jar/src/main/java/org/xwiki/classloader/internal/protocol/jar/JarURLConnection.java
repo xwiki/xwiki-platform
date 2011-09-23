489f4f7c2d3dd6255f8f8ea32ffe8fd98c9e5b57
Vincent Massol <vincent@massol.net>
2011-09-23 09:45:29 +0200

    XWIKI-2155: Add license header check to the build * Added BAT license header format * Fixed license headers in xwiki-platform * Moved specific excludes to their target modules

--- xwiki-platform-core/xwiki-platform-classloader/xwiki-platform-classloader-protocols/xwiki-platform-classloader-protocol-jar/src/main/java/org/xwiki/classloader/internal/protocol/jar/JarURLConnection.java
+++ xwiki-platform-core/xwiki-platform-classloader/xwiki-platform-classloader-protocols/xwiki-platform-classloader-protocol-jar/src/main/java/org/xwiki/classloader/internal/protocol/jar/JarURLConnection.java
@@ -16,12 +16,6 @@
  * License along with this software; if not, write to the Free
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
- * 
- * 
- * Originally written by Dawid Kurzyniec and released to the public domain, as explained
- * at http://creativecommons.org/licenses/publicdomain
- * 
- * Source: http://dcl.mathcs.emory.edu/php/loadPage.php?content=util/features.html#classloading
  */
 package org.xwiki.classloader.internal.protocol.jar;
 

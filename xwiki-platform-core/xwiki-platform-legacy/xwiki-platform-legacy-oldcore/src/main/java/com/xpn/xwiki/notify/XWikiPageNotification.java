15b1dd3b64bcf0466b47270becda480d06372e2d
Vincent Massol <vincent@massol.net>
2011-09-27 17:26:40 +0200

    Applied coding style

--- xwiki-platform-core/xwiki-platform-legacy/xwiki-platform-legacy-oldcore/src/main/java/com/xpn/xwiki/notify/XWikiPageNotification.java
+++ xwiki-platform-core/xwiki-platform-legacy/xwiki-platform-legacy-oldcore/src/main/java/com/xpn/xwiki/notify/XWikiPageNotification.java
@@ -19,39 +19,47 @@
  */
 package com.xpn.xwiki.notify;
 
-import com.xpn.xwiki.XWiki;
-import com.xpn.xwiki.XWikiContext;
-import com.xpn.xwiki.XWikiException;
-import com.xpn.xwiki.doc.XWikiDocument;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.xpn.xwiki.XWiki;
+import com.xpn.xwiki.XWikiContext;
+import com.xpn.xwiki.XWikiException;
+import com.xpn.xwiki.doc.XWikiDocument;
+
 @Deprecated
-public class XWikiPageNotification implements XWikiActionNotificationInterface {
+public class XWikiPageNotification implements XWikiActionNotificationInterface
+{
     private static final Logger LOGGER = LoggerFactory.getLogger(XWikiPageNotification.class);
 
-    public void notify(XWikiNotificationRule rule, XWikiDocument doc, String action, XWikiContext context) {
+    public void notify(XWikiNotificationRule rule, XWikiDocument doc, String action, XWikiContext context)
+    {
         try {
             String notifpages = context.getWiki().getXWikiPreference("notification_pages", context);
-            if ((notifpages!=null)&&(!notifpages.equals(""))) {
+            if ((notifpages != null) && (!notifpages.equals(""))) {
                 String[] notifpages2 = StringUtils.split(notifpages, " ,");
-                for (int i=0;i<notifpages2.length;i++) {
+                for (int i = 0; i < notifpages2.length; i++) {
                     notifyPage(notifpages2[i], rule, doc, action, context);
                 }
             }
-            String xnotif = (context.getRequest()!=null) ? context.getRequest().getParameter("xnotification") : null;
-            if ((xnotif!=null)&&(!xnotif.equals(""))) {
-              notifyPage(xnotif, rule, doc, action, context);
+            String xnotif = (context.getRequest() != null) ? context.getRequest().getParameter("xnotification") : null;
+            if ((xnotif != null) && (!xnotif.equals(""))) {
+                notifyPage(xnotif, rule, doc, action, context);
             }
         } catch (Throwable e) {
-            XWikiException e2 = new XWikiException(XWikiException.MODULE_XWIKI_NOTIFICATION, XWikiException.ERROR_XWIKI_NOTIFICATION, "Error executing notifications", e);
-            if (LOGGER.isErrorEnabled())
+            XWikiException e2 =
+                new XWikiException(XWikiException.MODULE_XWIKI_NOTIFICATION, XWikiException.ERROR_XWIKI_NOTIFICATION,
+                    "Error executing notifications", e);
+            if (LOGGER.isErrorEnabled()) {
                 LOGGER.error(e2.getFullMessage());
+            }
         }
     }
 
-    protected void notifyPage(String page, XWikiNotificationRule rule, XWikiDocument doc, String action, XWikiContext context) {
+    protected void notifyPage(String page, XWikiNotificationRule rule, XWikiDocument doc, String action,
+        XWikiContext context)
+    {
         XWikiActionNotificationInterface notif = null;
         try {
             XWiki xwiki = context.getWiki();
@@ -61,10 +69,13 @@ public class XWikiPageNotification implements XWikiActionNotificationInterface {
                 notif.notify(rule, doc, action, context);
             }
         } catch (Throwable e) {
-            Object[] args = { page };
-            XWikiException e2 = new XWikiException(XWikiException.MODULE_XWIKI_GROOVY, XWikiException.ERROR_XWIKI_GROOVY_EXECUTION_FAILED, "Error parsing groovy notification for page {0}", e, args);
-            if (LOGGER.isErrorEnabled())
+            Object[] args = {page};
+            XWikiException e2 = new XWikiException(XWikiException.MODULE_XWIKI_GROOVY,
+                XWikiException.ERROR_XWIKI_GROOVY_EXECUTION_FAILED, "Error parsing groovy notification for page {0}", e,
+                args);
+            if (LOGGER.isErrorEnabled()) {
                 LOGGER.error(e2.getFullMessage());
+            }
         }
     }
 }

9cb04a27bb12b1cefad51469ae519a2cc8b7e538
Marius Dumitru Florea <marius@xwiki.com>
2011-10-31 17:29:03 +0200

    XWIKI-7109: XWiki.XWikiPreferences is re-saved on each restart

--- xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/XWiki.java
+++ xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/XWiki.java
@@ -3237,13 +3237,14 @@ public class XWiki implements EventListener
 
         SheetBinder documentSheetBinder = Utils.getComponent(SheetBinder.class, "document");
         boolean hasDocumentSheets = documentSheetBinder.getSheets(doc).isEmpty();
-
-        needsUpdate |= setClassDocumentFields(doc, "XWiki Preferences");
-
-        // Don't use ClassSheet to display XWikiPreferences unless explicitly set.
         if (!hasDocumentSheets) {
-            DocumentReference sheet = new DocumentReference(context.getDatabase(), SYSTEM_SPACE, "ClassSheet");
-            needsUpdate |= documentSheetBinder.unbind(doc, sheet);
+            // Bind a document sheet to prevent the default class sheet from being used.
+            documentSheetBinder.bind(doc, doc.getDocumentReference());
+        }
+        needsUpdate |= setClassDocumentFields(doc, "XWiki Preferences");
+        if (!hasDocumentSheets) {
+            // Unbind the document sheet we bound earlier.
+            documentSheetBinder.unbind(doc, doc.getDocumentReference());
         }
 
         // Use AdminSheet to display documents having XWikiPreferences objects if no other class sheet is specified.

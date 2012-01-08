2bb735b5341207f784622ce2ff2845d7e2023416
Sergiu Dumitriu <sergiu@xwiki.com>
2012-01-07 23:08:27 -0500

    XWIKI-6687: Be able to delete a space from the UI Done.

---
+++ xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/web/DeleteSpaceAction.java
@@ -0,0 +1,119 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package com.xpn.xwiki.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xwiki.model.EntityType;
+import org.xwiki.model.reference.DocumentReference;
+import org.xwiki.model.reference.EntityReferenceResolver;
+import org.xwiki.query.Query;
+import org.xwiki.query.QueryException;
+import org.xwiki.query.QueryManager;
+
+import com.xpn.xwiki.XWiki;
+import com.xpn.xwiki.XWikiContext;
+import com.xpn.xwiki.XWikiException;
+
+/**
+ * Action for deleting an entire space, optionally saving all the deleted documents to the document trash, if enabled.
+ * 
+ * @version $Id$
+ * @since 3.4M1
+ */
+public class DeleteSpaceAction extends XWikiAction
+{
+    /** Logging helper object. */
+    private static final Logger LOGGER = LoggerFactory.getLogger(DeleteSpaceAction.class);
+
+    /** Confirm parameter name. */
+    private static final String CONFIRM_PARAM = "confirm";
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean action(XWikiContext context) throws XWikiException
+    {
+        XWiki xwiki = context.getWiki();
+        XWikiRequest request = context.getRequest();
+        XWikiResponse response = context.getResponse();
+        String space = context.getDoc().getDocumentReference().getLastSpaceReference().getName();
+        @SuppressWarnings("unchecked")
+        EntityReferenceResolver<String> nameResolver =
+            Utils.getComponent(EntityReferenceResolver.class, "current");
+        boolean redirected = false;
+        // If confirm=1 then delete the space. If not, the render action will go to the "deletespace" template asking
+        // for user confirmation. The "deletespace" template will then call the /deletespace/ action again with
+        // confirm=1.
+        if (!"1".equals(request.getParameter(CONFIRM_PARAM))) {
+            return true;
+        }
+        // CSRF prevention
+        if (!csrfTokenCheck(context)) {
+            return false;
+        }
+
+        if (xwiki.getSpaceDocsName(space, context).size() == 0) {
+            // Redirect the user to the view template so that he gets the "document doesn't exist" box.
+            sendRedirect(response, Utils.getRedirect("view", context));
+            redirected = true;
+        } else {
+            // Delete to recycle bin
+            try {
+                Query spaceDocsQuery =
+                    Utils.getComponent(QueryManager.class).createQuery("where doc.space = :space", Query.XWQL);
+                spaceDocsQuery.bindValue("space", space);
+                for (String docName : spaceDocsQuery.<String> execute()) {
+                    DocumentReference docReference =
+                        new DocumentReference(nameResolver.resolve(docName, EntityType.DOCUMENT));
+                    xwiki.deleteAllDocuments(xwiki.getDocument(docReference, context), context);
+                }
+            } catch (QueryException ex) {
+                LOGGER.warn("Failed to get the list of documents while trying to delete space {}: {}", space, ex
+                    .getMessage());
+            }
+        }
+        if (!redirected) {
+            // If a xredirect param is passed then redirect to the page specified instead of going to the default
+            // confirmation page.
+            String redirect = Utils.getRedirect(request, null);
+            if (redirect != null) {
+                sendRedirect(response, redirect);
+                redirected = true;
+            }
+        }
+        return !redirected;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String render(XWikiContext context) throws XWikiException
+    {
+        XWikiRequest request = context.getRequest();
+        String result = "deletespace";
+        if ("1".equals(request.getParameter(CONFIRM_PARAM))) {
+            result = "deletedspace";
+        }
+        return result;
+    }
+}

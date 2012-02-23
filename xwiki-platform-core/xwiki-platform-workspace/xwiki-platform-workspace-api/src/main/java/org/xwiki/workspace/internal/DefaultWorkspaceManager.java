07e39019b6134173c523a90a72ef598e80f35db8
tmortagne <thomas.mortagne@gmail.com>
2012-02-23 18:15:22 +0100

    JGit bug

--- xwiki-platform-core/xwiki-platform-workspace/xwiki-platform-workspace-api/src/main/java/org/xwiki/workspace/internal/DefaultWorkspaceManager.java
+++ xwiki-platform-core/xwiki-platform-workspace/xwiki-platform-workspace-api/src/main/java/org/xwiki/workspace/internal/DefaultWorkspaceManager.java
@@ -57,7 +57,7 @@ import com.xpn.xwiki.web.XWikiMessageTool;
 /**
  * Implementation of a <tt>WorkspaceManager</tt> component.
  * 
- * @version $Id: 4856526bd9a2ff24c653decee12e5ed26aaed5db $
+ * @version $Id$
  */
 @Component
 public class DefaultWorkspaceManager implements WorkspaceManager, Initializable

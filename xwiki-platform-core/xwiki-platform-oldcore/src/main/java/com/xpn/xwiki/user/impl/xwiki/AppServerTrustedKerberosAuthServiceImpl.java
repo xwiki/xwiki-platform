7afda5e4f89f6717a6358ace8b185e08f19de7a6
tmortagne <thomas.mortagne@gmail.com>
2012-03-29 11:32:05 +0200

    Fix checkstyle

--- xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/user/impl/xwiki/AppServerTrustedKerberosAuthServiceImpl.java
+++ xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/user/impl/xwiki/AppServerTrustedKerberosAuthServiceImpl.java
@@ -19,8 +19,6 @@
  */
 package com.xpn.xwiki.user.impl.xwiki;
 
-import java.text.MessageFormat;
-
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;

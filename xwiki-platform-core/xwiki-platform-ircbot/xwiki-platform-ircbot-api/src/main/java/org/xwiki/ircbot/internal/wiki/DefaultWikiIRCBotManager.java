70a948aadf784528e0136bf95bb9f1aa5ed24ed4
Vincent Massol <vincent@massol.net>
2012-04-06 23:27:44 +0200

    Fix checkstyle error caused by refactoring

--- xwiki-platform-core/xwiki-platform-ircbot/xwiki-platform-ircbot-api/src/main/java/org/xwiki/ircbot/internal/wiki/DefaultWikiIRCBotManager.java
+++ xwiki-platform-core/xwiki-platform-ircbot/xwiki-platform-ircbot-api/src/main/java/org/xwiki/ircbot/internal/wiki/DefaultWikiIRCBotManager.java
@@ -188,7 +188,8 @@ public class DefaultWikiIRCBotManager implements WikiIRCBotManager, WikiIRCBotCo
 
         // Step 2: Look for all registered bot listeners in the component manager but not already in the data structure
         try {
-            Map<String, IRCBotListener> botListeners = this.componentManager.getInstanceMap((Type) IRCBotListener.class);
+            Map<String, IRCBotListener> botListeners =
+                this.componentManager.getInstanceMap((Type) IRCBotListener.class);
             for (Map.Entry<String, IRCBotListener> entry : botListeners.entrySet()) {
                 BotListenerData listenerData =
                     new BotListenerData(entry.getKey(), entry.getValue().getName(), entry.getValue().getDescription());

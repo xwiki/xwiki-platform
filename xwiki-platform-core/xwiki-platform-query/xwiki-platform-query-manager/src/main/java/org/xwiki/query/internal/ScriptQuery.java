b70181dbcf2604b3d8a1900775b8c60bfaf2f09e
Jean-Vincent Drean <jv@drean.org>
2012-04-12 15:39:36 +0200

    XWIKI-7713 : ScriptQuery APIs returns the wrapped query

--- xwiki-platform-core/xwiki-platform-query/xwiki-platform-query-manager/src/main/java/org/xwiki/query/internal/ScriptQuery.java
+++ xwiki-platform-core/xwiki-platform-query/xwiki-platform-query-manager/src/main/java/org/xwiki/query/internal/ScriptQuery.java
@@ -105,7 +105,8 @@ public class ScriptQuery implements Query
 
     @Override
     public Query setWiki(String wiki) {
-        return query.setWiki(wiki);
+        query.setWiki(wiki);
+        return this;
     }
 
     @Override
@@ -115,17 +116,20 @@ public class ScriptQuery implements Query
 
     @Override
     public Query bindValue(String var, Object val) {
-        return query.bindValue(var, val);
+        query.bindValue(var, val);
+        return this;
     }
 
     @Override
     public Query bindValue(int index, Object val) {
-        return query.bindValue(index, val);
+        query.bindValue(index, val);
+        return this;
     }
 
     @Override
     public Query bindValues(List<Object> values) {
-        return query.bindValues(values);
+        query.bindValues(values);
+        return this;
     }
 
     @Override
@@ -140,7 +144,8 @@ public class ScriptQuery implements Query
 
     @Override
     public Query setFilter(QueryFilter filter) {
-        return query.setFilter(filter);
+        query.setFilter(filter);
+        return this;
     }
 
     @Override
@@ -150,12 +155,14 @@ public class ScriptQuery implements Query
 
     @Override
     public Query setLimit(int limit) {
-        return query.setLimit(limit);
+        query.setLimit(limit);
+        return this;
     }
 
     @Override
     public Query setOffset(int offset) {
-        return query.setOffset(offset);
+        query.setOffset(offset);
+        return this;
     }
 
     @Override

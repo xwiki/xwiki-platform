c8aa989c146347812920a92287f8a55b2540bfa5
tmortagne <thomas.mortagne@gmail.com>
2012-04-23 10:46:34 +0200

    XWIKI-7155: Upgrade to JGroups 3

--- xwiki-platform-core/xwiki-platform-observation/xwiki-platform-observation-remote/src/main/java/org/xwiki/observation/remote/internal/jgroups/JGroupsNetworkAdapter.java
+++ xwiki-platform-core/xwiki-platform-observation/xwiki-platform-observation-remote/src/main/java/org/xwiki/observation/remote/internal/jgroups/JGroupsNetworkAdapter.java
@@ -31,7 +31,6 @@ import javax.inject.Named;
 import javax.inject.Singleton;
 import javax.management.MBeanServer;
 
-import org.jgroups.ChannelException;
 import org.jgroups.JChannel;
 import org.jgroups.Message;
 import org.jgroups.conf.ConfiguratorFactory;
@@ -89,7 +88,7 @@ public class JGroupsNetworkAdapter implements NetworkAdapter
         // Send the message to the whole group
         Message message = new Message(null, null, remoteEvent);
 
-        // Send message to jgroups channels
+        // Send message to JGroups channels
         for (Map.Entry<String, JChannel> entry : this.channels.entrySet()) {
             try {
                 entry.getValue().send(message);
@@ -157,18 +156,12 @@ public class JGroupsNetworkAdapter implements NetworkAdapter
      * 
      * @param channelId the identifier of the channel to create
      * @return the new channel
-     * @throws ComponentLookupException failed to get default {@link JGroupsReceiver}
-     * @throws ChannelException failed to create channel
+     * @throws Exception failed to create new channel
      */
-    private JChannel createChannel(String channelId) throws ComponentLookupException, ChannelException
+    private JChannel createChannel(String channelId) throws Exception
     {
         // load configuration
-        ProtocolStackConfigurator channelConf;
-        try {
-            channelConf = loadChannelConfiguration(channelId);
-        } catch (IOException e) {
-            throw new ChannelException("Failed to load configuration for the channel [" + channelId + "]", e);
-        }
+        ProtocolStackConfigurator channelConf = loadChannelConfiguration(channelId);
 
         // get Receiver
         JGroupsReceiver channelReceiver;
@@ -182,7 +175,7 @@ public class JGroupsNetworkAdapter implements NetworkAdapter
         JChannel channel = new JChannel(channelConf);
 
         channel.setReceiver(channelReceiver);
-        channel.setOpt(JChannel.LOCAL, false);
+        channel.setDiscardOwnMessages(true);
 
         return channel;
     }
@@ -193,9 +186,8 @@ public class JGroupsNetworkAdapter implements NetworkAdapter
      * @param channelId the identifier of the channel
      * @return the channel configuration
      * @throws IOException failed to load configuration file
-     * @throws ChannelException failed to creation channel configuration
      */
-    private ProtocolStackConfigurator loadChannelConfiguration(String channelId) throws IOException, ChannelException
+    private ProtocolStackConfigurator loadChannelConfiguration(String channelId) throws IOException
     {
         String channelFile = channelId + ".xml";
         String path = "/WEB-INF/" + CONFIGURATION_PATH + channelFile;

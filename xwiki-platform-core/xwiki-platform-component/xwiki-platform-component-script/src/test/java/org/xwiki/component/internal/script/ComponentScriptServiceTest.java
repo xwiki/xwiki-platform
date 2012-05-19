676566f6849ee4d98884665b8e8519515c01cff1
Vincent Massol <vincent@massol.net>
2012-05-19 11:42:25 +0200

    XWIKI-7839: Make Component Manager accessible as a Component Script Service

---
+++ xwiki-platform-core/xwiki-platform-component/xwiki-platform-component-script/src/test/java/org/xwiki/component/internal/script/ComponentScriptServiceTest.java
@@ -0,0 +1,65 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.component.internal.script;
+
+import org.jmock.Expectations;
+import org.junit.Test;
+import org.xwiki.bridge.DocumentAccessBridge;
+import org.xwiki.component.manager.ComponentManager;
+import org.xwiki.test.AbstractMockingComponentTestCase;
+import org.xwiki.test.annotation.MockingRequirement;
+
+import junit.framework.Assert;
+
+/**
+ * Unit tests for {@link org.xwiki.component.internal.script.ComponentScriptService}.
+ *
+ * @version $Id$
+ * @since 4.1M2
+ */
+public class ComponentScriptServiceTest extends AbstractMockingComponentTestCase
+{
+    @MockingRequirement
+    private ComponentScriptService css;
+
+    @Test
+    public void getComponentManagerWhenNoProgrammingRights() throws Exception
+    {
+        final DocumentAccessBridge dab = getComponentManager().getInstance(DocumentAccessBridge.class);
+        getMockery().checking(new Expectations() {{
+            oneOf(dab).hasProgrammingRights();
+            will(returnValue(false));
+        }});
+
+        Assert.assertEquals(null, this.css.getComponentManager());
+    }
+
+    @Test
+    public void getComponentManagerWhenProgrammingRights() throws Exception
+    {
+        final DocumentAccessBridge dab = getComponentManager().getInstance(DocumentAccessBridge.class);
+        getMockery().checking(new Expectations() {{
+            oneOf(dab).hasProgrammingRights();
+            will(returnValue(true));
+        }});
+
+        Assert.assertTrue(this.css.getComponentManager() instanceof ComponentManager);
+    }
+}

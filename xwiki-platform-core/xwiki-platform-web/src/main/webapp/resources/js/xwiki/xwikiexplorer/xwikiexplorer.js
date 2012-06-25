3745adb3ff40442735086e770f5ffa00b4dd77af
Marius Dumitru Florea <marius@xwiki.com>
2012-06-25 15:05:26 +0300

    XWIKI-7889: wysiwyg: Cannot list attachments for pages with a period "." in their name - TRANSPORT_ERROR * Small clean up + reduce the usage of XWiki.resource in xwikiexplorer.js

--- xwiki-platform-core/xwiki-platform-web/src/main/webapp/resources/js/xwiki/xwikiexplorer/xwikiexplorer.js
+++ xwiki-platform-core/xwiki-platform-web/src/main/webapp/resources/js/xwiki/xwikiexplorer/xwikiexplorer.js
@@ -150,18 +150,17 @@ isc.XWEResultTree.addMethods({
         var treeRelations = this.treeRelations, childDataSources = nodeDS.getChildDataSources();
         // manage XWiki dynamic DS instantiation
         // it can't be managed with getChildDataSources since it needs the node to be created.
-        var resource = XWiki.resource.get(node.id);
-        node["resource"] = resource;
+        node.resource = XWiki.resource.get(node.id);
         if (nodeDS.Class == "XWEDataSource") {
-            childDSName = isc.XWEWikiDataSource.getOrCreate(resource.wiki).getID();
+            childDSName = isc.XWEWikiDataSource.getOrCreate(node.name).getID();
         } else if (nodeDS.Class == "XWEWikiDataSource") {
-            childDSName = isc.XWESpaceDataSource.getOrCreate(resource.wiki, resource.space).getID();
+            childDSName = isc.XWESpaceDataSource.getOrCreate(node.wiki, node.name).getID();
         } else if (nodeDS.Class == "XWESpaceDataSource") {
-            if (node["isXWikiAttachment"] == null) {
-                childDSName = isc.XWESpaceDataSource.getOrCreate(resource.wiki, resource.space).getID();
+            if (node.isXWikiAttachment) {
+                // The node that lists the files attached to a wiki page.
+                childDSName = isc.XWEAttachmentsDataSource.getOrCreate(node.wiki, node.space, node.name).getID();
             } else {
-                childDSName =
-                    isc.XWEAttachmentsDataSource.getOrCreate(resource.wiki, resource.space, resource.name).getID();
+                childDSName = isc.XWESpaceDataSource.getOrCreate(node.wiki, node.space).getID();
             }
         } else if (treeRelations) {
             // Default multi DataSource behavior
@@ -181,21 +180,17 @@ isc.XWEResultTree.addMethods({
      */
     dataArrived : function(parentNode) {
 
-        var parentDS = this.getNodeDataSource(parentNode);
-        var childrenDSClass = "";
         // getNodeDataSource returns null when parentNode is the root node.
-        if (parentDS == null) {
-          parentDS = this.getDataSource();
-        }
+        var parentDS = this.getNodeDataSource(parentNode) || this.getDataSource();
+        var childrenDSClass = "";
 
         // Various transformations on children nodes.
-        // Redo getChildren to avoid getting previously removed nodes (which have been nullified).
         var children = this.getChildren(parentNode);
         for (var i = 0; i < children.length; i++) {
             var currentDS = this.getNodeDataSource(children[i]);
             // Display the title for entities that have a title (e.g. wiki pages).
             var title = children[i].title || children[i].name;
-            var hint = currentDS.getHint(children[i]);
+            var hint = currentDS.getHint(children[i], parentNode);
             var enabled = true;
 
             // Transform title to a link if showLinks is activated.
@@ -209,6 +204,8 @@ isc.XWEResultTree.addMethods({
             isc.addProperties(children[i], {
                 // Overwrite children icon with the one defined in the XWiki DataSource.
                 icon: currentDS.icon,
+                // Store the entity reference to know that this node corresponds to a server-side entity.
+                reference: currentDS.getReference(children[i], this.getEntityReference(parentNode)),
                 plainTitle: children[i].title,
                 title: title,
                 isNewPage: false,
@@ -297,12 +294,11 @@ isc.XWEResultTree.addMethods({
     },
 
     addAddPageNode : function(node) {
-        var hint = isc.XWEResultTree.constants.addPageHint + ' '
-            + isc.XWEResultTree.formatPath(node.resource.wiki, node.resource.space);
+        var hint = isc.XWEResultTree.constants.addPageHint + ' ' + isc.XWEResultTree.formatPath(node.wiki, node.name);
         var newNode = {
             id: node.id + isc.XWEResultTree.constants.addNodeSuffix,
             wiki: node.wiki,
-            space: node.space,
+            name: node.name,
             title: isc.XWEResultTree.formatTitle(isc.XWEResultTree.constants.addPageTitle, hint),
             parentId: node.id,
             icon: "$xwiki.getSkinFile('icons/silk/bullet_add.png')",
@@ -310,9 +306,8 @@ isc.XWEResultTree.addMethods({
             isNewPage: true,
             isNewAttachment: false,
             clickCallback: function(viewer, node, recordNum) {
-                node.resource = XWiki.resource.get(node.resource.prefixedSpace);
-                viewer.input.value = "";
-              }
+                viewer.input.value = '';
+            }
         };
 
         // Determine node position in the children list.
@@ -330,18 +325,21 @@ isc.XWEResultTree.addMethods({
 
     addAddAttachmentsNode : function(node) {
         var hint = isc.XWEResultTree.constants.addAttachmentHint + ' '
-            + isc.XWEResultTree.formatPath(node.resource.wiki, node.resource.space, node.resource.name);
+            + isc.XWEResultTree.formatPath(node.wiki, node.space, node.name);
         var newNode = {
             id: node.id + isc.XWEResultTree.constants.addNodeSuffix,
             wiki: node.wiki,
             space: node.space,
+            name: node.name,
             title: isc.XWEResultTree.formatTitle(isc.XWEResultTree.constants.addAttachmentTitle, hint),
             parentId: node.id,
             icon: "$xwiki.getSkinFile('icons/silk/bullet_add.png')",
             resource: node.resource,
             isNewPage: false,
             isNewAttachment: true,
-            clickCallback: function(viewer, node, recordNum) { viewer.input.value = '' }
+            clickCallback: function(viewer, node, recordNum) {
+                viewer.input.value = '';
+            }
         };
 
         // Determine node position in the children list.
@@ -387,7 +385,7 @@ isc.XWEResultTree.addMethods({
 
             // Create attachments container node title.
             var hint = isc.XWEResultTree.constants.attachmentsHint + ' '
-                + isc.XWEResultTree.formatPath(node.resource.wiki, node.resource.space, node.resource.name);
+                + isc.XWEResultTree.formatPath(node.wiki, node.space, node.name);
             var plainTitle = isc.XWEResultTree.constants.attachmentsTitle + ' (' + node.plainTitle + ')';
             if (this.displayLinks == true) {
                 var title = isc.XWEResultTree.formatTitle(plainTitle, hint, node.xwikiRelativeUrl
@@ -399,11 +397,11 @@ isc.XWEResultTree.addMethods({
             // Create the node itself.
             var attachNode = {
                 id: node.id + XWiki.constants.anchorSeparator + XWiki.constants.docextraAttachmentsAnchor,
-                fullName: node.fullName + XWiki.constants.anchorSeparator + XWiki.constants.docextraAttachmentsAnchor,
                 wiki: node.wiki,
                 space: node.space,
-                title: title,
                 name: node.name,
+                anchor: XWiki.constants.docextraAttachmentsAnchor,
+                title: title,
                 parentId: node.id,
                 xwikiRelativeURL: node.xwikiRelativeURL + XWiki.constants.anchorSeparator +
                                   XWiki.constants.docextraAttachmentsAnchor,
@@ -427,9 +425,21 @@ isc.XWEResultTree.addMethods({
             // Add the attachments node to the node children list.
             this.add(attachNode, node, index);
         }
+    },
+
+    getEntityReference : function(node) {
+        // Only nodes that have a server side entity associated have a reference. Nodes added on the client side like
+        // the 'new page in space' or 'attachments' don't have a reference so we must take it from their ancestors.
+        while (node && !node.reference) {
+            node = this.getParent(node);
+        }
+        return node ? node.reference : null;
     }
 });
 
+/**
+ * Root data source: provides the list of wikis.
+ */
 isc.ClassFactory.defineClass("XWEDataSource", isc.DataSource);
 
 isc.XWEDataSource.addClassProperties({
@@ -461,8 +471,7 @@ isc.XWEDataSource.addProperties({
     fields : [ // Default fields (farm) in the resource.
         { name:"id", required: true, type: "text", primaryKey:true },
         { name:"name", type: "text" },
-        { name:"title", type: "text" },
-        { name:"xwikiRelativeUrl", type: "text" }
+        { name:"title", type: "text" }
     ],
 
     /*
@@ -516,11 +525,18 @@ isc.XWEDataSource.addMethods({
     getHint : function(item) {
         // No hint by default. Should be overwritten by specific data sources.
         return '';
+    },
+
+    /**
+     * @return a reference to the entity associated with the given item provided by this data source
+     */
+    getReference : function(item) {
+        return new XWiki.WikiReference(item.name);
     }
 });
 
 /*
- * Wiki DataSource
+ * Wiki DataSource: provides the list of spaces from a wiki.
  */
 isc.ClassFactory.defineClass("XWEWikiDataSource", isc.XWEDataSource);
 
@@ -540,6 +556,7 @@ isc.XWEWikiDataSource.addProperties({
     recordXPath : "/xwiki:spaces/xwiki:space",
     fields : [
         { name:"id", required: true, type: "text", primaryKey:true },
+        { name:"wiki", required: true, type: "text" },
         { name:"name", required: true, type: "text" },
         { name:"title", type: "text" },
         { name:"xwikiRelativeUrl", type: "text" }
@@ -557,12 +574,19 @@ isc.XWEWikiDataSource.addMethods({
      * @return the hint for the specified item obtained from this data source
      */
     getHint : function(item) {
-        return isc.XWEResultTree.constants.pageHint + ' ' + isc.XWEResultTree.formatPath(item.resource.wiki, item.name);
+        return isc.XWEResultTree.constants.pageHint + ' ' + isc.XWEResultTree.formatPath(item.wiki, item.name);
+    },
+
+    /**
+     * @return a reference to the entity associated with the given item provided by this data source
+     */
+    getReference : function(item) {
+        return new XWiki.SpaceReference(item.wiki, item.name);
     }
 });
 
 /*
- * Space DataSource
+ * Space DataSource: provides the list of pages from a space.
  */
 isc.ClassFactory.defineClass("XWESpaceDataSource", isc.XWEDataSource);
 
@@ -583,13 +607,11 @@ isc.XWESpaceDataSource.addProperties({
     recordXPath : "/xwiki:pages/xwiki:pageSummary",
     fields : [
         { name:"id", required: true, type: "text", primaryKey:true },
-        { name:"fullName", required: true, type: "text" },
         { name:"wiki", required: true, type: "text" },
         { name:"space", required: true, type: "text" },
         { name:"name", required: true, type: "text" },
         { name:"title", required: true, type: "text" },
         { name:"parentId", required: true, type: "text", foreignKey: "id" },
-        { name:"parent", required: true, type: "text" },
         { name:"xwikiRelativeUrl", type: "text" },
         { name:"link", propertiesOnly: true }
     ],
@@ -618,11 +640,18 @@ isc.XWESpaceDataSource.addMethods({
      */
     getHint : function(item) {
         return isc.XWEResultTree.constants.pageHint + ' ' + isc.XWEResultTree.formatPath(item.wiki, item.space, item.name);
+    },
+
+    /**
+     * @return a reference to the entity associated with the given item provided by this data source
+     */
+    getReference : function(item) {
+        return new XWiki.DocumentReference(item.wiki, item.space, item.name);
     }
 });
 
 /*
- * Page DataSource
+ * Page DataSource: provides information about a wiki page.
  */
 isc.ClassFactory.defineClass("XWEPageDataSource", isc.XWEDataSource);
 
@@ -648,7 +677,6 @@ isc.XWEPageDataSource.addProperties({
         { name:"space", required: true, type: "text" },
         { name:"name", required: true, type: "text" },
         { name:"parentId", required: true, type: "text" },
-        { name:"parent", required: true, type: "text" },
         { name:"link", propertiesOnly: true }
     ],
     icon : "$xwiki.getSkinFile('icons/silk/page_white_text.png')"
@@ -663,7 +691,7 @@ isc.XWEPageDataSource.addMethods({
 });
 
 /*
- * Attachments DataSource
+ * Attachments DataSource: provides the list of files attached to a wiki page.
  */
 isc.ClassFactory.defineClass("XWEAttachmentsDataSource", isc.XWEDataSource);
 
@@ -703,9 +731,16 @@ isc.XWEAttachmentsDataSource.addMethods({
     /**
      * @return the hint for the specified item obtained from this data source
      */
-    getHint : function(item) {
-        return isc.XWEResultTree.constants.attachmentHint + ' ' + isc.XWEResultTree.formatPath(item.resource.wiki,
-            item.resource.space, item.resource.name);
+    getHint : function(item, parent) {
+        return isc.XWEResultTree.constants.attachmentHint + ' ' + isc.XWEResultTree.formatPath(parent.wiki,
+            parent.space, parent.name);
+    },
+
+    /**
+     * @return a reference to the entity associated with the given item provided by this data source
+     */
+    getReference : function(item, parentReference) {
+        return new XWiki.AttachmentReference(item.name, parentReference);
     }
 });
 
@@ -870,7 +905,7 @@ isc.XWETreeGrid.addMethods({
                 var fetchCallback = function(xmlDoc, xmlText, rpcResponse, rpcRequest) {
                     if (xmlDoc.httpResponseCode == 200) {
                         // XWiki.resource.get(reference) resolves the passed reference relative to the current document reference.
-                        var parentRes = xmlDoc.data[0].parent ? XWiki.resource.get(xmlDoc.data[0].parent) : { prefixedFullName: '', name: '' };
+                        var parentRes = xmlDoc.data[0].parentId ? XWiki.resource.get(xmlDoc.data[0].parentId) : { prefixedFullName: '', name: '' };
                         // Store the parent / child relationship in the cache to avoid the need of another request if this
                         // relationship is searched again.
                         rt.parentMap[resource.prefixedFullName] = parentRes;
@@ -1081,30 +1116,23 @@ isc.XWETreeGrid.addMethods({
      */
     nodeClickCallback : function(viewer, node, recordNum) {
         if (node.clickCallback == null) {
-            var resId = node.id;
-            // If the resource is a wiki, add :Main.WebHome to the resource id.
-            if (!resId.include(XWiki.constants.wikiSpaceSeparator)
-                    && this.getData().getNodeDataSource(node).Class == "XWEDataSource") {
-                resId = resId + XWiki.constants.wikiSpaceSeparator + "Main"
-                          + XWiki.constants.spacePageSeparator + "WebHome";
+            var reference = this.data.getEntityReference(node);
+            if (reference.type == XWiki.EntityType.WIKI) {
+                // If the entity associated with this node is a wiki, use the wiki home page reference.
+                reference = new XWiki.DocumentReference(node.name, 'Main', 'WebHome');
+            } else if (reference.type == XWiki.EntityType.SPACE) {
+                // If the entity associated with this node is a space, use the space home page reference.
+                reference = new XWiki.DocumentReference(node.wiki, node.name, 'WebHome');
             }
-            // If the resource is a space, add .WebHome to the resource id.
-            if (!resId.include(XWiki.constants.spacePageSeparator)
-                    && this.getData().getNodeDataSource(node).Class == "XWEWikiDataSource") {
-                resId = resId + XWiki.constants.spacePageSeparator + "WebHome";
+            var currentDocumentReference = new XWiki.DocumentReference(XWiki.currentWiki, XWiki.currentSpace, XWiki.currentPage);
+            reference = reference.relativeTo(currentDocumentReference);
+            // Update the input value.
+            this.input.value = XWiki.Model.serialize(reference);
+            if (node.anchor) {
+              this.input.value += '#' + node.anchor;
             }
-            // If there's only the current wiki in the tree, remove the wiki prefix (ex: "xwiki:").
-            if (this.getDataSource().Class != "XWEDataSource") {
-                resId = resId.substring(resId.indexOf(XWiki.constants.wikiSpaceSeparator) + 1, resId.length);
-            }
-            // If the resource is located in the current space, remove the space prefix (ex: "Main.")
-            if (node.resource["space"] == XWiki.currentSpace) {
-                resId = resId.substring(resId.indexOf(XWiki.constants.spacePageSeparator) + 1, resId.length);
-            }
-            // Set the input value to the clean ID of the clicked node.
-            this.input.value = resId;
             // Set the cache to the value we've set to prevent the tree from jumping to the WebHome node afterwards.
-            this.inputValueCache = resId;
+            this.inputValueCache = this.input.value;
         } else {
             node.clickCallback(viewer, node, recordNum);
         }
@@ -1142,7 +1170,36 @@ isc.XWETreeGrid.addMethods({
         if (value.length > 0) {
             return XWiki.resource.get(value)[propertyName];
         } else if (selectedRecord != null) {
-            return selectedRecord.isNewPage && propertyName == 'name' ? null : selectedRecord.resource[propertyName];
+            return selectedRecord.isNewPage && propertyName == 'name' ? null : this.getResourceProperty(selectedRecord, propertyName);
+        }
+        return null;
+    },
+
+    getResourceProperty : function(node, propertyName) {
+        if (propertyName == 'anchor') {
+            return node.anchor;
+        } else {
+            var propertyNameToEntityType = {
+                wiki:       XWiki.EntityType.WIKI,
+                space:      XWiki.EntityType.SPACE,
+                name:       XWiki.EntityType.DOCUMENT,
+                attachment: XWiki.EntityType.ATTACHMENT
+            };
+            var entityType = propertyNameToEntityType[propertyName];
+            if (entityType) {
+                return this.getEntityName(node, entityType);
+            }
+        }
+        return null;
+    },
+
+    getEntityName : function(node, entityType) {
+        var entityReference = this.data.getEntityReference(node);
+        if (entityReference) {
+            var referenceForType = entityReference.extractReference(entityType);
+            if (referenceForType) {
+              return referenceForType.name;
+            }
         }
         return null;
     },

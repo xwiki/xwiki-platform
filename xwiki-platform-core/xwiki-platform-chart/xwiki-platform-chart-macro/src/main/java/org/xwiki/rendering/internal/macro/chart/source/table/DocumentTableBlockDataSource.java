39e996e974fa8cd814d3ae110b46dd708944cfe9
Vincent Massol <vincent@massol.net>
2012-10-02 11:38:38 +0200

    XWIKI-8267: Chart macro with an XDOM data source in the same document executes very slowly

--- xwiki-platform-core/xwiki-platform-chart/xwiki-platform-chart-macro/src/main/java/org/xwiki/rendering/internal/macro/chart/source/table/DocumentTableBlockDataSource.java
+++ xwiki-platform-core/xwiki-platform-chart/xwiki-platform-chart-macro/src/main/java/org/xwiki/rendering/internal/macro/chart/source/table/DocumentTableBlockDataSource.java
@@ -66,7 +66,8 @@ public class DocumentTableBlockDataSource extends AbstractTableBlockDataSource
     private static final String TABLE_PARAM = "table";
 
     /**
-     * The document name of the document holding the table.
+     * The document name of the document holding the table. If null then the data source is located in the current
+     * document.
      */
     private DocumentReference documentReference;
 
@@ -109,23 +110,13 @@ public class DocumentTableBlockDataSource extends AbstractTableBlockDataSource
     protected TableBlock getTableBlock(String macroContent, MacroTransformationContext context)
         throws MacroExecutionException
     {
-        // Parse the document content into an XDOM.
-        XDOM xdom;
-        try {
-            DocumentModelBridge document = this.docBridge.getDocument(this.documentReference);
-            DocumentDisplayerParameters parameters = new DocumentDisplayerParameters();
-            parameters.setContentTranslated(true);
-            xdom = this.documentDisplayer.display(document, parameters);
-        } catch (Exception e) {
-            throw new MacroExecutionException(String.format("Error getting Chart table from document [%s]",
-                this.documentReference, e));
-        }
+        XDOM xdom = computeXDOM(context);
 
         // Find the correct table block.
         List<TableBlock> tableBlocks = xdom.getBlocks(new ClassBlockMatcher(TableBlock.class), Block.Axes.DESCENDANT);
         TableBlock result = null;
         this.logger.debug("Table id is [{}], there are [{}] tables in the document [{}]",
-            new Object[]{this.tableId, tableBlocks.size(), this.documentReference });
+            new Object[]{this.tableId, tableBlocks.size(), this.documentReference});
         if (null != tableId) {
             for (TableBlock tableBlock : tableBlocks) {
                 String id = tableBlock.getParameter("id");
@@ -143,6 +134,36 @@ public class DocumentTableBlockDataSource extends AbstractTableBlockDataSource
         }
 
         return result;
+    }
+
+    /**
+     * Get the XDOM for the data source.
+     *
+     * @param context the Macro context from which we can get the XDOM if the source is in the current content
+     * @return the XDOM in which the data source is located
+     * @throws MacroExecutionException in case of an error getting the XDOM
+     */
+    private XDOM computeXDOM(MacroTransformationContext context) throws MacroExecutionException
+    {
+        XDOM xdom;
+
+        // Parse the document content into an XDOM. If the reference is to the current document then we should not
+        // Parse the content again since 1) that's unnecessary since we can hold of the XDOM from the Transformation
+        // Context and 2) it's going to cause a cycle...
+        if (this.documentReference == null) {
+            xdom = context.getXDOM();
+        } else {
+            try {
+                DocumentModelBridge document = this.docBridge.getDocument(this.documentReference);
+                DocumentDisplayerParameters parameters = new DocumentDisplayerParameters();
+                parameters.setContentTranslated(true);
+                xdom = this.documentDisplayer.display(document, parameters);
+            } catch (Exception e) {
+                throw new MacroExecutionException(String.format("Error getting Chart table from document [%s]",
+                    this.documentReference, e));
+            }
+        }
+        return xdom;
     }
 
     @Override
@@ -166,17 +187,17 @@ public class DocumentTableBlockDataSource extends AbstractTableBlockDataSource
     {
         super.validateParameters();
 
-        if (null == this.documentReference) {
-            this.documentReference = this.docBridge.getCurrentDocumentReference();
-        } else if (!authorizationManager.hasAccess(Right.VIEW,
-            this.docBridge.getCurrentUserReference(),
-            this.documentReference))
-        {
-            throw new MacroExecutionException("You do not have permission to view the document.");
-        }
+        if (this.documentReference != null) {
+            if (!authorizationManager.hasAccess(Right.VIEW, this.docBridge.getCurrentUserReference(),
+                this.documentReference))
+            {
+                throw new MacroExecutionException("You do not have permission to view the document.");
+            }
 
-        if (!this.docBridge.exists(this.documentReference)) {
-            throw new MacroExecutionException(String.format("Document [%s] does not exist.", this.documentReference));
+            if (!this.docBridge.exists(this.documentReference)) {
+                throw new MacroExecutionException(
+                    String.format("Document [%s] does not exist.", this.documentReference));
+            }
         }
     }
 }

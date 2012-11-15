58f9ea726e2e4fc41aac5b639c709f219f00bf14
Andreas Jonsson <andreas.jonsson@kreablo.se>
2012-11-15 16:11:11 +0100

    XWIKI-7879: Removed reduntant call to removeContext.

--- xwiki-platform-core/xwiki-platform-watchlist/xwiki-platform-watchlist-api/src/main/java/com/xpn/xwiki/plugin/watchlist/WatchListJob.java
+++ xwiki-platform-core/xwiki-platform-watchlist/xwiki-platform-watchlist-api/src/main/java/com/xpn/xwiki/plugin/watchlist/WatchListJob.java
@@ -34,7 +34,6 @@ import org.slf4j.LoggerFactory;
 import org.xwiki.container.Container;
 import org.xwiki.container.servlet.ServletContainerException;
 import org.xwiki.container.servlet.ServletContainerInitializer;
-import org.xwiki.context.Execution;
 
 import com.xpn.xwiki.XWikiContext;
 import com.xpn.xwiki.XWikiException;
@@ -132,14 +131,12 @@ public class WatchListJob extends AbstractJob implements Job
     protected void cleanupComponents()
     {
         Container container = Utils.getComponent(Container.class);
-        Execution execution = Utils.getComponent(Execution.class);
 
         // We must ensure we clean the ThreadLocal variables located in the Container and Execution
         // components as otherwise we will have a potential memory leak.
         container.removeRequest();
         container.removeResponse();
         container.removeSession();
-        execution.removeContext();
     }
 
     /**

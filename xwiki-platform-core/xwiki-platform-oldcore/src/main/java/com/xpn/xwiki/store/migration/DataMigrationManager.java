2378bad3e4bfac69cf110e7b77738cd5d7108b1c
Denis Gervalle <dgl@softec.lu>
2013-01-22 12:21:29 +0100

    XWIKI-8718: Improve the message in the UI when the database is not up to date to properly report the exact cause of failure

--- xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/store/migration/DataMigrationManager.java
+++ xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/store/migration/DataMigrationManager.java
@@ -38,6 +38,12 @@ public interface DataMigrationManager
     XWikiDBVersion getDBVersion() throws DataMigrationException;
 
     /**
+     * @return current DB migration status or null for a new database
+     * @throws DataMigrationException if any error
+     */
+    DataMigrationStatus getDataMigrationStatus() throws DataMigrationException;
+
+    /**
      * Check current database version and proceed to migrations.
      * Migration is processed only once, and depends on the following configuration:
      * @xwikicfg xwiki.store.migration  - 1 to enable migration, default to 0

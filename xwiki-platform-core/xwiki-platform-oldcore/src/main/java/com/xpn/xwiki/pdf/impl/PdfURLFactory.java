046ea3308a39666b3b9471fec2c79e365da5a3e1
Vincent Massol <vincent@massol.net>
2013-04-10 10:27:47 +0200

    XWIKI-8978: PDF Export does not handle XWiki links to attached files properly

--- xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/pdf/impl/PdfURLFactory.java
+++ xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/pdf/impl/PdfURLFactory.java
@@ -19,278 +19,88 @@
  */
 package com.xpn.xwiki.pdf.impl;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 import java.net.URL;
-import java.net.URLEncoder;
+import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.commons.io.FilenameUtils;
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.xwiki.model.reference.AttachmentReference;
 import org.xwiki.model.reference.DocumentReference;
 
-import com.xpn.xwiki.XWiki;
 import com.xpn.xwiki.XWikiContext;
-import com.xpn.xwiki.doc.XWikiAttachment;
-import com.xpn.xwiki.doc.XWikiDocument;
-import com.xpn.xwiki.util.Util;
-import com.xpn.xwiki.web.XWikiServletURLFactory;
 
 /**
- * Special URL Factory used during exports, which stores referenced attachments and resources on the filesystem, in a
- * temporary folder, so that they can be included in the export result. The returned URLs point to these resources as
- * {@code file://} links, and not as {@code http://} links.
+ * Computes Image and Link URLs for attachments by using an absolute URLs but stores a Map in the XWiki Context to
+ * associate attachment URLs to Attachment Entity References so that when executing a PDF export the custom URI Resolver
+ * we use can stream image attachment content so that they are embedded in the PDF.
  * 
  * @version $Id$
  */
-public class PdfURLFactory extends XWikiServletURLFactory
+public class PdfURLFactory extends FileSystemURLFactory
 {
-    /** Logging helper object. */
-    private static final Logger LOGGER = LoggerFactory.getLogger(PdfURLFactory.class);
-
-    /** Segment separator used in the collision-free key generation. */
-    private static final String SEPARATOR = "/";
-
     /**
-     * {@inheritDoc}
+     * Key used to save image attachment data.
+     * @see #saveAttachmentReference(java.net.URL, String, String, String, String, com.xpn.xwiki.XWikiContext)
      */
+    static final String PDF_EXPORT_CONTEXT_KEY = "pdfExportImageURLMap";
+
     @Override
     public URL createAttachmentURL(String filename, String space, String name, String action, String querystring,
         String wiki, XWikiContext context)
     {
-        try {
-            return getURL(wiki, space, name, filename, null, context);
-        } catch (Exception ex) {
-            LOGGER.warn("Failed to save image for PDF export", ex);
-            return super.createAttachmentURL(filename, space, name, action, null, wiki, context);
-        }
+        URL url = super.createAttachmentURL(filename, space, name, action, querystring, wiki, context);
+        saveAttachmentReference(url, wiki, space, name, filename, context);
+        return url;
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public URL createAttachmentRevisionURL(String filename, String space, String name, String revision, String wiki,
         XWikiContext context)
     {
-        try {
-            return getURL(wiki, space, name, filename, revision, context);
-        } catch (Exception ex) {
-            LOGGER.warn("Failed to save image for PDF export: " + ex.getMessage());
-            return super.createAttachmentRevisionURL(filename, space, name, revision, wiki, context);
-        }
+        URL url = super.createAttachmentURL(filename, space, name, revision, wiki, context);
+        saveAttachmentReference(url, wiki, space, name, filename, context);
+        return url;
     }
 
     @Override
-    public URL createSkinURL(String filename, String skin, XWikiContext context)
+    public URL createAttachmentRevisionURL(String filename, String web, String name, String revision,
+        String querystring, String xwikidb, XWikiContext context)
     {
-        try {
-            Map<String, File> usedFiles = getFileMapping(context);
-            String key = getSkinfileKey(filename, skin);
-            if (!usedFiles.containsKey(key)) {
-                if (!copyResource("/skins/" + skin + '/' + filename, key, usedFiles, context)) {
-                    // The resource does not exist, just return a http:// URL
-                    return super.createSkinURL(filename, skin, context);
-                }
-            }
-            return usedFiles.get(key).toURI().toURL();
-        } catch (Exception ex) {
-            // Shouldn't happen
-            return super.createSkinURL(filename, skin, context);
-        }
+        URL url = super.createAttachmentRevisionURL(filename, web, name, revision, querystring, xwikidb,
+            context);
+        saveAttachmentReference(url, xwikidb, web, name, filename, context);
+        return url;
     }
 
     @Override
-    public URL createResourceURL(String filename, boolean forceSkinAction, XWikiContext context)
+    public URL createAttachmentRevisionURL(String filename, String web, String name, String revision, long recycleId,
+        String querystring, String xwikidb, XWikiContext context)
     {
-        try {
-            Map<String, File> usedFiles = getFileMapping(context);
-            String key = getResourceKey(filename);
-            if (!usedFiles.containsKey(key)) {
-                if (!copyResource("/resources/" + filename, key, usedFiles, context)) {
-                    return super.createResourceURL(filename, forceSkinAction, context);
-                }
-            }
-            return usedFiles.get(key).toURI().toURL();
-        } catch (Exception ex) {
-            // Shouldn't happen
-            return super.createResourceURL(filename, forceSkinAction, context);
-        }
+        URL url = super.createAttachmentRevisionURL(filename, web, name, revision, recycleId, querystring, xwikidb,
+            context);
+        saveAttachmentReference(url, xwikidb, web, name, filename, context);
+        return url;
     }
 
     /**
-     * {@inheritDoc}
+     * @param url the URL to save in the attachment map
+     * @param wiki the wiki where the attachment is located
+     * @param space the space where the attachment is located
+     * @param page the page where the attachment is located
+     * @param fileName the name of the attachment file
+     * @param context the XWiki Context where to find the attachment map that we add to
+     * @see PDFURIResolver
      */
-    @Override
-    public String getURL(URL url, XWikiContext context)
+    private void saveAttachmentReference(URL url, String wiki, String space, String page, String fileName,
+        XWikiContext context)
     {
-        if (url == null) {
-            return "";
+        // Save Entity Reference pointed to by this URL in the Context so that it can be used at export time in the
+        // PDF URIResolver code.
+        Map<String, AttachmentReference> attachmentMap =
+            (Map<String, AttachmentReference>) context.get(PDF_EXPORT_CONTEXT_KEY);
+        if (attachmentMap == null) {
+            attachmentMap = new HashMap<String, AttachmentReference>();
+            context.put(PDF_EXPORT_CONTEXT_KEY, attachmentMap);
         }
-        return Util.escapeURL(url.toString());
-    }
-
-    /**
-     * Store the requested attachment on the filesystem and return a {@code file://} URL where FOP can access that file.
-     * 
-     * @param wiki the name of the owner document's wiki
-     * @param space the name of the owner document's space
-     * @param name the name of the owner document
-     * @param filename the name of the attachment
-     * @param revision an optional attachment version
-     * @param context the current request context
-     * @return a {@code file://} URL where the attachment has been stored
-     * @throws Exception if the attachment can't be retrieved from the database and stored on the filesystem
-     */
-    private URL getURL(String wiki, String space, String name, String filename, String revision, XWikiContext context)
-        throws Exception
-    {
-        Map<String, File> usedFiles = getFileMapping(context);
-        String key = getAttachmentKey(space, name, filename, revision);
-        if (!usedFiles.containsKey(key)) {
-            File file = getTemporaryFile(key, context);
-            LOGGER.debug("Temporary PDF export file [{}]", file.toString());
-            XWikiDocument doc =
-                context.getWiki()
-                    .getDocument(
-                        new DocumentReference(StringUtils.defaultString(wiki, context.getDatabase()), space, name),
-                        context);
-            XWikiAttachment attachment = doc.getAttachment(filename);
-            if (StringUtils.isNotEmpty(revision)) {
-                attachment = attachment.getAttachmentRevision(revision, context);
-            }
-            FileOutputStream fos = new FileOutputStream(file);
-            IOUtils.copy(attachment.getContentInputStream(context), fos);
-            fos.close();
-            usedFiles.put(key, file);
-        }
-        return usedFiles.get(key).toURI().toURL();
-    }
-
-    /**
-     * Copy a resource from the filesystem into a temporary file and map this resulting file to the requested resource
-     * location.
-     * 
-     * @param resourceName the name of the file to copy, possibly including a path to it, for example
-     *            {@code icons/silk/add.png}
-     * @param key the collision-free identifier of the resource
-     * @param usedFiles the mapping of resource keys to temporary files where to put the resulting temporary file
-     * @param context the current request context
-     * @return {@code true} if copying the resource succeeded and the new temporary file was mapped to the resource key,
-     *         {@code false} otherwise
-     */
-    private boolean copyResource(String resourceName, String key, Map<String, File> usedFiles, XWikiContext context)
-    {
-        try {
-            InputStream data = context.getWiki().getResourceAsStream(resourceName);
-            if (data != null) {
-                // Copy the resource to a temporary file
-                File file = getTemporaryFile(key, context);
-                FileOutputStream fos = new FileOutputStream(file);
-                IOUtils.copy(data, fos);
-                fos.close();
-                usedFiles.put(key, file);
-                return true;
-            }
-        } catch (Exception ex) {
-            // Can't access the resource, let's hope FOP can handle the http:// URL
-        }
-        return false;
-    }
-
-    /**
-     * Computes a safe identifier for an attachment, guaranteed to be collision-free.
-     * 
-     * @param space the name of the owner document's space
-     * @param name the name of the owner document
-     * @param filename the name of the attachment
-     * @param revision an optional attachment version
-     * @return an identifier for this attachment
-     */
-    private String getAttachmentKey(String space, String name, String filename, String revision)
-    {
-        try {
-            return "attachment" + SEPARATOR + URLEncoder.encode(space, XWiki.DEFAULT_ENCODING) + SEPARATOR
-                + URLEncoder.encode(name, XWiki.DEFAULT_ENCODING) + SEPARATOR
-                + URLEncoder.encode(filename, XWiki.DEFAULT_ENCODING) + SEPARATOR
-                + URLEncoder.encode(StringUtils.defaultString(revision), XWiki.DEFAULT_ENCODING);
-        } catch (UnsupportedEncodingException ex) {
-            // This should never happen, UTF-8 is always available
-            return space + SEPARATOR + name + SEPARATOR + filename + SEPARATOR + StringUtils.defaultString(revision);
-        }
-    }
-
-    /**
-     * Computes a safe identifier for a resource file, guaranteed to be collision-free.
-     * 
-     * @param filename the name of the file, possibly including a path to it, for example {@code icons/silk/add.png}
-     * @return an identifier for this file
-     */
-    private String getResourceKey(String filename)
-    {
-        try {
-            return "resource" + SEPARATOR + URLEncoder.encode(filename, XWiki.DEFAULT_ENCODING);
-        } catch (UnsupportedEncodingException ex) {
-            // This should never happen, UTF-8 is always available
-            return filename;
-        }
-    }
-
-    /**
-     * Computes a safe identifier for a skin filename, guaranteed to be collision-free.
-     * 
-     * @param filename the name of the file, possibly including a path to it, for example {@code css/colors/black.css}
-     * @param skin the name of the skin where the file is expected to be
-     * @return an identifier for this file
-     */
-    private String getSkinfileKey(String filename, String skin)
-    {
-        try {
-            return "skin" + SEPARATOR + URLEncoder.encode(skin, XWiki.DEFAULT_ENCODING) + SEPARATOR
-                + URLEncoder.encode(filename, XWiki.DEFAULT_ENCODING);
-        } catch (UnsupportedEncodingException ex) {
-            // This should never happen, UTF-8 is always available
-            return skin + SEPARATOR + filename;
-        }
-    }
-
-    /**
-     * Retrieve the Map that relates resource keys to their corresponding temporary file.
-     * 
-     * @param context the current request context
-     * @return the mapping as it was found in the context (read-write)
-     */
-    private Map<String, File> getFileMapping(XWikiContext context)
-    {
-        @SuppressWarnings("unchecked")
-        Map<String, File> usedFiles = (Map<String, File>) context.get("pdfexport-file-mapping");
-        return usedFiles;
-    }
-
-    /**
-     * Create a new temporary file for the given resource key and return it.
-     * 
-     * @param key the resource key, needed for getting the file extension, if any
-     * @param context the current request context
-     * @return a new empty file
-     * @throws IOException if creating the file fails
-     */
-    private File getTemporaryFile(String key, XWikiContext context) throws IOException
-    {
-        File tempdir = (File) context.get("pdfexportdir");
-        String prefix = "pdf";
-        String suffix = "." + FilenameUtils.getExtension(key);
-        try {
-            return File.createTempFile(prefix, suffix, tempdir);
-        } catch (IOException e) {
-            throw new IOException("Failed to create temporary PDF export file with prefix [" + prefix + "], suffix ["
-                + suffix + "] in directory [" + tempdir + "]", e);
-        }
+        attachmentMap.put(url.toString(), new AttachmentReference(fileName, new DocumentReference(wiki, space, page)));
     }
 }

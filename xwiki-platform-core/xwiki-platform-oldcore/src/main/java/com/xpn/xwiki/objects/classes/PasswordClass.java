14d95b4831d84553d9b4d0be48d6921e200ff6db
tdelafosse <thomas.delafosse@xwiki.com>
2013-04-12 11:19:41 +0200

    XWIKI-5261 : Password hashes dumpable.
    (cherry picked from commit a837eeab2a02ba32f0ca40885f18cd6f2b2ab511)

--- xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/objects/classes/PasswordClass.java
+++ xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/objects/classes/PasswordClass.java
@@ -21,6 +21,7 @@ package com.xpn.xwiki.objects.classes;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 
 import org.apache.ecs.xhtml.input;
 import org.slf4j.Logger;
@@ -168,6 +169,23 @@ public class PasswordClass extends StringClass
         }
         return DEFAULT_HASH_ALGORITHM;
     }
+    
+    /**
+    * @param password
+    * @return The salt used for the given password. If this is an unsalted password, let it be known by returning "".
+    */
+        public String getSaltFromPassword(String password)
+        {
+            String[] components = password.split(":");
+            if(components.length == 4)
+            {
+                return components[2];
+            }
+            else
+            {
+                return "";
+            }
+        }
 
     /**
      * Transforms a plain text password so that it has the same encryption as a password stored in the database. The
@@ -182,7 +200,7 @@ public class PasswordClass extends StringClass
     {
         String result = plainPassword;
         if (storedPassword.startsWith(HASH_IDENTIFIER + SEPARATOR)) {
-            result = getPasswordHash(result, getAlgorithmFromPassword(storedPassword));
+            result = getPasswordHash(result, getAlgorithmFromPassword(storedPassword), getSaltFromPassword(storedPassword));
         } else if (storedPassword.startsWith(CRYPT_IDENTIFIER + SEPARATOR)) {
             result = getPasswordCrypt(result, getAlgorithmFromPassword(storedPassword));
         }
@@ -214,17 +232,28 @@ public class PasswordClass extends StringClass
 
     public String getPasswordHash(String password)
     {
-        return getPasswordHash(password, getHashAlgorithm());
+        return getPasswordHash(password, getHashAlgorithm(), null);
     }
 
-    public String getPasswordHash(String password, String algorithmName)
+    public String getPasswordHash(String password, String algorithmName, String salt)
     {
+        //If no salt given, let's generate one.
+        if(salt == null)
+        {
+            salt = randomSalt();
+        }
         try {
             LOGGER.debug("Hashing password");
             MessageDigest hashAlgorithm = MessageDigest.getInstance(algorithmName);
-            hashAlgorithm.update(password.getBytes());
+            String saltedPassword = salt + password;
+            hashAlgorithm.update(saltedPassword.getBytes());
             byte[] digest = hashAlgorithm.digest();
             StringBuffer sb = new StringBuffer(HASH_IDENTIFIER + SEPARATOR + algorithmName + SEPARATOR);
+            //Backward compatibility concern : let's keep unsalted password the way they are.
+            if(!salt.equals(""))
+            {
+                sb.append(salt + SEPARATOR);
+            }
             for (int j = 0; j < digest.length; ++j) {
                 int b = digest[j] & 0xFF;
                 if (b < 0x10) {
@@ -239,5 +268,25 @@ public class PasswordClass extends StringClass
             LOGGER.error("Error hashing password", ex);
         }
         return password;
+    }
+    
+    public static String randomSalt()
+    {
+        String salt = "";
+        SecureRandom random = new SecureRandom();
+        byte bytes[] = new byte[32];
+        random.nextBytes(bytes);
+        for (int i = 0; i < bytes.length; i++)
+        {
+            byte temp = bytes[i];
+            String s = Integer.toHexString(new Byte(temp));
+            while (s.length() < 2)
+            {
+                s = "0" + s;
+            }
+            s = s.substring(s.length() - 2);
+            salt += s;
+        }
+        return salt;
     }
 }

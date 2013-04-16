38606e95b1ad008e4b09185fbca8c0c5544bb03b
Marius Dumitru Florea <marius@xwiki.com>
2013-04-16 17:51:02 +0300

    XWIKI-8744: Introduce per wiki distribution status * Refactor the last step (Extensions) to use the Extension Updater.

--- xwiki-platform-core/xwiki-platform-web/src/main/webapp/resources/uicomponents/extension/distribution.js
+++ xwiki-platform-core/xwiki-platform-web/src/main/webapp/resources/uicomponents/extension/distribution.js
@@ -254,61 +254,28 @@ XWiki.OutdatedExtensionsStep = Class.create({
     this.container = $('distributionWizard');
     // Listen to extension status change to be able to update the step buttons.
     document.observe('xwiki:extension:statusChanged', this._updateStepButtons.bind(this));
-    // Refresh the upgrade plan job status.
-    this._maybeScheduleRefresh(false);
-  },
-
-  _maybeScheduleRefresh : function(afterUpdate, timeout) {
-    if (this.container.down('.xdialog-content > .ui-progress')) {
-      // Disable the step buttons while the upgrade plan is being created.
-      $('stepButtons').up().disable();
-      this._refresh.bind(this).delay(timeout || 1);
-    } else if (afterUpdate) {
-      this._updateStepButtons();
-    }
-  },
-
-  _refresh : function() {
-    new Ajax.Request('', {
-      onSuccess: function(response) {
-        this._update(response.responseText);
-      }.bind(this),
-      onFailure : this._maybeScheduleRefresh.bind(this, false, 10)
-    });
-  },
-
-  _update : function(html) {
-    var content = this.container.down('.xdialog-content');
-    var progressBar = content.childElements().find(function(child) {
-      return child.hasClassName('ui-progress');
-    })
-    if (progressBar) {
-      // Remove the loading message.
-      progressBar.previous().remove();
-      // Insert the HTML response before the progress bar.
-      progressBar.insert({before: html});
-      // Remove the old upgrade log and the old progress bar.
-      progressBar.next().remove();
-      progressBar.remove();
-      // Enhance the inserted HTML content.
-      document.fire('xwiki:dom:updated', {elements: [content]});
-      this._maybeScheduleRefresh(true);
-    }
+    // Listen to DOM changes to catch when the list of extensions is reloaded.
+    document.observe('xwiki:dom:updated', function(event) {
+      event.memo.elements.each(function(element) {
+        element.id == 'extensionUpdater' && this._updateStepButtons();
+      }.bind(this));
+    }.bindAsEventListener(this));
   },
 
   _updateStepButtons : function() {
     var stepButtons = $('stepButtons');
     if (!stepButtons) return;
     stepButtons = stepButtons.select('button');
+    var checkForUpdatesLink = this.container.down('.checkForUpdates');
     // Disable the step buttons if there is any extension loading.
-    if (this.container.select('.extension-item-loading').size() > 0) {
+    if (this.container.down('.extension-item-loading') || this.container.down('.extension-log-item-loading')) {
       // Disable all step buttons.
       stepButtons.invoke('disable');
-      $('prepareUpgradeLink') && $('prepareUpgradeLink').up().hide();
+      checkForUpdatesLink && checkForUpdatesLink.up().hide();
     } else {
       // Enable all step buttons.
       stepButtons.invoke('enable');
-      $('prepareUpgradeLink') && $('prepareUpgradeLink').up().show();
+      checkForUpdatesLink && checkForUpdatesLink.up().show();
       // Show the Continue button if all the invalid extensions have been fixed.
       if (this._noInvalidExtensions()) {
         // Show the Continue button.

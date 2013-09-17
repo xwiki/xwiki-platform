b5ee0281abc15c672a74eeaf6a937da6b01445fc
tmortagne <thomas.mortagne@gmail.com>
2013-09-17 11:54:45 +0200

    [codestyle] Apply codestyle

--- xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/plugin/packaging/PackageAPI.java
+++ xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/plugin/packaging/PackageAPI.java
@@ -33,8 +33,8 @@ import com.xpn.xwiki.util.Util;
 
 public class PackageAPI extends Api
 {
-    Package plugin;
-    
+    Package pack;
+
     public PackageAPI(Package plugin, XWikiContext context) throws PackageException
     {
         super(context);
@@ -49,134 +49,133 @@ public class PackageAPI extends Api
 
     private void setPlugin(Package plugin)
     {
-        this.plugin = plugin;
+        this.pack = plugin;
     }
 
     public String getName()
     {
-        return this.plugin.getName();
+        return this.pack.getName();
     }
 
     public void setName(String name)
     {
-        this.plugin.setName(name);
+        this.pack.setName(name);
     }
 
     public Package getPackage()
     {
         if (hasProgrammingRights()) {
-            return this.plugin;
+            return this.pack;
         }
         return null;
     }
 
     public String getDescription()
     {
-        return this.plugin.getDescription();
+        return this.pack.getDescription();
     }
 
     public void setDescription(String description)
     {
-        this.plugin.setDescription(description);
+        this.pack.setDescription(description);
     }
 
     public String getVersion()
     {
-        return this.plugin.getVersion();
+        return this.pack.getVersion();
     }
 
     public void setVersion(String version)
     {
-        this.plugin.setVersion(version);
+        this.pack.setVersion(version);
     }
 
     public String getLicence()
     {
-        return this.plugin.getLicence();
+        return this.pack.getLicence();
     }
 
     public void setLicence(String licence)
     {
-        this.plugin.setLicence(licence);
+        this.pack.setLicence(licence);
     }
 
     public String getAuthorName()
     {
-        return this.plugin.getAuthorName();
+        return this.pack.getAuthorName();
     }
 
     public void setAuthorName(String authorName)
     {
-        this.plugin.setAuthorName(authorName);
+        this.pack.setAuthorName(authorName);
     }
 
     public boolean isBackupPack()
     {
-        return this.plugin.isBackupPack();
+        return this.pack.isBackupPack();
     }
 
     public void setBackupPack(boolean backupPack)
     {
-        this.plugin.setBackupPack(backupPack);
+        this.pack.setBackupPack(backupPack);
     }
-    
+
     /**
-     * Indicate if the current user has the right to import a package as a backup pack. In this implementation, to be 
-     * able to import has backup pack the user must have the admin right on the XWiki.XWikiPreferences document from
-     * the main wiki (xwiki:XWiki.XWikiPreferences). The goal is to prevent local wiki administrators from importing
-     * documents saved with a global administrator as the author (rights escalation). 
+     * Indicate if the current user has the right to import a package as a backup pack. In this implementation, to be
+     * able to import has backup pack the user must have the admin right on the XWiki.XWikiPreferences document from the
+     * main wiki (xwiki:XWiki.XWikiPreferences). The goal is to prevent local wiki administrators from importing
+     * documents saved with a global administrator as the author (rights escalation).
      * 
      * @return true if the current user has the rights to import a package as a backup pack, false otherwise
      */
     public boolean hasBackupPackImportRights()
     {
-        return plugin.hasBackupPackImportRights(context);
+        return pack.hasBackupPackImportRights(context);
     }
 
     public boolean isVersionPreserved()
     {
-        return this.plugin.isVersionPreserved();
+        return this.pack.isVersionPreserved();
     }
 
     /**
-     * Sets the flag for the packager to preserve or not existing versions of documents when installing 
-     * with {@link #install()}. If set to true, the existing history revisions of documents will be preserve,
-     * if not, the history will be overridden. 
+     * Sets the flag for the packager to preserve or not existing versions of documents when installing with
+     * {@link #install()}. If set to true, the existing history revisions of documents will be preserve, if not, the
+     * history will be overridden.
      * 
      * @param preserveVersion
      */
     public void setPreserveVersion(boolean preserveVersion)
     {
-        this.plugin.setPreserveVersion(preserveVersion);
+        this.pack.setPreserveVersion(preserveVersion);
     }
 
     public boolean isWithVersions()
     {
-        return this.plugin.isWithVersions();
+        return this.pack.isWithVersions();
     }
 
     /**
-     * Sets the flag for the packager to import or not history revisions included in the archive
-     * when installing with {@link #install()}.
-     * This flag will be ignored if {@link #isWithVersions()} flag is set to true. This means it's not possible
-     * to import with versions, preserving the existing document history. The behavior of the packager in this case
-     * fall backs on just adding a new version to the exsting history (ignoring the history from the package).  
+     * Sets the flag for the packager to import or not history revisions included in the archive when installing with
+     * {@link #install()}. This flag will be ignored if {@link #isWithVersions()} flag is set to true. This means it's
+     * not possible to import with versions, preserving the existing document history. The behavior of the packager in
+     * this case fall backs on just adding a new version to the exsting history (ignoring the history from the package).
      * 
      * @param withVersions should the versions contained in the archive (if any) be imported when installing.
      */
     public void setWithVersions(boolean withVersions)
     {
-        this.plugin.setWithVersions(withVersions);
+        this.pack.setWithVersions(withVersions);
     }
 
     public void addDocumentFilter(Object filter) throws PackageException
     {
-        this.plugin.addDocumentFilter(filter);
+        this.pack.addDocumentFilter(filter);
     }
 
     public List<DocumentInfoAPI> getFiles()
     {
-        List<DocumentInfo> files = this.plugin.getFiles();
+        List<DocumentInfo> files = this.pack.getFiles();
         ArrayList<DocumentInfoAPI> apiFiles = new ArrayList<DocumentInfoAPI>(files.size());
 
         for (DocumentInfo docInfo : files) {
@@ -188,17 +187,17 @@ public class PackageAPI extends Api
 
     public boolean add(String docFullName, int DefaultAction) throws XWikiException
     {
-        return this.plugin.add(docFullName, DefaultAction, getXWikiContext());
+        return this.pack.add(docFullName, DefaultAction, getXWikiContext());
     }
 
     public boolean add(String docFullName) throws XWikiException
     {
-        return this.plugin.add(docFullName, getXWikiContext());
+        return this.pack.add(docFullName, getXWikiContext());
     }
 
     public void setDocumentAction(String docFullName, int action)
     {
-        for (DocumentInfo docInfo : this.plugin.getFiles()) {
+        for (DocumentInfo docInfo : this.pack.getFiles()) {
             if (docInfo.getFullName().compareTo(docFullName) == 0) {
                 docInfo.setAction(action);
             }
@@ -207,7 +206,7 @@ public class PackageAPI extends Api
 
     public void setDocumentAction(String docFullName, String language, int action)
     {
-        for (DocumentInfo docInfo : this.plugin.getFiles()) {
+        for (DocumentInfo docInfo : this.pack.getFiles()) {
             if ((docInfo.getFullName().compareTo(docFullName) == 0) && (language.equals(docInfo.getLanguage()))) {
                 docInfo.setAction(action);
             }
@@ -218,57 +217,53 @@ public class PackageAPI extends Api
     {
         getXWikiContext().getResponse().setContentType("application/zip");
         getXWikiContext().getResponse().addHeader("Content-disposition",
-            "attachment; filename=" + Util.encodeURI(plugin.getName(), context) + ".xar");
+            "attachment; filename=" + Util.encodeURI(pack.getName(), context) + ".xar");
         getXWikiContext().setFinished(true);
 
-        return this.plugin.export(getXWikiContext().getResponse().getOutputStream(), getXWikiContext());
+        return this.pack.export(getXWikiContext().getResponse().getOutputStream(), getXWikiContext());
     }
 
-
-    /** 
-     * Similar to {@link #Import(byte[])}, except expected errors are catch. This version should be privileged
-     * when using the packager API from velocity scripts since it will not display stack-trace in case of error
-     * (for example if the passed file is not a valid package).
+    /**
+     * Similar to {@link #Import(byte[])}, except expected errors are catch. This version should be privileged when
+     * using the packager API from velocity scripts since it will not display stack-trace in case of error (for example
+     * if the passed file is not a valid package).
      * 
      * @param data the file to create the package from, as a byte array.
-     * 
-     * @return true if the package creation succeeded, false otherwise. If the package creation failed, 
-     * the error message is placed in the velocity context under the <code>import_error</code> key,
-     * 
+     * @return true if the package creation succeeded, false otherwise. If the package creation failed, the error message
+     *         is placed in the velocity context under the <code>import_error</code> key,
      * @since 2.2M1
      */
-    public boolean importPackageFromByteArray(byte data[]) {
+    public boolean importPackageFromByteArray(byte data[])
+    {
         try {
-            this.plugin.Import(data, getXWikiContext());
+            this.pack.Import(data, getXWikiContext());
             return true;
-        }
-        catch (XWikiException e) {
+        } catch (XWikiException e) {
+            getXWikiContext().put("import_error", e.getMessage());
+            return false;
+        } catch (IOException e) {
             getXWikiContext().put("import_error", e.getMessage());
             return false;
         }
-        catch (IOException e) {
-            getXWikiContext().put("import_error", e.getMessage());
-            return false;            
-        }
     }
-    
+
     /**
-     * Load a package in memory from a byte array. It may be installed later using {@link #install()}.
-     * Your should prefer {@link #Import(InputStream)} which may avoid loading the package twice in memory.
-     *
+     * Load a package in memory from a byte array. It may be installed later using {@link #install()}. Your should
+     * prefer {@link #Import(InputStream)} which may avoid loading the package twice in memory.
+     * 
      * @param file an byte array containing a zipped package file
      * @return an empty string, useless.
      * @throws IOException while reading the ZipFile
      * @throws XWikiException when package content is broken
      */
-   public String Import(byte file[]) throws IOException, XWikiException
+    public String Import(byte file[]) throws IOException, XWikiException
     {
-        return this.plugin.Import(file, getXWikiContext());
+        return this.pack.Import(file, getXWikiContext());
     }
 
     /**
      * Load a package in memory from an InputStream. It may be installed later using {@link #install()}.
-     *
+     * 
      * @param file is an InputStream of a zipped package file
      * @return an empty string, useless.
      * @throws IOException while reading the ZipFile
@@ -277,63 +272,62 @@ public class PackageAPI extends Api
      */
     public String Import(InputStream file) throws IOException, XWikiException
     {
-        return this.plugin.Import(file, getXWikiContext());
+        return this.pack.Import(file, getXWikiContext());
     }
 
     public int testInstall()
     {
-        return this.plugin.testInstall(false, getXWikiContext());
+        return this.pack.testInstall(false, getXWikiContext());
     }
 
     public int testInstall(boolean isAdmin)
     {
-        return this.plugin.testInstall(isAdmin, getXWikiContext());
+        return this.pack.testInstall(isAdmin, getXWikiContext());
     }
 
     public void backupWiki() throws XWikiException, IOException
     {
-        this.plugin.addAllWikiDocuments(getXWikiContext());
+        this.pack.addAllWikiDocuments(getXWikiContext());
         this.export();
     }
 
     public String toXml()
     {
-        return this.plugin.toXml(getXWikiContext());
+        return this.pack.toXml(getXWikiContext());
     }
-    
+
     /**
      * @return a representation of this package under the JSON format
-     * 
      * @since 2.2M1
      */
     public JSONObject toJSON()
     {
-        return this.plugin.toJSON(getXWikiContext());
+        return this.pack.toJSON(getXWikiContext());
     }
 
     public int install() throws XWikiException
     {
-        return this.plugin.install(getXWikiContext());
+        return this.pack.install(getXWikiContext());
     }
 
     public List<String> getErrors()
     {
-        return this.plugin.getErrors(getXWikiContext());
+        return this.pack.getErrors(getXWikiContext());
     }
 
     public List<String> getSkipped()
     {
-        return this.plugin.getSkipped(getXWikiContext());
+        return this.pack.getSkipped(getXWikiContext());
     }
 
     public List<String> getInstalled()
     {
-        return this.plugin.getInstalled(getXWikiContext());
+        return this.pack.getInstalled(getXWikiContext());
     }
 
     public int getStatus()
     {
-        return this.plugin.getStatus(getXWikiContext());
+        return this.pack.getStatus(getXWikiContext());
     }
-    
+
 }

9bc09fe08e389d2c678f8cb511526109bfb00cca
tmortagne <thomas.mortagne@gmail.com>
2013-09-17 09:44:34 +0200

    XWIKI-9463: Introduce EnityReference property converter

---
+++ xwiki-platform-core/xwiki-platform-model/src/test/java/org/xwiki/model/internal/reference/converter/EntityReferenceConverterTest.java
@@ -0,0 +1,118 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.model.internal.reference.converter;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.xwiki.model.EntityType;
+import org.xwiki.model.reference.EntityReference;
+import org.xwiki.model.reference.EntityReferenceResolver;
+import org.xwiki.model.reference.EntityReferenceSerializer;
+import org.xwiki.properties.ConverterManager;
+import org.xwiki.properties.converter.Converter;
+import org.xwiki.properties.internal.converter.ColorConverter;
+import org.xwiki.test.annotation.AllComponents;
+import org.xwiki.test.mockito.MockitoComponentMockingRule;
+
+/**
+ * Validate {@link ColorConverter} component.
+ * 
+ * @version $Id$
+ */
+@AllComponents
+public class EntityReferenceConverterTest
+{
+    @Rule
+    public MockitoComponentMockingRule<Converter<EntityReference>> mocker =
+        new MockitoComponentMockingRule<Converter<EntityReference>>(EntityReferenceConverter.class);
+
+    private ConverterManager converterManager;
+
+    private EntityReferenceResolver<String> mockStringResolver;
+
+    private EntityReferenceSerializer<String> mockSerialier;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        this.converterManager = mocker.getInstance(ConverterManager.class);
+
+        this.mockStringResolver = this.mocker.getInstance(EntityReferenceResolver.TYPE_STRING, "relative");
+        this.mockSerialier = this.mocker.getInstance(EntityReferenceSerializer.TYPE_STRING);
+    }
+
+    @Test
+    public void testConvertDocumentFromString()
+    {
+        EntityReference reference;
+
+        reference =
+            new EntityReference("page", EntityType.DOCUMENT, new EntityReference("space", EntityType.SPACE,
+                new EntityReference("wiki", EntityType.WIKI)));
+        Mockito.when(this.mockStringResolver.resolve("wiki:space.page", EntityType.DOCUMENT)).thenReturn(reference);
+        Assert
+            .assertEquals(reference, this.converterManager.convert(EntityReference.class, "document:wiki:space.page"));
+
+        reference = new EntityReference("page", EntityType.DOCUMENT, new EntityReference("space", EntityType.SPACE));
+        Mockito.when(this.mockStringResolver.resolve("space.page", EntityType.DOCUMENT)).thenReturn(reference);
+        Assert.assertEquals(reference, this.converterManager.convert(EntityReference.class, "document:space.page"));
+        Assert.assertEquals(reference, this.converterManager.convert(EntityReference.class, "space.page"));
+
+        reference = new EntityReference("page", EntityType.DOCUMENT);
+        Mockito.when(this.mockStringResolver.resolve("page", EntityType.DOCUMENT)).thenReturn(reference);
+        Assert.assertEquals(reference, this.converterManager.convert(EntityReference.class, "document:page"));
+        Assert.assertEquals(reference, this.converterManager.convert(EntityReference.class, "page"));
+    }
+
+    @Test
+    public void testConvertSpaceFromString()
+    {
+        EntityReference reference;
+
+        reference = new EntityReference("space", EntityType.SPACE, new EntityReference("wiki", EntityType.WIKI));
+        Mockito.when(this.mockStringResolver.resolve("wiki:space", EntityType.SPACE)).thenReturn(reference);
+        Assert.assertEquals(reference, this.converterManager.convert(EntityReference.class, "space:wiki:space"));
+
+        reference = new EntityReference("space", EntityType.SPACE);
+        Mockito.when(this.mockStringResolver.resolve("space", EntityType.SPACE)).thenReturn(reference);
+        Assert.assertEquals(reference, this.converterManager.convert(EntityReference.class, "space:space"));
+    }
+
+    @Test
+    public void testConvertFromNull()
+    {
+        Assert.assertNull(this.converterManager.convert(EntityReference.class, null));
+    }
+
+    @Test
+    public void testConvertToString()
+    {
+        EntityReference reference;
+
+        reference =
+            new EntityReference("page", EntityType.DOCUMENT, new EntityReference("space", EntityType.SPACE,
+                new EntityReference("wiki", EntityType.WIKI)));
+        Mockito.when(this.mockSerialier.serialize(reference)).thenReturn("wiki:space.page");
+        Assert.assertEquals("document:wiki:space.page", this.converterManager.convert(String.class, reference));
+    }
+}

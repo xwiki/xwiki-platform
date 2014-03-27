7889cd534fc69a1a69c19f71eb9891fcc8018d9f
Guillaume Delhumeau <guillaume.delhumeau@xwiki.com>
2014-03-27 18:36:20 +0100

    XWIKI-10181: Wrong server name set when a Wiki is created with a domain name setup. XWIKI-10185: Add a script service to know if path mode is enabled and what is the default alias suffix.
    * Move the computation of alias suffix from request URL from java (server-side) to javascript (client-side). This should never have been in DefaultWikiConfiguration.
    
    Conflicts:
    
    	xwiki-platform-core/xwiki-platform-wiki/xwiki-platform-wiki-default/pom.xml

--- xwiki-platform-core/xwiki-platform-wiki/xwiki-platform-wiki-default/src/main/java/org/xwiki/wiki/internal/configuration/DefaultWikiConfiguration.java
+++ xwiki-platform-core/xwiki-platform-wiki/xwiki-platform-wiki-default/src/main/java/org/xwiki/wiki/internal/configuration/DefaultWikiConfiguration.java
@@ -19,21 +19,12 @@
  */
 package org.xwiki.wiki.internal.configuration;
 
-import java.net.MalformedURLException;
-
 import javax.inject.Inject;
 import javax.inject.Named;
-import javax.inject.Provider;
 
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
 import org.xwiki.component.annotation.Component;
 import org.xwiki.configuration.ConfigurationSource;
-import org.xwiki.resource.ResourceCreationException;
-import org.xwiki.url.internal.ExtendedURL;
 import org.xwiki.wiki.configuration.WikiConfiguration;
-
-import com.xpn.xwiki.XWikiContext;
 
 /**
  * Default implementation for {@link WikiConfiguration}.
@@ -56,52 +47,9 @@ public class DefaultWikiConfiguration implements WikiConfiguration
     @Named("xwikiproperties")
     private ConfigurationSource configuration;
 
-    @Inject
-    private Provider<XWikiContext> xcontextProvider;
-
-    @Inject
-    private Logger logger;
-
     @Override
     public String getAliasSuffix()
     {
-        String suffix = configuration.getProperty(PREFIX + "alias.suffix");
-        if (suffix == null) {
-            // Try to deduce it from the request URL.
-            suffix = deduceAliasSuffixFromRequest();
-            // If it has failed, then we return an empty suffix
-            if (suffix == null) {
-                return "";
-            }
-        }
-        return suffix;
-    }
-
-    private String deduceAliasSuffixFromRequest()
-    {
-        String suffix = null;
-        String host = getHost();
-        // We cannot deduce a suffix with a request to "locahost" or to an IP.
-        if (host != null && !"localhost".equals(host) && !host.matches("[0-9]{1,3}(?:\\.[0-9]{1,3}){3}")) {
-            suffix = StringUtils.substringAfter(host, ".");
-        }
-        return suffix;
-    }
-
-    private String getHost()
-    {
-        String host = null;
-        final String error = "Failed to get the host name of the request.";
-        try {
-            XWikiContext xcontext = xcontextProvider.get();
-            ExtendedURL url = new ExtendedURL(new java.net.URL(xcontext.getRequest().getRequestURL().toString()),
-                    "xwiki");
-            host = url.getURI().getHost();
-        } catch (ResourceCreationException e) {
-            logger.error(error, e);
-        } catch (MalformedURLException e) {
-            logger.error(error, e);
-        }
-        return host;
+        return configuration.getProperty(PREFIX + "alias.suffix", "");
     }
 }

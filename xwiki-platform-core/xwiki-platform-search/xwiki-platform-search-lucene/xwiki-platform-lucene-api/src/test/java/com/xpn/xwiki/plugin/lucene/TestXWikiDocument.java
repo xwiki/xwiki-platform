4c0fe986511ae1e70a80af83b61bf521d020f9c3
tmortagne <thomas.mortagne@gmail.com>
2014-04-07 12:44:51 +0200

    Revert "XWIKI-10224: Move Lucene plugin in xwiki-contrib"
    This reverts commit 9ae42c341aaf18f5f13a4f96dd8125e6d5fa5af7.

--- xwiki-platform-core/xwiki-platform-search/xwiki-platform-search-lucene/xwiki-platform-lucene-api/src/test/java/com/xpn/xwiki/plugin/lucene/TestXWikiDocument.java
+++ xwiki-platform-core/xwiki-platform-search/xwiki-platform-search-lucene/xwiki-platform-lucene-api/src/test/java/com/xpn/xwiki/plugin/lucene/TestXWikiDocument.java
@@ -1,6 +1,4 @@
-<?xml version="1.0" encoding="UTF-8"?>
-
-<!--
+/*
  * See the NOTICE file distributed with this work for additional
  * information regarding copyright ownership.
  *
@@ -18,25 +16,35 @@
  * License along with this software; if not, write to the Free
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
--->
+ */
+package com.xpn.xwiki.plugin.lucene;
 
-<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">
-  <modelVersion>4.0.0</modelVersion>
-  <parent>
-    <groupId>org.xwiki.platform</groupId>
-    <artifactId>xwiki-platform-core</artifactId>
-    <version>6.0-SNAPSHOT</version>
-  </parent>
-  <artifactId>xwiki-platform-search</artifactId>
-  <name>XWiki Platform - Search - Parent POM</name>
-  <packaging>pom</packaging>
-  <description>XWiki Platform - Search - Parent POM</description>
-  <properties>
-    <lucene.version>4.7.1</lucene.version>
-  </properties>
-  <modules>
-    <module>xwiki-platform-search-lucene</module>
-    <module>xwiki-platform-search-solr</module>
-    <module>xwiki-platform-search-ui</module>
-  </modules>
-</project>
+import org.xwiki.model.reference.DocumentReference;
+import org.xwiki.rendering.syntax.Syntax;
+
+import com.xpn.xwiki.XWikiContext;
+import com.xpn.xwiki.XWikiException;
+import com.xpn.xwiki.doc.XWikiDocument;
+
+/**
+ * @version $Id$
+ */
+public class TestXWikiDocument extends XWikiDocument
+{
+    public TestXWikiDocument(DocumentReference documentReference)
+    {
+        super(documentReference);
+    }
+
+    @Override
+    public XWikiDocument getTranslatedDocument(String language, XWikiContext context) throws XWikiException
+    {
+        return this;
+    }
+
+    @Override
+    public String getRenderedTitle(Syntax outputSyntax, XWikiContext context)
+    {
+        return getTitle();
+    }
+}

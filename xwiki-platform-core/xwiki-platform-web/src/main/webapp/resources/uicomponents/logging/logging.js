7ca62425ffe1fed6d2e6457dddf94614c390c195
tmortagne <thomas.mortagne@gmail.com>
2014-07-22 12:58:12 +0200

    XWIKI-10669: Make log UI displayer toolkit usable outside of extension module

--- xwiki-platform-core/xwiki-platform-web/src/main/webapp/resources/uicomponents/logging/logging.js
+++ xwiki-platform-core/xwiki-platform-web/src/main/webapp/resources/uicomponents/logging/logging.js
@@ -2,19 +2,19 @@
  * Enhances the behaviour a log display.
  */
 require([ 'jquery' ], function($) {
-	$('.log-item').each(function(index, element) {
-		var stacktrace = $(this).find('.stacktrace');
-		if (stacktrace) {
-			// Hide the stacktrace by default.
-			stacktrace.toggle();
-			// Show the stacktrace when the log message is clicked.
-			var logMessage = $(this).find('div');
-			logMessage.css({
-				"cursor" : "pointer"
-			});
-			logMessage.click(function() {
-				stacktrace.toggle();
-			});
-		}
-	});
+  $('.log-item').each(function(index, element) {
+    var stacktrace = $(this).find('.stacktrace');
+    if (stacktrace) {
+      // Hide the stacktrace by default.
+      stacktrace.toggle();
+      // Show the stacktrace when the log message is clicked.
+      var logMessage = $(this).find('div');
+      logMessage.css({
+        "cursor" : "pointer"
+      });
+      logMessage.click(function() {
+        stacktrace.toggle();
+      });
+    }
+  });
 });

784280f952c213de15d7f483a1143aa06dc05d26
Eduard Moraru <Enygma2002@gmail.com>
2014-08-14 19:44:47 +0300

    XWIKI-9567: Cannot restore document translations from recycle bin - Not allowing to restore a translation if its main document is not restored or re-created before. - Also displaying the document language for ease of use. - Added tests.

--- xwiki-platform-core/xwiki-platform-oldcore/src/test/java/com/xpn/xwiki/web/UndeleteActionTest.java
+++ xwiki-platform-core/xwiki-platform-oldcore/src/test/java/com/xpn/xwiki/web/UndeleteActionTest.java
@@ -19,17 +19,28 @@
  */
 package com.xpn.xwiki.web;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertFalse;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Locale;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.Mockito;
 import org.xwiki.csrf.CSRFToken;
+import org.xwiki.localization.LocaleUtils;
+import org.xwiki.model.reference.DocumentReference;
 import org.xwiki.test.mockito.MockitoComponentManagerRule;
 
 import com.xpn.xwiki.XWiki;
 import com.xpn.xwiki.XWikiContext;
+import com.xpn.xwiki.doc.XWikiDeletedDocument;
 import com.xpn.xwiki.doc.XWikiDocument;
 import com.xpn.xwiki.store.XWikiRecycleBinStoreInterface;
 
@@ -56,6 +67,16 @@ public class UndeleteActionTest
      */
     private XWikiContext context = mock(XWikiContext.class);
 
+    /**
+     * A mock {@link XWiki};
+     */
+    private XWiki xwiki = mock(XWiki.class);
+
+    /**
+     * A mock {@link XWikiDocument};
+     */
+    private XWikiDocument document = mock(XWikiDocument.class);
+
     @Before
     public void setUp() throws Exception
     {
@@ -64,11 +85,10 @@ public class UndeleteActionTest
 
         when(context.getRequest()).thenReturn(mock(XWikiRequest.class));
 
-        XWiki xwiki = mock(XWiki.class);
         when(context.getWiki()).thenReturn(xwiki);
 
-        XWikiDocument document = mock(XWikiDocument.class);
         when(context.getDoc()).thenReturn(document);
+        when(document.getDocumentReference()).thenReturn(new DocumentReference("xwiki", "Main", "DeletedDocument"));
     }
 
     @Test
@@ -89,16 +109,119 @@ public class UndeleteActionTest
         CSRFToken csrfToken = mocker.getInstance(CSRFToken.class);
         when(csrfToken.isTokenValid(null)).thenReturn(true);
 
-        when(context.getDoc().isNew()).thenReturn(true);
-        when(context.getWiki().hasRecycleBin(context)).thenReturn(true);
+        when(xwiki.hasRecycleBin(context)).thenReturn(true);
 
         when(context.getRequest().getParameter("id")).thenReturn("13");
 
         XWikiRecycleBinStoreInterface recycleBin = mock(XWikiRecycleBinStoreInterface.class);
-        when(context.getWiki().getRecycleBinStore()).thenReturn(recycleBin);
+        when(xwiki.getRecycleBinStore()).thenReturn(recycleBin);
+
+        XWikiDeletedDocument deletedDocument = mock(XWikiDeletedDocument.class);
+        when(xwiki.getDeletedDocument(anyString(), anyString(), anyInt(), any(XWikiContext.class))).thenReturn(
+            deletedDocument);
+
+        when(deletedDocument.getLanguage()).thenReturn("");
+
+        when(xwiki.exists(any(DocumentReference.class), any(XWikiContext.class))).thenReturn(false);
 
         assertFalse(undeleteAction.action(context));
 
-        verify(context.getWiki()).restoreFromRecycleBin(context.getDoc(), 13, "Restored from recycle bin", context);
+        verify(xwiki).restoreFromRecycleBin(document, 13, "Restored from recycle bin", context);
+    }
+
+    /**
+     * When the recycle bin is disabled, the document should not be restored.
+     */
+    @Test
+    public void testRecycleBinDisabled() throws Exception
+    {
+        CSRFToken csrfToken = mocker.getInstance(CSRFToken.class);
+        when(csrfToken.isTokenValid(null)).thenReturn(true);
+
+        when(xwiki.hasRecycleBin(context)).thenReturn(false);
+
+        when(context.getRequest().getParameter("id")).thenReturn("13");
+
+        XWikiRecycleBinStoreInterface recycleBin = mock(XWikiRecycleBinStoreInterface.class);
+        when(xwiki.getRecycleBinStore()).thenReturn(recycleBin);
+
+        XWikiDeletedDocument deletedDocument = mock(XWikiDeletedDocument.class);
+        when(xwiki.getDeletedDocument(anyString(), anyString(), anyInt(), any(XWikiContext.class))).thenReturn(
+            deletedDocument);
+
+        when(deletedDocument.getLanguage()).thenReturn("");
+
+        when(xwiki.exists(any(DocumentReference.class), any(XWikiContext.class))).thenReturn(false);
+
+        assertFalse(undeleteAction.action(context));
+
+        verify(xwiki, Mockito.never()).restoreFromRecycleBin(document, 13, "Restored from recycle bin", context);
+    }
+
+    /**
+     * When the location where to restore the document already exists, don`t override.
+     */
+    @Test
+    public void testDocumentAlreadyExists() throws Exception
+    {
+        CSRFToken csrfToken = mocker.getInstance(CSRFToken.class);
+        when(csrfToken.isTokenValid(null)).thenReturn(true);
+
+        when(xwiki.hasRecycleBin(context)).thenReturn(true);
+
+        when(context.getRequest().getParameter("id")).thenReturn("13");
+
+        XWikiRecycleBinStoreInterface recycleBin = mock(XWikiRecycleBinStoreInterface.class);
+        when(xwiki.getRecycleBinStore()).thenReturn(recycleBin);
+
+        XWikiDeletedDocument deletedDocument = mock(XWikiDeletedDocument.class);
+        when(xwiki.getDeletedDocument(anyString(), anyString(), anyInt(), any(XWikiContext.class))).thenReturn(
+            deletedDocument);
+
+        when(deletedDocument.getLanguage()).thenReturn("");
+
+        when(xwiki.exists(any(DocumentReference.class), any(XWikiContext.class))).thenReturn(true);
+
+        assertFalse(undeleteAction.action(context));
+
+        verify(xwiki, Mockito.never()).restoreFromRecycleBin(document, 13, "Restored from recycle bin", context);
+    }
+
+    /**
+     * @see "XWIKI-9567: Cannot restore document translations from recycle bin"
+     */
+    @Test
+    public void testRestoringTranslation() throws Exception
+    {
+        CSRFToken csrfToken = mocker.getInstance(CSRFToken.class);
+        when(csrfToken.isTokenValid(null)).thenReturn(true);
+
+        when(xwiki.hasRecycleBin(context)).thenReturn(true);
+
+        when(context.getRequest().getParameter("id")).thenReturn("13");
+
+        XWikiRecycleBinStoreInterface recycleBin = mock(XWikiRecycleBinStoreInterface.class);
+        when(xwiki.getRecycleBinStore()).thenReturn(recycleBin);
+
+        XWikiDeletedDocument deletedDocument = mock(XWikiDeletedDocument.class);
+        when(xwiki.getDeletedDocument(anyString(), anyString(), anyInt(), any(XWikiContext.class))).thenReturn(
+            deletedDocument);
+
+        // Document to restore is a translation.
+        when(deletedDocument.getLanguage()).thenReturn("ro");
+
+        DocumentReference translationDocumentReference =
+            new DocumentReference(document.getDocumentReference(), LocaleUtils.toLocale(deletedDocument.getLanguage(),
+                Locale.ROOT));
+        when(xwiki.exists(translationDocumentReference, context)).thenReturn(false);
+
+        assertFalse(undeleteAction.action(context));
+
+        // Make sure that the main document is not checked for existence, but the translated document which we actually
+        // want to restore.
+        verify(xwiki, Mockito.never()).exists(document.getDocumentReference(), context);
+        verify(xwiki).exists(translationDocumentReference, context);
+
+        verify(xwiki).restoreFromRecycleBin(document, 13, "Restored from recycle bin", context);
     }
 }

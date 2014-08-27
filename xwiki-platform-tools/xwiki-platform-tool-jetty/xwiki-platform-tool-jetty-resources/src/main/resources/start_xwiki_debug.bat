7c9a53175008b9a067143032f0b0b306f9a70a2e
Manuel Smeria <manuel@xwiki.com>
2014-08-27 12:16:41 +0300

    XWIKI-10810: Use JAVA_HOME if the user has set it instead of searching in the registry

--- xwiki-platform-tools/xwiki-platform-tool-jetty/xwiki-platform-tool-jetty-resources/src/main/resources/start_xwiki_debug.bat
+++ xwiki-platform-tools/xwiki-platform-tool-jetty/xwiki-platform-tool-jetty-resources/src/main/resources/start_xwiki_debug.bat
@@ -66,11 +66,11 @@ if "%2"=="profiler" (
 REM Get javaw.exe from the latest properly installed JRE
 if not "%JAVA_HOME%" == "" (
   set JAVA_PATH=%JAVA_HOME\bin:java.exe
-  ) else (
-    for /f tokens^=2^ delims^=^" %%i in ('reg query HKEY_CLASSES_ROOT\jarfile\shell\open\command /ve') do set JAVAW_PATH=%%i
-    set JAVA_PATH=%JAVAW_PATH:\javaw.exe=%\java.exe
-    if "%JAVA_PATH%"=="" set JAVA_PATH=java
-    )
+) else (
+  for /f tokens^=2^ delims^=^" %%i in ('reg query HKEY_CLASSES_ROOT\jarfile\shell\open\command /ve') do set JAVAW_PATH=%%i
+  set JAVA_PATH=%JAVAW_PATH:\javaw.exe=%\java.exe
+  if "%JAVA_PATH%"=="" set JAVA_PATH=java
+)
 
 REM Location where XWiki stores generated data and where database files are.
 set XWIKI_DATA_DIR=${xwikiDataDir}

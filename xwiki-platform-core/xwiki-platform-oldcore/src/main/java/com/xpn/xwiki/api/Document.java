45797c8331538c689dcdfd5bc598170ea7de5756
Eduard Moraru <Enygma2002@gmail.com>
2014-10-20 18:40:55 +0300

    [misc] Applied code style.
    (cherry picked from commit 5c73e675ad10c6f38c72cb388b1e84941cc6b3ea)

--- xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/api/Document.java
+++ xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/api/Document.java
@@ -1964,10 +1964,9 @@ public class Document extends Api
             try {
                 BaseProperty bp = (BaseProperty) object.getBaseObject().safeget(fieldName);
                 PropertyClass p = (PropertyClass) object.getBaseObject().getXClass(getXWikiContext()).get(fieldName);
-                if ("Password".equals(p.getClassType()))
-                {
-                    if(!this.getXWikiContext().getWiki().getRightService().hasProgrammingRights(this.getXWikiContext()))
-                    {
+                if ("Password".equals(p.getClassType())) {
+                    if (!this.getXWikiContext().getWiki().getRightService()
+                        .hasProgrammingRights(this.getXWikiContext())) {
                         return null;
                     }
                 }

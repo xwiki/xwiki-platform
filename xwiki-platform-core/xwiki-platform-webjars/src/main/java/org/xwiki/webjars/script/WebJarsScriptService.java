167ee83a83c5fe5b105c9ccbf96b8e420dcc7b0c
Marius Dumitru Florea <marius@xwiki.com>
2014-11-01 09:38:50 +0200

    XWIKI-11371: Detect the webjar version instead of hard-coding it

--- xwiki-platform-core/xwiki-platform-webjars/src/main/java/org/xwiki/webjars/script/WebJarsScriptService.java
+++ xwiki-platform-core/xwiki-platform-webjars/src/main/java/org/xwiki/webjars/script/WebJarsScriptService.java
@@ -19,13 +19,24 @@
  */
 package org.xwiki.webjars.script;
 
+import java.net.URL;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.inject.Provider;
 import javax.inject.Singleton;
 
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
 import org.xwiki.component.annotation.Component;
-import org.xwiki.context.Execution;
+import org.xwiki.extension.Extension;
+import org.xwiki.extension.repository.CoreExtensionRepository;
+import org.xwiki.extension.repository.InstalledExtensionRepository;
 import org.xwiki.script.service.ScriptService;
+import org.xwiki.velocity.tools.EscapeTool;
 
 import com.xpn.xwiki.XWikiContext;
 import com.xpn.xwiki.web.XWikiURLFactory;
@@ -42,35 +53,129 @@ import com.xpn.xwiki.web.XWikiURLFactory;
 public class WebJarsScriptService implements ScriptService
 {
     /**
+     * The name of the parameter that specifies the WebJar version.
+     */
+    private static final String VERSION = "version";
+
+    @Inject
+    private Logger logger;
+
+    /**
      * @todo finish implementing URL serialization in the URL module to avoid this dependency on oldcore.
      */
     @Inject
-    private Execution execution;
+    private Provider<XWikiContext> xcontextProvider;
 
     /**
-     * Compute an XWiki WebJAR URL
-     * (of the form {@code http://server/bin/webjars/resource/path?value=(resource name)}.
+     * Used to check if the WebJar is a core extension and to get its version.
+     */
+    @Inject
+    private CoreExtensionRepository coreExtensionRepository;
+
+    /**
+     * Used to check if the WebJar is an installed extension and to get its version.
+     */
+    @Inject
+    private InstalledExtensionRepository installedExtensionRepository;
+
+    /**
+     * Used to serialize a {@link Map} as a URL query string.
+     */
+    private EscapeTool escapeTool = new EscapeTool();
+
+    /**
+     * Compute an XWiki WebJar URL of the form {@code http://server/bin/webjars/resource/path?value=(resource name)}.
      *
-     * @param resourceName the resource asked (eg {@code angular/2.1.11/angular.js"})
+     * @param resourceName the resource asked (e.g. {@code angular/2.1.11/angular.js"})
      * @return the computed URL
      */
     public String url(String resourceName)
     {
-        XWikiContext xcontext = getXWikiContext();
-        XWikiURLFactory urlFactory = xcontext.getURLFactory();
-        return urlFactory.getURL(
-            urlFactory.createURL("resources", "path", "webjars", String.format("value=%s", resourceName), null,
-                xcontext),
-            xcontext);
+        if (StringUtils.isEmpty(resourceName)) {
+            return null;
+        }
+
+        String[] parts = resourceName.split("/", 3);
+        if (parts.length < 3) {
+            logger.warn("Invalid webjar resource name [{}]. Expected format is 'webjarId/version/path'", resourceName);
+            return null;
+        }
+
+        return url(parts[0], parts[2], Collections.singletonMap(VERSION, parts[1]));
     }
 
-    private XWikiContext getXWikiContext()
+    /**
+     * Creates an URL that can be used to load a resource (JavaScript, CSS, etc.) from a WebJar.
+     *
+     * @param webjarId the id of the WebJar that contains the resource; the format of the WebJar id is
+     *            "groupId:artifactId" (e.g. org.xwiki.platform:xwiki-platform-job-webjar), where the groupId can be
+     *            omitted if it is 'org.webjars' (i.e. "angular" translates to "org.webjars:angular")
+     * @param path the path within the WebJar, starting from the version folder (e.g. you should pass just "angular.js"
+     *            if the actual path is "META-INF/resources/webjars/angular/2.1.11/angular.js")
+     * @return the URL to load the WebJar resource (relative to the context path of the web application)
+     */
+    public String url(String webjarId, String path)
     {
-        XWikiContext xwikiContext =
-            (XWikiContext) this.execution.getContext().getProperty(XWikiContext.EXECUTIONCONTEXT_KEY);
-        if (xwikiContext == null) {
-            throw new RuntimeException("The XWiki Context is not available in the Execution Context");
+        return url(webjarId, path, Collections.<String, Object>emptyMap());
+    }
+
+    /**
+     * Creates an URL that can be used to load a resource (JavaScript, CSS, etc.) from a WebJar.
+     *
+     * @param webjarId the id of the WebJar that contains the resource; the format of the WebJar id is
+     *            "groupId:artifactId" (e.g. org.xwiki.platform:xwiki-platform-job-webjar), where the groupId can be
+     *            omitted if it is 'org.webjars' (i.e. "angular" translates to "org.webjars:angular")
+     * @param path the path within the WebJar, starting from the version folder (e.g. you should pass just "angular.js"
+     *            if the actual path is "META-INF/resources/webjars/angular/2.1.11/angular.js")
+     * @param params additional query string parameters to add to the returned URL
+     * @return the URL to load the WebJar resource (relative to the context path of the web application)
+     */
+    public String url(String webjarId, String path, Map<String, ?> params)
+    {
+        if (StringUtils.isEmpty(webjarId)) {
+            return null;
         }
-        return xwikiContext;
+
+        String groupId = "org.webjars";
+        String artifactId = webjarId;
+        int groupSeparatorPosition = webjarId.indexOf(':');
+        if (groupSeparatorPosition >= 0) {
+            // A different group id.
+            groupId = webjarId.substring(0, groupSeparatorPosition);
+            artifactId = webjarId.substring(groupSeparatorPosition + 1);
+        }
+
+        Map<String, Object> urlParams = new LinkedHashMap<String, Object>();
+        if (params != null) {
+            urlParams.putAll(params);
+        }
+
+        Object version = urlParams.remove(VERSION);
+        if (version == null) {
+            version = getVersion(String.format("%s:%s", groupId, artifactId));
+        }
+
+        String fullPath = String.format("%s/%s/%s", artifactId, version, path);
+        urlParams.put("value", fullPath);
+
+        XWikiContext xcontext = xcontextProvider.get();
+        XWikiURLFactory urlFactory = xcontext.getURLFactory();
+        URL url = urlFactory.createURL("resources", "path", "webjars", escapeTool.url(urlParams), null, xcontext);
+        return urlFactory.getURL(url, xcontext);
+    }
+
+    private String getVersion(String extensionId)
+    {
+        // Look for WebJars that are core extensions.
+        Extension extension = coreExtensionRepository.getCoreExtension(extensionId);
+        if (extension == null) {
+            // Look for WebJars that are installed on the current wiki or the root namespace.
+            String namespace = "wiki:" + xcontextProvider.get().getWikiId();
+            extension = installedExtensionRepository.getInstalledExtension(extensionId, namespace);
+            if (extension == null) {
+                return null;
+            }
+        }
+        return extension.getId().getVersion().getValue();
     }
 }

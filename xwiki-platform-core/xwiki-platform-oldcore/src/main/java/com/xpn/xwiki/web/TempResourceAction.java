39694e8f6e0e4393a853df2862a31f03a72afcc7
Marius Dumitru Florea <marius@xwiki.com>
2014-12-08 17:59:00 +0200

    XWIKI-11528: Office viewer fails if the page where the file is attached has a special character in the name (whitespace) * There's no need to double encode the file path because the 'temp' action uses it as is to access the temporary file. Moreover the double encoding seems to cause problems when mode_proxy is used (with 'nocanon' option disabled).
    (cherry picked from commit 35a33b1463bc0eccf02ed54ff9d1ca73ac123f7c)

--- xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/web/TempResourceAction.java
+++ xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/web/TempResourceAction.java
@@ -140,6 +140,9 @@ public class TempResourceAction extends XWikiAction
             String space = withMinimalURLEncoding(matcher.group(1));
             String page = withMinimalURLEncoding(matcher.group(2));
             String module = withMinimalURLEncoding(matcher.group(3));
+            // The file path is used as is, without any modifications (no decoding/encoding is performed). The modules
+            // that create the temporary files and the corresponding URLs used to access them are responsible for
+            // encoding the file path components so that they don't contain invalid characters.
             String filePath = matcher.group(4);
             String prefix = String.format("temp/%s/%s/%s/%s/", module, wiki, space, page);
             String path = URI.create(prefix + filePath).normalize().toString();

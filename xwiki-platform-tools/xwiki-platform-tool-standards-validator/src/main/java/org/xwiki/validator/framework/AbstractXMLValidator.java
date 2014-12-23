298abd134bb711c4bebb23b98bdb21cabe5b6168
Guillaume Delhumeau <guillaume.delhumeau@xwiki.com>
2014-12-23 18:02:33 +0100

    XWIKI-11595: DutchWebGuidlinesValidator fails to parse HTML5 code.

--- xwiki-platform-tools/xwiki-platform-tool-standards-validator/src/main/java/org/xwiki/validator/framework/AbstractXMLValidator.java
+++ xwiki-platform-tools/xwiki-platform-tool-standards-validator/src/main/java/org/xwiki/validator/framework/AbstractXMLValidator.java
@@ -79,7 +79,7 @@ public abstract class AbstractXMLValidator implements Validator
         this.errorHandler = createXMLErrorHandler();
 
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        factory.setValidating(true);
+        factory.setValidating(validateXML);
 
         try {
             this.documentBuilder = factory.newDocumentBuilder();

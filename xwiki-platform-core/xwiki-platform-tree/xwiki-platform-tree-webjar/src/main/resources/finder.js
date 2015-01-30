63959650d0c2c2127f77f846e105d6873d58b967
Marius Dumitru Florea <marius@xwiki.com>
2015-01-30 13:55:34 +0200

    XWIKI-11736: Use the new tree on the WYSIWYG Editor's link creation wizards * Add support for displaying 'New document...' and 'Upload file...' meta nodes in the document tree (needed by the WYSIWYG editor). * Look only for default translations when suggesting documents (for the tree finder). * Take suggestion data (e.g. resultHint, resultInfo) from node.data (rather than node directly) * Add support to the suggest.js for specifying the type of each suggestion (used as a CSS class). * Display 'No results!' when the tree finder has no suggestions. * Scroll into view the node that corresponds to the selected suggestion. * Remove SmartClient files and xwikiexplorer.js and update all the places where they were used. * Enhance entityReference.js with a method to get an EntityType contant (enum value) by it's name. XWIKI-5484: Problem to load the wiki pages tree/list (Link > Wiki page > "All the pages" tab) where there is a lot of pages XWIKI-3796: No horizontal scrollbar for long page names in the treeview XWIKI-11550: Document Tree macro doesn't properly work when using the "openTo" parameter and setting Hidden Docs to True * Return the full node information when retrieving the path (rather than just the node id). * When performing pagination, add the new nodes only if they don't exist already. * Enhance openTo() to load the missing parent nodes (e.g. when they are 'hidden' by the pagination). * Call the openTo() callback even if the path cannot be fully opened (some nodes don't exist).
    (cherry picked from commit 08c41339ea214f8e6651c589c37f71f30f8fe767)

--- xwiki-platform-core/xwiki-platform-tree/xwiki-platform-tree-webjar/src/main/resources/finder.js
+++ xwiki-platform-core/xwiki-platform-tree/xwiki-platform-tree-webjar/src/main/resources/finder.js
@@ -2,14 +2,15 @@ define(['jquery', 'jsTree', 'xwiki-events-bridge'], function($) {
   'use strict';
 
   // Default configuration options.
-	$.jstree.defaults.finder = {
+  $.jstree.defaults.finder = {
     url: '',
     placeholder: 'find ...',
     suggestion: {
-      hint: 'hint',
-      info: 'info'
+      hint: 'data.hint',
+      info: 'data.info',
+      type: 'data.type'
     }
-	};
+  };
 
   var createSuggestInput = function(options) {
     var input = document.createElement('input');
@@ -28,9 +29,9 @@ define(['jquery', 'jsTree', 'xwiki-events-bridge'], function($) {
       minchars: 3,
       resultHint: options.finder.suggestion.hint,
       resultInfo: options.finder.suggestion.info,
+      resultType: options.finder.suggestion.type,
       resultValue: 'text',
       script: options.finder.url,
-      shownoresults: false,
       timeout: 0,
       varname: 'query'
     });
@@ -41,7 +42,13 @@ define(['jquery', 'jsTree', 'xwiki-events-bridge'], function($) {
   var findNode = function(event, data) {
     this.deselect_all();
     this.close_all();
-    this.openTo(data.id);
+    this.openTo(data.id, $.proxy(function(node) {
+      if (this.select_node(node) !== false) {
+        // Scroll only the node label into view because the entire node may take a lot of vertical space due to its
+        // descendants (when the node is expanded).
+        this.get_node(node, true).children('.jstree-anchor')[0].scrollIntoView(false);
+      }
+    }, this));
   };
 
   // Move the icon inside the value container because it's easier to style it like that.
@@ -53,12 +60,11 @@ define(['jquery', 'jsTree', 'xwiki-events-bridge'], function($) {
     }
   });
 
-	$.jstree.plugins.finder = function (options, parent) {
-		this.init = function (element, options) {
-			parent.init.call(this, element, options);
+  $.jstree.plugins.finder = function (options, parent) {
+    this.init = function (element, options) {
+      parent.init.call(this, element, options);
 
       $(createSuggestInput(options)).insertBefore(element).on('xwiki:suggest:selected', $.proxy(findNode, this));
-		};
-	};
+    };
+  };
 });
-

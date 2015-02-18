978203da51463ed559517998e6af796ec80df40b
tmortagne <thomas.mortagne@gmail.com>
2015-02-18 14:02:46 +0100

    XWIKI-11805: Wiki Macros current macro block don't have a parent anymore
    Signed-off-by: tmortagne <thomas.mortagne@gmail.com>

--- xwiki-platform-core/xwiki-platform-rendering/xwiki-platform-rendering-wikimacro/xwiki-platform-rendering-wikimacro-api/src/main/java/org/xwiki/rendering/internal/macro/wikibridge/DefaultWikiMacro.java
+++ xwiki-platform-core/xwiki-platform-rendering/xwiki-platform-rendering-wikimacro/xwiki-platform-rendering-wikimacro-api/src/main/java/org/xwiki/rendering/internal/macro/wikibridge/DefaultWikiMacro.java
@@ -223,12 +223,17 @@ public class DefaultWikiMacro implements WikiMacro, NestedScriptMacroEnabled
                 new MacroMarkerBlock(wikiMacroBlock.getId(), wikiMacroBlock.getParameters(),
                     wikiMacroBlock.getContent(), xdom.getChildren(), wikiMacroBlock.isInline());
 
-            // make sure to use provided metadatas
+            // Make sure to use provided metadatas
             MetaDataBlock metaDataBlock =
                 new MetaDataBlock(Collections.<Block> singletonList(wikiMacroMarker), xdom.getMetaData());
 
             // Make sure the context XDOM contains the wiki macro content
             wikiMacroBlock.getParent().replaceChild(metaDataBlock, wikiMacroBlock);
+
+            // "Emulate" the fact that wiki macro block is still part of the XDOM
+            wikiMacroBlock.setParent(metaDataBlock.getParent());
+            wikiMacroBlock.setNextSiblingBlock(metaDataBlock.getNextSibling());
+            wikiMacroBlock.setPreviousSiblingBlock(metaDataBlock.getPreviousSibling());
 
             try {
                 if (observation != null) {

524abca8f5d5426efbba28192b9da0d4eb913331
Eduard Moraru <Enygma2002@gmail.com>
2015-02-22 02:42:39 +0200

    XWIKI-11801: ResetPassword feature is broken by password salting - Added functional tests for the ResetPassword feature. Currently failing due to XWIKI-11801

---
+++ xwiki-platform-core/xwiki-platform-administration/xwiki-platform-administration-test/xwiki-platform-administration-test-pageobjects/src/main/java/org/xwiki/administration/test/po/ResetPasswordPage.java
@@ -0,0 +1,85 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.administration.test.po;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.xwiki.test.ui.po.ViewPage;
+
+/**
+ * Represents the actions possible on the XWiki.ResetPassword page.
+ * 
+ * @version $Id$
+ * @since 7.0M2
+ */
+public class ResetPasswordPage extends ViewPage
+{
+    @FindBy(id = "u")
+    private WebElement userNameInput;
+
+    @FindBy(css = ".xcontent form input[type='submit']")
+    private WebElement resetPasswordButton;
+
+    @FindBy(css = ".xcontent .box")
+    private WebElement messageBox;
+
+    @FindBy(xpath = "//.[@class='xcontent']//a[contains(text(), 'Retry')]")
+    private WebElement retryUserNameButton;
+
+    public static ResetPasswordPage gotoPage()
+    {
+        getUtil().gotoPage("XWiki", "ResetPassword");
+        return new ResetPasswordPage();
+    }
+
+    public String getUserName()
+    {
+        return userNameInput.getText();
+    }
+
+    public void setUserName(String userName)
+    {
+        this.userNameInput.sendKeys(userName);
+    }
+
+    public ResetPasswordPage clickResetPassword()
+    {
+        resetPasswordButton.click();
+        return new ResetPasswordPage();
+    }
+
+    public boolean isResetPasswordSent()
+    {
+        return getDriver().findElements(By.cssSelector("xcontent form")).isEmpty()
+            && messageBox.getAttribute("class").contains("infomessage");
+    }
+
+    public String getMessage()
+    {
+        return messageBox.getText();
+    }
+
+    public ResetPasswordPage clickRetry()
+    {
+        retryUserNameButton.click();
+        return new ResetPasswordPage();
+    }
+}

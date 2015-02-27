e6bbe2375db58870aa88070963a679bf6600e463
Marius Dumitru Florea <marius@xwiki.com>
2015-02-27 07:39:36 +0200

    XWIKI-11845: ImportMojo should mark the direct XAR dependencies as explicitly installed * Use the diamond operator for Java 7.

--- xwiki-platform-tools/xwiki-platform-tool-packager-plugin/src/main/java/com/xpn/xwiki/tool/backup/ImportMojo.java
+++ xwiki-platform-tools/xwiki-platform-tool-packager-plugin/src/main/java/com/xpn/xwiki/tool/backup/ImportMojo.java
@@ -173,11 +173,11 @@ public class ImportMojo extends AbstractMojo
         // We have to create our own Set because Maven changes the fields from the dependency Artifacts (e.g. resolves
         // their version) after they are added to the Set of dependencies and this causes the hash code to change. As a
         // result the #contains(Artifact) method doesn't work as expected because it uses the new hash code.
-        Set<Artifact> directDependencies = new HashSet<Artifact>(this.project.getDependencyArtifacts());
+        Set<Artifact> directDependencies = new HashSet<>(this.project.getDependencyArtifacts());
 
         // Reverse artifact order to have dependencies first (despite the fact that it's a Set it's actually an ordered
         // LinkedHashSet behind the scene)
-        List<Artifact> dependenciesFirstArtifacts = new ArrayList<Artifact>(this.project.getArtifacts());
+        List<Artifact> dependenciesFirstArtifacts = new ArrayList<>(this.project.getArtifacts());
         Collections.reverse(dependenciesFirstArtifacts);
 
         for (Artifact artifact : dependenciesFirstArtifacts) {

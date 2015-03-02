3d21f1db2f83eba54d95017b37e5478f0ba1ad20
Marius Dumitru Florea <marius@xwiki.com>
2015-03-02 09:39:24 +0200

    Fix failing functional test (tree view in document index).

--- xwiki-platform-core/xwiki-platform-tree/xwiki-platform-tree-test-pageobjects/src/main/java/org/xwiki/tree/test/po/TreeElement.java
+++ xwiki-platform-core/xwiki-platform-tree/xwiki-platform-tree-test-pageobjects/src/main/java/org/xwiki/tree/test/po/TreeElement.java
@@ -64,7 +64,12 @@ public class TreeElement extends BaseElement
      */
     public boolean hasNode(String nodeId)
     {
-        return getDriver().findElementsWithoutWaiting(this.element, By.id(nodeId)).size() > 0;
+        // We cannot use By.id(nodeId) because findElements returns 0 elements if the id contains special characters
+        // such as backslash (which is used to escape special characters in an entity reference which the node id can
+        // be). Such an element id is technically invalid but the browsers are handling it fine.
+        // See https://code.google.com/p/selenium/issues/detail?id=8173
+        return getDriver().findElementsWithoutWaiting(this.element,
+            By.xpath(".//*[@id = '" + nodeId + "']")).size() > 0;
     }
 
     /**

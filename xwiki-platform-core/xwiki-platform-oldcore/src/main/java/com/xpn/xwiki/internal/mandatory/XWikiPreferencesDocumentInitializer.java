c1283d61f99d2601cdcc5b2e8224859386a8b424
tmortagne <thomas.mortagne@gmail.com>
2015-03-03 17:19:03 +0100

    XWIKI-11873: Missing timezone property in XWikiPreference initializer

--- xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/internal/mandatory/XWikiPreferencesDocumentInitializer.java
+++ xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/internal/mandatory/XWikiPreferencesDocumentInitializer.java
@@ -21,6 +21,7 @@ package com.xpn.xwiki.internal.mandatory;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.inject.Provider;
 import javax.inject.Singleton;
 
 import org.xwiki.component.annotation.Component;
@@ -28,10 +29,12 @@ import org.xwiki.model.reference.DocumentReference;
 import org.xwiki.sheet.SheetBinder;
 
 import com.xpn.xwiki.XWiki;
+import com.xpn.xwiki.XWikiContext;
 import com.xpn.xwiki.doc.XWikiDocument;
 import com.xpn.xwiki.objects.PropertyInterface;
 import com.xpn.xwiki.objects.classes.BaseClass;
 import com.xpn.xwiki.objects.classes.BooleanClass;
+import com.xpn.xwiki.objects.classes.PropertyClass;
 
 /**
  * Update XWiki.XWikiPreferences document with all required informations.
@@ -50,6 +53,9 @@ public class XWikiPreferencesDocumentInitializer extends AbstractMandatoryDocume
     @Inject
     @Named("class")
     protected SheetBinder classSheetBinder;
+
+    @Inject
+    protected Provider<XWikiContext> xcontextProvider;
 
     /**
      * Default constructor.
@@ -70,6 +76,8 @@ public class XWikiPreferencesDocumentInitializer extends AbstractMandatoryDocume
             needsUpdate = true;
             bclass.setCustomMapping("internal");
         }
+
+        XWikiContext xcontext = this.xcontextProvider.get();
 
         needsUpdate |= bclass.addTextField("parent", "Parent Space", 30);
         needsUpdate |= bclass.addBooleanField("multilingual", "Multi-Lingual", "yesno");
@@ -158,6 +166,33 @@ public class XWikiPreferencesDocumentInitializer extends AbstractMandatoryDocume
             "---|Small|Medium|Large");
         needsUpdate |= bclass.addTextField("languages", "Supported languages", 30);
         needsUpdate |= bclass.addTextField("documentBundles", "Internationalization Document Bundles", 60);
+        needsUpdate |= bclass.addTextField("timezone", "Timezone", 30);
+        PropertyClass timezoneProperty = (PropertyClass) bclass.get("timezone");
+        if (!timezoneProperty.isCustomDisplayed(xcontext)) {
+            StringBuilder builder = new StringBuilder();
+            builder.append("{{velocity}}\n");
+            builder.append("#if ($xcontext.action == 'inline' || $xcontext.action == 'edit')\n");
+            builder.append("  {{html}}\n");
+            builder.append("    #if($xwiki.jodatime)\n");
+            builder.append("      <select id='$prefix$name' name='$prefix$name'>\n");
+            builder.append("        <option value=\"\" #if($value == $tz)selected=\"selected\"#end>"
+                + "$services.localization.render('XWiki.XWikiPreferences_timezone_default')</option>\n");
+            builder.append("        #foreach($tz in $xwiki.jodatime.getServerTimezone().getAvailableIDs())\n");
+            builder.append("          <option value=\"$tz\" #if($value == $tz)selected=\"selected\"#end>"
+                + "$tz</option>\n");
+            builder.append("        #end\n");
+            builder.append("      </select>\n");
+            builder.append("    #else\n");
+            builder.append("      <input id='$prefix$name' name='$prefix$name' type=\"text\" value=\"$!value\"/>\n");
+            builder.append("    #end\n");
+            builder.append("  {{/html}}\n");
+            builder.append("#else\n");
+            builder.append("  $!value\n");
+            builder.append("#end\n");
+            builder.append("{{/velocity}}\n");
+            timezoneProperty.setCustomDisplay(builder.toString());
+            needsUpdate = true;
+        }
 
         // Only used by LDAP authentication service
 

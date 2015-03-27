017fefe691da3f312e4de1b6ef0614f0405828a1
Marius Dumitru Florea <marius@xwiki.com>
2015-03-27 11:46:35 +0200

    XWIKI-11956: Source tab not working on IE8

--- xwiki-platform-core/xwiki-platform-web/src/main/webapp/resources/js/xwiki/eventsBridge.js
+++ xwiki-platform-core/xwiki-platform-web/src/main/webapp/resources/js/xwiki/eventsBridge.js
@@ -3,7 +3,8 @@ require(['jquery'], function($) {
   var triggerJQueryEvent = function(event) {
     if (event.eventName.substr(0, 6) === 'xwiki:') {
       var jQueryEvent = $.Event(event.eventName);
-      $(event.element()).trigger(jQueryEvent, event.memo);
+      // This function is executed in the context of the target element.
+      $(this).trigger(jQueryEvent, event.memo);
       if (jQueryEvent.isDefaultPrevented()) {
         event.stop();
       }
@@ -13,7 +14,14 @@ require(['jquery'], function($) {
 
   var bridge = function(oldPrototypeFire) {
     return function() {
-      return triggerJQueryEvent(oldPrototypeFire.apply(this, arguments));
+      // Prototype doesn't extend the event object if there are no registered event listeners. In IE8 the event object is
+      // missing the target element for custom events, if the event object is not extended. As a consequence calling
+      // Event#element() throws an exception. We need to know the target element in this case, and for this we execute
+      // triggerJQueryEvent() in the context of the target element.
+      //
+      // If this is an element or the document then use it as target, otherwise use the first method argument.
+      var target = (this.nodeType === 1 || this.nodeType === 9) ? this : arguments[0];
+      return triggerJQueryEvent.call(target, oldPrototypeFire.apply(this, arguments));
     };
   };
 

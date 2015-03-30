9e6c09ed7b93413b6043398a58953f80435f4383
Eduard Moraru <Enygma2002@gmail.com>
2015-03-30 17:17:19 +0300

    XWIKI-11979: Add $services.rendering.escape method for escaping XWiki syntax - Moved the code and tests to platform.

--- xwiki-platform-core/xwiki-platform-rendering/xwiki-platform-rendering-xwiki/src/test/java/org/xwiki/rendering/script/RenderingScriptServiceTest.java
+++ xwiki-platform-core/xwiki-platform-rendering/xwiki-platform-rendering-xwiki/src/test/java/org/xwiki/rendering/script/RenderingScriptServiceTest.java
@@ -53,26 +53,27 @@ import static org.mockito.Mockito.when;
 public class RenderingScriptServiceTest
 {
     @Rule
-    public MockitoComponentMockingRule<RenderingScriptService> mocker =
-        new MockitoComponentMockingRule<>(RenderingScriptService.class);
+    public MockitoComponentMockingRule<RenderingScriptService> mocker = new MockitoComponentMockingRule<>(
+        RenderingScriptService.class);
 
     @Test
     public void parseAndRender() throws Exception
     {
         Parser parser = this.mocker.registerMockComponent(Parser.class, "plain/1.0");
-        when(parser.parse(new StringReader("some [[TODO]] stuff"))).thenReturn(
-            new XDOM(Collections.<Block>emptyList()));
+        when(parser.parse(new StringReader("some [[TODO]] stuff")))
+            .thenReturn(new XDOM(Collections.<Block>emptyList()));
 
         BlockRenderer blockRenderer = this.mocker.registerMockComponent(BlockRenderer.class, "xwiki/2.0");
-        doAnswer(new Answer() {
-             @Override
-             public Object answer(InvocationOnMock invocationOnMock) throws Throwable
-             {
-                 WikiPrinter printer = (WikiPrinter) invocationOnMock.getArguments()[1];
-                 printer.print("some ~[~[TODO]] stuff");
-                 return null;
-                     }
-             }).when(blockRenderer).render(any(XDOM.class), any(WikiPrinter.class));
+        doAnswer(new Answer()
+        {
+            @Override
+            public Object answer(InvocationOnMock invocationOnMock) throws Throwable
+            {
+                WikiPrinter printer = (WikiPrinter) invocationOnMock.getArguments()[1];
+                printer.print("some ~[~[TODO]] stuff");
+                return null;
+            }
+        }).when(blockRenderer).render(any(XDOM.class), any(WikiPrinter.class));
 
         XDOM xdom = this.mocker.getComponentUnderTest().parse("some [[TODO]] stuff", "plain/1.0");
         Assert.assertEquals("some ~[~[TODO]] stuff", this.mocker.getComponentUnderTest().render(xdom, "xwiki/2.0"));
@@ -91,8 +92,8 @@ public class RenderingScriptServiceTest
     public void parseAndRenderWhenErrorInRender() throws Exception
     {
         Parser parser = this.mocker.registerMockComponent(Parser.class, "plain/1.0");
-        when(parser.parse(new StringReader("some [[TODO]] stuff"))).thenReturn(
-                new XDOM(Collections.<Block>emptyList()));
+        when(parser.parse(new StringReader("some [[TODO]] stuff")))
+            .thenReturn(new XDOM(Collections.<Block>emptyList()));
 
         XDOM xdom = this.mocker.getComponentUnderTest().parse("some [[TODO]] stuff", "plain/1.0");
         Assert.assertNull(this.mocker.getComponentUnderTest().render(xdom, "unknown"));
@@ -114,5 +115,116 @@ public class RenderingScriptServiceTest
         when(syntaxFactory.createSyntaxFromIdString("unknown")).thenThrow(new ParseException("invalid"));
 
         Assert.assertNull(this.mocker.getComponentUnderTest().resolveSyntax("unknown"));
+    }
+
+    @Test
+    public void escape10Syntax() throws Exception
+    {
+        // Since the logic is pretty simple (prepend every character with an escape character), the below tests are
+        // mostly for exemplification.
+        // Note: Java escaped string "\\" == "\" (real string).
+        Assert.assertEquals("\\\\", this.mocker.getComponentUnderTest().escape("\\", Syntax.XWIKI_1_0));
+        Assert.assertEquals("\\*\\t\\e\\s\\t\\*", this.mocker.getComponentUnderTest()
+            .escape("*test*", Syntax.XWIKI_1_0));
+        Assert.assertEquals("\\a\\\\\\\\\\[\\l\\i\\n\\k\\>\\X\\.\\Y\\]",
+            this.mocker.getComponentUnderTest().escape("a\\\\[link>X.Y]", Syntax.XWIKI_1_0));
+        Assert.assertEquals("\\{\\p\\r\\e\\}\\v\\e\\r\\b\\a\\t\\i\\m\\{\\/\\p\\r\\e\\}", this.mocker
+            .getComponentUnderTest().escape("{pre}verbatim{/pre}", Syntax.XWIKI_1_0));
+        Assert.assertEquals("\\{\\m\\a\\c\\r\\o\\:\\s\\o\\m\\e\\=\\p\\a\\r\\a\\m\\e\\t\\e\\r\\}"
+            + "\\c\\o\\n\\t\\e\\n\\t" + "\\{\\m\\a\\c\\r\\o\\}",
+            this.mocker.getComponentUnderTest().escape("{macro:some=parameter}content{macro}", Syntax.XWIKI_1_0));
+    }
+
+    @Test
+    public void escape20Syntax() throws Exception
+    {
+        // Since the logic is pretty simple (prepend every character with an escape character), the below tests are
+        // mostly for exemplification.
+        Assert.assertEquals("~~", this.mocker.getComponentUnderTest().escape("~", Syntax.XWIKI_2_0));
+        Assert.assertEquals("~*~*~t~e~s~t~*~*", this.mocker.getComponentUnderTest()
+            .escape("**test**", Syntax.XWIKI_2_0));
+        // Note: Java escaped string "\\" == "\" (real string).
+        Assert.assertEquals("~a~\\~\\~[~[~l~i~n~k~>~>~X~.~Y~]~]",
+            this.mocker.getComponentUnderTest().escape("a\\\\[[link>>X.Y]]", Syntax.XWIKI_2_0));
+        Assert.assertEquals("~{~{~{~v~e~r~b~a~t~i~m~}~}~}",
+            this.mocker.getComponentUnderTest().escape("{{{verbatim}}}", Syntax.XWIKI_2_0));
+        Assert
+            .assertEquals(
+                "~{~{~m~a~c~r~o~ ~s~o~m~e~=~'~p~a~r~a~m~e~t~e~r~'~}~}~c~o~n~t~e~n~t~{~{~/~m~a~c~r~o~}~}",
+                this.mocker.getComponentUnderTest().escape("{{macro some='parameter'}}content{{/macro}}",
+                    Syntax.XWIKI_2_0));
+    }
+
+    @Test
+    public void escape21Syntax() throws Exception
+    {
+        // Since the logic is pretty simple (prepend every character with an escape character), the below tests are
+        // mostly for exemplification.
+        Assert.assertEquals("~~", this.mocker.getComponentUnderTest().escape("~", Syntax.XWIKI_2_1));
+        Assert.assertEquals("~*~*~t~e~s~t~*~*", this.mocker.getComponentUnderTest()
+            .escape("**test**", Syntax.XWIKI_2_1));
+        // Note: Java escaped string "\\" == "\" (real string).
+        Assert.assertEquals("~a~\\~\\~[~[~l~i~n~k~>~>~X~.~Y~]~]",
+            this.mocker.getComponentUnderTest().escape("a\\\\[[link>>X.Y]]", Syntax.XWIKI_2_1));
+        Assert.assertEquals("~{~{~{~v~e~r~b~a~t~i~m~}~}~}",
+            this.mocker.getComponentUnderTest().escape("{{{verbatim}}}", Syntax.XWIKI_2_1));
+        Assert
+            .assertEquals(
+                "~{~{~m~a~c~r~o~ ~s~o~m~e~=~'~p~a~r~a~m~e~t~e~r~'~}~}~c~o~n~t~e~n~t~{~{~/~m~a~c~r~o~}~}",
+                this.mocker.getComponentUnderTest().escape("{{macro some='parameter'}}content{{/macro}}",
+                    Syntax.XWIKI_2_1));
+    }
+
+    @Test
+    public void escapeSpaces() throws Exception
+    {
+        Assert.assertEquals("\\a\\ \\*\\t\\e\\s\\t\\*",
+            this.mocker.getComponentUnderTest().escape("a *test*", Syntax.XWIKI_1_0));
+        Assert.assertEquals("~a~ ~*~*~t~e~s~t~*~*",
+            this.mocker.getComponentUnderTest().escape("a **test**", Syntax.XWIKI_2_0));
+        Assert.assertEquals("~a~ ~*~*~t~e~s~t~*~*",
+            this.mocker.getComponentUnderTest().escape("a **test**", Syntax.XWIKI_2_1));
+    }
+
+    @Test
+    public void escapeNewLines() throws Exception
+    {
+        Assert.assertEquals("\\a\\\n\\b", this.mocker.getComponentUnderTest().escape("a\nb", Syntax.XWIKI_1_0));
+        Assert.assertEquals("~a~\n~b", this.mocker.getComponentUnderTest().escape("a\nb", Syntax.XWIKI_2_0));
+        Assert.assertEquals("~a~\n~b", this.mocker.getComponentUnderTest().escape("a\nb", Syntax.XWIKI_2_1));
+    }
+
+    @Test
+    public void escapeWithNullInput() throws Exception
+    {
+        Assert.assertNull("Unexpected non-null output for null input",
+            this.mocker.getComponentUnderTest().escape(null, Syntax.XWIKI_2_1));
+    }
+
+    @Test
+    public void escapeWithEmptyInput() throws Exception
+    {
+        Assert.assertEquals("", this.mocker.getComponentUnderTest().escape("", Syntax.XWIKI_2_1));
+    }
+
+    @Test
+    public void escapeWithNullSyntax() throws Exception
+    {
+        Assert.assertNull("Unexpected non-null output for null syntax",
+            this.mocker.getComponentUnderTest().escape("anything", null));
+    }
+
+    @Test
+    public void escapeWithNullInputAndSyntax() throws Exception
+    {
+        Assert.assertNull("Unexpected non-null output for null input and syntax", this.mocker.getComponentUnderTest()
+            .escape(null, null));
+    }
+
+    @Test
+    public void escapeWithUnsupportedSyntax() throws Exception
+    {
+        Assert.assertNull("Unexpected non-null output for unsuported syntax", this.mocker.getComponentUnderTest()
+            .escape("unsuported", Syntax.XHTML_1_0));
     }
 }

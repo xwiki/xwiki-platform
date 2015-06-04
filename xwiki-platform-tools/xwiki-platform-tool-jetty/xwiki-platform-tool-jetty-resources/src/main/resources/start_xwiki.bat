dfbb125467912b9fba3f6929d0250a853d20f4af
Vincent Massol <vincent@massol.net>
2015-06-04 09:09:50 +0200

    XWIKI-12150: Windows installer fails to discover where java is located

--- xwiki-platform-tools/xwiki-platform-tool-jetty/xwiki-platform-tool-jetty-resources/src/main/resources/start_xwiki.bat
+++ xwiki-platform-tools/xwiki-platform-tool-jetty/xwiki-platform-tool-jetty-resources/src/main/resources/start_xwiki.bat
@@ -58,14 +58,12 @@ if not defined JETTY_STOP_PORT (
 
 echo Starting Jetty on port %JETTY_PORT%, please wait...
 
-REM Get javaw.exe from the latest properly installed JRE
-if not "%JAVA_HOME%" == "" (
-  set JAVA_PATH=%JAVA_HOME\bin:java.exe
-) else (
-  for /f tokens^=2^ delims^=^" %%i in ('reg query HKEY_CLASSES_ROOT\jarfile\shell\open\command /ve') do set JAVAW_PATH=%%i
-  set JAVA_PATH=%JAVAW_PATH:\javaw.exe=%\java.exe
-  if "%JAVA_PATH%"=="" set JAVA_PATH=java
-)
+REM Discover java.exe from the latest properly installed JRE
+for /f tokens^=2^ delims^=^" %%i in ('reg query HKEY_CLASSES_ROOT\jarfile\shell\open\command /ve') do set JAVAW_PATH=%%i
+set JAVA_PATH=%JAVAW_PATH:\javaw.exe=%\java.exe
+if "%JAVA_PATH%"=="" set JAVA_PATH=java
+REM Handle the case when JAVA_HOME is set by the user
+if not "%JAVA_HOME%" == "" set JAVA_PATH=%JAVA_HOME%\bin\java.exe
 
 REM Location where XWiki stores generated data and where database files are.
 set XWIKI_DATA_DIR=${xwikiDataDir}

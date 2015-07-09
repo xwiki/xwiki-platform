fb29c295bcf10a5c00da94dc5e9ed843dc76a8e0
Thomas Mortagne <thomas.mortagne@gmail.com>
2015-07-09 11:26:47 +0200

    [misc] Move to Mockito and various bulletproofing

--- xwiki-platform-core/xwiki-platform-cache/xwiki-platform-cache-oscache/src/main/java/org/xwiki/cache/oscache/internal/AbstractOSCacheCacheFactory.java
+++ xwiki-platform-core/xwiki-platform-cache/xwiki-platform-cache-oscache/src/main/java/org/xwiki/cache/oscache/internal/AbstractOSCacheCacheFactory.java
@@ -25,7 +25,11 @@ import org.slf4j.Logger;
 import org.xwiki.cache.CacheException;
 import org.xwiki.cache.CacheFactory;
 import org.xwiki.cache.config.CacheConfiguration;
-import org.xwiki.container.Container;
+import org.xwiki.component.manager.ComponentLookupException;
+import org.xwiki.component.manager.ComponentManager;
+import org.xwiki.component.phase.Initializable;
+import org.xwiki.component.phase.InitializationException;
+import org.xwiki.environment.Environment;
 
 /**
  * Base implementation for OSCache support.
@@ -33,14 +37,8 @@ import org.xwiki.container.Container;
  * @version $Id$
  * @since 1.9M2
  */
-public abstract class AbstractOSCacheCacheFactory implements CacheFactory
+public abstract class AbstractOSCacheCacheFactory implements CacheFactory, Initializable
 {
-    /**
-     * The container used to access configuration files.
-     */
-    @Inject
-    private Container container;
-
     /**
      * The logger to log.
      */
@@ -48,17 +46,42 @@ public abstract class AbstractOSCacheCacheFactory implements CacheFactory
     private Logger logger;
 
     /**
+     * Used to lookup the container.
+     */
+    @Inject
+    private ComponentManager componentManager;
+
+    /**
+     * Optional Environment used to access configuration files.
+     */
+    private Environment environment;
+
+    /**
      * @return the default configuration identifier used to load cache configuration file
      */
     protected abstract String getDefaultPropsId();
 
+    @Override
+    public void initialize() throws InitializationException
+    {
+        // Container
+        // Note that the reason we lazy load the container is because we want to be able to use the Cache in
+        // environments where there's no container.
+
+        try {
+            this.environment = this.componentManager.getInstance(Environment.class);
+        } catch (ComponentLookupException e) {
+            this.logger.debug("Can't find any Environment", e);
+        }   
+    }
+    
     @Override
     public <T> org.xwiki.cache.Cache<T> newCache(CacheConfiguration configuration) throws CacheException
     {
         this.logger.debug("Start OSCache initialisation");
 
         OSCacheCache<T> cache = new OSCacheCache<T>();
-        cache.initialize(new OSCacheCacheConfiguration(this.container, configuration, getDefaultPropsId()));
+        cache.initialize(new OSCacheCacheConfiguration(this.environment, configuration, getDefaultPropsId()));
 
         this.logger.debug("End OSCache initialisation");
 

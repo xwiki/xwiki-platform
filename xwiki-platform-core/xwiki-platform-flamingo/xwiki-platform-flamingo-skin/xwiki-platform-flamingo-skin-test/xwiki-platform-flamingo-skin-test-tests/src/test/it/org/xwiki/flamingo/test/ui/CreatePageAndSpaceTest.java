cc23888858a5cd565ad96c11fb91c8ab63fcadb8
Vincent Massol <vincent@massol.net>
2015-08-06 10:49:50 +0200

    XWIKI-12387: superadmin user should be able to create Terminal Pages
    + [Misc] Move internationalization test from xwiki-enterprise InternationalizationTest to CreatePageAndSpaceTest

--- xwiki-platform-core/xwiki-platform-flamingo/xwiki-platform-flamingo-skin/xwiki-platform-flamingo-skin-test/xwiki-platform-flamingo-skin-test-tests/src/test/it/org/xwiki/flamingo/test/ui/CreatePageAndSpaceTest.java
+++ xwiki-platform-core/xwiki-platform-flamingo/xwiki-platform-flamingo-skin/xwiki-platform-flamingo-skin-test/xwiki-platform-flamingo-skin-test-tests/src/test/it/org/xwiki/flamingo/test/ui/CreatePageAndSpaceTest.java
@@ -23,7 +23,9 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.xwiki.test.ui.AbstractTest;
 import org.xwiki.test.ui.SuperAdminAuthenticationRule;
+import org.xwiki.test.ui.po.CreatePagePage;
 import org.xwiki.test.ui.po.CreateSpacePage;
+import org.xwiki.test.ui.po.ViewPage;
 import org.xwiki.test.ui.po.editor.EditPage;
 
 import static org.junit.Assert.*;
@@ -40,30 +42,31 @@ public class CreatePageAndSpaceTest extends AbstractTest
     public SuperAdminAuthenticationRule authenticationRule = new SuperAdminAuthenticationRule(getUtil());
 
     @Test
-    public void createSpace()
+    public void createSpaceAndPage()
     {
         // Test 1:  Test Space creation when on an existing page (i.e. the space creation UI will open to ask for the
         //          space name.
 
         // Clean up before the test starts
-        String pageName = getTestMethodName();
-        String spaceName = getTestClassName();
+        // Note that we introduce a special character to verify we support non-ASCII characters in page and space names
+        String existingPageName = getTestMethodName() + "\u0219";
+        String spaceName = getTestClassName() + "\u0219";
         getUtil().deleteSpace(spaceName);
-        getUtil().deletePage(spaceName, pageName);
+        getUtil().deletePage(spaceName, existingPageName);
 
         // Create the page that's supposed to exist.
-        getUtil().createPage(spaceName, pageName, "Dummy", "Dummy Title");
+        getUtil().createPage(spaceName, existingPageName, "Dummy", "Dummy Title");
 
         // Since the Flamingo skin no longer supports creating a space from the UI, trigger the Space creation UI
         // by using directly the direct action URL for it.
-        getUtil().gotoPage(getUtil().getURL("create", new String[] {spaceName, pageName}, "tocreate=space"));
+        getUtil().gotoPage(getUtil().getURL("create", new String[] {spaceName, existingPageName}, "tocreate=space"));
         CreateSpacePage csp = new CreateSpacePage();
         EditPage editSpaceWebhomePage = csp.createSpace(spaceName);
 
         assertEquals(spaceName, editSpaceWebhomePage.getMetaDataValue("space"));
         assertEquals("WebHome", editSpaceWebhomePage.getMetaDataValue("page"));
         // The parent is the current document from where the space was created.
-        assertEquals(spaceName + "." + pageName, editSpaceWebhomePage.getParent());
+        assertEquals(spaceName + "." + existingPageName, editSpaceWebhomePage.getParent());
         // and the title the name of the space
         assertEquals(spaceName, editSpaceWebhomePage.getDocumentTitle());
 
@@ -72,7 +75,7 @@ public class CreatePageAndSpaceTest extends AbstractTest
 
         // Since the Flamingo skin no longer supports creating a space from the UI, trigger the Space creation UI
         // by using directly the direct action URL for it. This time on a non-exsiting page.
-        getUtil().gotoPage(getUtil().getURL("create", new String[] {spaceName, "NonExistingPage"}, "tocreate=space"));
+        getUtil().gotoPage(getUtil().getURL("create", new String[]{ spaceName, "NonExistingPage" }, "tocreate=space"));
         EditPage editPage = new EditPage();
 
         assertEquals(spaceName, editPage.getMetaDataValue("space"));
@@ -81,5 +84,25 @@ public class CreatePageAndSpaceTest extends AbstractTest
         assertEquals("Main.WebHome", editPage.getParent());
         // and the title the name of the space
         assertEquals("NonExistingPage", editPage.getDocumentTitle());
+
+        // Test 3:  Test Terminal Page creation when on an existing page (i.e. the space creation UI will open to ask
+        //          for the space + page names.
+
+        // Note that we introduce a special character to verify we support non-ASCII characters in page names
+        String newPageName = getTestMethodName() + "2" + "\u0219";
+        getUtil().deletePage(spaceName, newPageName);
+
+        // Navigate to an existing page before clicking on the Add button
+        ViewPage vp = getUtil().gotoPage(spaceName, existingPageName);
+        CreatePagePage cpp = vp.createPage();
+        editPage = cpp.createPage(spaceName, newPageName, true);
+
+        // Verify the title field
+        assertEquals(newPageName, editPage.getDocumentTitle());
+        // Verify the document name in the metadata
+        assertEquals(newPageName, editPage.getMetaDataValue("page"));
+        // Save the page to verify it can be saved with a non-ascii name
+        ViewPage savedPage = editPage.clickSaveAndView();
+        assertEquals(newPageName, savedPage.getMetaDataValue("page"));
     }
 }

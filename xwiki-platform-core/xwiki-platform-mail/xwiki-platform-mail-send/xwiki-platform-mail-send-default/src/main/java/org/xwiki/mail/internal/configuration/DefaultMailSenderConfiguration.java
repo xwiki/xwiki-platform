8146fee9fbd6bb1b607c2b52ddac2ed7d43ac778
Vincent Massol <vincent@massol.net>
2015-08-24 19:24:26 +0200

    [Misc] Fix javadoc typo

--- xwiki-platform-core/xwiki-platform-mail/xwiki-platform-mail-send/xwiki-platform-mail-send-default/src/main/java/org/xwiki/mail/internal/configuration/DefaultMailSenderConfiguration.java
+++ xwiki-platform-core/xwiki-platform-mail/xwiki-platform-mail-send/xwiki-platform-mail-send-default/src/main/java/org/xwiki/mail/internal/configuration/DefaultMailSenderConfiguration.java
@@ -39,7 +39,7 @@ import org.xwiki.mail.MailSenderConfiguration;
 /**
  * Gets the Mail Sending configuration. The configuration is checked in the following order:
  * <ul>
- *   <li>Look in Mail.SendMailConfigClass in the current wiki</li>
+ *   <li>Look in Mail.MailConfig in the current wiki</li>
  *   <li>Look in (current space).XWikiPreferences in the current wiki</li>
  *   <li>Look in XWiki.XWikiPreferences in the current wiki</li>
  *   <li>Look in the xwiki properties file</li>

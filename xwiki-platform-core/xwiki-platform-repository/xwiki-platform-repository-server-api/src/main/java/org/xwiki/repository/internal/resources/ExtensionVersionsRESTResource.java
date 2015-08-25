6db24ae25ca97e6965c428135dfd7571cff812ee
Thomas Mortagne <thomas.mortagne@gmail.com>
2015-08-25 18:00:50 +0200

    XWIKI-12443: Wrong versions order in XWiki Extension repository REST service
    (cherry picked from commit 59cc8db80bd0680b8cd265a0f221d6c81d3f6692)

--- xwiki-platform-core/xwiki-platform-repository/xwiki-platform-repository-server-api/src/main/java/org/xwiki/repository/internal/resources/ExtensionVersionsRESTResource.java
+++ xwiki-platform-core/xwiki-platform-repository/xwiki-platform-repository-server-api/src/main/java/org/xwiki/repository/internal/resources/ExtensionVersionsRESTResource.java
@@ -21,8 +21,12 @@
 package org.xwiki.repository.internal.resources;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import javax.inject.Singleton;
 import javax.ws.rs.DefaultValue;
@@ -36,6 +40,7 @@ import org.xwiki.component.annotation.Component;
 import org.xwiki.extension.repository.xwiki.model.jaxb.ExtensionVersionSummary;
 import org.xwiki.extension.repository.xwiki.model.jaxb.ExtensionVersions;
 import org.xwiki.extension.version.InvalidVersionRangeException;
+import org.xwiki.extension.version.Version;
 import org.xwiki.extension.version.VersionConstraint;
 import org.xwiki.extension.version.internal.DefaultVersion;
 import org.xwiki.extension.version.internal.DefaultVersionConstraint;
@@ -54,7 +59,7 @@ public class ExtensionVersionsRESTResource extends AbstractExtensionRESTResource
 {
     @GET
     public ExtensionVersions getExtensionVersions(@PathParam("extensionId") String extensionId,
-        @QueryParam(Resources.QPARAM_LIST_START) @DefaultValue("-1") int offset,
+        @QueryParam(Resources.QPARAM_LIST_START) @DefaultValue("0") int offset,
         @QueryParam(Resources.QPARAM_LIST_NUMBER) @DefaultValue("-1") int number,
         @QueryParam(Resources.QPARAM_VERSIONS_RANGES) String ranges) throws QueryException,
         InvalidVersionRangeException
@@ -82,6 +87,29 @@ public class ExtensionVersionsRESTResource extends AbstractExtensionRESTResource
                 }
             }
         }
+
+        // Order by version
+        final Map<String, Version> versionCache = new HashMap<String, Version>();
+        Collections.sort(extensions.getExtensionVersionSummaries(), new Comparator<ExtensionVersionSummary>()
+        {
+            @Override
+            public int compare(ExtensionVersionSummary o1, ExtensionVersionSummary o2)
+            {
+                return toVersion(o1.getVersion()).compareTo(toVersion(o2.getVersion()));
+            }
+
+            private Version toVersion(String versionString)
+            {
+                Version version = versionCache.get(versionString);
+
+                if (version == null) {
+                    version = new DefaultVersion(versionString);
+                    versionCache.put(versionString, version);
+                }
+
+                return version;
+            }
+        });
 
         extensions.setTotalHits(extensions.getExtensionVersionSummaries().size());
         extensions.setOffset(offset);

453d01379ff62777bd94de2147cb018db89d6462
Thomas Mortagne <thomas.mortagne@gmail.com>
2015-10-28 12:02:32 +0100

    XWIKI-12430: Enhance the breadcrumbs with tree navigation * try to fix test

--- xwiki-platform-core/xwiki-platform-index/xwiki-platform-index-test/xwiki-platform-index-test-pageobjects/src/main/java/org/xwiki/index/test/po/AllDocsPage.java
+++ xwiki-platform-core/xwiki-platform-index/xwiki-platform-index-test/xwiki-platform-index-test-pageobjects/src/main/java/org/xwiki/index/test/po/AllDocsPage.java
@@ -49,7 +49,7 @@ public class AllDocsPage extends ViewPage
     @FindBy(xpath = "//li[@id='xwikideletedAttachments']/a")
     private WebElement deletedAttachmentsTab;
 
-    @FindBy(className = "xtree")
+    @FindBy(xpath = "//div[@id='xwikitabpanescontainer']/div[contains(@class, 'xtree')]")
     private WebElement treeElement;
 
     public static AllDocsPage gotoPage()

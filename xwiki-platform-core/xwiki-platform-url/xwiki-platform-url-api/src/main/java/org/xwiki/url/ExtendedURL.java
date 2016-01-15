c44f6f7927060cc8f8db5f21b657373145a929c5
Vincent Massol <vincent@massol.net>
2016-01-15 10:53:58 +0100

    XWIKI-13007: Error in Filter RouterFilter when open Home page without / in linux

--- xwiki-platform-core/xwiki-platform-url/xwiki-platform-url-api/src/main/java/org/xwiki/url/ExtendedURL.java
+++ xwiki-platform-core/xwiki-platform-url/xwiki-platform-url-api/src/main/java/org/xwiki/url/ExtendedURL.java
@@ -123,9 +123,9 @@ public class ExtendedURL implements Cloneable
         }
         this.uri = internalURI;
 
+        // Extract the path after the ignore prefix
         String rawPath = getURI().getRawPath();
         if (!StringUtils.isEmpty(ignorePrefix)) {
-
             // Allow the passed ignore prefix to not contain the leading "/"
             String normalizedIgnorePrefix = ignorePrefix;
             if (!ignorePrefix.startsWith(URL_SEPARATOR)) {
@@ -136,8 +136,7 @@ public class ExtendedURL implements Cloneable
                 throw new CreateResourceReferenceException(
                     String.format("URL Path [%s] doesn't start with [%s]", getURI().getPath(), ignorePrefix));
             }
-            // Note: We also remove the leading "/" after the context path.
-            rawPath = rawPath.substring(ignorePrefix.length() + 1);
+            rawPath = rawPath.substring(normalizedIgnorePrefix.length());
         }
 
         // Remove leading "/" if any
@@ -226,6 +225,10 @@ public class ExtendedURL implements Cloneable
     private List<String> extractPathSegments(String rawPath)
     {
         List<String> urlSegments = new ArrayList<String>();
+
+        if (StringUtils.isEmpty(rawPath)) {
+            return urlSegments;
+        }
 
         // Note that we use -1 in the call below in order to get empty segments too. This is needed since in our URL
         // scheme a tailing "/" can have a meaning (for example "bin/view/Page" can represent a Page while

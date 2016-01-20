33a8ae77360c2344273be79168d87a9722227b22
Guillaume Delhumeau <guillaume.delhumeau@xwiki.com>
2016-01-20 19:07:56 +0100

    XWIKI-13016: Add a jQuery plugin to handle the XWiki Select Widget.

--- xwiki-platform-core/xwiki-platform-web/src/main/webapp/resources/uicomponents/widgets/select/select.js
+++ xwiki-platform-core/xwiki-platform-web/src/main/webapp/resources/uicomponents/widgets/select/select.js
@@ -18,15 +18,31 @@
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
 require(['jquery'], function($) {
-  
+
   /**
-   * Initializer called when the DOM is ready
+   * Represents an XWiki Select Widget.
+   * @since 7.4.1
    */
-  var init = function() {
-    // Register a callback when an option is clicked
-    $('.xwiki-select').find('.xwiki-select-option').click(function () {
+  var xwikiSelectWidget = function(htmlElement) {
+    // ----------------------
+    // Init fields
+    // ----------------------
+    var self = this;
+    self.selectWidget = $(htmlElement);
+    
+    /**
+     * Send an event to say that the selection have changed
+     */
+    self.triggerSelectionChange = function () {
+      self.selectWidget.trigger('xwiki:select:updated', {'elements': self.selectWidget[0]});
+    };
+
+    /**
+     * Callback used when an option from the select widget is clicked
+     */
+    self.onOptionClicked = function () {
       var option = $(this);
-      var input = option.find('input');  
+      var input = option.find('input');
       if (input.prop('checked')) {
         // The input is already selected, so we have nothing to do, and we do not trigger any event.
         // Note that if the user clicks on the <label> element of the widget, the "click" event is triggered twice:
@@ -34,15 +50,77 @@ require(['jquery'], function($) {
         return;
       }
       input.prop('checked', true);
-      var selectWidget = option.parents('.xwiki-select');
-      selectWidget.find('.xwiki-select-option-selected').removeClass('xwiki-select-option-selected');
+      self.selectWidget.find('.xwiki-select-option-selected').removeClass('xwiki-select-option-selected');
       option.addClass('xwiki-select-option-selected');
-      selectWidget.trigger('xwiki:select:updated', {'elements': selectWidget[0]});
-    });
+      self.triggerSelectionChange();
+    };
+    
+    /**
+    /* Initialization
+     */
+    self.init = function () {
+      self.selectWidget.find('.xwiki-select-option').click(self.onOptionClicked);
+    };
+    
+    /**
+     * Clear selection
+     */
+    self.clearSelection = function () {
+      self.selectWidget.find('.xwiki-select-option-selected').removeClass('xwiki-select-option-selected');
+      self.selectWidget.find('input[type="radio"]:checked').prop('checked', false);
+      self.triggerSelectionChange();
+    };
+
+    /**
+     * Return the current selected option
+     */
+    self.getValue = function () {
+      return self.selectWidget.find('input[type="radio"]:checked').val();
+    };
+    
+    self.init();
+  };
+  
+  
+  /**
+   * Define a jQuery plugin about the select widget.
+   * @since 7.4.1
+   */
+  $.fn.xwikiSelectWidget = function(action) {
+    //--------------------
+    // Handle actions
+    //--------------------
+    if (!action || action == 'init') {
+      // Handle each object separately
+      for (var i = 0; i < this.length; ++i) {
+        var htmlElement = this[i];
+        var selectWidget = new xwikiSelectWidget(htmlElement);
+        htmlElement.xwikiSelectWidget = selectWidget;
+      }
+    } else if (action == 'clearSelection') {
+      // Handle each object separately
+      for (var i = 0; i < this.length; ++i) {
+        this[i].xwikiSelectWidget.clearSelection();
+      }
+    } else if (action == 'getValue') {
+      // In such a case, there is no possible chaining
+      return this[0].xwikiSelectWidget.getValue();
+    }
+    
+    // Enable chaining
+    return this;
+  };
+  
+  /**
+   * Initializer called when the DOM is ready
+   */
+  var init = function() {
+    // Register a callback when an option is clicked
+    $('.xwiki-select').xwikiSelectWidget();
   };
 
   $(window).ready(init);
-
 });
+
 
 

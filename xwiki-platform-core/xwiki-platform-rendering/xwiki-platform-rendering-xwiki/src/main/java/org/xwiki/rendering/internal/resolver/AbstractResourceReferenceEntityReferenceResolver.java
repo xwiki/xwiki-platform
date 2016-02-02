41e9cb5ba9ee3b9f7a7860bb15bb1e3cd4908e9f
Thomas Mortagne <thomas.mortagne@gmail.com>
2016-02-02 18:02:58 +0100

    XWIKI-12920: Hiding WebHome in wiki link syntax * make resource reference to entity reference converter extensible

---
+++ xwiki-platform-core/xwiki-platform-rendering/xwiki-platform-rendering-xwiki/src/main/java/org/xwiki/rendering/internal/resolver/AbstractResourceReferenceEntityReferenceResolver.java
@@ -0,0 +1,128 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.rendering.internal.resolver;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.xwiki.model.EntityType;
+import org.xwiki.model.reference.EntityReference;
+import org.xwiki.model.reference.EntityReferenceResolver;
+import org.xwiki.rendering.listener.reference.ResourceReference;
+import org.xwiki.rendering.listener.reference.ResourceType;
+
+/**
+ * Base class helper for EntityReferenceResolver<ResourceReference> implementations.
+ * 
+ * @version $Id$
+ * @since 7.4.1
+ */
+public abstract class AbstractResourceReferenceEntityReferenceResolver
+    implements EntityReferenceResolver<ResourceReference>
+{
+    @Inject
+    @Named("current")
+    protected EntityReferenceResolver<String> currentEntityReferenceResolver;
+
+    @Inject
+    protected EntityReferenceResolver<EntityReference> defaultEntityReferenceResolver;
+
+    protected ResourceType type;
+
+    public AbstractResourceReferenceEntityReferenceResolver()
+    {
+    }
+
+    public AbstractResourceReferenceEntityReferenceResolver(ResourceType type)
+    {
+        this.type = type;
+    }
+
+    @Override
+    public EntityReference resolve(ResourceReference resourceReference, EntityType type, Object... parameters)
+    {
+        if (resourceReference == null) {
+            return null;
+        }
+
+        if (type != null && !resourceReference.getType().equals(type)) {
+            throw new IllegalArgumentException(String
+                .format("You must pass a resource reference of type [%s]. [%s] was passed", type, resourceReference));
+        }
+
+        EntityReference baseReference = getBaseReference(resourceReference, parameters);
+
+        EntityReference entityReference;
+
+        if (resourceReference.isTyped()) {
+            entityReference = resolveTyped(resourceReference, baseReference);
+        } else {
+            entityReference = resolveUntyped(resourceReference, baseReference);
+        }
+
+        return convertReference(entityReference, type);
+    }
+
+    protected EntityReference resolveUntyped(ResourceReference resourceReference, EntityReference baseReference)
+    {
+        return resolveTyped(resourceReference, baseReference);
+    }
+
+    protected abstract EntityReference resolveTyped(ResourceReference resourceReference, EntityReference baseReference);
+
+    protected EntityReference getBaseReference(ResourceReference resourceReference, Object... parameters)
+    {
+        EntityReference baseReference = (parameters.length > 0 && parameters[0] instanceof EntityReference)
+            ? (EntityReference) parameters[0] : null;
+
+        if (!resourceReference.getBaseReferences().isEmpty()) {
+            // If the passed reference has a base reference, resolve it first with a current resolver (it should
+            // normally be absolute but who knows what the API caller has specified...)
+            baseReference =
+                resolveBaseReference(resourceReference.getBaseReferences(), resourceReference.getType(), baseReference);
+        }
+
+        return baseReference;
+    }
+
+    protected EntityReference resolveBaseReference(List<String> baseReferences, ResourceType resourceType,
+        EntityReference defaultBaseReference)
+    {
+        EntityReference resolvedBaseReference = defaultBaseReference;
+        for (String baseReference : baseReferences) {
+            resolvedBaseReference =
+                this.currentEntityReferenceResolver.resolve(baseReference, EntityType.DOCUMENT, resolvedBaseReference);
+        }
+
+        return resolvedBaseReference;
+    }
+
+    protected EntityReference convertReference(EntityReference entityReference, EntityType type)
+    {
+        // Convert the entity reference if needed
+        if (entityReference != null && type != null && entityReference.getType() != type) {
+            return this.defaultEntityReferenceResolver.resolve(entityReference, type);
+        }
+
+        return entityReference;
+    }
+}
---
+++ xwiki-platform-core/xwiki-platform-rendering/xwiki-platform-rendering-xwiki/src/main/java/org/xwiki/rendering/internal/resolver/AttachmentResourceReferenceEntityReferenceResolver.java
@@ -0,0 +1,88 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.rendering.internal.resolver;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import org.xwiki.bridge.DocumentAccessBridge;
+import org.xwiki.component.annotation.Component;
+import org.xwiki.model.EntityType;
+import org.xwiki.model.reference.AttachmentReference;
+import org.xwiki.model.reference.AttachmentReferenceResolver;
+import org.xwiki.model.reference.DocumentReference;
+import org.xwiki.model.reference.EntityReference;
+import org.xwiki.model.reference.EntityReferenceProvider;
+import org.xwiki.rendering.listener.reference.ResourceReference;
+import org.xwiki.rendering.listener.reference.ResourceType;
+
+/**
+ * Convert attachment resource reference into entity reference.
+ * 
+ * @version $Id$
+ * @since 7.4.1
+ */
+@Component
+@Named("attach")
+@Singleton
+public class AttachmentResourceReferenceEntityReferenceResolver extends AbstractResourceReferenceEntityReferenceResolver
+{
+    @Inject
+    @Named("current")
+    private AttachmentReferenceResolver<String> currentAttachmentReferenceResolver;
+
+    @Inject
+    private EntityReferenceProvider defaultEntityReferenceProvider;
+
+    @Inject
+    @Named("currentspace")
+    private AttachmentReferenceResolver<String> currentSpaceAttachmentReferenceResolver;
+
+    @Inject
+    private DocumentAccessBridge documentAccessBridge;
+
+    public AttachmentResourceReferenceEntityReferenceResolver()
+    {
+        super(ResourceType.ATTACHMENT);
+    }
+
+    @Override
+    protected EntityReference resolveTyped(ResourceReference resourceReference, EntityReference baseReference)
+    {
+        AttachmentReference attachmentReference =
+            this.currentAttachmentReferenceResolver.resolve(resourceReference.getReference(), baseReference);
+
+        // See if the resolved (terminal or WebHome) document exists and, if so, use it.
+        DocumentReference documentReference = attachmentReference.getDocumentReference();
+        if (!this.documentAccessBridge.exists(documentReference)) {
+            // Also consider explicit "WebHome" references (i.e. the ones ending in "WebHome").
+            String defaultDocumentName =
+                this.defaultEntityReferenceProvider.getDefaultReference(EntityType.DOCUMENT).getName();
+            if (!documentReference.getName().equals(defaultDocumentName)) {
+                // Otherwise, handle it as a space reference for both cases when it exists or when it doesn't exist.
+                attachmentReference =
+                    this.currentSpaceAttachmentReferenceResolver.resolve(resourceReference.getReference());
+            }
+        }
+
+        return attachmentReference;
+    }
+}

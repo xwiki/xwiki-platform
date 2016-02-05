8a93ec7cabfa88907dfb4c6ce91fa84a832eaf04
Thomas Mortagne <thomas.mortagne@gmail.com>
2016-02-05 11:07:38 +0100

    XWIKI-12920: Hiding WebHome in wiki link syntax * fix tests
    (cherry picked from commit 7221e23b7408fa65fe1f9ea5fab98aa26c2cf972)

--- xwiki-platform-core/xwiki-platform-rendering/xwiki-platform-rendering-xwiki/src/main/java/org/xwiki/rendering/internal/resolver/AbstractResourceReferenceEntityReferenceResolver.java
+++ xwiki-platform-core/xwiki-platform-rendering/xwiki-platform-rendering-xwiki/src/main/java/org/xwiki/rendering/internal/resolver/AbstractResourceReferenceEntityReferenceResolver.java
@@ -49,6 +49,7 @@ public abstract class AbstractResourceReferenceEntityReferenceResolver
     protected EntityReferenceResolver<EntityReference> defaultEntityReferenceResolver;
 
     @Inject
+    @Named("current")
     protected Provider<DocumentReference> currentDocumentProvider;
 
     protected ResourceType resourceType;

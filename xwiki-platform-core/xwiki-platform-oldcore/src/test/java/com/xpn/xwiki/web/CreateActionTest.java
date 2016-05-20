f9b160d3706f981c16a1eb24dce176e894a90824
Eduard Moraru <Enygma2002@gmail.com>
2016-05-20 17:30:23 +0300

    XWIKI-13290: Templates not accessible from children pages (Nested documents) - Fixed both the CreateAction and the JS validation to consider the allowedSpaces as not only exact matches but also as prefix matches (i.e. on parents of the specified space reference) - Added Unit test

--- xwiki-platform-core/xwiki-platform-oldcore/src/test/java/com/xpn/xwiki/web/CreateActionTest.java
+++ xwiki-platform-core/xwiki-platform-oldcore/src/test/java/com/xpn/xwiki/web/CreateActionTest.java
@@ -878,6 +878,52 @@ public class CreateActionTest
     }
 
     @Test
+    public void existingDocumentFromUITemplateProviderSpecifiedRestrictionExistsOnParentSpace() throws Exception
+    {
+        // current document = xwiki:Main.WebHome
+        DocumentReference documentReference = new DocumentReference("xwiki", Arrays.asList("Main"), "WebHome");
+        XWikiDocument document = mock(XWikiDocument.class);
+        when(document.getDocumentReference()).thenReturn(documentReference);
+        when(document.isNew()).thenReturn(false);
+        context.setDoc(document);
+
+        // Submit from the UI spaceReference=X.Y.Z&name=W&templateProvider=XWiki.MyTemplateProvider
+        String templateProviderFullName = "XWiki.MyTemplateProvider";
+        String spaceReferenceString = "X.Y.Z";
+        when(mockRequest.getParameter("spaceReference")).thenReturn(spaceReferenceString);
+        when(mockRequest.getParameter("name")).thenReturn("W");
+        when(mockRequest.getParameter("templateprovider")).thenReturn(templateProviderFullName);
+
+        // Mock the resolving of the passed space reference.
+        SpaceReference resolvedSpaceReference = new SpaceReference("xwiki", "X", "Y", "Z");
+        when(mockCurrentStringReferenceResolver.resolve(spaceReferenceString, EntityType.SPACE)).thenReturn(
+            resolvedSpaceReference);
+
+        // Mock 1 existing template provider that allows usage in one of the target space's parents (top level in this
+        // case).
+        mockExistingTemplateProviders(templateProviderFullName, new DocumentReference("xwiki", Arrays.asList("XWiki"),
+            "MyTemplateProvider"), Arrays.asList("X"));
+
+        // Mock the serialization of the passed space reference.
+        when(mockLocalStringReferenceSerializer.serialize(resolvedSpaceReference)).thenReturn(spaceReferenceString);
+
+        // Run the action
+        String result = action.render(context);
+
+        // The tests are below this line!
+
+        // Verify null is returned (this means the response has been returned)
+        assertNull(result);
+
+        // Note1: We are allowed to create anything under space X or its children, be it a terminal or a non-terminal
+        // document
+        // Note2: We are creating X.Y.Z.W and using the template extracted from the template provider.
+        verify(mockURLFactory).createURL("X.Y.Z.W", "WebHome", "edit", "template=XWiki.MyTemplate&title=W", null,
+            "xwiki",
+            context);
+    }
+
+    @Test
     public void existingDocumentFromUITemplateProviderSpecifiedButNotAllowed() throws Exception
     {
         // current document = xwiki:Main.WebHome

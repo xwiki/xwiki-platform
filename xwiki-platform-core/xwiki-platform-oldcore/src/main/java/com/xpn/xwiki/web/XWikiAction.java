be22279294b5f1f53f3e679d254d2a5c40e8b29a
Marius Dumitru Florea <marius@xwiki.com>
2016-06-02 18:47:46 +0300

    XWIKI-13386: Redirects created when renaming a page send the HTTP code 302 as response, they should send 301

--- xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/web/XWikiAction.java
+++ xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/web/XWikiAction.java
@@ -729,12 +729,10 @@ public abstract class XWikiAction extends Action
         String url = context.getWiki().getURL(locationReference, context.getAction(),
                     context.getRequest().getQueryString(), null, context);
 
-        // Send the redirection
-        try {
-            context.getResponse().sendRedirect(url);
-        } catch (IOException e) {
-            throw new XWikiException("Failed to redirect.", e);
-        }
+        // Send the permanent redirection.
+        XWikiResponse response = context.getResponse();
+        response.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
+        response.setHeader("Location", url);
 
         return true;
     }

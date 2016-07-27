0782e3c2c0a5a8ab2b371687d7686f0a3a3125a8
Guillaume Delhumeau <guillaume.delhumeau@xwiki.com>
2016-07-27 16:07:35 +0200

    XWIKI-12544: Allow selection of children in the Export dialog.

---
+++ xwiki-platform-core/xwiki-platform-web/src/main/webapp/resources/uicomponents/exporter/exporter.js
@@ -0,0 +1,138 @@
+// TODO: consider putting it in a webjar instead
+require(['jquery', '$xwiki.getSkinFile('uicomponents/widgets/tree.min.js', true)'], function ($) {
+  'use strict';
+
+  require(['tree'], function () {
+    /**
+     * Class that represent a decoded URL.
+     */
+    var DecodedURL = function (path, params) {
+      
+      /**
+       * Fields
+       */
+      var self    = this;
+      self.path   = path;
+      self.params = params;
+      
+      /**
+       * Return a serialized version of the url
+       */
+      self.serialize = function () {
+        var url = self.path;
+        var separator = '?';
+        for (var param in self.params) {
+          var value = self.params[param];
+          if (param != '' && value !== undefined) {
+            if (Array.isArray(value)) {
+              for (var i = 0; i < value.length; ++i) {
+                url += separator + encodeURIComponent(param) + '=' + encodeURIComponent(value[i]);
+                separator = '&';
+              }
+            } else if (typeof value === 'string') {
+              url += separator + encodeURIComponent(param) + '=' + encodeURIComponent(value);
+              separator = '&';
+            }
+          }
+        }
+        return url;
+      };
+
+      /**
+       * Return a clone of the object
+       */
+      self.clone = function () {
+        var clone = new DecodedURL();
+        clone.path = self.path;
+        clone.params = [];
+        for (var param in self.params) {
+          if (Array.isArray(self.params[param])) {
+            clone.params[param] = [];
+            for (var i = 0; i < self.params[param].length; ++i) {
+              clone.params[param].push(self.params[param][i]);
+            }
+          } else if (typeof self.params[param] === 'string') {
+            clone.params[param] = self.params[param];
+          }
+        }
+        return clone;
+      }
+    };
+    
+    /**
+     * Parse an URL and returns an object containing the path and the parameters decoded from the query string.
+     * Hashes (#) are not supported.
+     * TODO: make it more generic and put it in some library.
+     */
+    var parseURL = function (url) {
+      var result = new DecodedURL();
+      var queryStringLocator = url.indexOf('?');
+      if (queryStringLocator == -1) {
+        result.path = url;
+      } else {
+        result.path = url.substring(0, queryStringLocator);
+        result.queryString = url.substring(queryStringLocator + 1);
+        result.params = {};
+        // Parse the query string
+        var parts = result.queryString.split('&');
+        for (var i = 0; i < parts.length; ++i) {
+          var temp  = parts[i].split('=');
+          var name  = decodeURIComponent(temp[0]);
+          var value = decodeURIComponent(temp[1]);
+          if (result.params[name] !== undefined) {
+            if (Array.isArray(result.params[name])) {
+              result.params[name].push(value);
+            } else {
+              var oldValue = result.params[name];
+              result.params[name] = [oldValue, value];
+            }
+          } else {
+            result.params[name] = value;
+          }
+        }
+      } 
+      
+      return result;
+    };
+    
+    
+    $(document).ready(function () {
+      
+      /**
+       * Save the default url
+       */
+      $('#exportModal a.btn').each(function() {
+        var button = $(this);
+        var url = parseURL(button.attr('href'));
+        button.data('url', url);
+      });
+      
+      /**
+       * Change the tree behaviour
+       */
+      var tree = $.jstree.reference($('#exportModal .xtree'));
+      tree.settings.checkbox.cascade = 'down';
+      tree.settings.checkbox.three_state = false;
+        
+      /**
+       * Store the decoded URL in the export buttons
+       */
+       $('#exportModal a.btn').click(function() {
+        var button = $(this);
+        var tree = $.jstree.reference($('#exportModal .xtree'));
+        var url = button.data('url').clone();
+        var checkedPages = tree.get_checked();
+        if (url.params === undefined) {
+          url.params = {'pages': []};
+        }
+        url.params.pages = [];
+        for (var i = 0; i < checkedPages.length; ++i) {
+          var page = checkedPages[i].substring('document:'.length);
+          url.params.pages.push(page);
+        }
+        button.attr('href', url.serialize());
+      });
+    });
+    
+  });
+});

bb5dd2ea1d7afd9fdeb3dac95469af570f360234
Thibault Clauzier <tclauzier@gmail.com>
2016-09-23 16:29:23 +0200

    XWIKI-13731: Make most expensive steps more visible

--- xwiki-platform-core/xwiki-platform-web/src/main/webapp/resources/uicomponents/debug/debug.js
+++ xwiki-platform-core/xwiki-platform-web/src/main/webapp/resources/uicomponents/debug/debug.js
@@ -19,7 +19,5 @@
  */
 require(["jquery", "$!services.webjars.url('jstree', 'jstree.js')"], function($) {
   // Create the tree and open all nodes when ready
-  $('#debug_performance_tree').jstree().on('ready.jstree', function () {
-    $('#debug_performance_tree').jstree('open_all');
-  });
+  $('#debug_performance_tree').jstree();
 });

45d7276d95123c5ffd1605dc69f6989a3ede9f0f
Vincent Massol <vincent@massol.net>
2016-09-26 11:35:08 +0200

    XWIKI-13735: Some special characters in request parameter keys cause a "missing end delimiter" exception

--- xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/web/XWikiRequestProcessor.java
+++ xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/web/XWikiRequestProcessor.java
@@ -22,10 +22,20 @@ package com.xpn.xwiki.web;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Collections;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.struts.Globals;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.util.RequestUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.xwiki.component.util.DefaultParameterizedType;
@@ -48,6 +58,62 @@ public class XWikiRequestProcessor extends org.apache.struts.action.RequestProce
     private ResourceReferenceResolver<ExtendedURL> resolver =
         Utils.getComponent(new DefaultParameterizedType(null, ResourceReferenceResolver.class, ExtendedURL.class));
 
+    /**
+     * Remove request parameters containing special characters having a meaning for BeanUtils since otherwise BeanUtils
+     * throws an exception if they are not used properly (for example if there's an open {@code (} but without a closing
+     * {@code )} it'll report a "missing end delimiter" exception. FTR BeanUtils uses those to represent Nested
+     * properties, Indexed properties, Mapped properties and Combined properties, see
+     * http://commons.apache.org/proper/commons-beanutils/apidocs/org/apache/commons/beanutils/package-summary.html#standard
+     */
+    public class RequestProcessorServletRequestWrapper extends HttpServletRequestWrapper
+    {
+        private final char[] FORBIDDEN = new char[] {'(', ')', '[', ']', '.'};
+
+        public RequestProcessorServletRequestWrapper(HttpServletRequest request)
+        {
+            super(request);
+        }
+
+        @Override
+        public String getParameter(String name)
+        {
+            if (!StringUtils.containsAny(name, FORBIDDEN)) {
+                return super.getParameter(name);
+            } else {
+                return null;
+            }
+        }
+
+        @Override
+        public Map<String, String[]> getParameterMap()
+        {
+            // Remove all forbidden names
+            Map<String, String[]> newParameterMap = new HashMap<>();
+            for (Map.Entry<String, String[]> entry : super.getParameterMap().entrySet()) {
+                if (!StringUtils.containsAny(entry.getKey(), FORBIDDEN)) {
+                    newParameterMap.put(entry.getKey(), entry.getValue());
+                }
+            }
+            return Collections.unmodifiableMap(newParameterMap);
+        }
+
+        @Override
+        public Enumeration<String> getParameterNames()
+        {
+            return Collections.enumeration(getParameterMap().keySet());
+        }
+
+        @Override
+        public String[] getParameterValues(String name)
+        {
+            if (!StringUtils.containsAny(name, FORBIDDEN)) {
+                return super.getParameterValues(name);
+            } else {
+                return null;
+            }
+        }
+    }
+
     @Override
     protected String processPath(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse)
         throws IOException
@@ -65,6 +131,36 @@ public class XWikiRequestProcessor extends org.apache.struts.action.RequestProce
             return "/" + entityResourceReference.getAction().getActionName() + "/";
         } catch (Exception e) {
             throw new IOException(String.format("Failed to extract the Entity Action from URL [%s]", url), e);
+        }
+    }
+
+    /**
+     * Override the implementation from Struts in order to remove request parameter keys using special BeanUtils syntax
+     * that we don't use and that can causes errors to be raised, see {@link RequestProcessorServletRequestWrapper}.
+     */
+    @Override
+    protected void processPopulate(HttpServletRequest request, HttpServletResponse response, ActionForm form,
+        ActionMapping mapping) throws ServletException
+    {
+        if (form == null) {
+            return;
+        }
+
+        form.setServlet(this.servlet);
+        form.reset(mapping, request);
+
+        if (mapping.getMultipartClass() != null) {
+            request.setAttribute(Globals.MULTIPART_KEY,
+                mapping.getMultipartClass());
+        }
+
+        RequestUtils.populate(form, mapping.getPrefix(), mapping.getSuffix(),
+            new RequestProcessorServletRequestWrapper(request));
+
+        // Set the cancellation request attribute if appropriate
+        if ((request.getParameter(Globals.CANCEL_PROPERTY) != null)
+            || (request.getParameter(Globals.CANCEL_PROPERTY_X) != null)) {
+            request.setAttribute(Globals.CANCEL_KEY, Boolean.TRUE);
         }
     }
 }

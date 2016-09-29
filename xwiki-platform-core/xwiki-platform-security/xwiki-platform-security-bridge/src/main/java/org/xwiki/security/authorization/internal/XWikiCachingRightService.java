860dd90ed222539fdc3d3e9d315ee643efccbdb0
Guillaume Delhumeau <guillaume.delhumeau@xwiki.com>
2016-09-29 17:38:32 +0200

    Revert "XWIKI-10136: A user should be able to edit her comments even if she does not have the edit right on the page."
    This reverts commit cc7c6af3b145561ce1bef08fd7005258ee5c4eb0.

--- xwiki-platform-core/xwiki-platform-security/xwiki-platform-security-bridge/src/main/java/org/xwiki/security/authorization/internal/XWikiCachingRightService.java
+++ xwiki-platform-core/xwiki-platform-security/xwiki-platform-security-bridge/src/main/java/org/xwiki/security/authorization/internal/XWikiCachingRightService.java
@@ -95,7 +95,6 @@ public class XWikiCachingRightService implements XWikiRightService
             .putAction("undelete", Right.EDIT)
             .putAction("reset", Right.DELETE)
             .putAction("commentadd", Right.COMMENT)
-            .putAction("commentsave", Right.COMMENT)
             .putAction("redirect", Right.VIEW)
             .putAction("export", Right.VIEW)
             .putAction("import", Right.ADMIN)

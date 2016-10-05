d3b82f14f6238ff53a81fce8ddcd33708fbfb8f5
Vincent Massol <vincent@massol.net>
2016-10-05 17:43:47 +0200

    XWIKI-12912: Improve authorization checks based on the current user for accessing a VFS attachment archive

--- xwiki-platform-core/xwiki-platform-vfs/xwiki-platform-vfs-api/src/main/java/org/xwiki/vfs/internal/attach/AttachVfsPermissionChecker.java
+++ xwiki-platform-core/xwiki-platform-vfs/xwiki-platform-vfs-api/src/main/java/org/xwiki/vfs/internal/attach/AttachVfsPermissionChecker.java
@@ -19,17 +19,23 @@
  */
 package org.xwiki.vfs.internal.attach;
 
+import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
 
 import org.xwiki.component.annotation.Component;
+import org.xwiki.model.reference.AttachmentReferenceResolver;
+import org.xwiki.model.reference.DocumentReference;
+import org.xwiki.security.authorization.ContextualAuthorizationManager;
+import org.xwiki.security.authorization.Right;
 import org.xwiki.vfs.VfsException;
 import org.xwiki.vfs.VfsPermissionChecker;
 import org.xwiki.vfs.VfsResourceReference;
 
 /**
- * Permission checker for the Attach VFS which allows everyone to access it since it's safe as the Attach VFS driver
- * checks permissions when accessing a Document's attachment.
+ * Permission checker for the Attach VFS URI scheme. We check that the current user has view permissions on the page
+ * holding the attachment. We need to do this here for the moment because of
+ * <a href="http://jira.xwiki.org/browse/XWIKI-12912">this issue</a>.
  *
  * @version $Id$
  * @since 7.4M2
@@ -39,10 +45,26 @@ import org.xwiki.vfs.VfsResourceReference;
 @Singleton
 public class AttachVfsPermissionChecker implements VfsPermissionChecker
 {
+    @Inject
+    private ContextualAuthorizationManager authorizationManager;
+
+    @Inject
+    private AttachmentReferenceResolver<String> defaultAttachmentReferenceresolver;
+
     @Override
     public void checkPermission(VfsResourceReference resourceReference) throws VfsException
     {
-        // We always allow using the Attach URI scheme since it's safe and the Attach VFS driver checks permissions
-        // when accessing a Document's attachment.
+        // Check for view permission for the page holding the attachment and for the current user.
+
+        // Extract the document reference from the VFS Resource Reference
+        // Use a default resolver (and not a current one) since we don't have any context, we're in a new
+        // request.
+
+        DocumentReference documentReference = this.defaultAttachmentReferenceresolver.resolve(
+            resourceReference.getURI().getSchemeSpecificPart()).getDocumentReference();
+
+        if (!this.authorizationManager.hasAccess(Right.VIEW, documentReference)) {
+            throw new VfsException(String.format("No View permission for document [%s]", documentReference));
+        }
     }
 }

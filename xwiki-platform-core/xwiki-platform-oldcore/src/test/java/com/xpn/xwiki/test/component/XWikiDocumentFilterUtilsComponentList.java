2a91d62b96326fd87c2c20e210c2a9d21d366b8f
Thomas Mortagne <thomas.mortagne@gmail.com>
2016-11-26 11:47:31 +0100

    XWIKI-13902: Rewrite entities to/fromXML methods based on Filter framework

--- xwiki-platform-core/xwiki-platform-oldcore/src/test/java/com/xpn/xwiki/test/component/XWikiDocumentFilterUtilsComponentList.java
+++ xwiki-platform-core/xwiki-platform-oldcore/src/test/java/com/xpn/xwiki/test/component/XWikiDocumentFilterUtilsComponentList.java
@@ -17,7 +17,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package com.xpn.xwiki.internal.filter;
+package com.xpn.xwiki.test.component;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.Inherited;

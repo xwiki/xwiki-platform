41bde8ad6daaefc1b7d4da999755c0c92ecb1b19
Guillaume Delhumeau <guillaume.delhumeau@xwiki.com>
2016-12-05 19:36:41 +0100

    XWIKI-13901: Make it easier to find an element in the select fields of the "Localization" section in the Administration.
    * Update LocalizationAdministrationSectionPage.
    * Update BootSelect page object to handle select with multiple selection disabled.
    
    (cherry picked from commit c02d9650427502c727c89d238b742113c7da267b)

--- xwiki-platform-core/xwiki-platform-test/xwiki-platform-test-ui/src/main/java/org/xwiki/test/ui/po/BootstrapSelect.java
+++ xwiki-platform-core/xwiki-platform-test/xwiki-platform-test-ui/src/main/java/org/xwiki/test/ui/po/BootstrapSelect.java
@@ -21,9 +21,12 @@ package org.xwiki.test.ui.po;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
@@ -70,48 +73,88 @@ public class BootstrapSelect
 
     public void selectByValues(List<String> values)
     {
+        Map<String, String> valueTitles = new HashMap();
+
         // Get the list of all possible values in the hidden <select> field
         // WARN: the order of the values is imported since their index will help us to identify
         // them in the enhanced bootstrap-select widget.
         List<String> options = new ArrayList<>();
         for (WebElement element : this.hiddenSelect.findElements(By.tagName("option"))) {
-            options.add(element.getAttribute("value"));
+            String value = element.getAttribute("value");
+            options.add(value);
+            valueTitles.put(value, element.getText());
         }
 
         // Open the enhanced bootstrap-select widget
         openMenu();
 
-        // For each of its option
-        for (WebElement element : this.menu.findElements(By.tagName("li"))) {
-            // We find the language associated to this item thanks to the attribute "data-original-index"
-            int index = Integer.parseInt(element.getAttribute("data-original-index"));
-            String value = options.get(index);
+        if (isMultiple()) {
+            // For each of its option
+            for (WebElement element : this.menu.findElements(By.tagName("li"))) {
+                // We find the language associated to this item thanks to the attribute "data-original-index"
+                int index = Integer.parseInt(element.getAttribute("data-original-index"));
+                String value = options.get(index);
 
-            // Now, click on the element to select/unselect it if its status is different from what we want
-            if (isElementSelected(element) != values.contains(value)) {
-                element.findElement(By.tagName("a")).click();
-                // If the element is not displayed inside the window (selenium does not handle scrolling inside an
-                // element) the previous action has actually closed the menu, without changing the state of the element.
-                // So we need to check if the menu has been close.
-                if (!this.menu.isDisplayed()) {
-                    // In that was, we reopen it
-                    openMenu();
-                    // When we reopen the menu, the element is now contained inside the viewport (the previous click
-                    // had some effect) so we can click it.
-                    // TODO: make this less hacky.
+                // Now, click on the element to select/unselect it if its status is different from what we want
+                if (isElementSelected(element) != values.contains(value)) {
                     element.findElement(By.tagName("a")).click();
+
+                    // If the element is not displayed inside the window (selenium does not handle scrolling inside an
+                    // element) the previous action has actually closed the menu, without changing the state of the element.
+                    // So we need to check if the menu has been close.
+                    if (!this.menu.isDisplayed()) {
+                        // In that was, we reopen it
+                        openMenu();
+                        // When we reopen the menu, the element is now contained inside the viewport (the previous click
+                        // had some effect) so we can click it.
+                        // TODO: make this less hacky.
+                        element.findElement(By.tagName("a")).click();
+                    }
+                }
+            }
+        } else {
+            WebElement filterInput = getFilterInput();
+            if (filterInput != null) {
+                filterInput.sendKeys(valueTitles.get(values.get(0)));
+            }
+            // For each of its option
+            for (WebElement element : this.menu.findElements(By.tagName("li"))) {
+                // We find the language associated to this item thanks to the attribute "data-original-index"
+                int index = Integer.parseInt(element.getAttribute("data-original-index"));
+                String value = options.get(index);
+
+                // Now, click on the element to select the value
+                if (element.isDisplayed() && values.contains(value)) {
+                    element.findElement(By.tagName("a")).click();
+                    waitUntilMenuIsClosed();
+                    if (!button.getAttribute("title").startsWith(valueTitles.get(value))) {
+                        throw new RuntimeException(
+                                String.format("Failed to set the value [%s] with the title [%s]. Got [%s] instead.",
+                                        value, valueTitles.get(value), button.getAttribute("title")));
+                    }
+                    break;
                 }
             }
         }
 
+        // To finish, close the enhanced bootstrap-select widget
         if (this.menu.isDisplayed()) {
             closeMenu();
         }
     }
 
-    private void openMenu()
+    private WebElement getFilterInput()
     {
-        this.button.click();
+        try {
+            return this.driver.findElementWithoutWaiting(this.menu, By.tagName("input"));
+        } catch (NoSuchElementException e) {
+            return null;
+        }
+    }
+
+    private void waitUntilMenuIsOpened()
+    {
+
         driver.waitUntilCondition(new ExpectedCondition<Object>()
         {
             @Override
@@ -122,9 +165,8 @@ public class BootstrapSelect
         });
     }
 
-    private void closeMenu()
+    private void waitUntilMenuIsClosed()
     {
-        this.button.click();
         driver.waitUntilCondition(new ExpectedCondition<Object>()
         {
             @Override
@@ -133,6 +175,18 @@ public class BootstrapSelect
                 return !menu.isDisplayed();
             }
         });
+    }
+
+    private void openMenu()
+    {
+        this.button.click();
+        waitUntilMenuIsOpened();
+    }
+
+    private void closeMenu()
+    {
+        this.button.click();
+        waitUntilMenuIsClosed();
     }
 
     private boolean isElementSelected(WebElement element)

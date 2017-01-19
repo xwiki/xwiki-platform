f14cb2478bacd326e10bf61ff52d29f88eb8f7e2
Guillaume Delhumeau <guillaume.delhumeau@xwiki.com>
2017-01-19 16:28:45 +0100

    XWIKI-13954: Default product pages should not be easily deleted.
    * Introduce a form in the UI, dynamically loaded, to ask users to confirm the deletion of pages that belong to extensions
    
    Note: the answer is not yet sent to the server

--- xwiki-platform-core/xwiki-platform-flamingo/xwiki-platform-flamingo-skin/xwiki-platform-flamingo-skin-resources/src/main/resources/flamingo/delete.js
+++ xwiki-platform-core/xwiki-platform-flamingo/xwiki-platform-flamingo-skin/xwiki-platform-flamingo-skin-resources/src/main/resources/flamingo/delete.js
@@ -1,14 +1,25 @@
-require(['jquery', 'xwiki-meta'], function($, xm) {
+require.config({
+  paths: {
+    jsTree: "$!services.webjars.url('jstree', 'jstree.min.js')",
+    tree: "$!services.webjars.url('org.xwiki.platform:xwiki-platform-tree-webjar', 'tree.min.js')"
+  },
+  shim: {
+    jsTree: {
+      deps: ['jquery']
+    }
+  }
+});
+require(['jquery', 'xwiki-meta', 'tree'], function($, xm) {
   $(document).ready(function() {
     var progressBar = $('#delete-progress-bar');
     var jobId = progressBar.data('job-id');
     var baseURL = xm.restURL.substr(0, xm.restURL.indexOf('/rest/'));
-    
+
     var updateProgressBar = function(progress) {
       progressBar.css('width', progress * 100 + '%');
       progressBar.data('progress', progress);
     };
-    
+
     var whenTerminated = function() {
       updateProgressBar(1);
       $('#delete-progress-bar-container').hide();
@@ -28,24 +39,55 @@ require(['jquery', 'xwiki-meta'], function($, xm) {
         }
       });
     };
-    
+
+    var handleQuestion = function() {
+      $.ajax(new XWiki.Document(xm.documentReference).getURL('get', 'xpage=refactoring/delete_question&jobId='+jobId)).done(function (data) {
+        $('#delete-progress-bar-container').after($('<div>').html(data));
+        $('.deleteTree').xtree({plugins: ['checkbox'], core: {themes: {icons: true, dots: true}}});
+        $('.btSelectAllTree').click(function(event){
+          event.preventDefault();
+           $('.deleteTree').jstree().check_all();
+        });
+        $('.btUnselectAllTree').click(function(event){
+          event.preventDefault();
+           $('.deleteTree').jstree().uncheck_all();
+        });
+        $('.btConfirmDelete').click(function(event){
+          event.preventDefault();
+          var selectedNodes = $('.deleteTree').jstree().get_selected(true);
+          var selectedPages = [];
+          for (var i = 0; i < selectedNodes.length; ++i) {
+            if (selectedNodes[i].data.page) {
+              selectedPages.push(selectedNodes[i].data.page);
+            }
+          }
+          console.log(selectedPages);
+          // TODO: send the answer
+        });
+      });
+    }
+
     var getProgressStatus = function() {
       // TODO: add a factory for the REST URL
       var url = baseURL + '/rest/jobstatus/' + jobId;
       // Note: we use JSON because it is easier to parse with javascript
       $.ajax(url, {'data': {'media': 'json'}}).done(function (data) {
+        updateProgressBar(data.progress.offset);
+        if (data.state == 'WAITING') {
+          handleQuestion();
+          return;
+        }
         if (data.progress.offset < 1) {
-          updateProgressBar(data.progress.offset);
           setTimeout(getProgressStatus, 1000);
         } else {
           whenTerminated();
         }
       });
     };
-    
+
     // Init
     updateProgressBar(progressBar.data('progress'));
     getProgressStatus();
-    
+
   });
 });

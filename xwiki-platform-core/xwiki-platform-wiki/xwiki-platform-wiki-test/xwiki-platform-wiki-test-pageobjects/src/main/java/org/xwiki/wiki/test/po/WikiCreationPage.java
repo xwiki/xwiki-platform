b4ae46f6ded4d2678d5398f70292734944614d17
Guillaume Delhumeau <guillaume.delhumeau@xwiki.com>
2017-01-24 14:52:44 +0100

    [Misc] Fix failing test.

--- xwiki-platform-core/xwiki-platform-wiki/xwiki-platform-wiki-test/xwiki-platform-wiki-test-pageobjects/src/main/java/org/xwiki/wiki/test/po/WikiCreationPage.java
+++ xwiki-platform-core/xwiki-platform-wiki/xwiki-platform-wiki-test/xwiki-platform-wiki-test-pageobjects/src/main/java/org/xwiki/wiki/test/po/WikiCreationPage.java
@@ -73,6 +73,7 @@ public class WikiCreationPage extends ExtendedViewPage
 
     public void waitForFinalizeButton(int timeout)
     {
+
         // The finalize button is not visible until the provisioning is done, so we wait for it
         getDriver().waitUntilElementIsVisible(By.id("finalize"), timeout);
     }

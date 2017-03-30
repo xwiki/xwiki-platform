0f802f51f6b5ab25edf75ae69cb79ca5718a51d9
Thomas Mortagne <thomas.mortagne@gmail.com>
2017-03-30 18:32:28 +0200

    XWIKI-14040: Wiki component registration code test the wrong author

--- xwiki-platform-core/xwiki-platform-component/xwiki-platform-component-wiki/src/main/java/org/xwiki/component/wiki/internal/DefaultWikiComponentBuilder.java
+++ xwiki-platform-core/xwiki-platform-component/xwiki-platform-component-wiki/src/main/java/org/xwiki/component/wiki/internal/DefaultWikiComponentBuilder.java
@@ -39,8 +39,6 @@ import org.xwiki.component.wiki.WikiComponentBuilder;
 import org.xwiki.component.wiki.WikiComponentException;
 import org.xwiki.component.wiki.internal.bridge.WikiComponentBridge;
 import org.xwiki.model.reference.DocumentReference;
-import org.xwiki.security.authorization.ContextualAuthorizationManager;
-import org.xwiki.security.authorization.Right;
 
 import com.xpn.xwiki.XWikiContext;
 import com.xpn.xwiki.XWikiException;
@@ -77,9 +75,6 @@ public class DefaultWikiComponentBuilder implements WikiComponentBuilder, WikiCo
     @Inject
     private WikiComponentBridge componentBridge;
 
-    @Inject
-    private ContextualAuthorizationManager authorization;
-
     @Override
     public List<DocumentReference> getDocumentReferences()
     {
@@ -107,7 +102,7 @@ public class DefaultWikiComponentBuilder implements WikiComponentBuilder, WikiCo
     {
         List<WikiComponent> components = new ArrayList<WikiComponent>();
 
-        if (!this.authorization.hasAccess(Right.PROGRAM, reference)) {
+        if (!this.componentBridge.hasProgrammingRights(reference)) {
             throw new WikiComponentException("Registering wiki components requires programming rights");
         }
 

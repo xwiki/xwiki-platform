22debcbcf51f18da625d15a935d3d3f2cdd094bb
Clément Aubin <clement.aubin@xwiki.com>
2017-04-27 12:00:16 +0200

    XWIKI-14218: Add the ability to disable the notifications * Expose `enabled` configuration flag to Velocity

--- xwiki-platform-core/xwiki-platform-notifications/xwiki-platform-notifications-api/src/main/java/org/xwiki/notifications/script/NotificationScriptService.java
+++ xwiki-platform-core/xwiki-platform-notifications/xwiki-platform-notifications-api/src/main/java/org/xwiki/notifications/script/NotificationScriptService.java
@@ -38,6 +38,7 @@ import org.xwiki.model.reference.EntityReferenceSerializer;
 import org.xwiki.notifications.CompositeEvent;
 import org.xwiki.notifications.CompositeEventStatus;
 import org.xwiki.notifications.CompositeEventStatusManager;
+import org.xwiki.notifications.NotificationConfiguration;
 import org.xwiki.notifications.NotificationException;
 import org.xwiki.notifications.NotificationManager;
 import org.xwiki.notifications.NotificationRenderer;
@@ -58,6 +59,9 @@ import org.xwiki.stability.Unstable;
 @Unstable
 public class NotificationScriptService implements ScriptService
 {
+    @Inject
+    private NotificationConfiguration notificationConfiguration;
+
     @Inject
     private NotificationManager notificationManager;
 
@@ -88,10 +92,8 @@ public class NotificationScriptService implements ScriptService
     public List<CompositeEvent> getEvents(boolean onyUnread, int expectedCount) throws NotificationException
     {
         return notificationManager.getEvents(
-                entityReferenceSerializer.serialize(documentAccessBridge.getCurrentUserReference()),
-                onyUnread,
-                expectedCount
-        );
+            entityReferenceSerializer.serialize(documentAccessBridge.getCurrentUserReference()), onyUnread,
+            expectedCount);
     }
 
     /**
@@ -103,15 +105,11 @@ public class NotificationScriptService implements ScriptService
      * @throws NotificationException if error happens
      */
     public List<CompositeEvent> getEvents(boolean onyUnread, int expectedCount, Date untilDate, String[] blackList)
-            throws NotificationException
+        throws NotificationException
     {
         return notificationManager.getEvents(
-                entityReferenceSerializer.serialize(documentAccessBridge.getCurrentUserReference()),
-                onyUnread,
-                expectedCount,
-                untilDate,
-                Arrays.asList(blackList)
-        );
+            entityReferenceSerializer.serialize(documentAccessBridge.getCurrentUserReference()), onyUnread,
+            expectedCount, untilDate, Arrays.asList(blackList));
     }
 
     /**
@@ -123,15 +121,11 @@ public class NotificationScriptService implements ScriptService
      * @throws NotificationException if error happens
      */
     public List<CompositeEvent> getEvents(boolean onyUnread, int expectedCount, Date untilDate, List<String> blackList)
-            throws NotificationException
+        throws NotificationException
     {
         return notificationManager.getEvents(
-                entityReferenceSerializer.serialize(documentAccessBridge.getCurrentUserReference()),
-                onyUnread,
-                expectedCount,
-                untilDate,
-                blackList
-        );
+            entityReferenceSerializer.serialize(documentAccessBridge.getCurrentUserReference()), onyUnread,
+            expectedCount, untilDate, blackList);
     }
 
     /**
@@ -145,14 +139,12 @@ public class NotificationScriptService implements ScriptService
     public long getEventsCount(boolean onlyUnread, int maxCount) throws NotificationException
     {
         return notificationManager.getEventsCount(
-                entityReferenceSerializer.serialize(documentAccessBridge.getCurrentUserReference()),
-                onlyUnread,
-                maxCount
-        );
+            entityReferenceSerializer.serialize(documentAccessBridge.getCurrentUserReference()), onlyUnread, maxCount);
     }
 
     /**
      * Generate a rendering Block for a given event to display as notification.
+     * 
      * @param event the event to render
      * @return a rendering block ready to display the event
      * @throws NotificationException if an error happens
@@ -167,13 +159,12 @@ public class NotificationScriptService implements ScriptService
      *
      * @param events a list of events
      * @return the list of statuses corresponding to each pair or event/entity
-     *
      * @throws Exception if an error occurs
      */
     public List<EventStatus> getEventStatuses(List<Event> events) throws Exception
     {
         return eventStatusManager.getEventStatus(events,
-                Arrays.asList(entityReferenceSerializer.serialize(documentAccessBridge.getCurrentUserReference())));
+            Arrays.asList(entityReferenceSerializer.serialize(documentAccessBridge.getCurrentUserReference())));
     }
 
     /**
@@ -181,18 +172,28 @@ public class NotificationScriptService implements ScriptService
      *
      * @param compositeEvents a list of composite events
      * @return the list of statuses corresponding to each pair or event/entity
-     *
      * @throws Exception if an error occurs
      * @since 9.4RC1
      */
     public List<CompositeEventStatus> getCompositeEventStatuses(List<CompositeEvent> compositeEvents) throws Exception
     {
         return compositeEventStatusManager.getCompositeEventStatuses(compositeEvents,
-                entityReferenceSerializer.serialize(documentAccessBridge.getCurrentUserReference()));
+            entityReferenceSerializer.serialize(documentAccessBridge.getCurrentUserReference()));
+    }
+
+    /**
+     * Get the enabled status of the module
+     * 
+     * @return true if the Notification module is enabled in the platform configuration
+     */
+    public boolean isEnabled()
+    {
+        return notificationConfiguration.isEnabled();
     }
 
     /**
      * Save a status for the current user.
+     * 
      * @param eventId id of the event
      * @param isRead either or not the current user has read the given event
      * @throws Exception if an error occurs

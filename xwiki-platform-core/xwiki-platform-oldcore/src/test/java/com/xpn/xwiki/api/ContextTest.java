77d770dc5cde72a455baad50d32fa1790f882253
Vincent Massol <vincent@massol.net>
2017-05-07 19:40:32 +0200

    [Misc] Convert test to Mockito and JUnit4 + remove exception in stdout for a component that cannot be found

--- xwiki-platform-core/xwiki-platform-oldcore/src/test/java/com/xpn/xwiki/api/ContextTest.java
+++ xwiki-platform-core/xwiki-platform-oldcore/src/test/java/com/xpn/xwiki/api/ContextTest.java
@@ -19,30 +19,19 @@
  */
 package com.xpn.xwiki.api;
 
-import java.util.Locale;
-
-import org.apache.velocity.VelocityContext;
-import org.hamcrest.Description;
-import org.hamcrest.Factory;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
-import org.jmock.Expectations;
-import org.jmock.lib.legacy.ClassImposteriser;
-import org.junit.Assert;
-import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
-import org.xwiki.context.Execution;
 import org.xwiki.model.reference.DocumentReference;
-import org.xwiki.rendering.syntax.Syntax;
-import org.xwiki.test.jmock.AbstractComponentTestCase;
-import org.xwiki.velocity.VelocityManager;
+import org.xwiki.test.annotation.AllComponents;
 
-import com.xpn.xwiki.CoreConfiguration;
 import com.xpn.xwiki.XWikiContext;
 import com.xpn.xwiki.doc.XWikiDocument;
 import com.xpn.xwiki.objects.BaseObject;
 import com.xpn.xwiki.objects.classes.BaseClass;
-import com.xpn.xwiki.web.Utils;
+import com.xpn.xwiki.test.MockitoOldcoreRule;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
 /**
  * Unit tests for {@link Context}.
@@ -50,104 +39,39 @@ import com.xpn.xwiki.web.Utils;
  * @version $Id$
  * @since 4.4RC1
  */
-public class ContextTest extends AbstractComponentTestCase
+@AllComponents
+public class ContextTest
 {
-    public static class XWikiDocumentMatcher extends TypeSafeMatcher<XWikiDocument>
-    {
-        private DocumentReference documentReference;
-
-        public XWikiDocumentMatcher(DocumentReference documentReference)
-        {
-            this.documentReference = documentReference;
-        }
-
-        @Override
-        protected boolean matchesSafely(XWikiDocument document)
-        {
-            return documentReference.equals(document.getDocumentReference());
-        }
-
-        @Override
-        public void describeTo(Description description)
-        {
-            description.appendText("a document with a reference equal to [")
-                .appendValue(this.documentReference)
-                .appendText("]");
-        }
-    }
-
-    @Factory
-    public static Matcher<XWikiDocument> anXWikiDocumentWithReference(DocumentReference documentReference)
-    {
-        return new XWikiDocumentMatcher(documentReference);
-    }
-
-    @Before
-    public void configure() throws Exception
-    {
-        getMockery().setImposteriser(ClassImposteriser.INSTANCE);
-        Utils.setComponentManager(getComponentManager());
-    }
+    @Rule
+    public MockitoOldcoreRule oldcoreRule = new MockitoOldcoreRule();
 
     /**
      * Tests that pages can override the default property display mode using {@code $xcontext.setDisplayMode}.
      *
-     * @see "XWIKI-2436."
+     * @see "XWIKI-2436"
      */
     @Test
     public void setDisplayMode() throws Exception
     {
-        // Setup Context and XWiki objects
-        final XWikiContext xcontext = new XWikiContext();
-        xcontext.setMainXWiki("testwiki");
-        xcontext.setWikiId("testwiki");
+        XWikiContext xcontext = this.oldcoreRule.getXWikiContext();
 
-        final com.xpn.xwiki.XWiki xwiki = getMockery().mock(com.xpn.xwiki.XWiki.class);
-        xcontext.setWiki(xwiki);
+        DocumentReference documentReference = new DocumentReference("wiki", "space", "page");
+        XWikiDocument document = new XWikiDocument(documentReference);
+        BaseClass baseClass = document.getXClass();
 
-        final CoreConfiguration coreConfiguration = registerMockComponent(CoreConfiguration.class);
-        final VelocityManager velocityManager = registerMockComponent(VelocityManager.class);
-
-        final DocumentReference documentReference = new DocumentReference("wiki", "space", "page");
-        final XWikiDocument document = new XWikiDocument(documentReference);
-        final BaseClass baseClass = document.getXClass();
-
-        getMockery().checking(new Expectations()
-        {{
-            allowing(xwiki).getCurrentContentSyntaxId("xwiki/2.1", xcontext);
-            will(returnValue("xwiki/2.1"));
-            allowing(coreConfiguration).getDefaultDocumentSyntax();
-            will(returnValue(Syntax.XWIKI_2_1));
-            allowing(velocityManager).getVelocityContext();
-            will(returnValue(new VelocityContext()));
-            allowing(xwiki).getLanguagePreference(xcontext);
-            will(returnValue("en"));
-            // Translated document
-            allowing(xwiki).getDocument(with(equal(new DocumentReference(documentReference, Locale.ENGLISH))), with(same(xcontext)));
-            will(returnValue(document));
-            allowing(xwiki).getXClass(documentReference, xcontext);
-            will(returnValue(baseClass));
-            // Decide that there's no custom Displayer for the String field
-            allowing(xwiki).exists(new DocumentReference("testwiki", "XWiki", "StringDisplayer"), xcontext);
-            will(returnValue(false));
-            allowing(xwiki).evaluateTemplate("displayer_string.vm", xcontext);
-            will(returnValue(""));
-        }});
+        doReturn("xwiki/2.1").when(this.oldcoreRule.getSpyXWiki()).getCurrentContentSyntaxId("xwiki/2.1", xcontext);
+        doReturn(baseClass).when(this.oldcoreRule.getSpyXWiki()).getXClass(documentReference, xcontext);
 
         baseClass.addTextField("prop", "prop", 5);
         BaseObject obj = (BaseObject) document.getXClass().newObject(xcontext);
         obj.setStringValue("prop", "value");
         document.addXObject(obj);
 
-        // Tie together Execution Context and old XWiki Context
-        Execution execution = getComponentManager().getInstance(Execution.class);
-        execution.getContext().setProperty("xwikicontext", xcontext);
-
         Context context = new Context(xcontext);
         context.setDisplayMode("edit");
 
         // We verify that the result contains a form input
-        Assert.assertEquals("<input size='5' id='space.page_0_prop' value='value' name='space.page_0_prop' "
+        assertEquals("<input size='5' id='space.page_0_prop' value='value' name='space.page_0_prop' "
             + "type='text'/>", document.display("prop", xcontext));
     }
 }

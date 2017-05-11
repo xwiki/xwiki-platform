f6b869d81d2331f4bd5349f1a98958cb6642b88c
Vincent Massol <vincent@massol.net>
2017-05-11 13:44:22 +0200

    XWIKI-14271: Error thrown when exporting as PDF/RTF a document imported from Word XWIKI-14224: text-autospace or text-justify break pdf export * Improve test strength by ensuring that the generated results is valid FO according to FOP

--- xwiki-platform-core/xwiki-platform-oldcore/src/test/java/com/xpn/xwiki/pdf/impl/XHTML2FOTest.java
+++ xwiki-platform-core/xwiki-platform-oldcore/src/test/java/com/xpn/xwiki/pdf/impl/XHTML2FOTest.java
@@ -19,19 +19,36 @@
  */
 package com.xpn.xwiki.pdf.impl;
 
+import java.io.File;
 import java.io.InputStream;
 import java.io.StringReader;
 
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.sax.SAXResult;
 import javax.xml.transform.sax.SAXSource;
+import javax.xml.transform.stream.StreamSource;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.output.NullOutputStream;
+import org.apache.fop.apps.EnvironmentProfile;
+import org.apache.fop.apps.EnvironmentalProfileFactory;
+import org.apache.fop.apps.FOUserAgent;
+import org.apache.fop.apps.Fop;
+import org.apache.fop.apps.FopFactory;
+import org.apache.fop.apps.FopFactoryBuilder;
+import org.apache.fop.apps.io.ResourceResolverFactory;
 import org.custommonkey.xmlunit.Diff;
 import org.custommonkey.xmlunit.XMLUnit;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.xml.sax.InputSource;
 import org.xml.sax.XMLReader;
 import org.xwiki.rendering.internal.parser.xhtml.wikimodel.XWikiXMLReaderFactory;
+import org.xwiki.test.AllLogRule;
 import org.xwiki.test.annotation.ComponentList;
 import org.xwiki.xml.EntityResolver;
 import org.xwiki.xml.XMLReaderFactory;
@@ -39,6 +56,7 @@ import org.xwiki.xml.XMLUtils;
 import org.xwiki.xml.internal.DefaultXMLReaderFactory;
 import org.xwiki.xml.internal.LocalEntityResolver;
 
+import com.xpn.xwiki.pdf.api.PdfExport;
 import com.xpn.xwiki.test.MockitoOldcoreRule;
 
 import static org.junit.Assert.*;
@@ -56,7 +74,28 @@ import static org.junit.Assert.*;
 public class XHTML2FOTest
 {
     @Rule
+    public AllLogRule logRule = new AllLogRule();
+
+    @Rule
     public MockitoOldcoreRule oldcore = new MockitoOldcoreRule();
+
+    private static FopFactory fopFactory;
+
+    private static FOUserAgent foUserAgent;
+
+    private static TransformerFactory transformerFactory;
+
+    @BeforeClass
+    public static void setUp() throws Exception
+    {
+        EnvironmentProfile environmentProfile = EnvironmentalProfileFactory.createDefault(new File(".").toURI(),
+            ResourceResolverFactory.createDefaultResourceResolver());
+        FopFactoryBuilder builder = new FopFactoryBuilder(environmentProfile);
+        fopFactory = builder.build();
+
+        foUserAgent = fopFactory.newFOUserAgent();
+        transformerFactory = TransformerFactory.newInstance();
+    }
 
     /**
      * Verifies that we can have some style using the "font" attribute.
@@ -140,6 +179,25 @@ public class XHTML2FOTest
         SAXSource xsltSource = new SAXSource(xmlReader, new InputSource(xslt));
 
         String transformedXML = XMLUtils.transform(xmlSource, xsltSource);
+
+        validateFOP(transformedXML);
+
         return transformedXML;
+    }
+
+    private void validateFOP(String transformedXML) throws Exception
+    {
+        // Run FOP to verify that the XML passed as input is some valid FO and doesn't generate errors
+
+        Fop fop = fopFactory.newFop(new PdfExport.ExportType("application/pdf", "pdf").getMimeType(), foUserAgent,
+            NullOutputStream.NULL_OUTPUT_STREAM);
+
+        Transformer transformer = transformerFactory.newTransformer();
+
+        Source source = new StreamSource(new StringReader(transformedXML));
+
+        Result res = new SAXResult(fop.getDefaultHandler());
+
+        transformer.transform(source, res);
     }
 }

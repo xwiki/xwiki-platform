cc4755097a74c95a0e726675e0cc8e141e402404
Pierre Bondoerffer <pierre@bondoer.fr>
2017-06-15 10:43:10 +0200

    XWIKI-5146: DBList livetable filter
    * Add missing license header
    * Remove shim config
    * Use a data-attribute for limit
    * Make a parameter name more explicit
    * Use $mathtool.toInteger instead of $numbertool.integer

--- xwiki-platform-core/xwiki-platform-web/src/main/webapp/resources/js/xwiki/table/livetabledblist.js
+++ xwiki-platform-core/xwiki-platform-web/src/main/webapp/resources/js/xwiki/table/livetabledblist.js
@@ -1,9 +1,26 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
 require.config({
   paths: {
     'typeahead': "$services.webjars.url('Bootstrap-3-Typeahead', 'bootstrap3-typeahead.min.js')"
-  },
-  shim: {
-    'typeahead': ['jquery', 'bootstrap']
   }
 });
 
@@ -13,17 +30,18 @@ require(['jquery', 'xwiki-meta', 'typeahead', 'xwiki-events-bridge'],
       $(livetable).find('input[data-type="suggest"]').each(function(i, element) {
         var input = $(element);
         var hidden = input.prev('input[type="hidden"]');
+        var limit = parseInt(input.attr('data-limit'), 10); // 10 is the radix, not a fallback
         input.typeahead({
           source: function(query, callback) {
             $.getJSON("$xwiki.getURL('XWiki.LiveTableDBListFilterService', 'get')", {
                 'outputSyntax': 'plain',
                 'class': input.attr('data-xclass'),
-                'prop': hidden.attr('name'),
+                'property': hidden.attr('name'),
                 'input': query,
-                'limit': 10
+                'limit': limit
             }, callback);
           },
-          items: 10,
+          items: limit,
           fitToElement: true,
           afterSelect: function(item) {
             hidden.val(item.id);

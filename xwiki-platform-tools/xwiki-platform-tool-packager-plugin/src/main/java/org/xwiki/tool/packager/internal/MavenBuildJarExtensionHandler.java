6850d6c2ed005b27a8a1a66b65cb1c99de0025f5
Thomas Mortagne <thomas.mortagne@gmail.com>
2017-07-20 15:51:28 +0200

    [misc] Optimize JAR handling

--- xwiki-platform-tools/xwiki-platform-tool-packager-plugin/src/main/java/org/xwiki/tool/packager/internal/MavenBuildJarExtensionHandler.java
+++ xwiki-platform-tools/xwiki-platform-tool-packager-plugin/src/main/java/org/xwiki/tool/packager/internal/MavenBuildJarExtensionHandler.java
@@ -23,23 +23,31 @@ import javax.inject.Singleton;
 
 import org.xwiki.component.annotation.Component;
 import org.xwiki.extension.InstallException;
+import org.xwiki.extension.InstalledExtension;
 import org.xwiki.extension.LocalExtension;
-import org.xwiki.extension.jar.internal.handler.JarExtensionHandler;
+import org.xwiki.extension.UninstallException;
+import org.xwiki.extension.handler.internal.AbstractExtensionHandler;
 import org.xwiki.job.Request;
 
 /**
- * Override {@link JarExtensionHandler} to skip the actual JAR/Components registration which does not make sense in a
- * Maven build (assuming the install job is used to generate a database).
+ * JAR handler doing nothing. Just here so that JAR extension are not failing.
  * 
  * @version $Id$
  * @since 9.5RC1
  */
-@Component(hints = { JarExtensionHandler.JAR, JarExtensionHandler.WEBJAR })
+@Component(hints = { "jar", "webjar" })
 @Singleton
-public class MavenBuildJarExtensionHandler extends JarExtensionHandler
+public class MavenBuildJarExtensionHandler extends AbstractExtensionHandler
 {
     @Override
     public void install(LocalExtension localExtension, String namespace, Request request) throws InstallException
+    {
+        // Do nothing
+    }
+
+    @Override
+    public void uninstall(InstalledExtension localExtension, String namespace, Request request)
+        throws UninstallException
     {
         // Do nothing
     }

eb66f7483aa22425cb722f1613f6590ec5959a6c
Sergiu Dumitriu <sergiu@phenotips.org>
2017-08-10 10:58:32 -0400

    [misc] Fixed typo

--- xwiki-platform-core/xwiki-platform-activeinstalls/xwiki-platform-activeinstalls-client/xwiki-platform-activeinstalls-client-api/src/main/java/org/xwiki/activeinstalls/internal/client/ActiveInstallsInitializerListener.java
+++ xwiki-platform-core/xwiki-platform-activeinstalls/xwiki-platform-activeinstalls-client/xwiki-platform-activeinstalls-client-api/src/main/java/org/xwiki/activeinstalls/internal/client/ActiveInstallsInitializerListener.java
@@ -57,7 +57,7 @@ public class ActiveInstallsInitializerListener implements EventListener
      * Note that we use a Provider since the Observation Manager will register listeners very early in the
      * initialization process and some of the components injected transitively by the {@link InstanceIdManager}
      * implementation have initialization code that require an Execution Context to be available, which is not the case
-     * early one in XWiki's initialization since no HTTP request has been made yet...
+     * early on in XWiki's initialization since no HTTP request has been made yet...
      */
     @Inject
     private Provider<PingSender> pingSenderProvider;

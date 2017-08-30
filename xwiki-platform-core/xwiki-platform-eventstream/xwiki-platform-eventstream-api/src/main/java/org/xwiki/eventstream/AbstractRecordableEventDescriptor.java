f283b89bb612a15214550b590185e692b836d17a
Thomas Mortagne <thomas.mortagne@gmail.com>
2017-08-30 15:53:42 +0200

    [misc] User proper classes

--- xwiki-platform-core/xwiki-platform-eventstream/xwiki-platform-eventstream-api/src/main/java/org/xwiki/eventstream/AbstractRecordableEventDescriptor.java
+++ xwiki-platform-core/xwiki-platform-eventstream/xwiki-platform-eventstream-api/src/main/java/org/xwiki/eventstream/AbstractRecordableEventDescriptor.java
@@ -21,8 +21,8 @@ package org.xwiki.eventstream;
 
 import javax.inject.Inject;
 
-import org.apache.commons.lang.builder.EqualsBuilder;
-import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.xwiki.localization.ContextualLocalizationManager;
 
 /**

72ffa0ec0f8f14646fc777ff3ddc7d80cf906d53
Marius Dumitru Florea <marius@xwiki.com>
2017-10-05 16:57:37 +0300

    XWIKI-14766: solr/indexer error message when tomcat start * Fix support for nested pages in DocumentReferenceComparator

--- xwiki-platform-core/xwiki-platform-search/xwiki-platform-search-solr/xwiki-platform-search-solr-api/src/test/java/org/xwiki/search/solr/internal/job/DocumentReferenceComparatorTest.java
+++ xwiki-platform-core/xwiki-platform-search/xwiki-platform-search-solr/xwiki-platform-search-solr-api/src/test/java/org/xwiki/search/solr/internal/job/DocumentReferenceComparatorTest.java
@@ -19,12 +19,14 @@
  */
 package org.xwiki.search.solr.internal.job;
 
-import static org.junit.Assert.*;
-
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
 
 import org.junit.Test;
 import org.xwiki.model.reference.DocumentReference;
+
+import static org.junit.Assert.*;
 
 /**
  * Unit tests for {@link DocumentReferenceComparator}.
@@ -34,19 +36,19 @@ import org.xwiki.model.reference.DocumentReference;
  */
 public class DocumentReferenceComparatorTest
 {
+    private DocumentReferenceComparator comparator = new DocumentReferenceComparator();
+
     @Test
     public void compare()
     {
-        DocumentReferenceComparator comparator = new DocumentReferenceComparator();
-
         DocumentReference reference = new DocumentReference("wiki", "Space", "Page");
         assertEquals(0, comparator.compare(reference, new DocumentReference(reference)));
 
-        assertEquals(0, comparator.compare(new DocumentReference(reference, Locale.FRENCH), new DocumentReference(
-            reference, Locale.FRENCH)));
+        assertEquals(0, comparator.compare(new DocumentReference(reference, Locale.FRENCH),
+            new DocumentReference(reference, Locale.FRENCH)));
 
-        assertTrue(comparator.compare(new DocumentReference(reference, Locale.ROOT), new DocumentReference(reference,
-            Locale.GERMAN)) < 0);
+        assertTrue(comparator.compare(new DocumentReference(reference, Locale.ROOT),
+            new DocumentReference(reference, Locale.GERMAN)) < 0);
 
         assertTrue(comparator.compare(new DocumentReference(reference, Locale.ITALIAN), reference) > 0);
 
@@ -55,5 +57,29 @@ public class DocumentReferenceComparatorTest
         assertTrue(comparator.compare(reference, new DocumentReference("wiki", "XSpace", "APage")) < 0);
 
         assertTrue(comparator.compare(reference, new DocumentReference("aWiki", "ASpace", "APage")) > 0);
+    }
+
+    @Test
+    public void compareNestedPages()
+    {
+        assertTrue(compare(Arrays.asList("math", "Path", "To", "Page"), Arrays.asList("math", "Path", "Page")) > 0);
+        assertTrue(compare(Arrays.asList("math", "Path", "Alice"), Arrays.asList("math", "Path", "To", "Bob")) < 0);
+        assertTrue(
+            compare(Arrays.asList("math", "Users", "Alice", "Files"), Arrays.asList("math", "Users", "Alice")) > 0);
+        assertTrue(
+            compare(Arrays.asList("math", "Users", "Alice"), Arrays.asList("math", "Users", "Alice", "Files")) < 0);
+        assertEquals(0, compare(Arrays.asList("math", "Users", "Alice", "Files"),
+            Arrays.asList("math", "Users", "Alice", "Files")));
+    }
+
+    private int compare(List<String> alice, List<String> bob)
+    {
+        assertTrue("Invalid document reference!", alice.size() >= 3);
+        assertTrue("Invalid document reference!", bob.size() >= 3);
+        DocumentReference aliceReference =
+            new DocumentReference(alice.get(0), alice.subList(1, alice.size() - 1), alice.get(alice.size() - 1));
+        DocumentReference bobReference =
+            new DocumentReference(bob.get(0), bob.subList(1, bob.size() - 1), bob.get(bob.size() - 1));
+        return this.comparator.compare(aliceReference, bobReference);
     }
 }

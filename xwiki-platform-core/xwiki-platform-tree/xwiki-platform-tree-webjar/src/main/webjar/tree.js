83a86959f6218e0ec6b468f4c2ff7699e6fe2c11
Marius Dumitru Florea <marius@xwiki.com>
2017-11-16 17:26:29 +0200

    XWIKI-14854: Navigation tree is not showing nested pages with trailing space in link * Escape the non breaking space when computing the CSS selector
    (cherry picked from commit b1c9e968ff8bba75e49851761fc7844aeaa6bcc2)

--- xwiki-platform-core/xwiki-platform-tree/xwiki-platform-tree-webjar/src/main/webjar/tree.js
+++ xwiki-platform-core/xwiki-platform-tree/xwiki-platform-tree-webjar/src/main/webjar/tree.js
@@ -6,9 +6,9 @@ define(['jquery', 'JobRunner', 'jsTree', 'tree-finder'], function($, JobRunner) 
 
   // Fix the regular expression used by jsTree to escape special characters in CSS selectors. It is mainly used to be
   // able to find a tree node by its id using Element#querySelector. We overwrite the default value used by jsTree in
-  // order to add the following special characters: ?`.
-  // TODO: Drop this after https://github.com/vakata/jstree/pull/914/ is merged.
-  $.jstree.idregex = /[\\:&!^|()\[\]<>@*'+~#";.,=\- \/${}%?`]/g;
+  // order to add the following special characters:
+  // * \s (any white space character, such as non breaking space, not just the plain space)
+  $.jstree.idregex = /[\\:&!^|()\[\]<>@*'+~#";.,=\-\s\/${}%?`]/g;
 
   var formToken = $('meta[name=form_token]').attr('content');
 

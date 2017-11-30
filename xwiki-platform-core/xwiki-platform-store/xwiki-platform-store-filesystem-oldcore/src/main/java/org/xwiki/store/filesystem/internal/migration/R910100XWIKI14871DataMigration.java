c47186dcfd839cf5e18119b898e1a3178c5dbf3a
Thomas Mortagne <thomas.mortagne@gmail.com>
2017-11-30 17:00:51 +0100

    [misc] Remove unused import

--- xwiki-platform-core/xwiki-platform-store/xwiki-platform-store-filesystem-oldcore/src/main/java/org/xwiki/store/filesystem/internal/migration/R910100XWIKI14871DataMigration.java
+++ xwiki-platform-core/xwiki-platform-store/xwiki-platform-store-filesystem-oldcore/src/main/java/org/xwiki/store/filesystem/internal/migration/R910100XWIKI14871DataMigration.java
@@ -25,7 +25,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
-import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
 import javax.inject.Inject;

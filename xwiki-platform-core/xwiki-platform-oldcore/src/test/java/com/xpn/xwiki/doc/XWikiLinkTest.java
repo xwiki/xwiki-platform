de2cc336f240e8914dce3b516924cfeee216e271
gonzaloUr <gonzalohugouran@gmail.com>
2018-01-02 16:51:43 -0300

    XWikiLink equals

---
+++ xwiki-platform-core/xwiki-platform-oldcore/src/test/java/com/xpn/xwiki/doc/XWikiLinkTest.java
@@ -0,0 +1,61 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package com.xpn.xwiki.doc;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.xpn.xwiki.XWikiList;
+
+/**
+ * Unit tests for the {@link XWikiList} class.
+ *
+ * @version $Id$
+ */
+public class XWikiListTest
+{
+
+    @Test
+    public void testXWikiListEquals()
+    {
+        final long docId = 101;
+        final String link = "test link";
+        final String fullName = "test fullName";
+
+        XWikiList l1 = new XWikiList(docId, link, fullName);
+        XWikiList l2 = new XWikiList(docId, link, fullName);
+        XWikiList l3 = new XWikiList(docId, link, fullName);
+
+        // Reflexive
+        Assert.assertEquals(l1, l1);
+
+        // Symmetric
+        Assert.assertEquals(l1, l2);
+        Assert.assertEquals(l2, l1);
+
+        // Transitive
+        Assert.assertEquals(l1, l2);
+        Assert.assertEquals(l2, l3);
+        Assert.assertEquals(l1, l3);
+
+        // equals null == false
+        Assert.assertFalse(l1.equals(null));
+    }
+}

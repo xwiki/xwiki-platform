e9d134163f532007d52c7fe84e4960b5519be0f3
akshitdewan <akshit.dewan@yahoo.com>
2018-01-02 20:23:31 -0500

    make method void

--- xwiki-platform-core/xwiki-platform-mail/xwiki-platform-mail-send/xwiki-platform-mail-send-default/src/main/java/org/xwiki/mail/script/ScriptMimeMessage.java
+++ xwiki-platform-core/xwiki-platform-mail/xwiki-platform-mail-send/xwiki-platform-mail-send-default/src/main/java/org/xwiki/mail/script/ScriptMimeMessage.java
@@ -84,13 +84,11 @@ public class ScriptMimeMessage extends ExtendedMimeMessage
     /**
      * Add some content to the mail to be sent. Can be called several times to add different content type to the mail.
      *
-     * @return the Mime Body Part object that was added. Returning it allows script to make modifications to that body
-     *         part after it's been set (get/set some headers, etc)
      * @param bodyPart a {@link BodyPart} object to include in the mail
      * @since 10.0-SNAPSHOT
      */
     @Unstable
-    public BodyPart addPart(BodyPart bodyPart)
+    public void addPart(BodyPart bodyPart)
     {
         return addPart(null, bodyPart);
     }

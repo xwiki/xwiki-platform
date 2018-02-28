b662ef34643fcbcca62624a6bc0f5e3444af89f9
Thomas Mortagne <thomas.mortagne@gmail.com>
2018-02-28 17:49:21 +0100

    XWIKI-15079: Include macro cannot manipulate xobject and attachment when the target document is translated

--- xwiki-platform-core/xwiki-platform-rendering/xwiki-platform-rendering-macros/xwiki-platform-rendering-macro-include/src/test/java/org/xwiki/rendering/internal/macro/IncludeMacroTest.java
+++ xwiki-platform-core/xwiki-platform-rendering/xwiki-platform-rendering-macros/xwiki-platform-rendering-macro-include/src/test/java/org/xwiki/rendering/internal/macro/IncludeMacroTest.java
@@ -282,7 +282,7 @@ public class IncludeMacroTest extends AbstractComponentTestCase
 
                 allowing(mockSetup.bridge).isDocumentViewable(with(any(DocumentReference.class)));
                 will(returnValue(true));
-                allowing(mockSetup.bridge).getDocument(with(any(DocumentReference.class)));
+                allowing(mockSetup.bridge).getDocumentInstance(with(any(DocumentReference.class)));
                 will(returnValue(null));
 
                 allowing(mockDocumentDisplayer).display(with(same((DocumentModelBridge) null)),

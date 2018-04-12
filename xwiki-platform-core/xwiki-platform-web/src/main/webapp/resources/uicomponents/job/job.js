ac865c37ea378fd32841b174d138a8f9d52564b6
Thomas Mortagne <thomas.mortagne@gmail.com>
2018-04-12 18:49:19 +0200

    XWIKI-15187: Copy progress is broken * tell yui compressor to not remove the comment with the Velocity code when minifying the JavaScript

--- xwiki-platform-core/xwiki-platform-web/src/main/webapp/resources/uicomponents/job/job.js
+++ xwiki-platform-core/xwiki-platform-web/src/main/webapp/resources/uicomponents/job/job.js
@@ -17,7 +17,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-/*
+/*!
 #set ($jsExtension = '.min')
 #if (!$services.debug.minify)
   #set ($jsExtension = '')

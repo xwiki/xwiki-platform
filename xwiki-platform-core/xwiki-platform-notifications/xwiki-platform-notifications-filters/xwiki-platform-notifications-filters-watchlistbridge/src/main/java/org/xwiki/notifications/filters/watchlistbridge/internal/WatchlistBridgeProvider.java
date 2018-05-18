05ee3d99504a56fc73e0f57f738b10a73f72bc9d
Vincent Massol <vincent@massol.net>
2018-05-18 23:44:23 +0200

    [Misc] Class wrongly made public! (And failing revapi as a consequence)

--- xwiki-platform-core/xwiki-platform-notifications/xwiki-platform-notifications-filters/xwiki-platform-notifications-filters-watchlistbridge/src/main/java/org/xwiki/notifications/filters/watchlistbridge/internal/WatchlistBridgeProvider.java
+++ xwiki-platform-core/xwiki-platform-notifications/xwiki-platform-notifications-filters/xwiki-platform-notifications-filters-watchlistbridge/src/main/java/org/xwiki/notifications/filters/watchlistbridge/internal/WatchlistBridgeProvider.java
@@ -17,7 +17,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.xwiki.notifications.filters.watchlistbridge;
+package org.xwiki.notifications.filters.watchlistbridge.internal;
 
 import java.util.Collections;
 import java.util.Date;

446eb4912232c6a9bb39a27f25472ec4257a8f9b
Guillaume Delhumeau <guillaume.delhumeau@xwiki.com>
2018-06-04 17:52:02 +0200

    XWIKI-15253: Handle user messages in the notifications.

--- xwiki-platform-core/xwiki-platform-messagestream/xwiki-platform-messagestream-api/src/main/java/org/xwiki/messagestream/internal/PersonalMessageStreamNotificationFilter.java
+++ xwiki-platform-core/xwiki-platform-messagestream/xwiki-platform-messagestream-api/src/main/java/org/xwiki/messagestream/internal/PersonalMessageStreamNotificationFilter.java
@@ -19,28 +19,25 @@
  */
 package org.xwiki.messagestream.internal;
 
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 
+import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
 
 import org.springframework.stereotype.Component;
 import org.xwiki.eventstream.Event;
-import org.xwiki.messagestream.DirectMessageDescriptor;
 import org.xwiki.messagestream.PersonalMessageDescriptor;
 import org.xwiki.model.reference.DocumentReference;
+import org.xwiki.model.reference.EntityReferenceSerializer;
 import org.xwiki.notifications.NotificationFormat;
 import org.xwiki.notifications.filters.NotificationFilter;
 import org.xwiki.notifications.filters.NotificationFilterPreference;
 import org.xwiki.notifications.filters.NotificationFilterType;
-import org.xwiki.notifications.filters.expression.EventProperty;
 import org.xwiki.notifications.filters.expression.ExpressionNode;
+import org.xwiki.notifications.filters.internal.user.EventUserFilterPreferencesGetter;
 import org.xwiki.notifications.preferences.NotificationPreference;
 import org.xwiki.notifications.preferences.NotificationPreferenceProperty;
-
-import static org.xwiki.notifications.filters.expression.generics.ExpressionBuilder.value;
 
 /**
  * Filter that make sure a message from the message stream is visible by the current user.
@@ -51,41 +48,34 @@ import static org.xwiki.notifications.filters.expression.generics.ExpressionBuil
  */
 @Component
 @Singleton
-@Named("MessageStreamNotificationFilter")
-public class MessageStreamNotificationFilter implements NotificationFilter
+@Named("PersonalMessageStreamNotificationFilter")
+public class PersonalMessageStreamNotificationFilter implements NotificationFilter
 {
-    private static final List<String> SUPPORTED_EVENT_TYPES = Arrays.asList(DirectMessageDescriptor.EVENT_TYPE,
-            PersonalMessageDescriptor.EVENT_TYPE);
+    @Inject
+    private EntityReferenceSerializer<String> serializer;
+
+    @Inject
+    private EventUserFilterPreferencesGetter preferencesGetter;
 
     @Override
     public FilterPolicy filterEvent(Event event, DocumentReference user,
             Collection<NotificationFilterPreference> filterPreferences, NotificationFormat format)
     {
-        if (DirectMessageDescriptor.EVENT_TYPE.equals(event.getType()) && !user.equals(event.getUser())) {
-            return FilterPolicy.FILTER;
-        }
-        // TODO
-        // Messages to the followers.
-        // Case 1: the sender of the message is followed by the current user
-        // Case 2: the notifications macro is used to displayed all events of the sender of this message
-
-        return FilterPolicy.NO_EFFECT;
+        String sender = serializer.serialize(event.getUser());
+        return preferencesGetter.isUsedFollowed(sender, filterPreferences, format) ? FilterPolicy.KEEP
+                : FilterPolicy.FILTER;
     }
 
     @Override
     public boolean matchesPreference(NotificationPreference preference)
     {
-        return SUPPORTED_EVENT_TYPES.contains(getEventType(preference));
+        return PersonalMessageDescriptor.EVENT_TYPE.equals(getEventType(preference));
     }
 
     @Override
     public ExpressionNode filterExpression(DocumentReference user,
             Collection<NotificationFilterPreference> filterPreferences, NotificationPreference preference)
     {
-        String eventType = getEventType(preference);
-        if (DirectMessageDescriptor.EVENT_TYPE.equals(eventType)) {
-            return value(EventProperty.STREAM).eq(value(user));
-        }
         return null;
     }
 
@@ -105,6 +95,6 @@ public class MessageStreamNotificationFilter implements NotificationFilter
     @Override
     public String getName()
     {
-        return "Message Stream Notification Filter";
+        return "Personal Message Stream Notification Filter";
     }
 }

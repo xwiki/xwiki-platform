54cb9ac511f13b730d6263fdf82ead2b549b9a5b
Adel Atallah <2213999+atallahade@users.noreply.github.com>
2018-06-27 17:17:49 +0200

    XWIKI-15398: Move WikiMacroBindingInitializer as a public interface
    * Move the WikiMacroBindingInitializer class out of the internal package.
    * Change the related imports.
    * Use the WikiMacro as parameter of the initialize method instead
      of its DocumentReference.

--- xwiki-platform-core/xwiki-platform-rendering/xwiki-platform-rendering-wikimacro/xwiki-platform-rendering-wikimacro-api/src/main/java/org/xwiki/rendering/macro/wikibridge/WikiMacroBindingInitializer.java
+++ xwiki-platform-core/xwiki-platform-rendering/xwiki-platform-rendering-wikimacro/xwiki-platform-rendering-wikimacro-api/src/main/java/org/xwiki/rendering/macro/wikibridge/WikiMacroBindingInitializer.java
@@ -17,18 +17,16 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.xwiki.rendering.internal.macro.wikibridge;
+package org.xwiki.rendering.macro.wikibridge;
 
 import java.util.Map;
 
 import org.xwiki.component.annotation.Role;
-import org.xwiki.model.reference.DocumentReference;
-import org.xwiki.rendering.macro.wikibridge.WikiMacroParameters;
 import org.xwiki.rendering.transformation.MacroTransformationContext;
 
 /**
  * Initialize the binding provided to the script macros. Called before executing each wiki macro is executed.
- * 
+ *
  * @version $Id$
  * @since 2.5M1
  */
@@ -37,13 +35,13 @@ public interface WikiMacroBindingInitializer
 {
     /**
      * Initialize the binding provided to the script macros.
-     * 
-     * @param macroDocumentReference the reference of the document containing the wiki macro
+     *
+     * @param wikiMacro the wiki macro
      * @param parameters the parameters of the macro
      * @param macroContent the content of the macro
      * @param context the macro execution context
      * @param macroBinding the binding map to fill
      */
-    void initialize(DocumentReference macroDocumentReference, WikiMacroParameters parameters, String macroContent,
+    void initialize(WikiMacro wikiMacro, WikiMacroParameters parameters, String macroContent,
         MacroTransformationContext context, Map<String, Object> macroBinding);
 }

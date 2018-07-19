50a4713ccb2b329d08c55cb7b406c4d098404ec7
oana.lavinia <florean.lavinia@gmail.com>
2018-07-19 13:18:22 +0300

    XWIKI-15422: Make comments modals consistent

--- xwiki-platform-core/xwiki-platform-web/src/main/webapp/resources/uicomponents/viewers/comments.js
+++ xwiki-platform-core/xwiki-platform-web/src/main/webapp/resources/uicomponents/viewers/comments.js
@@ -461,16 +461,26 @@ return XWiki;
 
 require(['jquery'], function ($) {
   /**
- * Permalink: Events triggered when the permalink modal is displayed
- */
-  $(document).on('show.bs.modal','#permalinkModal', function (event) {
+   * Permalink: Events triggered while the permalink modal is displayed
+   */
+  $(document).on('show.bs.modal' , '#permalinkModal', function (event) {
     // Updating the permalink inside modal
     var modal = $(this);
-    var button = $(event.relatedTarget);
-    var permalinkValue = button.attr('href');
+    var button = $(event.relatedTarget); 
+    var permalinkValue = button.prop('href');
     modal.find('.form-control').val(permalinkValue);
-    // Going to permalink locations
-    $('#permalinkModal .btn-group .btn-primary').on('click', function() {
+  });
+  /**
+   * Permalink: Events triggered after the permalink modal is displayed
+   */
+  $(document).on('shown.bs.modal' , '#permalinkModal', function (event) {
+    var modal = $(this);
+    var button = $(event.relatedTarget); 
+    var permalinkValue = button.prop('href');
+    // Autofocus on permalink field
+    modal.find('.form-control').focus();
+    // Go to permalink location
+    modal.find('input.btn.btn-primary').click( function() {
       window.location = permalinkValue;
     });
   })

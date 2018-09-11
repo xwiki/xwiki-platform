9d04e82209c2880e2842699c79d2525cb812284f
Simon Urli <simon.urli@inria.fr>
2018-09-11 10:02:18 +0200

    Fix package of AllITs

--- xwiki-platform-core/xwiki-platform-attachment/xwiki-platform-attachment-test/xwiki-platform-attachment-test-tests/src/test/it/org/xwiki/attachment/test/ui/AllITs.java
+++ xwiki-platform-core/xwiki-platform-attachment/xwiki-platform-attachment-test/xwiki-platform-attachment-test-tests/src/test/it/org/xwiki/attachment/test/ui/AllITs.java
@@ -17,7 +17,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.xwiki.scheduler.test.ui;
+package org.xwiki.attachment.test.ui;
 
 import org.junit.runner.RunWith;
 import org.xwiki.test.ui.PageObjectSuite;

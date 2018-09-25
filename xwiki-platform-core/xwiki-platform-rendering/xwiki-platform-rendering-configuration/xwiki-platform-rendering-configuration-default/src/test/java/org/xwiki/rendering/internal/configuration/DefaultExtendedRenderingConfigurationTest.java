8fe2cd5a5b6fafc9e17b9ca42ebadbd776447ec7
Simon Urli <simon.urli@inria.fr>
2018-09-25 12:38:50 +0200

    [Misc] xwiki-platform-rendering-configuration-default Merge both tests

--- xwiki-platform-core/xwiki-platform-rendering/xwiki-platform-rendering-configuration/xwiki-platform-rendering-configuration-default/src/test/java/org/xwiki/rendering/internal/configuration/DefaultExtendedRenderingConfigurationTest.java
+++ xwiki-platform-core/xwiki-platform-rendering/xwiki-platform-rendering-configuration/xwiki-platform-rendering-configuration-default/src/test/java/org/xwiki/rendering/internal/configuration/DefaultExtendedRenderingConfigurationTest.java
@@ -94,16 +94,9 @@ public class DefaultExtendedRenderingConfigurationTest
     {
         ConfigurationSource source = componentManager.getInstance(ConfigurationSource.class);
         when(source.getProperty("rendering.imageDimensionsIncludedInImageURL", true)).thenReturn(false);
-
         assertFalse(this.defaultExtendedRenderingConfiguration.isImageDimensionsIncludedInImageURL());
-    }
 
-    @Test
-    public void isImageDimensionsIncludedInImageURL2() throws Exception
-    {
-        ConfigurationSource source = componentManager.getInstance(ConfigurationSource.class);
         when(source.getProperty("rendering.imageDimensionsIncludedInImageURL", true)).thenReturn(true);
-
         assertTrue(this.defaultExtendedRenderingConfiguration.isImageDimensionsIncludedInImageURL());
     }
 

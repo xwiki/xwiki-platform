d4f02de955dc0341da6f534cd399a97de2bc22b2
Guillaume Delhumeau <guillaume.delhumeau@xwiki.com>
2018-10-01 12:45:04 +0200

    XWIKI-15645: "Load older notifications" button can leads to http 414.
    (cherry picked from commit 6ad616bb08cbb52f0dd0598f6e578633612a5346)

--- xwiki-platform-core/xwiki-platform-notifications/xwiki-platform-notifications-rest/src/main/java/org/xwiki/notifications/rest/NotificationsResource.java
+++ xwiki-platform-core/xwiki-platform-notifications/xwiki-platform-notifications-rest/src/main/java/org/xwiki/notifications/rest/NotificationsResource.java
@@ -19,7 +19,9 @@
  */
 package org.xwiki.notifications.rest;
 
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
@@ -37,7 +39,7 @@ import org.xwiki.stability.Unstable;
 public interface NotificationsResource
 {
     /**
-     * Get notifications for the given wiki.
+     * Get notifications for the given parameters.
      * @return notifications
      * @throws Exception if an error occurs
      */
@@ -58,4 +60,35 @@ public interface NotificationsResource
             @QueryParam("displayReadEvents") String displayReadEvents,
             @QueryParam("displayReadStatus") String displayReadStatus
             ) throws Exception;
+
+    /**
+     * Get notifications matching the given parameters. The POST method is used to allow large content in the
+     * parameters. For example, the parameter "blackList" could be very long, and the associated URL with the GET method
+     * would be too long (generating HTTP 414 error).
+     * @return notifications
+     * @throws Exception if an error occurs
+     * @since 10.8RC1
+     * @since 10.8.1
+     * @since 9.11.8
+     */
+    @POST
+    default Response postNotifications(
+            @FormParam("useUserPreferences") String useUserPreferences,
+            @FormParam("userId") String userId,
+            @FormParam("untilDate") String untilDate,
+            @FormParam("blackList") String blackList,
+            @FormParam("pages") String pages,
+            @FormParam("spaces") String spaces,
+            @FormParam("wikis") String wikis,
+            @FormParam("users") String users,
+            @FormParam("count") String count,
+            @FormParam("displayOwnEvents") String displayOwnEvents,
+            @FormParam("displayMinorEvents") String displayMinorEvents,
+            @FormParam("displaySystemEvents") String displaySystemEvents,
+            @FormParam("displayReadEvents") String displayReadEvents,
+            @FormParam("displayReadStatus") String displayReadStatus
+    ) throws Exception {
+        return getNotifications(useUserPreferences, userId, untilDate, blackList, pages, spaces, wikis, users, count,
+                displayOwnEvents, displayMinorEvents, displaySystemEvents, displayReadEvents, displayReadStatus);
+    }
 }

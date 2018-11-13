e3b889276f88bea1bf8b85b4b8cffcd12b5a9112
Thomas Mortagne <thomas.mortagne@gmail.com>
2018-11-13 15:12:15 +0100

    XWIKI-15640: Allow executing UI extensions and panels asynchronously * fix mistake introduced in a previous refactoring

--- xwiki-platform-core/xwiki-platform-uiextension/xwiki-platform-uiextension-api/src/main/java/org/xwiki/uiextension/internal/AbstractWikiUIExtension.java
+++ xwiki-platform-core/xwiki-platform-uiextension/xwiki-platform-uiextension-api/src/main/java/org/xwiki/uiextension/internal/AbstractWikiUIExtension.java
@@ -125,7 +125,7 @@ public abstract class AbstractWikiUIExtension extends AbstractAsyncContentBaseOb
     {
         // We need to clone the block to avoid transforming the original and make it useless after the first
         // transformation
-        XDOM transformedBlock = this.xdom;
+        XDOM transformedBlock = this.xdom.clone();
 
         BlockAsyncRendererConfiguration executorConfiguration =
             new BlockAsyncRendererConfiguration(Arrays.asList("uix", getId()), transformedBlock);

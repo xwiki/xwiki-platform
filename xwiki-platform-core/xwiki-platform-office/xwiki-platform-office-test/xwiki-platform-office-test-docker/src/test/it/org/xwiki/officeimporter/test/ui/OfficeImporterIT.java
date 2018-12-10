a328483096bbccbac4f747d68e9bf4639d7ee79d
Simon Urli <simon.urli@xwiki.com>
2018-12-10 14:48:25 +0100

    XWIKI-15922: Migrate platform-office tests to use docker test
      * Move the test to a proper package

--- xwiki-platform-core/xwiki-platform-office/xwiki-platform-office-test/xwiki-platform-office-test-docker/src/test/it/org/xwiki/officeimporter/test/ui/OfficeImporterIT.java
+++ xwiki-platform-core/xwiki-platform-office/xwiki-platform-office-test/xwiki-platform-office-test-docker/src/test/it/org/xwiki/officeimporter/test/ui/OfficeImporterIT.java
@@ -17,7 +17,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package ui;
+package org.xwiki.officeimporter.test.ui;
 
 import java.io.File;
 import java.util.Arrays;

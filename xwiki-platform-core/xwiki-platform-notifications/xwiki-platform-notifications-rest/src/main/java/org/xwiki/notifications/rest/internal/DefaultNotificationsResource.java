1433e88d51c078bd844b03a45f79e86d0f245719
Guillaume Delhumeau <guillaume.delhumeau@xwiki.com>
2019-01-02 17:31:55 +0100

    XWIKI-15532: Notifications macro on a subwiki shows events that come from the other wikis.

--- xwiki-platform-core/xwiki-platform-notifications/xwiki-platform-notifications-rest/src/main/java/org/xwiki/notifications/rest/internal/DefaultNotificationsResource.java
+++ xwiki-platform-core/xwiki-platform-notifications/xwiki-platform-notifications-rest/src/main/java/org/xwiki/notifications/rest/internal/DefaultNotificationsResource.java
@@ -23,6 +23,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
 import java.util.stream.Collectors;
 
 import javax.inject.Inject;

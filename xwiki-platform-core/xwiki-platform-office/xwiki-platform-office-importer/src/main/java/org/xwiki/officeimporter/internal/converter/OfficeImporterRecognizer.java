c97680d01b633663919ecef5998adeabdb5e0db9
Vincent Massol <vincent@massol.net>
2019-01-03 15:31:09 +0100

    XWIKI-9074: The syntax used to import office document is not configurable * Fix checkstyle error (ClassFanOutComplexity) by introduced a new OfficeImporterRecognizer component to recognize presentation formats.

---
+++ xwiki-platform-core/xwiki-platform-office/xwiki-platform-office-importer/src/main/java/org/xwiki/officeimporter/internal/converter/OfficeImporterRecognizer.java
@@ -0,0 +1,38 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.officeimporter.internal.converter;
+
+import org.xwiki.component.annotation.Role;
+
+/**
+ * Recognizes office formats.
+ *
+ * @version $Id$
+ * @since 11.0RC1
+ */
+@Role
+public interface OfficeImporterRecognizer
+{
+    /**
+     * @param officeFileName the office file name to recognize
+     * @return true if the file name / extension represents an office presentation format
+     */
+    boolean isPresentation(String officeFileName);
+}

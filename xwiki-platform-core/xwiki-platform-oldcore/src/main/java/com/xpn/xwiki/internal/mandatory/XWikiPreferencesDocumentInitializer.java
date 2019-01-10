4bb918b0c6ffe275a31fa26d2e7a3fdcc17366c5
Adel Atallah <2213999+atallahade@users.noreply.github.com>
2019-01-10 18:46:03 +0100

    XWIKI-16025: Skins dropdown doesn't always display the existing Skin pages
    * Use an explicit query to avoid suggested skin pages from being hidden.

--- xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/internal/mandatory/XWikiPreferencesDocumentInitializer.java
+++ xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/internal/mandatory/XWikiPreferencesDocumentInitializer.java
@@ -112,8 +112,10 @@ public class XWikiPreferencesDocumentInitializer extends AbstractMandatoryClassI
         xclass.addBooleanField("authenticate_view", "Authenticated View", "yesno");
         xclass.addBooleanField("auth_active_check", "Authentication Active Check", "yesno");
 
-        xclass.addPageField("skin", "Skin", 30, false, false, "", "XWiki.XWikiSkins", ListClass.DISPLAYTYPE_INPUT,
-                true, ListClass.FREE_TEXT_ALLOWED);
+        xclass.addPageField("skin", "Skin", 30, false, false, "select doc.fullName"
+                        + " from XWikiDocument doc, BaseObject obj"
+                        + " where doc.fullName = obj.name and obj.className = 'XWiki.XWikiSkins'",
+                "", ListClass.DISPLAYTYPE_INPUT, true, ListClass.FREE_TEXT_ALLOWED);
         xclass.addDBListField("colorTheme", "Color theme",
             "select doc.fullName, doc.title from XWikiDocument as doc, BaseObject as theme "
                 + "where doc.fullName=theme.name and (theme.className='ColorThemes.ColorThemeClass' "

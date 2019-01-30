52d98d5da5d0ca0f8e92cc9cd4c6c11936426cb0
Simon Urli <simon.urli@xwiki.com>
2019-01-30 13:50:29 +0100

    XWIKI-13362: Display the page reference for a page, for users to be able to copy-paste it
      * Add the capability to copy a page reference from the information
    tab/viewer

---
+++ xwiki-platform-core/xwiki-platform-web/src/main/webapp/resources/js/xwiki/viewers/information.js
@@ -0,0 +1,53 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+require(['jquery', 'xwiki-meta'], function($, xm) {
+
+  $(document).on('click', '#button-inout', function() {
+    var button = $('#button-inout');
+    var referenceValue= $('#reference-value');
+    var localReference = xm.documentReference.relativeTo(xm.documentReference.getRoot());
+    var globalReference = xm.documentReference;
+
+    if (button.hasClass('btn-default')) {
+      referenceValue.text(globalReference);
+      button.removeClass('btn-default');
+      button.attr('title',"$services.localization.render('core.viewers.information.pageReference.localButton')");
+    } else {
+      referenceValue.text(localReference);
+      button.addClass('btn-default');
+      button.attr('title',"$services.localization.render('core.viewers.information.pageReference.globalButton')");
+    }
+  });
+
+  var copyToClipboard = function(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    var value = $(element).text();
+    $temp.val(value).select();
+    document.execCommand("copy");
+    $temp.remove();
+    return value;
+  };
+
+  $(document).on('click', '#button-paste', function() {
+    copyToClipboard('#reference-value');
+    alert("$services.localization.render('core.viewers.information.pageReference.copied')");
+  });
+});

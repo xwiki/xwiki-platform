063d8ec14ae28a8c589cb817eb41cfcc45a2247f
Simon Urli <simon.urli@xwiki.com>
2019-02-05 11:38:25 +0100

    XWIKI-15641: Display information to user when a job is waiting another to finish/timeout
      * Process the info message in JS to remove it when the blocking job is
    finished.

--- xwiki-platform-core/xwiki-platform-web/src/main/webapp/resources/uicomponents/job/job.js
+++ xwiki-platform-core/xwiki-platform-web/src/main/webapp/resources/uicomponents/job/job.js
@@ -33,6 +33,15 @@ require.config({
 require(['jquery', 'xwiki-meta', 'JobRunner'], function($, xm, JobRunner) {
   'use strict';
   var updateProgress = function(jobUI, job) {
+    if (job.state === 'NONE') {
+      jobUI.find('.ui-progress-background').css('display', 'none');
+      jobUI.find('.ui-progress-message').css('display', 'none');
+    } else {
+      jobUI.find('#state-none-hint').remove();
+      jobUI.find('.ui-progress-background').css('display', 'block');
+      jobUI.find('.ui-progress-message').css('display', 'block');
+    }
+
     var percent = Math.floor((job.progress.offset || 0) * 100);
     jobUI.find('.ui-progress-bar').css('width', percent + '%');
     var jobLog = job.log.items || [];

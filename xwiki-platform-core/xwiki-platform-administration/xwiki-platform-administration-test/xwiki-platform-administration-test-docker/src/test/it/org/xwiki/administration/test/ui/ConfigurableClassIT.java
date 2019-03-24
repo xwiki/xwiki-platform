cf4a6a62e2dbff46c07d9de4cb63a9eebe1253a2
Vincent Massol <vincent@massol.net>
2019-03-24 12:52:03 +0100

    [Misc] Drastically improve test: follow best practices, never navigate in the test fixture for performance reasons (and test readability), improve performances, fix flickering caused by the setting of some xobject property when the xproperty is not visible in the viewport.

--- xwiki-platform-core/xwiki-platform-administration/xwiki-platform-administration-test/xwiki-platform-administration-test-docker/src/test/it/org/xwiki/administration/test/ui/ConfigurableClassIT.java
+++ xwiki-platform-core/xwiki-platform-administration/xwiki-platform-administration-test/xwiki-platform-administration-test-docker/src/test/it/org/xwiki/administration/test/ui/ConfigurableClassIT.java
@@ -20,18 +20,12 @@
 package org.xwiki.administration.test.ui;
 
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Keys;
 import org.xwiki.administration.test.po.AdministrationEditPage;
+import org.xwiki.model.reference.DocumentReference;
 import org.xwiki.test.docker.junit5.TestReference;
 import org.xwiki.test.docker.junit5.UITest;
+import org.xwiki.test.docker.junit5.servletengine.ServletEngine;
 import org.xwiki.test.ui.TestUtils;
-import org.xwiki.test.ui.po.CopyOrRenameOrDeleteStatusPage;
-import org.xwiki.test.ui.po.CopyPage;
-import org.xwiki.test.ui.po.SuggestInputElement;
-import org.xwiki.test.ui.po.ViewPage;
-import org.xwiki.test.ui.po.editor.ClassEditPage;
-import org.xwiki.test.ui.po.editor.ObjectEditPage;
-import org.xwiki.test.ui.po.editor.ObjectEditPane;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -41,111 +35,55 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * @version $Id$
  * @since 11.3RC1
  */
-@UITest
+@UITest(servletEngine = ServletEngine.TOMCAT)
 public class ConfigurableClassIT
 {
     /*
-     * If a value is specified for linkPrefix, then a link is generated with linkPrefix + prettyName of the property
-     * from the configuration class.
-     * linkPrefix = "http://www.xwiki.org/bin/view/Main/"
-     * property prettyName = "WebHome"
-     * generated link should equal "http://www.xwiki.org/bin/view/Main/WebHome"
-     *
-     * Tests: XWiki.ConfigurableClass
+     * Verify that if a value is specified for the {@code linkPrefix} xproperty, then a link is generated with
+     * linkPrefix + prettyName of the property from the configuration class.
      */
     @Test
     public void labelLinkGeneration(TestUtils setup, TestReference testReference)
     {
+        // Fixture
         setup.loginAsSuperAdmin();
+        setupConfigurableApplication(setup, testReference,
+            "displayInSection", "TestSection3",
+            "heading", "Some Heading",
+            "configureGlobally", "true",
+            "configurationClass", setup.serializeReference(testReference),
+            "linkPrefix", "TheLinkPrefix");
 
-        String space = testReference.getLastSpaceReference().getName();
-        String page = testReference.getName();
-        createConfigurableApplication(setup, space, page, "TestSection3", true);
-        ObjectEditPage objectEditPage = ObjectEditPage.gotoPage(space, page);
-        ObjectEditPane objectEditPane = objectEditPage.getObjectsOfClass("XWiki.ConfigurableClass").get(0);
-        objectEditPane.setPropertyValue("linkPrefix", "TheLinkPrefix");
-        objectEditPage.clickSaveAndView();
-
+        // Test
         AdministrationEditPage administrationEditPage = AdministrationEditPage.gotoPage("TestSection3");
+
+        // Assertions
         assertTrue(administrationEditPage.hasLink("TheLinkPrefixString"));
         assertTrue(administrationEditPage.hasLink("TheLinkPrefixBoolean"));
         assertTrue(administrationEditPage.hasLink("TheLinkPrefixTextArea"));
         assertTrue(administrationEditPage.hasLink("TheLinkPrefixSelect"));
     }
 
-    /**
-     * Creates a new page with a configuration class with some simple fields<br/>
-     * then adds an object of class configurable and one of it's own class.<br/>
-     * Tests: XWiki.ConfigurableClass
-     */
-    private void createConfigurableApplication(TestUtils setup, String space, String page, String section,
-        boolean global)
+    private void setupConfigurableApplication(TestUtils setup, DocumentReference testReference,
+        Object... configurableClassProperties)
     {
-        // We have to use an existing space because the copy page form doesn't allow entering a new space.
-        String storageSpace = "Sandbox";
-        String storagePage = "CreateConfigurableApplication";
+        setup.deletePage(testReference);
 
-        if (!tryToCopyPage(setup, storageSpace, storagePage, space, page)) {
-            // Create the page with a simple configuration class.
-            setup.createPage(space, page, "Test configurable application", "Configurable App");
-            ClassEditPage classEditPage = ClassEditPage.gotoPage(space, page);
-            classEditPage.addProperty("String", "String");
-            classEditPage.addProperty("Boolean", "Boolean");
-            classEditPage.addProperty("TextArea", "TextArea");
-            // Expand the TextArea property we just added in order to set the "editor" meta property.
-            classEditPage.getPropertyEditPane("TextArea")
-                .expand()
-                .setMetaProperty("editor", "Text");
-            classEditPage.clickSaveAndContinue();
-            classEditPage.addProperty("Select", "StaticList");
+        // Create the page with a simple configuration class.
+        setup.createPage(testReference, "Test configurable application", "Configurable App");
+        setup.addClassProperty(testReference, "String", "String");
+        setup.addClassProperty(testReference, "Boolean", "Boolean");
+        setup.addClassProperty(testReference, "TextArea", "TextArea");
 
-            // Go to the object section.
-            ObjectEditPage objectEditPage = ObjectEditPage.gotoPage(space, page);
-            // Add a configurable object which points to the new class as the configuration class.
-            objectEditPage.addObject("XWiki.ConfigurableClass");
-            objectEditPage.clickSaveAndView();
+        // Set the editor to Text and the select to static list
+        setup.updateClassProperty(testReference, "TextArea_editor", "Text");
 
-            // Try to place it in the storage area.
-            tryToCopyPage(setup, space, page, storageSpace, storagePage);
-        }
+        setup.addClassProperty(testReference, "Select", "StaticList");
 
-        // Go to the object section.
-        ObjectEditPage objectEditPage = ObjectEditPage.gotoPage(space, page);
+        // Add a ConfigurableClass xobject.
+        setup.addObject(testReference, "XWiki.ConfigurableClass", configurableClassProperties);
 
-        // Add an object of the new class.
-        objectEditPage.addObject(space + "." + page);
-        ObjectEditPane objectEditPane = objectEditPage.getObjectsOfClass("XWiki.ConfigurableClass").get(0);
-        objectEditPane.setPropertyValue("displayInSection", section)
-            .setPropertyValue("heading", "Some Heading");
-
-        SuggestInputElement configurationSuggest = objectEditPane.getSuggestInput("configurationClass");
-        configurationSuggest.sendKeys(space + "." + page).waitForSuggestions().sendKeys(Keys.ENTER);
-
-        // Unfold the XWiki.ConfigurableClass object so that we can modify its properties
-        if (global) {
-            objectEditPane.setPropertyValue("configureGlobally", "true");
-        } else {
-            objectEditPane.setPropertyValue("configureGlobally", "false");
-        }
-        // We won't set linkPrefix, propertiesToShow, codeToExecute, or iconAttachment.
-
-        objectEditPage.clickSaveAndView();
-    }
-
-    /**
-     * This is used by createConfigurableApplication to store a copy of the default configurable to speed up making
-     * them.
-     */
-    public boolean tryToCopyPage(TestUtils setup, String fromSpace, String fromPage, String toSpace, String toPage)
-    {
-        ViewPage viewPage = setup.gotoPage(fromSpace, fromPage);
-        if (!viewPage.exists()) {
-            return false;
-        }
-        CopyPage copyPage = viewPage.copy();
-        copyPage.setTargetSpaceName(toSpace);
-        copyPage.setTargetPageName(toPage);
-        CopyOrRenameOrDeleteStatusPage copyOrRenameOrDeleteStatusPage = copyPage.clickCopyButton();
-        return copyOrRenameOrDeleteStatusPage.waitUntilFinished().getInfoMessage().equals("Done.");
+        // Add an xobject of the new class.
+        setup.addObject(testReference, setup.serializeReference(testReference));
     }
 }

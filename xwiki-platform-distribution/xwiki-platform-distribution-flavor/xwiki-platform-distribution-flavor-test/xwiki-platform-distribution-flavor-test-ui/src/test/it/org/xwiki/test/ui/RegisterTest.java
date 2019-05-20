321cfe262f9d7eee5a339f87119336814ffe2d4b
Simon Urli <simon.urli@xwiki.com>
2019-05-20 11:48:28 +0200

    [Misc] Fix fixture in RegisterTest.

--- xwiki-platform-distribution/xwiki-platform-distribution-flavor/xwiki-platform-distribution-flavor-test/xwiki-platform-distribution-flavor-test-ui/src/test/it/org/xwiki/test/ui/RegisterTest.java
+++ xwiki-platform-distribution/xwiki-platform-distribution-flavor/xwiki-platform-distribution-flavor-test/xwiki-platform-distribution-flavor-test-ui/src/test/it/org/xwiki/test/ui/RegisterTest.java
@@ -47,31 +47,14 @@ public class RegisterTest extends AbstractTest
     public void setUp() throws Exception
     {
         deleteUser("JohnSmith");
+        getUtil().updateObject("XWiki", "RegistrationConfig", "XWiki.Registration", 0, "liveValidation_enabled",
+            useLiveValidation());
         switchUser();
+        getUtil().recacheSecretToken();
         this.registrationPage = this.getRegistrationPage();
-
-        // Switch LiveValidation on or off as needed.
-        int x = 0;
-        while (this.registrationPage.isLiveValidationEnabled() != useLiveValidation()) {
-            AdministrationSectionPage sectionPage = new AdministrationSectionPage("Registration");
-            getDriver().get(getUtil().getURLToLoginAsAdminAndGotoPage(sectionPage.getURL()));
-            getUtil().recacheSecretToken();
-            getUtil().setDefaultCredentials(TestUtils.ADMIN_CREDENTIALS);
-            getUtil().assertOnPage(sectionPage.getURL());
-            sectionPage.getFormContainerElement().setFieldValue(By.name("XWiki.Registration_0_liveValidation_enabled"),
-                Boolean.valueOf(useLiveValidation()).toString());
-            sectionPage.clickSave();
-            if (x > 2) {
-                throw new WebDriverException("Unable to set useLiveValidation to " + useLiveValidation());
-            }
-            x++;
-            this.registrationPage = this.getRegistrationPage();
-        }
-
         // The prepareName javascript function is the cause of endless flickering
         // since it trys to suggest a username every time the field is focused.
         getDriver().executeJavascript("document.getElementById('xwikiname').onfocus = null;");
-
         this.registrationPage.fillInJohnSmithValues();
     }
 

305aa808b1d138487c772ce845d4e9a34909055e
Simon Urli <simon.urli@xwiki.com>
2019-05-21 14:04:35 +0200

    XWIKI-16412: Allow to type a custom value in a StaticListClass
      * Use freeText property instead of adding a new one.

--- xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/objects/meta/ListMetaClass.java
+++ xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/objects/meta/ListMetaClass.java
@@ -69,7 +69,6 @@ public class ListMetaClass extends PropertyMetaClass
 
         addBooleanProperty("multiSelect", "Multiple Select", false);
         addBooleanProperty("picker", "Use Suggest", true);
-        addBooleanProperty("allowCustomValues", "Allow Custom Values", false);
         addBooleanProperty("largeStorage", "Allow large strings", false);
 
         NumberClass sizeClass = new NumberClass(this);

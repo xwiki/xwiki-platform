341f200f17b0655cca3ab76794c8767d6d8a2ce3
Simon Urli <simon.urli@xwiki.com>
2019-06-06 11:21:21 +0200

    XWIKI-13366: improve /preview/ action
      * Add check in preview action

--- xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/web/PreviewAction.java
+++ xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/web/PreviewAction.java
@@ -93,6 +93,10 @@ public class PreviewAction extends EditAction
             }
             return false;
         }
+        // CSRF prevention
+        if (!csrfTokenCheck(context, true)) {
+            return false;
+        }
         return true;
     }
 

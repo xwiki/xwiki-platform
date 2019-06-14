cea8b78e598741d933254833ce7eebf2a3da39a7
Thomas Mortagne <thomas.mortagne@gmail.com>
2019-06-14 16:56:18 +0200

    XWIKI-8286: Upgrade to Hibernate 5.4.3 * fix mistake

--- xwiki-platform-core/xwiki-platform-watchlist/xwiki-platform-watchlist-api/src/main/java/org/xwiki/watchlist/internal/DefaultWatchListEventMatcher.java
+++ xwiki-platform-core/xwiki-platform-watchlist/xwiki-platform-watchlist-api/src/main/java/org/xwiki/watchlist/internal/DefaultWatchListEventMatcher.java
@@ -121,7 +121,7 @@ public class DefaultWatchListEventMatcher implements WatchListEventMatcher
         try {
             // FIXME: Watch out for memory usage here, since the list of events could be huge in some cases.
             List<ActivityEvent> rawEvents = actStream.searchEvents(
-                "act.date > ?:1 and act.type in ('" + StringUtils.join(MATCHING_EVENT_TYPES, "','") + "')", false, true,
+                "act.date > ?1 and act.type in ('" + StringUtils.join(MATCHING_EVENT_TYPES, "','") + "')", false, true,
                 0, 0, parameters, context);
 
             // If the page has been modified several times we want to display only one diff, if the page has been

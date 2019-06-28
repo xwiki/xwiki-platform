536630d1e738c3fba4bd4c4deac3bcd3b6b92a88
Simon Urli <simon.urli@xwiki.com>
2019-06-28 17:04:52 +0200

    XWIKI-16520: WikiMacro parameter class type definition not inherited by macro variable
      * Add a missing AttachmentReferenceConverter
      * Fix AttachmentSelector

--- xwiki-platform-core/xwiki-platform-oldcore/src/test/java/com/xpn/xwiki/internal/model/reference/DocumentReferenceConverterTest.java
+++ xwiki-platform-core/xwiki-platform-oldcore/src/test/java/com/xpn/xwiki/internal/model/reference/DocumentReferenceConverterTest.java
@@ -19,55 +19,60 @@
  */
 package com.xpn.xwiki.internal.model.reference;
 
+import javax.inject.Named;
+
 import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.xwiki.component.internal.ContextComponentManagerProvider;
+import org.xwiki.component.manager.ComponentLookupException;
 import org.xwiki.model.EntityType;
 import org.xwiki.model.reference.DocumentReference;
 import org.xwiki.model.reference.DocumentReferenceResolver;
 import org.xwiki.model.reference.EntityReference;
 import org.xwiki.model.reference.EntityReferenceSerializer;
 import org.xwiki.properties.ConverterManager;
-import org.xwiki.properties.converter.Converter;
 import org.xwiki.properties.internal.DefaultConverterManager;
-import org.xwiki.properties.internal.converter.ColorConverter;
 import org.xwiki.properties.internal.converter.ConvertUtilsConverter;
 import org.xwiki.properties.internal.converter.EnumConverter;
 import org.xwiki.test.annotation.ComponentList;
-import org.xwiki.test.mockito.MockitoComponentMockingRule;
+import org.xwiki.test.junit5.mockito.ComponentTest;
+import org.xwiki.test.junit5.mockito.InjectMockComponents;
+import org.xwiki.test.junit5.mockito.MockComponent;
+import org.xwiki.test.mockito.MockitoComponentManager;
 
 /**
- * Validate {@link ColorConverter} component.
+ * Validate {@link DocumentReferenceConverter} component.
  * 
  * @version $Id$
  */
+@ComponentTest
 @ComponentList(value = { DefaultConverterManager.class, ContextComponentManagerProvider.class, EnumConverter.class,
 ConvertUtilsConverter.class })
 public class DocumentReferenceConverterTest
 {
-    @Rule
-    public MockitoComponentMockingRule<Converter<DocumentReference>> mocker =
-        new MockitoComponentMockingRule<Converter<DocumentReference>>(DocumentReferenceConverter.class);
+    @InjectMockComponents
+    private DocumentReferenceConverter documentReferenceConverter;
 
     private ConverterManager converterManager;
 
+    @MockComponent
+    @Named("current")
     private DocumentReferenceResolver<String> mockStringResolver;
 
+    @MockComponent
+    @Named("currentgetdocument")
     private DocumentReferenceResolver<EntityReference> mockReferenceResolver;
 
+    @MockComponent
+    @Named("compact")
     private EntityReferenceSerializer<String> mockSerialier;
 
-    @Before
-    public void setUp() throws Exception
+    @BeforeEach
+    public void setup(MockitoComponentManager componentManager) throws ComponentLookupException
     {
-        this.converterManager = mocker.getInstance(ConverterManager.class);
-
-        this.mockStringResolver = this.mocker.getInstance(DocumentReferenceResolver.TYPE_STRING, "current");
-        this.mockReferenceResolver = this.mocker.getInstance(DocumentReferenceResolver.TYPE_REFERENCE, "currentgetdocument");
-        this.mockSerialier = this.mocker.getInstance(EntityReferenceSerializer.TYPE_STRING, "compact");
+        this.converterManager = componentManager.getInstance(ConverterManager.class);
     }
 
     @Test

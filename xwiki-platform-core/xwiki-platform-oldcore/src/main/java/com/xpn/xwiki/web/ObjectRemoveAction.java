1301b74401eeee22f91e4e8990f19a65e4b650f4
Thomas Mortagne <thomas.mortagne@gmail.com>
2019-07-25 13:45:18 +0200

    XWIKI-13631: A user with EDIT rights can assign to himself the SCRIPT right even if it is denied at a higher level XWIKI-16266: Privilege escalation
    (cherry picked from commit aade985a42449e68ceb78bc84701ea57cb2a5e28)

--- xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/web/ObjectRemoveAction.java
+++ xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/web/ObjectRemoveAction.java
@@ -71,6 +71,12 @@ public class ObjectRemoveAction extends XWikiAction
         XWikiResponse response = context.getResponse();
         String username = context.getUser();
         XWikiDocument doc = context.getDoc();
+
+        // We need to clone this document first, since a cached storage would return the same object for the
+        // following requests, so concurrent request might get a partially modified object, or worse, if an error
+        // occurs during the save, the cached object will not reflect the actual document at all.
+        doc = doc.clone();
+
         BaseObject obj = getObject(doc, context);
         if (obj == null) {
             return true;

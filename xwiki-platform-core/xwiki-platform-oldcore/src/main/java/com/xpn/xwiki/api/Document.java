eff4914b855e761e15ed9532db4f0aff378387db
Thomas Mortagne <thomas.mortagne@gmail.com>
2019-07-29 19:36:51 +0200

    XWIKI-13631: A user with EDIT rights can assign to himself the SCRIPT right even if it is denied at a higher level XWIKI-16266: Privilege escalation * on't check the right in case of Document#saveWithProgrammingRights
    (cherry picked from commit ff48a34a488fab1a6404acf4a905a86a2b47d22c)

--- xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/api/Document.java
+++ xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/api/Document.java
@@ -2472,7 +2472,7 @@ public class Document extends Api
                 context.setWikiId(getWiki());
 
                 if (!context.getWiki().isReadOnly()) {
-                    saveDocument(comment, minorEdit);
+                    saveDocument(comment, minorEdit, false);
                 } else {
                     java.lang.Object[] args =
                         { getDefaultEntityReferenceSerializer().serialize(getDocumentReference()), getWiki() };
@@ -2550,6 +2550,11 @@ public class Document extends Api
 
     protected void saveDocument(String comment, boolean minorEdit) throws XWikiException
     {
+        saveDocument(comment, minorEdit, true);
+    }
+
+    private void saveDocument(String comment, boolean minorEdit, boolean checkSaving) throws XWikiException
+    {
         XWikiDocument doc = getDoc();
 
         DocumentReference currentUserReference = getXWikiContext().getUserReference();
@@ -2560,9 +2565,11 @@ public class Document extends Api
             doc.setCreatorReference(currentUserReference);
         }
 
-        // Make sure the user is allowed to make this modification
-        getXWikiContext().getWiki().checkSavingDocument(doc.getAuthorReference(), doc, comment, minorEdit,
-            getXWikiContext());
+        if (checkSaving) {
+            // Make sure the user is allowed to make this modification
+            getXWikiContext().getWiki().checkSavingDocument(doc.getAuthorReference(), doc, comment, minorEdit,
+                getXWikiContext());
+        }
 
         getXWikiContext().getWiki().saveDocument(doc, comment, minorEdit, getXWikiContext());
         this.initialDoc = this.doc;

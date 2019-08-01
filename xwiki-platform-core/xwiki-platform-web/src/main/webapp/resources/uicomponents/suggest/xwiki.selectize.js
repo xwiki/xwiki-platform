67f027450f0a3d914ffbc380440d5dc0913ee2d7
oana.lavinia <florean.lavinia@gmail.com>
2019-08-01 17:10:16 +0300

    XWIKI-16147: Livetable suggest input field has no title * set title of the input field from customize method since there is no default way of doing it using selectize.js library

--- xwiki-platform-core/xwiki-platform-web/src/main/webapp/resources/uicomponents/suggest/xwiki.selectize.js
+++ xwiki-platform-core/xwiki-platform-web/src/main/webapp/resources/uicomponents/suggest/xwiki.selectize.js
@@ -205,6 +205,11 @@ define('xwiki-selectize', ['jquery', 'selectize', 'xwiki-events-bridge'], functi
     if (this.selectize.settings.takeInputWidth) {
       this.selectize['$control'].width($(this).data('initialWidth'));
     }
+    // Set the title of the input field if it does exists in the select.
+    let inputTitle;
+    if (inputTitle = $(this).attr('title')) {
+      this.selectize.$control_input.attr('title', inputTitle);
+    }
   };
 
   var setDropDownAlignment = function(selectize) {

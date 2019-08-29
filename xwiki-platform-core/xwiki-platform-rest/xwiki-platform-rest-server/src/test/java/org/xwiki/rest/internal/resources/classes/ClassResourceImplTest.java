c5402e31bae2f3d3fd142d5f2e2168eba07bdfd1
Simon Urli <simon.urli@xwiki.com>
2019-08-29 15:28:04 +0200

    XWIKI-16675 Improve check for REST API involving classes XWIKI-16676 Same for objects

--- xwiki-platform-core/xwiki-platform-rest/xwiki-platform-rest-server/src/test/java/org/xwiki/rest/internal/resources/classes/ClassResourceImplTest.java
+++ xwiki-platform-core/xwiki-platform-rest/xwiki-platform-rest-server/src/test/java/org/xwiki/rest/internal/resources/classes/ClassResourceImplTest.java
@@ -20,11 +20,13 @@
 package org.xwiki.rest.internal.resources.classes;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import javax.inject.Provider;
 import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -34,15 +36,11 @@ import org.xwiki.component.manager.ComponentManager;
 import org.xwiki.component.util.ReflectionUtils;
 import org.xwiki.context.Execution;
 import org.xwiki.context.ExecutionContext;
-import org.xwiki.model.EntityType;
-import org.xwiki.model.reference.ClassPropertyReference;
 import org.xwiki.model.reference.DocumentReference;
 import org.xwiki.model.reference.DocumentReferenceResolver;
-import org.xwiki.rest.Relations;
+import org.xwiki.rest.XWikiRestException;
 import org.xwiki.rest.internal.ModelFactory;
-import org.xwiki.rest.model.jaxb.Link;
-import org.xwiki.rest.model.jaxb.PropertyValues;
-import org.xwiki.rest.resources.classes.ClassPropertyValuesProvider;
+import org.xwiki.rest.model.jaxb.Class;
 import org.xwiki.security.authorization.ContextualAuthorizationManager;
 import org.xwiki.security.authorization.Right;
 import org.xwiki.test.annotation.BeforeComponent;
@@ -56,24 +54,28 @@ import com.xpn.xwiki.XWiki;
 import com.xpn.xwiki.XWikiContext;
 import com.xpn.xwiki.doc.XWikiDocument;
 import com.xpn.xwiki.objects.classes.BaseClass;
-import com.xpn.xwiki.objects.classes.DBListClass;
+import com.xpn.xwiki.web.Utils;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.fail;
-import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
- * Unit tests for {@link ClassPropertyValuesResourceImpl}.
- * 
+ * Unit test for {@link ClassResourceImpl}
+ *
  * @version $Id$
- * @since 9.8RC1
+ * @since 10.11.10
+ * @since 11.3.4
+ * @since 11.8RC1
  */
 @ComponentTest
-public class ClassPropertyValuesResourceImplTest
+public class ClassResourceImplTest
 {
     @InjectMockComponents
-    private ClassPropertyValuesResourceImpl resource;
+    private ClassResourceImpl resource;
 
     @InjectComponentManager
     private MockitoComponentManager componentManager;
@@ -87,11 +89,20 @@ public class ClassPropertyValuesResourceImplTest
     @MockComponent
     private Provider<XWikiContext> xcontextProvider;
 
-    private ClassPropertyReference propertyReference =
-        new ClassPropertyReference("status", new DocumentReference("wiki", Arrays.asList("Path", "To"), "Class"));
+    @MockComponent
+    private ModelFactory modelFactory;
 
     @Mock
-    private BaseClass xclass;
+    private XWiki xWiki;
+
+    private List<String> availableClasses = Arrays.asList("Foo.Class1", "XWiki.User", "XWiki.Protected", "Bar.Other");
+    private List<DocumentReference> documentReferences = Arrays.asList(
+        new DocumentReference("xwiki", "Foo", "Class1"),
+        new DocumentReference("xwiki", "XWiki", "User"),
+        new DocumentReference("xwiki", "XWiki", "Protected"),
+        new DocumentReference("xwiki", "Bar", "Other")
+    );
+    private List<Class> restClasses;
 
     XWikiContext xcontext;
 
@@ -104,68 +115,59 @@ public class ClassPropertyValuesResourceImplTest
         Execution execution = componentManager.registerMockComponent(Execution.class);
         when(execution.getContext()).thenReturn(executionContext);
         componentManager.registerComponent(ComponentManager.class, "context", componentManager);
+        Utils.setComponentManager(componentManager);
     }
 
     @BeforeEach
     public void configure() throws Exception
     {
         when(xcontextProvider.get()).thenReturn(xcontext);
-        when(this.resolver.resolve("Path.To.Class", propertyReference.extractReference(EntityType.WIKI)))
-            .thenReturn((DocumentReference) propertyReference.getParent());
-
-        XWiki xwiki = mock(XWiki.class);
-        XWikiDocument classDocument = mock(XWikiDocument.class);
-        when(xcontext.getWiki()).thenReturn(xwiki);
-        when(xwiki.getDocument(propertyReference, xcontext)).thenReturn(classDocument);
-        when(classDocument.getXClass()).thenReturn(this.xclass);
-
+        when(xcontext.getWiki()).thenReturn(xWiki);
+        when(xWiki.getClassList(xcontext)).thenReturn(availableClasses);
         UriInfo uriInfo = mock(UriInfo.class);
         when(uriInfo.getBaseUri()).thenReturn(new URI("/xwiki/rest"));
         ReflectionUtils.setFieldValue(resource, "uriInfo", uriInfo);
-    }
+        when(authorization.hasAccess(eq(Right.VIEW), any())).thenReturn(true);
 
-    @Test
-    public void getClassPropertyValuesUnauthorized() throws Exception
-    {
-        try {
-            this.resource.getClassPropertyValues("wiki", "Path.To.Class", "status", 6, Arrays.asList("text"), false);
-            fail();
-        } catch (WebApplicationException expected) {
-            assertEquals(Status.UNAUTHORIZED.getStatusCode(), expected.getResponse().getStatus());
+        this.restClasses = new ArrayList<>();
+        for (int i = 0; i < availableClasses.size(); i++) {
+            when(resolver.resolve(eq(availableClasses.get(i)), any())).thenReturn(documentReferences.get(i));
+            XWikiDocument doc = mock(XWikiDocument.class);
+            BaseClass baseClass = mock(BaseClass.class);
+            Class zeclass = mock(Class.class);
+            when(xWiki.getDocument(documentReferences.get(i), xcontext)).thenReturn(doc);
+            when(doc.getXClass()).thenReturn(baseClass);
+            when(modelFactory.toRestClass(any(), eq(new com.xpn.xwiki.api.Class(baseClass, xcontext))))
+                .thenReturn(zeclass);
+            when(zeclass.getId()).thenReturn(availableClasses.get(i));
+            restClasses.add(zeclass);
         }
+
+        componentManager.registerComponent(DocumentReferenceResolver.TYPE_STRING, "currentmixed", resolver);
     }
 
     @Test
-    public void getClassPropertyValuesNotFound() throws Exception
+    public void authorizedClassesOnly() throws XWikiRestException
     {
-        when(this.authorization.hasAccess(Right.VIEW, this.propertyReference)).thenReturn(true);
+        when(authorization.hasAccess(eq(Right.VIEW), eq(new DocumentReference("xwiki", "XWiki", "Protected"))))
+            .thenReturn(false);
 
-        try {
-            this.resource.getClassPropertyValues("wiki", "Path.To.Class", "status", 6, Arrays.asList("text"), false);
-            fail();
-        } catch (WebApplicationException expected) {
-            assertEquals(Status.NOT_FOUND.getStatusCode(), expected.getResponse().getStatus());
+        String protectedClass = "XWiki.Protected";
+        for (String availableClass : availableClasses) {
+            try {
+                Class aClass = resource.getClass("xwiki", availableClass);
+                if (availableClass.equals(protectedClass)) {
+                    fail();
+                } else {
+                    assertEquals(availableClass, aClass.getId());
+                }
+            } catch (WebApplicationException e) {
+                if (availableClass.equals(protectedClass)) {
+                    assertEquals(Response.Status.UNAUTHORIZED.getStatusCode(), e.getResponse().getStatus());
+                } else {
+                    throw e;
+                }
+            }
         }
-    }
-
-    @Test
-    public void getClassPropertyValues() throws Exception
-    {
-        when(this.authorization.hasAccess(Right.VIEW, this.propertyReference)).thenReturn(true);
-        when(this.xclass.get("status")).thenReturn(new DBListClass());
-
-        PropertyValues values = new PropertyValues();
-        ClassPropertyValuesProvider propertyValuesProvider = this.componentManager
-            .getInstance(ClassPropertyValuesProvider.class);
-        when(propertyValuesProvider.getValues(this.propertyReference, 6, "one", "two")).thenReturn(values);
-
-        assertSame(values,
-            this.resource.getClassPropertyValues("wiki", "Path.To.Class", "status", 6, Arrays.asList("one", "two"),
-                false));
-
-        assertEquals(1, values.getLinks().size());
-        Link propertyLink = values.getLinks().get(0);
-        assertEquals("/xwiki/rest/wikis/wiki/classes/Path.To.Class/properties/status", propertyLink.getHref());
-        assertEquals(Relations.PROPERTY, propertyLink.getRel());
     }
 }

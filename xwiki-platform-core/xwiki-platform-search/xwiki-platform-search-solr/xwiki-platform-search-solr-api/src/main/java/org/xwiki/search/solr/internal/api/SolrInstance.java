9264e720340967352d7fe1c94dd39305c9184bd8
slauriere <slauriere@xwiki.com>
2019-09-18 12:33:18 +0200

    XWIKI-16719: Add ability to retrieve a SolrDocument by its identifier - Add method "SolrDocument get(String id)" in SolrInstance - Add implementation of the method in AbstractSolrInstance

--- xwiki-platform-core/xwiki-platform-search/xwiki-platform-search-solr/xwiki-platform-search-solr-api/src/main/java/org/xwiki/search/solr/internal/api/SolrInstance.java
+++ xwiki-platform-core/xwiki-platform-search/xwiki-platform-search-solr/xwiki-platform-search-solr-api/src/main/java/org/xwiki/search/solr/internal/api/SolrInstance.java
@@ -25,6 +25,7 @@ import java.util.List;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.StreamingResponseCallback;
 import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrInputDocument;
 import org.apache.solr.common.params.SolrParams;
 import org.xwiki.component.annotation.Role;
@@ -64,6 +65,15 @@ public interface SolrInstance extends Initializable
      * @throws IOException if problems occur.
      */
     void add(List<SolrInputDocument> solrDocuments) throws SolrServerException, IOException;
+
+    /**
+     * Retrieves a {@link SolrDocument} by its id.
+     * @param id a SolrDocument id.
+     * @return SolrDocument matching the given id, if found, null otherwise.
+     * @throws IOException if problems occur.
+     * @throws SolrServerException if problems occur.
+     */
+    SolrDocument get(String id) throws IOException, SolrServerException;
 
     /**
      * Delete a single entry from the Solr index.

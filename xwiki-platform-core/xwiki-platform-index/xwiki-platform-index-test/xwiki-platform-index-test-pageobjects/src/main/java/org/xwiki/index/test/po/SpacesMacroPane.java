57918a0662bf0028eb95db64d5fa9ed4bffeaa52
Simon Urli <simon.urli@xwiki.com>
2019-10-19 12:14:49 +0200

    XCOMMONS-862: Upgrade to Selenium 3.141.59
      * Need again a workaround for https://github.com/SeleniumHQ/selenium/issues/7691

--- xwiki-platform-core/xwiki-platform-index/xwiki-platform-index-test/xwiki-platform-index-test-pageobjects/src/main/java/org/xwiki/index/test/po/SpacesMacroPane.java
+++ xwiki-platform-core/xwiki-platform-index/xwiki-platform-index-test/xwiki-platform-index-test-pageobjects/src/main/java/org/xwiki/index/test/po/SpacesMacroPane.java
@@ -59,7 +59,12 @@ public class SpacesMacroPane extends BaseElement
         this.spaceCreateFormToggleSwitch.click();
         this.spaceNameTextField.clear();
         this.spaceNameTextField.sendKeys(spaceName);
+        // The WebElement#submit method does not wait anymore,
+        // so we need to explicitely wait on the page reload.
+        // cf https://github.com/SeleniumHQ/selenium/issues/7691
+        getDriver().addPageNotYetReloadedMarker();
         this.spaceNameTextField.submit();
+        getDriver().waitUntilPageIsReloaded();
         return new CreatePagePage();
     }
 

573709042aa0f03303a08bd8437f8cc3547e5a34
Thomas Mortagne <thomas.mortagne@gmail.com>
2019-11-15 11:32:45 +0100

    XWIKI-16823: Distribution Wizard should propose to uninstall invalid extension which are no longer needed * add missing classes

---
+++ xwiki-platform-core/xwiki-platform-extension/xwiki-platform-extension-test/xwiki-platform-extension-test-pageobjects/src/main/java/org/xwiki/extension/test/po/distribution/CleanApplyDistributionStep.java
@@ -0,0 +1,45 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.extension.test.po.distribution;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+/**
+ * The Orphaned Dependencies step UI.
+ * 
+ * @version $Id$
+ * @since 11.10RC1
+ */
+public class CleanApplyDistributionStep extends AbstractDistributionPage
+{
+    @FindBy(xpath = "//input[@name='cleanapplyfinalize']")
+    private WebElement continueButton;
+
+    public CleanApplyFinalizeDistributionStep clickContinue()
+    {
+        // FIXME: workaround for https://github.com/mozilla/geckodriver/issues/1026
+        getDriver().addPageNotYetReloadedMarker();
+        this.continueButton.click();
+        getDriver().waitUntilPageIsReloaded();
+
+        return new CleanApplyFinalizeDistributionStep();
+    }
+}
---
+++ xwiki-platform-core/xwiki-platform-extension/xwiki-platform-extension-test/xwiki-platform-extension-test-pageobjects/src/main/java/org/xwiki/extension/test/po/distribution/CleanApplyFinalizeDistributionStep.java
@@ -0,0 +1,51 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.extension.test.po.distribution;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+/**
+ * The Orphaned Dependencies step UI.
+ * 
+ * @version $Id$
+ * @since 11.10RC1
+ */
+public class CleanApplyFinalizeDistributionStep extends AbstractDistributionPage
+{
+    @FindBy(xpath = "//input[@name='cleanapplyreport']")
+    private WebElement continueButton;
+
+    public void waitForUninstallComplete()
+    {
+        getDriver().waitUntilElementIsVisible(By.xpath("//input[@name='cleanapplyreport' and not(@disabled)]"));
+    }
+
+    public CleanApplyReportDistributionStep clickContinue()
+    {
+        // FIXME: workaround for https://github.com/mozilla/geckodriver/issues/1026
+        getDriver().addPageNotYetReloadedMarker();
+        this.continueButton.click();
+        getDriver().waitUntilPageIsReloaded();
+
+        return new CleanApplyReportDistributionStep();
+    }
+}

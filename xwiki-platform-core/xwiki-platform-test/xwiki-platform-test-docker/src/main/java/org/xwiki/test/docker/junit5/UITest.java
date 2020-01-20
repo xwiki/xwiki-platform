0488106d0eed4386f10f3e0d6f76bbf374d209dc
Simon Urli <simon.urli@xwiki.com>
2020-01-20 09:56:28 +0100

    [Misc] Disable temporarily VNC because of an infra issue.

--- xwiki-platform-core/xwiki-platform-test/xwiki-platform-test-docker/src/main/java/org/xwiki/test/docker/junit5/UITest.java
+++ xwiki-platform-core/xwiki-platform-test/xwiki-platform-test-docker/src/main/java/org/xwiki/test/docker/junit5/UITest.java
@@ -122,7 +122,9 @@ public @interface UITest
      * @return true if VNC container is started and recording is done and saved on test exit
      * @since 10.10RC1
      */
-    boolean vnc() default true;
+    // FIXME: Default value should be true, we temporary switch it to false because of CI infra issues:
+    // we should definitely avoid this kind of thing in the future with XWIKI-16983
+    boolean vnc() default false;
 
     /**
      * @return the list of configuration properties to use when generating the XWiki configuration files such as as

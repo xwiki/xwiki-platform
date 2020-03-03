1a3d9a6fb820cea89809b88fa21d56b789660873
oanalavinia <florean.lavinia@gmail.com>
2020-03-03 15:14:09 +0200

    XWIKI-17072: Warnings for deprecated usage of HQL ordinal parameters when applying a date filter on Deleted Attachments livetable * moved test to docker and added helper methods to TestUtils
    (cherry picked from commit d76e8c0ebfa3aba81898883c77bec71f5d1dffdc)

---
+++ xwiki-platform-core/xwiki-platform-index/xwiki-platform-index-test/xwiki-platform-index-test-docker/src/test/it/org/xwiki/index/test/ui/DeletedAttachmentsIT.java
@@ -0,0 +1,93 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.index.test.ui;
+
+import java.io.ByteArrayInputStream;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.xwiki.index.test.po.DeletedAttachmentsPage;
+import org.xwiki.test.docker.junit5.TestReference;
+import org.xwiki.test.docker.junit5.UITest;
+import org.xwiki.test.ui.TestUtils;
+import org.xwiki.test.ui.po.AttachmentsPane;
+import org.xwiki.test.ui.po.LiveTableElement;
+import org.xwiki.test.ui.po.ViewPage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Tests for the DeletedAttachments page.
+ *
+ * @since 12.2RC1
+ */
+@UITest
+public class DeletedAttachmentsIT
+{
+    private static String FIRST_LIVETABLE_COLUMN_ID = "xwiki-livetable-attachmentTrash-filter-1";
+
+    private static String THIRD_LIVETABLE_COLUMN_ID = "xwiki-livetable-attachmentTrash-filter-3";
+
+    @BeforeEach
+    public void setUp(TestUtils setup, TestReference testReference) throws Exception
+    {
+        setup.loginAsSuperAdmin();
+
+        setup.deletePage(testReference);
+        setup.createPageWithAttachment(testReference, "", "Test Attachments", "deletedFile1.txt",
+            new ByteArrayInputStream("attachment content1".getBytes()), TestUtils.SUPER_ADMIN_CREDENTIALS);
+
+        ViewPage testPage = setup.gotoPage(testReference);
+        AttachmentsPane attachmentsPane = testPage.openAttachmentsDocExtraPane();
+        attachmentsPane.deleteAttachmentByFileByName("deletedFile1.txt");
+    }   
+
+    @Test
+    public void filterLivetableByName() throws Exception
+    {
+        DeletedAttachmentsPage deletedAttachmentsPage = DeletedAttachmentsPage.gotoPage();
+        LiveTableElement livetable = deletedAttachmentsPage.getDocumentsAttachmentsLiveTable();
+
+        livetable.filterColumn(FIRST_LIVETABLE_COLUMN_ID, "deletedFile1");
+        assertEquals("deletedFile1.txt", livetable.getCell(livetable.getRow(1), 1).getText());
+    }
+
+    @Test
+    public void filterLivetableByDate() throws Exception
+    {
+        DeletedAttachmentsPage deletedAttachmentsPage = DeletedAttachmentsPage.gotoPage();
+        LiveTableElement livetable = deletedAttachmentsPage.getDocumentsAttachmentsLiveTable();
+
+        livetable.filterColumn(THIRD_LIVETABLE_COLUMN_ID, getNowDateInterval());
+        assertEquals("deletedFile1.txt", livetable.getCell(livetable.getRow(1), 1).getText());
+    }
+
+    private String getNowDateInterval()
+    {
+        Calendar cal = Calendar.getInstance();
+        Date endDate = cal.getTime();
+        cal.add(Calendar.DATE, -1);
+        Date startDate = cal.getTime();
+
+        return Long.toString(startDate.getTime()) + '-' + Long.toString(endDate.getTime());
+    }
+}

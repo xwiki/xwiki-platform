878caf56971b83c3c7ed183fc6cbb8aff45154fd
Manuel Leduc <manuel.leduc@xwiki.com>
2020-04-02 17:06:04 +0200

    XWIKI-17175: User membership in the profile - missing groups

---
+++ xwiki-platform-core/xwiki-platform-user/xwiki-platform-user-profile/xwiki-platform-user-profile-test/xwiki-platform-user-profile-test-pageobjects/src/main/java/org/xwiki/user/test/po/GroupsUserProfilePage.java
@@ -0,0 +1,61 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.user.test.po;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.xwiki.test.ui.po.LiveTableElement;
+
+/**
+ * Represents the User Profile Preferences Tab.
+ *
+ * @version $Id$
+ */
+public class GroupsUserProfilePage extends AbstractUserProfilePage
+{
+    @FindBy(id = "Huser.profile.groups.title")
+    private WebElement groupsTitle;
+    
+    // @FindBy(xpath = "//div[@id='groupsPane']//table[@class='xwiki-livetable']")
+    private LiveTableElement liveTable = new LiveTableElement("user.profile.group.table");
+
+    public static GroupsUserProfilePage gotoPage(String username)
+    {
+        getUtil().gotoPage("XWiki", username, "view", "category=groups");
+        GroupsUserProfilePage page = new GroupsUserProfilePage(username);
+        return page;
+    }
+    
+    public GroupsUserProfilePage(String username)
+    {
+        super(username);
+    }
+
+    public String getPreferencesTitle()
+    {
+        return this.groupsTitle.getText();
+    }
+
+    public LiveTableElement getGroupsPaneLiveTable()
+    {
+        this.liveTable.waitUntilReady();
+        return this.liveTable;
+    }
+}

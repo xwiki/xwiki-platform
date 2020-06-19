7fc6eaea5c97a434cb0472f808770acae8a6d312
Clément <clement.desableau@xwiki.com>
2020-06-19 12:48:48 +0200

    XWIKI-17389 Modify polyfill.js so that it passes yuicompressor

--- xwiki-platform-core/xwiki-platform-livedata/xwiki-platform-livedata-webjar/src/main/webjar/polyfills.js
+++ xwiki-platform-core/xwiki-platform-livedata/xwiki-platform-livedata-webjar/src/main/webjar/polyfills.js
@@ -1,3 +1,23 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
 
 /**
  * Polyfill for the custom event function for IE 11
@@ -22,7 +42,7 @@
 if (!Array.prototype.find) {
   Object.defineProperty(Array.prototype, 'find', {
     value: function(predicate) {
-      if (this == null) {
+      if (this === null) {
         throw TypeError('"this" is null or not defined');
       }
       var o = Object(this);
@@ -54,7 +74,7 @@ if (!Array.prototype.find) {
 if (!Array.prototype.findIndex) {
   Object.defineProperty(Array.prototype, 'findIndex', {
     value: function(predicate) {
-      if (this == null) {
+      if (this === null) {
         throw new TypeError('"this" is null or not defined');
       }
       var o = Object(this);

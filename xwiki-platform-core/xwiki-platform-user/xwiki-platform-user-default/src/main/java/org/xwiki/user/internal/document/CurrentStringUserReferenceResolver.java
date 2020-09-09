26ac2f27a2402d934708cb16ce07664b5eee33cb
Manuel Leduc <manuel.leduc@xwiki.com>
2020-09-09 18:08:23 +0200

    XWIKI-17771: UserReferenceConverter resolution is always done in the context of the main wiki

---
+++ xwiki-platform-core/xwiki-platform-user/xwiki-platform-user-default/src/main/java/org/xwiki/user/internal/document/CurrentStringUserReferenceResolver.java
@@ -0,0 +1,68 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.user.internal.document;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import org.xwiki.component.annotation.Component;
+import org.xwiki.model.reference.DocumentReference;
+import org.xwiki.model.reference.DocumentReferenceResolver;
+import org.xwiki.model.reference.EntityReferenceProvider;
+import org.xwiki.user.CurrentUserReference;
+import org.xwiki.user.UserReference;
+import org.xwiki.user.UserReferenceResolver;
+
+/**
+ * Converts a {@link String} representing a user id into a {@link UserReference}.
+ * <p>
+ * The conversion takes into account the current wiki.
+ * Thus, "XWiki.U1" will be resolved to "xwiki:XWiki.U1" on the main wiki, and to "s1:XWiki.U1" in the context of the
+ * sub-wiki s1.
+ *
+ * @version $Id$
+ * @since 12.8RC1
+ */
+@Singleton
+@Component
+@Named("current")
+public class CurrentStringUserReferenceResolver implements UserReferenceResolver<String>
+{
+    @Inject
+    private EntityReferenceProvider entityReferenceProvider;
+
+    @Inject
+    @Named("current")
+    private DocumentReferenceResolver<String> documentReferenceResolver;
+
+    @Override
+    public UserReference resolve(String rawReference, Object... parameters)
+    {
+        UserReference ret;
+        if (rawReference == null) {
+            ret = CurrentUserReference.INSTANCE;
+        } else {
+            DocumentReference documentReference = this.documentReferenceResolver.resolve(rawReference);
+            ret = new DocumentUserReference(documentReference, this.entityReferenceProvider);
+        }
+        return ret;
+    }
+}

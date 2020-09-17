57c5a2ba1f99568764cbc4b27f7f85f63935eb7e
Simon Urli <simon.urli@xwiki.com>
2020-09-17 18:06:28 +0200

    XWIKI-17819: Tons of errors in the log when migrating events from a deleted wiki
      * Ensure that the wiki of the events exists before migrated them.

--- xwiki-platform-core/xwiki-platform-eventstream/xwiki-platform-eventstream-api/src/main/java/org/xwiki/eventstream/internal/LegacyEventMigrationJob.java
+++ xwiki-platform-core/xwiki-platform-eventstream/xwiki-platform-eventstream-api/src/main/java/org/xwiki/eventstream/internal/LegacyEventMigrationJob.java
@@ -29,6 +29,7 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.xwiki.component.annotation.Component;
 import org.xwiki.component.manager.ComponentLookupException;
 import org.xwiki.eventstream.Event;
@@ -41,6 +42,8 @@ import org.xwiki.job.Request;
 import org.xwiki.query.Query;
 import org.xwiki.query.QueryException;
 import org.xwiki.query.QueryManager;
+import org.xwiki.wiki.descriptor.WikiDescriptorManager;
+import org.xwiki.wiki.manager.WikiManagerException;
 
 /**
  * Copy legacy events to the new event store.
@@ -71,6 +74,9 @@ public class LegacyEventMigrationJob
 
     @Inject
     private QueryManager queryManager;
+
+    @Inject
+    private WikiDescriptorManager wikiDescriptorManager;
 
     @Override
     protected LegacyEventMigrationRequest castRequest(Request request)
@@ -193,8 +199,17 @@ public class LegacyEventMigrationJob
         List<Event> eventsToSave = new ArrayList<>(events.size());
         for (Event event : events) {
             // TODO: optimize this a bit but there seems to be a problem with the IN clause, see previous commented code
-            if (!this.eventStore.getEvent(event.getId()).isPresent()) {
-                eventsToSave.add(event);
+            try {
+                if (!this.eventStore.getEvent(event.getId()).isPresent()
+                    // Ensure that the event concerns a wiki that still exist.
+                    && (event.getWiki() == null || this.wikiDescriptorManager.exists(event.getWiki().getName())))
+                {
+                    eventsToSave.add(event);
+                }
+            } catch (WikiManagerException e) {
+                logger.warn("Error while checking if the wiki [{}] exists. The event (id: [{}]) referencing this wiki "
+                    + "won't be migrated. Root cause: [{}].", event.getWiki().getName(), event.getId(),
+                    ExceptionUtils.getRootCauseMessage(e));
             }
         }
 

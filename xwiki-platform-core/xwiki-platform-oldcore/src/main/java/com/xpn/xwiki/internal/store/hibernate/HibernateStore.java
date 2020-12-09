d9d7e42523af0d71d322bde57afff36d6ed82ce3
Thomas Mortagne <thomas.mortagne@gmail.com>
2020-12-09 14:17:37 +0100

    Revert "XWIKI-18128: Error produced by Hibernate during tables update should be reported"
    This reverts commit a181be20f2b29588d5764c54b2ed79b735295e75.

--- xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/internal/store/hibernate/HibernateStore.java
+++ xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/internal/store/hibernate/HibernateStore.java
@@ -1079,21 +1079,7 @@ public class HibernateStore implements Disposable, Integrator, Initializable
      */
     public void updateDatabase(Metadata metadata)
     {
-        SchemaUpdate updater = new SchemaUpdate();
-        updater.execute(EnumSet.of(TargetType.DATABASE), metadata);
-
-        List<Exception> exceptions = updater.getExceptions();
-
-        if (exceptions.isEmpty()) {
-            return;
-        }
-
-        // Print the errors
-        for (Exception exception : exceptions) {
-            this.logger.error(exception.getMessage(), exception);
-        }
-
-        throw new HibernateException("Failed to update the database. See the log for all errors", exceptions.get(0));
+        new SchemaUpdate().execute(EnumSet.of(TargetType.DATABASE), metadata);
     }
 
     /**

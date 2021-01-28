e5608089ffed469f93568c2195ff47d2f9352f46
Marius Dumitru Florea <marius@xwiki.com>
2021-01-28 21:58:34 +0200

    XWIKI-18215: Suggestion not working on Database List fields * Bulletproof the text query filter to work when the FROM and WHERE clauses are on separate lines

--- xwiki-platform-core/xwiki-platform-query/xwiki-platform-query-manager/src/main/java/org/xwiki/query/internal/TextQueryFilter.java
+++ xwiki-platform-core/xwiki-platform-query/xwiki-platform-query-manager/src/main/java/org/xwiki/query/internal/TextQueryFilter.java
@@ -20,14 +20,15 @@
 package org.xwiki.query.internal;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import javax.inject.Named;
 import javax.inject.Singleton;
 
-import org.apache.commons.lang3.StringUtils;
 import org.xwiki.component.annotation.Component;
 
 /**
@@ -41,6 +42,14 @@ import org.xwiki.component.annotation.Component;
 @Singleton
 public class TextQueryFilter extends AbstractWhereQueryFilter
 {
+    private static final Pattern SELECT = Pattern.compile("^\\s*select\\s+", Pattern.CASE_INSENSITIVE);
+
+    private static final Pattern DISTINCT = Pattern.compile("^distinct\\s+", Pattern.CASE_INSENSITIVE);
+
+    private static final Pattern AS = Pattern.compile("\\s+as\\s+", Pattern.CASE_INSENSITIVE);
+
+    private static final Pattern FROM = Pattern.compile("\\s+from\\s+", Pattern.CASE_INSENSITIVE);
+
     private static final Pattern COLUMN_SEPARATOR = Pattern.compile("\\s*,\\s*");
 
     @Override
@@ -70,17 +79,24 @@ public class TextQueryFilter extends AbstractWhereQueryFilter
 
     private List<String> getFilterableColumns(String statement)
     {
+        Matcher selectMatcher = SELECT.matcher(statement);
+        Matcher fromMatcher = FROM.matcher(statement);
+
+        if (!selectMatcher.lookingAt() || !fromMatcher.find()) {
+            return Collections.emptyList();
+        }
+
         List<String> columns = new ArrayList<>();
-        String selectClause =
-            statement.substring("select ".length(), StringUtils.indexOfIgnoreCase(statement, " from ")).trim();
-        selectClause = StringUtils.removeStartIgnoreCase(selectClause, "distinct ");
+        String selectClause = statement.substring(selectMatcher.end(), fromMatcher.start());
+        // Skip 'distinct' in the select clause.
+        selectClause = DISTINCT.matcher(selectClause).replaceFirst("");
         for (String column : COLUMN_SEPARATOR.split(selectClause)) {
             // Remove the column alias.
             String columnWithoutAlias = column;
-            int aliasPosition = column.lastIndexOf(" as ");
-            if (aliasPosition > 0) {
-                columnWithoutAlias = column.substring(0, aliasPosition).trim();
-                String alias = column.substring(aliasPosition + 4).trim();
+            Matcher asMatcher = AS.matcher(column);
+            if (asMatcher.find()) {
+                columnWithoutAlias = column.substring(0, asMatcher.start());
+                String alias = column.substring(asMatcher.end());
                 if (alias.startsWith("unfilterable")) {
                     continue;
                 }

95ad1f9da8913bec02131d32a14eeb411cddca31
Manuel Leduc <manuel.leduc@xwiki.com>
2021-05-25 17:53:23 +0200

    XWIKI-18676: Missing placeholder for the first load of a LD table
    Displays a loader:
    1. in the Live Data macro: before vue and its dependencies are loaded client side
    2. after vue has replaced the placeholder provided by the Live Data macro by its own template, and before the first layout and its dependencies are loaded client side

--- xwiki-platform-core/xwiki-platform-livedata/xwiki-platform-livedata-macro/src/main/java/org/xwiki/livedata/internal/macro/LiveDataMacro.java
+++ xwiki-platform-core/xwiki-platform-livedata/xwiki-platform-livedata-macro/src/main/java/org/xwiki/livedata/internal/macro/LiveDataMacro.java
@@ -99,6 +99,13 @@ public class LiveDataMacro extends AbstractMacro<LiveDataMacroParameters>
     private SkinExtension jsfx;
 
     /**
+     * The component used to load the CSS code of the Live Data widget.
+     */
+    @Inject
+    @Named("ssfx")
+    private SkinExtension ssfx;
+
+    /**
      * Default constructor.
      */
     public LiveDataMacro()
@@ -116,9 +123,11 @@ public class LiveDataMacro extends AbstractMacro<LiveDataMacroParameters>
         // Load the JavaScript code of the Live Data widget.
         Map<String, Object> skinExtensionParameters = Collections.singletonMap("forceSkinAction", Boolean.TRUE);
         this.jsfx.use("uicomponents/widgets/liveData.js", skinExtensionParameters);
+        // Load the CSS code of the Live Data widget.
+        this.ssfx.use("uicomponents/widgets/liveData.css");
 
         GroupBlock output = new GroupBlock();
-        output.setParameter("class", "liveData");
+        output.setParameter("class", "liveData loading");
         if (parameters.getId() != null) {
             output.setParameter("id", parameters.getId());
         }

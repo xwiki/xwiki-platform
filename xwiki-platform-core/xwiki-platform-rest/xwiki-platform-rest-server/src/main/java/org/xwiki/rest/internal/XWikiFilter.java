7d2af12baab19772994dd4a41e9651613b99d17e
Thomas Mortagne <thomas.mortagne@gmail.com>
2021-06-08 11:27:00 +0200

    [misc] Spare one return

--- xwiki-platform-core/xwiki-platform-rest/xwiki-platform-rest-server/src/main/java/org/xwiki/rest/internal/XWikiFilter.java
+++ xwiki-platform-core/xwiki-platform-rest/xwiki-platform-rest-server/src/main/java/org/xwiki/rest/internal/XWikiFilter.java
@@ -59,28 +59,26 @@ public class XWikiFilter extends Filter
             (ComponentManager) getContext().getAttributes().get(Constants.XWIKI_COMPONENT_MANAGER);
         XWikiContext xwikiContext = Utils.getXWikiContext(componentManager);
 
-        EntityReferenceSerializer<String> serializer;
         try {
-            serializer = componentManager.getInstance(EntityReferenceSerializer.TYPE_STRING);
+            EntityReferenceSerializer<String> serializer =
+                componentManager.getInstance(EntityReferenceSerializer.TYPE_STRING);
+
+            /*
+             * We add headers to the response to allow applications to verify if the authentication is still valid. We
+             * are also adding the XWiki version at the same moment.
+             */
+            Series<Header> responseHeaders =
+                (Series<Header>) response.getAttributes().get(HeaderConstants.ATTRIBUTE_HEADERS);
+            if (responseHeaders == null) {
+                responseHeaders = new Series<>(Header.class);
+                response.getAttributes().put(HeaderConstants.ATTRIBUTE_HEADERS, responseHeaders);
+            }
+            responseHeaders.add("XWiki-User", serializer.serialize(xwikiContext.getUserReference()));
+            responseHeaders.add("XWiki-Version", xwikiContext.getWiki().getVersion());
         } catch (ComponentLookupException e) {
             getLogger()
                 .warning("Failed to lookup the entity reference serializer: " + ExceptionUtils.getRootCauseMessage(e));
-
-            return CONTINUE;
         }
-
-        /*
-         * We add headers to the response to allow applications to verify if the authentication is still valid. We are
-         * also adding the XWiki version at the same moment.
-         */
-        Series<Header> responseHeaders =
-            (Series<Header>) response.getAttributes().get(HeaderConstants.ATTRIBUTE_HEADERS);
-        if (responseHeaders == null) {
-            responseHeaders = new Series<>(Header.class);
-            response.getAttributes().put(HeaderConstants.ATTRIBUTE_HEADERS, responseHeaders);
-        }
-        responseHeaders.add("XWiki-User", serializer.serialize(xwikiContext.getUserReference()));
-        responseHeaders.add("XWiki-Version", xwikiContext.getWiki().getVersion());
 
         return CONTINUE;
     }

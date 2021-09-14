732339089cffc0c4eeff8749df32ed9cf304b123
Manuel Leduc <manuel.leduc@xwiki.com>
2021-09-14 10:11:12 +0200

    [Misc] Add missing components in PageComponentList
    DefaultClassLoaderManager and ExtendedURLStreamHandlerFactory after the introduction of a dependency to ClassLoaderManager in InternalSkinManager

--- xwiki-platform-core/xwiki-platform-test/xwiki-platform-test-page/src/main/java/org/xwiki/test/page/PageComponentList.java
+++ xwiki-platform-core/xwiki-platform-test/xwiki-platform-test-page/src/main/java/org/xwiki/test/page/PageComponentList.java
@@ -24,6 +24,8 @@ import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+import org.xwiki.classloader.internal.DefaultClassLoaderManager;
+import org.xwiki.classloader.internal.ExtendedURLStreamHandlerFactory;
 import org.xwiki.context.internal.DefaultExecution;
 import org.xwiki.context.internal.DefaultExecutionContextManager;
 import org.xwiki.display.internal.ConfiguredDocumentDisplayer;
@@ -78,7 +80,6 @@ import org.xwiki.script.internal.service.DefaultScriptServiceManager;
 import org.xwiki.script.internal.service.ServicesScriptContextInitializer;
 import org.xwiki.sheet.internal.DefaultSheetManager;
 import org.xwiki.sheet.internal.SheetDocumentDisplayer;
-import org.xwiki.test.TestEnvironment;
 import org.xwiki.test.annotation.ComponentList;
 import org.xwiki.velocity.internal.DefaultVelocityConfiguration;
 import org.xwiki.velocity.internal.DefaultVelocityContextFactory;
@@ -209,6 +210,8 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
     InternalSkinManager.class,
     InternalSkinConfiguration.class,
     WikiSkinUtils.class,
+    DefaultClassLoaderManager.class,
+    ExtendedURLStreamHandlerFactory.class,
 
     // Velocity Macro
     VelocityMacro.class,

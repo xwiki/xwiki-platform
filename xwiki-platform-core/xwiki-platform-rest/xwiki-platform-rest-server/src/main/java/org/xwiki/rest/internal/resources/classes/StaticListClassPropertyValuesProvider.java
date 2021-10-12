3f8e77ba02a2957d9a34f2d8be95cb7a939ced77
Michael Hamann <michael.hamann@xwiki.com>
2021-10-12 17:32:08 +0200

    XWIKI-19048: Live data list filter should display and filter by (translated) labels for static list properties
    * Change the REST server to use the view displayer to populate the
      metadata with a label
    * Extend the integration test to test that labels are displayed and
      filtering by label and translated label works.
    * Cleanup the use of references in LiveDataIT

--- xwiki-platform-core/xwiki-platform-rest/xwiki-platform-rest-server/src/main/java/org/xwiki/rest/internal/resources/classes/StaticListClassPropertyValuesProvider.java
+++ xwiki-platform-core/xwiki-platform-rest/xwiki-platform-rest-server/src/main/java/org/xwiki/rest/internal/resources/classes/StaticListClassPropertyValuesProvider.java
@@ -24,10 +24,13 @@ import java.util.List;
 import javax.inject.Named;
 import javax.inject.Singleton;
 
+import org.apache.commons.text.StringEscapeUtils;
 import org.xwiki.component.annotation.Component;
 import org.xwiki.rest.model.jaxb.PropertyValue;
 import org.xwiki.rest.model.jaxb.PropertyValues;
 
+import com.xpn.xwiki.objects.BaseObject;
+import com.xpn.xwiki.objects.StringProperty;
 import com.xpn.xwiki.objects.classes.StaticListClass;
 
 /**
@@ -49,13 +52,68 @@ public class StaticListClassPropertyValuesProvider extends AbstractListClassProp
 
     @Override
     protected PropertyValues getAllowedValues(StaticListClass propertyDefinition, int limit, String filter)
-        throws Exception
     {
         final PropertyValues result = new PropertyValues();
         List<String> allValues = StaticListClass.getListFromString(propertyDefinition.getValues());
 
-        allValues.stream().filter(s -> s.toLowerCase().contains(filter.toLowerCase())).limit(limit)
-            .forEach(item -> result.withPropertyValues(new PropertyValue(item)));
+        String lowerFilter = filter.toLowerCase();
+
+        allValues.stream().map(id -> constructPropertyValueForId(id, propertyDefinition))
+            // Filter both by key and by (translated) label (that's why we construct a PropertyValue first)
+            .filter(val -> val.getValue().toString().toLowerCase().contains(lowerFilter)
+                || (val.getMetaData().containsKey(META_DATA_LABEL)
+                && val.getMetaData().get(META_DATA_LABEL).toString().toLowerCase().contains(lowerFilter)))
+            .limit(limit).forEach((result::withPropertyValues));
+
         return result;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @since 13.10RC1
+     */
+    @Override
+    protected PropertyValue getValueFromQueryResult(Object result, StaticListClass propertyDefinition)
+    {
+        PropertyValue value = super.getValueFromQueryResult(result, propertyDefinition);
+
+        if (value != null && value.getValue() instanceof String) {
+            String stringKey = (String) value.getValue();
+
+            return constructPropertyValueForId(stringKey, propertyDefinition);
+        }
+
+        return value;
+    }
+
+    /**
+     * Constructs a {@code PropertyValue} for the given id of the given {@code StaticListClass}.
+     *
+     * @param id The id/value of the item
+     * @param propertyDefinition The definition of the property
+     * @return The result, may contain a label if it is different from the id
+     * @since 13.10RC1
+     */
+    private PropertyValue constructPropertyValueForId(String id, StaticListClass propertyDefinition)
+    {
+        PropertyValue val = new PropertyValue(id);
+
+        BaseObject xObject = new BaseObject();
+        String propertyName = propertyDefinition.getName();
+        StringProperty stringProperty = new StringProperty();
+        stringProperty.setValue(id);
+        xObject.addField(propertyName, stringProperty);
+
+        String displayValue = propertyDefinition.displayView(propertyName, xObject, this.xcontextProvider.get());
+
+        // displayView uses XML escaping, but the label should not be escaped
+        displayValue = StringEscapeUtils.unescapeXml(displayValue);
+
+        if (!id.equals(displayValue)) {
+            val.getMetaData().put(META_DATA_LABEL, displayValue);
+        }
+
+        return val;
     }
 }

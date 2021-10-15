xwiki/xwiki-platform

commit 72c1c37bbf641e385bb9eb3968895e4cfb1c044e

[Misc] Improve slightly the error messages

xwiki-platform-core/xwiki-platform-eventstream/xwiki-platform-eventstream-api/src/main/java/org/xwiki/eventstream/internal/DefaultEventStore.java
@@ -98,19 +98,21 @@ public class DefaultEventStore implements EventStore, Initializable
                     this.store =
                         this.componentManager.getInstance(EventStore.class, this.configuration.getEventStore());
                 } catch (ComponentLookupException e) {
-                    throw new InitializationException("Failed to get the configured store", e);
+                    throw new InitializationException(
+                        String.format("Failed to get the configured event store [%s]", hint, e));
                 }
             }
         }
 
-        // retro compatibility: make sure to synchronize the old storage until the new store covers everything we
+        // Retro compatibility: make sure to synchronize the old storage until the new store covers everything we
         // want it to cover
         String legacyHint = this.store != null ? "legacy" : "legacy/verbose";
         if (this.componentManager.hasComponent(EventStore.class, legacyHint)) {
             try {
                 this.legacyStore = this.componentManager.getInstance(EventStore.class, legacyHint);
             } catch (ComponentLookupException e) {
-                throw new InitializationException("Failed to get the legacy event stream", e);
+                throw new InitializationException(
+                    String.format("Failed to get the legacy event stream [%s]", legacyHint, e));
             }
         }
     }

xwiki-platform-core/xwiki-platform-eventstream/xwiki-platform-eventstream-api/src/main/java/org/xwiki/eventstream/internal/LegacyEventMigrationJob.java
@@ -108,7 +108,7 @@ public class LegacyEventMigrationJob
         try {
             this.eventStore = this.componentManager.getInstance(EventStore.class, this.configuration.getEventStore());
         } catch (ComponentLookupException e) {
-            this.logger.error("Failed to get the configured store", e);
+            this.logger.error("Failed to get the configured event store [{}]", this.configuration.getEventStore(), e);
 
             return;
         }

xwiki-platform-core/xwiki-platform-eventstream/xwiki-platform-eventstream-api/src/test/java/org/xwiki/eventstream/internal/LegacyEventMigrationJobTest.java
@@ -106,7 +106,7 @@ public class LegacyEventMigrationJobTest
             .thenThrow(new ComponentLookupException("Error when looking for myStore"));
 
         this.migrationJob.runInternal();
-        assertEquals("Failed to get the configured store", logCapture.getMessage(2));
+        assertEquals("Failed to get the configured event store [myStore]", logCapture.getMessage(2));
         verify(this.componentManager).getInstance(EventStore.class, "myStore");
         verify(this.eventStream, never()).countEvents();
     }

commit d7bcb59e4eb27f89aa09ae502ab9cbc20c7a0d2d

[misc] Fix mistake introduced in https://github.com/xwiki/xwiki-platform/commit/72c1c37bbf641e385bb9eb3968895e4cfb1c044e

xwiki-platform-core/xwiki-platform-eventstream/xwiki-platform-eventstream-api/src/main/java/org/xwiki/eventstream/internal/DefaultEventStore.java
@@ -99,7 +99,7 @@ public class DefaultEventStore implements EventStore, Initializable
                         this.componentManager.getInstance(EventStore.class, this.configuration.getEventStore());
                 } catch (ComponentLookupException e) {
                     throw new InitializationException(
-                        String.format("Failed to get the configured event store [%s]", hint, e));
+                        String.format("Failed to get the configured event store [%s]", hint), e);
                 }
             }
         }
@@ -112,7 +112,7 @@ public class DefaultEventStore implements EventStore, Initializable
                 this.legacyStore = this.componentManager.getInstance(EventStore.class, legacyHint);
             } catch (ComponentLookupException e) {
                 throw new InitializationException(
-                    String.format("Failed to get the legacy event stream [%s]", legacyHint, e));
+                    String.format("Failed to get the legacy event stream [%s]", legacyHint), e);
             }
         }
     }
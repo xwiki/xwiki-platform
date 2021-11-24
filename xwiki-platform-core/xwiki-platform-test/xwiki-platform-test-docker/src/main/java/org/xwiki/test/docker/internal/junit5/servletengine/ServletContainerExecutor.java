31f751b1b16814e814e0c24976a786daadd8db9b
Vincent Massol <vincent@massol.net>
2021-11-24 10:32:47 +0100

    XWIKI-19081: Async Rendering is broken on Jetty 10.0.3+/9.4.x+ because double url encoded URI are rejected

--- xwiki-platform-core/xwiki-platform-test/xwiki-platform-test-docker/src/main/java/org/xwiki/test/docker/internal/junit5/servletengine/ServletContainerExecutor.java
+++ xwiki-platform-core/xwiki-platform-test/xwiki-platform-test-docker/src/main/java/org/xwiki/test/docker/internal/junit5/servletengine/ServletContainerExecutor.java
@@ -174,6 +174,11 @@ public class ServletContainerExecutor extends AbstractContainerExecutor
             commandPartList.add(ORACLE_TZ_WORKAROUND);
             this.servletContainer.setCommandParts(commandPartList.toArray(new String[0]));
         }
+
+        // Jetty 10.0.3+/9.4.x+ has now added a protection in URLs so that encoded characters such as % are
+        // prohibited by default. Since XWiki uses them, we need to configure Jetty to allow for it. See
+        // https://www.eclipse.org/jetty/documentation/jetty-10/operations-guide/index.html#og-module-server-compliance
+        this.servletContainer.setCommand("jetty.httpConfig.uriCompliance=RFC3986");
     }
 
     private void configureTomcat(File sourceWARDirectory) throws Exception

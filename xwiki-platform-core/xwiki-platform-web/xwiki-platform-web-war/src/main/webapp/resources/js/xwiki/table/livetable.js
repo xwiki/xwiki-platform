35f230a736fe9e8d6e94e0a86e792da628c8c311
Manuel Leduc <manuel.leduc@xwiki.com>
2021-11-26 16:02:50 +0100

    XWIKI-19183: Wrong number of elements in LiveTable in case of view denied
    Wrap the localization of platform.livetable.docNotViewable on a call to jsontool.serialize to form a valid javascript string.

--- xwiki-platform-core/xwiki-platform-web/xwiki-platform-web-war/src/main/webapp/resources/js/xwiki/table/livetable.js
+++ xwiki-platform-core/xwiki-platform-web/xwiki-platform-web-war/src/main/webapp/resources/js/xwiki/table/livetable.js
@@ -359,7 +359,7 @@ XWiki.widgets.LiveTable = Class.create({
       // When a cell is part of a non-viewable row (except for the actions that simply stays empty), we display a 
       // message indicating that the content is not present because the current user does not have the right to view it.
       if (!row['doc_viewable'] && !row[fieldName]) {
-        const notViewableCellMessage = $services.localization.render('platform.livetable.docNotViewable');
+        const notViewableCellMessage = $jsontool.serialize($services.localization.render('platform.livetable.docNotViewable'));
         var td = new Element('td', {
           'class': [
             fieldName,

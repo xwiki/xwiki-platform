b8a2f169746ed107f358d647a7090f14ce04d9c1
Manuel Leduc <manuel.leduc@xwiki.com>
2022-01-11 11:10:25 +0100

    --wip-- [skip ci]

---
+++ xwiki-platform-core/xwiki-platform-oldcore/src/test/java/com/xpn/xwiki/internal/redirection/RedirectClassRedirectionFilterTest.java
@@ -0,0 +1,49 @@
+package com.xpn.xwiki.internal.redirection;/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.xwiki.test.junit5.mockito.ComponentTest;
+import org.xwiki.test.junit5.mockito.InjectMockComponents;
+
+import com.xpn.xwiki.XWikiContext;
+
+/**
+ * Test of {@link RedirectClassRedirectionFilter}.
+ *
+ * @version $Id$
+ * @since 14.0RC1
+ */
+@ComponentTest
+class RedirectClassRedirectionFilterTest
+{
+    @InjectMockComponents
+    private RedirectClassRedirectionFilter target;
+
+    @Mock
+    private XWikiContext context;
+
+    @Test
+    void redirect()
+    {
+    }
+}

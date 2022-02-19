5db1260cfade54c2fecdfea3099359dbdc39a92a
Oana-Lavinia Florean <florean.lavinia@gmail.com>
2022-02-19 08:20:03 +0200

    XWIKI-19172: Display rendered images in view mode inside a lightbox  (#1733)
    XWIKI-19195: Integrate a modern lightbox library in XWiki
    * integrate the blueimp Gallery lightbox library inside the image module
    XWIKI-19196: Add a configuration section for the lightbox in the XWiki administration
    * add a lightbox configuration section in the look & feel category
    * make the lightbox disabled by default
    XWIKI-19172: Display rendered images in view mode inside a lightbox
    * for each image inside the xwikicontent, open a toolbar with imgae actions: open fullscreen and lightbox
    * for the lightbox action, open the image, along with some information if the image was found as attachment (on the current page for now)
    * keep toolbar open while hovering the complete popover
    * use the color theme for the lightbox and use nested structure (less)
    * toggle caption on slide click, as it is done for the other controls; refactor how the lightbox description is updated
    * display lightbox controls from the start
    * cache attachments metadata
    * add the image alt to the lightbox slide image
    * add a string entity reference resolver for extracting the EntityReference from a string URL, needed for the lightbox description
    * get the description of images by resolving the reference from url
    * modify the download attribute for each displayed image
    * add functional tests

---
+++ xwiki-platform-core/xwiki-platform-image/xwiki-platform-image-lightbox/xwiki-platform-image-lightbox-test/xwiki-platform-image-lightbox-test-pageobjects/src/main/java/org/xwiki/image/lightbox/test/po/ImagePopover.java
@@ -0,0 +1,71 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.image.lightbox.test.po;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+import org.xwiki.test.ui.po.BaseElement;
+
+/**
+ * The toolbar opened on image hover.
+ * 
+ * @version $Id$
+ * @since 14.1RC1
+ */
+public class ImagePopover extends BaseElement
+{
+    public ImagePopover()
+    {
+        waitUntilReady();
+    }
+
+    public Lightbox openLightbox(int index)
+    {
+        getDriver().findElementWithoutWaiting(By.cssSelector(".popover .openLightbox")).click();
+
+        return new Lightbox();
+    }
+
+    public boolean isImagePopoverDisplayed()
+    {
+        try {
+            By popoverSelector = By.cssSelector(".popover");
+            return getDriver().findElementWithoutWaiting(popoverSelector).isDisplayed();
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    public WebElement getDownloadButton()
+    {
+        By downloadSelector = By.cssSelector(".popover .imageDownload");
+        return getDriver().findElement(downloadSelector);
+    }
+
+    private ImagePopover waitUntilReady()
+    {
+        getDriver().waitUntilElementIsVisible(By.cssSelector(".popover .imageDownload"));
+        // This attribute is set when the show transition of the popover is complete.
+        getDriver().waitUntilElementHasNonEmptyAttributeValue(By.cssSelector(".popover .imageDownload"), "download");
+
+        return this;
+    }
+}

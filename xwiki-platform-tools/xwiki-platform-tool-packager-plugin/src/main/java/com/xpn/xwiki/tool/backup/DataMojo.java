7400422f81ceece0382107a48edea886d5ccc432
Manuel Leduc <manuel.leduc@xwiki.com>
2022-03-21 16:18:14 +0100

    XWIKI-19352: Indexing of the attachments usage on the wiki
    Wait for the document analysis tasks to be finished before continuing.

--- xwiki-platform-tools/xwiki-platform-tool-packager-plugin/src/main/java/com/xpn/xwiki/tool/backup/DataMojo.java
+++ xwiki-platform-tools/xwiki-platform-tool-packager-plugin/src/main/java/com/xpn/xwiki/tool/backup/DataMojo.java
@@ -25,9 +25,13 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.xwiki.component.manager.ComponentLookupException;
+import org.xwiki.component.manager.ComponentManager;
 import org.xwiki.extension.internal.validator.AbstractExtensionValidator;
 import org.xwiki.extension.job.InstallRequest;
 import org.xwiki.extension.script.ScriptExtensionRewriter;
+import org.xwiki.index.TaskManager;
+import org.xwiki.index.internal.DefaultTasksManager;
 import org.xwiki.model.reference.DocumentReference;
 import org.xwiki.tool.extension.util.ExtensionArtifact;
 import org.xwiki.tool.utils.AbstractOldCoreMojo;
@@ -70,5 +74,41 @@ public class DataMojo extends AbstractOldCoreMojo
             new DocumentReference("xwiki", "XWiki", XWikiRightService.SUPERADMIN_USER));
 
         this.extensionHelper.install(this.includes, installRequest, "wiki:xwiki", null);
+    }
+
+    @Override
+    protected void after() throws MojoExecutionException
+    {
+        consumeIndexingQueue();
+        super.after();
+    }
+
+    /**
+     * Waits for the document indexing tasks to be completed before continuing.
+     *
+     * @throws MojoExecutionException in case of error when resolving the components or when stopping the thread
+     */
+    private void consumeIndexingQueue() throws MojoExecutionException
+    {
+        ComponentManager componentManager =
+            (ComponentManager) this.oldCoreHelper.getXWikiContext().get(ComponentManager.class.getName());
+        try {
+            // Manually start the consumer threads as it is not automatically started in the packaging setup.
+            // This is also convenient as we can be sure that the tasks are all queued before being consumed.
+            TaskManager taskManager = componentManager.getInstance(TaskManager.class);
+            if (taskManager instanceof DefaultTasksManager) {
+                ((DefaultTasksManager) taskManager).startThread();
+            }
+
+            getLog().info("Waiting for the document indexing queue to be empty...");
+            while (taskManager.getQueueSize() > 0) {
+                Thread.sleep(100);
+            }
+            getLog().info("Document indexing queue empty.");
+        } catch (ComponentLookupException e) {
+            throw new MojoExecutionException("Failed to get task manager.", e);
+        } catch (InterruptedException e) {
+            throw new MojoExecutionException("Error while waiting for the task manager to finish.", e);
+        }
     }
 }

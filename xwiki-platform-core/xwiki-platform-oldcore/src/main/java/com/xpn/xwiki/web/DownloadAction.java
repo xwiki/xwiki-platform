9bc192d12ebeb3438af27796a12a9187262664f1
Thomas Mortagne <thomas.mortagne@gmail.com>
2022-04-11 15:14:37 +0200

    XWIKI-18810: Save of the document shouldn't be performed each time an upload is done * remove unused imports

--- xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/web/DownloadAction.java
+++ xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/web/DownloadAction.java
@@ -21,8 +21,6 @@ package com.xpn.xwiki.web;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;

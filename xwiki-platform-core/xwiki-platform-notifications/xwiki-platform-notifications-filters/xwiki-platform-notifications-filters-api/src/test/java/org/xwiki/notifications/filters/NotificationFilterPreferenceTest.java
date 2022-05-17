86076894ce258a0b7f2f093039579773b8ba553b
Manuel Leduc <manuel.leduc@xwiki.com>
2022-05-17 11:06:16 +0200

    XWIKI-15460: Notification filter preferences are not cleaned when a wiki is deleted
    Tests + updated coverage ratio.

---
+++ xwiki-platform-core/xwiki-platform-notifications/xwiki-platform-notifications-filters/xwiki-platform-notifications-filters-api/src/test/java/org/xwiki/notifications/filters/NotificationFilterPreferenceTest.java
@@ -0,0 +1,83 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.notifications.filters;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.xwiki.test.junit5.mockito.ComponentTest;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
+/**
+ * Test of {@link NotificationFilterPreference}.
+ *
+ * @version $Id$
+ * @since 14.4
+ * @since 13.10.6
+ */
+@ComponentTest
+class NotificationFilterPreferenceTest
+{
+    private NotificationFilterPreference notificationFilterPreference;
+
+    @BeforeEach
+    void setUp()
+    {
+        this.notificationFilterPreference = spy(NotificationFilterPreference.class);
+    }
+
+    @Test
+    void isFromWiki()
+    {
+        assertFalse(this.notificationFilterPreference.isFromWiki("wiki"));
+    }
+
+    @Test
+    void isFromWikiWikiDefined()
+    {
+        when(this.notificationFilterPreference.getWiki()).thenReturn("wikiid");
+        assertTrue(this.notificationFilterPreference.isFromWiki("wikiid"));
+    }
+
+    @Test
+    void isFromWikiUserDefined()
+    {
+        when(this.notificationFilterPreference.getUser()).thenReturn("wikiid:XWiki.User");
+        assertTrue(this.notificationFilterPreference.isFromWiki("wikiid"));
+    }
+
+
+    @Test
+    void isFromWikiPageDefined()
+    {
+        when(this.notificationFilterPreference.getPage()).thenReturn("wikiid:XWiki.Page");
+        assertTrue(this.notificationFilterPreference.isFromWiki("wikiid"));
+    }
+
+    @Test
+    void isFromWikiPageOnlyDefined()
+    {
+        when(this.notificationFilterPreference.getPageOnly()).thenReturn("wikiid:XWiki.Page");
+        assertTrue(this.notificationFilterPreference.isFromWiki("wikiid"));
+    }
+}
---
+++ xwiki-platform-core/xwiki-platform-notifications/xwiki-platform-notifications-filters/xwiki-platform-notifications-filters-default/src/test/java/org/xwiki/notifications/filters/internal/NotificationFilterPreferenceStoreTest.java
@@ -0,0 +1,132 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.notifications.filters.internal;
+
+import javax.inject.Provider;
+
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.xwiki.model.reference.WikiReference;
+import org.xwiki.notifications.NotificationException;
+import org.xwiki.test.junit5.mockito.ComponentTest;
+import org.xwiki.test.junit5.mockito.InjectMockComponents;
+import org.xwiki.test.junit5.mockito.MockComponent;
+
+import com.xpn.xwiki.XWiki;
+import com.xpn.xwiki.XWikiContext;
+import com.xpn.xwiki.XWikiException;
+import com.xpn.xwiki.store.XWikiHibernateStore;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Test of {@link NotificationFilterPreferenceStore}.
+ *
+ * @version $Id$
+ * @since 14.4
+ * @since 13.10.6
+ */
+@ComponentTest
+class NotificationFilterPreferenceStoreTest
+{
+    private static final String PREVIOUS_WIKI_ID = "previousWikiId";
+
+    private static final String MAIN_WIKI_ID = "mainWikiId";
+
+    @InjectMockComponents
+    private NotificationFilterPreferenceStore notificationFilterPreferenceStore;
+
+    @MockComponent
+    private Provider<XWikiContext> contextProvider;
+
+    @Mock
+    private XWikiContext context;
+
+    @Mock
+    private XWikiHibernateStore hibernateStore;
+
+    @Mock
+    private Session session;
+
+    @Mock
+    private Query query;
+
+    @BeforeEach
+    void setUp()
+    {
+        when(this.contextProvider.get()).thenReturn(this.context);
+        when(this.context.getWikiId()).thenReturn(PREVIOUS_WIKI_ID);
+        when(this.context.getMainXWiki()).thenReturn(MAIN_WIKI_ID);
+        XWiki wiki = mock(XWiki.class);
+        when(this.context.getWiki()).thenReturn(wiki);
+        when(wiki.getHibernateStore()).thenReturn(this.hibernateStore);
+        when(this.hibernateStore.getSession(this.context)).thenReturn(this.session);
+        when(this.session.createQuery(anyString())).thenReturn(this.query);
+        when(this.query.setParameter(anyString(), any())).thenReturn(this.query);
+    }
+
+    @Test
+    void deleteFilterPreference() throws Exception
+    {
+        this.notificationFilterPreferenceStore.deleteFilterPreference(new WikiReference("wikiid"));
+        verify(this.context).setWikiId(MAIN_WIKI_ID);
+        verify(this.hibernateStore).beginTransaction(this.context);
+        verify(this.session).createQuery("delete from DefaultNotificationFilterPreference "
+            + "where page like :wikiPrefix "
+            + "or pageOnly like :wikiPrefix "
+            + "or user like :wikiPrefix "
+            + "or wiki = :wikiId");
+        verify(this.query).setParameter("wikiPrefix", "wikiid:%");
+        verify(this.query).setParameter("wikiId", "wikiid");
+        verify(this.query).executeUpdate();
+        verify(this.hibernateStore).endTransaction(this.context, true);
+        verify(this.context).setWikiId(PREVIOUS_WIKI_ID);
+    }
+
+    @Test
+    void deleteFilterPreferenceHibernateException() throws Exception
+    {
+        WikiReference wikiReference = new WikiReference("wikiid");
+
+        when(this.hibernateStore.beginTransaction(this.context)).thenThrow(XWikiException.class);
+
+        NotificationException notificationException = assertThrows(NotificationException.class,
+            () -> this.notificationFilterPreferenceStore.deleteFilterPreference(
+                wikiReference));
+
+        assertEquals("Failed to delete the notification preferences for wiki [wikiid]",
+            notificationException.getMessage());
+        assertEquals(XWikiException.class, notificationException.getCause().getClass());
+
+        verify(this.context).setWikiId(MAIN_WIKI_ID);
+        verify(this.hibernateStore).beginTransaction(this.context);
+        verify(this.hibernateStore).endTransaction(this.context, false);
+        verify(this.context).setWikiId(PREVIOUS_WIKI_ID);
+    }
+}

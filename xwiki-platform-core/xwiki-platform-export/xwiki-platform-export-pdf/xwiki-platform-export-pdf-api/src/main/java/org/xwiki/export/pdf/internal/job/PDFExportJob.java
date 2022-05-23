ce04529ad2652f337dfc7d739bfa4bed10f9f563
Marius Dumitru Florea <marius@xwiki.com>
2022-05-23 23:16:14 +0300

    XWIKI-19269: Write a new print service that supports all current features but returns HTML instead of PDF bytes * Implement a PDF export job that renders a list of documents and collects the results (both XDOM and HTML) * Implement a PDF Table of Contents macro that uses as input the rendering results of the PDF export job * Modify the PDF sheet to use the rendering results of the PDF export job when a job id is specified on the request * Modify the PDF template to use the pdftoc macro when the PDF export job id is specified on the request * Modify the PDF export UI to trigger the PDF export job and wait for it to finish before showing the print preview modal.
    (cherry picked from commit c4492c462c41c7ae7a6842fbe944a228f5992505)

---
+++ xwiki-platform-core/xwiki-platform-export/xwiki-platform-export-pdf/xwiki-platform-export-pdf-api/src/main/java/org/xwiki/export/pdf/internal/job/PDFExportJob.java
@@ -0,0 +1,129 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.export.pdf.internal.job;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.xwiki.component.annotation.Component;
+import org.xwiki.export.pdf.job.PDFExportJobRequest;
+import org.xwiki.export.pdf.job.PDFExportJobStatus;
+import org.xwiki.job.AbstractJob;
+import org.xwiki.model.reference.DocumentReference;
+import org.xwiki.model.reference.EntityReference;
+import org.xwiki.security.authorization.AuthorizationManager;
+import org.xwiki.security.authorization.Right;
+
+/**
+ * The PDF export job.
+ * 
+ * @version $Id$
+ * @since 14.4
+ */
+@Component
+@Named(PDFExportJob.JOB_TYPE)
+public class PDFExportJob extends AbstractJob<PDFExportJobRequest, PDFExportJobStatus>
+{
+    /**
+     * The PDF export job type.
+     */
+    public static final String JOB_TYPE = "export/pdf";
+
+    /**
+     * Used to check access permissions.
+     * 
+     * @see #hasAccess(Right, EntityReference)
+     */
+    @Inject
+    private AuthorizationManager authorization;
+
+    @Inject
+    private DocumentRenderer documentRenderer;
+
+    @Override
+    public String getType()
+    {
+        return JOB_TYPE;
+    }
+
+    @Override
+    protected PDFExportJobStatus createNewStatus(PDFExportJobRequest request)
+    {
+        return new PDFExportJobStatus(getType(), request, this.observationManager, this.loggerManager);
+    }
+
+    @Override
+    protected void runInternal() throws Exception
+    {
+        if (!this.request.getDocuments().isEmpty()) {
+            render(this.request.getDocuments());
+
+            if (this.request.isServerSide() && !this.status.isCanceled()) {
+                saveAsPDF();
+                this.status.getDocumentRenderingResults().clear();
+            }
+        }
+    }
+
+    private void render(List<DocumentReference> documentReferences) throws Exception
+    {
+        this.progressManager.pushLevelProgress(documentReferences.size(), this);
+
+        try {
+            for (DocumentReference documentReference : documentReferences) {
+                if (this.status.isCanceled()) {
+                    break;
+                } else {
+                    this.progressManager.startStep(this);
+                    if (hasAccess(Right.VIEW, documentReference)) {
+                        this.status.getDocumentRenderingResults().add(this.documentRenderer.render(documentReference));
+                    }
+                    Thread.yield();
+                    this.progressManager.endStep(this);
+                }
+            }
+        } finally {
+            this.progressManager.popLevelProgress(this);
+        }
+    }
+
+    /**
+     * Check access rights taking into account the job request.
+     * 
+     * @param right the access right to check
+     * @param reference the target entity reference
+     * @return return {@code true} if the current user or the entity author have the specified access right on the
+     *         specified entity, depending on the job request
+     */
+    private boolean hasAccess(Right right, EntityReference reference)
+    {
+        return ((!this.request.isCheckRights()
+            || this.authorization.hasAccess(right, this.request.getUserReference(), reference))
+            && (!this.request.isCheckAuthorRights()
+                || this.authorization.hasAccess(right, this.request.getAuthorReference(), reference)));
+    }
+
+    private void saveAsPDF()
+    {
+        // TODO
+    }
+}
---
+++ xwiki-platform-core/xwiki-platform-export/xwiki-platform-export-pdf/xwiki-platform-export-pdf-default/src/main/java/org/xwiki/export/pdf/internal/job/DefaultPDFExportJobRequestFactory.java
@@ -0,0 +1,164 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.export.pdf.internal.job;
+
+import java.io.Serializable;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Provider;
+import javax.inject.Singleton;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.xwiki.component.annotation.Component;
+import org.xwiki.container.servlet.HttpServletUtils;
+import org.xwiki.context.concurrent.ContextStoreManager;
+import org.xwiki.export.pdf.job.PDFExportJobRequest;
+import org.xwiki.export.pdf.job.PDFExportJobRequestFactory;
+import org.xwiki.model.reference.DocumentReferenceResolver;
+
+import com.xpn.xwiki.XWikiContext;
+import com.xpn.xwiki.internal.context.XWikiContextContextStore;
+import com.xpn.xwiki.internal.export.DocumentSelectionResolver;
+import com.xpn.xwiki.web.XWikiRequest;
+
+/**
+ * Default implementation of {@link PDFExportJobRequestFactory}.
+ * 
+ * @version $Id$
+ * @since 14.4
+ */
+@Component
+@Singleton
+public class DefaultPDFExportJobRequestFactory implements PDFExportJobRequestFactory
+{
+    private static final String EXPORT = "export";
+
+    @Inject
+    private Provider<XWikiContext> xcontextProvider;
+
+    @Inject
+    private DocumentSelectionResolver documentSelectionResolver;
+
+    @Inject
+    @Named("current")
+    private DocumentReferenceResolver<String> currentDocumentReferenceResolver;
+
+    @Inject
+    private ContextStoreManager contextStoreManager;
+
+    @Override
+    public PDFExportJobRequest createRequest() throws Exception
+    {
+        PDFExportJobRequest request = new PDFExportJobRequest();
+
+        String suffix = new Date().getTime() + "-" + ThreadLocalRandom.current().nextInt(100, 1000);
+        request.setId(EXPORT, "pdf", suffix);
+
+        setRightsProperties(request);
+        setContextProperties(request);
+        readPDFExportOptionsFromHTTPRequest(request);
+
+        return request;
+    }
+
+    @Override
+    public void setRightsProperties(PDFExportJobRequest request)
+    {
+        XWikiContext xcontext = this.xcontextProvider.get();
+
+        request.setCheckRights(true);
+        request.setUserReference(xcontext.getUserReference());
+        request.setAuthorReference(xcontext.getAuthorReference());
+    }
+
+    private void setContextProperties(PDFExportJobRequest request) throws Exception
+    {
+        Map<String, Serializable> pdfExportContext =
+            this.contextStoreManager.save(this.contextStoreManager.getSupportedEntries());
+
+        XWikiContext xcontext = this.xcontextProvider.get();
+        XWikiRequest httpRequest = xcontext.getRequest();
+        String queryString = Objects.toString(httpRequest.get("pdfQueryString"), "");
+        // The request URL hash (fragment identifier) is not sent to the server but in the case of server-side PDF
+        // export we need it as it can influence the behavior of the JavaScript code when the PDF template is loaded in
+        // the headless web browser. In order to overcome this we receive the hash as a request parameter.
+        String hash = Objects.toString(httpRequest.get("pdfHash"), "");
+
+        pdfExportContext.put(XWikiContextContextStore.PROP_ACTION, EXPORT);
+        pdfExportContext.put(XWikiContextContextStore.PROP_REQUEST_PARAMETERS,
+            (Serializable) getRequestParameters(queryString));
+
+        // We want the documents to be rendered with the same parameters (query string) and hash (anchor or fragment
+        // identifier) as in view mode. For this the saved request URL should use the specified query string and hash.
+        URL requestURL =
+            new URL(HttpServletUtils.getSourceURL(httpRequest), String.format("?%s#%s", queryString, hash));
+        pdfExportContext.put(XWikiContextContextStore.PROP_REQUEST_URL, requestURL);
+
+        request.setContext(pdfExportContext);
+    }
+
+    private void readPDFExportOptionsFromHTTPRequest(PDFExportJobRequest request)
+    {
+        XWikiContext xcontext = this.xcontextProvider.get();
+        XWikiRequest httpRequest = xcontext.getRequest();
+        String template = httpRequest.get("pdftemplate");
+        if (StringUtils.isEmpty(template)) {
+            template = "XWiki.PDFExport.Template";
+        }
+        request.setTemplate(this.currentDocumentReferenceResolver.resolve(template));
+
+        request.setWithCover(!"0".equals(httpRequest.get("pdfcover")));
+        request.setWithToc(!"0".equals(httpRequest.get("pdftoc")));
+        request.setWithHeader(!"0".equals(httpRequest.get("pdfheader")));
+        request.setWithFooter(!"0".equals(httpRequest.get("pdffooter")));
+        request
+            .setDocuments(this.documentSelectionResolver.getSelectedDocuments().stream().collect(Collectors.toList()));
+    }
+
+    private Map<String, String[]> getRequestParameters(String queryString)
+    {
+        Map<String, List<String>> params = new LinkedHashMap<>();
+        for (NameValuePair pair : URLEncodedUtils.parse(queryString, Charset.forName("UTF-8"))) {
+            List<String> values = params.getOrDefault(pair.getName(), new ArrayList<>());
+            values.add(pair.getValue());
+            params.put(pair.getName(), values);
+        }
+
+        Map<String, String[]> parameters = new LinkedHashMap<>();
+        for (Map.Entry<String, List<String>> entry : params.entrySet()) {
+            parameters.put(entry.getKey(), entry.getValue().toArray(new String[entry.getValue().size()]));
+        }
+
+        return parameters;
+    }
+}

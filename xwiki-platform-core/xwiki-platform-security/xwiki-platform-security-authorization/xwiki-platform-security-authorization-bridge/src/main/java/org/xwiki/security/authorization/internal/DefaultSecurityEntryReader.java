d1e23745f6bc15f75d86758325165161b894eae8
Simon Urli <simon.urli@xwiki.com>
2022-06-16 15:23:56 +0200

    XWIKI-19806: BridgeAuthorizationManager errors when deleting a subwiki  which cause errors when creating another subwiki (#1857)
      * Prevent the DefaultSecurityEntryReader to throw an exception if the
        wiki does not exist anymore: just handle the exception as if the
    returned owner is null, as it seems to have been planned for
      * Improve exception handling to only fallback on null if it's an
        exception because the wiki does not exist.
      * Provide a new unit test for this

--- xwiki-platform-core/xwiki-platform-security/xwiki-platform-security-authorization/xwiki-platform-security-authorization-bridge/src/main/java/org/xwiki/security/authorization/internal/DefaultSecurityEntryReader.java
+++ xwiki-platform-core/xwiki-platform-security/xwiki-platform-security-authorization/xwiki-platform-security-authorization-bridge/src/main/java/org/xwiki/security/authorization/internal/DefaultSecurityEntryReader.java
@@ -253,17 +253,19 @@ public class DefaultSecurityEntryReader implements SecurityEntryReader
 
     /**
      * @param wikiReference the wiki to look for owner
-     * @return a reference to the owner of the wiki
-     * @throws AuthorizationException if the owner could not be retrieved.
+     * @return a reference to the owner of the wiki or {@code null}
      */
     private DocumentReference getWikiOwner(WikiReference wikiReference) throws AuthorizationException
     {
         XWikiContext context = this.xcontextProvider.get();
-        String wikiOwner;
+        String wikiOwner = null;
         try {
             wikiOwner = context.getWiki().getWikiOwner(wikiReference.getName(), context);
         } catch (XWikiException e) {
-            throw new AuthorizationException(wikiReference, "Could not retrieve the owner of this wiki", e);
+            if (e.getCode() != XWikiException.ERROR_XWIKI_DOES_NOT_EXIST) {
+                throw new AuthorizationException(wikiReference,
+                    String.format("Could not retrieve the owner of the wiki [%s]", wikiReference.getName()), e);
+            }
         }
 
         if (wikiOwner == null) {

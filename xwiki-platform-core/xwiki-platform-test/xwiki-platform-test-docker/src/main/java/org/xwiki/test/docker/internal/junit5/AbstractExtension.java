78caae6df231a927686ec5a67af1b513a94b2eb7
Manuel Leduc <manuel.leduc@xwiki.com>
2022-06-17 12:10:30 +0200

    [Misc] Save screenshot and video recording on test fail during BeforeEach/AfterEach
    - Fix checkstyle issues
    - Fix test coverage issues

--- xwiki-platform-core/xwiki-platform-test/xwiki-platform-test-docker/src/main/java/org/xwiki/test/docker/internal/junit5/AbstractExtension.java
+++ xwiki-platform-core/xwiki-platform-test/xwiki-platform-test-docker/src/main/java/org/xwiki/test/docker/internal/junit5/AbstractExtension.java
@@ -27,6 +27,7 @@ import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExecutionCondition;
 import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.LifecycleMethodExecutionExceptionHandler;
 import org.junit.jupiter.api.extension.ParameterResolver;
 import org.junit.jupiter.api.extension.TestExecutionExceptionHandler;
 import org.testcontainers.containers.BrowserWebDriverContainer;
@@ -45,7 +46,8 @@ import org.xwiki.test.ui.XWikiWebDriver;
  * @since 10.10RC1
  */
 public abstract class AbstractExtension implements BeforeAllCallback, AfterAllCallback,
-    BeforeEachCallback, AfterEachCallback, ParameterResolver, TestExecutionExceptionHandler, ExecutionCondition
+    BeforeEachCallback, AfterEachCallback, ParameterResolver, TestExecutionExceptionHandler, ExecutionCondition,
+    LifecycleMethodExecutionExceptionHandler
 {
     private static final ExtensionContext.Namespace NAMESPACE =
         ExtensionContext.Namespace.create(XWikiDockerExtension.class);

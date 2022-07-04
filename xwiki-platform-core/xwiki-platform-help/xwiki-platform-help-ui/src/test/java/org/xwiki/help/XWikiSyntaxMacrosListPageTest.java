c08a8ef131f0bf1a0aa1492748545da17ac1f0f5
Manuel Leduc <manuel.leduc@xwiki.com>
2022-07-04 08:59:24 +0200

    --wip-- [skip ci]

--- xwiki-platform-core/xwiki-platform-help/xwiki-platform-help-ui/src/test/java/org/xwiki/help/XWikiSyntaxMacrosListPageTest.java
+++ xwiki-platform-core/xwiki-platform-help/xwiki-platform-help-ui/src/test/java/org/xwiki/help/XWikiSyntaxMacrosListPageTest.java
@@ -19,19 +19,38 @@
  */
 package org.xwiki.help;
 
+import java.util.List;
+import java.util.Set;
+
 import org.jsoup.nodes.Document;
 import org.junit.jupiter.api.Test;
 import org.xwiki.model.reference.DocumentReference;
+import org.xwiki.query.Query;
+import org.xwiki.query.script.QueryManagerScriptService;
 import org.xwiki.rendering.RenderingScriptServiceComponentList;
 import org.xwiki.rendering.internal.configuration.DefaultExtendedRenderingConfiguration;
 import org.xwiki.rendering.internal.configuration.RenderingConfigClassDocumentConfigurationSource;
+import org.xwiki.rendering.macro.Macro;
+import org.xwiki.rendering.macro.MacroId;
+import org.xwiki.rendering.macro.descriptor.DefaultMacroDescriptor;
+import org.xwiki.rendering.syntax.Syntax;
+import org.xwiki.rendering.wikimacro.internal.DefaultWikiMacro;
+import org.xwiki.script.service.ScriptService;
 import org.xwiki.test.annotation.ComponentList;
 import org.xwiki.test.page.HTML50ComponentList;
 import org.xwiki.test.page.PageTest;
 import org.xwiki.test.page.XWikiSyntax21ComponentList;
 import org.xwiki.xml.internal.html.filter.ControlCharactersFilter;
 
+import com.xpn.xwiki.doc.XWikiDocument;
+import com.xpn.xwiki.objects.BaseObject;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.xwiki.rendering.wikimacro.internal.WikiMacroConstants.MACRO_ID_PROPERTY;
+import static org.xwiki.rendering.wikimacro.internal.WikiMacroConstants.MACRO_VISIBILITY_PROPERTY;
 
 /**
  * Page Test of the {@code XWiki.XWikiSyntaxMacrosList} document.
@@ -54,10 +73,31 @@ class XWikiSyntaxMacrosListPageTest extends PageTest
     public static final DocumentReference
         DOCUMENT_REFERENCE = new DocumentReference("xwiki", "XWiki", "XWikiSyntaxMacrosList");
 
+    public static final DocumentReference XCLASS_REFERENCE = new DocumentReference("xwiki", "XWiki", "WikiMacroClass");
+
     @Test
     void renderTable() throws Exception
     {
-        // TODO: load at least one Wiki Macro and factorize duplicated code with AttachmentSelectorPageTest.
+        XWikiDocument mymacro = this.xwiki.getDocument(new DocumentReference("xwiki", "XWiki", "MyMacro"),
+            this.context);
+        mymacro.setSyntax(Syntax.XWIKI_2_1);
+        BaseObject macroObject = mymacro.newXObject(XCLASS_REFERENCE, this.context);
+        macroObject.setStringValue(MACRO_VISIBILITY_PROPERTY, "WIKI");
+        macroObject.setStringValue(MACRO_ID_PROPERTY, "mymacro");
+        this.xwiki.saveDocument(mymacro, this.context);
+
+        QueryManagerScriptService queryManagerScriptService =
+            this.componentManager.registerMockComponent(ScriptService.class, "query", QueryManagerScriptService.class,
+                false);
+        DefaultWikiMacro mymacro1 =
+            this.componentManager.registerMockComponent(Macro.class, "mymacro", DefaultWikiMacro.class, false);
+        DefaultMacroDescriptor t =
+            new DefaultMacroDescriptor(new MacroId("mymacro"), "My Macro", "My Macro Description");
+        t.setDefaultCategories(Set.of("Category1", "Category2"));
+        when(mymacro1.getDescriptor()).thenReturn(t);
+        Query query = mock(Query.class);
+        when(queryManagerScriptService.xwql(any())).thenReturn(query);
+        when(query.execute()).thenReturn(List.of("xwiki:XWiki.MyMacro"));
         Document document = renderHTMLPage(DOCUMENT_REFERENCE);
         System.out.println(document);
         assertNotNull(document);

258b703b2b8450b81ae39b91611c4d4ddf873d92
Oana-Lavinia Florean <florean.lavinia@gmail.com>
2022-07-27 15:17:48 +0300

    XWIKI-20031: The date displayed in the lightbox is not considering user timezone (#1854)
    * Consider user timezone when formating the image date displayed in lightbox
    * use proper case for userTimeZone and dateFormat and add escaping
    * added test to verify if the timezone is indeed taken into consideration
    
    (cherry picked from commit 62ee8c1fdff6fb0031e883ed94f6288b955ca221)

--- xwiki-platform-core/xwiki-platform-image/xwiki-platform-image-lightbox/xwiki-platform-image-lightbox-webjar/src/main/webjar/lightbox.js
+++ xwiki-platform-core/xwiki-platform-image/xwiki-platform-image-lightbox/xwiki-platform-image-lightbox-webjar/src/main/webjar/lightbox.js
@@ -34,7 +34,8 @@ define('xwiki-lightbox-description', [
   'jquery',
   'xwiki-l10n!xwiki-lightbox-messages',
   'moment',
-  'moment-jdateformatparser'
+  'moment-jdateformatparser',
+  'moment-timezone'
 ], function($, l10n, moment) {
   var _cachedAttachments = {};
 
@@ -93,8 +94,11 @@ define('xwiki-lightbox-description', [
     }
 
     if (attachmentData.date) {
-      var dateFormat = moment().toMomentFormatString($('.lightboxDescription .date').attr('data-dateFormat'));
-      $('.lightboxDescription .date').text(l10n.get('date', moment(attachmentData.date).format(dateFormat)));
+      // This should be updated after fixing XWIKI-19808: Proper javascript API to perform Date Formatting.
+      var dateFormat = moment().toMomentFormatString($('.lightboxDescription .date').data('dateFormat'));
+      var timezone = $('.lightboxDescription .date').data('userTimeZone');
+      $('.lightboxDescription .date')
+        .text(l10n.get('date', moment(attachmentData.date).tz(timezone).format(dateFormat)));
     }
   };
 

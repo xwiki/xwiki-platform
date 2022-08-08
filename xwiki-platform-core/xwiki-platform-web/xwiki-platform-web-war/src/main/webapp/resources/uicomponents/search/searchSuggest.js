dbab8d2cf515b5919b0c87fed48c1c2be2d8f4e8
slauriere <slauriere@xwiki.com>
2022-08-08 23:06:50 +0200

    XWIKI-11062: Make "Search Suggest" responsive on mobile - Move control of search suggestions element style attributes to   stylesheet searchSuggest.css instead of JS - Remove irrelevant media query rule using LESS variable in suggest.css   and restore original width

--- xwiki-platform-core/xwiki-platform-web/xwiki-platform-web-war/src/main/webapp/resources/uicomponents/search/searchSuggest.js
+++ xwiki-platform-core/xwiki-platform-web/xwiki-platform-web-war/src/main/webapp/resources/uicomponents/search/searchSuggest.js
@@ -152,10 +152,6 @@ var XWiki = (function (XWiki) {
         }
       });
       var allResults = allResultsNode.getElement();
-      var suggestWidth = 500;
-      if (window.innerWidth < 768) {
-        suggestWidth =  250;
-      }
       this.suggest = new XWiki.widgets.Suggest( this.searchInput, {
         parentContainer: $('searchSuggest'),
         className: 'searchSuggest horizontalLayout',
@@ -168,7 +164,7 @@ var XWiki = (function (XWiki) {
         displayValueText: "$escapetool.javascript($services.localization.render('platform.search.suggestResultLocatedIn'))",
         resultInfoHTML: true,
         timeout: 0,
-        width: suggestWidth,
+        width: 500,
         unifiedLoader: true,
         loaderNode: allResults.down("li"),
         shownoresults: false,

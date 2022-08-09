c3d415674e06a4b41535e47dc8ac6149b087ae4d
Vincent Massol <vincent@massol.net>
2022-08-09 14:25:22 +0200

    XWIKI-20023: Move the Formula macro to XWiki Contrib * Refactor XWiki oldcore which was using several classes from the Formula Renderer using a hack so that that PDF export would be able to include formulas. The refactoring was done to offer a generic implementation for PDF export, based on the Resource module, to handle all type of resources. The Formula macro moved in contrib was also modified to use Temporary Resources, thus making it compatible with the refactoring done for PDF export.

--- xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/web/ExportAction.java
+++ xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/web/ExportAction.java
@@ -165,9 +165,11 @@ public class ExportAction extends XWikiAction
         // Note 1: exportType will be null if no office server is started or it doesn't support the passed format
         // Note 2: we don't use the office server for PDF exports since it doesn't work OOB. Instead we use FOP.
         if ("pdf".equalsIgnoreCase(format)) {
-            // The export format is PDF or the office converter can't be used (either it doesn't support the specified
-            // format or the office server is not started).
-            urlFactory = new PdfURLFactory();
+            // Use the factory to generate view URLs for browser-consumption. FOP will resolve any relative URL to
+            // a full URI, see FOPXSLFORenderer#getBaseURI(). Note that this is a better implementation than using
+            // ExternalServletURLFactory since that would only work for URLs that are computed using XWikiURLFactory
+            // and not those using a ResourceReferenceSerializer (e.g. ExtendedURLTemporaryResourceReferenceSerializer).
+            urlFactory = new XWikiServletURLFactory();
             exporter = new PdfExportImpl();
             exportType = ExportType.PDF;
         } else if (exportType == null) {

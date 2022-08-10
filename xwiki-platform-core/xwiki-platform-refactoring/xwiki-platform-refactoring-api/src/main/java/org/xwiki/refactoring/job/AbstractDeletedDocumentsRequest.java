eb0bca3866c962074b89fd46297a4de61c0716a9
Thomas Mortagne <thomas.mortagne@gmail.com>
2022-08-10 15:20:28 +0200

    XCOMMONS-2492: Automatically try to convert the input job request when it does not have the expected type * don't change the visibility of the default constructor

--- xwiki-platform-core/xwiki-platform-refactoring/xwiki-platform-refactoring-api/src/main/java/org/xwiki/refactoring/job/AbstractDeletedDocumentsRequest.java
+++ xwiki-platform-core/xwiki-platform-refactoring/xwiki-platform-refactoring-api/src/main/java/org/xwiki/refactoring/job/AbstractDeletedDocumentsRequest.java
@@ -42,7 +42,7 @@ public abstract class AbstractDeletedDocumentsRequest extends AbstractCheckRight
     /**
      * Default constructor.
      */
-    protected AbstractDeletedDocumentsRequest()
+    public AbstractDeletedDocumentsRequest()
     {
     }
 

7d1312414c062d65988528ea82957ca8e16fc75a
Alex Cotiugă <ancotiuga@gmail.com>
2022-08-18 10:48:56 +0300

    XWIKI-20060: Database Tree property changes are not saved anymore in object mode (#1889)
    * move the inline js code to and external file so the dataeditors.js will append it in HTML head
    * make sure the moved code is active only when it necessary (when data.instance.settings.xwiki.fieldName is defined)

--- xwiki-platform-core/xwiki-platform-tree/xwiki-platform-tree-webjar/src/main/webjar/tree.js
+++ xwiki-platform-core/xwiki-platform-tree/xwiki-platform-tree-webjar/src/main/webjar/tree.js
@@ -693,6 +693,15 @@ define(['jquery', 'JobRunner', 'jsTree', 'tree-finder'], function($, JobRunner) 
         data.menu.remove._disabled = !canRemoveNodes(selectedNodes);
       }
 
+    }).on('changed.jstree', function(event, data) {
+      if (typeof data.instance.settings.xwiki.fieldName !== 'undefined') {
+        var fieldName = data.instance.settings.xwiki.fieldName;
+        var fieldValue = data.selected.join('|');
+        $('input[type="hidden"]').filter(function() {
+          return $(this).attr('name') === fieldName;
+        }).val(fieldValue);
+      }
+
     //
     // Un-wrap the links generated from wiki syntax so that they are taken into account by jsTree.
     //

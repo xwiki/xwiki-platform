c270d90cfa6229f4ab4ea11148250373f7b243cb
Manuel Leduc <manuel.leduc@xwiki.com>
2022-08-19 15:28:31 +0200

    XWIKI-20068: The context document should automatically be marked as used in the AsyncContext only when async is disabled
    - Add defensive code to prevent a call to `asyncContext.useEntity` with a `null` entity.
    - Applies the same logic on AsyncRendererJob

--- xwiki-platform-core/xwiki-platform-rendering/xwiki-platform-rendering-async/xwiki-platform-rendering-async-default/src/test/java/org/xwiki/rendering/async/internal/AsyncRendererJobTest.java
+++ xwiki-platform-core/xwiki-platform-rendering/xwiki-platform-rendering-async/xwiki-platform-rendering-async-default/src/test/java/org/xwiki/rendering/async/internal/AsyncRendererJobTest.java
@@ -22,11 +22,20 @@ package org.xwiki.rendering.async.internal;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.xwiki.bridge.DocumentAccessBridge;
+import org.xwiki.model.reference.DocumentReference;
 import org.xwiki.rendering.RenderingException;
+import org.xwiki.rendering.async.AsyncContext;
 import org.xwiki.test.junit5.mockito.ComponentTest;
 import org.xwiki.test.junit5.mockito.InjectMockComponents;
+import org.xwiki.test.junit5.mockito.MockComponent;
 
+import static com.xpn.xwiki.internal.context.XWikiContextContextStore.PROP_DOCUMENT_REFERENCE;
+import static java.util.Collections.singletonMap;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * Validate {@link AsyncRendererJob}.
@@ -34,10 +43,16 @@ import static org.mockito.Mockito.verify;
  * @version $Id$
  */
 @ComponentTest
-public class AsyncRendererJobTest
+class AsyncRendererJobTest
 {
     @InjectMockComponents
     private AsyncRendererJob job;
+
+    @MockComponent
+    private AsyncContext asyncContext;
+
+    @MockComponent
+    private DocumentAccessBridge documentAccessBridge;
 
     @Mock
     private AsyncRenderer renderer;
@@ -58,5 +73,31 @@ public class AsyncRendererJobTest
         this.job.run();
 
         verify(this.renderer).render(true, false);
+    }
+
+    @Test
+    void runInternalDocumentRequested()
+    {
+        this.request.setContext(singletonMap(PROP_DOCUMENT_REFERENCE, "Document"));
+
+        DocumentReference currentDocumentReference = new DocumentReference("xwiki", "XWiki", "CurrentDoc");
+        when(this.documentAccessBridge.getCurrentDocumentReference())
+            .thenReturn(currentDocumentReference);
+
+        this.job.initialize(this.request);
+        this.job.run();
+
+        verify(this.asyncContext).useEntity(currentDocumentReference);
+    }
+
+    @Test
+    void runInternalDocumentRequestedButNotInContext()
+    {
+        this.request.setContext(singletonMap(PROP_DOCUMENT_REFERENCE, "Document"));
+
+        this.job.initialize(this.request);
+        this.job.run();
+
+        verify(this.asyncContext, never()).useEntity(any());
     }
 }

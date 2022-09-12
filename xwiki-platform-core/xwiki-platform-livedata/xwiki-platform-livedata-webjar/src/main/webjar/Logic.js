b112db434ce869969db113ac1135ec958512720c
Simon Urli <simon.urli@xwiki.com>
2022-09-12 09:52:36 +0200

    Update xwiki-platform-core/xwiki-platform-livedata/xwiki-platform-livedata-webjar/src/main/webjar/Logic.js
    Co-authored-by: Manuel Leduc <manuel.leduc@xwiki.com>

--- xwiki-platform-core/xwiki-platform-livedata/xwiki-platform-livedata-webjar/src/main/webjar/Logic.js
+++ xwiki-platform-core/xwiki-platform-livedata/xwiki-platform-livedata-webjar/src/main/webjar/Logic.js
@@ -583,10 +583,7 @@ define('xwiki-livedata', [
       return this.fetchEntries()
         .then(data => {
           this.data.data = Object.freeze(data);
-          let self = this;
-          Vue.nextTick(function () {
-            self.triggerEvent('entriesUpdated', {});
-          });
+          Vue.nextTick(() => this.triggerEvent('entriesUpdated', {}));
           // Remove the outdated footnotes, they will be recomputed by the new entries.
           this.footnotes.reset()
         })

28e1ee10aa5e11ab5cc5ebf4da3a04fd90457530
Simon Urli <simon.urli@xwiki.com>
2022-11-09 11:11:15 +0100

    XWIKI-20289: Deleted terminal documents cannot be viewed
      * Refactor DeletePageIT to use TestReference instead of a shared
        reference in all tests: it avoids having deleted documents stacking
    up in same recycle bin and ensure better testing
      * Edit a bit the behaviour of new test API to return empty collection
        if there's no deleted documents
    
    (cherry picked from commit 666fd6d7bc361a8162beacb24323e6a374648d6b)

--- xwiki-platform-core/xwiki-platform-test/xwiki-platform-test-ui/src/main/java/org/xwiki/test/ui/po/DeletePageOutcomePage.java
+++ xwiki-platform-core/xwiki-platform-test/xwiki-platform-test-ui/src/main/java/org/xwiki/test/ui/po/DeletePageOutcomePage.java
@@ -20,10 +20,12 @@
 package org.xwiki.test.ui.po;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
@@ -83,9 +85,13 @@ public class DeletePageOutcomePage extends ViewPage
      */
     public List<DeletedPageEntry> getDeletedPagesEntries()
     {
-        WebElement table =
-            getDriver().findElementWithoutWaiting(By.cssSelector("div.xwikimessage div.docs table"));
-        return getDeletedEntries(table, false);
+        try {
+            WebElement table =
+                getDriver().findElementWithoutWaiting(By.cssSelector("div.xwikimessage div.docs table"));
+            return getDeletedEntries(table, false);
+        } catch (NoSuchElementException e) {
+            return Collections.emptyList();
+        }
     }
 
     /**
@@ -93,9 +99,13 @@ public class DeletePageOutcomePage extends ViewPage
      */
     public List<DeletedPageEntry> getDeletedTerminalPagesEntries()
     {
-        WebElement table =
-            getDriver().findElementWithoutWaiting(By.cssSelector("div.xwikimessage div.terminal-docs table"));
-        return getDeletedEntries(table, true);
+        try {
+            WebElement table =
+                getDriver().findElementWithoutWaiting(By.cssSelector("div.xwikimessage div.terminal-docs table"));
+            return getDeletedEntries(table, true);
+        } catch (NoSuchElementException e) {
+            return Collections.emptyList();
+        }
     }
 
     /**

b09b71705c90371b4c0a6f156c22b1f279a92b1f
Michael Hamann <michael.hamann@xwiki.com>
2022-12-01 17:56:50 +0100

    XWIKI-20190: Properly handle AWM creation without script right
    * Warn when the user doesn't have script right.
    * Ensure that in integration tests the users have script right.
    * Add an explicit test for the behavior without script right.
    * Fix missing dependency.
    * Actually delete the preferences in AppsLiveTableIT.
    
    (cherry picked from commit e73b890623efa604adc484ad82f37e31596fe1a6)

--- xwiki-platform-core/xwiki-platform-appwithinminutes/xwiki-platform-appwithinminutes-test/xwiki-platform-appwithinminutes-test-docker/src/test/it/org/xwiki/appwithinminutes/test/ui/AllITs.java
+++ xwiki-platform-core/xwiki-platform-appwithinminutes/xwiki-platform-appwithinminutes-test/xwiki-platform-appwithinminutes-test-docker/src/test/it/org/xwiki/appwithinminutes/test/ui/AllITs.java
@@ -79,4 +79,10 @@ public class AllITs
     class NestedClassEditorIT extends ClassEditorIT
     {
     }
+
+    @Nested
+    @DisplayName("Without script right test")
+    class NestedWithoutScriptRightIT extends WithoutScriptRightIT
+    {
+    }
 }

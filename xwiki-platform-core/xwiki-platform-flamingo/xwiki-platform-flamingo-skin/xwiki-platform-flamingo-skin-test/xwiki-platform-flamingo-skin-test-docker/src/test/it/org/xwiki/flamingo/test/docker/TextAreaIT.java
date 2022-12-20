eb719522ab97c4af80bc41773caf42828b19b58f
Thomas Mortagne <thomas.mortagne@gmail.com>
2022-12-20 16:49:01 +0100

    XWIKI-20366: Introduce a "restricted" switch to TextArea object properties * fix test

--- xwiki-platform-core/xwiki-platform-flamingo/xwiki-platform-flamingo-skin/xwiki-platform-flamingo-skin-test/xwiki-platform-flamingo-skin-test-docker/src/test/it/org/xwiki/flamingo/test/docker/TextAreaIT.java
+++ xwiki-platform-core/xwiki-platform-flamingo/xwiki-platform-flamingo-skin/xwiki-platform-flamingo-skin-test/xwiki-platform-flamingo-skin-test-docker/src/test/it/org/xwiki/flamingo/test/docker/TextAreaIT.java
@@ -108,8 +108,8 @@ class TextAreaIT
 
         viewPage = testUtils.gotoPage(testReference);
 
-        assertEquals("Failed to execute the [velocity] macro. "
-            + "Cause: [The execution of the [velocity] script macro is not allowed. "
+        assertEquals("Failed to execute the [velocity] macro. Cause:"
+            + " [The execution of the [velocity] script macro is not allowed in [xwiki:TextAreaIT.restricted.WebHome]. "
             + "Check the rights of its last author or the parameters if it's rendered from another script.]. "
             + "Click on this message for details.", viewPage.getContent());
     }

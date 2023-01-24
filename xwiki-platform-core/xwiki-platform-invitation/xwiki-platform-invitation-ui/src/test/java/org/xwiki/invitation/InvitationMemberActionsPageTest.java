fc37540693afe6e7f47c6a82dfb4083a9e14aefa
Manuel Leduc <manuel.leduc@xwiki.com>
2023-01-24 08:38:28 +0100

    XWIKI-20285: Improve escaping of the Invitation Application
    - Introduce an internal role (DefaultXARExtensionIndex) to know if a document is provided by an extension
    - Split index-api to index-api/index-default to be able to use the api in oldcore
    - Fix the escaping in invitation-ui
    - Provide an automatic fix during upgrade for potential escaping issues in generated code

---
+++ xwiki-platform-core/xwiki-platform-invitation/xwiki-platform-invitation-ui/src/test/java/org/xwiki/invitation/InvitationMemberActionsPageTest.java
@@ -0,0 +1,91 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.invitation;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.xwiki.model.reference.DocumentReference;
+import org.xwiki.rendering.RenderingScriptServiceComponentList;
+import org.xwiki.rendering.internal.configuration.DefaultExtendedRenderingConfiguration;
+import org.xwiki.rendering.internal.configuration.RenderingConfigClassDocumentConfigurationSource;
+import org.xwiki.rendering.internal.macro.message.InfoMessageMacro;
+import org.xwiki.test.annotation.ComponentList;
+import org.xwiki.test.page.HTML50ComponentList;
+import org.xwiki.test.page.PageTest;
+import org.xwiki.test.page.TestNoScriptMacro;
+import org.xwiki.test.page.XWikiSyntax21ComponentList;
+
+import com.xpn.xwiki.doc.XWikiDocument;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Test of {@code Invitation.InvitationMemberActions}.
+ *
+ * @version $Id$
+ * @since 15.0RC1
+ * @since 14.4.8
+ * @since 14.10.4
+ */
+@HTML50ComponentList
+@XWikiSyntax21ComponentList
+@RenderingScriptServiceComponentList
+@ComponentList({
+    InfoMessageMacro.class,
+    TestNoScriptMacro.class,
+    // Start - Required in addition of RenderingScriptServiceComponentList
+    DefaultExtendedRenderingConfiguration.class,
+    RenderingConfigClassDocumentConfigurationSource.class,
+    // End - Required in additional of RenderingScriptServiceComponentList
+})
+class InvitationMemberActionsPageTest extends PageTest
+{
+    private static final DocumentReference INVITATION_COMMON_DOCUMENT_REFERENCE =
+        new DocumentReference("xwiki", "Invitation", "InvitationCommon");
+
+    private static final DocumentReference INVITATION_MEMBER_ACTIONS_DOCUMENT_REFERENCE =
+        new DocumentReference("xwiki", "Invitation", "InvitationMemberActions");
+
+    @BeforeEach
+    void setUp() throws Exception
+    {
+        loadPage(INVITATION_COMMON_DOCUMENT_REFERENCE);
+    }
+
+    @Test
+    void escapeInfoMessageInternalDocumentParameter() throws Exception
+    {
+        XWikiDocument invitationGuestActionsDocument = loadPage(INVITATION_MEMBER_ACTIONS_DOCUMENT_REFERENCE);
+
+        // Set a non-guest user as otherwise the rendering stops early.
+        this.context.setUserReference(new DocumentReference("xwiki", "XWiki", "U1"));
+
+        // Set up the current doc in the context so that $doc is bound in scripts
+        this.context.setDoc(
+            this.xwiki.getDocument(new DocumentReference("xwiki", "]] {{noscript/}}", "Page"), this.context));
+
+        Document document = Jsoup.parse(invitationGuestActionsDocument.getRenderedContent(this.context));
+        Element infomessage = document.selectFirst(".infomessage");
+        assertEquals("xe.invitation.internalDocument []] {{noscript/}}.WebHome]", infomessage.text());
+    }
+}

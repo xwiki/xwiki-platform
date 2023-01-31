61b5cec4d2a1ae681d47e1ba3a5fa4f1012e6560
Thomas Mortagne <thomas.mortagne@gmail.com>
2023-01-31 15:31:55 +0100

    XWIKI-20596: In groovy a strange object is injected as variable having the name of the space * cleanup

--- xwiki-platform-core/xwiki-platform-rendering/xwiki-platform-rendering-macros/xwiki-platform-rendering-macro-python/src/main/java/org/xwiki/rendering/internal/macro/python/PythonMacro.java
+++ xwiki-platform-core/xwiki-platform-rendering/xwiki-platform-rendering-macros/xwiki-platform-rendering-macro-python/src/main/java/org/xwiki/rendering/internal/macro/python/PythonMacro.java
@@ -21,9 +21,6 @@ package org.xwiki.rendering.internal.macro.python;
 
 import javax.inject.Named;
 import javax.inject.Singleton;
-import javax.script.ScriptContext;
-import javax.script.ScriptEngine;
-import javax.script.ScriptException;
 
 import org.xwiki.component.annotation.Component;
 import org.xwiki.rendering.macro.descriptor.DefaultContentDescriptor;
@@ -57,35 +54,5 @@ public class PythonMacro extends AbstractJSR223ScriptMacro<JSR223ScriptMacroPara
     public PythonMacro()
     {
         super("Python", DESCRIPTION, new DefaultContentDescriptor(CONTENT_DESCRIPTION));
-    }
-
-    @Override
-    protected Object eval(String content, ScriptEngine engine, ScriptContext scriptContext) throws ScriptException
-    {
-        // Workaround a bug in Jython which only support ASCII in the JSR223 filename property
-        // See https://github.com/jythontools/jython/issues/170
-        String filename = (String) scriptContext.getAttribute(ScriptEngine.FILENAME);
-        if (filename != null) {
-            StringBuilder builder = null;
-            final int length = filename.length();
-            for (int i = 0; i < length; i++) {
-                char c = filename.charAt(i);
-                if (c >= 0x100) {
-                    if (builder == null) {
-                        builder = new StringBuilder();
-                        builder.append(filename.substring(0, i));
-                    }
-                    builder.append('_');
-                } else if (builder != null) {
-                    builder.append(c);
-                }
-            }
-
-            if (builder != null) {
-                scriptContext.setAttribute(ScriptEngine.FILENAME, builder.toString(), ScriptContext.ENGINE_SCOPE);
-            }
-        }
-
-        return super.eval(content, engine, scriptContext);
     }
 }

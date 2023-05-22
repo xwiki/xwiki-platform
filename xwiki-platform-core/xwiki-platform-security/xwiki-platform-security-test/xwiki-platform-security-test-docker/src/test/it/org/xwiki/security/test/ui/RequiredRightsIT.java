c5e571fd25f632b970bb6575e745069debfc14c4
Manuel Leduc <manuel.leduc@xwiki.com>
2023-05-22 15:45:16 +0200

    --wip-- [skip ci]

--- xwiki-platform-core/xwiki-platform-security/xwiki-platform-security-test/xwiki-platform-security-test-docker/src/test/it/org/xwiki/security/test/ui/RequiredRightsIT.java
+++ xwiki-platform-core/xwiki-platform-security/xwiki-platform-security-test/xwiki-platform-security-test-docker/src/test/it/org/xwiki/security/test/ui/RequiredRightsIT.java
@@ -19,13 +19,50 @@
  */
 package org.xwiki.security.test.ui;
 
+import org.junit.jupiter.api.Test;
+import org.xwiki.security.test.po.RequiredRightsAdministration;
+import org.xwiki.test.docker.junit5.TestReference;
 import org.xwiki.test.docker.junit5.UITest;
+import org.xwiki.test.ui.TestUtils;
+import org.xwiki.test.ui.po.ViewPage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.xwiki.security.test.po.RequiredRightsAdministration.goToRights;
 
 /**
+ * Test creating a page with required rights.
+ *
  * @version $Id$
  * @since 15.5RC1
  */
 @UITest
-public class RequiredRightsIT
+class RequiredRightsIT
 {
+    @Test
+    void requiredRights(TestUtils setup, TestReference testReference)
+    {
+        // Need to do the test with admin as superadmin always has all the rights.
+        setup.createAdminUser();
+        setup.loginAsAdmin();
+        // Cleanup in case of re-run.
+        setup.deletePage(testReference);
+        // Create a page with a macro requiring the Script right (velocity macro).
+        String content = "content";
+        ViewPage page = setup.createPage(testReference, "{{velocity}}" + content + "{{/velocity}}");
+        assertEquals(content, page.getContent());
+        RequiredRightsAdministration requiredRightsAdministration = goToRights(testReference);
+        requiredRightsAdministration.submit();
+        ViewPage viewPage = setup.gotoPage(testReference);
+        assertTrue(viewPage.getContent().contains(String.format("Failed to execute the [velocity] macro. "
+                + "Cause: [The execution of the [velocity] script macro is not allowed in [%s]. "
+                + "Check the rights of its last author, the required rights of the document, or the parameters if it's "
+                + "rendered from another script.]",
+            setup.serializeReference(testReference))));
+        requiredRightsAdministration = goToRights(testReference);
+        requiredRightsAdministration.clickScriptRequiredRight();
+        requiredRightsAdministration.submit();
+        setup.gotoPage(testReference);
+        assertEquals(content, page.getContent());
+    }
 }

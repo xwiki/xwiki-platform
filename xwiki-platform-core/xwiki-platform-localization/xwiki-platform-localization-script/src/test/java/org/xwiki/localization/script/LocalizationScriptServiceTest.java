2c2b13ce08a6e12fe6ca587a6cbdcada9652b3e9
Vincent Massol <vincent@massol.net>
2023-06-06 13:32:05 +0200

    [Misc] Move test to JUnit 5 + apply best practices

--- xwiki-platform-core/xwiki-platform-localization/xwiki-platform-localization-script/src/test/java/org/xwiki/localization/script/LocalizationScriptServiceTest.java
+++ xwiki-platform-core/xwiki-platform-localization/xwiki-platform-localization-script/src/test/java/org/xwiki/localization/script/LocalizationScriptServiceTest.java
@@ -24,105 +24,101 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Set;
 
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.xwiki.component.manager.ComponentManager;
 import org.xwiki.environment.Environment;
 import org.xwiki.localization.LocalizationContext;
 import org.xwiki.localization.LocalizationManager;
 import org.xwiki.rendering.renderer.BlockRenderer;
 import org.xwiki.rendering.syntax.Syntax;
-import org.xwiki.script.service.ScriptService;
-import org.xwiki.test.mockito.MockitoComponentMockingRule;
+import org.xwiki.test.junit5.mockito.ComponentTest;
+import org.xwiki.test.junit5.mockito.InjectMockComponents;
+import org.xwiki.test.junit5.mockito.MockComponent;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.when;
 
-public class LocalizationScriptServiceTest
+@ComponentTest
+class LocalizationScriptServiceTest
 {
-    @Rule
-    public MockitoComponentMockingRule<ScriptService> mocker =
-        new MockitoComponentMockingRule<>(LocalizationScriptService.class);
+    @InjectMockComponents
+    private LocalizationScriptService localizationScriptService;
 
     private BlockRenderer renderer;
 
     private ComponentManager componentManager;
 
+    @MockComponent
     private LocalizationContext localizationContext;
 
+    @MockComponent
     private LocalizationManager localizationManager;
 
-    private LocalizationScriptService localizationScriptService;
-
+    @MockComponent
     private Environment environment;
 
-    @Before
-    public void setUp() throws Exception
+    @BeforeEach
+    void setUp() throws Exception
     {
-        localizationContext = mocker.getInstance(LocalizationContext.class);
-        localizationManager = mocker.getInstance(LocalizationManager.class);
-        localizationScriptService = (LocalizationScriptService) mocker.getComponentUnderTest();
-
-        when(localizationManager.getTranslation(eq("key"), eq(Locale.ROOT), eq(Syntax.PLAIN_1_0), any())).thenReturn(
-            "messageWithParam");
-        when(localizationManager.getTranslation(eq("key"), eq(Locale.ROOT), eq(Syntax.PLAIN_1_0))).thenReturn(
+        when(this.localizationManager.getTranslation(eq("key"), eq(Locale.ROOT), eq(Syntax.PLAIN_1_0), any()))
+            .thenReturn("messageWithParam");
+        when(this.localizationManager.getTranslation("key", Locale.ROOT, Syntax.PLAIN_1_0)).thenReturn(
             "messageNoParam");
-        when(localizationContext.getCurrentLocale()).thenReturn(Locale.ROOT);
-
-        environment = mocker.getInstance(Environment.class);
+        when(this.localizationContext.getCurrentLocale()).thenReturn(Locale.ROOT);
     }
 
     @Test
-    public void render() throws Exception
+    void render()
     {
-        assertEquals("messageNoParam", localizationScriptService.render("key"));
+        assertEquals("messageNoParam", this.localizationScriptService.render("key"));
         assertEquals("messageNoParam",
-            localizationScriptService.render(Arrays.asList(null, "not existing key", "key", "another key")));
+            this.localizationScriptService.render(Arrays.asList(null, "not existing key", "key", "another key")));
     }
 
     @Test
-    public void renderWithSyntax()
+    void renderWithSyntax()
     {
-        assertEquals("messageNoParam", localizationScriptService.render("key", Syntax.PLAIN_1_0));
-        assertEquals("messageNoParam", localizationScriptService
+        assertEquals("messageNoParam", this.localizationScriptService.render("key", Syntax.PLAIN_1_0));
+        assertEquals("messageNoParam", this.localizationScriptService
             .render(Arrays.asList(null, "not existing key", "key", "another key"), Syntax.PLAIN_1_0));
     }
 
     @Test
-    public void renderWithSyntaxAndParameters()
+    void renderWithSyntaxAndParameters()
     {
-        assertEquals("messageWithParam", localizationScriptService.render("key", Syntax.PLAIN_1_0, List.of("param")));
-        assertEquals("messageWithParam", localizationScriptService
+        assertEquals("messageWithParam",
+            this.localizationScriptService.render("key", Syntax.PLAIN_1_0, List.of("param")));
+        assertEquals("messageWithParam", this.localizationScriptService
             .render(Arrays.asList(null, "not existing key", "key", "another key"), Syntax.PLAIN_1_0, List.of("param")));
     }
 
     @Test
-    public void renderWithParameters()
+    void renderWithParameters()
     {
-        assertEquals("messageWithParam", localizationScriptService.render("key", List.of("param")));
-        assertEquals("messageWithParam", localizationScriptService
+        assertEquals("messageWithParam", this.localizationScriptService.render("key", List.of("param")));
+        assertEquals("messageWithParam", this.localizationScriptService
             .render(Arrays.asList(null, "not existing key", "key", "another key"), List.of("param")));
     }
 
     @Test
-    public void getCurrentLocale()
+    void getCurrentLocale()
     {
-        when(localizationContext.getCurrentLocale()).thenReturn(Locale.ENGLISH);
-        assertEquals(Locale.ENGLISH, localizationScriptService.getCurrentLocale());
+        when(this.localizationContext.getCurrentLocale()).thenReturn(Locale.ENGLISH);
+        assertEquals(Locale.ENGLISH, this.localizationScriptService.getCurrentLocale());
     }
 
     @Test
-    public void getAvailableLocales()
+    void getAvailableLocales()
     {
-        when(environment.getResourceAsStream(eq("/WEB-INF/xwiki-locales.txt")))
+        when(this.environment.getResourceAsStream(eq("/WEB-INF/xwiki-locales.txt")))
             .thenReturn(getClass().getResourceAsStream("/xwiki-locales.txt"));
-        Set<Locale> locales = localizationScriptService.getAvailableLocales();
+        Set<Locale> locales = this.localizationScriptService.getAvailableLocales();
         assertNotNull(locales);
         assertFalse(locales.isEmpty());
         assertTrue(locales.contains(new Locale("fr")));

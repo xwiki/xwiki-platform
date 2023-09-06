356dfb4a35ec44c13e1923ddbff3118c16030d86
Manuel Leduc <manuel.leduc@xwiki.com>
2023-09-06 13:58:17 +0200

    XWIKI-21162: Advice column of the extension security vulnerability is wrong (#2314)

--- xwiki-platform-core/xwiki-platform-extension/xwiki-platform-extension-security/xwiki-platform-extension-security-api/src/main/java/org/xwiki/extension/security/internal/SolrToLiveDataEntryMapper.java
+++ xwiki-platform-core/xwiki-platform-extension/xwiki-platform-extension-security/xwiki-platform-extension-security-api/src/main/java/org/xwiki/extension/security/internal/SolrToLiveDataEntryMapper.java
@@ -25,7 +25,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.function.Function;
 import java.util.function.IntPredicate;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -36,12 +38,15 @@ import javax.script.ScriptContext;
 import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.solr.common.SolrDocument;
+import org.slf4j.Logger;
 import org.xwiki.bridge.DocumentAccessBridge;
 import org.xwiki.component.annotation.Component;
+import org.xwiki.context.ExecutionContext;
+import org.xwiki.context.ExecutionContextException;
+import org.xwiki.context.ExecutionContextManager;
 import org.xwiki.extension.ExtensionId;
 import org.xwiki.extension.InstalledExtension;
 import org.xwiki.extension.index.internal.ExtensionIndexStore;
-import org.xwiki.localization.ContextualLocalizationManager;
 import org.xwiki.model.reference.LocalDocumentReference;
 import org.xwiki.script.ScriptContextManager;
 import org.xwiki.search.solr.SolrUtils;
@@ -54,6 +59,7 @@ import static java.util.Map.ofEntries;
 import static java.util.stream.Collectors.joining;
 import static javax.script.ScriptContext.ENGINE_SCOPE;
 import static org.apache.commons.lang.StringEscapeUtils.escapeXml;
+import static org.apache.commons.lang3.exception.ExceptionUtils.getRootCauseMessage;
 import static org.xwiki.extension.index.internal.ExtensionIndexSolrCoreInitializer.IS_REVIEWED_SAFE;
 import static org.xwiki.extension.index.internal.ExtensionIndexSolrCoreInitializer.IS_SAFE_EXPLANATIONS;
 import static org.xwiki.extension.index.internal.ExtensionIndexSolrCoreInitializer.SECURITY_ADVICE;
@@ -63,6 +69,7 @@ import static org.xwiki.extension.index.internal.ExtensionIndexSolrCoreInitializ
 import static org.xwiki.extension.index.internal.ExtensionIndexSolrCoreInitializer.SECURITY_FIX_VERSION;
 import static org.xwiki.extension.index.internal.ExtensionIndexSolrCoreInitializer.SECURITY_MAX_CVSS;
 import static org.xwiki.extension.index.internal.ExtensionIndexSolrCoreInitializer.SOLR_FIELD_ID;
+import static org.xwiki.extension.security.internal.ExtensionSecurityAdvice.TRANSITIVE_DEPENDENCY_ADVICE;
 import static org.xwiki.extension.security.internal.livedata.ExtensionSecurityLiveDataConfigurationProvider.ADVICE;
 import static org.xwiki.extension.security.internal.livedata.ExtensionSecurityLiveDataConfigurationProvider.CVE_ID;
 import static org.xwiki.extension.security.internal.livedata.ExtensionSecurityLiveDataConfigurationProvider.FIX_VERSION;
@@ -82,14 +89,13 @@ public class SolrToLiveDataEntryMapper
 {
     private static final String EXTENSION_ID = "extensionId";
 
+    private static final String EXTENSION_VERSION = "extensionVersion";
+
     @Inject
     private SolrUtils solrUtils;
 
     @Inject
     private DocumentAccessBridge documentAccessBridge;
-
-    @Inject
-    private ContextualLocalizationManager l10n;
 
     @Inject
     private ExtensionIndexStore extensionIndexStore;
@@ -99,6 +105,15 @@ public class SolrToLiveDataEntryMapper
 
     @Inject
     private TemplateManager templateManager;
+
+    @Inject
+    private ExecutionContextManager contextManager;
+
+    @Inject
+    private BackwardDependenciesResolver backwardDependenciesResolver;
+
+    @Inject
+    private Logger logger;
 
     /**
      * @param doc the document to convert to Live Data entries.
@@ -121,22 +136,24 @@ public class SolrToLiveDataEntryMapper
     private String buildCVEList(SolrDocument doc)
     {
         // The CVEs of the current extension vulnerabilities.
-        ScriptContext currentScriptContext = this.scriptContextManager.getCurrentScriptContext();
-        currentScriptContext.setAttribute("cveIds", mapToStrings(doc, SECURITY_CVE_ID), ENGINE_SCOPE);
-        // The CVE links of the current extension vulnerabilities.
-        currentScriptContext.setAttribute("cveLinks", mapToStrings(doc, SECURITY_CVE_LINK), ENGINE_SCOPE);
+        return withNewExecutionContext(() -> {
+            ScriptContext currentScriptContext = this.scriptContextManager.getCurrentScriptContext();
+            currentScriptContext.setAttribute("cveIds", mapToStrings(doc, SECURITY_CVE_ID), ENGINE_SCOPE);
+            // The CVE links of the current extension vulnerabilities.
+            currentScriptContext.setAttribute("cveLinks", mapToStrings(doc, SECURITY_CVE_LINK), ENGINE_SCOPE);
 
-        // The CVSS of the current extension vulnerabilities.
-        currentScriptContext.setAttribute("cveCVSS", mapToStrings(doc, SECURITY_CVE_CVSS), ENGINE_SCOPE);
+            // The CVSS of the current extension vulnerabilities.
+            currentScriptContext.setAttribute("cveCVSS", mapToStrings(doc, SECURITY_CVE_CVSS), ENGINE_SCOPE);
 
-        List<Boolean> safe = getSafe(doc);
-        currentScriptContext.setAttribute("notSafeCVEsIndex", getNotSafeCVEsIndex(doc, safe), ENGINE_SCOPE);
-        // The index of safe CVEs.
-        currentScriptContext.setAttribute("safeCVEsIndex", getSafeCVEsIndex(doc, safe), ENGINE_SCOPE);
-        currentScriptContext.setAttribute(EXTENSION_ID, buildExtensionId(doc), ENGINE_SCOPE);
-        currentScriptContext.setAttribute("messages", mapToStrings(doc, IS_SAFE_EXPLANATIONS), ENGINE_SCOPE);
+            List<Boolean> safe = getSafe(doc);
+            currentScriptContext.setAttribute("notSafeCVEsIndex", getNotSafeCVEsIndex(doc, safe), ENGINE_SCOPE);
+            // The index of safe CVEs.
+            currentScriptContext.setAttribute("safeCVEsIndex", getSafeCVEsIndex(doc, safe), ENGINE_SCOPE);
+            currentScriptContext.setAttribute(EXTENSION_ID, buildExtensionId(doc), ENGINE_SCOPE);
+            currentScriptContext.setAttribute("messages", mapToStrings(doc, IS_SAFE_EXPLANATIONS), ENGINE_SCOPE);
 
-        return this.templateManager.renderNoException("extension/security/liveData/cveID.vm");
+            return this.templateManager.renderNoException("extension/security/liveData/cveID.vm");
+        }).orElse("");
     }
 
     private static List<Boolean> getSafe(SolrDocument doc)
@@ -177,11 +194,27 @@ public class SolrToLiveDataEntryMapper
 
     private String buildAdvice(SolrDocument doc)
     {
-        String advice = this.l10n.getTranslationPlain(this.solrUtils.get(SECURITY_ADVICE, doc));
-        if (advice == null) {
-            return "";
-        }
-        return advice;
+        return withNewExecutionContext(() -> {
+            ExtensionId extensionId = this.extensionIndexStore.getExtensionId(doc);
+            ScriptContext currentScriptContext = this.scriptContextManager.getCurrentScriptContext();
+            currentScriptContext.setAttribute(EXTENSION_ID, extensionId.getId(), ENGINE_SCOPE);
+            currentScriptContext.setAttribute(EXTENSION_VERSION, extensionId.getVersion(), ENGINE_SCOPE);
+            String adviceId = this.solrUtils.get(SECURITY_ADVICE, doc);
+            currentScriptContext.setAttribute("adviceId", adviceId, ENGINE_SCOPE);
+            if (Objects.equals(adviceId, TRANSITIVE_DEPENDENCY_ADVICE.getTranslationId())) {
+                currentScriptContext.setAttribute("backwardDependencies",
+                    this.backwardDependenciesResolver.getExplicitlyInstalledBackwardDependencies(extensionId),
+                    ENGINE_SCOPE);
+            }
+
+            currentScriptContext.setAttribute("extensionManagerUrl", getExtensionManagerLink(extensionId),
+                ENGINE_SCOPE);
+
+            // Provide a lambda to easily resolve a link to an extension in the extension manager by its id.
+            currentScriptContext.setAttribute("extensionManagerLinkResolver",
+                (Function<ExtensionId, String>) this::getExtensionManagerLink, ENGINE_SCOPE);
+            return this.templateManager.renderNoException("extension/security/liveData/advice.vm");
+        }).orElse("");
     }
 
     private String buildFixVersion(SolrDocument doc)
@@ -209,8 +242,6 @@ public class SolrToLiveDataEntryMapper
         // If the extension does not have a name and version indexed, we fall back to an extensionId parsing.
         // Note: this is not supposed to happen in practice.
         ExtensionId extensionId = this.extensionIndexStore.getExtensionId(doc);
-        List<BasicNameValuePair> parameters =
-            buildExtensionURLParameters(extensionId.getId(), extensionId.getVersion().getValue());
         if (doc.get(FieldUtils.NAME) != null) {
             extensionName = this.solrUtils.get(FieldUtils.NAME, doc);
         } else {
@@ -218,8 +249,7 @@ public class SolrToLiveDataEntryMapper
             String[] versionId = this.solrUtils.getId(doc).split("/");
             extensionName = versionId[0];
         }
-        String url = this.documentAccessBridge.getDocumentURL(new LocalDocumentReference("XWiki", "Extensions"),
-            VIEW_ACTION, URLEncodedUtils.format(parameters, Charset.defaultCharset()), null);
+        String url = getExtensionManagerLink(extensionId);
 
         String extensionNameEscaped = escapeXml(String.valueOf(extensionName));
         String extensionIdEscaped = escapeXml(buildExtensionId(doc));
@@ -229,15 +259,6 @@ public class SolrToLiveDataEntryMapper
             extensionNameEscaped,
             extensionIdEscaped,
             extensionIdEscaped
-        );
-    }
-
-    private static List<BasicNameValuePair> buildExtensionURLParameters(String extensionId, String extensionVersion)
-    {
-        return List.of(
-            new BasicNameValuePair("section", "XWiki.Extensions"),
-            new BasicNameValuePair(EXTENSION_ID, extensionId),
-            new BasicNameValuePair("extensionVersion", extensionVersion)
         );
     }
 
@@ -252,5 +273,45 @@ public class SolrToLiveDataEntryMapper
             .map(it -> it.replaceFirst("wiki:", ""))
             .filter(it -> !Objects.equals(it, "{root}"))
             .collect(joining(", "));
+    }
+
+    /**
+     * Executes the given supplier in a new execution context.
+     *
+     * @param supplier the supplier to execute
+     * @param <T> the type of the result
+     * @return an {@link Optional} containing the result of the supplier execution, or an {@link Optional#empty()} if
+     *     the execution context fails to initialize
+     */
+    private <T> Optional<T> withNewExecutionContext(Supplier<T> supplier)
+    {
+        try {
+            this.contextManager.pushContext(new ExecutionContext(), true);
+            try {
+                return Optional.of(supplier.get());
+            } finally {
+                this.contextManager.popContext();
+            }
+        } catch (ExecutionContextException e) {
+            this.logger.warn("Failed to initialize a new execution context. Cause: [{}]", getRootCauseMessage(e));
+            return Optional.empty();
+        }
+    }
+
+    /**
+     * Returns the link to the extension manager for a given extension.
+     *
+     * @param extensionId the ID of the extension
+     * @return the link to the extension in the extension manager
+     */
+    private String getExtensionManagerLink(ExtensionId extensionId)
+    {
+        List<BasicNameValuePair> parameters = List.of(
+            new BasicNameValuePair("section", "XWiki.Extensions"),
+            new BasicNameValuePair(SolrToLiveDataEntryMapper.EXTENSION_ID, extensionId.getId()),
+            new BasicNameValuePair(EXTENSION_VERSION, extensionId.getVersion().getValue())
+        );
+        return this.documentAccessBridge.getDocumentURL(new LocalDocumentReference("XWiki", "Extensions"),
+            VIEW_ACTION, URLEncodedUtils.format(parameters, Charset.defaultCharset()), null);
     }
 }

88305846a84dbfc3445d1b955edf507157d4252e
Simon Urli <simon.urli@xwiki.com>
2023-09-11 10:04:50 +0200

    XWIKI-21299: Multiple mail received with live email notifications (#2333)
      * UserEventDispatcher should take care of not querying twice the same
        events
      * Ensure to wait for the latest save to be done before moving to the
        next event, so that the event status is properly set and cannot be
    queried anymore
      * Split existing dispatcher class to have the scheduler and the
        dispatching code separated to simplify testing
      * Provide unit test for the dispatcher
      * Small improvment in MailEntityQueryCondition to allow comparing them
        in tests
      * Change a bit the implem of UserEventDispatcher to make dispatch
        returns the latest save, instead of having a field for it

--- xwiki-platform-core/xwiki-platform-notifications/xwiki-platform-notifications-notifiers/xwiki-platform-notifications-notifiers-default/src/main/java/org/xwiki/notifications/notifiers/internal/UserEventDispatcherListener.java
+++ xwiki-platform-core/xwiki-platform-notifications/xwiki-platform-notifications-notifiers/xwiki-platform-notifications-notifiers-default/src/main/java/org/xwiki/notifications/notifiers/internal/UserEventDispatcherListener.java
@@ -48,7 +48,7 @@ public class UserEventDispatcherListener extends AbstractEventListener
     public static final String NAME = "org.xwiki.notifications.notifiers.internal.UserEventDispatcherListener";
 
     @Inject
-    private UserEventDispatcher dispatcher;
+    private UserEventDispatcherScheduler scheduler;
 
     @Inject
     private NotificationConfiguration notificationConfiguration;
@@ -72,10 +72,10 @@ public class UserEventDispatcherListener extends AbstractEventListener
             if (event instanceof EventStreamAddedEvent) {
                 if (!this.remoteState.isRemoteState()) {
                     // Make sure to wakeup dispatcher
-                    this.dispatcher.onEvent((org.xwiki.eventstream.Event) source);
+                    this.scheduler.onEvent((org.xwiki.eventstream.Event) source);
                 }
             } else if (event instanceof ApplicationReadyEvent) {
-                this.dispatcher.initialize();
+                this.scheduler.initialize();
             }
         }
     }

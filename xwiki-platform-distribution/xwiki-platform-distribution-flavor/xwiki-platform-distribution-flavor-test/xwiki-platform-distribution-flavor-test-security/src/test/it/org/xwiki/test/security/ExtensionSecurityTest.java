49b940cd5a3d2d9dd25dfea80fa90df366a63c7a
Manuel Leduc <manuel.leduc@xwiki.com>
2023-10-09 16:00:47 +0200

    XWIKI-21413: Make the Security Vulnerabilities Application not bundled by default

--- xwiki-platform-distribution/xwiki-platform-distribution-flavor/xwiki-platform-distribution-flavor-test/xwiki-platform-distribution-flavor-test-security/src/test/it/org/xwiki/test/security/ExtensionSecurityTest.java
+++ xwiki-platform-distribution/xwiki-platform-distribution-flavor/xwiki-platform-distribution-flavor-test/xwiki-platform-distribution-flavor-test-security/src/test/it/org/xwiki/test/security/ExtensionSecurityTest.java
@@ -20,24 +20,16 @@
 package org.xwiki.test.security;
 
 import java.util.List;
-import java.util.UUID;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.xwiki.component.embed.EmbeddableComponentManager;
 import org.xwiki.extension.ExtensionId;
-import org.xwiki.extension.job.InstallRequest;
-import org.xwiki.extension.job.internal.InstallJob;
 import org.xwiki.extension.security.test.po.ExtensionVulnerabilitiesAdminPage;
 import org.xwiki.extension.test.po.ExtensionAdministrationPage;
-import org.xwiki.model.reference.DocumentReference;
-import org.xwiki.rest.internal.ModelFactory;
-import org.xwiki.rest.model.jaxb.JobRequest;
-import org.xwiki.test.docker.internal.junit5.JobExecutor;
+import org.xwiki.test.extension.RestExtensionInstaller;
 import org.xwiki.test.ui.AbstractTest;
 import org.xwiki.test.ui.AdminAuthenticationRule;
-import org.xwiki.test.ui.TestUtils;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.xwiki.extension.security.test.po.ExtensionVulnerabilitiesAdminPage.goToExtensionVulnerabilitiesAdmin;
@@ -59,7 +51,11 @@ public class ExtensionSecurityTest extends AbstractTest
     @Test
     public void checkForExtensionSecurityScanResult() throws Exception
     {
-        installExtensionSecurityUI();
+        ExtensionId extensionId =
+            new ExtensionId("org.xwiki.platform:xwiki-platform-extension-security-ui", getUtil().getVersion());
+        componentManager.initialize(getClass().getClassLoader());
+        new RestExtensionInstaller(componentManager).installExtensions(getUtil().getBaseURL(), List.of(extensionId),
+            ADMIN_CREDENTIALS, ADMIN_CREDENTIALS.getUserName(), null, false);
 
         ExtensionAdministrationPage.gotoPage().startIndex();
         waitForSuccessMessage();
@@ -80,59 +76,6 @@ public class ExtensionSecurityTest extends AbstractTest
             }
             return String.format("The following list of security issues are to be analyzed:\n%s", stringBuilder);
         });
-    }
-
-    private static void installExtensionSecurityUI() throws Exception
-    {
-        TestUtils testUtils = new TestUtils();
-        String xwikiRESTURL = String.format("%srest", getUtil().getBaseURL());
-
-        // Install the extensions
-        try {
-            ExtensionId extensionId =
-                new ExtensionId("org.xwiki.platform:xwiki-platform-extension-security-ui", testUtils.getVersion());
-            InstallRequest installRequest =
-                createInstallRequest(extensionId, List.of("wiki:" + testUtils.getCurrentWiki()),
-                    ADMIN_CREDENTIALS.getUserName());
-
-            EmbeddableComponentManager componentManager = new EmbeddableComponentManager();
-            componentManager.initialize(ExtensionSecurityTest.class.getClassLoader());
-
-            JobExecutor jobExecutor = new JobExecutor();
-            JobRequest request =
-                componentManager.<ModelFactory>getInstance(ModelFactory.class).toRestJobRequest(installRequest);
-            jobExecutor.execute(InstallJob.JOBTYPE, request, xwikiRESTURL, ADMIN_CREDENTIALS);
-        } catch (Exception e) {
-            throw new Exception(String.format("Failed to install Extension(s) into XWiki at [%s]", xwikiRESTURL), e);
-        }
-    }
-
-    private static InstallRequest createInstallRequest(ExtensionId extensionId, List<String> namespaces,
-        String installUserReference)
-    {
-        InstallRequest installRequest = new InstallRequest();
-
-        // Set a job id to save the job result
-        installRequest.setId("extension", "provision", UUID.randomUUID().toString());
-
-        installRequest.setInteractive(false);
-        installRequest.setFailOnExist(true);
-
-        // Set the extension list to install
-
-        installRequest.addExtension(extensionId);
-
-        // Set the namespaces into which to install the extensions
-        for (String namespace : namespaces) {
-            installRequest.addNamespace(namespace);
-        }
-
-        // Set any user for installing pages (if defined)
-        if (installUserReference != null) {
-            installRequest.setProperty("user.reference",
-                new DocumentReference("xwiki", "XWiki", ADMIN_CREDENTIALS.getUserName()));
-        }
-        return installRequest;
     }
 
     private static void waitForSuccessMessage()

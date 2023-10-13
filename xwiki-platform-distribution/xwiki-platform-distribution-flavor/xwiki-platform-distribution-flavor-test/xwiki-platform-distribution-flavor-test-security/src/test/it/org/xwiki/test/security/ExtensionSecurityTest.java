77b4f8738127b2f7700977e37b944bd108b18368
Manuel Leduc <manuel.leduc@xwiki.com>
2023-10-13 14:02:59 +0200

    XWIKI-21413: Make the extension security extension non bundled by default (#2445)
    (cherry picked from commit 2ad006349c791d68d16dde6253c6260b0987fecd)

--- xwiki-platform-distribution/xwiki-platform-distribution-flavor/xwiki-platform-distribution-flavor-test/xwiki-platform-distribution-flavor-test-security/src/test/it/org/xwiki/test/security/ExtensionSecurityTest.java
+++ xwiki-platform-distribution/xwiki-platform-distribution-flavor/xwiki-platform-distribution-flavor-test/xwiki-platform-distribution-flavor-test-security/src/test/it/org/xwiki/test/security/ExtensionSecurityTest.java
@@ -24,13 +24,16 @@ import java.util.List;
 import org.junit.Rule;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.xwiki.extension.ExtensionId;
 import org.xwiki.extension.security.test.po.ExtensionVulnerabilitiesAdminPage;
 import org.xwiki.extension.test.po.ExtensionAdministrationPage;
+import org.xwiki.test.extension.RestExtensionInstaller;
 import org.xwiki.test.ui.AbstractTest;
 import org.xwiki.test.ui.AdminAuthenticationRule;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.xwiki.extension.security.test.po.ExtensionVulnerabilitiesAdminPage.goToExtensionVulnerabilitiesAdmin;
+import static org.xwiki.test.ui.TestUtils.ADMIN_CREDENTIALS;
 
 /**
  * Performs a security scan and checks for any security issues. The test fails if any non-review security issue is
@@ -46,9 +49,15 @@ public class ExtensionSecurityTest extends AbstractTest
     public AdminAuthenticationRule adminAuthenticationRule = new AdminAuthenticationRule(getUtil());
 
     @Test
-    public void checkForExtensionSecurityScanResult()
+    public void checkForExtensionSecurityScanResult() throws Exception
     {
-        ExtensionAdministrationPage.gotoPage();
+        ExtensionId extensionId =
+            new ExtensionId("org.xwiki.platform:xwiki-platform-extension-security-ui", getUtil().getVersion());
+        componentManager.initialize(getClass().getClassLoader());
+        new RestExtensionInstaller(componentManager).installExtensions(getUtil().getBaseURL(), List.of(extensionId),
+            ADMIN_CREDENTIALS, ADMIN_CREDENTIALS.getUserName(), null, false);
+
+        ExtensionAdministrationPage.gotoPage().startIndex();
         waitForSuccessMessage();
 
         ExtensionVulnerabilitiesAdminPage extensionVulnerabilitiesAdminPage = goToExtensionVulnerabilitiesAdmin();

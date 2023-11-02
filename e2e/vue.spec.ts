d03fb82b16d0ec0b7a67446005a5fe8c09c3a69b
Manuel Leduc <manuel.leduc@xwiki.com>
2023-11-02 16:59:38 +0100

    [Misc] Axe core integration

--- e2e/vue.spec.ts
+++ e2e/vue.spec.ts
@@ -1,8 +1,13 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from "@playwright/test";
+import { AxeBuilder } from "@axe-core/playwright";
 
 // See here how to get started:
 // https://playwright.dev/docs/intro
-test('visits the app root url', async ({ page }) => {
-  await page.goto('/');
-  await expect(page.locator('div.greetings > h1')).toHaveText('You did it!');
-})
+test("visits the app root url", async ({ page }) => {
+  await page.goto("/");
+  await expect(page.locator("div.greetings > h1")).toHaveText("You did it!");
+  const axeResults = await new AxeBuilder({ page })
+    .withTags(["wcag2a", "wcag2aa"])
+    .analyze();
+  expect(axeResults.violations).toBe([])
+});
--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,10 @@ const config: PlaywrightTestConfig = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['junit', { outputFile: 'e2e-tests.xml' }]],
+  reporter: [
+    ['junit', { outputFile: 'e2e-tests.xml' }],
+    ['html']
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */

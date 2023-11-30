cb0e3aafff88714108634db01f49a84e774dd17a
Manuel Leduc <manuel.leduc@xwiki.com>
2023-11-30 14:32:53 +0100

    CRISTAL-15: Allow to define the start port from the environment

--- web/src/server.js
+++ web/src/server.js
@@ -1,3 +1,28 @@
+/**
+ * See the LICENSE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ *
+ * This file is part of the Cristal Wiki software prototype
+ * @copyright  Copyright (c) 2023 XWiki SAS
+ * @license    http://opensource.org/licenses/AGPL-3.0 AGPL-3.0
+ *
+ **/
+
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
@@ -12,6 +37,11 @@ app.get("*", function (req, res) {
   res.send(contentFromHtmlFile);
 });
 
-app.listen(9001, function () {
-  console.log("Application is running on http://localhost:9000");
+let port = 9000;
+const env_port = parseInt(process.env.HTTP_PORT);
+if (!isNaN(env_port) && env_port > 0) {
+  port = env_port;
+}
+app.listen(port, function () {
+  console.log(`Application is running on http://localhost:${port}`);
 });

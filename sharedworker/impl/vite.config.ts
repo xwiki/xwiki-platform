743ba578f5e7c562374eba9e853464d14441ff14
Ludovic Dubost <ludovic@xwiki.com>
2023-12-01 17:09:47 +0100

    misc: changed shareworker module name, fix formatting

--- sharedworker/impl/vite.config.ts
+++ sharedworker/impl/vite.config.ts
@@ -1,23 +1,19 @@
-import { defineConfig, mergeConfig } from 'vite';
-import defaultConfig from './../../vite.vue.config.js';
-import { comlink } from 'vite-plugin-comlink';
+import { defineConfig, mergeConfig } from "vite";
+import defaultConfig from "./../../vite.vue.config.js";
+import { comlink } from "vite-plugin-comlink";
 
-export default mergeConfig( 
-  defaultConfig, 
+export default mergeConfig(
+  defaultConfig,
   defineConfig({
-        plugins: [
-		comlink()
-        ],
-	build: {
-    		lib: {
-      			entry: "./src/index.ts",
-			name: 'shareworker'
-    		}
-  	},
-        worker: {
-		plugins : [
-			comlink()
-		]
-	}
-}));
-
+    plugins: [comlink()],
+    build: {
+      lib: {
+        entry: "./src/index.ts",
+        name: "shareworkerimpl",
+      },
+    },
+    worker: {
+      plugins: [comlink()],
+    },
+  }),
+);

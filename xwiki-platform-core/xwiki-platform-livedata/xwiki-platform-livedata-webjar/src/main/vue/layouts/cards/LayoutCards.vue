f3f4082bbdf0cafe29e4436c6336bb3b7f543000
Simon Urli <simon.urli@xwiki.com>
2023-12-04 11:51:15 +0100

    XWIKI-21659: Provide entry index in data attribute of LiveData (#2680)
    Add data-livedata-entry-index and data-livedata-entry-id attributes on the parent DOM node of the entries, for both cards and table layouts
    
    ---------
    
    Co-authored-by: Manuel Leduc <manuel.leduc@xwiki.com>
    (cherry picked from commit e8792642fd9a0567cdc09a69c4b7bfbd516ef2a1)

--- xwiki-platform-core/xwiki-platform-livedata/xwiki-platform-livedata-webjar/src/main/vue/layouts/cards/LayoutCards.vue
+++ xwiki-platform-core/xwiki-platform-livedata/xwiki-platform-livedata-webjar/src/main/vue/layouts/cards/LayoutCards.vue
@@ -65,6 +65,7 @@
         v-for="(entry, idx) in entries"
         :key="`card-${logic.getEntryId(entry)}-${idx}`"
         :entry="entry"
+        :entry-idx="idx"
       />
 
       <!-- Component to create a new entry -->

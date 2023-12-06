864b32deb5575857a81accc10eb3e6f40c6ffc8f
Sereza7 <lucas.charpentier@imt-atlantique.net>
2023-12-06 11:53:33 +0100

    XWIKI-21492: Underline inline links * Added back the preference for text size (flawed implementation like what it used to be) * Fixed the place where we loaded the CSS

--- xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/internal/mandatory/XWikiUsersDocumentInitializer.java
+++ xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/internal/mandatory/XWikiUsersDocumentInitializer.java
@@ -102,6 +102,7 @@ public class XWikiUsersDocumentInitializer extends AbstractMandatoryClassInitial
         xclass.addTextField("imaccount", "imaccount", 30);
         xclass.addStaticListField("editor", "Default Editor", "Text|Wysiwyg");
         xclass.addStaticListField("usertype", "User type", "Simple|Advanced", "Simple");
+        xclass.addStaticListField("font_size", "Font size", "Regular|Large|Larger|Largest", "Regular");
         xclass.addStaticListField("underline", "Underline links", "Yes|Only inline links|No", "Only inline links");
         xclass.addBooleanField("displayHiddenDocuments", "Display Hidden Documents", "yesno");
         xclass.addTimezoneField(TIMEZONE_FIELD, "Time Zone", 30);

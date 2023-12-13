cee7e328f34caeb2e45fc82b593dffdf96334f2e
Sereza7 <lucas.charpentier@imt-atlantique.net>
2023-12-13 15:59:05 +0100

    XWIKI-21452: Macros info, success, warning and error are only distinguished by colors * Implemented an alternative to isntantiate icons in rendering * Updated the POM * Fixed the components.txt file
    Note: I decided to not factorize witht the code in the DisplayIconMacro because we don't need anything related to a parameter.

--- xwiki-platform-core/xwiki-platform-rendering/xwiki-platform-rendering-transformations/xwiki-platform-rendering-transformation-icon/src/main/java/org/xwiki/rendering/internal/transformation/icon/XWikiIconProvider.java
+++ xwiki-platform-core/xwiki-platform-rendering/xwiki-platform-rendering-transformations/xwiki-platform-rendering-transformation-icon/src/main/java/org/xwiki/rendering/internal/transformation/icon/XWikiIconProvider.java
@@ -20,25 +20,74 @@
 package org.xwiki.rendering.internal.transformation.icon;
 
 import org.xwiki.component.annotation.Component;
+import org.xwiki.icon.IconException;
+import org.xwiki.icon.IconRenderer;
+import org.xwiki.icon.IconSet;
+import org.xwiki.icon.IconSetManager;
 import org.xwiki.rendering.block.Block;
-import org.xwiki.rendering.block.NewLineBlock;
+import org.xwiki.rendering.block.ImageBlock;
+import org.xwiki.rendering.block.RawBlock;
+import org.xwiki.rendering.syntax.Syntax;
 
+import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Component to use the icon theme to provide a proper block for displaying an icon.
+ *
+ * @version $Id$
+ * @since 15.10.2
  */
 @Component
 @Singleton
 public class XWikiIconProvider extends DefaultIconProvider
 {
+    /*
+    We need to update the icon class in order to take into account the situations
+    where we fall back on the default behaviour (the current icon theme does not have
+    the requested icon).
+     */
+    private static final List<Class> ICON_CLASS = new ArrayList<Class>(List.of(RawBlock.class, ImageBlock.class));
+
+    @Inject
+    private IconSetManager iconSetManager;
+    @Inject
+    private IconRenderer iconRenderer;
+
     /**
      * Uses the icon theme to provide the right block for displaying an icon.
      * @param iconName the name of the icon to display
      * @return the block containing an icon.
      */
+    @Override
     public Block get(String iconName)
     {
-        return new RawBlock(rawContent, syntax);
+        IconSet iconSet = null;
+        try {
+            iconSet = getIconSet(iconName);
+            String iconContent = this.iconRenderer.renderHTML(iconName, iconSet);
+            return new RawBlock(iconContent, Syntax.HTML_5_0);
+        } catch (IconException e) {
+            return super.get(iconName);
+        }
+    }
+
+    /**
+     * @return the java class of the icon block created.
+     */
+    public List<Class> getIconClass()
+    {
+        return ICON_CLASS;
+    }
+
+    private IconSet getIconSet(String iconName) throws IconException
+    {
+        IconSet iconSet = this.iconSetManager.getCurrentIconSet();
+        if (iconSet == null || !iconSet.hasIcon(iconName)) {
+            iconSet = this.iconSetManager.getDefaultIconSet();
+        }
+        return iconSet;
     }
 }

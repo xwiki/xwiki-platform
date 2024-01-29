68d2b034c250d4235f9354da0cd80e43776fa44d
Sereza7 <lucas.charpentier@imt-atlantique.net>
2024-01-29 18:19:50 +0100

    XWIKI-12824: Add buttons to hide/show panels * Added logic for animation of panel columns * Fixed the toggler caret rotation on click
    TODO:
    * Add the transition in itself on the aria-expanded attribute of the toggler.
    * Review the style changes to optimize * organize them.

--- xwiki-platform-core/xwiki-platform-web/xwiki-platform-web-war/src/main/webapp/resources/js/xwiki/togglePanels.js
+++ xwiki-platform-core/xwiki-platform-web/xwiki-platform-web-war/src/main/webapp/resources/js/xwiki/togglePanels.js
@@ -18,5 +18,75 @@
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
 require(['jquery'], function($) {
-    let
+  let rightToggler = $("#rightPanelsToggler");
+  let leftToggler = $("#leftPanelsToggler");
+
+  /* The body update hides completely the panel column. We want to make sure the panel column is visible for the whole
+  animation before hiding it. */
+  let updateBody = function (isRight, newExpandedState) {
+    let controlledBody = $('body.content');
+    if (isRight) {
+      if (newExpandedState) {
+        // We remove the hideright class
+        if (controlledBody.hasClass('hidelefthideright')) {
+          controlledBody.removeClass('hidelefthideright');
+          controlledBody.addClass('hideleft');
+        } else {
+          controlledBody.removeClass('hideright');
+        }
+      } else {
+        // We add the hideright class
+        if (controlledBody.hasClass('hideleft')) {
+          controlledBody.addClass('hidelefthideright');
+          controlledBody.removeClass('hideleft');
+        } else {
+          controlledBody.addClass('hideright');
+        }
+      }
+    } else {
+      if (newExpandedState) {
+        // We remove the hideleft class
+        if (controlledBody.hasClass('hidelefthideright')) {
+          controlledBody.removeClass('hidelefthideright');
+          controlledBody.addClass('hideright');
+        } else {
+          controlledBody.removeClass('hideleft');
+        }
+      } else {
+        // We add the hideleft class
+        if (controlledBody.hasClass('hideright')) {
+          controlledBody.addClass('hidelefthideright');
+          controlledBody.removeClass('hideright');
+        } else {
+          controlledBody.addClass('hideleft');
+        }
+      }
+    }
+  }
+  var togglePanels = function (toggler, isRight) {
+    let newExpandedState = toggler.attr('aria-expanded') === 'false';
+
+    if (newExpandedState) {
+      // If we want to expand the panel, we first show it, then we slide it in view.
+      updateBody(isRight, newExpandedState);
+      toggler.attr('aria-expanded', newExpandedState);
+    } else {
+      // If we want to collapse the panel, we first slide it out of view and then we hide it.
+      toggler.attr('aria-expanded', newExpandedState);
+      let panels;
+      if (isRight) {
+        panels = $('#rightPanels');
+      } else {
+        panels = $('#leftPanels');
+      }
+      panels.on('transitionend', function () {
+        updateBody(isRight, newExpandedState);
+        // Remove this listener after one call.
+        panels.off('transitionend');
+      });
+    }
+  }
+
+  rightToggler.on('click', function(){togglePanels(rightToggler, true)});
+  leftToggler.on('click', function(){togglePanels(leftToggler, false)});
 });

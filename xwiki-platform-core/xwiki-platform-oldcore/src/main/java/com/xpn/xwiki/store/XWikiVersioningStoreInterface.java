eaddc5e29d7b3c2ff7115d78c7cbed1b1dbf2b46
pjeanjean <pierre.jeanjean@xwiki.com>
2024-02-23 13:33:25 +0100

    XWIKI-21922: Introduce methods to fetch a subset of revisions in XWikiVersioningStoreInterface
    * Introduce default methods XWikiVersioningStoreInterface.getXWikiDocVersions and XWikiVersioningStoreInterface.getXWikiDocVersionsCount, taking RevisionCriteria as parameter
    * Introduce implementations for both in XWikiHibernateVersioningStore
    * Add XWikiDocument.getRevisionsCount and Document.getRevisionsCount
    * Edit XWikiDocument.getRevisions(RevisionCriteria) to use the new method
    * Move hibernate queries from XWikiHibernateVersioningStore to VersioningStoreQueryFactory to even out class fan-out complexity

--- xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/store/XWikiVersioningStoreInterface.java
+++ xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/store/XWikiVersioningStoreInterface.java
@@ -19,15 +19,23 @@
  */
 package com.xpn.xwiki.store;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
 import org.suigeneris.jrcs.rcs.Version;
 import org.xwiki.component.annotation.Role;
+import org.xwiki.stability.Unstable;
 
 import com.xpn.xwiki.XWikiContext;
 import com.xpn.xwiki.XWikiException;
+import com.xpn.xwiki.criteria.impl.RevisionCriteria;
 import com.xpn.xwiki.doc.XWikiDocument;
 import com.xpn.xwiki.doc.XWikiDocumentArchive;
 import com.xpn.xwiki.doc.rcs.XWikiRCSNodeContent;
 import com.xpn.xwiki.doc.rcs.XWikiRCSNodeId;
+import com.xpn.xwiki.doc.rcs.XWikiRCSNodeInfo;
 
 /**
  * Interface for manipulate document history.
@@ -46,6 +54,79 @@ public interface XWikiVersioningStoreInterface
     void updateXWikiDocArchive(XWikiDocument doc, boolean bTransaction, XWikiContext context) throws XWikiException;
 
     Version[] getXWikiDocVersions(XWikiDocument doc, XWikiContext context) throws XWikiException;
+
+    /**
+     * Gets versions from a given document matching criteria like author, minimum creation date, etc.
+     *
+     * @param doc the document
+     * @param criteria criteria used to match versions
+     * @param context the XWiki context
+     * @return the matching versions
+     * @since 15.10.7
+     * @since 16.2.0RC1
+     */
+    @Unstable
+    default Collection<Version> getXWikiDocVersions(XWikiDocument doc, RevisionCriteria criteria, XWikiContext context)
+        throws XWikiException
+    {
+        List<String> results = new ArrayList<>();
+
+        Version[] revisions = getXWikiDocVersions(doc, context);
+        XWikiDocumentArchive archive = getXWikiDocumentArchive(doc, context);
+
+        XWikiRCSNodeInfo nextNodeinfo = null;
+        XWikiRCSNodeInfo nodeinfo;
+        for (Version revision : revisions) {
+            nodeinfo = nextNodeinfo;
+            nextNodeinfo = archive.getNode(revision);
+
+            if (nodeinfo == null) {
+                continue;
+            }
+
+            // Minor/Major version matching
+            if (criteria.getIncludeMinorVersions() || !nextNodeinfo.isMinorEdit()) {
+                // Author matching
+                if (criteria.getAuthor().equals("") || criteria.getAuthor().equals(nodeinfo.getAuthor())) {
+                    // Date range matching
+                    Date versionDate = nodeinfo.getDate();
+                    if (versionDate.after(criteria.getMinDate()) && versionDate.before(criteria.getMaxDate())) {
+                        results.add(nodeinfo.getVersion().toString());
+                    }
+                }
+            }
+        }
+
+        nodeinfo = nextNodeinfo;
+        if (nodeinfo != null) {
+            if (criteria.getAuthor().isEmpty() || criteria.getAuthor().equals(nodeinfo.getAuthor())) {
+                // Date range matching
+                Date versionDate = nodeinfo.getDate();
+                if (versionDate.after(criteria.getMinDate()) && versionDate.before(criteria.getMaxDate())) {
+                    results.add(nodeinfo.getVersion().toString());
+                }
+            }
+        }
+
+        return criteria.getRange().subList(results).stream().map(Version::new).toList();
+    }
+
+    /**
+     * Gets the number of versions from a given document matching criteria like author, minimum creation date, etc.
+     *
+     * @param doc the document
+     * @param criteria criteria used to match versions
+     * @param context the XWiki context
+     * @return the number of matching versions
+     * @since 15.10.7
+     * @since 16.2.0RC1
+     */
+    @Unstable
+    default long getXWikiDocVersionsCount(XWikiDocument doc, RevisionCriteria criteria, XWikiContext context)
+        throws XWikiException
+    {
+        return getXWikiDocVersions(doc, criteria, context).size();
+    };
 
     XWikiDocument loadXWikiDoc(XWikiDocument doc, String version, XWikiContext context) throws XWikiException;
 

ea97599cde03cf3c2bd3e52f28c19dfb57f67889
Michael Hamann <michael.hamann@xwiki.com>
2024-03-01 14:59:24 +0100

    XWIKI-20632: Page searched by its exact name is not found in a page picker for a macro parameter when its subtree is larger than 10 pages
    * Add tests.

--- xwiki-platform-core/xwiki-platform-flamingo/xwiki-platform-flamingo-skin/xwiki-platform-flamingo-skin-test/xwiki-platform-flamingo-skin-test-docker/src/test/it/org/xwiki/flamingo/test/docker/PagePickerIT.java
+++ xwiki-platform-core/xwiki-platform-flamingo/xwiki-platform-flamingo-skin/xwiki-platform-flamingo-skin-test/xwiki-platform-flamingo-skin-test-docker/src/test/it/org/xwiki/flamingo/test/docker/PagePickerIT.java
@@ -19,13 +19,18 @@
  */
 package org.xwiki.flamingo.test.docker;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.xwiki.model.reference.DocumentReference;
 import org.xwiki.test.docker.junit5.TestReference;
 import org.xwiki.test.docker.junit5.UITest;
 import org.xwiki.test.ui.TestUtils;
 import org.xwiki.test.ui.po.SuggestInputElement;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Functional tests for the Page Picker.
@@ -35,6 +40,8 @@ import org.xwiki.test.ui.po.SuggestInputElement;
 @UITest
 class PagePickerIT
 {
+    private static final String PICKER_ID = "pagePickerTest";
+
     /**
      * See XWIKI-16078: Selected (and deleted) users/subgroups are not displayed properly in drop-down when editing a
      * group
@@ -45,12 +52,12 @@ class PagePickerIT
     {
         setup.loginAsSuperAdmin();
         String pageName = reference.getLastSpaceReference().getName();
-        setup.createPage(reference,
-            "{{velocity}}{{html}}#pagePicker({'id': 'pagePickerTest', 'multiple': true}){{/html}}{{/velocity}}",
+        setup.createPage(reference, String.format(
+                "{{velocity}}{{html}}#pagePicker({'id': '%s', 'multiple': true}){{/html}}{{/velocity}}", PICKER_ID),
             pageName);
 
         SuggestInputElement pagePicker =
-            new SuggestInputElement(setup.getDriver().findElementWithoutWaiting(By.id("pagePickerTest")));
+            new SuggestInputElement(setup.getDriver().findElementWithoutWaiting(By.id(PICKER_ID)));
 
         // Make sure the picker is ready. TODO: remove once XWIKI-19056 is closed.
         pagePicker.click().waitForSuggestions();
@@ -58,5 +65,39 @@ class PagePickerIT
         pagePicker.sendKeys(pageName.substring(0, 3)).waitForSuggestions().selectByVisibleText(pageName);
         pagePicker.clearSelectedSuggestions().sendKeys(pageName.substring(0, 3)).waitForSuggestions()
             .selectByVisibleText(pageName);
+    }
+
+    /**
+     * Verify that spaces can be selected even if they contain many children.
+     */
+    @Test
+    @Order(2)
+    void selectSpaceWithManyChildren(TestUtils setup, TestReference reference) throws Exception
+    {
+        setup.loginAsSuperAdmin();
+
+        String childName = "Child";
+        for (int i = 0; i < 20; i++) {
+            DocumentReference childReference = new DocumentReference(childName + i,
+                reference.getLastSpaceReference());
+            setup.createPage(childReference, "Test page " + i, "Child page " + i);
+        }
+
+        String pageName = reference.getLastSpaceReference().getName();
+        setup.createPage(reference,
+            String.format("{{velocity}}{{html}}#pagePicker({'id': '%s'}){{/html}}{{/velocity}}", PICKER_ID),
+            pageName);
+
+        SuggestInputElement pagePicker =
+            new SuggestInputElement(setup.getDriver().findElementWithoutWaiting(By.id(PICKER_ID)));
+
+        // Search for the space and ensure that we get it (and just that space, not any of the children).
+        List<SuggestInputElement.SuggestionElement> suggestions =
+            pagePicker.sendKeys(pageName).waitForSuggestions().getSuggestions();
+        assertEquals(1, suggestions.size());
+        assertEquals(pageName, suggestions.get(0).getLabel());
+        // Just to be sure that searching for the children also works, search and select the first child.
+        pagePicker.clear().sendKeys(childName).waitForSuggestions()
+            .selectByVisibleText("Child page 0");
     }
 }

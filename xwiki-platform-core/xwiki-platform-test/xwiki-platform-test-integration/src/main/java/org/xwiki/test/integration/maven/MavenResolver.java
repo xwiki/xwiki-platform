1fb6c1be0d3788295ea5ed824f6ccd60e262054c
Thomas Mortagne <thomas.mortagne@gmail.com>
2024-03-18 15:15:09 +0100

    [misc] Fix checkstyle

--- xwiki-platform-core/xwiki-platform-test/xwiki-platform-test-integration/src/main/java/org/xwiki/test/integration/maven/MavenResolver.java
+++ xwiki-platform-core/xwiki-platform-test/xwiki-platform-test-integration/src/main/java/org/xwiki/test/integration/maven/MavenResolver.java
@@ -91,7 +91,7 @@ public class MavenResolver
     /**
      * @return the version of the XWiki platform artifacts to download/resolve
      * @throws Exception if an error occurred during reading and parsing of the POM
-     * @since 16.2RC1
+     * @since 16.2.0RC1
      */
     public String getCommonsVersion() throws Exception
     {

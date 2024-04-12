ac1d273e5aef944da867daa1f1335c3533899b97
Marius Dumitru Florea <marius@xwiki.com>
2024-04-12 13:43:53 +0300

    XWIKI-22034: Realtime WYSIWYG editor is very slow even on medium size content * Synchronize using the editor output HTML (i.e. what is sent to the server to be converted to wiki syntax) in order to avoid dealing with local changes and also avoid applying complex (HyperJSON) filters to obtain the normalized content (the editor output HTML is the normalized content). * The editor output HTML can contain XML comments (annotations like macro markers)  but HyperJSON doesn't support comments, so we need to protect/unprotect them when converting to/from HyperJSON. * Restoring the diff-based text selection is costly and is not needed most of the time (when users edit in different sections of the content) so we also save a "relative" selection that can be invalidated when applying the DOM patch
    (cherry picked from commit edbe5408b040f6e73a584d412aa7083d2eedffd6)

--- xwiki-platform-core/xwiki-platform-realtime/xwiki-platform-realtime-wysiwyg/xwiki-platform-realtime-wysiwyg-webjar/src/main/webjar/editor.js
+++ xwiki-platform-core/xwiki-platform-realtime/xwiki-platform-realtime-wysiwyg/xwiki-platform-realtime-wysiwyg-webjar/src/main/webjar/editor.js
@@ -59,17 +59,16 @@ define('xwiki-realtime-wysiwyg-editor', [], function () {
     }
 
     /**
-     * Notify the editor that its content has been updated as a result of a remote change.
+     * Update the edited content as a result of a remote change.
      *
-     * @param {Node[]} updatedNodes the DOM nodes that have been updated (added, modified directly or with removed
-     *   descendants)
-     *
+     * @param {Function} updater a function that takes the current content of the editor, modifies it and returns the
+     *   updated nodes
      * @param {boolean} propagate true when the new content should be propagated to coeditors
      * @returns {Promise} a promise that resolves when the editor has finished handling the content update (some
      *   changes, like for instance if you modify some macro parameters, might require a full refresh of the edited
      *   content).
      */
-    async contentUpdated(updatedNodes, propagate) {
+    async updateContent(updater, propagate) {
       throw new Error('Not implemented!');
     }
 
@@ -83,8 +82,12 @@ define('xwiki-realtime-wysiwyg-editor', [], function () {
     }
 
     /**
-     * @returns {Selection} the current DOM selection in the editor
-     * @see https://developer.mozilla.org/en-US/docs/Web/API/Selection
+     * Note that the editor might not be focused so the returned selection ranges don't always match the current window
+     * selection. The returned ranges correspond to the selection (e.g. the caret position) that the editor remembers
+     * even when it loses focus (e.g. when a dialog is opened).
+     *
+     * @returns {Array[Range]} the currently selected DOM ranges in the editor, {@code []} if there is no selection
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/Range
      */
     getSelection() {
       throw new Error('Not implemented!');
@@ -98,9 +101,12 @@ define('xwiki-realtime-wysiwyg-editor', [], function () {
     }
 
     /**
-     * Restore the selection saved previously.
+     * Restore the given DOM selection ranges, or the last saved text selection if no ranges are specified.
+     *
+     * @param {Range[]} ranges the DOM selection ranges to restore; if not specified, the last saved text selection is
+     *   restored
      */
-    restoreSelection() {
+    restoreSelection(ranges) {
       throw new Error('Not implemented!');
     }
 
@@ -116,6 +122,18 @@ define('xwiki-realtime-wysiwyg-editor', [], function () {
      *   if you were to load the given HTML directly in the editor; see also {@link #getContentWrapper()}
      */
     parseInputHTML(html) {
+      throw new Error('Not implemented!');
+    }
+
+    /**
+     * Allows the editor to ignore some of the changes found when comparing the remote content with the local content.
+     * Each filter function taks a DOM change as input and returns a boolean indicating whether that change should be
+     * ignored or not.
+     *
+     * @returns {Array[Function]} an array of functions that must be used to filter the DOM changes before applying a
+     *   patch (due to a remote change)
+     */
+    getFilters() {
       throw new Error('Not implemented!');
     }
 

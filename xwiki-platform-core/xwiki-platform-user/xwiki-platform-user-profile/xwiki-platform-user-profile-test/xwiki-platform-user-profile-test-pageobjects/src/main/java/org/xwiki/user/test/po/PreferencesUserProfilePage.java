918bd99c2b6486a21d64e7fe594fda07fd1f3ccb
LucasC <lucas.charpentier@imt-atlantique.net>
2024-04-18 16:19:31 +0200

    XWIKI-21492: Underline inline links * Fixed the xpaths to account for the new preference category

--- xwiki-platform-core/xwiki-platform-user/xwiki-platform-user-profile/xwiki-platform-user-profile-test/xwiki-platform-user-profile-test-pageobjects/src/main/java/org/xwiki/user/test/po/PreferencesUserProfilePage.java
+++ xwiki-platform-core/xwiki-platform-user/xwiki-platform-user-profile/xwiki-platform-user-profile-test/xwiki-platform-user-profile-test-pageobjects/src/main/java/org/xwiki/user/test/po/PreferencesUserProfilePage.java
@@ -33,13 +33,13 @@ public class PreferencesUserProfilePage extends AbstractUserProfilePage
     @FindBy(xpath = "//div[@id='preferencesPane']//div[@class='editProfileCategory']/a")
     private WebElement editPreferences;
 
-    @FindBy(xpath = "//div[@id='preferencesPane']/div[1]/div/dl[2]/dd[1]")
+    @FindBy(xpath = "//div[@id='preferencesPane']/div[1]/div/dl[3]/dd[1]")
     private WebElement timezone;
 
-    @FindBy(xpath = "//div[@id='preferencesPane']/div[1]/div/dl[3]/dd[2]")
+    @FindBy(xpath = "//div[@id='preferencesPane']/div[1]/div/dl[4]/dd[2]")
     private WebElement userType;
 
-    @FindBy(xpath = "//div[@id='preferencesPane']/div[1]/div/dl[3]/dd[1]")
+    @FindBy(xpath = "//div[@id='preferencesPane']/div[1]/div/dl[4]/dd[1]")
     private WebElement defaultEditorToUse;
 
     @FindBy(xpath = "//a[@id='changePassword']")

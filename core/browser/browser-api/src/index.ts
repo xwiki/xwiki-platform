b4b7bb6108f1f5551980f183b2900f91c0cff801
Manuel Leduc <manuel.leduc@xwiki.com>
2024-05-16 08:38:46 +0200

    CRISTAL-183: Changing config in Electron does not reload
    Add missing license headers.

--- core/browser/browser-api/src/index.ts
+++ core/browser/browser-api/src/index.ts
@@ -1,5 +1,35 @@
+/**
+ * See the LICENSE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ *
+ * This file is part of the Cristal Wiki software prototype
+ * @copyright  Copyright (c) 2023 XWiki SAS
+ * @license    http://opensource.org/licenses/AGPL-3.0 AGPL-3.0
+ *
+ **/
 import { WikiConfig } from "@cristal/api";
 
+/**
+ * Provide the operations to interact with the browser. We need this abstraction
+ * because interacting with the browser is not equivalent when Cristal is
+ * running directly on a browser, or in an Electron app managing a browser.
+ * @since 0.8
+ */
 export interface BrowserApi {
   switchLocation(wikiConfig: WikiConfig): void;
 }

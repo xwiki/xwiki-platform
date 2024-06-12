5f90563c041ef79f9c5b599f6cb6562e1da6ae31
Simon Urli <simon.urli@xwiki.com>
2024-06-12 09:10:23 +0200

    XWIKI-19751: Move the "watch" button for a page to the page content menu (#3059)
    Goal of this change is to simplify usage of the watch page feature.
    It comes with important changes:
       - the watch location switches in the bell menu area have been removed
       - a new button with a logo and a status has been introduced next to edit button
       - a new modal has been introduced when clicking on the button providing various choices
       - the watch page API have been modified to not just provide boolean value watch / not watched, but to provide a larger panel of possible statuses
       - a REST resource have been provided to allow watching / blocking / removing a filter on a location
      
    
    * Start providing REST API for watch pages to simplify operations
      * Distinguish more cases for watched / ignored pages to improve UI
      * Provide implementation for REST API for watching pages
      * Improve WatchEntitiesManager API to support more operations
      * Improve WatchedEntityReference API to use UserReference
      * Provide a dedicated page to handle watch settings
      * Start writing javascript code to handle choices
      * Fix issue in WatchedEntitiesManager
      * Start providing doc / fixing checkstyle
      * Handle a bit more actions in UI
      * Fix a bug in state computer related to the fact that the scoe filter
        hierarchy is not really a hierarchy
      * Provide a new API to allow computing the reference of the immediate
        ancestor of a page for which a filter exists
      * Improve UI to base possible actions on multiple criteria: current
        watch status, presence or not of a filter on an ancestor, and check
    if the page is terminal or not
      * Encode all options for the watch settings modal
      * Change the URL scheme for the REST API
      * Minor improvments
      * Provide test for notification rest module
      * Fix checkstyle
      * Put revapi ignores
      * Start fixing since
      * Provide translations and improve UIX code
      * Provide page object and fix integration tests
      * Ensure to not display the watch status for guest
      * Fix a few since and some problems in UIX
      * Fix API of WatchedUserReference
      * Fix bad escaping
      * Better exception handling for creating references
      * Various improvments in translations (active voice + use of ignore)
      * Improve a bit the UI
      * Improve UI when a page is ignored/followed by an ancestor and check
        proper rights
      * Review available options and improve javascript to allow chaining
        operations: following space when the page is followed implies to
    first unfollow the page to not keep unnecessary filters and mess up the
    UI
      * Improve translations
      * Clean up some unnecessary code
      * Make the UI a bit more clear
      * Fix some stuff after rebsae
      * Slightly improve UX
      * Fix missing unstable
      * Fix bad deprecated API call

--- xwiki-platform-core/xwiki-platform-notifications/xwiki-platform-notifications-test/xwiki-platform-notifications-test-docker/src/test/it/org/xwiki/platform/notifications/test/ui/NotificationsIT.java
+++ xwiki-platform-core/xwiki-platform-notifications/xwiki-platform-notifications-test/xwiki-platform-notifications-test-docker/src/test/it/org/xwiki/platform/notifications/test/ui/NotificationsIT.java
@@ -29,9 +29,11 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.xwiki.model.reference.DocumentReference;
 import org.xwiki.platform.notifications.test.po.GroupedNotificationElementPage;
+import org.xwiki.platform.notifications.test.po.NotificationWatchButtonElement;
 import org.xwiki.platform.notifications.test.po.NotificationsRSS;
 import org.xwiki.platform.notifications.test.po.NotificationsTrayPage;
 import org.xwiki.platform.notifications.test.po.NotificationsUserProfilePage;
+import org.xwiki.platform.notifications.test.po.NotificationsWatchModal;
 import org.xwiki.platform.notifications.test.po.preferences.filters.SystemNotificationFilterPreference;
 import org.xwiki.test.docker.junit5.TestConfiguration;
 import org.xwiki.test.docker.junit5.TestReference;
@@ -153,12 +155,15 @@ class NotificationsIT
         p.getApplication(SYSTEM).setCollapsed(false);
         p.setEventTypeState(SYSTEM, CREATE, ALERT_FORMAT, BootstrapSwitch.State.ON);
 
-        tray.showNotificationTray();
-        assertFalse(tray.isPageOnlyWatched());
-        assertFalse(tray.arePageAndChildrenWatched());
-        assertFalse(tray.isWikiWatched());
+        NotificationWatchButtonElement watchButtonElement = new NotificationWatchButtonElement();
+        assertTrue(watchButtonElement.isNotSet());
         // And he will watch the entire wiki.
-        tray.setWikiWatchedState(true);
+        NotificationsWatchModal notificationsWatchModal = watchButtonElement.openModal();
+        assertEquals(List.of(
+            NotificationsWatchModal.WatchOptions.WATCH_PAGE,
+            NotificationsWatchModal.WatchOptions.WATCH_WIKI
+        ), notificationsWatchModal.getAvailableOptions());
+        notificationsWatchModal.selectOptionAndSave(NotificationsWatchModal.WatchOptions.WATCH_WIKI);
 
         // We create a lot of pages in order to test the notification badge
         setup.login(FIRST_USER_NAME, FIRST_USER_PASSWORD);
@@ -248,11 +253,21 @@ class NotificationsIT
         assertEquals(1, minorEvent.size());
         minorEvent.get(0).setEnabled(false);
         setup.gotoPage("Main", "WebHome");
+        NotificationWatchButtonElement watchButtonElement = new NotificationWatchButtonElement();
+        assertTrue(watchButtonElement.isNotSet());
+        // And he will watch the entire wiki.
+        NotificationsWatchModal notificationsWatchModal = watchButtonElement.openModal();
+        assertEquals(List.of(
+            NotificationsWatchModal.WatchOptions.WATCH_PAGE,
+            NotificationsWatchModal.WatchOptions.WATCH_SPACE,
+            NotificationsWatchModal.WatchOptions.WATCH_WIKI
+        ), notificationsWatchModal.getAvailableOptions());
+        notificationsWatchModal.selectOptionAndSave(NotificationsWatchModal.WatchOptions.WATCH_WIKI);
+
         tray = new NotificationsTrayPage();
         tray.showNotificationTray();
         tray.clearAllNotifications();
-        // Watch the entire wiki so that we receive notifications
-        tray.setWikiWatchedState(true);
+
 
         // Create a page, edit it 20 times, and finally add a comment
         setup.login(FIRST_USER_NAME, FIRST_USER_PASSWORD);
@@ -353,9 +368,15 @@ class NotificationsIT
             p.setEventTypeState(SYSTEM, UPDATE, ALERT_FORMAT, BootstrapSwitch.State.ON);
 
             // Watch the entire wiki so that we receive notifications
-            NotificationsTrayPage tray = new NotificationsTrayPage();
-            tray.showNotificationTray();
-            tray.setWikiWatchedState(true);
+            NotificationWatchButtonElement watchButtonElement = new NotificationWatchButtonElement();
+            assertTrue(watchButtonElement.isNotSet());
+            // And he will watch the entire wiki.
+            NotificationsWatchModal notificationsWatchModal = watchButtonElement.openModal();
+            assertEquals(List.of(
+                NotificationsWatchModal.WatchOptions.WATCH_PAGE,
+                NotificationsWatchModal.WatchOptions.WATCH_WIKI
+            ), notificationsWatchModal.getAvailableOptions());
+            notificationsWatchModal.selectOptionAndSave(NotificationsWatchModal.WatchOptions.WATCH_WIKI);
 
             // Login as second user and modify ARandomPageThatShouldBeModified
             setup.login(SECOND_USER_NAME, SECOND_USER_PASSWORD);
@@ -373,7 +394,7 @@ class NotificationsIT
 
             // Ensure the notification has been received.
             NotificationsTrayPage.waitOnNotificationCount("xwiki:XWiki." + FIRST_USER_NAME, "xwiki", 1);
-            tray = new NotificationsTrayPage();
+            NotificationsTrayPage tray = new NotificationsTrayPage();
             assertEquals("This is a test template", tray.getNotificationRawContent(0));
         } finally {
             setup.loginAsSuperAdmin();
@@ -404,9 +425,15 @@ class NotificationsIT
             assertFalse(p.getSystemNotificationFilterPreferences().get(filterPreferenceNumber).isEnabled());
 
             // Watch the entire wiki so that we receive notifications
-            NotificationsTrayPage tray = new NotificationsTrayPage();
-            tray.showNotificationTray();
-            tray.setWikiWatchedState(true);
+            NotificationWatchButtonElement watchButtonElement = new NotificationWatchButtonElement();
+            assertTrue(watchButtonElement.isNotSet());
+            // And he will watch the entire wiki.
+            NotificationsWatchModal notificationsWatchModal = watchButtonElement.openModal();
+            assertEquals(List.of(
+                NotificationsWatchModal.WatchOptions.WATCH_PAGE,
+                NotificationsWatchModal.WatchOptions.WATCH_WIKI
+            ), notificationsWatchModal.getAvailableOptions());
+            notificationsWatchModal.selectOptionAndSave(NotificationsWatchModal.WatchOptions.WATCH_WIKI);
 
             setup.createPage(testReference, "", "");
             // Ensure the notification has been received.

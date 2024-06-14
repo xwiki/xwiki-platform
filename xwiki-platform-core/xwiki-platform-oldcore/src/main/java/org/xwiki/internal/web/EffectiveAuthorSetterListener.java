ba5339454bd2c2c12d26f0c48b412a013a7a4f7a
Marius Dumitru Florea <marius@xwiki.com>
2024-06-14 20:10:11 +0300

    XWIKI-22222: Add the concept of "request effective author" * Use proper package name * Improve code and comment.

--- xwiki-platform-core/xwiki-platform-oldcore/src/main/java/org/xwiki/internal/web/EffectiveAuthorSetterListener.java
+++ xwiki-platform-core/xwiki-platform-oldcore/src/main/java/org/xwiki/internal/web/EffectiveAuthorSetterListener.java
@@ -17,7 +17,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package com.xpn.xwiki.internal.web;
+package org.xwiki.internal.web;
 
 import javax.inject.Inject;
 import javax.inject.Named;

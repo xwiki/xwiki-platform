48d67850c0d86067c797118a2b7cd7c191f83fc8
Manuel Leduc <manuel.leduc@xwiki.com>
2024-06-19 16:45:26 +0200

    [Misc] Uniformize the licenses to LGPL 2.1

--- core/extra-tabs/extra-tabs-default/src/DefaultExtraTabsService.ts
+++ core/extra-tabs/extra-tabs-default/src/DefaultExtraTabsService.ts
@@ -1,9 +1,9 @@
-/**
+/*
  * See the LICENSE file distributed with this work for additional
  * information regarding copyright ownership.
  *
  * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Affero General Public License as
+ * under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation; either version 2.1 of
  * the License, or (at your option) any later version.
  *
@@ -12,16 +12,11 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Affero General Public
+ * You should have received a copy of the GNU Lesser General Public
  * License along with this software; if not, write to the Free
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
- *
- * This file is part of the Cristal Wiki software prototype
- * @copyright  Copyright (c) 2023 XWiki SAS
- * @license    http://opensource.org/licenses/AGPL-3.0 AGPL-3.0
- *
- **/
+ */
 
 import { ExtraTab, ExtraTabsService } from "@xwiki/cristal-extra-tabs-api";
 

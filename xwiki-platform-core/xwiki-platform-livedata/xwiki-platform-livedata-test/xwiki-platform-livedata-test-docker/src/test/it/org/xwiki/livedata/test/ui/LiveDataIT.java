3a81b6d6b560022511c19f901e3170bdfeff88f7
Michael Hamann <michael.hamann@xwiki.com>
2024-07-04 17:33:52 +0200

    [Misc] LiveDataIT: wait for row counts before asserting them
    This should avoid flickering due to Live Data not refreshing as
    expected.
    
    (cherry picked from commit b3b0715154551eae15ec3997a36f9373d019a5ff)

--- xwiki-platform-core/xwiki-platform-livedata/xwiki-platform-livedata-test/xwiki-platform-livedata-test-docker/src/test/it/org/xwiki/livedata/test/ui/LiveDataIT.java
+++ xwiki-platform-core/xwiki-platform-livedata/xwiki-platform-livedata-test/xwiki-platform-livedata-test-docker/src/test/it/org/xwiki/livedata/test/ui/LiveDataIT.java
@@ -246,6 +246,7 @@ class LiveDataIT
         // Test filter on boolean values
         suggestInputElement.clear().hideSuggestions();
         liveDataElement.waitUntilReady();
+        tableLayout.waitUntilRowCountEqualsTo(2);
         assertEquals(2, tableLayout.countRows());
 
         // Take the focus on the is active filter.
@@ -266,11 +267,13 @@ class LiveDataIT
         assertEquals(1, suggestionElements.size());
         suggestionElements.get(0).select();
         liveDataElement.waitUntilReady();
+        tableLayout.waitUntilRowCountEqualsTo(1);
         assertEquals(1, tableLayout.countRows());
         tableLayout.assertRow(NAME_COLUMN, NAME_NIKOLAY);
 
         suggestInputElement.clear().hideSuggestions();
         liveDataElement.waitUntilReady();
+        tableLayout.waitUntilRowCountEqualsTo(2);
         assertEquals(2, tableLayout.countRows());
     }
 
@@ -354,6 +357,7 @@ class LiveDataIT
         assertEquals(3, tableLayout.countRows());
         tableLayout.clickAction(1, "delete");
         viewPage.waitForNotificationSuccessMessage("Delete Success");
+        tableLayout.waitUntilRowCountEqualsTo(2);
         assertEquals(2, tableLayout.countRows());
     }
 

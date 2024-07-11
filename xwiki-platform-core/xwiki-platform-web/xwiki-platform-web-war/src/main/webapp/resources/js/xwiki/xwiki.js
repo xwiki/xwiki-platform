c38bc99326724a1c6ea954b06bdfccfb64701b40
Simon Urli <simon.urli@xwiki.com>
2024-07-11 09:46:16 +0200

    [Misc] Deprecate extractFilename in xwiki.js

--- xwiki-platform-core/xwiki-platform-web/xwiki-platform-web-war/src/main/webapp/resources/js/xwiki/xwiki.js
+++ xwiki-platform-core/xwiki-platform-web/xwiki-platform-web-war/src/main/webapp/resources/js/xwiki/xwiki.js
@@ -596,33 +596,6 @@ Object.extend(XWiki, {
   },
 
   /**
-   * Extracts the file name from the value of the specified file input.
-   */
-  extractFileName: function(fileInput) {
-    fileInput = $(fileInput);
-    if (fileInput.files && fileInput.files.length > 0) {
-      // Modern browsers provide additional information about the selected file(s).
-      return fileInput.files[0].name;
-    } else if (fileInput.value.substr(0, 12) == 'C:\\fakepath\\') {
-      // Most browsers hide the real path for security reasons.
-      return fileInput.value.substr(12);
-    } else {
-      var lastPathSeparatorIndex = fileInput.value.lastIndexOf('/');
-      if (lastPathSeparatorIndex >= 0) {
-        // Unix-based path.
-        return fileInput.value.substr(lastPathSeparatorIndex + 1);
-      }
-      lastPathSeparatorIndex = fileInput.value.lastIndexOf('\\');
-      if (lastPathSeparatorIndex >= 0) {
-        // Windows-based path.
-        return fileInput.value.substr(lastPathSeparatorIndex + 1);
-      }
-      // The file input value is just the file name.
-      return fileInput.value;
-    }
-  },
-
-  /**
    * Initialize method for the XWiki object. This is to be called only once upon dom loading.
    * It makes rendering errors expandable and fixes external links on the body content.
    * Then it fires an custom event to signify the (modified) DOM is now loaded.

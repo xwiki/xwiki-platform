45a331914482d81962da16a2264acacb0d0d057a
Thomas Mortagne <thomas.mortagne@gmail.com>
2024-08-06 13:40:27 +0200

    XWIKI-22395: Filter locales listed in Administration in order to ensure that the list contains only locales with the correct format

--- xwiki-platform-core/xwiki-platform-localization/xwiki-platform-localization-script/src/main/java/org/xwiki/localization/script/LocalizationScriptService.java
+++ xwiki-platform-core/xwiki-platform-localization/xwiki-platform-localization-script/src/main/java/org/xwiki/localization/script/LocalizationScriptService.java
@@ -21,7 +21,6 @@ package org.xwiki.localization.script;
 
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
@@ -389,8 +388,7 @@ public class LocalizationScriptService implements ScriptService
      */
     public Set<Locale> getAvailableLocales()
     {
-        Set<Locale> locales = new HashSet<>();
-        locales.addAll(Arrays.asList(Locale.getAvailableLocales()));
+        Set<Locale> locales = new HashSet<>(LocaleUtils.availableLocaleSet());
 
         try (InputStream resource = this.environment.getResourceAsStream("/WEB-INF/xwiki-locales.txt")) {
             LineIterator iterator = IOUtils.lineIterator(resource, StandardCharsets.US_ASCII);

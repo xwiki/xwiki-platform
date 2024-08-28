c17fe57f209097766d3ff5062af04966fceba6aa
Simon Urli <simon.urli@xwiki.com>
2024-08-28 14:01:12 +0200

    XWIKI-22459: Firefox doesn't support anymore confirmation dialog in tests
      * Remove ignores on tests that relies on confirmation dialog
      * Provide a new solution for handling the confirmation dialog on
        ObjectEditorIT

--- xwiki-platform-core/xwiki-platform-flamingo/xwiki-platform-flamingo-skin/xwiki-platform-flamingo-skin-test/xwiki-platform-flamingo-skin-test-docker/src/test/it/org/xwiki/flamingo/test/docker/ObjectEditorIT.java
+++ xwiki-platform-core/xwiki-platform-flamingo/xwiki-platform-flamingo-skin/xwiki-platform-flamingo-skin-test/xwiki-platform-flamingo-skin-test-docker/src/test/it/org/xwiki/flamingo/test/docker/ObjectEditorIT.java
@@ -25,14 +25,15 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.UnhandledAlertException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.bidi.browsingcontext.BrowsingContext;
+import org.openqa.selenium.bidi.module.BrowsingContextInspector;
+import org.openqa.selenium.remote.Augmenter;
 import org.xwiki.model.reference.DocumentReference;
 import org.xwiki.test.docker.junit5.TestReference;
 import org.xwiki.test.docker.junit5.UITest;
 import org.xwiki.test.ui.TestUtils;
-import org.xwiki.test.ui.browser.IgnoreBrowser;
 import org.xwiki.test.ui.po.FormContainerElement;
 import org.xwiki.test.ui.po.HistoryPane;
 import org.xwiki.test.ui.po.SuggestInputElement;
@@ -46,7 +47,6 @@ import org.xwiki.test.ui.po.editor.StaticListClassEditElement;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 
 /**
  * Tests for the object editor.
@@ -79,8 +79,6 @@ class ObjectEditorIT
     }
     
     @Test
-    @IgnoreBrowser(value = "firefox", reason = "Alert handling in Firefox currently isn't working, see also "
-        + "https://jira.xwiki.org/browse/XWIKI-22282")
     @Order(1)
     void preventUsersToLeaveTheEditorWithoutSaving(TestUtils testUtils, TestReference testReference)
     {
@@ -110,15 +108,15 @@ class ObjectEditorIT
         assertTrue(objectEditPane.isDeleteLinkDisplayed());
         assertFalse(objectEditPane.isEditLinkDisplayed());
 
-        try {
-            // should open a confirmation modal for leaving since we didn't save
+        WebDriver driver = new Augmenter().augment(testUtils.getDriver().getWrappedDriver());
+        try (BrowsingContextInspector inspector = new BrowsingContextInspector(driver)) {
+            BrowsingContext context = new BrowsingContext(driver, driver.getWindowHandle());
+            inspector.onUserPromptOpened(userPromptOpened -> {
+                assertEquals(context.getId(), userPromptOpened.getBrowsingContextId());
+                context.handleUserPrompt(false);
+            });
             testUtils.gotoPage(testReference);
-            fail("A confirm alert should be triggered");
-        } catch (UnhandledAlertException e) {
-            Alert alert = testUtils.getDriver().switchTo().alert();
-            alert.dismiss(); // remain on the page
         }
-
         objectEditPage.deleteObject(NUMBER_CLASS, 1);
 
         // State should be same as before adding
@@ -139,14 +137,13 @@ class ObjectEditorIT
 
         // Delete the saved object
         objectEditPage.deleteObject(NUMBER_CLASS, 0);
-
-        try {
-            // should open a confirmation modal for leaving
+        try (BrowsingContextInspector inspector = new BrowsingContextInspector(driver)) {
+            BrowsingContext context = new BrowsingContext(driver, driver.getWindowHandle());
+            inspector.onUserPromptOpened(userPromptOpened -> {
+                assertEquals(context.getId(), userPromptOpened.getBrowsingContextId());
+                context.handleUserPrompt(false);
+            });
             testUtils.gotoPage(testReference);
-            fail("A confirm alert should be triggered");
-        } catch (UnhandledAlertException e) {
-            Alert alert = testUtils.getDriver().switchTo().alert();
-            alert.dismiss();
         }
 
         objectEditPage.clickSaveAndContinue();

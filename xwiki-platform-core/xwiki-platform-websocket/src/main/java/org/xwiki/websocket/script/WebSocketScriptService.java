fee69113b960f86162605da30062b2357cb589d3
Adrian Nöthlich <git@promasu.tech>
2024-10-05 20:28:42 +0200

    XWIKI-22547: Make websocket work with Root servlet context

--- xwiki-platform-core/xwiki-platform-websocket/src/main/java/org/xwiki/websocket/script/WebSocketScriptService.java
+++ xwiki-platform-core/xwiki-platform-websocket/src/main/java/org/xwiki/websocket/script/WebSocketScriptService.java
@@ -84,9 +84,13 @@ public class WebSocketScriptService implements ScriptService
             XWikiContext xcontext = this.xcontextProvider.get();
 
             StringBuilder path = new StringBuilder("/");
-            path.append(xcontext.getWiki().getWebAppPath(xcontext));
-            path.append("websocket").append(path(pathOrRoleHint));
 
+            // Test if installed with ROOT servlet context and avoid double slash
+            if (!"/".equals(xcontext.getWiki().getWebAppPath(xcontext))) {
+                path.append(xcontext.getWiki().getWebAppPath(xcontext));
+            }
+
+            path.append("websocket").append(path(pathOrRoleHint));
             URL serverURL = xcontext.getURLFactory().getServerURL(xcontext);
             String scheme = "https".equals(serverURL.getProtocol()) ? "wss" : "ws";
             // We have to add the path afterwards because the URI constructor double encodes it.

bbb5452a94b8d392fcd2e22d7e4e1f44c2772e35
Manuel Leduc <manuel.leduc@xwiki.com>
2024-10-17 16:41:14 +0200

    [Misc] Fix integration tests

--- web/e2e/pageObjects/NavigationTree.ts
+++ web/e2e/pageObjects/NavigationTree.ts
@@ -48,8 +48,9 @@ export class NavigationTreePageObject {
 
   private async findItemsShoelace(): Promise<Array<NavigationTreeNode>> {
     return await NavigationTreePageObject.findItemsInternal(
-      this.page.locator("#sidebar sl-tree").nth(1).locator("sl-tree-item"),
-      NavigationTreePageObject.wrapShoelace);
+      this.page.locator("#sidebar sl-tree:visible sl-tree-item"),
+      NavigationTreePageObject.wrapShoelace,
+    );
   }
 
   private static wrapShoelace(element: Locator): NavigationTreeNode {

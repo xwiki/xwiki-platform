d49ceb2f0c433fddb5d02e998c6d02b9c2219266
Sereza7 <lucas.charpentier@xwiki.com>
2024-10-30 16:45:31 +0100

    XWIKI-22495: Missing text content in the resource picker dropdown * Added a translated title/content to the dropdown toggle * Updated the style to allow for the use of .sr-only in this specific context (overriding CKEditor CSS reset)

--- xwiki-platform-core/xwiki-platform-ckeditor/xwiki-platform-ckeditor-plugins/src/main/webjar/xwiki-resource/resourcePicker.js
+++ xwiki-platform-core/xwiki-platform-ckeditor/xwiki-platform-ckeditor-plugins/src/main/webjar/xwiki-resource/resourcePicker.js
@@ -19,7 +19,8 @@
  */
 define('resourcePickerTranslationKeys', [], [
   'attach.hint',
-  'doc.hint'
+  'doc.hint',
+  'dropdown.toggle.title'
 ]);
 
 define('resourcePicker', [
@@ -35,7 +36,9 @@ define('resourcePicker', [
           '<button type="button" class="resourceType btn btn-default">' +
             '<span class="icon">' +
           '</button>' +
-          '<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">' +
+          '<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" ' +
+            'title="' + translations.get('dropdown.toggle.title') + '">' +
+            '<span class="sr-only">' + translations.get('dropdown.toggle.title') + '</span>' +
             '<span class="caret"></span>' +
           '</button>' +
           '<ul class="resourceTypes dropdown-menu dropdown-menu-right"></ul>' +

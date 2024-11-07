22f87811ee19ff9f4472aa009d872029689883bc
Michael Hamann <michael.hamann@xwiki.com>
2024-11-07 09:39:37 +0100

    XWIKI-22635: On restart, documents to re-index aren't correctly computed (#3622)
    * Use an internal comparator that reproduces the order of Solr/the
      database.
    * Add an integration test to verify that the sort order corresponds to
      the used comparator.
    
    (cherry picked from commit 4a5e7b0480628fc3dc2213a858cc1ea5c681e1cf)

--- xwiki-platform-core/xwiki-platform-search/xwiki-platform-search-solr/xwiki-platform-search-solr-test/xwiki-platform-search-solr-test-docker/src/test/it/org/xwiki/search/solr/test/ui/SolrIndexerIT.java
+++ xwiki-platform-core/xwiki-platform-search/xwiki-platform-search-solr/xwiki-platform-search-solr-test/xwiki-platform-search-solr-test-docker/src/test/it/org/xwiki/search/solr/test/ui/SolrIndexerIT.java
@@ -21,12 +21,14 @@ package org.xwiki.search.solr.test.ui;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 import org.xwiki.model.reference.DocumentReference;
 import org.xwiki.model.reference.SpaceReference;
 import org.xwiki.rendering.syntax.Syntax;
 import org.xwiki.repository.test.SolrTestUtils;
+import org.xwiki.search.solr.internal.job.DiffDocumentIterator;
 import org.xwiki.test.docker.junit5.TestConfiguration;
 import org.xwiki.test.docker.junit5.TestReference;
 import org.xwiki.test.docker.junit5.UITest;
@@ -40,6 +42,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.startsWith;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Integration tests for the Solr indexer, verifying re-indexing works as expected.
@@ -55,13 +58,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 })
 class SolrIndexerIT
 {
-    private static final String TEST_SCRIPT = "{{groovy}}\n"
+    private static final String TEST_SCRIPT =
+        "{{groovy}}\n"
         + "import java.lang.reflect.ParameterizedType\n"
         + "\n"
         + "import org.apache.commons.lang3.tuple.Pair\n"
         + "import org.xwiki.component.util.DefaultParameterizedType\n"
         + "import org.xwiki.model.reference.DocumentReference\n"
         + "import org.xwiki.search.solr.internal.job.DatabaseDocumentIterator\n"
+        + "import org.xwiki.search.solr.internal.job.DiffDocumentIterator\n"
         + "import org.xwiki.search.solr.internal.job.DocumentIterator\n"
         + "import org.xwiki.velocity.tools.JSONTool\n"
         + "\n"
@@ -69,34 +74,53 @@ class SolrIndexerIT
         + "    ParameterizedType documentIterator =\n"
         + "        new DefaultParameterizedType(null, DocumentIterator.class, String.class)\n"
         + "    DocumentIterator<String> databaseIterator = services.component.getInstance(documentIterator, "
-                                              + "\"database\")\n"
+            + "\"database\")\n"
         + "    DocumentIterator<String> solrIterator = services.component.getInstance(documentIterator, \"solr\")\n"
         + "\n"
-        + "     // Convert both iterators to lists and return them as JSON.\n"
-        + "     // Set the content type to text/plain to not trigger the JSON UI of the browser.\n"
-        + "     response.setContentType(\"text/plain\")\n"
-        + "     response.setCharacterEncoding(\"UTF-8\")\n"
-        + "     def output = (new JSONTool()).serialize([\n"
+        + "    // Store both iterators converted to list for the output.\n"
+        + "    def outputData = [\n"
         + "         \"database\": toList(databaseIterator),\n"
         + "         \"solr\": toList(solrIterator)\n"
-        + "     ])\n"
+        + "     ]\n"
         + "\n"
-        + "     response.writer.print(output)\n"
-        + "     response.setContentLength(output.getBytes(\"UTF-8\").size())\n"
-        + "     response.flushBuffer()\n"
-        + "     xcontext.setFinished(true)\n"
-        + " }\n"
+        + "    // Re-create both iterators and use a diff iterator to verify that the diff computation works.\n"
+        + "    databaseIterator = services.component.getInstance(documentIterator, \"database\")\n"
+        + "    solrIterator = services.component.getInstance(documentIterator, \"solr\")\n"
+        + "\n"
+        + "    DocumentIterator<DiffDocumentIterator.Action> diffIterator =\n"
+        + "        new DiffDocumentIterator(solrIterator, databaseIterator)\n"
+        + "    outputData.put(\"diff\", toList(diffIterator))\n"
+        + "\n"
+        + "    // Set the content type to text/plain to not trigger the JSON UI of the browser.\n"
+        + "    response.setContentType(\"text/plain\")\n"
+        + "    response.setCharacterEncoding(\"UTF-8\")\n"
+        + "    def output = (new JSONTool()).serialize(outputData)\n"
+        + "\n"
+        + "    response.writer.print(output)\n"
+        + "    response.setContentLength(output.getBytes(\"UTF-8\").size())\n"
+        + "    response.flushBuffer()\n"
+        + "    xcontext.setFinished(true)\n"
+        + "}\n"
         + "\n"
         + "// Method to convert an iterator of pairs to a list of two-element lists.\n"
-        + "static def toList(Iterator<Pair<DocumentReference, String>> iterator) {\n"
+        + "static def toList(Iterator<Pair<DocumentReference, ?>> iterator) {\n"
         + "    def list = []\n"
+        + "    Pair<DocumentReference, ?> previous = null\n"
+        + "    def comparator = DiffDocumentIterator.getComparator()\n"
         + "    while (iterator.hasNext()) {\n"
         + "        def pair = iterator.next()\n"
-        + "        list.add([pair.getLeft().toString(), pair.getRight()])\n"
+        + "        int comparison = -1\n"
+        + "        if (previous != null) {\n"
+        + "            comparison = comparator.compare(previous.getLeft(), pair.getLeft())\n"
+        + "        }\n"
+        + "        previous = pair\n"
+        + "        list.add([pair.getLeft().toString(), pair.getRight().toString(), String.valueOf(comparison)])\n"
         + "    }\n"
         + "    return list\n"
         + "}\n"
         + "// {{/groovy}}\n";
+
+    private static final String WEB_HOME = "WebHome";
 
     @Test
     void sortOrder(TestReference testReference, TestUtils testUtils, TestConfiguration testConfiguration)
@@ -114,9 +138,16 @@ class SolrIndexerIT
             DocumentReference pageReference = new DocumentReference(name, testSpace);
             testUtils.rest().savePage(pageReference, "Terminal page content", name);
             SpaceReference spaceReference = new SpaceReference(name, testSpace);
-            DocumentReference nonTerminalPageReference = new DocumentReference("WebHome", spaceReference);
+            DocumentReference nonTerminalPageReference = new DocumentReference(WEB_HOME, spaceReference);
             testUtils.rest().savePage(nonTerminalPageReference, "Non-terminal page content", name);
         }
+
+        // Test confusion between space separator and similar strings inside the space.
+        testUtils.rest().savePage(new DocumentReference(WEB_HOME, new SpaceReference("A.b", testSpace)));
+        testUtils.rest().savePage(new DocumentReference(WEB_HOME,
+            new SpaceReference("b", new SpaceReference("A", testSpace))));
+        testUtils.rest().savePage(new DocumentReference(WEB_HOME, new SpaceReference("A-b", testSpace)));
+        testUtils.rest().savePage(new DocumentReference(WEB_HOME, new SpaceReference("AAb", testSpace)));
 
         new SolrTestUtils(testUtils, computedHostURL(testConfiguration)).waitEmptyQueue();
 
@@ -130,7 +161,30 @@ class SolrIndexerIT
             {
             });
 
-        assertEquals(iteratorOutput.get("solr"), iteratorOutput.get("database"));
+        String skipAction = DiffDocumentIterator.Action.SKIP.toString();
+        List<List<String>> nonSkipOperations = iteratorOutput.get("diff").stream()
+            .filter(item -> !skipAction.equals(item.get(1)))
+            .collect(Collectors.toList());
+        List<List<String>> databaseDocuments = iteratorOutput.get("database");
+        List<List<String>> solrDocuments = iteratorOutput.get("solr");
+
+        assertTrue(nonSkipOperations.isEmpty(),
+            String.format("The list of operations contains non-skip operations: %s.\n"
+            + "Documents in Solr: %s\n"
+            + "Documents in the database: %s\n",
+            nonSkipOperations, solrDocuments, databaseDocuments)
+        );
+
+        // Just to be sure, also explicitly compare the two lists of documents even though the diff iterator should
+        // already have done that job.
+        assertEquals(solrDocuments, databaseDocuments);
+
+        List<List<String>> wronglyOrderedItems =
+            solrDocuments.stream().filter(item -> Integer.parseInt(item.get(2)) >= 0).collect(Collectors.toList());
+        assertTrue(wronglyOrderedItems.isEmpty(), String.format(
+            "Some documents aren't \"larger\" than the documents before them: %s%nAll documents: %s",
+            wronglyOrderedItems, solrDocuments)
+        );
     }
 
     private String computedHostURL(TestConfiguration testConfiguration)

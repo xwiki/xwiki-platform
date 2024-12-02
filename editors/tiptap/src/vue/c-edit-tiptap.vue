1332a85fa2da882d5d01c84ac3cdbcc8ad1f5a68
Manuel Leduc <manuel.leduc@xwiki.com>
2024-12-02 13:57:33 +0100

    CRISTAL-193: Add support for drag handles in the editor

--- editors/tiptap/src/vue/c-edit-tiptap.vue
+++ editors/tiptap/src/vue/c-edit-tiptap.vue
@@ -36,6 +36,7 @@ import StarterKit from "@tiptap/starter-kit";
 import { Editor, EditorContent } from "@tiptap/vue-3";
 import { CristalApp, PageData } from "@xwiki/cristal-api";
 import { name as documentServiceName } from "@xwiki/cristal-document-api";
+import GlobalDragHandle from "tiptap-extension-global-drag-handle";
 import { computed, inject, ref, watch } from "vue";
 import { useRoute } from "vue-router";
 import type { DocumentService } from "@xwiki/cristal-document-api";
@@ -160,6 +161,7 @@ async function loadEditor(page: PageData | undefined) {
     editor.value = new Editor({
       content: content.value || "",
       extensions: [
+        GlobalDragHandle,
         StarterKit.configure({
           // Disable the default history in order to use Collaboration's history management so that users undo / redo
           // only their own changes.
@@ -386,5 +388,31 @@ TODO: should be moved to a css specific to the empty line placeholder plugin.
   top: -20px;
   user-select: none;
   white-space: nowrap;
+}
+
+/* Drag handle specific CSS */
+.editor :deep(.drag-handle) {
+  position: fixed;
+  opacity: 1;
+  transition: opacity ease-in 0.2s;
+  border-radius: 0.25rem;
+
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 10 10' style='fill: rgba(0, 0, 0, 0.5)'%3E%3Cpath d='M3,2 C2.44771525,2 2,1.55228475 2,1 C2,0.44771525 2.44771525,0 3,0 C3.55228475,0 4,0.44771525 4,1 C4,1.55228475 3.55228475,2 3,2 Z M3,6 C2.44771525,6 2,5.55228475 2,5 C2,4.44771525 2.44771525,4 3,4 C3.55228475,4 4,4.44771525 4,5 C4,5.55228475 3.55228475,6 3,6 Z M3,10 C2.44771525,10 2,9.55228475 2,9 C2,8.44771525 2.44771525,8 3,8 C3.55228475,8 4,8.44771525 4,9 C4,9.55228475 3.55228475,10 3,10 Z M7,2 C6.44771525,2 6,1.55228475 6,1 C6,0.44771525 6.44771525,0 7,0 C7.55228475,0 8,0.44771525 8,1 C8,1.55228475 7.55228475,2 7,2 Z M7,6 C6.44771525,6 6,5.55228475 6,5 C6,4.44771525 6.44771525,4 7,4 C7.55228475,4 8,4.44771525 8,5 C8,5.55228475 7.55228475,6 7,6 Z M7,10 C6.44771525,10 6,9.55228475 6,9 C6,8.44771525 6.44771525,8 7,8 C7.55228475,8 8,8.44771525 8,9 C8,9.55228475 7.55228475,10 7,10 Z'%3E%3C/path%3E%3C/svg%3E");
+  background-size: calc(0.5em + 0.375rem) calc(0.5em + 0.375rem);
+  background-repeat: no-repeat;
+  background-position: center;
+  width: 1.2rem;
+  height: 1.5rem;
+  z-index: 50;
+  cursor: grab;
+}
+
+.editor :deep(.drag-handle:active) {
+  cursor: grabbing;
+}
+
+.editor :deep(.drag-handle.hide) {
+  opacity: 0;
+  pointer-events: none;
 }
 </style>

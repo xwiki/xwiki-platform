00538418f57d841d5f6bb06fd9cc672ec0f57959
LucasC <lucas.charpentier@xwiki.com>
2024-12-09 18:02:43 +0100

    XWIKI-22540: Slow loading time when changing the icons theme to Silk in the Icon picker
    * Fixed codestyle
    
    I didn't rerun all the tests.
    
    Co-authored-by: Marius Dumitru Florea <marius@xwiki.com>

--- xwiki-platform-core/xwiki-platform-ckeditor/xwiki-platform-ckeditor-plugins/src/main/webjar/xwiki-icon/iconService.js
+++ xwiki-platform-core/xwiki-platform-ckeditor/xwiki-platform-ckeditor-plugins/src/main/webjar/xwiki-icon/iconService.js
@@ -52,7 +52,7 @@ define('xwiki-iconService', [
       iconsPromise = iconsPromisesPerTheme[query] = $.getJSON(getResourceURL('data_icons', {
         iconTheme,
         query,
-        'metadata': 'true'
+        metadata: true
       }));
       iconsPromise.catch(() => {
         // Reset the promise so that we can try again later.

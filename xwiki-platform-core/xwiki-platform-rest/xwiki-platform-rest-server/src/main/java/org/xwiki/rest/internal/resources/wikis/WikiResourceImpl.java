83cfb2c6f588dd9fb05519e256ce20164d925653
Thomas Mortagne <thomas.mortagne@gmail.com>
2024-12-12 17:35:18 +0100

    XWIKI-21071: 'Import a XAR in a wiki' REST API is not working as expected when "backup=true" and "history=REPLACE"
    (cherry picked from commit 0a2d4f1c4c6c1a39733948e631b50d7b1df6c7de)

--- xwiki-platform-core/xwiki-platform-rest/xwiki-platform-rest-server/src/main/java/org/xwiki/rest/internal/resources/wikis/WikiResourceImpl.java
+++ xwiki-platform-core/xwiki-platform-rest/xwiki-platform-rest-server/src/main/java/org/xwiki/rest/internal/resources/wikis/WikiResourceImpl.java
@@ -19,25 +19,27 @@
  */
 package org.xwiki.rest.internal.resources.wikis;
 
-import java.io.IOException;
 import java.io.InputStream;
-import java.util.List;
 
+import javax.inject.Inject;
 import javax.inject.Named;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 
 import org.xwiki.component.annotation.Component;
+import org.xwiki.filter.input.DefaultInputStreamInputSource;
+import org.xwiki.filter.input.InputSource;
+import org.xwiki.internal.filter.Importer;
+import org.xwiki.model.reference.WikiReference;
 import org.xwiki.rest.XWikiResource;
 import org.xwiki.rest.XWikiRestException;
 import org.xwiki.rest.internal.DomainObjectFactory;
-import org.xwiki.rest.internal.Utils;
 import org.xwiki.rest.model.jaxb.Wiki;
 import org.xwiki.rest.resources.wikis.WikiResource;
+import org.xwiki.wiki.descriptor.WikiDescriptorManager;
+import org.xwiki.wiki.manager.WikiManagerException;
 
 import com.xpn.xwiki.XWikiContext;
-import com.xpn.xwiki.XWikiException;
-import com.xpn.xwiki.plugin.packaging.PackageAPI;
 
 /**
  * Resource for interacting with a specific wiki.
@@ -48,141 +50,57 @@ import com.xpn.xwiki.plugin.packaging.PackageAPI;
 @Named("org.xwiki.rest.internal.resources.wikis.WikiResourceImpl")
 public class WikiResourceImpl extends XWikiResource implements WikiResource
 {
-    /**
-     * The possible option for managing history when importing wiki documents.
-     */
-    private enum HistoryOptions
-    {
-        /**
-         * Add a new version.
-         */
-        ADD,
-        /**
-         * Reset the version to 1.1.
-         */
-        RESET,
-        /**
-         * Replace the current version.
-         */
-        REPLACE
-    }
+    @Inject
+    private Importer importer;
+
+    @Inject
+    private WikiDescriptorManager wikis;
 
     @Override
     public Wiki get(String wikiName) throws XWikiRestException
     {
         try {
-            if (wikiExists(wikiName)) {
-                return DomainObjectFactory.createWiki(objectFactory, uriInfo.getBaseUri(), wikiName);
+            if (this.wikis.exists(wikiName)) {
+                return DomainObjectFactory.createWiki(this.objectFactory, this.uriInfo.getBaseUri(), wikiName);
             }
 
             throw new WebApplicationException(Response.Status.NOT_FOUND);
-        } catch (XWikiException e) {
+        } catch (WikiManagerException e) {
             throw new XWikiRestException(e);
         }
     }
 
     @Override
-    public Wiki importXAR(String wikiName, Boolean backup, String history, InputStream is) throws XWikiRestException
+    public Wiki importXAR(String wikiName, Boolean backup, String historyStrategy, InputStream is)
+        throws XWikiRestException
     {
         try {
-            if (!wikiExists(wikiName)) {
+            if (!this.wikis.exists(wikiName)) {
                 throw new WebApplicationException(Response.Status.NOT_FOUND);
             }
 
             /* Use the package plugin for importing pages */
             XWikiContext xwikiContext = getXWikiContext();
-            PackageAPI importer = ((PackageAPI) xwikiContext.getWiki().getPluginApi("package", xwikiContext));
-            if (importer == null) {
-                throw new XWikiException(XWikiException.MODULE_XWIKI_ACCESS, XWikiException.ERROR_XWIKI_ACCESS_DENIED,
-                        "Can't access Package plugin API. Generally mean you don't have enough rights.");
-            }
 
-            String database = xwikiContext.getWikiId();
+            WikiReference currentWiki = xwikiContext.getWikiReference();
 
             try {
                 xwikiContext.setWikiId(wikiName);
-                importer.setBackupPack(backup);
 
-                importer.Import(is);
+                // Define the data source to import
+                InputSource source = new DefaultInputStreamInputSource(is, false);
 
-                HistoryOptions historyOption = parseHistoryOption(history, HistoryOptions.ADD);
-
-                switch (historyOption) {
-                    case RESET:
-                        importer.setPreserveVersion(false);
-                        importer.setWithVersions(false);
-                        break;
-                    case REPLACE:
-                        importer.setPreserveVersion(false);
-                        importer.setWithVersions(true);
-                        break;
-                    default:
-                    case ADD:
-                        importer.setPreserveVersion(true);
-                        importer.setWithVersions(false);
-                        break;
-                }
-
-                // Set the backup pack option
-                importer.setBackupPack(backup);
-
-                if (importer.install() == com.xpn.xwiki.plugin.packaging.DocumentInfo.INSTALL_IMPOSSIBLE) {
-                    throw new WebApplicationException(Response.Status.INTERNAL_SERVER_ERROR);
-                }
-            } catch (IOException e) {
-                throw new WebApplicationException(e);
+                // Execute the import
+                this.importer.importXAR(source, null, historyStrategy, backup == Boolean.TRUE, getXWikiContext());
+            } catch (Exception e) {
+                throw new WebApplicationException("Failed to import the XAR package", e);
             } finally {
-                xwikiContext.setWikiId(database);
+                xwikiContext.setWikiReference(currentWiki);
             }
 
-            return DomainObjectFactory.createWiki(objectFactory, uriInfo.getBaseUri(), wikiName);
-        } catch (XWikiException e) {
+            return DomainObjectFactory.createWiki(this.objectFactory, this.uriInfo.getBaseUri(), wikiName);
+        } catch (WikiManagerException e) {
             throw new XWikiRestException(e);
         }
-    }
-
-    /**
-     * Check if a wiki exists.
-     *
-     * @param wikiName the wiki name.
-     * @return true if the wiki exists.
-     * @throws XWikiException if something goes wrong.
-     */
-    protected boolean wikiExists(String wikiName) throws XWikiException
-    {
-        List<String> databaseNames =
-                Utils.getXWiki(componentManager).getVirtualWikisDatabaseNames(Utils.getXWikiContext(componentManager));
-
-        if (databaseNames.isEmpty()) {
-            databaseNames.add("xwiki");
-        }
-
-        for (String databaseName : databaseNames) {
-            if (databaseName.equals(wikiName)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    /**
-     * Return the HistoryOptions enum object corresponding to a string.
-     *
-     * @param value a string representing a history option.
-     * @param defaultValue the value to be returned in the case no corresponding history option is found.
-     * @return the history option enum object,
-     */
-    protected HistoryOptions parseHistoryOption(String value, HistoryOptions defaultValue)
-    {
-        try {
-            if (value != null) {
-                return HistoryOptions.valueOf(value.toUpperCase());
-            }
-        } catch (IllegalArgumentException e) {
-            // Invalid query type string.
-        }
-
-        return defaultValue;
     }
 }

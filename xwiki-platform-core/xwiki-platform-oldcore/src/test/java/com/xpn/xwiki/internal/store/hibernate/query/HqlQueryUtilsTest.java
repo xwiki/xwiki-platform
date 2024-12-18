ae57ed393eed86042634b0174adfa16ee06d5317
Thomas Mortagne <thomas.mortagne@gmail.com>
2024-12-18 13:34:53 +0100

    XWIKI-22734: Improve the standard query validator
    (cherry picked from commit 4d06d40ba809f0e8e2e8fc6c5f55ff2b5acd11db)

--- xwiki-platform-core/xwiki-platform-oldcore/src/test/java/com/xpn/xwiki/internal/store/hibernate/query/HqlQueryUtilsTest.java
+++ xwiki-platform-core/xwiki-platform-oldcore/src/test/java/com/xpn/xwiki/internal/store/hibernate/query/HqlQueryUtilsTest.java
@@ -22,8 +22,6 @@ package com.xpn.xwiki.internal.store.hibernate.query;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Validate {@link HqlQueryUtils}.
@@ -32,45 +30,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  */
 public class HqlQueryUtilsTest
 {
-    @Test
-    public void isSafe()
-    {
-        // allowed
-
-        assertTrue(HqlQueryUtils.isSafe("select name from XWikiDocument"));
-        assertTrue(HqlQueryUtils.isSafe("select doc.name, space.name from XWikiDocument doc, XWikiSpace space"));
-        assertTrue(HqlQueryUtils
-            .isSafe("select doc.name, space.name from XWikiDocument doc, XWikiSpace space, OtherTable as ot"));
-        assertTrue(HqlQueryUtils.isSafe("select count(name) from XWikiDocument"));
-        assertTrue(HqlQueryUtils.isSafe("select count(doc.name) from XWikiDocument doc"));
-        assertTrue(HqlQueryUtils
-            .isSafe("select doc.fullName from XWikiDocument as doc, com.xpn.xwiki.objects.StringProperty as str"));
-
-        assertTrue(HqlQueryUtils.isSafe("select count(*) from XWikiSpace"));
-        assertTrue(HqlQueryUtils.isSafe("select count(space.*) from XWikiSpace space"));
-
-        assertTrue(HqlQueryUtils.isSafe("select attachment.filename from XWikiAttachment attachment"));
-        assertTrue(HqlQueryUtils.isSafe("select count(*) from XWikiAttachment"));
-
-        // not allowed
-
-        assertFalse(HqlQueryUtils.isSafe("select name from OtherTable"));
-        assertFalse(HqlQueryUtils.isSafe("select doc.* from XWikiDocument doc, XWikiSpace space"));
-        assertFalse(HqlQueryUtils.isSafe("select * from XWikiDocument doc"));
-        assertFalse(HqlQueryUtils.isSafe("select * from XWikiAttachment"));
-        assertFalse(HqlQueryUtils.isSafe("select attachment.mimeType from XWikiAttachment attachment"));
-        assertFalse(HqlQueryUtils
-            .isSafe("select doc.name, ot.field from XWikiDocument doc, XWikiSpace space, OtherTable as ot"));
-        assertFalse(HqlQueryUtils.isSafe("select count(*) from OtherTable"));
-        assertFalse(HqlQueryUtils.isSafe("select count(other.*) from OtherTable other"));
-        assertFalse(
-            HqlQueryUtils.isSafe("select doc.fullName from XWikiDocument doc union all select name from OtherTable"));
-        assertFalse(HqlQueryUtils
-            .isSafe("select doc.fullName from XWikiDocument doc where 1<>'1\\'' union select name from OtherTable #'"));
-        assertFalse(HqlQueryUtils.isSafe(
-            "select doc.fullName from XWikiDocument doc where $$='$$=concat( chr( 61 ),(chr( 39 )) ) ;select 1 -- comment'"));
-    }
-
     @Test
     public void replaceLegacyQueryParameters()
     {

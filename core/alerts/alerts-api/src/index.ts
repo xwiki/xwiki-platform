xwiki-contrib/cristal

CRISTAL-382: No clear message when the current user is not allowed to view a page
* Add Provider for AlertsService
* Detect duplicated alerts

core/alerts/alerts-api/src/index.ts
@@ -31,6 +31,11 @@ interface Alert {
   type: Type;
   message: string;
   actions?: Action[];
+  /**
+   * Number of times this specific alert has already been created.
+   * @since 0.13
+   */
+  nDuplicates: number;
 }
 
 /**
@@ -80,4 +85,17 @@ interface AlertsService {
   dismiss(id: number): void;
 }
 
-export type { Action, Alert, AlertsService, Type };
+/**
+ * Helper to resolve an instance of {@link AlertsService} lazily.
+ *
+ * @since 0.13
+ */
+interface AlertsServiceProvider {
+  /**
+   * Get an instance of {@link AlertsService}.
+   * @returns the instance wanted
+   */
+  get(): AlertsService;
+}
+
+export type { Action, Alert, AlertsService, AlertsServiceProvider, Type };

core/alerts/alerts-ui/src/vue/AlertsToasts.vue
@@ -18,7 +18,9 @@ Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
 02110-1301 USA, or see the FSF site: http://www.fsf.org.
 -->
 <script setup lang="ts">
+import messages from "../translations";
 import { inject } from "vue";
+import { useI18n } from "vue-i18n";
 import type { Alert, AlertsService } from "@xwiki/cristal-alerts-api";
 import type { CristalApp } from "@xwiki/cristal-api";
 import type { Ref } from "vue";
@@ -29,6 +31,10 @@ const alertsService = cristal
   .get<AlertsService>("AlertsService")!;
 
 const alerts: Ref<Alert[]> = alertsService.list();
+
+const { t } = useI18n({
+  messages,
+});
 </script>
 
 <template>
@@ -39,6 +45,11 @@ const alerts: Ref<Alert[]> = alertsService.list();
       closable
       :type="alert.type"
       :description="alert.message"
+      :details="
+        alert.nDuplicates > 0
+          ? t('alerts.more', { n: alert.nDuplicates })
+          : undefined
+      "
       :actions="alert.actions"
       @update:model-value="
         (open: boolean) => {

xwiki/xwiki-platform

core/alerts/alerts-api/src/index.ts
@@ -31,6 +31,11 @@ interface Alert {
   type: Type;
   message: string;
   actions?: Action[];
+  /**
+   * Number of times this specific alert has already been created.
+   * @since 0.13
+   */
+  duplicatesCount: number;
 }
 
 /**
@@ -80,4 +85,17 @@ interface AlertsService {
   dismiss(id: number): void;
 }
 
-export type { Action, Alert, AlertsService, Type };
+/**
+ * Helper to resolve an instance of {@link AlertsService} lazily.
+ *
+ * @since 0.13
+ */
+interface AlertsServiceProvider {
+  /**
+   * Get an instance of {@link AlertsService}.
+   * @returns the instance wanted
+   */
+  get(): AlertsService;
+}
+
+export type { Action, Alert, AlertsService, AlertsServiceProvider, Type };

core/alerts/alerts-ui/src/vue/AlertsToasts.vue
@@ -18,7 +18,9 @@ Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
 02110-1301 USA, or see the FSF site: http://www.fsf.org.
 -->
 <script setup lang="ts">
+import messages from "../translations";
 import { inject } from "vue";
+import { useI18n } from "vue-i18n";
 import type { Alert, AlertsService } from "@xwiki/cristal-alerts-api";
 import type { CristalApp } from "@xwiki/cristal-api";
 import type { Ref } from "vue";
@@ -29,6 +31,10 @@ const alertsService = cristal
   .get<AlertsService>("AlertsService")!;
 
 const alerts: Ref<Alert[]> = alertsService.list();
+
+const { t } = useI18n({
+  messages,
+});
 </script>
 
 <template>
@@ -39,6 +45,11 @@ const alerts: Ref<Alert[]> = alertsService.list();
       closable
       :type="alert.type"
       :description="alert.message"
+      :details="
+        alert.duplicatesCount > 0
+          ? t('alerts.more', { n: alert.duplicatesCount })
+          : undefined
+      "
       :actions="alert.actions"
       @update:model-value="
         (open: boolean) => {
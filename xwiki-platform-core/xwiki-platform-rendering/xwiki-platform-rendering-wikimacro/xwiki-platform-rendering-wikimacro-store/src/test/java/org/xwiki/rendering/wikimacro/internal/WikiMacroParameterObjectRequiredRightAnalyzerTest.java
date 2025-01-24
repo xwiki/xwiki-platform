044e8210ca3ccdd04f597a1beda751cf5e387452
Michael Hamann <michael.hamann@xwiki.com>
2025-01-24 15:27:01 +0100

    XWIKI-22760: Execute wiki macro parameter's default values in the macro's context
    * Rewrite the WikiMacroParameterObjectRequiredRightAnalyzer to not
      require xwiki-platform-security-requiredrights-default but instead
      duplicate few lines of code.
    * Remove the xwiki-platform-security-requiredrights-default dependency
      as it pulls this dependency into integration tests where it wasn't
      present before and breaks them as they don't expect the required
      rights analyzer preventing editing.
    * Adapt the test case.
    
    (cherry picked from commit 6f5a9a003da52ffe0cc8d22c19b6a808680875f4)

--- xwiki-platform-core/xwiki-platform-rendering/xwiki-platform-rendering-wikimacro/xwiki-platform-rendering-wikimacro-store/src/test/java/org/xwiki/rendering/wikimacro/internal/WikiMacroParameterObjectRequiredRightAnalyzerTest.java
+++ xwiki-platform-core/xwiki-platform-rendering/xwiki-platform-rendering-wikimacro/xwiki-platform-rendering-wikimacro-store/src/test/java/org/xwiki/rendering/wikimacro/internal/WikiMacroParameterObjectRequiredRightAnalyzerTest.java
@@ -21,26 +21,36 @@ package org.xwiki.rendering.wikimacro.internal;
 
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mock;
+import org.xwiki.model.reference.DocumentReference;
+import org.xwiki.model.reference.EntityReference;
 import org.xwiki.platform.security.requiredrights.RequiredRightAnalysisResult;
-import org.xwiki.platform.security.requiredrights.internal.analyzer.ObjectPropertyRequiredRightAnalyzer;
+import org.xwiki.platform.security.requiredrights.RequiredRightAnalyzer;
+import org.xwiki.rendering.block.WordBlock;
+import org.xwiki.rendering.block.XDOM;
+import org.xwiki.rendering.parser.ContentParser;
+import org.xwiki.rendering.syntax.Syntax;
 import org.xwiki.test.junit5.mockito.ComponentTest;
 import org.xwiki.test.junit5.mockito.InjectMockComponents;
 import org.xwiki.test.junit5.mockito.MockComponent;
 
+import com.xpn.xwiki.doc.XWikiDocument;
 import com.xpn.xwiki.objects.BaseObject;
 import com.xpn.xwiki.objects.PropertyInterface;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.xwiki.rendering.wikimacro.internal.WikiMacroConstants.PARAMETER_DEFAULT_VALUE_PROPERTY;
+import static org.xwiki.rendering.wikimacro.internal.WikiMacroConstants.PARAMETER_DESCRIPTION_PROPERTY;
 import static org.xwiki.rendering.wikimacro.internal.WikiMacroConstants.PARAMETER_TYPE_PROPERTY;
 import static org.xwiki.rendering.wikimacro.internal.WikiMacroConstants.PARAMETER_TYPE_WIKI;
 
@@ -52,39 +62,78 @@ import static org.xwiki.rendering.wikimacro.internal.WikiMacroConstants.PARAMETE
 @ComponentTest
 class WikiMacroParameterObjectRequiredRightAnalyzerTest
 {
+    protected static final String DEFAULT_VALUE = "wiki content";
+
+    protected static final String DESCRIPTION = "wiki description";
+
+    private static final EntityReference DEFAULT_VALUE_REFERENCE = mock();
+
+    private static final EntityReference DESCRIPTION_REFERENCE = mock();
+
+    private static final DocumentReference DOCUMENT_REFERENCE = new DocumentReference("wiki", "space", "page");
+
     @InjectMockComponents
     private WikiMacroParameterObjectRequiredRightAnalyzer analyzer;
 
     @MockComponent
-    private ObjectPropertyRequiredRightAnalyzer propertyRequiredRightAnalyzer;
+    private ContentParser contentParser;
+
+    @MockComponent
+    private RequiredRightAnalyzer<XDOM> xdomRequiredRightAnalyzer;
 
     @Mock
     private BaseObject baseObject;
 
     @Mock
-    private PropertyInterface propertyInterface;
+    private PropertyInterface defaultValueProperty;
+
+    @Mock
+    private PropertyInterface descriptionProperty;
+
+    @Mock
+    private XWikiDocument document;
+
+    private final XDOM descriptionXDOM = new XDOM(List.of(new WordBlock("description")));
+
+    private final XDOM defaultXDOM = new XDOM(List.of(new WordBlock("default")));
+
+    @BeforeEach
+    void setUp() throws Exception
+    {
+        when(this.baseObject.getDocumentReference()).thenReturn(DOCUMENT_REFERENCE);
+        when(this.baseObject.getOwnerDocument()).thenReturn(this.document);
+        when(this.document.getSyntax()).thenReturn(Syntax.XWIKI_2_1);
+
+        when(this.baseObject.getStringValue(PARAMETER_DEFAULT_VALUE_PROPERTY)).thenReturn(DEFAULT_VALUE);
+        when(this.baseObject.getField(PARAMETER_DEFAULT_VALUE_PROPERTY)).thenReturn(this.defaultValueProperty);
+        when(this.defaultValueProperty.getReference()).thenReturn(DEFAULT_VALUE_REFERENCE);
+
+        when(this.baseObject.getStringValue(PARAMETER_DESCRIPTION_PROPERTY)).thenReturn(DESCRIPTION);
+        when(this.baseObject.getField(PARAMETER_DESCRIPTION_PROPERTY)).thenReturn(this.descriptionProperty);
+        when(this.descriptionProperty.getReference()).thenReturn(DESCRIPTION_REFERENCE);
+
+        when(this.contentParser.parse(DEFAULT_VALUE, Syntax.XWIKI_2_1, DOCUMENT_REFERENCE))
+            .thenReturn(this.defaultXDOM);
+        when(this.contentParser.parse(DESCRIPTION, Syntax.XWIKI_2_1, DOCUMENT_REFERENCE))
+            .thenReturn(this.descriptionXDOM);
+    }
 
     @ParameterizedTest
     @ValueSource(strings = { PARAMETER_TYPE_WIKI, "java.util.List<org.xwiki.rendering.block.Block>" })
     void analyzeWithWikiParameterType(String type) throws Exception
     {
         when(this.baseObject.getStringValue(PARAMETER_TYPE_PROPERTY)).thenReturn(type);
-        String defaultValue = "wiki content";
-        when(this.baseObject.getStringValue(PARAMETER_DEFAULT_VALUE_PROPERTY)).thenReturn(defaultValue);
-        when(this.baseObject.getField(PARAMETER_DEFAULT_VALUE_PROPERTY)).thenReturn(this.propertyInterface);
-        RequiredRightAnalysisResult allPropertiesResult = mock();
-        when(this.propertyRequiredRightAnalyzer.analyzeAllProperties(this.baseObject))
-            .thenReturn(List.of(allPropertiesResult));
-        RequiredRightAnalysisResult wikiContentResult = mock();
-        when(this.propertyRequiredRightAnalyzer.analyzeWikiContent(this.baseObject, defaultValue,
-            this.propertyInterface.getReference())).thenReturn(List.of(wikiContentResult));
+
+        RequiredRightAnalysisResult defaultValueResult = mock();
+        when(this.xdomRequiredRightAnalyzer.analyze(this.defaultXDOM)).thenReturn(List.of(defaultValueResult));
+        RequiredRightAnalysisResult descriptionResult = mock();
+        when(this.xdomRequiredRightAnalyzer.analyze(this.descriptionXDOM)).thenReturn(List.of(descriptionResult));
 
         List<RequiredRightAnalysisResult> results = this.analyzer.analyze(this.baseObject);
 
-        assertEquals(List.of(allPropertiesResult, wikiContentResult), results);
-        verify(this.propertyRequiredRightAnalyzer).analyzeAllProperties(this.baseObject);
-        verify(this.propertyRequiredRightAnalyzer).analyzeWikiContent(this.baseObject, defaultValue,
-            this.propertyInterface.getReference());
+        assertEquals(List.of(descriptionResult, defaultValueResult), results);
+        assertEquals(DESCRIPTION_REFERENCE, this.descriptionXDOM.getMetaData().getMetaData("entityReference"));
+        assertEquals(DEFAULT_VALUE_REFERENCE, this.defaultXDOM.getMetaData().getMetaData("entityReference"));
     }
 
     @ParameterizedTest
@@ -93,14 +142,16 @@ class WikiMacroParameterObjectRequiredRightAnalyzerTest
     void analyzeWithNonWikiParameterType(String parameterType) throws Exception
     {
         when(this.baseObject.getStringValue(PARAMETER_TYPE_PROPERTY)).thenReturn(parameterType);
-        RequiredRightAnalysisResult result = mock();
-        when(this.propertyRequiredRightAnalyzer.analyzeAllProperties(this.baseObject)).thenReturn(List.of(result));
+        RequiredRightAnalysisResult descriptionResult = mock();
+        when(this.xdomRequiredRightAnalyzer.analyze(this.descriptionXDOM)).thenReturn(List.of(descriptionResult));
 
         List<RequiredRightAnalysisResult> results = this.analyzer.analyze(this.baseObject);
 
-        assertEquals(List.of(result), results);
-        verify(this.propertyRequiredRightAnalyzer).analyzeAllProperties(this.baseObject);
-        verify(this.propertyRequiredRightAnalyzer, never()).analyzeWikiContent(any(), any(), any());
-        verify(this.baseObject, never()).getStringValue(PARAMETER_DEFAULT_VALUE_PROPERTY);
+        assertEquals(List.of(descriptionResult), results);
+
+        verify(this.xdomRequiredRightAnalyzer, never()).analyze(this.defaultXDOM);
+        verify(this.contentParser, never()).parse(eq(DEFAULT_VALUE), any(), any());
+        verify(this.contentParser).parse(eq(DESCRIPTION), any(), any());
     }
 }
+

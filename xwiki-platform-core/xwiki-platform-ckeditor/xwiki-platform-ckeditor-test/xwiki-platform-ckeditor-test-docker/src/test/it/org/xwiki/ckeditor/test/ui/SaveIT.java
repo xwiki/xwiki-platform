10410406fca08c0014d14b8484bcae10ffa48d47
Marius Dumitru Florea <marius@xwiki.com>
2025-01-29 16:58:11 +0200

    XWIKI-22834: Standalone ID Macro not visible in the WYSIWYG editor * Remove the bogus BR element added by CKEditor in empty block elements from the macro output.
    (cherry picked from commit b4306ef1a4230a63a5770741f0ebfd2f50a19939)

--- xwiki-platform-core/xwiki-platform-ckeditor/xwiki-platform-ckeditor-test/xwiki-platform-ckeditor-test-docker/src/test/it/org/xwiki/ckeditor/test/ui/SaveIT.java
+++ xwiki-platform-core/xwiki-platform-ckeditor/xwiki-platform-ckeditor-test/xwiki-platform-ckeditor-test-docker/src/test/it/org/xwiki/ckeditor/test/ui/SaveIT.java
@@ -41,8 +41,25 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * @version $Id$
  * @since 1.13
  */
-@UITest
-public class SaveIT extends AbstractCKEditorIT
+@UITest(
+    properties = {
+        "xwikiDbHbmCommonExtraMappings=notification-filter-preferences.hbm.xml"
+    },
+    extraJARs = {
+        // It's currently not possible to install a JAR contributing a Hibernate mapping file as an Extension. Thus
+        // we need to provide the JAR inside WEB-INF/lib. See https://jira.xwiki.org/browse/XWIKI-8271
+        "org.xwiki.platform:xwiki-platform-notifications-filters-default",
+
+        // The macro service uses the extension index script service to get the list of uninstalled macros (from
+        // extensions) which expects an implementation of the extension index. The extension index script service is a
+        // core extension so we need to make the extension index also core.
+        "org.xwiki.platform:xwiki-platform-extension-index",
+        // Solr search is used to get suggestions for the link quick action.
+        "org.xwiki.platform:xwiki-platform-search-solr-query"
+    },
+    resolveExtraJARs = true
+)
+class SaveIT extends AbstractCKEditorIT
 {
     @BeforeAll
     public static void configure(TestUtils setup)

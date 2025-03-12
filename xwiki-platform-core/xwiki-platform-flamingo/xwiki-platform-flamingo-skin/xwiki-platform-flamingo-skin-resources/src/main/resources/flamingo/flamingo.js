fb0f39d5d510cab7e31821d43b75c7512f773058
LucasC <lucas.charpentier@xwiki.com>
2025-03-12 16:23:46 +0100

    XWIKI-22727: Make the width of panel column flexible * Factorized code.

--- xwiki-platform-core/xwiki-platform-flamingo/xwiki-platform-flamingo-skin/xwiki-platform-flamingo-skin-resources/src/main/resources/flamingo/flamingo.js
+++ xwiki-platform-core/xwiki-platform-flamingo/xwiki-platform-flamingo-skin/xwiki-platform-flamingo-skin-resources/src/main/resources/flamingo/flamingo.js
@@ -125,25 +125,19 @@ require(['jquery'], function($) {
   This script makes sure they are resizable using the mouse.
 */
 require(['jquery', 'jquery-ui'], function($) {
-  let resizeCustomFunction = function(event, ui, columnPosition) {
+  let resizeCustomFunction = function(columnPosition, event, ui) {
     // We remove the default inline CSS properties.
     ui.element.attr('style', '');
     // We set the one we want when displayed in the flex layout.
     document.body.style.setProperty('--panel-column-' + columnPosition + '-width', ui.size.width + 'px');
   };
-  let resizeCustomFunctionRight = function( event, ui ) {
-    resizeCustomFunction(event, ui, 'right');
-  };
-  let resizeCustomFunctionLeft = function(event, ui) {
-    resizeCustomFunction(event, ui, 'left');
-  };
   $("#rightPanels").resizable({
     handles: 'w',
-    resize: resizeCustomFunctionRight
+    resize: resizeCustomFunction.bind(null, 'right')
   });
   $( "#leftPanels" ).resizable({
     handles: 'e',
-    resize: resizeCustomFunctionLeft
+    resize: resizeCustomFunction.bind(null, 'left')
   });
 });
 

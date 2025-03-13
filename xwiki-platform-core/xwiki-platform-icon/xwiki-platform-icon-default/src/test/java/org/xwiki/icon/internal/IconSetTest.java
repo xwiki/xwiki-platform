4c686e680c9cbf28703dd0217c3b4e02a5490085
Vincent Massol <vincent@massol.net>
2025-03-13 16:14:32 +0100

    [Misc] Migrate test to JUnit5

--- xwiki-platform-core/xwiki-platform-icon/xwiki-platform-icon-default/src/test/java/org/xwiki/icon/internal/IconSetTest.java
+++ xwiki-platform-core/xwiki-platform-icon/xwiki-platform-icon-default/src/test/java/org/xwiki/icon/internal/IconSetTest.java
@@ -19,7 +19,7 @@
  */
 package org.xwiki.icon.internal;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.xwiki.icon.IconSet;
 
 import static org.junit.Assert.assertEquals;
@@ -30,10 +30,10 @@ import static org.junit.Assert.assertEquals;
  * @since 6.3RC1
  * @version $Id$
  */
-public class IconSetTest
+class IconSetTest
 {
     @Test
-    public void simpleTest() throws Exception
+    void simpleTest()
     {
         IconSet iconSet = new IconSet("myIconSet");
         assertEquals("myIconSet", iconSet.getName());

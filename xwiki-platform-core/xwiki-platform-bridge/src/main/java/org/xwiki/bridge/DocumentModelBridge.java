71c089c5acae63e01be2bcac080baf2b99d6409e
Thomas Mortagne <thomas.mortagne@gmail.com>
2025-03-25 10:37:33 +0100

    XWIKI-23018: Introduce an API in DocumentModelBridge to check if the document exist * fix `@since`

--- xwiki-platform-core/xwiki-platform-bridge/src/main/java/org/xwiki/bridge/DocumentModelBridge.java
+++ xwiki-platform-core/xwiki-platform-bridge/src/main/java/org/xwiki/bridge/DocumentModelBridge.java
@@ -235,7 +235,8 @@ public interface DocumentModelBridge
 
     /**
      * @return true if the document is a new one (i.e. it has never been saved) or false otherwise
-     * @since 17.2.0RC1
+     * @since 17.2.0
+     * @since 16.10.6
      */
     default boolean isNew()
     {

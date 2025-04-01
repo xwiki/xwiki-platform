2f50e4246e4af6c2cf4a860270dbf11566d003f9
Simon Urli <simon.urli@xwiki.com>
2025-04-01 10:47:53 +0200

    XWIKI-20528: Add parameter to open nodes of DocumentTree to given level/depth
      * Simplify the solution
      * Provide an integration test

--- xwiki-platform-core/xwiki-platform-index/xwiki-platform-index-tree/xwiki-platform-index-tree-api/src/main/java/org/xwiki/index/tree/IndexTreeScriptService.java
+++ xwiki-platform-core/xwiki-platform-index/xwiki-platform-index-tree/xwiki-platform-index-tree-api/src/main/java/org/xwiki/index/tree/IndexTreeScriptService.java
@@ -19,7 +19,6 @@
  */
 package org.xwiki.index.tree;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -28,10 +27,7 @@ import javax.inject.Singleton;
 
 import org.xwiki.component.annotation.Component;
 import org.xwiki.index.tree.internal.nestedpages.pinned.PinnedChildPagesManager;
-import org.xwiki.model.EntityType;
 import org.xwiki.model.reference.DocumentReference;
-import org.xwiki.model.reference.EntityReference;
-import org.xwiki.model.reference.EntityReferenceProvider;
 import org.xwiki.script.service.ScriptService;
 import org.xwiki.stability.Unstable;
 
@@ -52,9 +48,6 @@ public class IndexTreeScriptService implements ScriptService
     @Inject
     private PinnedChildPagesManager pinnedChildPagesManager;
 
-    @Inject
-    private EntityReferenceProvider entityReferenceProvider;
-
     /**
      * Retrieve the list of pinned child pages of the given parent.
      * @param parent the document for which to find pinned child pages.
@@ -63,37 +56,5 @@ public class IndexTreeScriptService implements ScriptService
     public List<DocumentReference> getPinnedChildPages(DocumentReference parent)
     {
         return this.pinnedChildPagesManager.getPinnedChildPages(parent);
-    }
-
-    /**
-     * Compute a distance between 2 tree nodes represented by their reference. The distance can be understood as the
-     * number of hierarchy in the tree between two nodes, and makes a difference between terminal and non-terminal
-     * documents. For example, the distance between space {@code xwiki:Space1} and document {@code xwiki:Space1.Sub
-     * .MyDocument} is {@code 2}, but with document {@code xwiki:Space1.Sub.WebHome} it's {@code 1}. Also the result is
-     * same if the given parent is document {@code xwiki:Space1.WebHome} instead of space {@code xwiki:Space1}.
-     * Finally if the given parent argument is not a parent of the other argument, then returned value is {@code -1}.
-     * @param nodeParent the reference supposed to be a parent of the other parameter
-     * @param nodeChildren the node for which to compute the distance with the given parent
-     * @return {@code -1} if there's no parent relationship between nodes, else returns the computed distance as
-     * explained above.
-     */
-    public int computeDistanceBetweenNodes(EntityReference nodeParent, EntityReference nodeChildren)
-    {
-        int result = -1;
-        if (nodeParent.equals(nodeChildren)) {
-            result = 0;
-        } else if (nodeChildren.hasParent(nodeParent)) {
-            List<EntityReference> chainElements = new ArrayList<>(nodeChildren.getReversedReferenceChain());
-            chainElements.removeAll(nodeParent.getReversedReferenceChain());
-            result = chainElements.size();
-            if (nodeChildren.getType() == EntityType.DOCUMENT
-                && nodeChildren.getName().equals(
-                    entityReferenceProvider.getDefaultReference(EntityType.DOCUMENT).getName())) {
-                result--;
-            }
-        } else if (nodeParent.getType() == EntityType.DOCUMENT) {
-            result = computeDistanceBetweenNodes(nodeParent.getParent(), nodeChildren);
-        }
-        return result;
     }
 }

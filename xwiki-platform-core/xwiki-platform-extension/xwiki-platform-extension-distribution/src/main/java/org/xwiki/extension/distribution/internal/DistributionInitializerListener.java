f01f4600159a7793b43c8f464a3d86821d9f1930
Thomas Mortagne <thomas.mortagne@gmail.com>
2025-04-10 14:49:49 +0200

    XWIKI-23092: Accessing a page in /view/ with a guest user can cause a useless datase query to count users

--- xwiki-platform-core/xwiki-platform-extension/xwiki-platform-extension-distribution/src/main/java/org/xwiki/extension/distribution/internal/DistributionInitializerListener.java
+++ xwiki-platform-core/xwiki-platform-extension/xwiki-platform-extension-distribution/src/main/java/org/xwiki/extension/distribution/internal/DistributionInitializerListener.java
@@ -29,6 +29,8 @@ import javax.inject.Singleton;
 import org.xwiki.bridge.event.ActionExecutingEvent;
 import org.xwiki.component.annotation.Component;
 import org.xwiki.extension.distribution.internal.DistributionManager.DistributionState;
+import org.xwiki.extension.distribution.internal.job.DistributionJob;
+import org.xwiki.job.event.status.JobStatus.State;
 import org.xwiki.observation.EventListener;
 import org.xwiki.observation.event.Event;
 
@@ -75,13 +77,18 @@ public class DistributionInitializerListener implements EventListener
     @Override
     public void onEvent(Event event, Object arg1, Object arg2)
     {
+        // Check if current distribution job was triggered already
+        if (this.distributionManager.getCurrentDistributionJob() != null) {
+            return;
+        }
+
         XWikiContext xcontext = (XWikiContext) arg2;
-        
+
         // Do nothing if the automatic start of DW is disabled
         if (!isAutoDistributionWizardEnabled(xcontext)) {
             return;
         }
-        
+
         DistributionState distributionState = this.distributionManager.getFarmDistributionState();
 
         // Start the Distribution Wizard only if the current user has the right to access it

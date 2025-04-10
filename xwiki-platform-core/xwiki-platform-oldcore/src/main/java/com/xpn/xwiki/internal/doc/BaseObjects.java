1300df6ca219cbe40186ad5b61a062225b3ce2e0
Thomas Mortagne <thomas.mortagne@gmail.com>
2025-04-10 08:35:00 +0200

    Revert "XWIKI-23079: XWikiDocument#clone can switch the initial document dirty flag to true"
    This reverts commit f88122891f8bae331acd67c63a7f6990644a6ddb.

--- xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/internal/doc/BaseObjects.java
+++ xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/internal/doc/BaseObjects.java
@@ -217,7 +217,6 @@ public class BaseObjects extends AbstractList<BaseObject>
      * @param deep true if the dirty flag should be set to all children
      * @since 17.2.1
      * @since 17.3.0RC1
-     * @since 16.10.6
      */
     @Unstable
     public void setDirty(boolean dirty, boolean deep)

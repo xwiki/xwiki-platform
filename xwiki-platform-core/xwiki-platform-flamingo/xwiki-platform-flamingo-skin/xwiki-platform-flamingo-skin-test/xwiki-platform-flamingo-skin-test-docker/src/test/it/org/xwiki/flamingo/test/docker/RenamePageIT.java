aed29a8dbced7264408b31ba4e368caed9595a6d
Simon Urli <simon.urli@xwiki.com>
2025-04-17 11:28:30 +0200

    XWIKI-23107: Moving a page with a relative link from one wiki to another breaks the link
      * don't check if the link is relative or not for refactoring it as
        it's a bad criteria
      * cover with tests in both single and multi wikis

--- xwiki-platform-core/xwiki-platform-flamingo/xwiki-platform-flamingo-skin/xwiki-platform-flamingo-skin-test/xwiki-platform-flamingo-skin-test-docker/src/test/it/org/xwiki/flamingo/test/docker/RenamePageIT.java
+++ xwiki-platform-core/xwiki-platform-flamingo/xwiki-platform-flamingo-skin/xwiki-platform-flamingo-skin-test/xwiki-platform-flamingo-skin-test-docker/src/test/it/org/xwiki/flamingo/test/docker/RenamePageIT.java
@@ -678,8 +678,12 @@ class RenamePageIT
         throws Exception
     {
         SpaceReference rootSpaceReference = testReference.getLastSpaceReference();
+        DocumentReference externalLinkPage = new DocumentReference("xwiki", "RenamePageIT", "ExternalPage");
+        testUtils.rest().savePage(externalLinkPage);
         testUtils.rest().savePage(testReference, 
-            String.format("[[Alice]]%n[[page:../%s/Alice]]%n[[Bob]]%n[[Eve]]", rootSpaceReference.getName()), 
+            String.format("[[Alice]]%n[[page:../%s/Alice]]%n[[Bob]]%n[[Eve]]"
+                    + "%n[[Other link>>RenamePageIT.ExternalPage]]",
+                rootSpaceReference.getName()),
             "Test relative links");
         
         SpaceReference aliceSpace = new SpaceReference("Alice", rootSpaceReference);
@@ -701,7 +705,8 @@ class RenamePageIT
             new SpaceReference(rootSpaceReference.getName() + "Foo", rootSpaceReference.getParent());
         WikiEditPage wikiEditPage = statusPage.gotoNewPage().editWiki();
         String newRootSpaceSerialized = testUtils.serializeLocalReference(newRootSpace).replaceAll("\\.", "/");
-        assertEquals(String.format("[[Alice]]%n[[page:%s/Alice]]%n[[Bob]]%n[[Eve]]", newRootSpaceSerialized),
+        assertEquals(String.format("[[Alice]]%n[[page:%s/Alice]]%n[[Bob]]%n[[Eve]]"
+                + "%n[[Other link>>RenamePageIT.ExternalPage]]", newRootSpaceSerialized),
             wikiEditPage.getContent());
 
         SpaceReference newBobSpace = new SpaceReference("Bob", newRootSpace);
@@ -720,7 +725,8 @@ class RenamePageIT
         DocumentReference alice2Reference = new DocumentReference("WebHome", alice2Space);
         wikiEditPage = WikiEditPage.gotoPage(new DocumentReference("WebHome", newRootSpace));
         String serializedAlice2Reference = testUtils.serializeLocalReference(alice2Reference);
-        assertEquals(String.format("[[%s]]%n[[page:%s/Alice2]]%n[[Bob]]%n[[Eve]]",
+        assertEquals(String.format("[[%s]]%n[[page:%s/Alice2]]%n[[Bob]]%n[[Eve]]"
+                + "%n[[Other link>>RenamePageIT.ExternalPage]]",
             serializedAlice2Reference, newRootSpaceSerialized), wikiEditPage.getContent());
 
         // FIXME: ideally this one should be refactored too, however it's not a regression.

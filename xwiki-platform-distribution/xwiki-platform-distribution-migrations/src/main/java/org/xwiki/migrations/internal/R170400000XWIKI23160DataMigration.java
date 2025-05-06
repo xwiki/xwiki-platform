7796101133910281669da76836f2c10a6e8c215f
Manuel Leduc <manuel.leduc@xwiki.com>
2025-05-06 11:01:16 +0200

    --wip-- [skip ci]

--- xwiki-platform-distribution/xwiki-platform-distribution-migrations/src/main/java/org/xwiki/migrations/internal/R170400000XWIKI23160DataMigration.java
+++ xwiki-platform-distribution/xwiki-platform-distribution-migrations/src/main/java/org/xwiki/migrations/internal/R170400000XWIKI23160DataMigration.java
@@ -122,7 +122,7 @@ public class R170400000XWIKI23160DataMigration extends AbstractHibernateDataMigr
             xwikiPreferencesDocument.getXObject(xwikiPreferencesDocumentReferenceNoLocale);
         BaseObject xwikiPreferencesXObjectFromXar =
             xwikiPreferencesDocumentFromXar.getXObject(xwikiPreferencesDocumentReferenceNoLocale);
-        if(xwikiPreferencesXObject != null && xwikiPreferencesXObjectFromXar != null) {
+        if (xwikiPreferencesXObject != null && xwikiPreferencesXObjectFromXar != null) {
             String template = xwikiPreferencesXObject.getStringValue(META_FIELD);
             if (StringUtils.isNotEmpty(template)) {
                 String templateFromXar = xwikiPreferencesXObjectFromXar.getStringValue(META_FIELD);

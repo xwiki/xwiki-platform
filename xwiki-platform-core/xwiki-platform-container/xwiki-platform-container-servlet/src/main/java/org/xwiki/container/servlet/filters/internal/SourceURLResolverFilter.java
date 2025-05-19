fe031cee0df1c049eea148368271e504da7bcb0f
Thomas Mortagne <thomas.mortagne@gmail.com>
2025-05-19 10:12:23 +0200

    XWIKI-23207: Wrong scheme returned by ServletRequest#getScheme() and HttpServletRequest#getRequestURL() in Tomcat * HttpServletRequest#getRequestURL() is not supposed to contain the query string
    (cherry picked from commit 2e2a4d72cb6ca5ecd53442c94084d91575c80091)

--- xwiki-platform-core/xwiki-platform-container/xwiki-platform-container-servlet/src/main/java/org/xwiki/container/servlet/filters/internal/SourceURLResolverFilter.java
+++ xwiki-platform-core/xwiki-platform-container/xwiki-platform-container-servlet/src/main/java/org/xwiki/container/servlet/filters/internal/SourceURLResolverFilter.java
@@ -23,6 +23,7 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -108,8 +109,8 @@ public class SourceURLResolverFilter implements Filter
 
         if (filteredRequest instanceof HttpServletRequest httpRequest) {
             try {
-                filteredRequest =
-                    new SourceURLResolverRequest(httpRequest, HttpServletUtils.getSourceURL(httpRequest).toURI());
+                filteredRequest = new SourceURLResolverRequest(httpRequest,
+                    new URL(HttpServletUtils.getSourceBaseURL(httpRequest), httpRequest.getRequestURI()).toURI());
             } catch (URISyntaxException | MalformedURLException e) {
                 // It's very unlikely that the source URL would be invalid, but just ignore it in this case
                 LOGGER.warn("Failed to resolve the source URL ([{}]), falling back to standard ServletRequest.",

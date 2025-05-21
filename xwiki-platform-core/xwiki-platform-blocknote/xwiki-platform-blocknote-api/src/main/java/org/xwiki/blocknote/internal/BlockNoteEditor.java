9ac45cc1495ce2551c85e96d1d958b226486e41f
Marius Dumitru Florea <marius@xwiki.com>
2025-05-21 13:03:43 +0300

    XWIKI-23025: Introduce Blocknote as an editor for XWiki * Boilerplate code to load a Vue-based editor in standalone and in-place edit mode.

---
+++ xwiki-platform-core/xwiki-platform-blocknote/xwiki-platform-blocknote-api/src/main/java/org/xwiki/blocknote/internal/BlockNoteEditor.java
@@ -0,0 +1,71 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.blocknote.internal;
+
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.inject.Singleton;
+
+import org.xwiki.component.annotation.Component;
+import org.xwiki.component.phase.Initializable;
+import org.xwiki.component.phase.InitializationException;
+import org.xwiki.edit.AbstractTemplateEditor;
+import org.xwiki.edit.EditorDescriptor;
+import org.xwiki.edit.EditorDescriptorBuilder;
+import org.xwiki.rendering.syntax.SyntaxContent;
+
+/**
+ * Adds support for editing wiki syntax using the BlockNote editor.
+ *
+ * @version $Id$
+ * @since 17.4.0
+ */
+@Component
+@Singleton
+@Named(BlockNoteEditor.ROLE_HINT)
+public class BlockNoteEditor extends AbstractTemplateEditor<SyntaxContent> implements Initializable
+{
+    /**
+     * The editor component hint.
+     */
+    public static final String ROLE_HINT = "blocknote";
+
+    @Inject
+    private EditorDescriptorBuilder editorDescriptorBuilder;
+
+    @Override
+    public String getTemplateName()
+    {
+        return "blocknote/syntaxContentEditor.wiki";
+    }
+
+    @Override
+    public EditorDescriptor getDescriptor()
+    {
+        // Re-build the descriptor for the current localization context.
+        return this.editorDescriptorBuilder.build();
+    }
+
+    @Override
+    public void initialize() throws InitializationException
+    {
+        this.editorDescriptorBuilder.setId(BlockNoteEditor.ROLE_HINT).setName("BlockNote").setCategory("wysiwyg");
+    }
+}
---
+++ xwiki-platform-core/xwiki-platform-blocknote/xwiki-platform-blocknote-api/src/main/java/org/xwiki/blocknote/internal/BlockNoteRequireJSModuleUIExtension.java
@@ -0,0 +1,92 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.blocknote.internal;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.inject.Singleton;
+
+import org.xwiki.component.annotation.Component;
+import org.xwiki.component.phase.Initializable;
+import org.xwiki.component.phase.InitializationException;
+import org.xwiki.rendering.block.Block;
+import org.xwiki.rendering.block.XDOM;
+import org.xwiki.uiextension.UIExtension;
+import org.xwiki.webjars.WebJarsUrlFactory;
+
+/**
+ * Publishes the BlockNote editor as a RequireJS module.
+ *
+ * @version $Id$
+ * @since 17.4.0
+ */
+@Component
+@Named(BlockNoteRequireJSModuleUIExtension.ID)
+@Singleton
+public class BlockNoteRequireJSModuleUIExtension implements UIExtension, Initializable
+{
+    /**
+     * The id of the UI extension.
+     */
+    public static final String ID = "org.xwiki.platform.requirejs.module.blocknote";
+
+    @Inject
+    private WebJarsUrlFactory webJarsUrlFactory;
+
+    private Map<String, String> parameters;
+
+    @Override
+    public void initialize() throws InitializationException
+    {
+        this.parameters = new HashMap<>();
+        this.parameters.put("id", "xwiki-blocknote");
+        this.parameters.put("bundles", "xwiki-blocknote-inline");
+        this.parameters.put("path",
+            this.webJarsUrlFactory.url("org.xwiki.platform:xwiki-platform-blocknote-webjar", "main.es.js"));
+    }
+
+    @Override
+    public String getId()
+    {
+        return ID;
+    }
+
+    @Override
+    public String getExtensionPointId()
+    {
+        return "org.xwiki.platform.requirejs.module";
+    }
+
+    @Override
+    public Map<String, String> getParameters()
+    {
+        return this.parameters;
+    }
+
+    @Override
+    public Block execute()
+    {
+        return new XDOM(List.of());
+    }
+}

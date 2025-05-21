4c23289b3c90caf40f64cbb3c33b6bfe05735f31
Marius Dumitru Florea <marius@xwiki.com>
2025-05-21 13:03:43 +0300

    XWIKI-23025: Introduce Blocknote as an editor for XWiki * Boilerplate code to load a Vue-based editor in standalone and in-place edit mode.

---
+++ xwiki-platform-core/xwiki-platform-blocknote/xwiki-platform-blocknote-webjar/src/main/node/src/main.js
@@ -0,0 +1,39 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+import { Factory } from "@/services/factory.js";
+import "@/services/inplace.js";
+
+const factory = new Factory();
+
+define("xwiki-blocknote", [], () => factory);
+
+function init(event, data) {
+  const containers = data?.elements || [document];
+  containers
+    .flatMap((container) => [...container.querySelectorAll(".xwiki-blocknote-wrapper")])
+    .forEach((host) => factory.create(host));
+}
+
+require(["jquery"], ($) => {
+  $(document).on("xwiki:dom:updated", init);
+  $(init);
+});
+
+export { factory };

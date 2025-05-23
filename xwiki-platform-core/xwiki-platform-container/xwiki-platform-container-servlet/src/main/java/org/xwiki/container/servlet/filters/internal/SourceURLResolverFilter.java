453272d8a2abcee2098f8f43d8456080ba27e157
Thomas Mortagne <thomas.mortagne@gmail.com>
2025-05-23 10:01:52 +0200

    XWIKI-23207: Wrong scheme returned by ServletRequest#getScheme() and HttpServletRequest#getRequestURL() in Tomcat * fix `@since` * codestyle

--- xwiki-platform-core/xwiki-platform-container/xwiki-platform-container-servlet/src/main/java/org/xwiki/container/servlet/filters/internal/SourceURLResolverFilter.java
+++ xwiki-platform-core/xwiki-platform-container/xwiki-platform-container-servlet/src/main/java/org/xwiki/container/servlet/filters/internal/SourceURLResolverFilter.java
@@ -40,8 +40,7 @@ import org.xwiki.container.servlet.HttpServletUtils;
  * URL.
  * 
  * @version $Id$
- * @since 17.4.0RC1
- * @since 16.10.9
+ * @since 17.5.0RC1
  */
 public class SourceURLResolverFilter implements Filter
 {
@@ -61,10 +60,14 @@ public class SourceURLResolverFilter implements Filter
 
         private int superServerPort;
 
+        // HttpServletRequest#getRequestURL() returns a StringBuffer so we don't really have a choice here
+        @SuppressWarnings("java:S1149")
         private StringBuffer superRequestURL;
 
         private URL baseURL;
 
+        // HttpServletRequest#getRequestURL() returns a StringBuffer so we don't really have a choice here
+        @SuppressWarnings("java:S1149")
         private StringBuffer requestURL;
 
         /**
@@ -75,12 +78,20 @@ public class SourceURLResolverFilter implements Filter
             super(request);
         }
 
+        // We just want to know if something changed in the wrapped request, we don't need to actually compare the
+        // content of the Strings for that.
+        @SuppressWarnings("java:S4973")
+        private boolean superBaseURLChanged()
+        {
+            return this.superScheme != super.getScheme() || this.superServerName != super.getServerName()
+                || this.superServerPort != super.getServerPort();
+        }
+
         private boolean checkBaseURL()
         {
             // Check if super reference changes (which would suggest the values changed and we should recalculate the
             // URL)
-            if (this.superScheme != super.getScheme() || this.superServerName != super.getServerName()
-                || this.superServerPort != super.getServerPort()) {
+            if (superBaseURLChanged()) {
                 try {
                     this.baseURL = HttpServletUtils.getSourceBaseURL((HttpServletRequest) getRequest());
                 } catch (MalformedURLException e) {
@@ -102,21 +113,19 @@ public class SourceURLResolverFilter implements Filter
 
         private boolean checkURL()
         {
-            if (checkBaseURL() || this.superRequestURL != super.getRequestURL()) {
-                if (this.baseURL != null) {
-                    try {
-                        this.requestURL = new StringBuffer(new URL(this.baseURL, super.getRequestURI()).toString());
-                    } catch (MalformedURLException e) {
-                        LOGGER.error("Failed to generate the source URL, falling back to standard ServletRequest.", e);
+            if ((checkBaseURL() || this.superRequestURL != super.getRequestURL()) && this.baseURL != null) {
+                try {
+                    this.requestURL = new StringBuffer(new URL(this.baseURL, super.getRequestURI()).toString());
+                } catch (MalformedURLException e) {
+                    LOGGER.error("Failed to generate the source URL, falling back to standard ServletRequest.", e);
 
-                        this.requestURL = null;
-                    }
-
-                    // Remember current super references
-                    this.superRequestURL = super.getRequestURL();
-
-                    return true;
+                    this.requestURL = null;
                 }
+
+                // Remember current super references
+                this.superRequestURL = super.getRequestURL();
+
+                return true;
             }
 
             return false;

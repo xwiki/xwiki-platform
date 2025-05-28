912cf184b469c0856bfd206ee42a9d0cadabee1a
Marius Dumitru Florea <marius@xwiki.com>
2025-05-28 11:07:16 +0300

    XWIKI-23227: Add support for overriding the default WYSIWYG editor from request

--- xwiki-platform-core/xwiki-platform-container/xwiki-platform-container-default/src/main/java/org/xwiki/container/internal/RequestConfigurationSource.java
+++ xwiki-platform-core/xwiki-platform-container/xwiki-platform-container-default/src/main/java/org/xwiki/container/internal/RequestConfigurationSource.java
@@ -27,61 +27,57 @@ import jakarta.inject.Named;
 import jakarta.inject.Singleton;
 
 import org.xwiki.component.annotation.Component;
-import org.xwiki.configuration.internal.AbstractConfigurationSource;
+import org.xwiki.configuration.internal.AbstractPropertiesConfigurationSource;
 import org.xwiki.container.Container;
 
 /**
  * Configuration source based on the current request parameters.
  *
  * @version $Id$
- * @since 17.4.0
+ * @since 17.5.0RC1
  */
 @Component
 @Singleton
 @Named("request")
-// TODO: Extend AbstractPropertiesConfigurationSource once we move to 17.5.0.
-public class RequestConfigurationSource extends AbstractConfigurationSource
+public class RequestConfigurationSource extends AbstractPropertiesConfigurationSource
 {
     @Inject
     private Container container;
 
     @Override
-    public boolean containsKey(String key)
+    protected boolean containsKeyInternal(String key)
     {
         return this.container.getRequest().getParameter(key) != null;
     }
 
     @Override
-    public List<String> getKeys()
+    protected List<String> getKeysInternal()
     {
         return Collections.list(this.container.getRequest().getParameterNames());
     }
 
+    @Override
+    protected List<String> getKeysInternal(String prefix)
+    {
+        return getKeysInternal().stream().filter(key -> key.startsWith(prefix)).toList();
+    }
+
     @SuppressWarnings("unchecked")
     @Override
-    public <T> T getProperty(String key)
+    protected <T> T getPropertyInternal(String key)
     {
         return (T) this.container.getRequest().getParameter(key);
     }
 
     @Override
-    public <T> T getProperty(String key, T defaultValue)
-    {
-        if (containsKey(key)) {
-            return getProperty(key);
-        }
-        return defaultValue;
-    }
-
-    @Override
-    public <T> T getProperty(String key, Class<T> valueClass)
-    {
-        return getProperty(key);
-    }
-
-    @Override
-    public boolean isEmpty()
+    protected boolean isEmptyInternal()
     {
         return this.container.getRequest().getParameterNames().hasMoreElements();
+    }
+
+    @Override
+    protected boolean isEmptyInternal(String prefix)
+    {
+        return getKeysInternal(prefix).isEmpty();
     }
 }

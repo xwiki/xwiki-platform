f8f84ccc6a46024d03aa41038f0ec642ed22f553
Marius Dumitru Florea <marius@xwiki.com>
2025-05-29 14:44:35 +0300

    XWIKI-23025: Introduce Blocknote as an editor for XWiki * Simplify imports

--- xwiki-platform-core/xwiki-platform-blocknote/xwiki-platform-blocknote-webjar/src/main/node/src/services/factory.test.js
+++ xwiki-platform-core/xwiki-platform-blocknote/xwiki-platform-blocknote-webjar/src/main/node/src/services/factory.test.js
@@ -17,7 +17,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-import { Factory } from "./factory.js";
+import { Factory } from "./factory";
 import { describe, expect, it, vi } from "vitest";
 
 describe("factory.js", () => {

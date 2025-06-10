554fd97adcc7bf9d82106a46fbdb4e819b546ce8
Thomas Mortagne <thomas.mortagne@gmail.com>
2025-06-10 13:33:54 +0200

    XWIKI-23247: Improve REST search query execution
    (cherry picked from commit 743ebf8696ffa55161ed2c5ecf26b09f69e6bcf1)

--- xwiki-platform-core/xwiki-platform-rest/xwiki-platform-rest-server/src/main/java/org/xwiki/rest/internal/resources/pages/PageTranslationHistoryResourceImpl.java
+++ xwiki-platform-core/xwiki-platform-rest/xwiki-platform-rest-server/src/main/java/org/xwiki/rest/internal/resources/pages/PageTranslationHistoryResourceImpl.java
@@ -44,6 +44,7 @@ import org.xwiki.security.authorization.ContextualAuthorizationManager;
 import org.xwiki.security.authorization.Right;
 
 import com.xpn.xwiki.doc.rcs.XWikiRCSNodeId;
+import com.xpn.xwiki.internal.store.hibernate.query.HqlQueryUtils;
 
 /**
  * @version $Id$
@@ -73,15 +74,17 @@ public class PageTranslationHistoryResourceImpl extends XWikiResource implements
         History history = new History();
 
         try {
+            String validOrder = HqlQueryUtils.getValidQueryOrder(order, "desc");
+
             String query = String.format("select doc.space, doc.name, rcs.id, rcs.date, rcs.author, rcs.comment"
                 + " from XWikiRCSNodeInfo as rcs, XWikiDocument as doc where rcs.id.docId = doc.id and"
                 + " doc.space = :space and doc.name = :name and doc.language = :language"
-                + " order by rcs.date %s, rcs.id.version1 %s, rcs.id.version2 %s", order, order, order);
+                + " order by rcs.date %s, rcs.id.version1 %s, rcs.id.version2 %s", validOrder, validOrder, validOrder);
             
             List<Object> queryResult = null;
-            queryResult = queryManager.createQuery(query, Query.XWQL).bindValue("space", spaceId).bindValue("name",
-                    pageName).setLimit(number).bindValue("language", language).setOffset(start)
-                        .setWiki(wikiName).execute();
+            queryResult = this.queryManager.createQuery(query, Query.XWQL).bindValue("space", spaceId)
+                .bindValue("name", pageName).setLimit(number).bindValue("language", language).setOffset(start)
+                .setWiki(wikiName).execute();
 
             for (Object object : queryResult) {
                 Object[] fields = (Object[]) object;

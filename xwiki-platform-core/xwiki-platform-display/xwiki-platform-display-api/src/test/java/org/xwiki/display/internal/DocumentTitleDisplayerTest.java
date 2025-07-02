8b409025834c68245db6f653f30b310188e13347
Michael Hamann <michael.hamann@xwiki.com>
2025-07-02 10:34:55 +0200

    [Misc] Migrate DocumentTitleDisplayerTest to JUnit 5
    (cherry picked from commit 28991f7cd8fb1c949277c12dfc405fad13720b0e)

--- xwiki-platform-core/xwiki-platform-display/xwiki-platform-display-api/src/test/java/org/xwiki/display/internal/DocumentTitleDisplayerTest.java
+++ xwiki-platform-core/xwiki-platform-display/xwiki-platform-display-api/src/test/java/org/xwiki/display/internal/DocumentTitleDisplayerTest.java
@@ -21,13 +21,15 @@ package org.xwiki.display.internal;
 
 import java.io.Reader;
 import java.io.StringReader;
-import java.util.Arrays;
+import java.io.Writer;
 import java.util.HashMap;
+import java.util.List;
+
+import javax.inject.Named;
 
 import org.apache.commons.io.IOUtils;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 import org.xwiki.bridge.DocumentAccessBridge;
@@ -45,101 +47,130 @@ import org.xwiki.rendering.block.XDOM;
 import org.xwiki.rendering.parser.Parser;
 import org.xwiki.security.authorization.DocumentAuthorizationManager;
 import org.xwiki.security.authorization.Right;
-import org.xwiki.test.mockito.MockitoComponentMockingRule;
+import org.xwiki.test.junit5.mockito.ComponentTest;
+import org.xwiki.test.junit5.mockito.InjectMockComponents;
+import org.xwiki.test.junit5.mockito.MockComponent;
+import org.xwiki.velocity.VelocityEngine;
+import org.xwiki.velocity.VelocityManager;
+import org.xwiki.velocity.VelocityTemplate;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertSame;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.same;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 /**
  * Unit tests for {@link DocumentTitleDisplayer}.
- * 
+ *
  * @version $Id$
  */
-public class DocumentTitleDisplayerTest
+@ComponentTest
+class DocumentTitleDisplayerTest
 {
-    @Rule
-    public final MockitoComponentMockingRule<DocumentDisplayer> mocker =
-        new MockitoComponentMockingRule<DocumentDisplayer>(DocumentTitleDisplayer.class);
+    @InjectMockComponents
+    private DocumentTitleDisplayer documentTitleDisplayer;
 
-    @Before
-    public void configure() throws Exception
+    @MockComponent
+    private Execution execution;
+
+    @MockComponent
+    private EntityReferenceProvider defaultEntityReferenceProvider;
+
+    @MockComponent
+    @Named("plain/1.0")
+    private Parser plainTextParser;
+
+    @MockComponent
+    private ModelContext modelContext;
+
+    @MockComponent
+    private DocumentAuthorizationManager authorizationManager;
+
+    @MockComponent
+    private DocumentAccessBridge dab;
+
+    @MockComponent
+    private VelocityManager velocityManager;
+
+    @BeforeEach
+    void configure()
     {
         // The execution context is expected to have the "xwikicontext" property set.
-        Execution mockExecution = this.mocker.getInstance(Execution.class);
         ExecutionContext executionContext = new ExecutionContext();
         executionContext.setProperty("xwikicontext", new HashMap<String, Object>());
-        Mockito.when(mockExecution.getContext()).thenReturn(executionContext);
+        Mockito.when(this.execution.getContext()).thenReturn(executionContext);
     }
 
     @Test
-    public void fallbackOnSpaceNameWhenSpaceHomePageTitleIsEmpty() throws Exception
+    void fallbackOnSpaceNameWhenSpaceHomePageTitleIsEmpty() throws Exception
     {
-        EntityReferenceProvider defaultEntityReferenceProvider = this.mocker.getInstance(EntityReferenceProvider.class);
-        when(defaultEntityReferenceProvider.getDefaultReference(EntityType.DOCUMENT)).thenReturn(
+        when(this.defaultEntityReferenceProvider.getDefaultReference(EntityType.DOCUMENT)).thenReturn(
             new EntityReference("Page", EntityType.DOCUMENT));
 
         DocumentModelBridge document = mock(DocumentModelBridge.class);
-        when(document.getDocumentReference()).thenReturn(new DocumentReference("wiki", Arrays.asList("Space"), "Page"));
+        when(document.getDocumentReference()).thenReturn(new DocumentReference("wiki", List.of("Space"), "Page"));
 
-        XDOM titleXDOM = new XDOM(Arrays.asList(new WordBlock("Space")));
+        XDOM titleXDOM = new XDOM(List.of(new WordBlock("Space")));
 
-        Parser plainTextParser = this.mocker.getInstance(Parser.class, "plain/1.0");
-        when(plainTextParser.parse(any(StringReader.class))).thenReturn(titleXDOM);
+        when(this.plainTextParser.parse(any(StringReader.class))).thenReturn(titleXDOM);
 
         DocumentDisplayerParameters params = new DocumentDisplayerParameters();
         params.setTitleDisplayed(true);
 
-        assertSame(titleXDOM, this.mocker.getComponentUnderTest().display(document, params));
+        assertSame(titleXDOM, this.documentTitleDisplayer.display(document, params));
 
         ArgumentCaptor<Reader> argument = ArgumentCaptor.forClass(Reader.class);
-        verify(plainTextParser).parse(argument.capture());
+        verify(this.plainTextParser).parse(argument.capture());
         assertEquals("Space", IOUtils.toString(argument.getValue()));
     }
 
     @Test
-    public void whenSettingTheContextDocumentTheContextWikiIsAlsoSet() throws Exception
+    void whenSettingTheContextDocumentTheContextWikiIsAlsoSet() throws Exception
     {
-        EntityReferenceProvider defaultEntityReferenceProvider = this.mocker.getInstance(EntityReferenceProvider.class);
-        when(defaultEntityReferenceProvider.getDefaultReference(EntityType.DOCUMENT)).thenReturn(
+        when(this.defaultEntityReferenceProvider.getDefaultReference(EntityType.DOCUMENT)).thenReturn(
             new EntityReference("Page", EntityType.DOCUMENT));
 
         DocumentModelBridge document = mock(DocumentModelBridge.class);
-        DocumentReference documentReference = new DocumentReference("wiki", Arrays.asList("Space"), "Page");
+        DocumentReference documentReference = new DocumentReference("wiki", List.of("Space"), "Page");
         when(document.getDocumentReference()).thenReturn(documentReference);
         when(document.getTitle()).thenReturn("title");
 
-        XDOM titleXDOM = new XDOM(Arrays.asList(new WordBlock("title")));
+        XDOM titleXDOM = new XDOM(List.of(new WordBlock("title")));
 
-        Parser plainTextParser = this.mocker.getInstance(Parser.class, "plain/1.0");
-        when(plainTextParser.parse(any(StringReader.class))).thenReturn(titleXDOM);
+        when(this.plainTextParser.parse(any(StringReader.class))).thenReturn(titleXDOM);
 
-        ModelContext modelContext = this.mocker.getInstance(ModelContext.class);
         WikiReference currentWikiReference = new WikiReference("currentWiki");
-        when(modelContext.getCurrentEntityReference()).thenReturn(currentWikiReference);
+        when(this.modelContext.getCurrentEntityReference()).thenReturn(currentWikiReference);
 
-        DocumentAuthorizationManager authorizationManager = this.mocker.getInstance(DocumentAuthorizationManager.class);
-        when(authorizationManager.hasAccess(eq(Right.SCRIPT), eq(EntityType.DOCUMENT), any(), any())).thenReturn(true);
+        when(this.authorizationManager.hasAccess(eq(Right.SCRIPT), eq(EntityType.DOCUMENT), any(), any()))
+            .thenReturn(true);
 
-        DocumentAccessBridge dab = this.mocker.getInstance(DocumentAccessBridge.class);
+        VelocityEngine velocityEngine = mock();
+        when(this.velocityManager.getVelocityEngine()).thenReturn(velocityEngine);
+
+        doAnswer(invocationOnMock -> {
+            Writer output = invocationOnMock.getArgument(1);
+            output.write("title");
+            return null;
+        }).when(velocityEngine).evaluate(any(), any(), any(), any(VelocityTemplate.class));
 
         DocumentDisplayerParameters params = new DocumentDisplayerParameters();
         params.setTitleDisplayed(true);
         params.setExecutionContextIsolated(true);
 
-        this.mocker.getComponentUnderTest().display(document, params);
+        this.documentTitleDisplayer.display(document, params);
 
         // Check that the context is set.
-        verify(dab).pushDocumentInContext(any(), same(document));
-        verify(modelContext).setCurrentEntityReference(documentReference.getWikiReference());
+        verify(this.dab).pushDocumentInContext(any(), same(document));
+        verify(this.modelContext).setCurrentEntityReference(documentReference.getWikiReference());
 
         // Check that the context is restored.
-        verify(dab).popDocumentFromContext(any());
-        verify(modelContext).setCurrentEntityReference(currentWikiReference);
+        verify(this.dab).popDocumentFromContext(any());
+        verify(this.modelContext).setCurrentEntityReference(currentWikiReference);
     }
 }

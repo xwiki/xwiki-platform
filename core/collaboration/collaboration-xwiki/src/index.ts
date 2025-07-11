84c97f329a1c568b37f40b8f7560b1199c8f8d73
Manuel Leduc <manuel.leduc@xwiki.com>
2025-07-11 14:04:18 +0200

    CRISTAL-581: Introduce a yjs websocket client for XWiki
    Make createXWikiWebsocketProvider importable.

--- core/collaboration/collaboration-xwiki/src/index.ts
+++ core/collaboration/collaboration-xwiki/src/index.ts
@@ -17,4 +17,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-export { ComponentInit } from "./componentInit";
+import { ComponentInit } from "./componentInit";
+import { createXWikiWebsocketProvider } from "./xwikiProviderWebsocket";
+
+export { ComponentInit, createXWikiWebsocketProvider };

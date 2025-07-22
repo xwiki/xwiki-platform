0d4ceb18a83bb2684e2bd6eb118d6554ab59d9d9
Marius Dumitru Florea <marius@xwiki.com>
2025-07-23 01:01:13 +0300

    XWIKI-23228: When clicking "Done", the realtime editor sometimes asks if the user wants to leave the page * Allow registering asynchronous editor destroy handlers, that delay destroy operation. Realtime editing needs this to make sure uncommitted work is pushed to the server (and acknowledgment is received) before the editor is destroyed. * Allow registering asynchronous mode change handlers (e.g. WYSIWYG to Source), in order to be able to push uncommitted work before disconnecting from the realtime session when switching to Source mode * Fix flickering ImageIT#quickInsertImageOtherPage * When editing in-place, wait for the editor to be destroyed async (e.g. uncommitted work pushed to the server) before switching to view mode * Auto-saver needs to push its state to the server before being destroyed
    (cherry picked from commit 23e6afd7279a822231c0f03dba6f272509fe5150)
    (cherry picked from commit 8355b1eae2dacb20b3c04d783c9a0da567cab260)
    (cherry picked from commit 56480374241c6a2077376ed2af9dcdffd9503a4c)

--- xwiki-platform-core/xwiki-platform-ckeditor/xwiki-platform-ckeditor-plugins/src/main/webjar/xwiki-loading/plugin.js
+++ xwiki-platform-core/xwiki-platform-ckeditor/xwiki-platform-ckeditor-plugins/src/main/webjar/xwiki-loading/plugin.js
@@ -130,13 +130,40 @@
           // Fire the instanceReady event only after all the instance ready promises have been settled. Note that we
           // don't want to prevent the instanceReady event if the asynchronous plugin initialization fails because we
           // consider such plugins as optional (we expect them to fail gracefully if they can't be initialized).
-          return Promise.allSettled(this._instanceReadyPromises).then(() => {
+          return Promise.allSettled(this._instanceReadyPromises).then((results) => {
+            logRejectedPromises(
+              `The [${editor.name}] editor is ready, but there were some initialization errors: `,
+              results
+            );
             originalFireOnce.call(this, eventName, ...args);
           });
         } else {
           // Fire the event immediately.
           return originalFireOnce.call(this, eventName, ...args);
         }
+      };
+
+      function logRejectedPromises(message, results) {
+        const errors = results
+          .filter((result) => result.status === "rejected")
+          .map((result) => result.reason);
+        if (errors.length) {
+          console.warn(message, errors);
+        }
+      }
+
+      // Allow plugins to register asynchronous destroy handlers that will be called before the editor is destroyed.
+      const originalDestroy = editor.destroy;
+      editor.destroy = async function(...args) {
+        const promises = [];
+        // Collect the asynchronous destroy handlers.
+        this.fire('beforeDestroyAsync', {promises});
+        // Wait for all the asynchronous destroy handlers to be settled.
+        logRejectedPromises(
+          `We encountered some errors while destroying the [${this.name}] editor: `,
+          await Promise.allSettled(promises)
+        );
+        return originalDestroy.call(this, ...args);
       };
     }
   });

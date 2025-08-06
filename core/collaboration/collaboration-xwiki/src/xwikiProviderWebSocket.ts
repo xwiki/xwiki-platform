f7e672cb236d16e8b07dbce81040c5f4165eed08
Manuel Leduc <manuel.leduc@xwiki.com>
2025-08-06 10:15:03 +0200

    CRISTAL-613: Use WebSocket instead of Websocket
    Update the since version createXWikiWebSocketProvider after its rename

--- core/collaboration/collaboration-xwiki/src/xwikiProviderWebSocket.ts
+++ core/collaboration/collaboration-xwiki/src/xwikiProviderWebSocket.ts
@@ -38,7 +38,7 @@ function readClientId(handlerDecoder: decoding.Decoder) {
  *
  * @param url - the url of the websocket endpoint
  * @param room - the string serialization of the room
- * @since 0.20
+ * @since 0.21
  */
 export function createXWikiWebSocketProvider(
   url: string,

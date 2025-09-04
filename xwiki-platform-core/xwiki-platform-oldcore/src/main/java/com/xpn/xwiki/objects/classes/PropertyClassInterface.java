b014ad74dca9d338bbbd3e16afccf8448970db04
Simon Urli <simon.urli@xwiki.com>
2025-09-04 16:23:00 +0200

    [Misc] Improve oldcore checkstyle

--- xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/objects/classes/PropertyClassInterface.java
+++ xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/objects/classes/PropertyClassInterface.java
@@ -37,17 +37,63 @@ import com.xpn.xwiki.objects.PropertyInterface;
  */
 public interface PropertyClassInterface extends ObjectInterface, PropertyInterface
 {
+    /**
+     * Display the string representation of a property value.
+     * @param property the property for which to display the value
+     * @return the string representation of the property value or an empty string.
+     * @see BaseProperty#toText()
+     */
     String toString(BaseProperty property);
 
+    /**
+     * Create a new property of the current class and parse the given value to set the new property with it.
+     * @param value the string representation of the value to use in the new property
+     * @return a new property with a value set or {@code null} if the class cannot create a new property instance.
+     * @throws XWikiException in case of problem when parsing the value
+     */
     BaseProperty fromString(String value) throws XWikiException;
 
+    /**
+     * Create a new property of the current class and set the new property value with the given value.
+     * @param value the value to use in the new property
+     * @return a new property with a value set or {@code null} if the class cannot create a new property instance.
+     */
     BaseProperty fromValue(Object value);
 
+    /**
+     * Display a hidden input for the given property of the given object in the given buffer.
+     * @param buffer where to write the output
+     * @param name the name of the property to display
+     * @param prefix the prefix to use for the name of the field
+     * @param object the object where to find the property to display
+     * @param context the wiki context to use for computing the values
+     * @see com.xpn.xwiki.objects.classes.PropertyClassInterface#displayHidden(StringBuffer, String, String,
+     * BaseCollection, XWikiContext)
+     */
     void displayHidden(StringBuffer buffer, String name, String prefix, BaseCollection object, XWikiContext context);
 
+    /**
+     * Display the value of the given property in the given buffer.
+     * @param buffer where to write the output
+     * @param name the name of the property to display
+     * @param prefix the prefix to use for the name of the field
+     * @param object the object where to find the property to display
+     * @param context the wiki context to use for computing the values
+     * @see com.xpn.xwiki.objects.classes.PropertyClassInterface#displayView(StringBuffer, String, String,
+     * BaseCollection, XWikiContext)
+     */
     void displayView(StringBuffer buffer, String name, String prefix, BaseCollection object, XWikiContext context);
 
     /**
+     * Display the value of the given property in the given buffer.
+     * @param buffer where to write the output
+     * @param name the name of the property to display
+     * @param prefix the prefix to use for the name of the field
+     * @param object the object where to find the property to display
+     * @param isolated true if the content should be executed in this document's context
+     * @param context the wiki context to use for computing the values
+     * @see com.xpn.xwiki.objects.classes.PropertyClassInterface#displayView(StringBuffer, String, String,
+     * BaseCollection, XWikiContext)
      * @since 13.0
      */
     default void displayView(StringBuffer buffer, String name, String prefix, BaseCollection object, boolean isolated,
@@ -56,9 +102,26 @@ public interface PropertyClassInterface extends ObjectInterface, PropertyInterfa
         displayView(buffer, name, prefix, object, context);
     }
 
+    /**
+     * Display an edit input of the given property in the given buffer.
+     * @param buffer where to write the output
+     * @param name the name of the property to display
+     * @param prefix the prefix to use for the name of the field
+     * @param object the object where to find the property to display
+     * @param context the wiki context to use for computing the values
+     * @see com.xpn.xwiki.objects.classes.PropertyClassInterface#displayEdit(StringBuffer, String, String,
+     * BaseCollection, XWikiContext)
+     */
     void displayEdit(StringBuffer buffer, String name, String prefix, BaseCollection object, XWikiContext context);
 
+    /**
+     * Create a new property of the current class.
+     * @return a new property instance or {@code null} if the class cannot create new properties.
+     */
     BaseProperty newProperty();
 
+    /**
+     * Flush the caches that the class might maintain.
+     */
     void flushCache();
 }

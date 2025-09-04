53ce83181a6e2ec2a97587190c1b6948c1bc6b6c
Thomas Mortagne <thomas.mortagne@gmail.com>
2025-09-04 17:28:34 +0200

    XCOMMONS-3419: Log messages coming from translation file are not parsed the same way as other translation messages * make sure to use the right parser when for translations coming from the translation framework

--- xwiki-platform-core/xwiki-platform-logging/xwiki-platform-logging-script/src/main/java/org/xwiki/logging/script/LoggingScriptService.java
+++ xwiki-platform-core/xwiki-platform-logging/xwiki-platform-logging-script/src/main/java/org/xwiki/logging/script/LoggingScriptService.java
@@ -182,7 +182,10 @@ public class LoggingScriptService implements ScriptService
                 Translation translation = this.localization.getTranslation(message.getTranslationKey());
 
                 if (translation != null) {
-                    return LogUtils.translate(message, (String) translation.getRawSource());
+                    // FIXME: it might not actually be a MessageFormat based translation, introduce a more accurate
+                    // extension point
+                    return LogUtils.translate(message, (String) translation.getRawSource(),
+                        LogUtils.MESSAGE_FORMAT_SYNTAX);
                 }
             }
         }

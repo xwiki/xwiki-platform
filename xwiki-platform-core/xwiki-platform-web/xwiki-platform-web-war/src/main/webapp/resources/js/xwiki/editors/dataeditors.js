29c9a73c6267e01cd8b9e2b910b2264fc9375e50
Simon Urli <simon.urli@xwiki.com>
2025-10-14 10:38:03 +0200

    XWIKI-23589: No warning displayed when leaving object editor if the objects already exist
      * Also listen to textarea changes and not only inputs

--- xwiki-platform-core/xwiki-platform-web/xwiki-platform-web-war/src/main/webapp/resources/js/xwiki/editors/dataeditors.js
+++ xwiki-platform-core/xwiki-platform-web/xwiki-platform-web-war/src/main/webapp/resources/js/xwiki/editors/dataeditors.js
@@ -117,17 +117,17 @@
           self.editorStatus.deletedXObjects = {};
           self.unsavedChanges = false;
         });
-        // We don't want to listen on inputs related to an xclass or an xobject, but not the actual inputs allowing
+        // We want to listen on inputs related to an xclass or an xobject, but not the actual inputs allowing
         // to create a property or an object.
         let filterInputs = function () {
             return $(this).parents('#add_xproperty,#add_xobject').length === 0
                 && $(this).parents('.xclass').length > 0;
         };
-        $('input').filter(filterInputs).on('change', function(e) {
+        $('input,textarea').filter(filterInputs).on('change', function(e) {
           self.unsavedChanges = true;
         });
         $(document).on('xwiki:dom:updated', function (event, data) {
-          $(data.elements).find('input').filter(filterInputs).on('change', function (e) {
+          $(data.elements).find('input,textarea').filter(filterInputs).on('change', function (e) {
             self.unsavedChanges = true;
           });
         });

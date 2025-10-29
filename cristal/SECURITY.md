569929da273cb5b7a2628adacef5d288e80a60d2
Manuel Leduc <manuel.leduc@xwiki.com>
2025-10-29 15:18:36 +0100

    --wip-- [skip ci]

--- cristal/SECURITY.md
+++
@@ -1,3 +0,0 @@
-# Security Policy
-
-Check the [XWiki Security Policy](https://dev.xwiki.org/xwiki/bin/view/Community/SecurityPolicy/).
--- cristal/web/e2e/DesignSystem.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Closed list of supported design systems.
- * All page objects must support those design systems.
- */
-export enum DesignSystem {
-  VUETIFY,
-  SHOELACE,
-}

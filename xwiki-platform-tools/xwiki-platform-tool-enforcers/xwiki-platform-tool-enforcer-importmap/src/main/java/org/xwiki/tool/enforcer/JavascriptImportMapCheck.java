84c5fb6bf1069e9b902ebc5839609513869542b2
Manuel Leduc <manuel.leduc@xwiki.com>
2025-11-13 13:49:46 +0100

    --wip-- [skip ci]

--- xwiki-platform-tools/xwiki-platform-tool-enforcers/xwiki-platform-tool-enforcer-importmap/src/main/java/org/xwiki/tool/enforcer/JavascriptImportMapCheck.java
+++ xwiki-platform-tools/xwiki-platform-tool-enforcers/xwiki-platform-tool-enforcer-importmap/src/main/java/org/xwiki/tool/enforcer/JavascriptImportMapCheck.java
@@ -23,7 +23,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
-import jakarta.inject.Named;
+import javax.inject.Named;
 
 import org.apache.maven.enforcer.rule.api.EnforcerRuleException;
 import org.apache.maven.model.Dependency;

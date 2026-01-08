c20f0879cc3e8919d54c3a50642dc307b53b4932
Michael Hamann <michael.hamann@xwiki.com>
2026-01-08 18:35:56 +0100

    XWIKI-23634: JSX resources loaded twice when loading docextra tab UIX
    * Add a UI test that verifies that DocExtra UI extensions work and that they can load on demand skin extensions but don't load on this page skin extensions again.

--- xwiki-platform-core/xwiki-platform-uiextension/xwiki-platform-uiextension-test/src/test/it/org/xwiki/uiextension/test/ui/UIExtensionTest.java
+++ xwiki-platform-core/xwiki-platform-uiextension/xwiki-platform-uiextension-test/src/test/it/org/xwiki/uiextension/test/ui/UIExtensionTest.java
@@ -30,8 +30,11 @@ import org.xwiki.rest.model.jaxb.Page;
 import org.xwiki.test.ui.AbstractTest;
 import org.xwiki.test.ui.SuperAdminAuthenticationRule;
 import org.xwiki.test.ui.TestUtils.RestTestUtils;
+import org.xwiki.test.ui.po.DocExtraPane;
 import org.xwiki.test.ui.po.ViewPage;
 import org.xwiki.uiextension.internal.WikiUIExtensionConstants;
+
+import com.xpn.xwiki.plugin.skinx.JsSkinExtensionPlugin;
 
 /**
  * UI tests for the UI Extension feature.
@@ -104,5 +107,95 @@ public class UIExtensionTest extends AbstractTest
             + "{HelloWorldKey=HelloWorldValue}\n"
             + "{HelloWorldKey=zz1_XWiki.superadmin}\n"
             + "{HelloWorldKey=zz2_XWiki.superadmin}", page.getContent());
+    }
+
+    @Test
+    public void testDocExtraUIExtension() throws Exception
+    {
+        Page extensionPage = getUtil().rest().page(new LocalDocumentReference(getTestClassName(), "DocExtra"));
+        extensionPage.setContent(
+            "{{velocity}}$xwiki.jsx.use($doc.getDocumentReference(), {'parameter': 'My Custom Value'}){{/velocity}}");
+
+        Objects objects = new Objects();
+        extensionPage.setObjects(objects);
+
+        // Create a JavaScript extension that adds content to the page depending on the parameter. This allows seeing
+        // if the JavaScript extension is loaded again by the DocExtra UI extension.
+        Object jsxObject = RestTestUtils.object(JsSkinExtensionPlugin.JSX_CLASS_NAME);
+        jsxObject.setNumber(0);
+        jsxObject.withProperties(RestTestUtils.property("name", "DocContent"));
+        jsxObject.withProperties(RestTestUtils.property("code", """
+            function doSomething() {
+              document.getElementById('xwikicontent')?.append(
+                $jsontool.serialize("Parameter value: $!request.parameter")
+              );
+            }
+            if (document.readyState === 'loading') {
+              // Loading hasn't finished yet
+              document.addEventListener('DOMContentLoaded', doSomething);
+            } else {
+              // `DOMContentLoaded` has already fired
+              doSomething();
+            }
+            """));
+        jsxObject.withProperties(RestTestUtils.property("cache", "forbid"));
+        jsxObject.withProperties(RestTestUtils.property("parse", "1"));
+        jsxObject.withProperties(RestTestUtils.property("use", "currentPage"));
+        objects.withObjectSummaries(jsxObject);
+
+        // Create a Wiki UI extension that adds a DocExtra pane.
+        Object extensionsObject = RestTestUtils.object(WikiUIExtensionConstants.CLASS_REFERENCE_STRING);
+        extensionsObject.setNumber(0);
+        extensionsObject.withProperties(RestTestUtils.property("name", "DocExtra"));
+        extensionsObject.withProperties(
+            RestTestUtils.property("extensionPointId", "org.xwiki.plaftorm.template.docextra"));
+        extensionsObject.withProperties(RestTestUtils.property("content", """
+            {{velocity output="false"}}
+            ## Load the OnDemandJS extension to verify that the UI extension can load JSX.
+            $xwiki.jsx.use('%s.OnDemandJS')
+            {{/velocity}}
+            
+            Doc extra content
+            """.formatted(getTestClassName())));
+        // Construct parameters
+        String parameters = """
+            show=true
+            title=HelloDocExtra
+            itemnumber=-1
+            name=DocExtra
+            shortcut=alt+k
+            order=1
+            """;
+        extensionsObject.withProperties(RestTestUtils.property("parameters", parameters));
+        objects.withObjectSummaries(extensionsObject);
+
+        getUtil().rest().save(extensionPage);
+
+        // Create a second page with a pure on-demand JavaScript extension that adds a different content to the page.
+        Page onDemandJSPage =
+            getUtil().rest().page(new LocalDocumentReference(getTestClassName(), "OnDemandJS"));
+        Objects onDemandJSObjects = new Objects();
+        onDemandJSPage.setObjects(onDemandJSObjects);
+        Object onDemandJSObject = RestTestUtils.object(JsSkinExtensionPlugin.JSX_CLASS_NAME);
+        onDemandJSObject.setNumber(0);
+        onDemandJSObject.withProperties(RestTestUtils.property("name", "OnDemandJS"));
+        onDemandJSObject.withProperties(RestTestUtils.property("code",
+            "document.getElementById('xwikicontent')?.append('OnDemandJS');"));
+        onDemandJSObject.withProperties(RestTestUtils.property("cache", "long"));
+        onDemandJSObject.withProperties(RestTestUtils.property("parse", "0"));
+        onDemandJSObject.withProperties(RestTestUtils.property("use", "onDemand"));
+        onDemandJSObjects.withObjectSummaries(onDemandJSObject);
+        getUtil().rest().save(onDemandJSPage);
+
+        ViewPage page = getUtil().gotoPage(getTestClassName(), "DocExtra");
+        // Verify that the JavaScript extension is loaded.
+        String expectedPageContent = "Parameter value: My Custom Value";
+        Assert.assertEquals(expectedPageContent, page.getContent().trim());
+        Assert.assertTrue(page.hasDocExtraPane("DocExtra"));
+        DocExtraPane docExtra = page.openDocExtraPane("DocExtra");
+        Assert.assertEquals("Doc extra content", docExtra.getText());
+        // Verify that the page content is not modified by the "on this page" JavaScript extension but is modified by
+        // the JavaScript extension loaded by the DocExtra UI extension.
+        Assert.assertEquals(expectedPageContent + "OnDemandJS", page.getContent().trim());
     }
 }

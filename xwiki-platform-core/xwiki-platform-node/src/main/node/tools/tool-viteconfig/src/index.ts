35ad8a765dd1191e36499fa79f0e5cea092aac06
Manuel Leduc <manuel.leduc@xwiki.com>
2026-03-16 17:20:04 +0100

    [Misc] Introduce a vite config package

--- xwiki-platform-core/xwiki-platform-node/src/main/node/tools/tool-viteconfig/src/index.ts
+++ xwiki-platform-core/xwiki-platform-node/src/main/node/tools/tool-viteconfig/src/index.ts
@@ -17,8 +17,12 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-import { defineConfig } from "vite";
-import { dirname, resolve } from "node:path";
+import vue from "@vitejs/plugin-vue";
+import { defineConfig, mergeConfig } from "vite";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+import dts from "vite-plugin-dts";
+import { copyFileSync, existsSync, readFileSync } from "node:fs";
+import { basename, dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import type { UserConfig } from "vite";
 
@@ -65,4 +69,90 @@ function generateWebjarNodeConfig(
   });
 }
 
-export { generateWebjarNodeConfig };
+function pathsComputation(path: string) {
+  const dir = dirname(fileURLToPath(path));
+  const packageDirName = basename(dir);
+  const pkg = JSON.parse(
+    readFileSync(resolve(dir, "package.json"), { encoding: "utf-8" }),
+  );
+  return { packageDirName, pkg };
+}
+
+/**
+ *
+ * @param path - the path of the build module
+ * @param distPath - an optional parameter in case the target directory is not the default dist
+ * @param entryRoot - an optional parameter in case the entry point is not the default src/index.ts
+ */
+function generateConfig(
+  path: string,
+  distPath: string = "dist",
+  entryRoot: string = "./src/",
+): UserConfig {
+  const { packageDirName, pkg } = pathsComputation(path);
+
+  const libFileName = (format: string) => `index.${format}.js`;
+
+  return defineConfig({
+    build: {
+      sourcemap: true,
+      lib: {
+        entry: `${entryRoot}/index.ts`,
+        name: `cristal_${packageDirName}`,
+        fileName: libFileName,
+      },
+      rollupOptions: {
+        external: [
+          ...Object.keys(pkg.dependencies || {}),
+          ...Object.keys(pkg.peerDependencies || {}),
+        ],
+      },
+    },
+    plugins: [
+      dts({
+        insertTypesEntry: true,
+        entryRoot,
+        afterBuild: () => {
+          // publint suggests having a specific extensions for the exported types for each kind of module
+          // system (esm/cjs). The goal is to make sure packages are supported by all consumers.
+          const originTypeFile = `${distPath}/index.d.ts`;
+          // First check if the module is producing types.
+          if (existsSync(originTypeFile)) {
+            copyFileSync(originTypeFile, `${distPath}/index.d.cts`);
+          }
+        },
+      }),
+    ],
+  });
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function generateConfigVue(path: string): Record<string, any> {
+  const baseConfig = generateConfig(path);
+  return mergeConfig(
+    baseConfig,
+    defineConfig({
+      build: {
+        cssCodeSplit: true,
+        rollupOptions: {
+          // external: Object.keys(pkg.dependencies || {}),
+          output: {
+            globals: {
+              vue: "Vue",
+            },
+          },
+        },
+      },
+      plugins: [
+        ...(baseConfig.plugins ?? []),
+        vue(),
+        // This plugin is useful to make the CSS of a given module loaded by the
+        // module itself, allowing CSS to be loaded even when Cristal is
+        // imported in an external project.
+        cssInjectedByJsPlugin(),
+      ],
+    }),
+  );
+}
+
+export { generateConfig, generateConfigVue, generateWebjarNodeConfig };

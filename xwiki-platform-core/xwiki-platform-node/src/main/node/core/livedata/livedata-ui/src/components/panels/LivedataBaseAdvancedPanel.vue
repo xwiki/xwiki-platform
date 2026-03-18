2f05bb2649134fcbd402bdcb1a36a3d3fcb69458
LucasC <lucas.charpentier@xwiki.com>
2026-03-18 10:49:38 +0100

    XWIKI-24124: LiveData's "More actions" (kebab) dropdown menu is not reachable using the keyboard * Updated CSS

--- xwiki-platform-core/xwiki-platform-node/src/main/node/core/livedata/livedata-ui/src/components/panels/LivedataBaseAdvancedPanel.vue
+++ xwiki-platform-core/xwiki-platform-node/src/main/node/core/livedata/livedata-ui/src/components/panels/LivedataBaseAdvancedPanel.vue
@@ -51,19 +51,28 @@
       <!-- Panel header buttons -->
       <div class="actions">
         <!-- Collapse panel button -->
-        <button class="action collapse-button" @click="collapsed = !collapsed">
+        <button
+          class="action collapse-button btn btn-default"
+          @click="collapsed = !collapsed"
+        >
           <XWikiIcon
             v-if="!collapsed"
             :icon-descriptor="{ name: 'arrow_in' }"
           />
           <XWikiIcon v-else :icon-descriptor="{ name: 'arrow_out' }" />
+          <span class="sr-only">
+            {{ $t("livedata.panel.heading.actions.collapse.hint") }}
+          </span>
         </button>
         <!-- Close panel button -->
         <button
-          class="action close-button"
+          class="action close-button btn btn-default"
           @click="logic.uniqueArrayRemove(logic.openedPanels, panelId)"
         >
           <XWikiIcon :icon-descriptor="{ name: 'cross' }" />
+          <span class="sr-only">
+            {{ $t("livedata.panel.heading.actions.close.hint") }}
+          </span>
         </button>
       </div>
     </div>
@@ -151,5 +160,7 @@ export default {
   align-items: center;
   padding: 0 15px;
   cursor: pointer;
+  border: none;
+  background: none;
 }
 </style>

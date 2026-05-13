d25369c6ae874bed6f0348937edb3fe2b7469b35
Manuel Leduc <manuel.leduc@xwiki.com>
2026-05-13 13:17:11 +0200

    [Misc] Fix UserDirectoryIT failures

--- xwiki-platform-core/xwiki-platform-user/xwiki-platform-user-directory/xwiki-platform-user-directory-test/xwiki-platform-user-directory-test-docker/src/test/it/org/xwiki/user/directory/test/ui/UserDirectoryIT.java
+++ xwiki-platform-core/xwiki-platform-user/xwiki-platform-user-directory/xwiki-platform-user-directory-test/xwiki-platform-user-directory-test-docker/src/test/it/org/xwiki/user/directory/test/ui/UserDirectoryIT.java
@@ -21,6 +21,7 @@ package org.xwiki.user.directory.test.ui;
 
 import java.util.Map;
 
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.xwiki.test.docker.junit5.UITest;
 import org.xwiki.test.integration.junit.LogCaptureConfiguration;
@@ -68,12 +69,44 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class UserDirectoryIT
 {
     @Test
+    @Order(1)
+    void verifyLiveTableUserResults(TestUtils testUtils) throws Exception
+    {
+        testUtils.loginAsSuperAdmin();
+        testUtils.createUser("UserDirectoryITUser", "foobar", null, "email", "foo@xwiki.com");
+        testUtils.forceGuestUser();
+
+        String jsonResult = testUtils.getString("/bin/get/XWiki/UserDirectoryLivetableResults", Map.of(
+            "outputSyntax", "plain",
+            "transprefix", "xe.userdirectory.",
+            "classname", "XWiki.XWikiUsers",
+            "collist", "doc.name,first_name,last_name,email,password",
+            "sort", "doc.creationDate"
+        ));
+        System.out.println(jsonResult);
+        JsonNode jsonNode = new ObjectMapper().readTree(jsonResult);
+        assertInstanceOf(ObjectNode.class, jsonNode);
+        ObjectNode objectNode = (ObjectNode) jsonNode;
+        ArrayNode rows = (ArrayNode) objectNode.get("rows");
+        assertEquals(1, rows.size());
+
+        ObjectNode userCreated = (ObjectNode) rows.get(0);
+        assertEquals("XWiki.UserDirectoryITUser", userCreated.get("doc_fullName").asText());
+        assertEquals("********",  userCreated.get("password").asText().trim());
+        assertEquals("",  userCreated.get("password_value").asText().trim());
+        assertEquals("f...@xwiki.com",  userCreated.get("email").asText().trim());
+        assertEquals("f...@xwiki.com",  userCreated.get("email_value").asText().trim());
+    }
+
+    @Test
+    @Order(2)
     void verifyUserIsListed(TestUtils setup, LogCaptureConfiguration logCaptureConfiguration)
     {
         setup.loginAsSuperAdmin();
 
-        // Delete possible existing user
+        // Delete possible existing users
         setup.deletePage("XWiki", "test");
+        setup.deletePage("XWiki", "UserDirectoryITUser");
 
         UserDirectoryPage page = UserDirectoryPage.gotoPage();
 
@@ -106,33 +139,5 @@ class UserDirectoryIT
             "Exception in macro #displayCheckedIfWatched called at",
             "Exception in macro #generateNotificationInput called at"
         );
-    }
-
-    @Test
-    void verifyLiveTableUserResults(TestUtils testUtils) throws Exception
-    {
-        testUtils.loginAsSuperAdmin();
-        testUtils.createUser("UserDirectoryITUser", "foobar", null, "email", "foo@xwiki.com");
-        testUtils.forceGuestUser();
-
-        String jsonResult = testUtils.getString("/bin/get/XWiki/UserDirectoryLivetableResults", Map.of(
-            "outputSyntax", "plain",
-            "transprefix", "xe.userdirectory.",
-            "classname", "XWiki.XWikiUsers",
-            "collist", "doc.name,first_name,last_name,email,password",
-            "sort", "doc.creationDate"
-        ));
-        JsonNode jsonNode = new ObjectMapper().readTree(jsonResult);
-        assertInstanceOf(ObjectNode.class, jsonNode);
-        ObjectNode objectNode = (ObjectNode) jsonNode;
-        ArrayNode rows = (ArrayNode) objectNode.get("rows");
-        assertEquals(1, rows.size());
-
-        ObjectNode userCreated = (ObjectNode) rows.get(0);
-        assertEquals("XWiki.UserDirectoryITUser", userCreated.get("doc_fullName").asText());
-        assertEquals("********",  userCreated.get("password").asText().trim());
-        assertEquals("",  userCreated.get("password_value").asText().trim());
-        assertEquals("f...@xwiki.com",  userCreated.get("email").asText().trim());
-        assertEquals("f...@xwiki.com",  userCreated.get("email_value").asText().trim());
     }
 }

f0be94b34549970aed2e2abf79dd73f976a01886
Simon Urli <simon.urli@xwiki.com>
2026-05-18 16:36:37 +0200

    XWIKI-24346: Fix headers in attachment-version REST APIs
    (cherry picked from commit ed3471cd3ab69a704560785a104129d428b9ce64)

--- xwiki-platform-core/xwiki-platform-rest/xwiki-platform-rest-server/src/main/java/org/xwiki/rest/internal/resources/attachments/AttachmentResourceImpl.java
+++ xwiki-platform-core/xwiki-platform-rest/xwiki-platform-rest-server/src/main/java/org/xwiki/rest/internal/resources/attachments/AttachmentResourceImpl.java
@@ -22,7 +22,6 @@ package org.xwiki.rest.internal.resources.attachments;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
-import javax.inject.Inject;
 import javax.inject.Named;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
@@ -30,7 +29,6 @@ import javax.ws.rs.core.Response.Status;
 
 import org.xwiki.attachment.validation.AttachmentValidationException;
 import org.xwiki.component.annotation.Component;
-import org.xwiki.internal.attachment.XWikiAttachmentSecurityManager;
 import org.xwiki.rest.XWikiRestException;
 import org.xwiki.rest.internal.Utils;
 import org.xwiki.rest.internal.resources.BaseAttachmentsResource;
@@ -41,7 +39,6 @@ import com.xpn.xwiki.XWikiException;
 import com.xpn.xwiki.api.Document;
 import com.xpn.xwiki.doc.XWikiAttachment;
 import com.xpn.xwiki.doc.XWikiDocument;
-import com.xpn.xwiki.util.Util;
 
 /**
  * @version $Id$
@@ -50,9 +47,6 @@ import com.xpn.xwiki.util.Util;
 @Named("org.xwiki.rest.internal.resources.attachments.AttachmentResourceImpl")
 public class AttachmentResourceImpl extends BaseAttachmentsResource implements AttachmentResource
 {
-    @Inject
-    private XWikiAttachmentSecurityManager attachmentSecurityManager;
-
     @Override
     public Response getAttachment(String wikiName, String spaceName, String pageName, String attachmentName)
         throws XWikiRestException
@@ -60,31 +54,7 @@ public class AttachmentResourceImpl extends BaseAttachmentsResource implements A
         try {
             DocumentInfo documentInfo = getDocumentInfo(wikiName, spaceName, pageName, null, null, true, false);
             Document doc = documentInfo.getDocument();
-
-            final com.xpn.xwiki.api.Attachment xwikiAttachment = doc.getAttachment(attachmentName);
-            if (xwikiAttachment == null) {
-                throw new WebApplicationException(Status.NOT_FOUND);
-            }
-
-            String ofilename = Util.encodeURI(xwikiAttachment.getFilename(), getXWikiContext())
-                .replaceAll("\\+", "%20");
-            // The inline attribute of Content-Disposition tells the browser that they should display
-            // the downloaded file in the page (see http://www.ietf.org/rfc/rfc1806.txt for more
-            // details). We do this so that JPG, GIF, PNG, etc are displayed without prompting a Save
-            // dialog box. However, all mime types that cannot be displayed by the browser do prompt a
-            // Save dialog box (exe, zip, xar, etc).
-            String dispType = "inline";
-            // If the mimetype is not authorized to be displayed inline,
-            // let's force its content disposition to download.
-            if (attachmentSecurityManager.shouldBeDownloaded(xwikiAttachment)) {
-                dispType = "attachment";
-            }
-            return Response
-                .ok()
-                .type(xwikiAttachment.getMimeType())
-                .entity(xwikiAttachment.getContent())
-                .header("Content-Disposition", dispType + "; filename*=utf-8''" + ofilename)
-                .build();
+            return answerWithAttachment(doc.getAttachment(attachmentName));
         } catch (XWikiException e) {
             throw new XWikiRestException(e);
         }

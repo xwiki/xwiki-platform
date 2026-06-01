blog2i2j/xwiki.._..xwiki-platform

XWIKI-24200: Improve header handling for downloading attachments XWIKI-24235: Improve header handling for VFS
(cherry picked from commit 7472126ffada28b1a443b98ef0f6ec1ab7ccdba9)

xwiki-platform-core/xwiki-platform-oldcore/src/main/java/org/xwiki/internal/attachment/XWikiAttachmentSecurityManager.java
@@ -1,0 +1,134 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.internal.attachment;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.xwiki.component.annotation.Component;
+import org.xwiki.configuration.ConfigurationSource;
+import org.xwiki.container.Container;
+import org.xwiki.container.Request;
+import org.xwiki.model.reference.DocumentReference;
+import org.xwiki.security.authorization.AuthorizationManager;
+import org.xwiki.security.authorization.Right;
+
+import com.xpn.xwiki.XWikiContext;
+import com.xpn.xwiki.doc.XWikiAttachment;
+import com.xpn.xwiki.web.DownloadAction;
+
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.inject.Provider;
+import jakarta.inject.Singleton;
+
+/**
+ * Generic check to define if an attachment can be provided inline or if it needs to be downloaded.
+ *
+ * @version $Id$
+ * @since 18.4.0RC1
+ */
+@Component(roles = XWikiAttachmentSecurityManager.class)
+@Singleton
+public class XWikiAttachmentSecurityManager
+{
+    @Inject
+    @Named("xwikiproperties")
+    private ConfigurationSource configuration;
+
+    @Inject
+    private AuthorizationManager authorizationManager;
+
+    @Inject
+    private Container container;
+
+    @Inject
+    private Provider<XWikiContext> contextProvider;
+
+    /**
+     * Check if an attachment should be downloaded or can be displayed inline.
+     * Attachments should be downloaded if:
+     * <ul>
+     *     <li>the request contains a force-download parameter with the value 1</li>
+     *     <li>or the mimetype is part of the forceDownload property list</li>
+     *     <li>or no custom whitelist exists and a custom blacklist exists and contains the mimetype and the attachment
+     *     has not been added by a user with programming rights</li>
+     *     <li>or the whitelist (default or custom) does not contains the mimetype and the attachment has not been added
+     *     by a user with programming rights</li>
+     * </ul>
+     *
+     * Note that in the case of the request does not contain a force-downloaded parameter with the value 1 and the
+     * mimetype is not part of the forceDownload property list, but the attachment has been added by a user with
+     * programming right, then the attachment is always displayed inline.
+     *
+     * @param attachment the attachment to check if it should be downloaded or provided inline.
+     * @return {@code true} if the attachment should be downloaded, and {@code false} if it can be displayed inline.
+     */
+    public boolean shouldBeDownloaded(XWikiAttachment attachment)
+    {
+        DocumentReference authorReference = attachment.getAuthorReference();
+        boolean hasPR = this.authorizationManager.hasAccess(Right.PROGRAM, authorReference,
+            this.contextProvider.get().getWikiReference());
+        String mimeType = attachment.getMimeType(this.contextProvider.get());
+        return shouldBeDownloaded(mimeType, hasPR);
+    }
+
+    private boolean shouldBeDownloaded(String mimeType, boolean hasPR)
+    {
+        boolean result;
+        Request request = this.container.getRequest();
+        boolean whiteListExists = configuration.containsKey(DownloadAction.WHITELIST_PROPERTY);
+        boolean blackListExists = configuration.containsKey(DownloadAction.BLACKLIST_PROPERTY);
+        List<String> blackList = configuration.getProperty(DownloadAction.BLACKLIST_PROPERTY, Collections.emptyList());
+        List<String> whiteList =
+            configuration.getProperty(DownloadAction.WHITELIST_PROPERTY, DownloadAction.MIMETYPE_WHITELIST);
+        List<String> forceDownloadList =
+            configuration.getProperty(DownloadAction.FORCE_DOWNLOAD_PROPERTY, Collections.emptyList());
+
+        if ("1".equals(request.getParameter("force-download")) || forceDownloadList.contains(mimeType)) {
+            result = true;
+        } else if (hasPR) {
+            result = false;
+        } else if (blackListExists && !whiteListExists) {
+            result = blackList.contains(mimeType);
+        } else {
+            result = !whiteList.contains(mimeType);
+        }
+        return result;
+    }
+
+    /**
+     * Check if a resource with a given mimetype should be downloaded or can be displayed inline.
+     * Attachments should be downloaded if:
+     * <ul>
+     *     <li>the request contains a force-download parameter with the value 1</li>
+     *     <li>or the mimetype is part of the forceDownload property list</li>
+     *     <li>or no custom whitelist exists and a custom blacklist exists and contains the mimetype</li>
+     *     <li>or the whitelist (default or custom) does not contains the mimetype</li>
+     * </ul>
+     *
+     * @param mimeType the mimetype of the resource to check if it should be downloaded or provided inline.
+     * @return {@code true} if the resource should be downloaded, and {@code false} if it can be displayed inline.
+     */
+    public boolean shouldBeDownloaded(String mimeType)
+    {
+        return shouldBeDownloaded(mimeType, false);
+    }
+}

xwiki-platform-core/xwiki-platform-rest/xwiki-platform-rest-server/src/main/java/org/xwiki/rest/internal/resources/attachments/AttachmentResourceImpl.java
@@ -29,6 +29,7 @@ import javax.ws.rs.core.Response.Status;
 
 import org.xwiki.attachment.validation.AttachmentValidationException;
 import org.xwiki.component.annotation.Component;
+import org.xwiki.internal.attachment.XWikiAttachmentSecurityManager;
 import org.xwiki.rest.XWikiRestException;
 import org.xwiki.rest.internal.resources.BaseAttachmentsResource;
 import org.xwiki.rest.resources.attachments.AttachmentResource;
@@ -37,6 +38,9 @@ import org.xwiki.security.authorization.Right;
 import com.xpn.xwiki.XWikiException;
 import com.xpn.xwiki.api.Attachment;
 import com.xpn.xwiki.api.Document;
+import com.xpn.xwiki.util.Util;
+
+import jakarta.inject.Inject;
 
 /**
  * @version $Id$
@@ -45,6 +49,9 @@ import com.xpn.xwiki.api.Document;
 @Named("org.xwiki.rest.internal.resources.attachments.AttachmentResourceImpl")
 public class AttachmentResourceImpl extends BaseAttachmentsResource implements AttachmentResource
 {
+    @Inject
+    private XWikiAttachmentSecurityManager attachmentSecurityManager;
+
     @Override
     public Response getAttachment(String wikiName, String spaceName, String pageName, String attachmentName)
         throws XWikiRestException
@@ -58,7 +65,25 @@ public class AttachmentResourceImpl extends BaseAttachmentsResource implements A
                 throw new WebApplicationException(Status.NOT_FOUND);
             }
 
-            return Response.ok().type(xwikiAttachment.getMimeType()).entity(xwikiAttachment.getContent()).build();
+            String ofilename = Util.encodeURI(xwikiAttachment.getFilename(), getXWikiContext())
+                .replaceAll("\\+", "%20");
+            // The inline attribute of Content-Disposition tells the browser that they should display
+            // the downloaded file in the page (see http://www.ietf.org/rfc/rfc1806.txt for more
+            // details). We do this so that JPG, GIF, PNG, etc are displayed without prompting a Save
+            // dialog box. However, all mime types that cannot be displayed by the browser do prompt a
+            // Save dialog box (exe, zip, xar, etc).
+            String dispType = "inline";
+            // If the mimetype is not authorized to be displayed inline,
+            // let's force its content disposition to download.
+            if (attachmentSecurityManager.shouldBeDownloaded(xwikiAttachment.getAttachment())) {
+                dispType = "attachment";
+            }
+            return Response
+                .ok()
+                .type(xwikiAttachment.getMimeType())
+                .entity(xwikiAttachment.getContent())
+                .header("Content-Disposition", dispType + "; filename*=utf-8''" + ofilename)
+                .build();
         } catch (XWikiException e) {
             throw new XWikiRestException(e);
         }

xwiki/xwiki-platform

(cherry picked from commit e795561fe570eef2b37a84ef3ada55667f607445)

xwiki-platform-core/xwiki-platform-oldcore/src/main/java/org/xwiki/internal/attachment/XWikiAttachmentSecurityManager.java
@@ -1,0 +1,135 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.internal.attachment;
+
+import java.util.Collections;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Provider;
+import javax.inject.Singleton;
+
+import org.xwiki.component.annotation.Component;
+import org.xwiki.configuration.ConfigurationSource;
+import org.xwiki.container.Container;
+import org.xwiki.container.Request;
+import org.xwiki.model.reference.DocumentReference;
+import org.xwiki.security.authorization.AuthorizationManager;
+import org.xwiki.security.authorization.Right;
+
+import com.xpn.xwiki.XWikiContext;
+import com.xpn.xwiki.doc.XWikiAttachment;
+import com.xpn.xwiki.web.DownloadAction;
+
+
+/**
+ * Generic check to define if an attachment can be provided inline or if it needs to be downloaded.
+ *
+ * @version $Id$
+ * @since 18.4.0RC1
+ */
+@Component(roles = XWikiAttachmentSecurityManager.class)
+@Singleton
+public class XWikiAttachmentSecurityManager
+{
+    @Inject
+    @Named("xwikiproperties")
+    private ConfigurationSource configuration;
+
+    @Inject
+    private AuthorizationManager authorizationManager;
+
+    @Inject
+    private Container container;
+
+    @Inject
+    private Provider<XWikiContext> contextProvider;
+
+    /**
+     * Check if an attachment should be downloaded or can be displayed inline.
+     * Attachments should be downloaded if:
+     * <ul>
+     *     <li>the request contains a force-download parameter with the value 1</li>
+     *     <li>or the mimetype is part of the forceDownload property list</li>
+     *     <li>or no custom whitelist exists and a custom blacklist exists and contains the mimetype and the attachment
+     *     has not been added by a user with programming rights</li>
+     *     <li>or the whitelist (default or custom) does not contains the mimetype and the attachment has not been added
+     *     by a user with programming rights</li>
+     * </ul>
+     *
+     * Note that in the case of the request does not contain a force-downloaded parameter with the value 1 and the
+     * mimetype is not part of the forceDownload property list, but the attachment has been added by a user with
+     * programming right, then the attachment is always displayed inline.
+     *
+     * @param attachment the attachment to check if it should be downloaded or provided inline.
+     * @return {@code true} if the attachment should be downloaded, and {@code false} if it can be displayed inline.
+     */
+    public boolean shouldBeDownloaded(XWikiAttachment attachment)
+    {
+        DocumentReference authorReference = attachment.getAuthorReference();
+        boolean hasPR = this.authorizationManager.hasAccess(Right.PROGRAM, authorReference,
+            this.contextProvider.get().getWikiReference());
+        String mimeType = attachment.getMimeType(this.contextProvider.get());
+        return shouldBeDownloaded(mimeType, hasPR);
+    }
+
+    private boolean shouldBeDownloaded(String mimeType, boolean hasPR)
+    {
+        boolean result;
+        Request request = this.container.getRequest();
+        boolean whiteListExists = configuration.containsKey(DownloadAction.WHITELIST_PROPERTY);
+        boolean blackListExists = configuration.containsKey(DownloadAction.BLACKLIST_PROPERTY);
+        List<String> blackList = configuration.getProperty(DownloadAction.BLACKLIST_PROPERTY, Collections.emptyList());
+        List<String> whiteList =
+            configuration.getProperty(DownloadAction.WHITELIST_PROPERTY, DownloadAction.MIMETYPE_WHITELIST);
+        List<String> forceDownloadList =
+            configuration.getProperty(DownloadAction.FORCE_DOWNLOAD_PROPERTY, Collections.emptyList());
+
+        if ("1".equals(request.getProperty("force-download")) || forceDownloadList.contains(mimeType)) {
+            result = true;
+        } else if (hasPR) {
+            result = false;
+        } else if (blackListExists && !whiteListExists) {
+            result = blackList.contains(mimeType);
+        } else {
+            result = !whiteList.contains(mimeType);
+        }
+        return result;
+    }
+
+    /**
+     * Check if a resource with a given mimetype should be downloaded or can be displayed inline.
+     * Attachments should be downloaded if:
+     * <ul>
+     *     <li>the request contains a force-download parameter with the value 1</li>
+     *     <li>or the mimetype is part of the forceDownload property list</li>
+     *     <li>or no custom whitelist exists and a custom blacklist exists and contains the mimetype</li>
+     *     <li>or the whitelist (default or custom) does not contains the mimetype</li>
+     * </ul>
+     *
+     * @param mimeType the mimetype of the resource to check if it should be downloaded or provided inline.
+     * @return {@code true} if the resource should be downloaded, and {@code false} if it can be displayed inline.
+     */
+    public boolean shouldBeDownloaded(String mimeType)
+    {
+        return shouldBeDownloaded(mimeType, false);
+    }
+}

xwiki-platform-core/xwiki-platform-rest/xwiki-platform-rest-server/src/main/java/org/xwiki/rest/internal/resources/attachments/AttachmentResourceImpl.java
@@ -22,6 +22,7 @@ package org.xwiki.rest.internal.resources.attachments;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
+import javax.inject.Inject;
 import javax.inject.Named;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
@@ -29,6 +30,7 @@ import javax.ws.rs.core.Response.Status;
 
 import org.xwiki.attachment.validation.AttachmentValidationException;
 import org.xwiki.component.annotation.Component;
+import org.xwiki.internal.attachment.XWikiAttachmentSecurityManager;
 import org.xwiki.rest.XWikiRestException;
 import org.xwiki.rest.internal.Utils;
 import org.xwiki.rest.internal.resources.BaseAttachmentsResource;
@@ -39,6 +41,7 @@ import com.xpn.xwiki.XWikiException;
 import com.xpn.xwiki.api.Document;
 import com.xpn.xwiki.doc.XWikiAttachment;
 import com.xpn.xwiki.doc.XWikiDocument;
+import com.xpn.xwiki.util.Util;
 
 /**
  * @version $Id$
@@ -47,6 +50,9 @@ import com.xpn.xwiki.doc.XWikiDocument;
 @Named("org.xwiki.rest.internal.resources.attachments.AttachmentResourceImpl")
 public class AttachmentResourceImpl extends BaseAttachmentsResource implements AttachmentResource
 {
+    @Inject
+    private XWikiAttachmentSecurityManager attachmentSecurityManager;
+
     @Override
     public Response getAttachment(String wikiName, String spaceName, String pageName, String attachmentName)
         throws XWikiRestException
@@ -60,7 +66,25 @@ public class AttachmentResourceImpl extends BaseAttachmentsResource implements A
                 throw new WebApplicationException(Status.NOT_FOUND);
             }
 
-            return Response.ok().type(xwikiAttachment.getMimeType()).entity(xwikiAttachment.getContent()).build();
+            String ofilename = Util.encodeURI(xwikiAttachment.getFilename(), getXWikiContext())
+                .replaceAll("\\+", "%20");
+            // The inline attribute of Content-Disposition tells the browser that they should display
+            // the downloaded file in the page (see http://www.ietf.org/rfc/rfc1806.txt for more
+            // details). We do this so that JPG, GIF, PNG, etc are displayed without prompting a Save
+            // dialog box. However, all mime types that cannot be displayed by the browser do prompt a
+            // Save dialog box (exe, zip, xar, etc).
+            String dispType = "inline";
+            // If the mimetype is not authorized to be displayed inline,
+            // let's force its content disposition to download.
+            if (attachmentSecurityManager.shouldBeDownloaded(xwikiAttachment.getAttachment())) {
+                dispType = "attachment";
+            }
+            return Response
+                .ok()
+                .type(xwikiAttachment.getMimeType())
+                .entity(xwikiAttachment.getContent())
+                .header("Content-Disposition", dispType + "; filename*=utf-8''" + ofilename)
+                .build();
         } catch (XWikiException e) {
             throw new XWikiRestException(e);
         }
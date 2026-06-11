28492cc00a721aba9ca3847ee6004674da994759
Simon Urli <simon.urli@xwiki.com>
2026-06-11 09:51:48 +0200

    XWIKI-24174: Wrong author used in toAnnotatedXHTML in edge cases
    (cherry picked from commit 1ff1ad666c39e1bcab80a1822b4725763a8a3447)

--- xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/objects/classes/TextAreaClass.java
+++ xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/objects/classes/TextAreaClass.java
@@ -397,7 +397,7 @@ public class TextAreaClass extends StringClass
         parameters.put("rows", getRows());
         parameters.put("disabled", isDisabled());
         parameters.put(RESTRICTED, isRestricted() || (ownerDocument != null && ownerDocument.isRestricted()));
-        parameters.put("sourceDocumentReference", object.getDocumentReference());
+        parameters.put("sourceDocumentReference", ownerDocument.getDocumentReferenceWithLocale());
         Syntax syntax = null;
         String contentType = getContentType();
 

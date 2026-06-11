f33a3373ada813aa772c1f38737d87e9bd89810c
Simon Urli <simon.urli@xwiki.com>
2026-06-11 09:51:48 +0200

    XWIKI-24174: Wrong author used in toAnnotatedXHTML in edge cases
    (cherry picked from commit 1ff1ad666c39e1bcab80a1822b4725763a8a3447)

---
+++ xwiki-platform-core/xwiki-platform-wysiwyg/xwiki-platform-wysiwyg-api/src/test/java/org/xwiki/wysiwyg/script/WysiwygEditorScriptServiceTest.java
@@ -0,0 +1,122 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.wysiwyg.script;
+
+import java.util.concurrent.Callable;
+
+import javax.inject.Named;
+import javax.inject.Provider;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.xwiki.model.document.DocumentAuthors;
+import org.xwiki.model.reference.DocumentReference;
+import org.xwiki.rendering.syntax.Syntax;
+import org.xwiki.security.authorization.AuthorExecutor;
+import org.xwiki.test.junit5.mockito.ComponentTest;
+import org.xwiki.test.junit5.mockito.InjectMockComponents;
+import org.xwiki.test.junit5.mockito.MockComponent;
+import org.xwiki.user.UserReference;
+import org.xwiki.user.UserReferenceSerializer;
+import org.xwiki.wysiwyg.converter.HTMLConverter;
+
+import com.xpn.xwiki.XWiki;
+import com.xpn.xwiki.XWikiContext;
+import com.xpn.xwiki.doc.XWikiDocument;
+import com.xpn.xwiki.web.XWikiRequest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Tests for {@link WysiwygEditorScriptService}.
+ * 
+ * @version $Id$
+ * @since 17.5.0
+ */
+@ComponentTest
+class WysiwygEditorScriptServiceTest
+{
+    @InjectMockComponents
+    private WysiwygEditorScriptService editorScriptService;
+
+    @MockComponent
+    private Provider<XWikiContext> contextProvider;
+
+    @MockComponent
+    @Named("document")
+    private UserReferenceSerializer<DocumentReference> userDocumentReferenceSerializer;
+
+    @MockComponent
+    private AuthorExecutor authorExecutor;
+
+    @MockComponent
+    private HTMLConverter htmlConverter;
+
+    @Mock
+    private XWikiContext context;
+
+    @BeforeEach
+    void setup() throws Exception
+    {
+        when(this.contextProvider.get()).thenReturn(context);
+        when(this.authorExecutor.call(any(), any(), any()))
+            .then(invocation -> ((Callable) invocation.getArgument(0)).call());
+    }
+
+    @Test
+    void toAnnotatedXHTML() throws Exception
+    {
+        String source = "some source";
+        Syntax syntax = Syntax.XWIKI_2_1;
+        DocumentReference sourceReference = new DocumentReference("xwiki", "Sandbox", "MyPage");
+        boolean restricted = false;
+        XWikiDocument contextDoc = mock(XWikiDocument.class, "contextDoc");
+        when(this.context.getDoc()).thenReturn(contextDoc);
+        when(contextDoc.clone()).thenReturn(contextDoc);
+        when(contextDoc.getAuthors()).thenReturn(mock(DocumentAuthors.class));
+        XWikiRequest request = mock(XWikiRequest.class);
+        when(this.context.getRequest()).thenReturn(request);
+
+        XWiki wiki = mock(XWiki.class);
+        when(this.context.getWiki()).thenReturn(wiki);
+        XWikiDocument sourceDoc = mock(XWikiDocument.class, "sourceDoc");
+        when(wiki.getDocument(sourceReference, context)).thenReturn(sourceDoc);
+        UserReference contentAuthor = mock(UserReference.class);
+        DocumentReference contentAuthorRef = mock(DocumentReference.class);
+        DocumentAuthors authors = mock(DocumentAuthors.class);
+        when(authors.getContentAuthor()).thenReturn(contentAuthor);
+        when(sourceDoc.getAuthors()).thenReturn(authors);
+        when(this.userDocumentReferenceSerializer.serialize(contentAuthor)).thenReturn(contentAuthorRef);
+        when(sourceDoc.getContent()).thenReturn(source);
+
+        String expectedSource = "obtained source";
+        when(this.htmlConverter.toHTML(source, syntax, sourceReference, restricted)).thenReturn(expectedSource);
+        assertEquals(expectedSource, this.editorScriptService
+            .toAnnotatedXHTML(source, syntax, sourceReference, restricted));
+        verify(this.authorExecutor)
+            .call(any(Callable.class), eq(contentAuthorRef), eq((DocumentReference) sourceReference));
+    }
+}

e80db6e9b4bd03bd6d69aa07df4b0a81fd629441
LucasC <lucas.charpentier@xwiki.com>
2026-07-01 16:09:51 +0200

    XWIKI-20349: Add automated test for "Footer Space Copyright" (#5348)
    * Added tests for customizing version and copyright
    * Added methods to get the version and copyright that is displayed in the footer of a page
    
    (cherry picked from commit f8b8b7aa4253efcf002c42622ef7090c1c03f9a1)

--- xwiki-platform-core/xwiki-platform-test/xwiki-platform-test-ui/src/main/java/org/xwiki/test/ui/po/ViewPage.java
+++ xwiki-platform-core/xwiki-platform-test/xwiki-platform-test-ui/src/main/java/org/xwiki/test/ui/po/ViewPage.java
@@ -429,4 +429,22 @@ public class ViewPage extends BasePage
         reviewButton.click();
         return new RequiredRightsModal();
     }
+
+    /**
+     * @return the content of the copyright section in the page footer
+     * @since 18.6.0RC1
+     */
+    public String getFooterCopyright()
+    {
+        return getDriver().findElement(By.id("xwikilicence")).getText();
+    }
+
+    /**
+     * @return the content of the version section in the page footer
+     * @since 18.6.0RC1
+     */
+    public String getFooterVersion()
+    {
+        return getDriver().findElement(By.id("xwikiplatformversion")).getText();
+    }
 }

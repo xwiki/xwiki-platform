4cb5717c51408d64f2057cbf7044bcacbb197e25
Vincent Massol <vincent@massol.net>
2026-07-03 11:14:44 +0200

    XWIKI-24507: New annotations are saved as major version in history * Also fix edit + delete to be minor (similar to comments)

--- xwiki-platform-core/xwiki-platform-annotation/xwiki-platform-annotation-test/xwiki-platform-annotation-test-pageobjects/src/main/java/org/xwiki/annotation/test/po/AnnotatableViewPage.java
+++ xwiki-platform-core/xwiki-platform-annotation/xwiki-platform-annotation-test/xwiki-platform-annotation-test-pageobjects/src/main/java/org/xwiki/annotation/test/po/AnnotatableViewPage.java
@@ -268,13 +268,12 @@ public class AnnotatableViewPage extends BaseElement
 
     public void simulateCTRL_M()
     {
+        // The annotation shortcut is registered as "Meta+M". The Keypress library resolves the "meta" key to the
+        // actual Command key only when the browser runs on macOS; on any other platform it resolves to the Control
+        // key. Since the browser always runs in a Linux Docker container, the shortcut is effectively "Ctrl+M" there,
+        // regardless of the operating system running these tests. We therefore always send Ctrl+M.
         WebElement body = getDriver().findElement(By.id("body"));
-        String os = System.getProperty("os.name");
-        if ("Mac OS X".equals(os)) {
-            body.sendKeys(Keys.chord(Keys.COMMAND, "m"));
-        } else {
-            body.sendKeys(Keys.chord(Keys.CONTROL, "m"));
-        }
+        body.sendKeys(Keys.chord(Keys.CONTROL, "m"));
     }
 
     /**

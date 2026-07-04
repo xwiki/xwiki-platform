523c893234f5f2d3996e94b499c0608e337627c3
Vincent Massol <vincent@massol.net>
2026-07-04 11:05:10 +0200

    XWIKI-20349: Add automated test for "Footer Space Copyright"
    Adjusted @since: added backport versions on the new class where applicable, and dropped @since on test-support methods.

--- xwiki-platform-core/xwiki-platform-test/xwiki-platform-test-ui/src/main/java/org/xwiki/test/ui/po/ViewPage.java
+++ xwiki-platform-core/xwiki-platform-test/xwiki-platform-test-ui/src/main/java/org/xwiki/test/ui/po/ViewPage.java
@@ -432,7 +432,6 @@ public class ViewPage extends BasePage
 
     /**
      * @return the content of the copyright section in the page footer
-     * @since 18.6.0RC1
      */
     public String getFooterCopyright()
     {
@@ -441,7 +440,6 @@ public class ViewPage extends BasePage
 
     /**
      * @return the content of the version section in the page footer
-     * @since 18.6.0RC1
      */
     public String getFooterVersion()
     {

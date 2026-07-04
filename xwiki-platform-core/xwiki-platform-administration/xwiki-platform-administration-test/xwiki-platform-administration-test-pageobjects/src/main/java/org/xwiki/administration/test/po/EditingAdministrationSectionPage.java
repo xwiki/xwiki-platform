e3033fbff309cb69e11d3fa4681f58ad2c1fd3bd
Vincent Massol <vincent@massol.net>
2026-07-04 09:56:49 +0200

    XWIKI-18672: Add automated test for "Change Default Editor"
    * cherry picked from commit a83ba1d8a5d18677fa1b74b1053b0a4c811b2e34
    * Add @since annotations for the backport.

---
+++ xwiki-platform-core/xwiki-platform-administration/xwiki-platform-administration-test/xwiki-platform-administration-test-pageobjects/src/main/java/org/xwiki/administration/test/po/EditingAdministrationSectionPage.java
@@ -0,0 +1,90 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.administration.test.po;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.xwiki.test.ui.po.Select;
+
+/**
+ * Represents the actions possible on the Editing administration section, where the wiki-level default editor (the
+ * default edit mode used when editing a page) can be configured.
+ *
+ * @version $Id$
+ * @since 17.10.10
+ * @since 18.4.3
+ * @since 18.5.0RC1
+ */
+public class EditingAdministrationSectionPage extends AdministrationSectionPage
+{
+    private static final String SECTION_ID = "Editing";
+
+    /**
+     * The dropdown used to select the default editor (i.e. the default edit mode). The empty value means "Default",
+     * "Text" means the Wiki editor, and "Wysiwyg" means the WYSIWYG editor.
+     */
+    @FindBy(name = "XWiki.XWikiPreferences_0_editor")
+    private WebElement defaultEditorSelect;
+
+    /**
+     * Open the Editing administration section.
+     *
+     * @return the Editing administration section
+     */
+    public static EditingAdministrationSectionPage gotoPage()
+    {
+        AdministrationSectionPage.gotoPage(SECTION_ID);
+        return new EditingAdministrationSectionPage();
+    }
+
+    public EditingAdministrationSectionPage()
+    {
+        super(SECTION_ID);
+    }
+
+    /**
+     * @return the dropdown element used to select the default editor
+     */
+    public Select getDefaultEditorSelect()
+    {
+        return new Select(this.defaultEditorSelect);
+    }
+
+    /**
+     * Set the default editor and save the changes.
+     *
+     * @param value the value of the editor to select ({@code ""} for "Default", {@code "Text"} for the Wiki editor and
+     *            {@code "Wysiwyg"} for the WYSIWYG editor)
+     */
+    public void setDefaultEditor(String value)
+    {
+        getDefaultEditorSelect().selectByValue(value);
+        clickSave();
+    }
+
+    /**
+     * @return the value of the currently selected default editor ({@code ""} for "Default", {@code "Text"} for the Wiki
+     *         editor and {@code "Wysiwyg"} for the WYSIWYG editor)
+     */
+    public String getDefaultEditor()
+    {
+        return getDefaultEditorSelect().getFirstSelectedOption().getAttribute("value");
+    }
+}

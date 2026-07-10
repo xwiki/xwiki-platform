9d5e39cab8f01e9620fa308326b8446e57f2b8df
Pierre Jeanjean <rhiobet@gmail.com>
2026-07-10 16:50:45 +0200

    Update xwiki-platform-core/xwiki-platform-livedata/xwiki-platform-livedata-livetable/src/main/java/org/xwiki/livedata/internal/livetable/ModelBridge.java
    Co-authored-by: Manuel Leduc <manuel.leduc@xwiki.com>

--- xwiki-platform-core/xwiki-platform-livedata/xwiki-platform-livedata-livetable/src/main/java/org/xwiki/livedata/internal/livetable/ModelBridge.java
+++ xwiki-platform-core/xwiki-platform-livedata/xwiki-platform-livedata-livetable/src/main/java/org/xwiki/livedata/internal/livetable/ModelBridge.java
@@ -381,7 +381,7 @@ public class ModelBridge
      * @param documentReference the reference of the document to check
      * @return true if the document already exists
      * @throws XWikiException if there was a problem during lookup
-     * @since 18.5.0RC1
+     * @since 18.6.0RC1
      */
     @Unstable
     public boolean exists(DocumentReference documentReference) throws XWikiException

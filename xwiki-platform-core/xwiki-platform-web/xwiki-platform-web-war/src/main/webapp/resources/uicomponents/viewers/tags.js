dbbfb94add254643fd6256d2f3705e835be8659c
LucasC <lucas.charpentier@xwiki.com>
2026-07-15 10:57:25 +0200

    XWIKI-15069: Make Tags use IconThemes * Added needed escaping

--- xwiki-platform-core/xwiki-platform-web/xwiki-platform-web-war/src/main/webapp/resources/uicomponents/viewers/tags.js
+++ xwiki-platform-core/xwiki-platform-web/xwiki-platform-web-war/src/main/webapp/resources/uicomponents/viewers/tags.js
@@ -173,7 +173,7 @@ viewers.Tags = Class.create({
       varname: 'input',
       seps: "${xwiki.getDocument('XWiki.TagClass').xWikiClass.tags.getProperty('separators').value}",
       shownoresults : false,
-      icon: '${services.icon.renderHTML("tag")} '
+      icon: '$escapetool.javascript($services.icon.renderHTML("tag")) '
     });
   }
 });

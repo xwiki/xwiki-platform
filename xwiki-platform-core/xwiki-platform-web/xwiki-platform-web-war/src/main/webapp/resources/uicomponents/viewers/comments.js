28d4f8520a78ee382873358aba683cd10de48471
LucasC <lucas.charpentier@xwiki.com>
2026-07-15 18:06:10 +0200

    XWIKI-23740: Improve the UI of Comments: Annotation look * Implemented the share button

--- xwiki-platform-core/xwiki-platform-web/xwiki-platform-web-war/src/main/webapp/resources/uicomponents/viewers/comments.js
+++ xwiki-platform-core/xwiki-platform-web/xwiki-platform-web-war/src/main/webapp/resources/uicomponents/viewers/comments.js
@@ -679,6 +679,16 @@ require(['jquery'], function($) {
   $(document).on('click', '#permalinkModal input.btn-primary', function() {
     window.location = $('#permalinkModal').find('.form-control').val();
   });
+  /**
+   * Copy the permalink to the clipboard.
+   */
+  $(document).on('click', '#permalink-copy-button', function() {
+    var button = this;
+    var permalinkValue = $('#permalinkModal').find('.form-control').val();
+    navigator.clipboard.writeText(permalinkValue).then(function() {
+      new XWiki.widgets.Notification($(button).data('copiedMessage'), 'info');
+    });
+  });
 });
 
 /**

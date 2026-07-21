91f677fb2a36da2b4369a4aef95f4d9fdf66fc02
Vincent Massol <vincent@massol.net>
2026-07-21 14:16:36 +0200

    [Misc] Fix sonarqube-reported issues that block the quality gate
    (cherry picked from commit 630fe598748109ba75f6de268c972ad08c5c3ce3)

--- xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/doc/XWikiDocument.java
+++ xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/doc/XWikiDocument.java
@@ -5493,7 +5493,7 @@ public class XWikiDocument implements DocumentModelBridge, Cloneable, Disposable
         DocumentInstanceOutputProperties documentProperties = new DocumentInstanceOutputProperties();
         XWikiContext xcontext = getXWikiContext();
         if (xcontext != null) {
-            documentProperties.setDefaultReference(getXWikiContext().getWikiReference());
+            documentProperties.setDefaultReference(xcontext.getWikiReference());
         }
 
         // Input

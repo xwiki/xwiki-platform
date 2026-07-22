891637f5dc94a0aa0098d0a9ca1ce85b9b4e4cb7
Vincent Massol <vincent@massol.net>
2026-07-22 13:35:27 +0200

    [Misc] Migrate the WebJars functional test to the JUnit5 Docker test framework
    Convert WebJarsTest (JUnit4, xwiki-platform-test-ui, packager plugin) to the JUnit5 Docker
    framework (@UITest) in a new xwiki-platform-webjars-test-docker module, and remove the old
    packager-based xwiki-platform-webjars-test-tests module. Both tests (webJars, pathTraversal)
    are preserved with identical assertions.

--- xwiki-platform-core/xwiki-platform-webjars/xwiki-platform-webjars-test/xwiki-platform-webjars-test-docker/src/test/it/org/xwiki/webjars/test/ui/WebJarsIT.java
+++ xwiki-platform-core/xwiki-platform-webjars/xwiki-platform-webjars-test/xwiki-platform-webjars-test-docker/src/test/it/org/xwiki/webjars/test/ui/WebJarsIT.java
@@ -23,69 +23,65 @@ import java.net.URI;
 
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.lang3.Strings;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.xwiki.test.ui.AbstractTest;
-import org.xwiki.test.ui.SuperAdminAuthenticationRule;
+import org.xwiki.test.docker.junit5.UITest;
+import org.xwiki.test.ui.TestUtils;
 import org.xwiki.test.ui.po.ViewPage;
 
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Functional tests for the WebJars integration.
  *
  * @version $Id$
- * @since 7.4M2
+ * @since 18.7.0RC1
  */
-public class WebJarsTest extends AbstractTest
+@UITest
+class WebJarsIT
 {
-    @Rule
-    public SuperAdminAuthenticationRule superAdminAuthenticationRule = new SuperAdminAuthenticationRule(getUtil());
+    @BeforeAll
+    static void beforeAll(TestUtils setup)
+    {
+        setup.loginAsSuperAdmin();
+    }
 
     @Test
-    public void testWebJars() throws Exception
+    void webJars(TestUtils setup) throws Exception
     {
-        // Delete pages that we create in the test
-        getUtil().rest().deletePage(getTestClassName(), getTestMethodName());
+        // Delete pages that we create in the test.
+        setup.rest().deletePage("WebJarsIT", "webJars");
 
-        // Create a page in which:
-        // - we install a webjar in the main wiki
-        // - we use the WebJars Script Service to generate a URL for a resource inside that webjar
-        // - we create a link to that resource
-        // Then we click that link and verify we get the resource content.
-        //
-        // This test validates both the ability to generate webjars URL, the serving of webjars through the webjars URL
-        // and the ability to access a webjars installed in a specific wiki.
+        // Create a page in which we install a webjar in a wiki namespace, generate a URL to a
+        // resource inside it, and link to it. Then follow the link and verify the served content.
         String content = "{{velocity}}\n"
             + "#set ($job = $services.extension.install('org.webjars:AjaxQ', '0.0.2', 'wiki:mywiki'))\n"
             + "#set ($discard = $job.join())\n"
             + "installed: $services.extension.installed.getInstalledExtension('org.webjars:AjaxQ', 'wiki:mywiki').id\n"
             + "[[AjaxQ>>path:$services.webjars.url('org.webjars:AjaxQ', 'ajaxq.js', {'wiki' : 'mywiki'})]]\n"
             + "{{/velocity}}";
-        ViewPage vp = getUtil().createPage(getTestClassName(), getTestMethodName(), content, "WebJars Test");
+        ViewPage vp = setup.createPage("WebJarsIT", "webJars", content, "WebJars Test");
 
         assertTrue(vp.getContent().contains("installed: org.webjars:AjaxQ/0.0.2"));
 
-        // Click the link!
-        // Note: For understanding why there's a r=1 query string, see WebJarsScriptService#getResourceReference()
-        WebElement link = getDriver().findElementWithoutWaiting(
+        // Click the link! (For why there's a r=1 query string, see WebJarsScriptService#getResourceReference().)
+        WebElement link = setup.getDriver().findElementWithoutWaiting(
             By.xpath("//a[@href = '/xwiki/webjars/wiki%3Amywiki/AjaxQ/0.0.2/ajaxq.js?r=1']"));
         link.click();
 
-        // Verify that the served resource is the one from the webjars
-        assertTrue(getDriver().getPageSource().contains("// AjaxQ jQuery Plugin"));
+        assertTrue(setup.getDriver().getPageSource().contains("// AjaxQ jQuery Plugin"));
     }
 
     @Test
-    public void pathTraversal() throws Exception
+    void pathTraversal(TestUtils setup) throws Exception
     {
-        URI uri = new URI(Strings.CS.removeEnd(getUtil().rest().getBaseURL(), "rest")
+        URI uri = new URI(Strings.CS.removeEnd(setup.rest().getBaseURL(), "rest")
             + "webjars/wiki%3Axwiki/..%2F..%2F..%2F..%2F..%2FWEB-INF%2Fxwiki.cfg");
 
-        GetMethod response = getUtil().rest().executeGet(uri);
+        GetMethod response = setup.rest().executeGet(uri);
 
         assertNotEquals(200, response.getStatusCode());
 

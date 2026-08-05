765739c6db147e2ad31f3e7849d769bdd351d68e
Thomas Mortagne <thomas.mortagne@gmail.com>
2026-08-05 10:20:04 +0200

    XWIKI-24659: Provider a public helper to sent events about authenticated users
    (cherry picked from commit b9f72df76b6ff1ea63446559f25ad4718cc1a7ef)

--- xwiki-platform-core/xwiki-platform-oldcore/src/main/java/org/xwiki/internal/authentication/DefaultUserAuthenticatedEventNotifier.java
+++ xwiki-platform-core/xwiki-platform-oldcore/src/main/java/org/xwiki/internal/authentication/DefaultUserAuthenticatedEventNotifier.java
@@ -18,33 +18,36 @@
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
 
-package com.xpn.xwiki.internal.user;
+package org.xwiki.internal.authentication;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 import javax.inject.Singleton;
 
 import org.slf4j.Logger;
-
 import org.xwiki.component.annotation.Component;
+import org.xwiki.model.reference.DocumentReference;
 import org.xwiki.observation.ObservationManager;
 import org.xwiki.security.authentication.UserAuthenticatedEvent;
+import org.xwiki.security.authentication.UserAuthenticatedEventNotifier;
 import org.xwiki.user.UserReference;
 import org.xwiki.user.UserReferenceResolver;
 
 /**
  * This notifier helps dealing with events triggered when a user is authenticated through XWiki Oldcore's
- * authenticators. It wraps an {@code ObservationManager} and a {@code UserReferenceResolver} to
- * notify about user who are authenticated through {@code MyFormAuthenticator} and
- * {@code MyBasicAuthenticator}.
+ * authenticators. It wraps an {@code ObservationManager} and a {@code UserReferenceResolver} to notify about user who
+ * are authenticated through {@code MyFormAuthenticator} and {@code MyBasicAuthenticator}.
  *
  * @version $Id$
- * @since 13.3RC1
+ * @since 16.10.19
+ * @since 18.7.0RC1
+ * @since 18.4.4
+ * @since 17.10.12
  */
-@Component(roles = UserAuthenticatedEventNotifier.class)
+@Component
 @Singleton
-public class UserAuthenticatedEventNotifier
+public class DefaultUserAuthenticatedEventNotifier implements UserAuthenticatedEventNotifier
 {
-
     @Inject
     private Logger logger;
 
@@ -52,19 +55,28 @@ public class UserAuthenticatedEventNotifier
     private ObservationManager observationManager;
 
     @Inject
-    private UserReferenceResolver<String> userReferenceResolver;
+    private UserReferenceResolver<String> stringResolver;
 
-    /**
-     * Resolve a string as a {@code UserReference} and notify a {@code UserAuthenticatedEvent} created with that user
-     * reference.
-     *
-     * @param stringUserReference string form of the reference of user that will be resolved as a {@code
-     * UserReference} and passed to the {@code UserAuthenticatedEvent} instance creation
-     */
-    public void notify(String stringUserReference)
+    @Inject
+    @Named("document")
+    private UserReferenceResolver<DocumentReference> documentResolver;
+
+    @Override
+    public void notify(String userReference)
     {
-        UserReference userReference = this.userReferenceResolver.resolve(stringUserReference);
-        this.notify(new UserAuthenticatedEvent(userReference));
+        notify(this.stringResolver.resolve(userReference));
+    }
+
+    @Override
+    public void notify(DocumentReference userReference)
+    {
+        notify(this.documentResolver.resolve(userReference));
+    }
+
+    @Override
+    public void notify(UserReference userReference)
+    {
+        notify(new UserAuthenticatedEvent(userReference));
     }
 
     /**
@@ -74,9 +86,8 @@ public class UserAuthenticatedEventNotifier
      */
     private void notify(UserAuthenticatedEvent event)
     {
-        if (this.logger.isDebugEnabled()) {
-            this.logger.debug("User authenticated for [{}]", event.getUserReference());
-        }
+        this.logger.debug("User authenticated for [{}]", event.getUserReference());
+
         this.observationManager.notify(event, null);
     }
 }

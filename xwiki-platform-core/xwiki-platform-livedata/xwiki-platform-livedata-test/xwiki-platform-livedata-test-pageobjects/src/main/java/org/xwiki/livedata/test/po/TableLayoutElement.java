431aa80cf30d8d013515f5212162f8d505283cc3
Pierre Jeanjean <pierre.jeanjean@xwiki.com>
2026-08-06 13:58:14 +0200

    XWIKI-17627: Livedata Add entry from layout
    * Fix UI test on Chrome

--- xwiki-platform-core/xwiki-platform-livedata/xwiki-platform-livedata-test/xwiki-platform-livedata-test-pageobjects/src/main/java/org/xwiki/livedata/test/po/TableLayoutElement.java
+++ xwiki-platform-core/xwiki-platform-livedata/xwiki-platform-livedata-test/xwiki-platform-livedata-test-pageobjects/src/main/java/org/xwiki/livedata/test/po/TableLayoutElement.java
@@ -869,7 +869,10 @@ public class TableLayoutElement extends BaseElement
      */
     public void setNewEntryCell(String columnLabel, String fieldName, String value)
     {
-        setNewEntryCell(countRows(), columnLabel, fieldName, value);
+        WebElement input = setNewEntryCell(countRows(), columnLabel, fieldName, value);
+        // Ensure all editors have closed by clicking a fixed element outside the livedata.
+        getDriver().findElement(By.cssSelector("#document-title")).click();
+        getDriver().waitUntilCondition(ExpectedConditions.stalenessOf(input));
     }
 
     /**

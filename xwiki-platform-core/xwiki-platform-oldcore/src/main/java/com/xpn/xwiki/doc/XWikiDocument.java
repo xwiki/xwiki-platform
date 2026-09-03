8a30d50d3eae5be002e4476850d184f85276f1dd
Thomas Mortagne <thomas.mortagne@gmail.com>
2026-09-03 14:35:07 +0200

    XWIKI-24792: XWikiDocument#apply return false if the only changes are related to attachments

--- xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/doc/XWikiDocument.java
+++ xwiki-platform-core/xwiki-platform-oldcore/src/main/java/com/xpn/xwiki/doc/XWikiDocument.java
@@ -9537,7 +9537,7 @@ public class XWikiDocument implements DocumentModelBridge, Cloneable, Disposable
             modified = true;
         }
 
-        if (isHidden() != document.isHidden()) {
+        if (!Objects.equals(isHidden(), document.isHidden())) {
             setHidden(document.isHidden());
             modified = true;
         }
@@ -9601,6 +9601,7 @@ public class XWikiDocument implements DocumentModelBridge, Cloneable, Disposable
             for (XWikiAttachment attachment : new ArrayList<XWikiAttachment>(getAttachmentList())) {
                 if (document.getAttachment(attachment.getFilename()) == null) {
                     removeAttachment(attachment);
+                    modified = true;
                 }
             }
         }
@@ -9609,8 +9610,9 @@ public class XWikiDocument implements DocumentModelBridge, Cloneable, Disposable
             XWikiAttachment originalAttachment = getAttachment(attachment.getFilename());
             if (originalAttachment == null) {
                 addAttachment(attachment);
+                modified = true;
             } else {
-                originalAttachment.apply(attachment);
+                modified |= originalAttachment.apply(attachment);
             }
         }
 

a52a53a89823fa6683f2f0bca83a4b8f4c2a6411
Simon Urli <simon.urli@xwiki.com>
2026-09-15 18:08:15 +0200

    XWIKI-24886: Translation resources are read using wrong API
      * Use the proper ClassLoaderUtils#getResources
      * Provide test to cover it
    
    (cherry picked from commit bc314c51addae158585b1c36263b822856462f31)

--- xwiki-platform-core/xwiki-platform-localization/xwiki-platform-localization-sources/xwiki-platform-localization-source-jar/src/main/java/org/xwiki/localization/jar/internal/RootClassLoaderTranslationBundle.java
+++ xwiki-platform-core/xwiki-platform-localization/xwiki-platform-localization-sources/xwiki-platform-localization-source-jar/src/main/java/org/xwiki/localization/jar/internal/RootClassLoaderTranslationBundle.java
@@ -34,6 +34,7 @@ import javax.inject.Named;
 import javax.inject.Singleton;
 
 import org.apache.commons.collections.EnumerationUtils;
+import org.xwiki.classloader.internal.ClassLoaderUtils;
 import org.xwiki.component.annotation.Component;
 import org.xwiki.localization.TranslationBundleContext;
 import org.xwiki.localization.internal.AbstractCachedTranslationBundle;
@@ -108,7 +109,7 @@ public class RootClassLoaderTranslationBundle extends AbstractCachedTranslationB
 
         Enumeration<URL> urls;
         try {
-            urls = getClass().getClassLoader().getResources(resourceName);
+            urls = ClassLoaderUtils.getResources(getClass().getClassLoader(), resourceName);
         } catch (IOException e) {
             this.logger.error("Failed to get resource URLs from class loader for name [{}]", resourceName, e);
 
